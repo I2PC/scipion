c9ae429784abcf033e0dc0a19841548618d6dac5
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2002-06-28 17:54:21 +0000

    Denoising creation
    git-svn-id: file:///home/roberto/Git/svn@25 1a721e8d-d38c-4e91-ab98-6a0be4d754af

---
+++ trunk/Xmipp/Applications/Src/Denoising/denoising.cc
@@ -0,0 +1,121 @@
+/***************************************************************************
+ *
+ * Authors:     Carlos Oscar S. Sorzano (coss@cnb.uam.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * Copyright (c) 2000 , CSIC .
+ *
+ * Permission is granted to copy and distribute this file, for noncommercial
+ * use, provided (a) this copyright notice is preserved, (b) no attempt
+ * is made to restrict redistribution of this file, and (c) this file is
+ * restricted by a compilation copyright.
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.uam.es'
+ *
+ *****************************************************************************/
+
+#include <XmippData/xmippProgs.hh>
+#include <XmippData/xmippArgs.hh>
+#include <XmippData/xmippWavelets.hh>
+#include <XmippData/xmippHistograms.hh>
+
+#define REMOVE_SCALE 0
+#define SOFT_THRESHOLDING 1
+
+class Denoising_parameters: public Prog_parameters {
+public:
+   string DWT_type;
+   int    denoising_type;
+   int    scale;
+   double threshold;
+public:
+   void read(int argc, char **argv) _THROW {
+      Prog_parameters::read(argc,argv);
+      DWT_type=get_param(argc,argv,"-type","DAUB12");
+      if      (DWT_type=="DAUB4")  set_DWT_type(DAUB4);
+      else if (DWT_type=="DAUB12") set_DWT_type(DAUB12);
+      else if (DWT_type=="DAUB20") set_DWT_type(DAUB20);
+      else REPORT_ERROR(1,"DWT::read: Unknown DWT type");
+      string aux=get_param(argc,argv,"-denoising","remove_scales");
+      if (aux=="remove_scales") denoising_type=REMOVE_SCALE;
+      else if (aux=="soft_thresholding") denoising_type=SOFT_THRESHOLDING;
+      scale=AtoI(get_param(argc,argv,"-scale","0"));
+      threshold=AtoF(get_param(argc,argv,"-th","50"));
+   }
+   
+   void show() {
+      Prog_parameters::show();
+      cout << "DWT type: " << DWT_type << endl;
+      cout << "Denoising: ";
+      switch (denoising_type) {
+         case REMOVE_SCALE:
+            cout << " Remove scale " << scale << endl;
+            break;
+         case SOFT_THRESHOLDING:
+            cout << " Soft thresholding " << threshold << endl;
+            break;
+      }
+   }
+
+   void usage() {
+      Prog_parameters::usage();
+      cerr << "  [-type <str=DAUB12]        : DWT type. Valid types are:\n"
+           << "                               DAUB4, DAUB12, DAUB20\n"
+           << "  [-denoising <str=remove_scale] : Denoising action\n"
+           << "                               remove_scale\n"
+           << "                               soft_thresholding\n"
+           << "  [-scale <s=0>]             : scale to remove\n"
+           << "  [-threshold <th=50>]       : threshold of values (%) to remove\n"
+      ;
+   }
+};
+
+void process_img(ImageXmipp &img, const Prog_parameters *prm) {
+   Denoising_parameters *eprm=(Denoising_parameters *) prm;
+   DWT(img(),img());
+   double th;
+   histogram1D hist;
+   switch (eprm->denoising_type) {
+      case REMOVE_SCALE:
+         clean_quadrant(img(),eprm->scale, "01");
+         clean_quadrant(img(),eprm->scale, "10");
+         clean_quadrant(img(),eprm->scale, "11");
+         break;
+      case SOFT_THRESHOLDING:
+         compute_hist(img(),hist,100);
+         soft_thresholding(img(),hist.percentil(eprm->threshold));
+         break;
+   }
+   IDWT(img(),img());
+}
+
+void process_vol(VolumeXmipp &vol, const Prog_parameters *prm) {
+   Denoising_parameters *eprm=(Denoising_parameters *) prm;
+   DWT(vol(),vol());
+   double th;
+   histogram1D hist;
+   switch (eprm->denoising_type) {
+      case REMOVE_SCALE:
+         clean_quadrant(vol(),eprm->scale, "001");
+         clean_quadrant(vol(),eprm->scale, "010");
+         clean_quadrant(vol(),eprm->scale, "011");
+         clean_quadrant(vol(),eprm->scale, "100");
+         clean_quadrant(vol(),eprm->scale, "101");
+         clean_quadrant(vol(),eprm->scale, "110");
+         clean_quadrant(vol(),eprm->scale, "111");
+         break;
+      case SOFT_THRESHOLDING:
+         compute_hist(vol(),hist,100);
+         soft_thresholding(vol(),hist.percentil(eprm->threshold));
+         break;
+   }
+   IDWT(vol(),vol());
+}
+
+int main (int argc, char **argv) {
+   Denoising_parameters prm;
+   SF_main(argc, argv, &prm, (void*)&process_img, (void*)&process_vol);
+}
+
