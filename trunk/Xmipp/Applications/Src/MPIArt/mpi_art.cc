d9e46be9b09d302b1ee509c774ddfaff625ce0b3
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2002-11-14 14:53:48 +0000

    Comments added, MPI_Finalize for all processes, output volumes are saved as .vol
    git-svn-id: file:///home/roberto/Git/svn@133 1a721e8d-d38c-4e91-ab98-6a0be4d754af

--- trunk/Xmipp/Applications/Src/MPIArt/mpi_art.cc
+++ trunk/Xmipp/Applications/Src/MPIArt/mpi_art.cc
@@ -1,20 +1,28 @@
 /***************************************************************************
  *
- * Authors:     José Román Bilbao Castro (jrbcast@teleline.es)
+ * Authors:     Jose Roman Bilbao Castro (jrbcast@cnb.uam.es)
  *
  * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
  *
- * Copyright (c) 1999 , CSIC .
- *
- * Permission is granted to copy and distribute this file, for noncommercial
- * use, provided (a) this copyright notice is preserved, (b) no attempt
- * is made to restrict redistribution of this file, and (c) this file is
- * restricted by a compilation copyright.
- *
- *  All comments concerning this program package may be sent to the
- *  e-mail address 'xmipp@cnb.uam.es'
- *
- *****************************************************************************/
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+
 
 #include <Reconstruction/Programs/Prog_art.hh>
 #include <Reconstruction/Programs/Prog_art_crystal.hh>
@@ -34,6 +42,7 @@ typedef struct {
 	double wall;  /* Wall time. */
 } USWtime_t;
 
+// Gets User and System times for use with MPI
 void uswtime(USWtime_t *tm)
 { 
 	struct rusage buffer; 
@@ -52,25 +61,25 @@ int main (int argc, char *argv[]) {
    Basic_ART_Parameters   art_prm;
    Plain_ART_Parameters   eprm;
    Crystal_ART_Parameters crystal_art_prm;
-   VolumeXmipp            vol_voxels,vol_voxels_aux;
+   VolumeXmipp            vol_voxels,vol_voxels_aux;  // Volume to reconstruct
    GridVolume             vol_blobs;
    int                    crystal_mode;
-   MPI_Status		  status;
+   MPI_Status		  status;            	// Stores MPI directives status
    int 			  num_img_tot;		// The total amount of images 
    int			  num_img_node;		// Stores the total amount of images each node deals with
-   int 			  remaining;
-   int 			  Npart;
-   int 			  myFirst, myLast;	// Initial and finishing indexes for each node (absolute value)
-
-   double		  comms_t,aux_comm_t;		// Comunications time
+   int 			  remaining;         	// Number of projections still to compute
+   int 			  Npart;             	// Number of projection to process
+   int 			  myFirst, myLast;	// Limits projections to process
+   double		  comms_t,aux_comm_t;	// Comunications time
    double		  it_t;			// iteration time
    double 		  cav_t;		// time for CAV weights calculation
-   double 		  cavk_total_t,cavk_it_t;
+   double            	  cavk_it_t;         	// BiCAV weights calculation time (1 iter.)
+   double 		  cavk_total_t;      	// Sum( cavk_it_t )
    USWtime_t		  recons_t;		// Reconstruction time
    double		  total_t;		// Program execution time
-   double 		  comms_t_it,aux_t;		// communicattions time in one iteration
+   double 		  comms_t_it,aux_t;	// communicattions time in one iteration
 	
-   int 			  rank, size;	// MPI number of proccess and number of proccesses
+   int 			  rank, size;	     	// MPI number of proccess and number of proccesses
    
 	
 	// Init Parallel interface		
@@ -98,9 +107,9 @@ int main (int argc, char *argv[]) {
 	/*************************** PARAMETERS INITIALIZATION ***************************/
 	int num_iter = art_prm.no_it;
 	cavk_total_t = 0.0;
-	if( rank == 0 )
-	{  				// Master code
-		comms_t = 0.0;
+	if( rank == 0 ) // Master code
+	{  	
+		comms_t = 0.0; // Initializes time
 	
       	 	art_prm.produce_Side_Info(vol_blobs,FULL,rank);
 		eprm.produce_Side_Info(art_prm,vol_blobs);
@@ -161,13 +170,15 @@ int main (int argc, char *argv[]) {
 	// Print some data
 	if( rank == 0 ){
 		if      ( art_prm.parallel_mode == Basic_ART_Parameters::SART ){ 
-	             if (art_prm.block_size < size ) art_prm.block_size = size; // Assures that each processor will have at least one projection
+	             if (art_prm.block_size < size ) art_prm.block_size = size; // Each processor will have at least one projection
+		     if (art_prm.block_size > num_img_tot ) art_prm.block_size = num_img_tot; // block_size is as much equal to num_img_tot
 		     cout << "SART " << "TB = " << art_prm.block_size << endl;
 		}
 		else if ( art_prm.eq_mode == CAV )
 		     cout << "CAV" << endl;
 		else if ( art_prm.parallel_mode == Basic_ART_Parameters::BiCAV ){
-		     if (art_prm.block_size < size ) art_prm.block_size = size; // Assures that each processor will have at least one projection
+		     if (art_prm.block_size < size ) art_prm.block_size = size; // Each processor will have at least one projection
+		     if (art_prm.block_size > num_img_tot ) art_prm.block_size = num_img_tot; // block_size is as much equal to num_img_tot 
 		     cout << "BiCAV " << "TB = " << art_prm.block_size << endl;
 	        }
 		else if ( art_prm.parallel_mode == Basic_ART_Parameters::SIRT )
@@ -181,6 +192,8 @@ int main (int argc, char *argv[]) {
 		cout << "________________________________________________________________________________\n\n" << endl;
 	}
 	
+	art_prm.block_size /= size; // Now this variable stores how many projs. from each block belong to each node.
+	
 	/*************************** CAV weights precalculation *************************/
 	if ( art_prm.eq_mode == CAV ){
 		
@@ -189,7 +202,7 @@ int main (int argc, char *argv[]) {
 		
 		/*
 		EXTRA CALCULATIONS FOR CAV WEIGHTS: Each node computes its own part related to its images
-		and after that send each others their results and sum up them. This part of code has been taken and
+		and after that they send each other their results and sum up them. This part of code has been taken and
 		modified from Basic_art.cc produce_side_info().
 		*/
 		cav_t = MPI_Wtime();
@@ -243,6 +256,8 @@ int main (int argc, char *argv[]) {
 			
 				blocksize = art_prm.numIMG * size;
 				
+				// All processors send their result and get the other's so all of them
+				// have the same volume for the next step.
 				for ( int j = 0 ; j < vol_blobs.VolumesNo() ; j++)
 				{
 					vol_blobs(j)() = vol_blobs(j)()-vol_aux2(j)(); // Adapt result to parallel ennvironment from sequential routine
@@ -257,7 +272,7 @@ int main (int argc, char *argv[]) {
 			}
 	        }
 		else if( art_prm.parallel_mode==Basic_ART_Parameters::BiCAV ){
-			// Creates and initializes special variables needed to CAVK weights computation.
+			// Creates and initializes special variables needed to BICAV weights computation.
 			GridVolumeT<int> GVNeq_aux; // This is a buffer for communications
 			
 			int numsteps = Npart / art_prm.block_size;
@@ -275,7 +290,7 @@ int main (int argc, char *argv[]) {
 				art_prm.ordered_list.startingX()= -processed;	
 				
 			        /*
-				EXTRA CALCULATIONS FOR CAVK WEIGHTS: Each node computes its own part related to its images
+				EXTRA CALCULATIONS FOR BICAV WEIGHTS: Each node computes its own part related to its images
 				and after that send each others their results and sum up them. This part of code has been taken and
 				modified from Basic_art.cc produce_side_info().
 				*/
@@ -284,6 +299,8 @@ int main (int argc, char *argv[]) {
                                 art_prm.compute_CAV_weights(vol_blobs,art_prm.numIMG);
 				GVNeq_aux = *(art_prm.GVNeq);
 				 
+				// All processors send their result and get the other's so all of them
+				// have the weights.
 				for ( int n = 0 ; n < (art_prm.GVNeq)->VolumesNo(); n++ )
 				{
 					aux_comm_t = MPI_Wtime();
@@ -302,6 +319,8 @@ int main (int argc, char *argv[]) {
 				
 				processed += art_prm.numIMG;
 				
+				// All processors send their result and get the other's so all of them
+				// have the same volume for the next step.
 				for ( int j = 0 ; j < vol_blobs.VolumesNo() ; j++)
 				{
 					vol_blobs(j)() = vol_blobs(j)()-vol_aux2(j)(); // Adapt result to parallel ennvironment from sequential routine
@@ -317,12 +336,14 @@ int main (int argc, char *argv[]) {
 		}
 		else if( art_prm.eq_mode == CAV){
 		
-			//CAV weights calculations are done before iterations begin in order to avoid recalculate them
+			// CAV weights calculations are done before iterations begin in order to avoid recalculate them
 			for ( int j = 0 ; j < vol_blobs.VolumesNo() ; j++)
 				vol_aux2(j)() = vol_blobs(j)();
 				
 			Basic_ART_iterations(art_prm, eprm, vol_blobs, rank);
 				
+			// All processors send their result and get the other's so all of them
+			// have the same volume for the next step.
 			for ( int j = 0 ; j < vol_blobs.VolumesNo() ; j++)
 			{
 				vol_blobs(j)() = vol_blobs(j)()-vol_aux2(j)(); // Adapt result to parallel ennvironment from sequential routine
@@ -341,8 +362,11 @@ int main (int argc, char *argv[]) {
 			
 			Basic_ART_iterations(art_prm, eprm, vol_blobs, rank);
 		
+			// All processors send their result and get the other's so all of them
+			// have the same volume for the next step.
 			for ( int j = 0 ; j < vol_blobs.VolumesNo() ; j++)
 			{
+			        // SIRT Alg. needs to store previous results but AVSP doesn't
 				if(art_prm.SIRT)
 				{
 					vol_blobs(j)() = vol_blobs(j)()-vol_aux2(j)(); // Adapt result to parallel ennvironment from sequential routine
@@ -353,6 +377,7 @@ int main (int argc, char *argv[]) {
 				aux_t = MPI_Wtime() - aux_comm_t;
 				comms_t += aux_t;
 				comms_t_it += aux_t;
+				// SIRT uses the previously stored volume
 				if( !art_prm.SIRT )
 				{
 					vol_blobs(j)() = vol_blobs_aux(j)();
@@ -362,6 +387,8 @@ int main (int argc, char *argv[]) {
 			}
 		}
 		cavk_total_t += cavk_it_t;
+		
+		// Only the proccess with rank=0 (Master) will output the results
 		if ( rank == 0 ){
 			cout << "\nIteration " << i << endl;
 			cout << "Time: " << MPI_Wtime() - it_t << " secs." << endl; 
@@ -378,11 +405,10 @@ int main (int argc, char *argv[]) {
 				    art_prm.projXdim:art_prm.Zoutput_volume_size;
 		 		blobs2voxels(vol_blobs, art_prm.blob, &vol_voxels, art_prm.D, 
         			Zoutput_volume_size, Youtput_volume_size, Xoutput_volume_size);
-        			vol_voxels.write(art_prm.fn_root+"it"+ItoA(i+1)+".spi");
-			}
-			if ((art_prm.tell&TELL_SAVE_BLOBS) && (i < num_iter-1 ))
- 			{
-				vol_blobs.write(art_prm.fn_root+"it"+ItoA(i+1)+".blob");
+        			vol_voxels.write(art_prm.fn_root+"it"+ItoA(i+1)+".vol");
+			
+			        if ((art_prm.tell&TELL_SAVE_BLOBS) && (i < num_iter-1 ))
+ 					vol_blobs.write(art_prm.fn_root+"it"+ItoA(i+1)+".blob");
 			}
 		}
 	}	
@@ -391,7 +417,8 @@ int main (int argc, char *argv[]) {
 		
 	if( rank > 0 ){
 		art_prm.fh_hist.close();			
-		return 0;
+	        MPI_Finalize();	  // Must exist for each proccess on MPI evironment
+        	return 0;
 	}
 		
  	int Xoutput_volume_size=(art_prm.Xoutput_volume_size==0) ?
@@ -403,10 +430,10 @@ int main (int argc, char *argv[]) {
 			 		
 	blobs2voxels(vol_blobs, art_prm.blob, &vol_voxels, art_prm.D,Zoutput_volume_size, Youtput_volume_size, Xoutput_volume_size);
  		
-	vol_voxels.write(art_prm.fn_root+".spi");
+	vol_voxels.write(art_prm.fn_root+".vol");
 		
 	if (art_prm.tell&TELL_SAVE_BLOBS) 
-	vol_blobs.write(art_prm.fn_root+".blob");
+	    vol_blobs.write(art_prm.fn_root+".blob");
 
 	art_prm.fh_hist.close();			
    	uswtime( &recons_t );
