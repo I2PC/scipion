cff80a87db00a834bb63b5c8726aebe9347dbd1a
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2003-02-19 18:01:09 +0000

    program creation
    git-svn-id: file:///home/roberto/Git/svn@215 1a721e8d-d38c-4e91-ab98-6a0be4d754af

---
+++ trunk/Xmipp/Applications/Src/Phantom_transform/phantom_transform.cc
@@ -0,0 +1,139 @@
+/***************************************************************************
+ *
+ * Authors:     Carlos Oscar S. Sorzano (coss@cnb.uam.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+
+#include <XmippData/xmippProgs.hh>
+#include <XmippData/xmippArgs.hh>
+#include <XmippData/xmippGeometry.hh>
+#include <Reconstruction/phantom.hh>
+
+class Phantom_transform_parameters {
+public:
+   FileName fn_in, fn_out;
+   bool Euler_mode;
+      double rot, tilt, psi;
+   bool Align_mode;
+   bool Axis_mode;
+      matrix1D<double> axis;
+      double ang;
+   matrix1D<double> shift;
+   matrix1D<double> scale;
+   
+   matrix2D<double> A3D;
+
+   void read(int argc, char **argv) _THROW {
+      fn_in=get_param(argc,argv,"-i");
+      fn_out=get_param(argc,argv,"-o");
+      Euler_mode=Align_mode=Axis_mode=FALSE;
+      if (check_param(argc,argv,"-euler")) {
+         Euler_mode=TRUE;
+         int i=position_param(argc,argv,"-euler");
+         if (i+3>=argc)
+            REPORT_ERROR(1,"Not enough parameters after -euler");
+         rot  = AtoF(argv[i+1]);
+         tilt = AtoF(argv[i+2]);
+         psi  = AtoF(argv[i+3]);
+         A3D=Euler_rot3D_matrix(rot,tilt,psi);
+      } else if (check_param(argc,argv,"-align_with_Z")) {
+         Align_mode=TRUE;
+         axis=get_vector_param(argc,argv,"-align_with_Z",3);
+         A3D=align_with_Z(axis);
+      } else if (check_param(argc,argv,"-axis")) {
+         Axis_mode=TRUE;
+         axis=get_vector_param(argc, argv, "-axis", 3);
+         ang=AtoF(get_param(argc, argv, "-ang"));
+         A3D=rot3D_matrix(ang,axis);
+      } else
+         A3D.init_identity(4);
+      if (check_param(argc,argv,"-shift"))
+         shift=get_vector_param(argc,argv,"-shift",3);
+      else shift.init_zeros(3);
+      if (check_param(argc,argv,"-scale"))
+         scale=get_vector_param(argc,argv,"-scale",3);
+      else {scale.resize(3); scale.init_constant(1);}
+      
+      // Apply shift
+      A3D(0,3)=XX(shift);
+      A3D(1,3)=YY(shift);
+      A3D(2,3)=ZZ(shift);
+
+      // Apply scale
+      A3D(0,0)*=XX(scale); A3D(0,1)*=YY(scale); A3D(0,2)*=ZZ(scale);
+      A3D(1,0)*=XX(scale); A3D(1,1)*=YY(scale); A3D(1,2)*=ZZ(scale);
+      A3D(2,0)*=XX(scale); A3D(2,1)*=YY(scale); A3D(2,2)*=ZZ(scale);
+}
+   
+   void show() {
+      cout << "Input file : " << fn_in  << endl
+           << "Output file: " << fn_out << endl;
+      if (Euler_mode)
+         cout << "Euler angles (rot, tilt, psi): " << rot << " " << tilt
+              << " " << psi << endl;
+      else if (Align_mode)
+         cout << "Aligning " << axis.transpose() << " with Z\n";
+      else if (Axis_mode)
+         cout << "Rotating " << ang << " degrees around " << axis.transpose()
+              << endl;
+   }
+
+   void usage() {
+      cerr << "Usage: phantom_transform [Options]\n"
+           << "   -i <input filename>              : Phantom description file\n"
+	   << "   -o <output filename>             : Phantom description file\n";
+      cerr << "  [-euler <rot> <tilt> <psi>        : Rotate with these Euler angles\n"
+           << "  [-align_with_Z [<x>,<y>,<z>]]     : Align (x,y,z) with Z\n"
+           << "                                      Notice that brackets for the\n"
+           << "                                      vector must be written and do not\n"
+           << "                                      represent optional parameters\n"
+           << "  [-axis [<x>,<y>,<z>] -ang <ang>]  : Rotate <ang> degrees around (x,y,z),\n"
+           << "                                      by default (0,0,1)\n"
+           << "  [-shift [<x>,<y>,<z>]             : Shift vector\n"
+           << "  [-scale [<x>,<y>,<z>]             : Scale vector\n"
+     ;
+   }
+};
+
+bool process_phantom(const FileName &fn_in, const FileName &fn_out,
+   const Phantom_transform_parameters *prm) {
+   Phantom P;
+   P.read(fn_in,false); // Read phantom without applying scale
+   P.self_apply_geom(prm->A3D,IS_NOT_INV);
+   P.write(fn_out);
+   return TRUE;
+}
+
+int main (int argc, char **argv) {
+   Phantom_transform_parameters prm;
+   try {
+      prm.read(argc,argv);
+   } catch (Xmipp_error XE) {
+      cout << XE; prm.usage();exit(1);
+   }
+   try {
+      prm.show();
+      process_phantom(prm.fn_in,prm.fn_out,&prm);
+   } catch (Xmipp_error XE) {
+      cout << XE;
+   }
+}
