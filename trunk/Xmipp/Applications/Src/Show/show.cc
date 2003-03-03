27701c9a6932cab890db34eedb95ab443069bf36
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2003-03-03 14:49:56 +0000

    Program creation
    git-svn-id: file:///home/roberto/Git/svn@245 1a721e8d-d38c-4e91-ab98-6a0be4d754af

---
+++ trunk/Xmipp/Applications/Src/Show/show.cc
@@ -0,0 +1,93 @@
+/***************************************************************************
+ *
+ * Authors:     Alberto Pascual Montano (pascual@cnb.uam.es)
+ *              Carlos Oscar Sanchez Sorzano (coss@cnb.uam.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+
+#include <XmippData/xmippArgs.hh>
+#include <XmippGraphics/show2D.hh>
+#include <XmippGraphics/showSel.hh>
+#include <XmippGraphics/showVol.hh>
+#include <qapplication.h>
+
+void Usage();
+
+int main( int argc, char **argv ) {
+    int numCols;
+    int numRows;
+    int  mode;
+    bool poll;
+    try {
+       if (check_param(argc,argv,"-img"))       mode=0;
+       else if (check_param(argc,argv,"-sel"))  mode=1;
+       else if (check_param(argc,argv,"-vol"))  mode=2;
+       else
+          REPORT_ERROR(1,"No mode (img/sel/vol) supplied");
+       numCols = AtoI(get_param(argc, argv, "-w", "10"));
+       numRows = AtoI(get_param(argc, argv, "-h", "10"));
+       poll=check_param(argc,argv,"-poll");
+   } catch (Xmipp_error) {Usage(); exit(1);}
+
+   QApplication::setFont( QFont("Helvetica", 12) );
+   QApplication a(argc,argv);
+
+   for ( int i=1; i<argc; i++ ) {
+       if (!exists(argv[i])) {
+          // Check that the last letter is not x or y
+	  FileName fn=argv[i];
+	  if (fn[fn.length()-1]=='x' || fn[fn.length()-1]=='y') {
+	     fn=fn.substr(0,fn.length()-1);
+	     if (!exists(fn.c_str())) continue;
+	  } else continue;
+       } 
+       if (mode==0) {
+          ImageViewer *showimg = new ImageViewer(argv[i], poll);
+          showimg->loadImage( argv[i] );
+          showimg->show();
+       } else if (mode==1) {
+          ShowSel *showsel = new ShowSel;
+	  showsel->initWithFile(numRows, numCols, argv[i]);
+	  showsel->show();
+       } else if (mode==2) {
+          ShowVol *showvol = new ShowVol;
+	  if (poll) showvol->setPoll();
+	  showvol->initWithFile(numRows, numCols, argv[i]);
+	  showvol->show();
+       }
+   }
+
+   QObject::connect(qApp, SIGNAL(lastWindowClosed()), qApp, SLOT(quit()));
+   return a.exec();
+}
+
+void Usage() {
+    cout << "Usage: showsel [options]\n"
+         << "    -img <images> |    : Input images\n"
+         << "    -sel <selfiles> |  : Input selfiles\n"
+         << "    -vol <XmippVolumes>: Add x or y to the filename\n"
+	 << "                         to see slices in that direction\n"
+         << "   [-w]                : width (default: 10)\n"
+         << "   [-h]                : height (default: 10)\n"
+         << "   [-poll]             : check file change, only for volumes\n"
+    ;
+}
