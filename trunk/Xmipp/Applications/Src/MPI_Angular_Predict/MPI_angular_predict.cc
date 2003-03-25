5cede3470ce71cce989a3d9c7d0a5321281cd827
jrbcast <jrbcast@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2003-03-25 09:28:50 +0000

    First test version
    git-svn-id: file:///home/roberto/Git/svn@282 1a721e8d-d38c-4e91-ab98-6a0be4d754af

--- trunk/Xmipp/Applications/Src/MPI_Angular_Predict/MPI_angular_predict.cc
+++ trunk/Xmipp/Applications/Src/MPI_Angular_Predict/MPI_angular_predict.cc
@@ -1,6 +1,6 @@
 /***************************************************************************
  *
- * Authors:     Jose Roman Bilbao Castro (coss@cnb.uam.es)
+ * Authors:     Jose Roman Bilbao Castro (jrbcast@cnb.uam.es)
  *
  * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
  *
@@ -24,17 +24,21 @@
  ***************************************************************************/
 
 #include <Reconstruction/Programs/Prog_Angular_Predict.hh>
+#include <XmippData/xmippImages.hh>
 #include <mpi.h>
 
 int main (int argc, char **argv) {
-SF ImagesFile;
+SelFile ImagesFile;
 FileName fn_sym;
 int MyFirst, MyLast;	// First and last file to process each node
 Prog_angular_predict_prm prm;
 double shiftX, shiftY, psi, rot, tilt;
 int remaining;
-int NPart;
-   	
+int Npart;
+int rank, size, Img_Nbr, myFirst, myLast;
+MPI_Status  status;            	// Stores MPI directives status
+double buff[8];
+   		   	
 	MPI_Init(&argc, &argv);
    	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -43,8 +47,10 @@ int NPart;
 	ImagesFile.read( prm.fn_in );
 	Img_Nbr = prm.get_images_to_process();
 	
-	Npart = (int) ceil ((float)Img_Nbr / (float)size);
-	remaining = num_img_tot % size;
+//	Npart = (int) ceil ((float)Img_Nbr / (float)size);
+	Npart = (int) ((float)Img_Nbr / (float)size);
+
+	remaining = Img_Nbr % size;
 	
 	if( !remaining || rank < remaining)
         	myFirst = rank * Npart;
@@ -52,52 +58,56 @@ int NPart;
         	myFirst = rank * Npart - (rank - remaining) - 1;
 
      	myLast = myFirst + Npart - 1;
-	 
-	for( int i = MyFirst; i <= MyLast; i++)
-	{
-		XmippImage image;
-		image.read(ImagesFile[i].fn_proj);
-     		double corr = prm->predict_angles( image, shiftX, shiftY, rot, tilt, psi);
-	} 
 	
-	if( rank == 0){
-		// Receives results from others and stores values in a file.
-		waiting_for = Img_Nbr - MyLast; 
-		MPI_DOUBLE buff[8];
-		while( waiting_for > 0){
-			MPI_Recv( buff, 8, MPI_DOUBLE, MPI_ANY, MPI_ANY, MPI_WORLD_COMM, &status );
-			waiting_for--;
-		}
-	   	// Save predicted angles
-   		int p = (prm.predicted_rot).size();
-   		DocFile DF;
+	int p = (prm.predicted_rot).size();	
+   	 
+	if(rank == 0)
+	{	
+  		matrix1D<double> v(7);
+ 		DocFile DF;
    		DF.reserve(p+1);
-   		DF.append_comment("Predicted_Rot Predicted_Tilt Predicted_Psi Predicted_ShiftX Predicted_ShiftY Corr");
-   		matrix1D<double> v(7);
-   		for (int i=0; i<p; i++) {
-      			v(0) = prm.predicted_rot[i];
-      			v(1) = prm.predicted_tilt[i];
-      			v(2) = prm.predicted_psi[i];
-      			v(3) = prm.predicted_shiftX[i];
-      			v(4) = prm.predicted_shiftY[i];
-      			v(5) = prm.predicted_corr[i];
-      			DF.append_data_line(v);
-   		}
-	   	DF.write(prm.fn_out_ang);
+   		DF.append_comment("Predicted_Rot Predicted_Tilt Predicted_Psi Predicted_ShiftX Predicted_ShiftY Corr");		
+		
+		int to_save = Img_Nbr;
+		while( to_save > 0)
+		{
+			MPI_Recv( buff, 8, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
+			   	
+      			v(0) = buff[ 0 ];
+      			v(1) = buff[ 1 ];
+      			v(2) = buff[ 2 ];
+      			v(3) = buff[ 3 ];
+      			v(4) = buff[ 4 ];
+      			v(5) = buff[ 5 ];
+		
+			// position in the file
+			int position = (int)buff[ 6 ];
+			DF.go_first_data_line();
+			DF.jump( position );
+	      		DF.insert_data_line(v);	
+			to_save --;	
+		}	
+		DF.write(prm.fn_out_ang);
 	}
-   	else{
-	   	// Sends predicted angles
-   		MPI_DOUBLE buff[8];
-   		for (int i=myFirst; i<=MyLast; i++) {
-      			buff[0] = prm.predicted_rot[i];
+	else
+	{
+		ImageXmipp image;
+		ImagesFile.go_beginning();
+		ImagesFile.jump(MyFirst); 
+		for( int i = MyFirst; i <= MyLast; i++)
+		{
+			image.read(ImagesFile.get_current_file());
+			double corr = prm.predict_angles( image, shiftX, shiftY, rot, tilt, psi);
+			buff[0] = prm.predicted_rot[i];
       			buff[1] = prm.predicted_tilt[i];
       			buff[2] = prm.predicted_psi[i];
       			buff[3] = prm.predicted_shiftX[i];
       			buff[4] = prm.predicted_shiftY[i];
       			buff[5] = prm.predicted_corr[i];
 			buff[6] = i;
-			MPI_Send(buff, 8, MPI_DOUBLE, 0, 0, MPI_WORLD_COMM);
-   		}
+			MPI_Send(buff, 8, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD );
+   			ImagesFile.next();
+		}
 	}
 	
         MPI_Finalize();	
