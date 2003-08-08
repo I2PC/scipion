ae3002b3795b4185edc26194d9bf67ceb2ee67ac
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2003-08-08 06:36:39 +0000

    Now TIFF 16 bits can be read. Reformatting tabs in the text
    git-svn-id: file:///home/roberto/Git/svn@358 1a721e8d-d38c-4e91-ab98-6a0be4d754af

--- trunk/Xmipp/Applications/Src/Tiff2Raw/tiff2raw.cc
+++ trunk/Xmipp/Applications/Src/Tiff2Raw/tiff2raw.cc
@@ -48,23 +48,23 @@
 //
 */
 int initFile( char *filename, size_t len ) {
-	unsigned char* 	buffer = NULL;
-	size_t         	i;
-	FILE *					fd;
+  unsigned char*  buffer = NULL;
+  size_t          i;
+  FILE *                                  fd;
 
-	buffer = (unsigned char*)malloc( sizeof(unsigned char) * 100000 );
-        for (int i=0; i<100000; i++) buffer[i]=0;
-	if ( buffer == NULL ) return( -1 );
+  buffer = (unsigned char*)malloc( sizeof(unsigned char) * 100000 );
+  for (int i=0; i<100000; i++) buffer[i]=0;
+  if ( buffer == NULL ) return( -1 );
 
-	fd = fopen( filename, "w" );
+  fd = fopen( filename, "w" );
 
   for (i=0; i< len/100000; i++)
       fwrite( buffer, sizeof(unsigned char), 100000, fd );
-	fwrite( buffer, sizeof(unsigned char), len%100000 , fd );
+  fwrite( buffer, sizeof(unsigned char), len%100000 , fd );
 
-	fclose( fd );
+  fclose( fd );
 
-	return( 0 );
+  return( 0 );
 }
 
 /*
@@ -74,27 +74,50 @@ int initFile( char *filename, size_t len ) {
 // write the content of a tile from a TIFF file to an image array
 //
 */
-void rawWriteTile( unsigned char* raw_buf, unsigned char* tif_buf, 
-    		  unsigned int x, unsigned int y,
-		  unsigned int imageWidth, unsigned int imageLength, 
-		  unsigned int tileWidth, unsigned int tileLength,
-                  unsigned int *minval, unsigned int *maxval) {
-	unsigned int 	i, j;
-	unsigned int 	x_max = x + tileWidth,
-			y_max = y + tileLength;
+void rawWriteTile(
+   unsigned char* raw_buf, unsigned char* tif_buf, 
+   unsigned int x, unsigned int y,
+   unsigned int imageWidth, unsigned int imageLength, 
+   unsigned int tileWidth, unsigned int tileLength,
+   unsigned short bitsPerSample, int byte_swapped,
+   unsigned int *minval, unsigned int *maxval) {
+   unsigned int i, j;
+   unsigned int x_max = x + tileWidth,
+                y_max = y + tileLength;
 
-	if ( x_max > imageWidth )  x_max = imageWidth;
-	if ( y_max > imageLength ) y_max = imageLength;
+   if ( x_max > imageWidth )  x_max = imageWidth;
+   if ( y_max > imageLength ) y_max = imageLength;
 
-	for( j = y; j < y_max; j++ )
-		for( i = x; i < x_max; i++) {
-                        unsigned char val=
-                           ((unsigned char*)tif_buf)[(j-y)*tileWidth+(i-x)];
-                       /* printf("%d %d %d\n",val,minval,maxval);*/
-                        if (val<*minval) *minval=val;
-                        if (val>*maxval) *maxval=val;
-			raw_buf[j*imageWidth + i] = val;
-                }
+   for ( j = y; j < y_max; j++ )
+       for ( i = x; i < x_max; i++) {
+            unsigned int val;
+            switch (bitsPerSample) {
+               case  8:
+                  val=(unsigned int)(tif_buf[(j-y)*tileWidth+(i-x)]);
+                  break;
+               case 16:
+                  if (!byte_swapped)
+                     val=256*tif_buf[((j-y)*tileWidth+(i-x))*2+1]+
+                             tif_buf[((j-y)*tileWidth+(i-x))*2];
+                  else
+                     val=256*tif_buf[((j-y)*tileWidth+(i-x))*2]+
+                             tif_buf[((j-y)*tileWidth+(i-x))*2+1];
+                  break;      
+            }
+            if (val<*minval) *minval=val;
+            if (val>*maxval) *maxval=val;
+            switch (bitsPerSample) {
+               case  8:
+                  raw_buf[j*imageWidth + i] = (unsigned char) val;
+                  break;
+               case 16:
+                  raw_buf[(j*imageWidth + i)*2]   =
+                     (unsigned char) (val & 0xFF);
+                  raw_buf[(j*imageWidth + i)*2+1] =
+                     (unsigned char) ((val>>8) & 0xFF);
+                  break;
+            }
+       }
 }
 
 /*
@@ -109,83 +132,86 @@ void rawWriteTile( unsigned char* raw_buf, unsigned char* tif_buf,
 */
 int main(int argc, char *argv[])
 {
-  TIFF*          	tif     = NULL;
-	unsigned char*	tif_buf = NULL;
-	int							raw_fd;
-	unsigned char*  raw_buf = NULL;
-	FILE*						inf     = NULL;
+  TIFF*           tif     = NULL;
+  unsigned char*  tif_buf = NULL;
+  int             raw_fd;
+  unsigned char*  raw_buf = NULL;
+  FILE*           inf     = NULL;
 
-  unsigned short	bitsPerSample;
-  unsigned short	samplesPerPixel;
-  unsigned int		imageWidth;
-  unsigned int		imageLength;
-	unsigned int		tileWidth;
-	unsigned int		tileLength;
-	size_t          len;
+  unsigned short  bitsPerSample;
+  unsigned short  samplesPerPixel;
+  unsigned int	  imageWidth;
+  unsigned int	  imageLength;
+  unsigned int    tileWidth;
+  unsigned int    tileLength;
+  int             byte_swapped;
+  size_t          len;
 
-	unsigned int		x, y;
-        unsigned int    minval, maxval;
+  unsigned int    x, y;
+  unsigned int    minval, maxval;
 
-	if ( argc < 3 ) {
-		fprintf( stderr, "Usage: tiff2raw inputfile outputfile\n" );
-		exit(-1);
+  if ( argc < 3 ) {
+     fprintf( stderr, "Usage: tiff2raw inputfile outputfile\n" );
+     exit(-1);
   }
 
-         minval=255;
-         maxval=0;
+  minval=65535;
+  maxval=0;
 
-	/* Open TIFF image */
-	tif = TIFFOpen( argv[1], "r" );
-	if ( tif == NULL ) exit( -1 );
+  /* Open TIFF image */
+  tif = TIFFOpen( argv[1], "r" );
+  if ( tif == NULL ) exit( -1 );
 
-	/* Get TIFF image properties */
-	TIFFGetField( tif, TIFFTAG_BITSPERSAMPLE, &bitsPerSample );
-	TIFFGetField( tif, TIFFTAG_SAMPLESPERPIXEL, &samplesPerPixel );
-	TIFFGetField( tif, TIFFTAG_IMAGEWIDTH, &imageWidth );
-	TIFFGetField( tif, TIFFTAG_IMAGELENGTH, &imageLength );
-	TIFFGetField( tif, TIFFTAG_TILEWIDTH, &tileWidth );
-	TIFFGetField( tif, TIFFTAG_TILELENGTH, &tileLength );
-        
-	/* Calculates the TIFF image size */
-	len = (imageLength * imageWidth * bitsPerSample * samplesPerPixel)/8;
-        initFile( argv[2], len );
+  /* Get TIFF image properties */
+  TIFFGetField( tif, TIFFTAG_BITSPERSAMPLE,   &bitsPerSample );
+  TIFFGetField( tif, TIFFTAG_SAMPLESPERPIXEL, &samplesPerPixel );
+  TIFFGetField( tif, TIFFTAG_IMAGEWIDTH,      &imageWidth );
+  TIFFGetField( tif, TIFFTAG_IMAGELENGTH,     &imageLength );
+  TIFFGetField( tif, TIFFTAG_TILEWIDTH,       &tileWidth );
+  TIFFGetField( tif, TIFFTAG_TILELENGTH,      &tileLength );
+  byte_swapped=TIFFIsByteSwapped( tif );
+  
+  /* Calculates the TIFF image size */
+  len = (imageLength * imageWidth * bitsPerSample * samplesPerPixel)/8;
+  initFile( argv[2], len );
 
-	/* Mapping from file to memory (it will save primary memory) */
-	raw_fd  = open( argv[2], O_RDWR );
-	raw_buf = (unsigned char*)mmap( NULL, len, PROT_READ|PROT_WRITE, MAP_SHARED, 
-																	raw_fd, 0 );
+  /* Mapping from file to memory (it will save primary memory) */
+  raw_fd  = open( argv[2], O_RDWR );
+  raw_buf = (unsigned char*)mmap( NULL, len, PROT_READ|PROT_WRITE, MAP_SHARED, 
+                                                                                                                                  raw_fd, 0 );
+  /* Writes the TIFF image properties to the .inf file */
+  inf     = fopen( strcat( argv[2], ".inf" ), "w" );
+  fprintf( inf, "# Bits per sample\n" );
+  fprintf( inf, "bitspersample= %d\n", bitsPerSample );
+  fprintf( inf, "# Samples per pixel\n" );
+  fprintf( inf, "samplesperpixel= %d\n", samplesPerPixel );
+  fprintf( inf, "# Image width\n" );
+  fprintf( inf, "Xdim= %d\n", imageWidth );
+  fprintf( inf, "# Image length\n" );
+  fprintf( inf, "Ydim= %d\n", imageLength );
 
-	/* Writes the TIFF image properties to the .inf file */
-	inf     = fopen( strcat( argv[2], ".inf" ), "w" );
-	fprintf( inf, "# Bits per sample\n" );
-	fprintf( inf, "bitspersample= %d\n", bitsPerSample );
-	fprintf( inf, "# Samples per pixel\n" );
-	fprintf( inf, "samplesperpixel= %d\n", samplesPerPixel );
-	fprintf( inf, "# Image width\n" );
-	fprintf( inf, "Xdim= %d\n", imageWidth );
-	fprintf( inf, "# Image length\n" );
-	fprintf( inf, "Ydim= %d\n", imageLength );
+  /* Start to convert the TIFF image to the RAW file */
+  tif_buf = (unsigned char*)_TIFFmalloc( TIFFTileSize(tif) );
+  
+  for ( y = 0; y < imageLength; y +=tileLength )
+      for ( x = 0; x < imageWidth; x += tileWidth ) {
+              TIFFReadTile( tif, tif_buf, x, y, 0, 0 );
+              rawWriteTile( raw_buf, tif_buf, x, y, 
+                 imageWidth, imageLength,
+                 tileWidth, tileLength,
+                 bitsPerSample, byte_swapped,
+                 &minval, &maxval );
+      }
 
-	/* Start to convert the TIFF image to the RAW file */
-	tif_buf = (unsigned char*)_TIFFmalloc( TIFFTileSize(tif) );
-        
-	for( y = 0; y < imageLength; y +=tileLength )
-		for( x = 0; x < imageWidth; x += tileWidth ) {
-			TIFFReadTile( tif, tif_buf, x, y, 0, 0 );
-			rawWriteTile( raw_buf, tif_buf, x, y, 
-			   imageWidth, imageLength,
-			   tileWidth, tileLength, &minval, &maxval );
-		}
+  _TIFFfree( tif_buf );
+  munmap( (char *) raw_buf, len );
 
-	_TIFFfree( tif_buf );
-	munmap( (char *) raw_buf, len );
+  TIFFClose( tif );
+  close( raw_fd );
 
-	TIFFClose( tif );
-	close( raw_fd );
-
-	fprintf( inf, "# Minimum value=%d\n",minval );
-	fprintf( inf, "# Maximum value=%d\n",maxval );
-	fclose( inf );
+  fprintf( inf, "# Minimum value=%d\n",minval );
+  fprintf( inf, "# Maximum value=%d\n",maxval );
+  fclose( inf );
 }
 
 /* Colimate menu =========================================================== */
