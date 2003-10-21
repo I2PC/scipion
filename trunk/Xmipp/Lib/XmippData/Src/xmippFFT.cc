d8a0e4670b755f1488b427128756c74c2076f86f
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2003-10-21 08:07:08 +0000

    VTK->Bilib FFT
    git-svn-id: file:///home/roberto/Git/svn@431 1a721e8d-d38c-4e91-ab98-6a0be4d754af

---
+++ trunk/Xmipp/Lib/XmippData/Src/xmippFFT.cc
@@ -0,0 +1,468 @@
+/***************************************************************************
+ *
+ * Authors:     Carlos Oscar S. Sorzano (coss@cnb.uam.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+
+#include "../xmippFFT.hh"
+#include "../Bilib/headers/dft.h"
+
+/* Format conversions ------------------------------------------------------ */
+template <class T>
+void RealImag2Complex(const T *_real, const T *_imag,
+   complex<double> *_complex, int length) {
+   T *aux_real   =(T *)_real;
+   T *aux_imag   =(T *)_imag;
+   double *aux_complex=(double *)_complex;
+   for (int i=0; i<length; i++) {
+      *aux_complex++=(double)(*aux_real++);
+      *aux_complex++=(double)(*aux_imag++);
+   }
+}
+
+template <class T>
+void AmplPhase2Complex(const T *_ampl, const T *_phase,
+   complex<double> *_complex, int length) {
+   T *aux_ampl   =(T *)_ampl;
+   T *aux_phase  =(T *)_phase;
+   double *aux_complex=(double *)_complex;
+   for (int i=0; i<length; i++) {
+      double ampl =(double)(*aux_ampl++);
+      double phase=(double)(*aux_phase++);
+      *aux_complex++=ampl*cos(phase);
+      *aux_complex++=ampl*sin(phase);
+   }
+}
+
+template <class T>
+void Complex2RealImag(const complex<double> *_complex,
+   T *_real, T *_imag, int length) {
+   T *aux_real   =(T *)_real;
+   T *aux_imag   =(T *)_imag;
+   double *aux_complex=(double *)_complex;
+   for (int i=0; i<length; i++) {
+      *aux_real++=(T)(*aux_complex++);
+      *aux_imag++=(T)(*aux_complex++);
+   }
+}
+
+template <class T>
+void Complex2AmplPhase(const complex<double> *_complex,
+   T *_ampl, T *_phase, int length) {
+   T *aux_ampl   =(T *)_ampl;
+   T *aux_phase  =(T *)_phase;
+   double *aux_complex=(double *)_complex;
+   for (int i=0; i<length; i++) {
+      double re=*aux_complex++;
+      double im=*aux_complex++;
+      *aux_ampl++=sqrt(re*re+im*im);
+      *aux_phase++=atan2(im,re);
+   }
+}
+
+/** Direct Fourier Transform 1D ------------------------------------------- */
+void FourierTransform(const matrix1D<double> &in,
+   matrix1D< complex<double> > &out) {
+   int N=XSIZE(in);
+   matrix1D<double> re(in), tmp(N), im(N), cas(N);
+   out.resize(N);
+   
+   GetCaS(MULTIDIM_ARRAY(cas),N);
+   DftRealToRealImaginary (MULTIDIM_ARRAY(re),MULTIDIM_ARRAY(im),
+      MULTIDIM_ARRAY(tmp), MULTIDIM_ARRAY(cas), N);
+   RealImag2Complex(MULTIDIM_ARRAY(re),MULTIDIM_ARRAY(im),
+      MULTIDIM_ARRAY(out),N);
+}
+
+/** Direct Fourier Transform 2D. ------------------------------------------ */
+void FourierTransform(const matrix2D<double> &in,
+   matrix2D< complex<double> > &out) {
+   int Status;
+   matrix2D<double> re(in), im;
+   im.resize(in);
+   out.resize(in);
+   VolumeDftRealToRealImaginary(MULTIDIM_ARRAY(re),
+      MULTIDIM_ARRAY(im), XSIZE(in), YSIZE(in), 1, &Status); 
+   RealImag2Complex(MULTIDIM_ARRAY(re),MULTIDIM_ARRAY(im),
+      MULTIDIM_ARRAY(out),XSIZE(in)*YSIZE(in));
+}
+
+/** Direct Fourier Transform 3D. ------------------------------------------ */
+void FourierTransform(const matrix3D<double> &in,
+   matrix3D< complex<double> > &out) {
+   int Status;
+   matrix3D<double> re(in), im;
+   im.resize(in);
+   out.resize(in);
+   VolumeDftRealToRealImaginary(MULTIDIM_ARRAY(re),
+      MULTIDIM_ARRAY(im), XSIZE(in), YSIZE(in), ZSIZE(in), &Status); 
+   RealImag2Complex(MULTIDIM_ARRAY(re),MULTIDIM_ARRAY(im),
+      MULTIDIM_ARRAY(out),XSIZE(in)*YSIZE(in)*ZSIZE(in));
+}
+
+/** Inverse Fourier Transform 1D. ----------------------------------------- */
+void InverseFourierTransform(const matrix1D< complex<double> > &in,
+   matrix1D<double> &out) {
+   int N=XSIZE(in);
+   matrix1D<double> tmp(N), im(N), cas(N);
+   out.resize(N);
+   
+   GetCaS(MULTIDIM_ARRAY(cas),N);
+   Complex2RealImag(MULTIDIM_ARRAY(in),MULTIDIM_ARRAY(out),
+      MULTIDIM_ARRAY(im),N);
+   InvDftRealImaginaryToReal(MULTIDIM_ARRAY(out),MULTIDIM_ARRAY(im),
+      MULTIDIM_ARRAY(tmp), MULTIDIM_ARRAY(cas), N);
+}
+
+/** Inverse Fourier Transform 2D. ----------------------------------------- */
+void InverseFourierTransform(const matrix2D< complex<double> > &in,
+   matrix2D<double> &out) {
+   int Status;
+   matrix2D<double> im;
+   out.resize(in);
+   im.resize(in);
+   Complex2RealImag(MULTIDIM_ARRAY(in),MULTIDIM_ARRAY(out),
+      MULTIDIM_ARRAY(im),XSIZE(in)*YSIZE(in));
+   VolumeInvDftRealImaginaryToReal(MULTIDIM_ARRAY(out),
+      MULTIDIM_ARRAY(im), XSIZE(in), YSIZE(in), 1, &Status); 
+}
+
+/** Inverse Fourier Transform 3D. ----------------------------------------- */
+void InverseFourierTransform(const matrix3D< complex<double> > &in,
+   matrix3D<double> &out) {
+   int Status;
+   matrix3D<double> im;
+   out.resize(in);
+   im.resize(in);
+   Complex2RealImag(MULTIDIM_ARRAY(in),MULTIDIM_ARRAY(out),
+      MULTIDIM_ARRAY(im),XSIZE(in)*YSIZE(in)*ZSIZE(in));
+   VolumeInvDftRealImaginaryToReal(MULTIDIM_ARRAY(out),
+      MULTIDIM_ARRAY(im), XSIZE(in), YSIZE(in), ZSIZE(in), &Status); 
+}
+
+/* CenterFFT 1D. ----------------------------------------------------------- */
+template <class T>
+void CenterFFT(matrix1D<T> &v, bool forward) {
+   matrix1D<T> aux;
+   int l, shift;
+   
+   l=XSIZE(v); aux.resize(l); shift=(int)(l/2);
+   if (!forward) shift=-shift;
+   // Shift the input in an auxiliar vector
+   for (int i=0; i<l; i++) {
+      int ip=i+shift; if (ip<0) ip+=l; else if (ip>=l) ip-=l;
+      aux(ip)=DIRECT_VEC_ELEM(v,i);
+   }
+   // Copy the vector
+   for (int i=0; i<l; i++)
+      DIRECT_VEC_ELEM(v,i)=DIRECT_VEC_ELEM(aux,i);
+}
+
+/* CenterFFT 2D. ----------------------------------------------------------- */
+template <class T>
+void CenterFFT(matrix2D<T> &v, bool forward) {
+   matrix1D<T> aux;
+   int l, shift;
+
+   // Shift in the X direction
+   l=XSIZE(v); aux.resize(l); shift=(int)(l/2);
+   if (!forward) shift=-shift;
+   for (int i=0; i<YSIZE(v); i++) {
+      // Shift the input in an auxiliar vector
+      for (int j=0; j<l; j++) {
+         int jp=j+shift; if (jp<0) jp+=l; else if (jp>=l) jp-=l;
+         aux(jp)=DIRECT_MAT_ELEM(v,i,j);
+      }
+      // Copy the vector
+      for (int j=0; j<l; j++)
+         DIRECT_MAT_ELEM(v,i,j)=DIRECT_VEC_ELEM(aux,j);
+   }
+
+   // Shift in the Y direction
+   l=YSIZE(v); aux.resize(l); shift=(int)(l/2);
+   if (!forward) shift=-shift;
+   for (int j=0; j<XSIZE(v); j++) {
+      // Shift the input in an auxiliar vector
+      for (int i=0; i<l; i++) {
+         int ip=i+shift; if (ip<0) ip+=l; else if (ip>=l) ip-=l;
+         aux(ip)=DIRECT_MAT_ELEM(v,i,j);
+      }
+      // Copy the vector
+      for (int i=0; i<l; i++)
+         DIRECT_MAT_ELEM(v,i,j)=DIRECT_VEC_ELEM(aux,i);
+   }
+}
+
+/* CenterFFT 3D. ----------------------------------------------------------- */
+template <class T>
+void CenterFFT(matrix3D<T> &v, bool forward) {
+   matrix1D<T> aux;
+   int l, shift;
+
+   // Shift in the X direction
+   l=XSIZE(v); aux.resize(l); shift=(int)(l/2);
+   if (!forward) shift=-shift;
+   for (int k=0; k<ZSIZE(v); k++)
+      for (int i=0; i<YSIZE(v); i++) {
+         // Shift the input in an auxiliar vector
+         for (int j=0; j<l; j++) {
+            int jp=j+shift; if (jp<0) jp+=l; else if (jp>=l) jp-=l;
+            aux(jp)=DIRECT_VOL_ELEM(v,k,i,j);
+         }
+         // Copy the vector
+         for (int j=0; j<l; j++)
+            DIRECT_VOL_ELEM(v,k,i,j)=DIRECT_VEC_ELEM(aux,j);
+      }
+
+   // Shift in the Y direction
+   l=YSIZE(v); aux.resize(l); shift=(int)(l/2);
+   if (!forward) shift=-shift;
+   for (int k=0; k<ZSIZE(v); k++)
+      for (int j=0; j<XSIZE(v); j++) {
+         // Shift the input in an auxiliar vector
+         for (int i=0; i<l; i++) {
+            int ip=i+shift; if (ip<0) ip+=l; else if (ip>=l) ip-=l;
+            aux(ip)=DIRECT_VOL_ELEM(v,k,i,j);
+         }
+         // Copy the vector
+         for (int i=0; i<l; i++)
+            DIRECT_VOL_ELEM(v,k,i,j)=DIRECT_VEC_ELEM(aux,i);
+      }
+
+   // Shift in the Z direction
+   l=ZSIZE(v); aux.resize(l); shift=(int)(l/2);
+   if (!forward) shift=-shift;
+   for (int i=0; i<YSIZE(v); i++)
+      for (int j=0; j<XSIZE(v); j++) {
+         // Shift the input in an auxiliar vector
+         for (int k=0; k<l; k++) {
+            int kp=k+shift; if (kp<0) kp+=l; else if (kp>=l) kp-=l;
+            aux(kp)=DIRECT_VOL_ELEM(v,k,i,j);
+         }
+         // Copy the vector
+         for (int k=0; k<l; k++)
+            DIRECT_VOL_ELEM(v,k,i,j)=DIRECT_VEC_ELEM(aux,k);
+      }
+}
+
+/* FFT Magnitude 1D. ------------------------------------------------------- */
+void FFT_magnitude(const matrix1D< complex<double> > &v,
+   matrix1D<double> &mag) {
+   mag.resize(v);
+   FOR_ALL_ELEMENTS_IN_MATRIX1D(v) mag(i)=abs(v(i));
+}
+
+void FFT_magnitude(const matrix2D< complex<double> > &v,
+   matrix2D<double> &mag) {
+   mag.resize(v);
+   FOR_ALL_ELEMENTS_IN_MATRIX2D(v) mag(i,j)=abs(v(i,j));
+}
+
+void FFT_magnitude(const matrix3D< complex<double> > &v,
+   matrix3D<double> &mag) {
+   mag.resize(v);
+   FOR_ALL_ELEMENTS_IN_MATRIX3D(v) mag(k,i,j)=abs(v(k,i,j));
+}
+
+/* FFT Phase 1D. ------------------------------------------------------- */
+void FFT_phase(const matrix1D< complex<double> > &v,
+   matrix1D<double> &phase) {
+   phase.resize(v);
+   FOR_ALL_ELEMENTS_IN_MATRIX1D(v) phase(i)=atan2(v(i).imag(),v(i).real());
+}
+
+void FFT_phase(const matrix2D< complex<double> > &v,
+   matrix2D<double> &phase) {
+   phase.resize(v);
+   FOR_ALL_ELEMENTS_IN_MATRIX2D(v) phase(i,j)=atan2(v(i,j).imag(),v(i,j).real());
+}
+
+void FFT_phase(const matrix3D< complex<double> > &v,
+   matrix3D<double> &phase) {
+   phase.resize(v);
+   FOR_ALL_ELEMENTS_IN_MATRIX3D(v) phase(k,i,j)=atan2(v(k,i,j).imag(),
+      v(k,i,j).real());
+}
+
+/* Correlation and Autocorrelation ----------------------------------------- */
+template <class T>
+void auto_correlation_matrix(const matrix2D<T> &Img, matrix2D<double> &R) {
+   // Compute the Fourier Transform
+   matrix2D< complex<double> > FFT1; FourierTransform(Img, FFT1);
+
+   // Multiply FFT1 * FFT1'
+   FOR_ALL_ELEMENTS_IN_MATRIX2D(FFT1)
+      FFT1(i,j)*=conj(FFT1(i,j));
+
+   // Invert the product, in order to obtain the correlation image
+   InverseFourierTransform(FFT1,R);
+	  
+   // Center the resulting image to obtain a centered autocorrelation
+   CenterFFT(R,true);
+}
+
+template <class T>
+void correlation_matrix(const matrix2D<T> &m1, const matrix2D<T> &m2,
+   matrix2D<double> &R) {
+   // Compute the Fourier Transforms
+   matrix2D< complex<double> > FFT1; FourierTransform(m1, FFT1);
+   matrix2D< complex<double> > FFT2; FourierTransform(m2, FFT2);
+
+   // Multiply FFT1 * FFT2'
+   FOR_ALL_ELEMENTS_IN_MATRIX2D(FFT1)
+      FFT1(i,j)*=conj(FFT2(i,j));
+
+   // Invert the product, in order to obtain the correlation image
+   InverseFourierTransform(FFT1,R);
+	  
+   // Center the resulting image to obtain a centered autocorrelation
+   CenterFFT(R,true);
+}
+
+/* Convolution of series --------------------------------------------------- */
+template <class T>
+void series_convolution(matrix1D<T> &series1, matrix1D<T> &series2,
+   matrix1D<T> &result, bool FullConvolution) {
+   // Store dimension of series
+   int dim1=series1.get_dim();
+   int dim2=series2.get_dim();
+
+   // Resize series to the size of the resulting series
+   // (Zeros are stored in the expanded values)
+   series1.resize(dim1+dim2-1);
+   series2.resize(series1);
+   result .resize(series1);
+
+    // Fourier Transform the two series
+    matrix1D<complex <double> > FFT1; FourierTransform(series1, FFT1);
+    matrix1D<complex <double> > FFT2; FourierTransform(series2, FFT2);
+
+    // Multiply the vectors element by element to do the 
+    // convolution in the Fourier space.
+    FFT1*=FFT2;
+
+    // Recover the convolution result by inverse FFT
+    InverseFourierTransform(FFT1,result);
+
+    // Restore the dimensions
+    series1.resize(dim1);
+    series2.resize(dim2);
+	
+    /* If the full convolution is required, nothing more remains to be done.
+       Otherwise, if the valid values are required, return the central ones. */
+    if (FullConvolution==FALSE) {
+       // First get the maximum dimension of the series,
+       // which is the dimension of the result
+       int dim=MAX(dim1,dim2);
+
+       // Determine the number of values to discard
+       int discard=result.get_dim()-dim;
+
+       // Divide it by two as we have to discard them in both sides of
+       // the vector
+       discard/=2;  // Integer division is intended
+
+       // Copy required values (simple displacement of values)
+       for (int i=STARTINGX(result); i<STARTINGX(result)+dim; i++)
+          result(i)=result(i+discard);
+
+       // and finally resize to discard not copied values
+       result.resize(dim);
+    }
+}
+
+/* Numerical derivative of a matrix ----------------------------- */
+void numerical_derivative(matrix2D<double> &M, matrix2D<double> &D,
+   char direction,int order, int window_size,int polynomial_order) {
+
+   // Set D to be a copy in shape of M
+   D.copy_shape(M);
+
+   matrix1D<double> v,rotated; 
+   matrix1D<double> ans; // To obtain results 
+
+   // Wrap around version of the Savitzky-Golay coefficients
+   int dim=2*window_size+1;
+   rotated.resize(dim);
+
+   double *pans=ans.adapt_for_numerical_recipes();
+   double *pv=v.adapt_for_numerical_recipes();
+   double *protated=rotated.adapt_for_numerical_recipes();
+
+   // Calculate the Savitzky-Golay filter coeficients
+   savgol(protated, 2*window_size+1, window_size,
+      window_size, order, polynomial_order);
+
+   // Savitzky-Golay filter is returned in wrap-around style, so
+   // correct it to use with the convolution routine
+   matrix1D<double> coefficients(dim);
+   FOR_ALL_ELEMENTS_IN_MATRIX1D(coefficients) {
+      int j=i+window_size;
+      if(j<dim) coefficients(j)=rotated(i);
+      else      coefficients(j-dim)=rotated(i);
+   }
+   
+   // Apply the Savitzky-Golay filter to every row or column
+   if (direction=='x') {
+      // For every row (values in a row are values of the X direction)
+      for (int i=STARTINGY(M);i<=FINISHINGY(M);i++) {
+          M.getRow(i,v); 
+          series_convolution(v,coefficients,ans,false);
+          ans.setRow();
+          D.setRow(i,ans);			
+      }
+   } else if(direction=='y') {
+      // For every column (values in a column are values of the Y direction)
+      for (int i=STARTINGX(M);i<=FINISHINGX(M);i++) {
+          M.getCol(i,v);
+          series_convolution(v,coefficients,ans,false);
+          ans.setCol();
+          D.setCol(i,ans);						
+      }
+   }	
+}
+
+/* Instantiation ---------------------------------------------------------- */
+void instantiate_FFT() {
+   double          *ptr_ta, *ptr_tb;
+   complex<double> *ptr_complex;
+   
+    RealImag2Complex(ptr_ta, ptr_tb, ptr_complex, 0);
+   AmplPhase2Complex(ptr_ta, ptr_tb, ptr_complex, 0);
+    Complex2RealImag(ptr_complex, ptr_ta, ptr_tb, 0);
+   Complex2AmplPhase(ptr_complex, ptr_ta, ptr_tb, 0);
+
+   matrix2D<double> m1;
+   matrix2D<double> R;
+   auto_correlation_matrix(m1,R);
+   correlation_matrix(m1,m1,R);
+   
+   matrix1D<double> series1;
+   series_convolution(series1, series1, series1, false);
+
+   matrix1D<double> v; CenterFFT(v,false);
+   matrix2D<double> m; CenterFFT(m,false);
+   matrix3D<double> V; CenterFFT(V,false);
+   matrix1D< complex<double> > vc; CenterFFT(vc,false);
+   matrix2D< complex<double> > mc; CenterFFT(mc,false);
+   matrix3D< complex<double> > Vc; CenterFFT(Vc,false);
+}
---
+++ trunk/Xmipp/Lib/XmippData/xmippFFT.hh
@@ -0,0 +1,251 @@
+/***************************************************************************
+ *
+ * Authors:     Carlos Oscar S. Sorzano (coss@cnb.uam.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+/*****************************************************************************/
+/* Fourier Transforms                                                        */
+/*****************************************************************************/
+#ifndef _XMIPP_FFT_HH
+   #define _XMIPP_FFT_HH
+
+#include <complex>
+#include "xmippMatrices1D.hh"
+#include "xmippMatrices2D.hh"
+#include "xmippMatrices3D.hh"
+
+/**@name Fourier Transforms */
+//@{
+/**@name Index <--> Frequency, Continuous <--> Discrete*/
+//@{
+   /** Index to frequency.
+       Given an index and a size of the FFT, this function returns the
+       corresponding digital frequency (-1/2 to 1/2). */
+   #define FFT_IDX2DIGFREQ(idx,size,freq) \
+       freq=((double)((idx)<(size)/2)?(idx):-(size)+(idx))/(double)(size);
+
+   /** Frequency to index (int).
+       Given a frequency and a size of the FFT, this macro returns the
+       corresponding integer index. */
+   #define DIGFREQ2FFT_IDX(freq,size,idx) {\
+       (idx)=(int)(ROUND((size)*(freq))); if ((idx)<0) (idx)+=(int)(size);}
+
+   /** Frequency to index (double).
+       Given a frequency and a size of the FFT, this macro returns the
+       corresponding double index. */
+   #define DIGFREQ2FFT_IDX_DOUBLE(freq,size,idx) {\
+       (idx)=((size)*(freq)); if ((idx)<0) (idx)+=(size);}
+
+   /** Index to frequency.
+       This function can be used with vectors of any size (1,2,3).
+       The Digital spectrum is limited between -1/2 and 1/2.
+       If the vector has got more than 3 coordinates, then an exception
+       is thrown*/
+   template <class T>
+   void FFT_idx2digfreq(T &v, const matrix1D<int> &idx,
+      matrix1D<double> &freq) _THROW {
+         if (XSIZE(idx)<1 || XSIZE(idx)>3)
+            REPORT_ERROR(1,"FFT_idx2digfreq: Index is not of the correct size");
+         freq.resize(XSIZE(idx));
+
+         int size[3]; v.get_size(size);
+         FOR_ALL_ELEMENTS_IN_MATRIX1D(idx)
+            FFT_IDX2DIGFREQ(VEC_ELEM(idx,i),size[i],VEC_ELEM(freq,i));
+   }
+
+   /** Frequency to index.
+       This function can be used with vectors of any size (1,2,3).
+       The Digital spectrum is limited between -1/2 and 1/2.
+       If the vector has got more than 3 coordinates, then an exception
+       is thrown*/
+   template <class T>
+   void digfreq2FFT_idx(T &v, const matrix1D<double> &freq,
+      matrix1D<int> &idx) _THROW {
+         if (XSIZE(freq)<1 || XSIZE(freq)>3)
+            REPORT_ERROR(1,"digfreq2FFT_idx: freq is not of the correct size");
+         idx.resize(XSIZE(freq));
+
+         int size[3]; v.get_size(size);
+         FOR_ALL_ELEMENTS_IN_MATRIX1D(idx)
+            DIGFREQ2FFT_IDX(VEC_ELEM(freq,i),size[i],VEC_ELEM(idx,i));
+   }
+
+
+   /** Digital to Continuous frequency.
+       The pixel size must be given in Amstrongs. The digital frequency is
+       between [-1/2,1/2].*/
+   inline void digfreq2contfreq(const matrix1D<double> &digfreq,
+      matrix1D<double> &contfreq, double pixel_size)
+      {contfreq=digfreq/pixel_size;}
+   
+   /** Continuous to Digital frequency.
+       The pixel size must be given in Amstrongs. The digital frequency is
+       between [-1/2,1/2].*/
+   inline void contfreq2digfreq(const matrix1D<double> &contfreq,
+      matrix1D<double> &digfreq, double pixel_size)
+      {digfreq=contfreq*pixel_size;}
+//@}
+
+/**@name Format conversions.*/
+//@{
+    /** Real/Imaginary --> Complex.
+        The output array(s) must be already resized*/
+    template <class T>
+       void RealImag2Complex(const T *_real, const T *_imag,
+          complex<double> *_complex, int length);
+
+    /** Amplitude/Phase --> Complex.
+        The output array(s) must be already resized*/
+    template <class T>
+       void AmplPhase2Complex(const T *_ampl, const T *_phase,
+          complex<double> *_complex, int length);
+
+    /** Complex --> Real/Imag.
+        The output array(s) must be already resized*/
+    template <class T>
+       void Complex2RealImag(const complex<double> *_complex,
+          T *_real, T *_imag, int length);
+
+    /** Complex --> Amplitude/Phase.
+        The output array(s) must be already resized*/
+    template <class T>
+       void Complex2AmplPhase(const complex<double> *_complex,
+          T *_ampl, T *_phase, int length);
+//@}
+
+/**@name Fourier Transforms */
+//@{
+    /** Direct Fourier Transform 1D.*/
+    void FourierTransform(const matrix1D<double> &in,
+       matrix1D< complex<double> > &out);
+
+    /** Direct Fourier Transform 2D.*/
+    void FourierTransform(const matrix2D<double> &in,
+       matrix2D< complex<double> > &out);
+
+    /** Direct Fourier Transform 3D.*/
+    void FourierTransform(const matrix3D<double> &in,
+       matrix3D< complex<double> > &out);
+
+    /** Inverse Fourier Transform 1D.*/
+    void InverseFourierTransform(const matrix1D< complex<double> > &in,
+       matrix1D<double> &out);
+
+    /** Inverse Fourier Transform 2D.*/
+    void InverseFourierTransform(const matrix2D< complex<double> > &in,
+       matrix2D<double> &out);
+
+    /** Inverse Fourier Transform 3D.*/
+    void InverseFourierTransform(const matrix3D< complex<double> > &in,
+       matrix3D<double> &out);
+//@}
+
+/**@name Operations with the Fourier Transforms */
+//@{
+    /** CenterFFT 1D. */
+    template <class T>
+    void CenterFFT(matrix1D<T> &v, bool forward);
+
+    /** CenterFFT 2D. */
+    template <class T>
+    void CenterFFT(matrix2D<T> &v, bool forward);
+
+    /** CenterFFT 3D. */
+    template <class T>
+    void CenterFFT(matrix3D<T> &v, bool forward);
+
+    /** FFT Magnitude 1D. */
+    void FFT_magnitude(const matrix1D< complex<double> > &v,
+       matrix1D<double> &mag);
+
+    /** FFT Magnitude 2D. */
+    void FFT_magnitude(const matrix2D< complex<double> > &v,
+       matrix2D<double> &mag);
+
+    /** FFT Magnitude 3D. */
+    void FFT_magnitude(const matrix3D< complex<double> > &v,
+       matrix3D<double> &mag);
+
+    /** FFT Phase 1D. */
+    void FFT_phase(const matrix1D< complex<double> > &v,
+       matrix1D<double> &phase);
+
+    /** FFT Phase 2D. */
+    void FFT_phase(const matrix2D< complex<double> > &v,
+       matrix2D<double> &phase);
+
+    /** FFT Phase 3D. */
+    void FFT_phase(const matrix3D< complex<double> > &v,
+       matrix3D<double> &phase);
+
+    /** Autocorrelation function of an Xmipp matrix.
+        Fast calcuation of the autocorrelation matrix of a given one using
+        Fast Fourier Transform. (Using the correlation theorem) */
+  template <class T>
+     void auto_correlation_matrix(matrix2D<T> const &Img,
+        matrix2D<double> &R);
+
+   /** Autocorrelation function of an Xmipp matrix.
+       Fast calcuation of the correlation matrix on two matrices using
+       Fast Fourier Transform. (Using the correlation theorem).
+       The output matrix must be already resized*/
+  template <class T>
+     void correlation_matrix(matrix2D<T> const &m1,matrix2D<T> const &m2,
+        matrix2D<double> &R);
+
+
+   /** Series convolution function. Gives the convolution of two series
+       given as Xmipp Vectors. Result is stored in result vector.
+       Fast calcuation of the convolution result using
+       Fast Fourier Transform. If FullConvolution, set by default to
+       FALSE, is TRUE the full convolution series is returned. Otherwise
+       the convolution vector refers only to the valid values, whose
+       number is the greater dimension of the two series.
+       Note: Complex numbers are allowed */
+   template <class T>
+      void series_convolution(matrix1D<T> &series1,matrix1D<T> &series2,
+         matrix1D<T> &result, bool FullConvolution=FALSE);
+
+   /** Numerical_derivative: This function computes the numerical derivative
+       of a matrix in Y direction (rows) or X direction (columns) of a given
+       matrix, using a Savitzky-Golay filter on every row or column, and then
+       convolving.Input matrix is M, result is stored in D, direction can have
+       values of 'x' or 'y'. Order is the derivative order.
+       Window size and polynomial order are parameters for the
+       Savitzky-Golay filter that define the number of points forward
+       (+window_size)
+       and backward (-window_size) considered to calculate the filter, and the degree of
+       the polynomial to interpolate these values, respectively. Default values are
+       window_size=2 and polynomial_order=4, which are equivalent to a
+       5-point algorithm to calculate the derivate and give good results.
+       But they can be changed provided that polynomial_order <= 2*window_size.
+       As one can expect, the values of the matrix in a border of size
+       window_size are not accurate ones, as there aren't enough points to perform
+       the estimation. As a rule of thumb, the greater the window, the more the
+       filtering and the less the precission of the derivatives, and the
+       greater the order of the polynomial, the greater the precission.  */
+    void numerical_derivative(matrix2D<double> &M,matrix2D<double> &D,
+       char direction, int order, int window_size=2, int polynomial_order=4);
+//@}
+
+//@}
+#endif
