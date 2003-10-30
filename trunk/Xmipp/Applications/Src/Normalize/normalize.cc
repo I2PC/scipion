9d6e786ba922967c57098b9b9fde2314a21a1db1
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2003-10-30 17:29:26 +0000

    Added (default) option to apply the (inverse) transformation stored in the header of the image to the background mask.
    git-svn-id: file:///home/roberto/Git/svn@439 1a721e8d-d38c-4e91-ab98-6a0be4d754af

--- trunk/Xmipp/Applications/Src/Normalize/normalize.cc
+++ trunk/Xmipp/Applications/Src/Normalize/normalize.cc
@@ -27,6 +27,7 @@
 
 void process_img(ImageXmipp &img, const Prog_parameters *prm) {
    Normalize_parameters * eprm=(Normalize_parameters *) prm;
+   if (eprm->apply_geo) eprm->apply_geo_mask(img);
    eprm->apply(&img);
 }
 
