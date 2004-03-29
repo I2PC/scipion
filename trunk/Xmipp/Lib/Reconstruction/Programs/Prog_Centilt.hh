7ac6d84a8fe06b66f85a83cc08fe066956825a4e
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2004-03-29 16:17:33 +0000

    Ported OldXmipp centilt
    git-svn-id: file:///home/roberto/Git/svn@571 1a721e8d-d38c-4e91-ab98-6a0be4d754af

---
+++ trunk/Xmipp/Lib/Reconstruction/Programs/Prog_Centilt.hh
@@ -0,0 +1,71 @@
+/***************************************************************************
+ *
+ * Authors:    Sjors Scheres           scheres@cnb.uam.es (2002)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+#include <XmippData/xmippFFT.hh>
+#include <XmippData/xmippArgs.hh>
+#include <XmippData/xmippFuncs.hh>
+#include <XmippData/xmippDocFiles.hh>
+#include <XmippData/xmippSelFiles.hh>
+#include <XmippData/xmippImages.hh>
+#include <XmippData/xmippGeometry.hh>
+#include <XmippData/xmippFilters.hh>
+
+/**@name Centilt */
+//@{
+/** Centilt parameters. */
+class Prog_centilt_prm {
+public:
+  /** SelFiles for untilted and tilted images */
+  SelFile SFu, SFt;
+  /**  Filename output document file */
+  FileName fn_doc;
+  /**  Filename output extension */
+  FileName oext;
+  /** Discard images that shift more than max_shift*/
+  double max_shift;
+  /** Force x-shift to be zero */
+  bool force_x_zero;
+  /** Perform cosine stretching */
+  bool do_stretch;
+  /** Perform centering */
+  bool do_center;
+
+public:
+  /// Read arguments from command line
+  void read(int argc, char **argv) _THROW;
+  
+  /// Show
+  void show();
+
+  /// Usage
+  void usage();
+  
+  /// Center one tilted image
+  bool center_tilted_image(const ImageXmipp &Iu, ImageXmipp &It, double &ccf) _THROW;
+
+  /// Main routine
+  void centilt() _THROW;
+
+};				    
+//@}
