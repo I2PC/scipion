3a74e4288a799fc354eacda3b626b3a2df7f116d
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2004-12-15 14:51:35 +0000

    Creating MLalign2D
    git-svn-id: file:///home/roberto/Git/svn@771 1a721e8d-d38c-4e91-ab98-6a0be4d754af

---
+++ trunk/Xmipp/Applications/Src/MLalign2D/MLalign2D.cc
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *
+ * Authors: Sjors Scheres (scheres@cnb.uam.es)   
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+
+/* INCLUDES ---------------------------------------------------------------- */
+#include <Reconstruction/Programs/Prog_MLalign2D.hh> 
+
+/* MAIN -------------------------------------------------------------------- */
+int main(int argc, char **argv) {
+
+  Prog_MLalign2D_prm prm;
+
+  // Get input parameters
+  try {
+    prm.read(argc,argv);
+    prm.show();
+  } catch (Xmipp_error XE) {cout << XE; prm.usage(); exit(0);}
+    
+  try {
+    prm.MLalign2D();
+  } catch (Xmipp_error XE) {cout << XE; prm.usage(); exit(0);}
+}
+
+
+
+
