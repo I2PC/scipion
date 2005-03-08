e0cb2b27c904f7c971fe8b52b25668d6016702d1
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2005-03-08 07:42:39 +0000

    Adapted to mark pairs
    git-svn-id: file:///home/roberto/Git/svn@833 1a721e8d-d38c-4e91-ab98-6a0be4d754af

--- trunk/Xmipp/Applications/Src/Scissor/scissor.cc
+++ trunk/Xmipp/Applications/Src/Scissor/scissor.cc
@@ -30,19 +30,19 @@ void Usage();
 
 int main (int argc, char **argv) {
    FileName fn_orig, fn_micrograph, fn_pos, fn_root;
+   FileName fn_tilted, fn_root_tilted;
    int      Ydim, Xdim;
    int      startN;
    bool     reverse_endian;
    bool     compute_transmitance=FALSE;
    bool     compute_inverse=FALSE;
    double   alpha;
+   bool     pair_mode;
    try {
       fn_micrograph = get_param(argc,argv,"-i");
-      fn_pos        = get_param(argc,argv,"-pos");
+      pair_mode=check_param(argc,argv,"-tilted");
       fn_root       = get_param(argc,argv,"-root");
-      fn_orig       = get_param(argc,argv,"-orig","");
       Xdim          = AtoI(get_param(argc,argv,"-Xdim"));
-      alpha         = AtoF(get_param(argc,argv,"-alpha","0"));
       if (check_param(argc,argv,"-Ydim"))
          Ydim       = AtoI(get_param(argc,argv,"-Ydim"));
       else Ydim=Xdim;
@@ -50,37 +50,87 @@ int main (int argc, char **argv) {
       reverse_endian= check_param(argc,argv,"-reverse_endian");
       compute_inverse= check_param(argc,argv,"-invert");
       compute_transmitance= check_param(argc,argv,"-log");
+      if (!pair_mode) {
+	 fn_pos        = get_param(argc,argv,"-pos");
+	 fn_orig       = get_param(argc,argv,"-orig","");
+	 alpha         = AtoF(get_param(argc,argv,"-alpha","0"));
+      } else {
+         fn_root_tilted= get_param(argc,argv,"-root_tilted");
+         fn_tilted     = get_param(argc,argv,"-tilted");
+      }
    } catch (Xmipp_error XE) {cout << XE; Usage(); exit(1);}
    try {
-      Micrograph m;
-      m.open_micrograph(fn_micrograph,reverse_endian);
-      m.set_window_size(Xdim,Ydim);
-      m.read_coordinates(0,fn_pos);
-      m.add_label("");
-      m.set_transmitance_flag(compute_transmitance);
-      m.set_inverse_flag(compute_inverse);
-      m.produce_all_images(0,fn_root,startN,fn_orig,alpha);
-cout << "compute_transmitance " << compute_transmitance
-     << " compute_inverse " << compute_inverse << endl;
+      if (!pair_mode) {
+	 Micrograph m;
+	 m.open_micrograph(fn_micrograph,reverse_endian);
+	 m.set_window_size(Xdim,Ydim);
+	 m.read_coordinates(0,fn_pos);
+	 m.add_label("");
+	 m.set_transmitance_flag(compute_transmitance);
+	 m.set_inverse_flag(compute_inverse);
+	 m.produce_all_images(0,fn_root,startN,fn_orig,alpha);
+      } else {
+         // Read angles
+	 FileName fn_ang=fn_micrograph.without_extension();
+	 fn_ang=fn_ang.add_extension("ang");
+	 ifstream fh_ang;
+	 fh_ang.open(fn_ang.c_str());
+	 if (!fh_ang)
+	    REPORT_ERROR(1,(string)"Scissor: Cannot open file"+fn_ang);
+	 string aux;
+	 getline(fh_ang,aux);
+	 double alpha_u, alpha_t;
+	 fh_ang >> alpha_u >> alpha_t;
+	 fh_ang.close();
+	 
+	 // Generate the images for the untilted image
+	 Micrograph m;
+	 m.open_micrograph(fn_micrograph,reverse_endian);
+	 m.set_window_size(Xdim,Ydim);
+	 m.read_coordinates(0,fn_micrograph.add_extension("Common.pos"));
+	 m.add_label("");
+	 m.set_transmitance_flag(compute_transmitance);
+	 m.set_inverse_flag(compute_inverse);
+	 m.produce_all_images(0,fn_root,startN,"",alpha_u);
+	 m.close_micrograph();
+
+	 // Generate the images for the tilted image
+	 Micrograph mt;
+	 mt.open_micrograph(fn_tilted,reverse_endian);
+	 mt.set_window_size(Xdim,Ydim);
+	 mt.read_coordinates(0,fn_tilted.add_extension("Common.pos"));
+	 mt.add_label("");
+	 mt.set_transmitance_flag(compute_transmitance);
+	 mt.set_inverse_flag(compute_inverse);
+	 mt.produce_all_images(0,fn_root_tilted,startN,"",alpha_t);
+	 mt.close_micrograph();
+      }
    } catch (Xmipp_error XE) {cout << XE;}
 }
 
 void Usage() {
    cerr << "Purpose: Cut the images marked with xmipp_mark\n"
         << "Usage: scissor [options]\n"
+	<< "For single images -------------------------\n"
         << "   -i <input micrograph>      : From which the images will be cutted\n"
         << "  [-orig <original micrograph>: unless this parameter is specified\n"
         << "   -root <root name>          : for the cutted images\n"
         << "   -pos <position file>       : order X,Y\n"
+	<< "                                from transmitance\n"
+	<< "  [-alpha <ang>]              : Angle from Y axis to tilt axis\n"
+	<< "                                as it comes out from xmipp_mark\n"
+	<< "For image pairs ---------------------------\n"
+        << "   -i <untilted micrograph>   : From which the images will be cutted\n"
+        << "   -tilted <tilted micrograph>: From which the images will be cutted\n"
+        << "   -root <root name>          : for the cutted images\n"
+        << "   -root_tilted <root name>   : for the tilted cutted images\n"
+	<< "For both ----------------------------------\n"
         << "   -Xdim <window X dim>       : in pixels\n"
         << "  [-Ydim <window Y dim>]      : if not given Ydim=Xdim\n"
         << "  [-start <N=1>]              : Number of the first image\n"
         << "  [-reverse_endian]           : of the input micrographs\n"
-	<< "  [-alpha <ang>]              : Angle from Y axis to tilt axis\n"
-	<< "                                as it comes out from xmipp_mark\n"
 	<< "  [-invert]                   : Invert contrast\n"
 	<< "  [-log]                      : Compute optical density\n"
-	<< "                                from transmitance\n"
    ;
 }
 
