16df6b6079fd188e778f052908bf590ed559f5c4
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2005-03-29 06:36:51 +0000

    Spline wavelets added
    git-svn-id: file:///home/roberto/Git/svn@846 1a721e8d-d38c-4e91-ab98-6a0be4d754af

---
+++ trunk/Xmipp/Lib/XmippData/Bilib/headers/waveletfilters.h
@@ -0,0 +1,10 @@
+/* ----------------------------------------------------------------------------
+ Function:  WaveletFilters.h
+ 
+ Purpose:	Header file for WaveletFilter.c
+---------------------------------------------------------------------------- */
+
+extern int WaveletFiltersGetSize(short Filter, short Order, long *nh, long *ng);
+extern int WaveletFiltersGetCoef(short Filter, short Order, double *h, double *g);
+extern int WaveletFiltersGetCoef_Fact(double Alpha, double *h, double *g);
+
---
+++ trunk/Xmipp/Lib/XmippData/Bilib/headers/wavelettools.h
@@ -0,0 +1,21 @@
+
+
+extern int	WaveletSplit_3D( 
+					double	*Input, 
+					double	*Output,
+					long	nx, long ny, long nz,
+					short	Filter,
+					short	Order,
+					double	Alpha,
+					short	BoundaryConditions,
+					int		*Status);
+
+extern int	WaveletMerge_3D( 
+					double	*Input, 
+					double	*Output,
+					long	nx, long ny, long nz,
+					short	Filter,
+					short	Order,
+					double	Alpha,
+					short	BoundaryConditions,
+					int		*Status);
