b546128c0b67746da987b68a07cb0583ae8b0b55
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2005-04-13 15:07:25 +0000

    Simple projection matching 3D-angular assignment program
    git-svn-id: file:///home/roberto/Git/svn@865 1a721e8d-d38c-4e91-ab98-6a0be4d754af

---
+++ trunk/Xmipp/Applications/Src/Projection_matching/projection_matching.cc
@@ -0,0 +1,63 @@
+/***************************************************************************
+ *
+ * Authors: Sjors Scheres (scheres@cnb.uam.es)   
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+
+/* INCLUDES ---------------------------------------------------------------- */
+#include <Reconstruction/Programs/Prog_projection_matching.hh> 
+
+/* MAIN -------------------------------------------------------------------- */
+int main(int argc, char **argv) {
+
+  double                        sumCC,sumZ;
+  DocFile                       DFo;
+  Prog_projection_matching_prm  prm;
+
+  // Get input parameters
+  try {
+    // Read command line & produce side info
+    prm.read(argc,argv);
+    prm.show();
+
+    prm.produce_Side_info();
+
+  } catch (Xmipp_error XE) {cout << XE; prm.usage(); exit(0);}
+    
+  try {
+
+    DFo.clear();
+    DFo.append_comment("Headerinfo columns: rot (1), tilt (2), psi (3), Xoff (4), Yoff (5), maxCC (6), Z-score (7)");
+
+    // Process all images
+    prm.PM_loop_over_all_images(prm.SF,DFo,sumCC,sumZ);
+ 
+    cerr << " Average maxCC = "<<sumCC/prm.SF.ImgNo()<<" average Z-score = "<<sumZ/prm.SF.ImgNo() <<endl;
+    DFo.write(prm.fn_out);
+
+  } catch (Xmipp_error XE) {cout << XE; prm.usage(); exit(0);}
+
+}
+
+
+
+
