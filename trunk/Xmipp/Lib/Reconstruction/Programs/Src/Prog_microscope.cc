a32b6de3e64128110d3fa9afce5c437b716b7b94
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2005-12-16 11:52:31 +0000

    change FALSE by false
    git-svn-id: file:///home/roberto/Git/svn@1172 1a721e8d-d38c-4e91-ab98-6a0be4d754af

--- trunk/Xmipp/Lib/Reconstruction/Programs/Src/Prog_microscope.cc
+++ trunk/Xmipp/Lib/Reconstruction/Programs/Src/Prog_microscope.cc
@@ -85,7 +85,7 @@ void Prog_Microscope_Parameters::produce_side_info() {
       } else {
          ctf.FilterBand=CTF;
          ctf.ctf.read(fn_ctf);
-         ctf.ctf.enable_CTFnoise=FALSE;
+         ctf.ctf.enable_CTFnoise=false;
          ctf.ctf.Produce_Side_Info();
          aux.resize(Ydim,Xdim); aux.set_Xmipp_origin();
          ctf.generate_mask(aux);
@@ -111,8 +111,8 @@ void Prog_Microscope_Parameters::produce_side_info() {
          after_ctf.resize_mask(Ydim,Xdim);
       } else {
          after_ctf.FilterBand=CTF;
-	 after_ctf.ctf.enable_CTFnoise=TRUE;
-         after_ctf.ctf.enable_CTF=FALSE;
+	 after_ctf.ctf.enable_CTFnoise=true;
+         after_ctf.ctf.enable_CTF=false;
          after_ctf.ctf.read(fn_after_ctf);
          after_ctf.ctf.Produce_Side_Info();
          aux.resize(Ydim,Xdim); aux.set_Xmipp_origin();
