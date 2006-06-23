26a734d968b29618b585c89859e2574ad52b76ec
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2006-06-23 07:38:22 +0000

    creation
    git-svn-id: file:///home/roberto/Git/svn@1331 1a721e8d-d38c-4e91-ab98-6a0be4d754af

---
+++ trunk/Xmipp/Lib/Reconstruction/Programs/Prog_projection_matching_crystal.hh
@@ -0,0 +1,126 @@
+/***************************************************************************
+ *
+ * Authors:    Roberto Marabini
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+#include <XmippData/xmippFFT.hh>
+#include <XmippData/xmippArgs.hh>
+#include <XmippData/xmippFuncs.hh>
+#include <XmippData/xmippSelFiles.hh>
+#include <XmippData/xmippDocFiles.hh>
+#include <XmippData/xmippImages.hh>
+#include <XmippData/xmippFilters.hh>
+#include <XmippData/xmippMasks.hh>
+#include <Reconstruction/projection.hh>
+#include <Reconstruction/directions.hh>
+#include <Reconstruction/symmetries.hh>
+#include <malloc.h>
+
+#define FOR_ALL_DIRECTIONS() for (int dirno=0;dirno<nr_dir; dirno++)
+#define FOR_ALL_ROTATIONS() for (int ipsi=0; ipsi<nr_psi; ipsi++ ) 
+
+/**@name projection_matching_crystal */
+//@{
+/** projection_matching_crystal parameters. */
+class Prog_projection_matching_crystal_prm {
+public:
+
+  /** Selfile with reference images */
+  SelFile SFref;
+  /** Selfile with experimental images */
+  SelFile SFexp;
+  /** dimension of the images */
+  int dim;
+  /** Filename output rootname */
+  FileName fn_root;
+  /** Maximum distance to exp angles and shifts plus number 
+     of samples in psi and shift*/
+  double psi_sampling, psi_distance;
+  double rot_distance;
+  double tilt_distance;
+  double shift_sampling,shift_distance ;
+  /** Number of projection directions */
+  int nr_dir;
+  /** Vector with reference library projections */
+  vector<matrix2D<double> > ref_img;
+  /** Vectors for standard deviation and mean of reference library projections */
+  double * ref_stddev, * ref_mean;
+  /** Vector with reference library angles */
+  double * ref_rot, * ref_tilt;
+  /** Vectors to stored valid shifts */
+  vector <matrix1D<double> >shift_vector;
+  /** Maximum allowed shift */
+  double max_shift;
+#ifdef NEVERDEFINE
+  vector<matrix2D<double> >::iterator idirno;
+  /** Number of steps to sample in-plane rotation in 90 degrees */
+  int nr_psi;
+  /** Verbose level:
+      1: gives progress bar (=default)
+      0: gives no output to screen at all */
+  int verb;
+  /** Flag whether to store optimal transformations in the image headers */
+  bool modify_header;
+  /** Flag whether to output reference projections, selfile and docfile */
+  bool output_refs;
+  /** Flag whether to input files are in Fourier, so far it is 
+  implemented only with the -ref flag */
+  bool fourier_input;
+  /** Angular sampling rate */
+  double sampling;
+  /** Mask for shifts */
+  matrix2D<int> rotmask;
+  /** Number of white pixels in rotmask */
+  int nr_pixels_rotmask;
+  /** Inner and outer radii to limit the rotational search */
+  double Ri, Ro;
+
+  
+
+
+  /// Extended Usage
+  void extended_usage();
+
+
+
+#endif
+
+public:
+  /** Actual projection matching for one image */
+  void PM_process_one_image(matrix2D<double> &Mexp,
+			    float img_rot, float img_tilt, float img_psi, 
+			    int &opt_dirno, double &opt_psi,
+			    double &opt_xoff, double &opt_yoff, 
+			    double &maxCC, double &Zscore);
+  /// Read arguments from command line
+  void read(int argc, char **argv);
+  /// Show
+  void show();
+  /// Usage
+  void usage();
+  /** Make shiftmask and calculate nr_psi */
+  void produce_Side_info();
+  /** Loop over all images */
+  void PM_loop_over_all_images( DocFile &DFo, double &sumCC);
+
+};				    
+//@}
---
+++ trunk/Xmipp/Lib/Reconstruction/Programs/Src/Prog_projection_matching_crystal.cc
@@ -0,0 +1,357 @@
+/***************************************************************************
+ *
+ * Authors:    Roberto Marabini           
+ *
+ * Unidad de Bioinformatica del Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+#include "../Prog_projection_matching_crystal.hh"
+
+// Read arguments ==========================================================
+void Prog_projection_matching_crystal_prm::read(int argc, char **argv)  {
+
+  // Read command line
+  SFref.read(get_param(argc,argv,"-ref"));
+  SFref.ImgSize(dim,dim);
+  SFexp.read(get_param(argc,argv,"-exp"));
+  fn_root=get_param(argc,argv,"-o","out");
+  psi_distance  = AtoF(get_param(argc,argv,   "-psi_distance","0."));
+  rot_distance  = AtoF(get_param(argc,argv,   "-rot_distance","0."));
+  tilt_distance = AtoF(get_param(argc,argv,   "-tilt_distance","0."));
+  shift_distance  = AtoF(get_param(argc,argv, "-shift_distance","0."));
+  psi_sampling  = AtoF(get_param(argc,argv,   "-psi_sam","1."));
+  if ( shift_sampling==0. ) 
+    REPORT_ERROR(1," shift_sampling must be inizialized!");
+  if ( psi_sampling==0.) 
+    REPORT_ERROR(1," psi_sampling must be inizialized!");
+  max_shift  = AtoF(get_param(argc,argv, "-max_shift","0."));
+  shift_sampling  = AtoF(get_param(argc,argv, "-shift_sam","1."));
+
+}
+
+// Usage ===================================================================
+void Prog_projection_matching_crystal_prm::usage() {
+  cerr << "Usage:  projection_matching [options] "<<endl;
+  cerr << "   -ref <selfile>                : Selfile with reference images \n"
+       << "   -exp <selfile>                : Selfile with experimenal images \n"
+       << "   -o filerootname               : Output file root name \n"
+       << "   -psi_sam number               : number of samples \n"
+       << "   -psi_distance degrees  : psi_distance to experimental \
+       proj\n"
+       << "   -rot_distance degrees  : rot_distance to experimental \
+       proj\n"
+       << "   -tilt_distance degrees : tilt_distance to experimental \
+       proj\n"
+       << "   -max_shift double      : maximun allowed shift in pixels \n"
+       ;
+}
+
+// Show ====================================================================
+void Prog_projection_matching_crystal_prm::show() {
+
+    cerr << "  Reference images            : "<< SFref.name()<<" ("<<SFref.ImgNo()<<")"<<endl;
+    cerr << "  Experimental images         : "<< SFexp.name()<<" ("<<SFexp.ImgNo()<<")"<<endl;
+    cerr << "  Output rootname             : "<< fn_root<<endl;
+    cerr << "  Psi distance                : "<< psi_distance<<endl;
+    cerr << "  Shift distance              : "<< shift_distance<<endl;
+    cerr << "  Rot distance                : "<< rot_distance<<endl;
+    cerr << "  Tilt distance               : "<< tilt_distance<<endl;
+    cerr << "  Psi number samples          : "<< psi_sampling<<endl;
+    cerr << "  Shift number samples        : "<< shift_sampling<<endl;
+    cerr << "  Maximum allowed shift       : "<< max_shift<<endl;
+
+    cerr << " ================================================================="<<endl;
+} 
+
+// Side info stuff ===================================================================
+void Prog_projection_matching_crystal_prm::produce_Side_info() {
+  Projection       proj;
+    
+  SFref.go_beginning();
+  double           mean_ref,stddev_ref,dummy,psi=0.;
+  // Read projections from selfile
+  int nl=SFref.ImgNo();
+  ref_img.clear();
+  ref_rot=(double*)malloc(nl*sizeof(double));
+  ref_tilt=(double*)malloc(nl*sizeof(double));
+  ref_mean=(double*)malloc(nl*sizeof(double));
+  ref_stddev=(double*)malloc(nl*sizeof(double));
+  SFref.go_beginning();
+  nr_dir=0;
+
+  while (!SFref.eof()) {
+    //readimage
+    proj.read(SFref.NextImg());
+    proj().set_Xmipp_origin();
+    ref_rot[nr_dir] = proj.rot();
+    ref_tilt[nr_dir]  = proj.tilt();
+    proj().compute_stats(mean_ref,stddev_ref,dummy,dummy);
+    proj()-=mean_ref;
+    ref_img.push_back(proj());
+    ref_stddev[nr_dir]=stddev_ref;
+    ref_mean[nr_dir]=mean_ref;
+    nr_dir++;
+  }
+
+  //compute shift range
+  double min_shift,max_shift,increment_shift,my_shift2;
+  double shift_distance2=shift_distance*shift_distance;
+  shift_vector.clear();
+  matrix1D<double> v_aux(2);
+  //shift already applied to the image
+  //psi has not been applied
+  min_shift= (-1.0)* shift_distance;
+  max_shift=   1.0 * shift_distance;
+  if (shift_sampling==1) increment_shift=0;
+  else increment_shift=2*shift_distance/(shift_sampling-1);
+
+  for (int ishift_x=0; ishift_x<shift_sampling; ishift_x++ ) 
+     for (int ishift_y=0; ishift_y<shift_sampling; ishift_y++ ){
+        my_shift2= (min_shift+increment_shift*(double)ishift_x) *
+	           (min_shift+increment_shift*(double)ishift_x) +
+	           (min_shift+increment_shift*(double)ishift_y) * 
+		   (min_shift+increment_shift*(double)ishift_y);
+        if( my_shift2 > shift_distance2 )
+	        continue;
+	XX(v_aux) = min_shift+increment_shift*(double)ishift_x;	   
+	YY(v_aux) = min_shift+increment_shift*(double)ishift_y;	   
+        shift_vector.push_back(v_aux);
+      }
+
+
+//#define DEBUG
+#ifdef DEBUG
+for (int ii=0 ; ii< nr_dir; ii++)
+   cout << ref_rot[ii] << " " << ref_tilt[ii] << endl;
+for (int jj=0 ; jj<shift_vector.size(); jj++)
+   cout << shift_vector[jj] << endl;
+#endif
+#undef DEBUG
+}
+
+
+
+
+void Prog_projection_matching_crystal_prm::PM_process_one_image(matrix2D<double> &Mexp,
+							float img_rot, float img_tilt, float img_psi, 
+							int &opt_dirno, double &opt_psi,
+							double &opt_xoff, double &opt_yoff, 
+							double &maxCC, double &Zscore) {
+
+
+  // Rotational search ====================================================
+  matrix2D<double> Mimg,Mref,Maux,Mcorr;
+  double act_rot_range,psi,thisCC,oldCC,aveCC=0.,varCC=0.;
+  double stddev_img,mean_img,dummy,xmax,ymax;
+  int c=0,ioptpsi=0,ioptflip=0;
+  bool search;
+  vector<matrix2D<double> >::iterator ipp;
+
+  maxCC=-99.e99; 
+  Mimg.resize(dim,dim);
+  Mimg.set_Xmipp_origin();
+  Mref.resize(dim,dim);
+  Mref.set_Xmipp_origin();
+  Maux.resize(dim,dim);
+  Maux.set_Xmipp_origin();
+
+  // Calculate mean_img,stddev_img and apply rotmask
+  Maux=Mexp;
+  Mexp.compute_stats(mean_img,stddev_img,dummy,dummy);
+  Maux-=mean_img;
+  
+  //compute psi range
+  double min_psi,max_psi,increment_psi,my_psi;
+  min_psi=img_psi-psi_distance;
+  max_psi=img_psi+psi_distance;
+  if (psi_sampling==1) increment_psi=0;
+  else increment_psi=2*psi_distance/(psi_sampling-1);
+  double dim2=dim*dim;
+  matrix2D<double> psi_mat, shift_mat;
+  for (int ipsi=0; ipsi<psi_sampling; ipsi++ ) 
+     {
+     my_psi=min_psi+increment_psi*(double)ipsi;
+     psi_mat=rot2D_matrix(my_psi);
+     //Mimg=Maux.rotate(my_psi,WRAP);
+     for (int ishift=0; ishift<shift_vector.size(); ishift++ ){ 
+        psi_mat(0,2)=XX(shift_vector[ishift]);
+        psi_mat(1,2)=YY(shift_vector[ishift]);
+        apply_geom_Bspline(Mimg,psi_mat,Maux,3,IS_NOT_INV,true,(double)0.);
+	ipp=ref_img.begin();
+	for (int dir_counter=0; dir_counter<nr_dir; dir_counter++ ) 
+            {
+            Mref=*(ipp); ipp++;
+            // For some strange reason I need to access the vector via its pointer
+            // otherwise it goes 50x slower on jumilla (Alpha-Unix)
+
+	    if ( (ABS(realWRAP(ref_tilt[dir_counter]-img_tilt,-180.,180.))<=
+	              tilt_distance) &&
+		 (ABS(realWRAP(ref_rot [dir_counter]-img_rot,-180.,180.))<=
+	              rot_distance) ){
+		 thisCC=0.;
+		 FOR_ALL_DIRECT_ELEMENTS_IN_MATRIX2D(Mimg) {
+		   thisCC+=dMij(Mref,i,j)*dMij(Mimg,i,j);
+		   }//FOR_ALL
+		 thisCC/=ref_stddev[dir_counter]*stddev_img*dim2;
+        	 //#define DEBUG
+        	 #ifdef DEBUG
+		 xmax = XX(shift_vector[ishift]);
+		 ymax = YY(shift_vector[ishift]);
+		 cout <<  SFref.get_file_number(dir_counter)<< 
+	                           " rot= "  << ref_rot[dir_counter]  << 
+	                           " tilt= " << ref_tilt[dir_counter] << 
+				   " psi= "  << my_psi <<" CC= "<<thisCC<<endl;
+				   " xmax= "  << xmax <<" ymax= "<<ymax<<endl;
+		 #endif
+		 #undef DEBUG
+		 if (thisCC>maxCC) {
+   		    xmax = XX(shift_vector[ishift]);
+		    ymax = YY(shift_vector[ishift]);
+		    maxCC=thisCC;
+		    opt_psi=my_psi;
+		    opt_dirno=dir_counter;
+                    opt_xoff=-xmax*COSD(opt_psi)-ymax*SIND(opt_psi);
+                    opt_yoff=xmax*SIND(opt_psi)-ymax*COSD(opt_psi);
+                    }//if (thisCC>maxCC)
+
+		 }//if ( (ABS(real
+	    }//for dir_counter
+       }// for ishift
+     }//for ipsi
+     
+  // Interpolated translational search for optimal angles ===================================
+
+	 
+	 
+  #define DEBUG
+  #ifdef DEBUG
+  cout <<  SFref.get_file_number(opt_dirno)<< 
+	          " rot= "  << ref_rot[opt_dirno]  << 
+	          " tilt= " << ref_tilt[opt_dirno] << 
+		  " psi= "  << opt_psi <<
+                  " opt_xoff= "  << opt_xoff <<
+  		  " opt_yoff= "  << opt_yoff <<
+		  " CC= "<<maxCC<<endl;
+  #endif
+  #undef DEBUG
+  //#define DEBUG
+  #ifdef DEBUG
+  FileName fn_tmp;
+  ImageXmipp save; 
+  save()=Mimg;
+  fn_tmp.compose("tmp",ipsi,"xmp");
+  save.write(fn_tmp);
+  #endif
+  #undef DEBUG
+} 
+void Prog_projection_matching_crystal_prm::PM_loop_over_all_images( DocFile &DFo, 
+							   double &sumCC) {
+  // Loop over all images
+  int imgno=0,nn;
+  double opt_psi,opt_xoff,opt_yoff,maxCC,Zscore;
+  int opt_dirno;
+  ImageXmipp img;
+  FileName fn_img,fn_tmp;
+  sumCC=0.;
+
+  // Initialize
+  nn=SFexp.ImgNo();
+  init_progress_bar(nn);
+  
+  SFexp.go_beginning();
+  while ((!SFexp.eof())) {
+    imgno++;
+    fn_img=SFexp.NextImg();
+    img.read(fn_img,false,false,false,true);
+    img().set_Xmipp_origin();
+    // Perform the projection matching for each image separately
+    PM_process_one_image(img(),img.Phi(),img.Theta(),img.Psi(),
+			 opt_dirno,opt_psi,opt_xoff,opt_yoff,maxCC,Zscore);
+    }
+       
+
+#ifdef NEVERDEFINED
+  ImageXmipp img;
+  FourierImageXmipp IMG;
+  matrix1D<double> dataline(8);  
+  double opt_psi,opt_xoff,opt_yoff,maxCC,Zscore;
+
+  if (verb>0) cerr << "--> Projection matching ... "<<endl;
+
+  // Initialize
+  nn=SF.ImgNo();
+  if (verb>0) init_progress_bar(nn);
+  c=MAX(1,nn/60);
+  
+  // Loop over all images
+  sumCC=0.;
+  imgno=0;
+  SF.go_beginning();
+  while ((!SF.eof())) {
+    fn_img=SF.NextImg();
+    if(fourier_input){
+       IMG.read(fn_img); IMG().set_Xmipp_origin();
+    }
+    else{//apply only shifts              **
+       img.read(fn_img,false,false,false,true);
+       img().set_Xmipp_origin();
+    }
+       
+    // Perform the projection matching for each image separately
+    PM_process_one_image(img(),img.Phi(),img.Theta(),img.Psi(),
+			 opt_dirno,opt_psi,opt_xoff,opt_yoff,maxCC,Zscore);
+
+    opt_xoff+=img.Xoff();
+    opt_yoff+=img.Yoff();
+
+    sumCC+=maxCC;
+    dataline(0)=ref_rot[opt_dirno];      // rot
+    dataline(1)=ref_tilt[opt_dirno];     // tilt
+    dataline(2)=opt_psi;                 // psi
+    dataline(3)=opt_xoff;                // Xoff
+    dataline(4)=opt_yoff;                // Yoff
+    dataline(5)=opt_dirno+1;             // optimal direction number
+    dataline(6)=maxCC;                   // maximum CC
+    dataline(7)=Zscore;                   // maximum CC
+    DFo.append_comment(img.name());
+    DFo.append_data_line(dataline);
+
+    if (modify_header) {
+      // Re-read image to get the untransformed image matrix again
+      img.read(fn_img);
+      img.set_eulerAngles(ref_rot[opt_dirno],ref_tilt[opt_dirno],opt_psi);
+      img.set_originOffsets(opt_xoff,opt_yoff);
+      img.write(fn_img);
+    }
+
+    if (verb>0) if (imgno%c==0) progress_bar(imgno);
+    imgno++;
+  }
+
+  if (verb>0) progress_bar(nn);
+  if (verb>0) cerr << " ================================================================="<<endl;
+
+  // free memory
+  free(ref_mean);
+  free(ref_rot);
+  free(ref_tilt);
+  ref_img.clear();
+#endif
+}
