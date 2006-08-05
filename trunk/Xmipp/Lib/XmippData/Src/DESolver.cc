6c957a14d58ff65f3471655c57846892667647db
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2006-08-05 04:30:55 +0000

    Generate the first solution within the allowed range (min,max)
    git-svn-id: file:///home/roberto/Git/svn@1348 1a721e8d-d38c-4e91-ab98-6a0be4d754af

--- trunk/Xmipp/Lib/XmippData/Src/DESolver.cc
+++ trunk/Xmipp/Lib/XmippData/Src/DESolver.cc
@@ -36,7 +36,7 @@ DESolver::~DESolver(void)
 }
 
 void DESolver::Setup(double *min,double *max,
-   int deStrategy,double diffScale,double crossoverProb)
+						int deStrategy,double diffScale,double crossoverProb)
 {
 	int i;
 
@@ -51,16 +51,10 @@ void DESolver::Setup(double *min,double *max,
 			Element(population,i,j) = RandomUniform(min[j],max[j]);
 
 		popEnergy[i] = 1.0E20;
-/*                if (nDim>40) {
-                cout << "Setup: ";
-		for (int j=0; j < nDim; j++)
-			cout << Element(population,i,j) << " ";
-                cout << endl;
-                }*/
 	}
 
 	for (i=0; i < nDim; i++)
-		bestSolution[i] = 0.0;
+		bestSolution[i] = (min[i]+max[i])/2.0;
 
 	switch (strategy)
 	{
@@ -116,20 +110,7 @@ bool DESolver::Solve(int maxGenerations)
 	for (generation=0;(generation < maxGenerations) && !bAtSolution;generation++)
 		for (int candidate=0; candidate < nPop; candidate++)
 		{
-/*                if (nDim>40) {
-                cout << "Candidate: ";
-		for (int j=0; j < nDim; j++)
-			cout << Element(population,candidate,j) << " ";
-                cout << endl;
-                }*/
 			(this->*calcTrialSolution)(candidate);
-/*                if (nDim>40) {
-                cout << "TrialSolution: ";
-		for (int j=0; j < nDim; j++)
-			cout << trialSolution[j] << " ";
-                cout << endl;
-                }
-                cout.flush();*/
 			trialEnergy = EnergyFunction(trialSolution,bAtSolution);
 
 			if (trialEnergy < popEnergy[candidate])
