05dea0fb425a23ec9ce6fa4db3b7490a6ff32c18
asolano <asolano@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-02-16 12:50:10 +0000

    Updated formatting.
    git-svn-id: file:///home/roberto/Git/svn@1449 1a721e8d-d38c-4e91-ab98-6a0be4d754af

--- trunk/Xmipp/Lib/XmippData/MultidimCommon.hh
+++ trunk/Xmipp/Lib/XmippData/MultidimCommon.hh
@@ -1,83 +1,113 @@
-/* Speed up ------------------------------------------------------------- */
-/**@name Macros for all multidimensional arrays
-   This macros are defined to allow high speed in critical parts of
-   your program. They shouldn't be used systemultiTically as usually there
-   is no checking on the correctness of the operation you are performing.
-   Speed comes from three facts: first, they are macros and no function
-   call is performed (although most of the critical functions are
-   inline functions), there is no checking on the correctness of the
-   operation (it could be wrong and you are not warned of it), and
-   destination vectors are not returned saving time in the copy
-   constructor and in the creation/destruction of temporary vectors.*/
-//@{
+/** defgroup MultidimMacros Macros for all multidimensional arrays
+ * 
+ * This macros are defined to allow high speed in critical parts of your
+ * program. They shouldn't be used systematically as usually there is no
+ * checking on the correctness of the operation you are performing.
+ * 
+ * Speed comes from three facts: first, they are macros and no function call is
+ * performed (although most of the critical functions are inline functions),
+ * there is no checking on the correctness of the operation (it could be wrong
+ * and you are not warned of it), and destination vectors are not returned
+ * saving time in the copy constructor and in the creation/destruction of
+ * temporary vectors
+ */
 
-/** Access to dimension (size).*/
+/** Access to dimension (size)
+ * @ingroup MultidimMacros
+ */
 #ifndef MULTIDIM_SIZE
-   #define MULTIDIM_SIZE(v) ((v).__dim)
+#define MULTIDIM_SIZE(v) ((v).__dim)
 #endif
 
-/** Dimension of the space in which it is defined (1D, 2D or 3D).*/
+/** Dimension of the space in which it is defined (1D, 2D or 3D)
+ * @ingroup MultidimMacros
+ */
 #ifndef SPACE_DIM
-   #define SPACE_DIM(v) ((v).__spcdim)
+#define SPACE_DIM(v) ((v).__spcdim)
 #endif
 
-/** For all elements in the array.
-    This macro is used to generate loops for the array in an easy manner.
-    It defines an internal index 'i' which ranges the array using its
-    physical definition
-    \\Ex:
-    \begin{verbatim}
-    FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY(v) 
-       cout << MULTIDIM_ELEM(v,i) << " ";
-    \end{verbatim}*/
+/** For all elements in the array
+ * @ingroup MultidimMacros
+ * 
+ * This macro is used to generate loops for the array in an easy manner. It
+ * defines an internal index 'i' which ranges the array using its physical
+ * definition
+ * 
+ * @code
+ * FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY(v)
+ *     std::cout << MULTIDIM_ELEM(v, i) << " ";
+ * @endcode
+ */
 #ifndef FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY
-   #define FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY(v) \
-       for (int i=0; i<(v).__dim; i++)
+#define FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY(v) \
+    for (int i=0; i<(v).__dim; i++)
 #endif
 
-/** For all elements in the array (pointer version).
-    This macro is used to generate loops for the array in an easy manner.
-    It uses an external pointer 'ptr' which ranges the array using its
-    physical definition
-    \\Ex:
-    \begin{verbatim}
-    T* ptr;
-    FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY_ptr(v) 
-       cout << *ptr << " ";
-    \end{verbatim}*/
+/** For all elements in the array (pointer version)
+ * @ingroup MultidimMacros
+ * 
+ * This macro is used to generate loops for the array in an easy manner. It
+ * uses an external pointer 'ptr' which ranges the array using its physical
+ * definition
+ * 
+ * @code
+ * T* ptr;
+ * FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY_ptr(v)
+ *     std::cout << *ptr << " ";
+ * @endcode
+ */
 #ifndef FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY_ptr
-   #define FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY_ptr(v) \
-       for (int i=0, ptr=__m; i<(v).__dim; i++, ptr++)
+#define FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY_ptr(v) \
+    for (int i=0, ptr=__m; i<(v).__dim; i++, ptr++)
 #endif
 
-/** Vector element: Physical access.
-    Be careful because this is physical access, usually vectors follow
-    the C convention of starting index==0.
-    \\ Ex: MULTIDIM_ELEM(v,0)=1;
-    \\ Ex: val=MULTIDIM_ELEM(v,0);*/
+/** Vector element: Physical access
+ * @ingroup MultidimMacros
+ * 
+ * Be careful because this is physical access, usually vectors follow the C
+ * convention of starting index==0
+ * 
+ * @code
+ * MULTIDIM_ELEM(v, 0) = 1;
+ * val = MULTIDIM_ELEM(v, 0);
+ * @endcode
+ */
 #ifndef MULTIDIM_ELEM
-   #define MULTIDIM_ELEM(v,i) ((v).__m[(i)])
+#define MULTIDIM_ELEM(v, i) ((v).__m[(i)])
 #endif
 
-/** Array access.
-    This macro gives you access to the array (T *).
-    \\ Ex: cout << "This is an int *" << MULTIDIM_ARRAY(v) << endl; */
+/** Array access
+ * @ingroup MultidimMacros
+ * 
+ * This macro gives you access to the array (T *)
+ * 
+ * @code
+ * std::cout << "This is an int *" << MULTIDIM_ARRAY(v) << std::endl;
+ * @endcode
+ */
 #ifndef MULTIDIM_ARRAY
-   #define MULTIDIM_ARRAY(v) ((v).__m)
+#define MULTIDIM_ARRAY(v) ((v).__m)
 #endif
-//@}
 
-/* Other useful functions -------------------------------------------------- */
-/**@name Functions for all multidimensional arrays */
-//@{
-/** Conversion from one type to another.
-    If we have an integer vector and we need a double one, we can use this
-    function. The conversion is done through a type casting of each element.*/
-template <class T, class T1>
-   void type_cast(const maT &v1, maT1 &v2) {
-         if (MULTIDIM_SIZE(v1)==0) {v2.clear(); return;}
-         v2.resize(v1);
-         FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY(v2)
-            MULTIDIM_ELEM(v2,i)=(T1) MULTIDIM_ELEM(v1,i);
-      }
-//@}
+/// @defgroup MultidimFunctions Functions for all multidimensional arrays
+
+/** Conversion from one type to another
+ * @ingroup MultidimFunctions
+ * 
+ * If we have an integer vector and we need a double one, we can use this
+ * function. The conversion is done through a type casting of each element
+ */
+template<typename T, typename T1>
+void type_cast(const maT& v1, maT1& v2)
+{
+    if (MULTIDIM_SIZE(v1) == 0)
+    {
+        v2.clear();
+        return;
+    }
+    
+    v2.resize(v1);
+    
+    FOR_ALL_ELEMENTS_IN_MULTIDIM_ARRAY(v2)
+            MULTIDIM_ELEM(v2, i) = (T1) MULTIDIM_ELEM(v1, i);
+}
