c51429ef0d45fa5eec08a8ad2220b4bd75bc555e
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-04-02 20:17:53 +0000

    solve identation problem
    git-svn-id: file:///home/roberto/Git/svn@1571 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: f47b4d7e963e3c215933155e13165b64074ede00

--- trunk/Xmipp/Applications/Batches/Protocols_lib/launch_parallel_job.py
+++ trunk/Xmipp/Applications/Batches/Protocols_lib/launch_parallel_job.py
@@ -1,28 +1,28 @@
-    def launch_parallel_job(mpiprogramname,
-                            params,
-                            ParallelScript,
-                            LaunchJobCommand,
-                            log,
-                            MyNumberOfCPUs,
-                            MyMachineFile):
-        import os
-        fh=open(ParallelScript,'r')
-        lines=fh.readlines()
-        newlines=[]
-        for line in lines:
-            line=line.replace('MyNumberOfCPUs',str(MyNumberOfCPUs))
-            line=line.replace('MyMachineFile' ,str(MyMachineFile))
-            newlines+=line 
-        line="`which "+ str(mpiprogramname)+"` "+params
-        newlines+=line
-        scriptname=str(WorkingDir)+'.script'
-        fh=open(scriptname,'w')
-        fh.writelines(newlines)
-        os.chmod(scriptname,0777)
-        if (LaunchJobCommand==""):
-            command=scriptname+' & \n' 
-        else:
-            command=LaunchJobCommand + ' ' + scriptname + ' & \n' 
-        print '* ',command
-        log.info(command)
-        os.system(command)
+def launch_parallel_job(mpiprogramname,
+                        params,
+                        ParallelScript,
+                        LaunchJobCommand,
+                        log,
+                        MyNumberOfCPUs,
+                        MyMachineFile):
+    import os
+    fh=open(ParallelScript,'r')
+    lines=fh.readlines()
+    newlines=[]
+    for line in lines:
+        line=line.replace('MyNumberOfCPUs',str(MyNumberOfCPUs))
+        line=line.replace('MyMachineFile' ,str(MyMachineFile))
+        newlines+=line 
+    line="`which "+ str(mpiprogramname)+"` "+params
+    newlines+=line
+    scriptname=str(WorkingDir)+'.script'
+    fh=open(scriptname,'w')
+    fh.writelines(newlines)
+    os.chmod(scriptname,0777)
+    if (LaunchJobCommand==""):
+        command=scriptname+' & \n' 
+    else:
+        command=LaunchJobCommand + ' ' + scriptname + ' & \n' 
+    print '* ',command
+    log.info(command)
+    os.system(command)
