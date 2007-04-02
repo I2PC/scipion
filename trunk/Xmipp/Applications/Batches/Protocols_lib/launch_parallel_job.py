a4beb72da3a458623d620104c8ab4b5cd3d1d2a1
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-04-02 13:22:54 +0000

    proramecreation
    git-svn-id: file:///home/roberto/Git/svn@1564 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: f5175ffe66d346cf00ed650271ddd041ef84bf2b

---
+++ trunk/Xmipp/Applications/Batches/Protocols_lib/launch_parallel_job.py
@@ -0,0 +1,29 @@
+    def launch_parallel_job(mpiprogramname,
+                            params,
+                            ParallelScript,
+                            mpi_script_name,
+                            LaunchJobCommand,
+                            log,
+                            MyNumberOfCPUs,
+                            MyMachineFile):
+        import os
+        fh=open(ParallelScript,'r')
+        lines=fh.readlines()
+        newlines=[]
+        for line in lines:
+            line=line.replace('MyNumberOfCPUs',str(MyNumberOfCPUs))
+            line=line.replace('MyMachineFile' ,str(MyMachineFile))
+            newlines+=line 
+        line="`which "+ str(mpiprogramname)+"` "+params
+        newlines+=line
+        scriptname=str(WorkingDir)+'.script'
+        fh=open(scriptname,'w')
+        fh.writelines(newlines)
+        os.chmod(scriptname,0777)
+        if (LaunchJobCommand==""):
+            command=scriptname+' & \n' 
+        else:
+            command=LaunchJobCommand + ' ' + scriptname + ' & \n' 
+        print '* ',command
+        log.info(command)
+        os.system(command)
