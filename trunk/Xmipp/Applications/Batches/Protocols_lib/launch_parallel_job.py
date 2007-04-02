a876542fff7a0c9b5e35961f52578cabddbb10e7
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-04-02 21:10:04 +0000

    close all the open files efore executing script or 'busy text file' error will appear
    git-svn-id: file:///home/roberto/Git/svn@1574 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: d18ca18fc807eec9edb2ec042217461b50abff4c

--- trunk/Xmipp/Applications/Batches/Protocols_lib/launch_parallel_job.py
+++ trunk/Xmipp/Applications/Batches/Protocols_lib/launch_parallel_job.py
@@ -5,25 +5,38 @@ def launch_parallel_job(mpiprogramname,
                         log,
                         MyNumberOfCPUs,
                         MyMachineFile,
-                        WorkingDir):
+                        WorkingDir,
+                        RunInBackground=True):
     import os
+
     fh=open(ParallelScript,'r')
     lines=fh.readlines()
-    newlines=[]
+    newlines=""
     for line in lines:
         line=line.replace('MyNumberOfCPUs',str(MyNumberOfCPUs))
         line=line.replace('MyMachineFile' ,str(MyMachineFile))
-        newlines+=line 
+        if len(line)>0:
+           newlines+=line 
+    print newlines
     line="`which "+ str(mpiprogramname)+"` "+params
+    print line
     newlines+=line
-    scriptname=str(WorkingDir)+'.script'
+    scriptname=str(WorkingDir)+'/'+mpiprogramname+'.script'
+    fh.close()
     fh=open(scriptname,'w')
     fh.writelines(newlines)
+    fh.close()
     os.chmod(scriptname,0777)
     if (LaunchJobCommand==""):
-        command=scriptname+' & \n' 
+        command=scriptname
+        if RunInBackground==False:
+           command = command + ' &'
+        command = command + '\n'    
     else:
-        command=LaunchJobCommand + ' ' + scriptname + ' & \n' 
+        command=LaunchJobCommand + ' ' + scriptname
+        if RunInBackground==False:
+           command = command + ' &'
+        command = command + '\n'    
     print '* ',command
     log.info(command)
     os.system(command)
