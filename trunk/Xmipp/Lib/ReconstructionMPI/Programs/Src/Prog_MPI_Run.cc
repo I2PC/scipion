0bd156c21ab86a30d93d8a6f52fdc9c87e9cfdef
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-04-05 14:52:31 +0000

    This program cannot be executed in a single mode, test it
    git-svn-id: file:///home/roberto/Git/svn@1605 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: b86c6c6ce9eba28c4bb01b89b35b66e42e7d8ea9

--- trunk/Xmipp/Lib/ReconstructionMPI/Programs/Src/Prog_MPI_Run.cc
+++ trunk/Xmipp/Lib/ReconstructionMPI/Programs/Src/Prog_MPI_Run.cc
@@ -35,7 +35,8 @@
 Prog_MPI_Run_Parameters::Prog_MPI_Run_Parameters(int argc, char **argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &(nprocs));
   MPI_Comm_rank(MPI_COMM_WORLD, &(rank));
-  
+  if(nprocs<2) 
+     error_exit("This program cannot be executed in a single working node");
   //Blocks until all process have reached this routine.
   MPI_Barrier(MPI_COMM_WORLD);
 }
