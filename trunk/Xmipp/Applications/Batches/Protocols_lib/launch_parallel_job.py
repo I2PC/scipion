1cf3c94aabb4e8063a14cf7bdae2f1ff008792e5
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-04-11 10:04:04 +0000

    Be prepared for empty machinefiles
    git-svn-id: file:///home/roberto/Git/svn@1637 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: e4fa1641ec35e27a4b7972b80465a7e125efbad7

--- trunk/Xmipp/Applications/Batches/Protocols_lib/launch_parallel_job.py
+++ trunk/Xmipp/Applications/Batches/Protocols_lib/launch_parallel_job.py
@@ -31,8 +31,7 @@ def launch_sequential_job(programname,
     command=programname+' '+params
     if RunInBackground==True:
         command += ' &'
-    command += '\n'    
-    print '* ',command
+    print '* ',command,'\n'
     log.info(command)
     os.system(command)
    
@@ -46,16 +45,21 @@ def launch_parallel_job(mpiprogramname,
 
     import os
 
-    if (MyMachineFile[0]=="$"):
+    if (len(MyMachineFile)==0):
+        machineParam=""
+	nr_cpus=MyNumberOfCPUs
+    elif (MyMachineFile[0]=="$"):
         machinefile=os.environ.get(MyMachineFile[1:])
         # Get the real number of CPUs available
         fh = open(machinefile)
         lines = fh.readlines()
         fh.close()
         nr_cpus=len(lines)
+	machineParam=' -machinefile ' + machinefile
     else:
         machinefile=MyMachineFile
         nr_cpus=MyNumberOfCPUs
+	machineParam=' -machinefile ' + machinefile
 
     command='mpirun -np ' + str(nr_cpus)
     if not machinefile=="":
@@ -64,8 +68,8 @@ def launch_parallel_job(mpiprogramname,
 
     if RunInBackground==True:
         command = command + ' &'
-    command = command + '\n'    
+    command = command
 
-    print '* ',command
+    print '* ',command,'\n'
     log.info(command)
     os.system(command)
