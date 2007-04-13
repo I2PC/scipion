6eb5f4e90d17d0450280b6df8b4dd141facbba0b
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-04-13 14:36:51 +0000

    dont do anything if length Names==0
    git-svn-id: file:///home/roberto/Git/svn@1665 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: de51a92f7cbb493c1fd3aacb97d376d00a4bc2da

--- trunk/Xmipp/Applications/Batches/Protocols_lib/visualization.py
+++ trunk/Xmipp/Applications/Batches/Protocols_lib/visualization.py
@@ -1,50 +1,52 @@
 # Visualize a list of volumes using Xmipp or Chimera
 def visualize_volumes(Names,ShowSliceZ,ShowSliceX,ShowSliceY,ShowChimera):
     import os
-    chimera_command='chimera '
-    xmipp_command='xmipp_show -vol '
-    for name in Names:
-        if not os.path.exists(name):
-            print '* WARNING: '+name+' does not exist, skipping...'
-        else:
-            if (ShowSliceZ):
-                xmipp_command+=name+' '
-            if (ShowSliceX):
-                xmipp_command+=name+'x '
-            if (ShowSliceY):
-                xmipp_command+=name+'y '
+    if (len(Names)>0):
+        chimera_command='chimera '
+        xmipp_command='xmipp_show -vol '
+        for name in Names:
+            if not os.path.exists(name):
+                print '* WARNING: '+name+' does not exist, skipping...'
+            else:
+                if (ShowSliceZ):
+                    xmipp_command+=name+' '
+                if (ShowSliceX):
+                    xmipp_command+=name+'x '
+                if (ShowSliceY):
+                    xmipp_command+=name+'y '
+                if (ShowChimera):
+                    chimera_command+='spider:'+name+' '
+
+            if (ShowSliceZ or ShowSliceX or ShowSliceY): 
+                xmipp_command+=' &'
+                print '* ',xmipp_command
+                os.system(xmipp_command)
+
             if (ShowChimera):
-                chimera_command+='spider:'+name+' '
-
-    if (ShowSliceZ or ShowSliceX or ShowSliceY): 
-        xmipp_command+=' &'
-        print '* ',xmipp_command
-        os.system(xmipp_command)
-
-    if (ShowChimera):
-        chimera_command+=' &'
-        print '* ',chimera_command
-        os.system(chimera_command)
+                chimera_command+=' &'
+                print '* ',chimera_command
+                os.system(chimera_command)
 
 def visualize_images(Names,AreSelFiles=False,SelFileWidth="",SelFileHeight=""):
     import os
-    if (AreSelFiles):
-        command='xmipp_show '
-        if not (SelFileWidth==""):
-            command+=' -w '+str(SelFileWidth)
-        if not (SelFileHeight==""):
-            command+=' -h '+str(SelFileHeight)
-        command+=' -sel '
-    else:
-        command='xmipp_show -img '
-
-    for name in Names:
-        if not os.path.exists(name):
-            print '* WARNING: '+name+' does not exist, skipping...'
+    if (len(Names)>0):
+        if (AreSelFiles):
+            command='xmipp_show '
+            if not (SelFileWidth==""):
+                command+=' -w '+str(SelFileWidth)
+            if not (SelFileHeight==""):
+                command+=' -h '+str(SelFileHeight)
+            command+=' -sel '
         else:
-            command+=name+' '
-    command+=' &'
-    print '* ',command
-    os.system(command)
+            command='xmipp_show -img '
+        
+        for name in Names:
+            if not os.path.exists(name):
+                print '* WARNING: '+name+' does not exist, skipping...'
+            else:
+                command+=name+' '
+        command+=' &'
+        print '* ',command
+        os.system(command)
 
 
