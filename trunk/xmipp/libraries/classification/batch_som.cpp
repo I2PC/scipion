6d7d3ea5e36abb6996ad298a3bb72bef99208d13
asolano <asolano@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-04-17 10:09:04 +0000

    Changed all file names. Still requires changes for compilation.
    git-svn-id: file:///home/roberto/Git/svn@1692 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: cf8c290e142b3017d91323bd2bd68a9068835f34

--- trunk/xmipp/libraries/classification/batch_som.cpp
+++ trunk/xmipp/libraries/classification/batch_som.cpp
@@ -6,21 +6,21 @@
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or   
- * (at your option) any later version.                                 
- *                                                                     
- * This program is distributed in the hope that it will be useful,     
- * but WITHOUT ANY WARRANTY; without even the implied warranty of      
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
- * GNU General Public License for more details.                        
- *                                                                     
- * You should have received a copy of the GNU General Public License   
- * along with this program; if not, write to the Free Software         
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
- * 02111-1307  USA                                                     
- *                                                                     
- *  All comments concerning this program package may be sent to the    
- *  e-mail address 'xmipp@cnb.uam.es'                                  
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.uam.es'
  ***************************************************************************/
 
 /*-----------------------------------------------------------------------------
@@ -44,7 +44,7 @@
 
   /**
    * Trains the SOM
-   * @param _som  The som to train           
+   * @param _som  The som to train
    * @param _ts   The training set
    */
   void xmippBatchSOM::train (xmippMap& _som, const xmippCTVectors& _ts) const
@@ -55,42 +55,42 @@
 
     int verbosity = listener->getVerbosity();
     if (verbosity)
-  	listener->OnReportOperation((string) "Batch Training Kohonen SOM....\n");  
-    if (verbosity == 1 || verbosity == 3)    
+  	listener->OnReportOperation((string) "Batch Training Kohonen SOM....\n");
+    if (verbosity == 1 || verbosity == 3)
       	listener->OnInitOperation(somNSteps);
 
-    SomIn aveVector(_som.theItems[0].size());	 
+    SomIn aveVector(_som.theItems[0].size());	
     vector<unsigned> tmpVector;
-    
+
     while (t < somNSteps)
     {
-	    _som.classify(&_ts); 
-	    // Check for each SOM unit    
+	    _som.classify(&_ts);
+	    // Check for each SOM unit
 	    for (unsigned it = 0; it < _som.size(); it++) {
 	  	for (unsigned a = 0; a < aveVector.size(); a++)
-	  	   aveVector[a] = 0.;   
+	  	   aveVector[a] = 0.;
 	  	long total = 0;
 	  	   // Collects a list of the input vectors assigned to the neighborhood
 	  	vector<unsigned> neig=_som.neighborhood(_som.indexToPos(it), ceil(somRadius(t, somNSteps)));
 	  	for (vector<unsigned>::iterator itt = neig.begin();itt<neig.end();itt++)
 	  	{
-	  		tmpVector =  _som.classifAt(*itt);	  
+	  		tmpVector =  _som.classifAt(*itt);	
 	  		for (unsigned j=0 ; j < tmpVector.size() ; j++) {
 	  			SomIn v = _ts.theItems[tmpVector[j]];
 	  			for (unsigned a = 0; a < v.size(); a++)
-	  				aveVector[a] += v[a];   
+	  				aveVector[a] += v[a];
 	  			total++;
 	  		}
 
 	  	 }
-		 if (total != 0) {	  
+		 if (total != 0) {	
 	  	 	for (unsigned a = 0; a < aveVector.size(); a++)
-	  	     		aveVector[a] /= (xmippFeature) total;   
+	  	     		aveVector[a] /= (xmippFeature) total;
 	  	 	_som.theItems[it] = aveVector;
 		 }
 	    }
 
-      	    if (verbosity == 1 || verbosity == 3)    
+      	    if (verbosity == 1 || verbosity == 3)
       		listener->OnProgress(t);
       	    if (verbosity >= 2) {
 	   	char s[100];
@@ -98,12 +98,12 @@
   	   	listener->OnReportOperation((string) s);
       	    }	
 	    t++;
-	    
+	
     } // while t < somSteps
 
-    if (verbosity == 1 || verbosity == 3)    
+    if (verbosity == 1 || verbosity == 3)
         listener->OnProgress(somNSteps);
 
-  }; 
-  
+  };
+
 
