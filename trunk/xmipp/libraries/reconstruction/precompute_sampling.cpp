77812b98421de9d03c7b3fb617ad841dc0b94533
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-04-20 21:37:39 +0000

    new
    git-svn-id: file:///home/roberto/Git/svn@1710 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: c51763c0fc813c97f864e1a3e8a718b3b3df0b05

---
+++ trunk/xmipp/libraries/reconstruction/precompute_sampling.cpp
@@ -0,0 +1,89 @@
+/***************************************************************************
+ *
+ * Authors:
+ *
+ * Roberto Marabini 
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+
+#include "precompute_sampling.h"
+
+/* Empty constructor ------------------------------------------------------- */
+Prog_Sampling_Parameters::Prog_Sampling_Parameters() {
+   /** sampling object 1 by default*/
+   mysampling.SetSampling(1);
+}
+
+
+/* Read parameters --------------------------------------------------------- */
+void Prog_Sampling_Parameters::read(int argc, char **argv) {
+   int i=0;
+#ifdef NEVERDEFINED
+   fn_pdb=get_param(argc,argv,"-i");
+   fn_out=get_param(argc,argv,"-o","");
+   if (fn_out=="") fn_out=fn_pdb.without_extension();
+   Ts=AtoF(get_param(argc,argv,"-sampling_rate","1"));
+   highTs=AtoF(get_param(argc,argv,"-high_sampling_rate","0.1"));
+   output_dim=AtoI(get_param(argc,argv,"-output_dim","-1"));
+   #endif
+}
+
+/* Usage ------------------------------------------------------------------- */
+void Prog_Sampling_Parameters::usage() {
+   cerr << "PDBphantom\n"
+        << "   -i <pdb file>                    : File to process\n"
+        << "  [-o <fn_root>]                    : Root name for output\n"
+        << "  [-sampling_rate <Ts=1>]           : Sampling rate (Angstroms/pixel)\n"
+        << "  [-high_sampling_rate <highTs=0.1>]: Sampling rate before downsampling\n"
+        << "  [-size <output_dim>]              : Final size in pixels (must be a power of 2)\n"
+        << "\n"
+        << "Example of use: Sample at 1.6A and limit the frequency to 10A\n"
+        << "   xmipp_pdbphantom -i 1o7d.pdb -sampling_rate 1.6\n"
+        << "   xmipp_fourierfilter -i 1o7d.vol -o 1o7d_filtered.vol -low_pass 10 -sampling 1.6 -fourier_mask raised_cosine 0.1\n"
+   ;
+}
+
+/* Show -------------------------------------------------------------------- */
+void Prog_Sampling_Parameters::show() {
+   cout
+        << "Sampling rate:      " << sampling    << endl
+        << "symmetry file name: " << symmetry_file << endl
+   ;
+}
+
+/* Run --------------------------------------------------------------------- */
+void Prog_Sampling_Parameters::run() {
+   sampling=5;
+   symmetry_file="ico.sym";
+   mysampling.SetSampling(sampling);
+   mysampling.Compute_sampling_points(false);
+   SL.read_sym_file(symmetry_file);
+   //fill vector with symmetry axis
+   mysampling.remove_redundant_points(SL);
+   #define DEBUG6
+   #ifdef DEBUG6
+       for (int i=0; i<mysampling.no_redundant_sampling_points_vector.size(); i++) {
+           cout << mysampling.no_redundant_sampling_points_vector[i].transpose() << " 1 1 " << endl;
+       }
+   #endif
+   #undef DEBUG6
+}
+
---
+++ trunk/xmipp/libraries/reconstruction/precompute_sampling.h
@@ -0,0 +1,76 @@
+/***************************************************************************
+ *
+ * Authors:     Carlos Oscar S. Sorzano (coss@cnb.uam.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.uam.es'                                  
+ ***************************************************************************/
+#ifndef _PROG_PDBPHANTOM_HH
+#define _PROG_PDBPHANTOM_HH
+
+#include "sampling.h"
+#include <reconstruction/symmetries.h>
+#include <data/args.h>
+#include <fstream>
+
+/**@name PDB Phantom program */
+//@{
+/* PDB Phantom Program Parameters ------------------------------------------ */
+/** Parameter class for the PDB Phantom program */
+class Prog_Sampling_Parameters {
+public:
+   /** sampling object */    
+   XmippSampling mysampling;
+
+   /** Sampling rate */
+   double sampling;
+
+   /** symmetry file */
+   FileName symmetry_file;
+
+   /** symmetry information **/
+   SymList  SL;
+   /** fil vector with symmetry axis */
+    vector <matrix1D<double> > symmetry_vectors;
+public:
+   /** Empty constructor */
+   Prog_Sampling_Parameters();
+
+   /** Read from a command line.
+       An exception might be thrown by any of the internal conversions,
+       this would mean that there is an error in the command line and you
+       might show a usage message. */
+   void read(int argc, char **argv);
+
+   /** Usage message.
+       This function shows the way of introducing this parameters. */
+   void usage();
+
+   /** Show parameters. */
+   void show();
+   
+   /** Run. */
+   void run();
+   /** get all directions related by symmetry to (1,0,0)  */
+   void get_sym_vectors(vector< matrix1D<double > > &sym_points);
+
+};
+//@}
+#endif
