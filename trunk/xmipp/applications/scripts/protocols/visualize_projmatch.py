29e744e53ad89441b6c306db18a0f088ac7c366b
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-04-26 09:30:25 +0000

    added polar plots
    git-svn-id: file:///home/roberto/Git/svn@1781 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 46f465d3aa276511cdf648a7e89806a2dd990d1e

--- trunk/xmipp/applications/scripts/protocols/visualize_projmatch.py
+++ trunk/xmipp/applications/scripts/protocols/visualize_projmatch.py
@@ -7,10 +7,15 @@
 #
 # This script requires that protocol_projmatch.py is in the current directory
 #
-# Author: Roberto Marabini, March 2007
+# Author: Sjors Scheres, March 2007
 #
 #------------------------------------------------------------------------------------------------
 # {section} Global parameters
+#------------------------------------------------------------------------------------------------
+#show results for iteration
+DisplayIterNo=1
+#------------------------------------------------------------------------------------------------
+# {section} Volume visualization
 #------------------------------------------------------------------------------------------------
 # Visualize volumes in slices along Z?
 VisualizeVolZ=False
@@ -25,219 +30,84 @@ VisualizeVolChimera=True
 # {expert} Width of Matrix-views (even value!):
 MatrixWidth=10
 #------------------------------------------------------------------------------------------------
-# {section} Visualization of seeds preparation steps
+# {section}Reference volume 
 #------------------------------------------------------------------------------------------------
-# Visualize the library projections and the averages of the grey-scale correction?
-DoVisualizeMatrixCorrectReference=True
-# Visualize the grey-scale corrected reference volume?
-DoVisualizeCorrectReference=True
-# Visualize the low-pass filtered reference volume?
-DoVisualizeFilteredReference=True
-# Visualize the library projections and the averages of the seed generation runs?
-DoVisualizeMatrixSeeds=True
-# Visualize the generated seeds?
-DoVisualizeGeneratedSeeds=True
-#------------------------------------------------------------------------------------------------
-# {section} Visualization of projmatch iterations
-#------------------------------------------------------------------------------------------------
-# Which iterations to visualize? (Separate numbers by comma's)
-SelectIterations="2"
-# Visualize the reference volumes for the given iterations?
-VisualizeprojmatchReferences=True
-# Visualize weighted 2D-averages?
-VisualizeprojmatchAvgs=True
-# Output to screen the number of particles that change optimal orientation/class?
-""" As described in Scheres et al. (2007) Nature Methods, 4, 27-29 
-"""
-DoShowMovingParticles=True
-# Output overall statistics of all iterations to screen?
-DoShowStatsAllIter=True
-# Visualize matrixview of library projections and weighted averages of the last iteration?
-VisualizeMatrixLastIter=True
+#show masked volume
+DisplayMask=False
+#Show projection maching library and alignes classes
+DisplayProjectionMatching=False
+#display angular distribution
+DisplayAngularDistribution=False
+#display reconstructed volume
+DisplayReconstruction=False
+
+
+
 #------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------
 # {end-of-header} USUALLY YOU DO NOT NEED TO MODIFY ANYTHING BELOW THIS LINE ...
 #------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------
 #
-import protocol_projmatch
-#
 class visualize_projmatch_class:
 
     #init variables
     def __init__(self,
-                 VisualizeVolZ,
-                 VisualizeVolX,
-                 VisualizeVolY,
-                 VisualizeVolChimera,
-                 MatrixWidth,
-                 DoVisualizeMatrixCorrectReference,
-                 DoVisualizeMatrixSeeds,
-                 DoVisualizeGeneratedSeeds,
-                 SelectIterations,
-                 VisualizeprojmatchReferences,
-                 VisualizeprojmatchAvgs,
-                 DoShowMovingParticles,
-                 DoShowStatsAllIter,
-                 VisualizeMatrixLastIter
-                 ):
+                _VisualizeVolZ,
+                _VisualizeVolX,
+                _VisualizeVolY,
+                _VisualizeVolChimera,
+                _DisplayIterNo,
+                _DisplayMask,
+                _DisplayProjectionMatching,
+                _DisplayReconstruction,
+                _MatrixWidth
+                ):
 	     
         import os,sys,shutil
-        scriptdir=os.path.expanduser('~')+'/trunk/Xmipp/Applications/Batches/Protocols_lib'
+        scriptdir=os.path.expanduser('~')+'/scripts/'
         sys.path.append(scriptdir) # add default search path
         import log
         import visualization
 
-
-        self.DoVisualizeMatrixCorrectReference=DoVisualizeMatrixCorrectReference
-        self.DoVisualizeCorrectReference=DoVisualizeCorrectReference
-        self.DoVisualizeFilteredReference=DoVisualizeFilteredReference
-        self.DoVisualizeGeneratedSeeds=DoVisualizeGeneratedSeeds
-        self.DoVisualizeMatrixSeeds=DoVisualizeMatrixSeeds
-
-        self.VisualizeprojmatchReferences=VisualizeprojmatchReferences
-        self.VisualizeprojmatchAvgs=VisualizeprojmatchAvgs
-        self.SelectIterations=SelectIterations
-        self.VisualizeMatrixLastIter=VisualizeMatrixLastIter;
-        self.DoShowMovingParticles=DoShowMovingParticles
-        self.DoShowStatsAllIter=DoShowStatsAllIter
-        self.ShowVolumes=[]
-        self.ShowSelfiles=[]
-
-        os.chdir(protocol_projmatch.WorkingDir)
-
-#        self.visualize_preparation()
-
-#        self.visualize_seed_generation()
-
-#        self.visualize_projmatch_iterations()
-        
-#        self.visualize_projmatch_overall()
-
-        # Do the actual visualization of volumes and selfiles
-#        visualization.visualize_volumes(self.ShowVolumes,VisualizeVolZ,VisualizeVolX,VisualizeVolY,VisualizeVolChimera)
-#        visualization.visualize_images(self.ShowSelfiles,True,MatrixWidth)
-
+        # import corresponding protocol
+        import protocol_projmatch
+        self._MatrixWidth=_MatrixWidth
+        self._WorkDirectory=protocol_projmatch.WorkDirectory
+        self._LogDir=protocol_projmatch.LogDir
+        self._ProjectDir=protocol_projmatch.ProjectDir
+        self._ReferenceVolume=protocol_projmatch.ReferenceVolume
+        self._multi_align2d_sel=protocol_projmatch.multi_align2d_sel
+        self._SelFileName=self._ProjectDir+'/'+str(protocol_projmatch.SelFileName)
+        self._ReferenceVolume=protocol_projmatch.ReferenceVolume
+        self.mylog=log.init_log_system(self._ProjectDir,
+                                       self._LogDir,
+                                       sys.argv[0],
+                                       self._WorkDirectory)
+        self._iteration_number=_DisplayIterNo
+        Iteration_Working_Directory=self._WorkDirectory+'/Iter_'+\
+                                   str(self._iteration_number)
+        self.ShowVolumes=[] 
+        self.ShowVolumes.append(self._ReferenceVolume)
+        os.chdir(Iteration_Working_Directory)
+        if (_DisplayMask):
+           visualization.visualize_volumes(self.ShowVolumes,
+                                                _VisualizeVolZ,
+                                                _VisualizeVolX,
+                                                _VisualizeVolY,
+                                                _VisualizeVolChimera)
+        self.ShowSelfiles=[] 
+        self.ShowSelfiles.append(self._multi_align2d_sel)
+        if (_DisplayProjectionMatching):
+            visualization.visualize_images(self.ShowSelfiles,
+                             True,
+                             self._MatrixWidth)
+        #if (_DisplayReconstruction):
+        #    self.visualize_Reconstruction(self._SomName,self._SpectraName)
+            
         # Return to parent dir
         os.chdir(os.pardir)
 
-    def visualize_preparation(self):
-        if (self.DoVisualizeMatrixCorrectReference):
-            dirname='CorrectGreyscale/'
-            selfile1=dirname+'corrected_reference_lib.sel'
-            selfile2=dirname+'corrected_reference_classes.sel'
-            self.join_selfiles(selfile1,selfile2,'matrixview_greyscale_correction.sel')
-            self.ShowSelfiles.append('matrixview_greyscale_correction.sel')
-        if (self.DoVisualizeCorrectReference):
-            self.ShowVolumes.append('corrected_reference.vol')
-        if (self.DoVisualizeFilteredReference):
-            self.ShowVolumes.append('filtered_reference.vol')
-
-    def visualize_seed_generation(self):
-        for i in range(protocol_projmatch.NumberOfReferences):
-            dirname='GenerateSeed_'+str(i+1)+'/'
-            outname='seeds_split_'+str(i+1)
-            if (self.DoVisualizeGeneratedSeeds):
-                seedname=dirname+outname+'_it00001.vol'
-                self.ShowVolumes.append(seedname)
-            if (self.DoVisualizeMatrixSeeds):
-                selfile1=dirname+outname+'_lib.sel'
-                selfile2=dirname+outname+'_it00001.sel'
-                matrixname='matrixview_'+outname+'.sel'
-                self.join_selfiles(selfile1,selfile2,matrixname)
-                self.ShowSelfiles.append(matrixname)
-
-
-    def visualize_projmatch_iterations(self):
-        import os,glob
-        iters=self.SelectIterations.split(',')
-        for iter in iters:
-            if (self.VisualizeprojmatchAvgs):
-                selfiles=glob.glob('Runprojmatch/projmatch_it'+str(iter).zfill(5)+'_vol?????.sel')
-                for selfile in selfiles:
-                    self.ShowSelfiles.append(selfile)
-
-            if (self.VisualizeprojmatchReferences):
-                volumes=glob.glob('Runprojmatch/projmatch_it'+str(iter).zfill(5)+'_vol?????.vol')
-                for volume in volumes:
-                    self.ShowVolumes.append(volume)
-
-    def visualize_projmatch_overall(self):
-
-        if (self.DoShowMovingParticles):
-            self.show_moving_particles()
-        
-        if (self.DoShowStatsAllIter):
-            self.show_statistics_alliter()
-
-        if (self.VisualizeMatrixLastIter):
-            self.visualize_matrix_last_iter()
-        
-    def visualize_matrix_last_iter(self):
-        import os,glob
-        selfiles=glob.glob('Runprojmatch/projmatch_it?????.sel')
-        if len(selfiles)==0:
-            print "No selfiles yet. Visualize only after job completion..."
-        else:
-            lastselfile=selfiles[-1]
-        selfile1='Runprojmatch/projmatch_lib.sel'
-        selfile2=lastselfile
-        self.join_selfiles(selfile1,selfile2,'matrixview_projmatch_lastiter.sel')
-        self.ShowSelfiles.append('matrixview_projmatch_lastiter.sel')
-            
-
-    def show_statistics_alliter(self):
-        import os,glob
-        logfiles=glob.glob('Runprojmatch/projmatch_it?????.log')
-        if len(logfiles)==0:
-            print "No logfiles yet. Visualize only after job completion..."
-        else:
-            for logfile in logfiles:
-                fh=open(logfile,'r')
-                line1=fh.readline()
-                line2=fh.readline()
-                fh.close()
-                words1=line1.split()
-                words2=line2.split()
-                print logfile,words1[6],words1[7],words1[8],words1[9],\
-                      words2[1],words2[2],words2[3],words2[4]
-
-    def show_moving_particles(self):
-        import os,glob
-        docfiles=glob.glob('Runprojmatch/projmatch_it?????.doc')
-        if os.path.exists('moving_particles.dat'):
-            os.remove('moving_particles.dat')
-        print 'The output of moving particles remains to be added to this script...'
-
-    def join_selfiles(self,selfile1,selfile2,newselfile):
-        import selfile
-        sel1=selfile.selfile()
-        sel2=selfile.selfile()
-        sel1.read(selfile1)
-        sel2.read(selfile2)
-        newsel=sel1.intercalate_union(sel2)
-        newsel.write(newselfile)
-
-    def show_last_iter(self):
-        # Visualize class averages:
-        import os,glob
-        selfiles=glob.glob('*_it?????.sel')
-        if len(selfiles)==0:
-            print "No selfiles yet. Visualize after job completion..."
-        else:
-            lastselfile=selfiles[-1]
-            command='xmipp_show -sel '+str(lastselfile)+ ' & '
-            print '* ',command
-            os.system(command)
-
-            # print logfile to screen:
-            logfiles=glob.glob('*_it?????.log')
-            lastlogfile=logfiles[-1]
-            fh=open(lastlogfile,'r')
-            loglines=fh.readlines()
-            print "Logfile "+str(lastlogfile)+": "
-            for line in loglines:
-                print line[:-1]
 
     def close(self):
         message='Done!'
@@ -251,20 +121,14 @@ if __name__ == '__main__':
 
     # create projmatch_class object
     visualize_projmatch=visualize_projmatch_class(VisualizeVolZ,
-                                        VisualizeVolX,
-                                        VisualizeVolY,
-                                        VisualizeVolChimera,
-                                        MatrixWidth,
-                                        DoVisualizeMatrixCorrectReference,
-                                        DoVisualizeMatrixSeeds,
-                                        DoVisualizeGeneratedSeeds,
-                                        SelectIterations,
-                                        VisualizeprojmatchReferences,
-                                        VisualizeprojmatchAvgs,
-                                        DoShowMovingParticles,
-                                        DoShowStatsAllIter,
-                                        VisualizeMatrixLastIter
-                                        )
+                                                  VisualizeVolX,
+                                                  VisualizeVolY,
+                                                  VisualizeVolChimera,
+                                                  DisplayIterNo,
+                                                  DisplayMask,
+                                                  DisplayProjectionMatching,
+                                                  DisplayReconstruction,
+                                                  MatrixWidth)
     # close 
     visualize_projmatch.close()
 
