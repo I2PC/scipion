c2901c5d1bfbe83ba89a10cc4754fca11d4206c0
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-05-07 15:16:51 +0000

    added option to display sort_junk.sumZ
    git-svn-id: file:///home/roberto/Git/svn@1851 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 887860ad679cae32e123845076fb67f8c7ebac36

--- trunk/xmipp/applications/scripts/protocols/visualize_preprocess_particles.py
+++ trunk/xmipp/applications/scripts/protocols/visualize_preprocess_particles.py
@@ -21,6 +21,8 @@ DoShowParticles=True
 # Visualize the individual particles, sorted by statistics?
 """ Most common particles will be displayed at the top, and outliers will be displayed at the bottom of the selection file"""
 DoShowSortedParticles=True
+# Visualize the Z-score used for particles sorting?
+DoShowZscore=True
 #------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------
 # {end-of-header} USUALLY YOU DO NOT NEED TO MODIFY ANYTHING BELOW THIS LINE ...
@@ -34,13 +36,13 @@ class visualize_particles_class:
                  DoShowCTFs,
                  DoShowParticles,
                  DoShowSortedParticles,
+                 DoShowZscore,
                  WorkingDir):
 	     
         import os,sys
         scriptdir=os.path.expanduser('~')+'/scripts/'
         sys.path.append(scriptdir)
         import visualization
-
 
         self.WorkingDir=WorkingDir
         os.chdir(self.WorkingDir)
@@ -58,6 +60,8 @@ class visualize_particles_class:
             ShowSelfiles.append(selfile)
         if (DoShowSortedParticles):
             ShowSelfiles.append('sort_junk.sel')
+        if (DoShowZscore):
+            self.show_z_score()
 
         visualization.visualize_images(ShowSelfiles,True)
 
@@ -80,6 +84,33 @@ class visualize_particles_class:
         print '* '
         os.system(command)
 
+    def show_z_score(self):
+        import os,sys
+        import visualization
+        if not os.path.exists('sort_junk.sumZ'):
+            message=" Error: sort_junk.sumZ file does not exists!"
+            print '* '+message
+            sys.exit()
+        else:
+            fh=open('sort_junk.sumZ','r')
+            lines=fh.readlines()
+            fh.close()
+            newlines=[]
+            i = 0
+            for line in lines:
+                words=line.split()
+                i = i + 1
+                newlines.append(str(i)+' '+words[0]+'\n')
+            fh=open('sort_junk.sumZ.dat','w')
+            fh.writelines(newlines)
+            fh.close()
+            plot1=visualization.gnuplot()
+            plot1.plot_xy_file('sort_junk.sumZ.dat',
+                               'Z-score used for particle sorting (high values identify strange particles',
+                               'particle number',
+                               'sumZ')
+
+
     def close(self):
         message='Done!'
         print '*',message
@@ -97,6 +128,7 @@ if __name__ == '__main__':
     visualize=visualize_particles_class(DoShowCTFs,
                                         DoShowParticles,
                                         DoShowSortedParticles,
+                                        DoShowZscore,
                                         WorkingDir)
     visualize.close()
 
