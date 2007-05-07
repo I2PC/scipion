31ae15f07bdd7073c00d61b34ee5a319387ad777
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-05-07 10:43:27 +0000

    created
    git-svn-id: file:///home/roberto/Git/svn@1845 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 81af7b40893ff69674c8d30b0528b1e459de5098

---
+++ trunk/xmipp/applications/scripts/protocols/visualize_preprocess_micrographs.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+#------------------------------------------------------------------------------------------------
+# Protocol for visualization of the results of protocol_preprocess_micrographs.py
+#
+# Example use:
+# python visualize_preprocess_micrographs.py
+#
+# This script requires that protocol_preprocess_micrographs.py is in the current directory
+#
+# Author: Sjors Scheres, March 2007
+#
+#------------------------------------------------------------------------------------------------
+# {section} Global parameters
+#------------------------------------------------------------------------------------------------
+# Visualize the power spectral densities of all micrographs?
+""" You may select bad micrographs by double clicking and save them as discarded in the same selfile
+"""
+DoShowPSDs=True
+#------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
+# {end-of-header} USUALLY YOU DO NOT NEED TO MODIFY ANYTHING BELOW THIS LINE ...
+#------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
+#
+class visualize_micrographs_class:
+
+    #init variables
+    def __init__(self,
+                 DoShowPSDs):
+	     
+        import os,sys,shutil
+        scriptdir=os.path.expanduser('~')+'/scripts/'
+        sys.path.append(scriptdir) # add default search path
+
+        # import corresponding protocol
+        import protocol_preprocess_micrographs
+        self.MicrographSelfile=protocol_preprocess_micrographs.MicrographSelfile
+
+        if (DoShowPSDs):
+            self.visualize_psds()
+            self.update_micrograph_selfile()
+            
+    def visualize_psds(self):
+        import os
+        print '*********************************************************************'
+        print '*  Visualizing all PSDs: '
+        command='xmipp_show -psdsel all_psds.sel'
+        print '* ',command
+        print '* '
+        print '* You may select bad micrographs and save them as discarded.'
+        print '* Make sure to use save this file again as "all_psds.sel'
+        print '* This way, the discarded micrographs will no longer be processed!'
+        os.system(command )
+
+    def update_micrograph_selfile(self):
+        import sys
+        import selfile
+        
+        sel1=selfile.selfile()
+        sel2=selfile.selfile()
+        sel3=selfile.selfile()
+        sel1.read(self.MicrographSelfile)
+        sel2.read('all_psds.sel')
+        
+        if not (sel1.length_even_no_actives() == sel2.length_even_no_actives()):
+            message='Error: all_psds.sel and '+str(self.MicrographSelfile)+' are of unequal length'
+            print '* ',message
+            sys.exit()
+        else:
+            for i in range(len(sel1.sellines)):
+                name1,state1=sel1.sellines[i]
+                name2,state2=sel2.sellines[i]
+                sel3.insert(name1,state2)
+            sel3.write(str(self.MicrographSelfile))
+            message='Updated '+str(self.MicrographSelfile)
+            print '* ',message
+
+    def close(self):
+        message='Done!'
+        print '*',message
+        print '*********************************************************************'
+
+#		
+# Main
+#     
+if __name__ == '__main__':
+
+    # create preprocess_micrographs_class object
+    visualize=visualize_micrographs_class(DoShowPSDs)
+    visualize.close()
+
---
+++ trunk/xmipp/applications/scripts/protocols/visualize_preprocess_particles.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+#------------------------------------------------------------------------------------------------
+# Protocol for visualization of the results of protocol_preprocess_particles.py
+#
+# Example use:
+# python visualize_preprocess_particles.py
+#
+# This script requires that protocol_preprocess_particles.py is in the current directory
+#
+# Author: Sjors Scheres, March 2007
+#
+#------------------------------------------------------------------------------------------------
+# {section} Global parameters
+#------------------------------------------------------------------------------------------------
+# Visualize the estimated CTF models for all micrographs?
+""" You may re-estimate CTFs by selecting a micrograph, and selecting re-estimate CTF from the right-mouse button menu 
+"""
+DoShowCTFs=True
+# Visualize the individual particles, sorted by micrograph?
+DoShowParticles=True
+# Visualize the individual particles, sorted by statistics?
+""" Most common particles will be displayed at the top, and outliers will be displayed at the bottom of the selection file"""
+DoShowSortedParticles=True
+#------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
+# {end-of-header} USUALLY YOU DO NOT NEED TO MODIFY ANYTHING BELOW THIS LINE ...
+#------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------
+#
+class visualize_particles_class:
+
+    #init variables
+    def __init__(self,
+                 DoShowCTFs,
+                 DoShowParticles,
+                 DoShowSortedParticles):
+	     
+        import os,sys
+        scriptdir=os.path.expanduser('~')+'/scripts/'
+        sys.path.append(scriptdir)
+        import visualization
+        import protocol_preprocess_particles
+
+        ShowSelfiles=[]
+        if (DoShowCTFs):
+            self.visualize_ctfs()
+        if (DoShowParticles):
+            selfile=protocol_preprocess_particles.ProjectDir+ '/' + \
+                     protocol_preprocess_particles.OutSelFile
+            ShowSelfiles.append(selfile)
+        if (DoShowSortedParticles):
+            ShowSelfiles.append('sort_junk.sel')
+
+        visualization.visualize_images(ShowSelfiles,True)
+            
+    def visualize_ctfs(self):
+        import os
+        print '*********************************************************************'
+        print '*  Visualizing all CTFs: '
+        command='xmipp_show -ctfsel all_ctfs.sel &'
+        print '* ',command
+        print '* '
+        print '* You may re-estimate CTFs by selecting the corresponding micrograph (double-clicking) '
+        print '* and then selecting re-estimate CTF from the right-mouse button menu '
+        print '* Adjust the red circle to coincide with the first zero in the PSD '
+        print '* After re-esimtation re-run the script (skipping CTF-estimation, '
+        print '* but performing extraction, normalization, phase flipping etc.) '
+        print '* '
+        os.system(command)
+
+    def close(self):
+        message='Done!'
+        print '*',message
+        print '*********************************************************************'
+
+#		
+# Main
+#     
+if __name__ == '__main__':
+
+    # create preprocess_particles_class object
+    visualize=visualize_particles_class(DoShowCTFs,
+                                        DoShowParticles,
+                                        DoShowSortedParticles)
+    visualize.close()
+
