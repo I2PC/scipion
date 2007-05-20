24029e5716d686fdd33affa01d8e8bbc44d9857f
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-05-20 20:26:06 +0000

    fix bug with selfile when empty selfile
    git-svn-id: file:///home/roberto/Git/svn@1951 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: fc999dcf650f9fc4cba0e29e54b6845b12fdf923

--- trunk/xmipp/libraries/reconstruction/basic_art.cpp
+++ trunk/xmipp/libraries/reconstruction/basic_art.cpp
@@ -522,7 +522,7 @@ void Basic_ART_Parameters::produce_Side_Info(GridVolume &vol_basis0, int level,
                  }
               SF_aux.NextImg();   
          }     
-         if(SF_aux.ImgNo()==0)
+         if(selfile.ImgNo()==0)
          {
            cerr << "there is no input file with weight!=0" << endl;
            exit(1);
