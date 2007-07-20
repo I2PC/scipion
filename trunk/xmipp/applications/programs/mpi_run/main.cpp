a24b1444565346901d82c4f07e4013bbbb3560be
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-07-20 23:44:56 +0000

    remove mpi library since it is very problematic
    git-svn-id: file:///home/roberto/Git/svn@2144 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: b9914ae9eb07f7c96382eb6114177de52f5b4e9e

--- trunk/xmipp/applications/programs/mpi_run/main.cpp
+++ trunk/xmipp/applications/programs/mpi_run/main.cpp
@@ -23,9 +23,190 @@
  *  e-mail address 'xmipp@cnb.uam.es'
  ***************************************************************************/
 
-#include <reconstruction_mpi/mpi_run.h>
+//#include "mpi_run.h"
 
+#include <data/args.h>
+
+#include <cstring>
+#include <cstdlib>
+#include <data/funcs.h>
 #include <mpi.h>
+
+#define TAG_WORK   0
+#define TAG_STOP   1
+#define TAG_WAIT   2
+class Prog_MPI_Run_Parameters
+{
+    public:
+        /** PDB file */
+        FileName fn_commands;
+
+        /** Number of Procesors **/
+        int nprocs;
+
+        /** computing node number. Master=0 */
+        int rank;
+
+        /** status after am MPI call */
+        MPI_Status status;
+    /** Empty constructor */
+    //Prog_MPI_Run_Parameters(int argc, char **argv);
+
+    /** Read from a command line.
+        An exception might be thrown by any of the internal conversions,
+        this would mean that there is an error in the command line and you
+        might show a usage message. */
+    //void read(int argc, char **argv);
+
+    /** Usage message.
+        This function shows the way of introducing this parameters. */
+    //void usage();
+
+    /** Show parameters. */
+    //void show();
+
+    /** Run. */
+    //void run();
+
+    /* Empty constructor ------------------------------------------------------- */
+    Prog_MPI_Run_Parameters(int argc, char **argv)
+    {
+        MPI_Comm_size(MPI_COMM_WORLD, &(nprocs));
+        MPI_Comm_rank(MPI_COMM_WORLD, &(rank));
+        if (nprocs < 2)
+            error_exit("This program cannot be executed in a single working node");
+        //Blocks until all process have reached this routine.
+        MPI_Barrier(MPI_COMM_WORLD);
+    }
+
+
+    /* Read parameters --------------------------------------------------------- */
+    void read(int argc, char **argv)
+    {
+        fn_commands = getParameter(argc, argv, "-i");
+    }
+
+    /* Usage ------------------------------------------------------------------- */
+    void usage()
+    {
+        cerr << "MPI_Run\n"
+        << "   -i <command file>    : File with commands to send to mpirun\n"
+        << "\n"
+        << "Example of use:\n"
+        << "   xmipp_mpi_run -i commandd_file\n"
+        ;
+    }
+
+    /* Show -------------------------------------------------------------------- */
+    void show()
+    {
+        cout << "Commands  file:           " << fn_commands << endl
+        ;
+    }
+
+
+    /* Run --------------------------------------------------------------------- */
+    void run()
+    {
+
+
+        if (rank == 0)
+        {
+            ifstream fh_in;
+            fh_in.open(fn_commands.c_str());
+            if (!fh_in)
+                REPORT_ERROR(1, (string)"Cannot open " + fn_commands);
+
+    #define MAX_LINE 1024
+            string line;
+            char szline[MAX_LINE];
+            int number_of_node_waiting = 0; // max is nprocs -1
+            while (!fh_in.eof())
+            {
+                //wait untill a server is free
+                MPI_Recv(0, 0, MPI_INT, MPI_ANY_SOURCE, 0,
+                         MPI_COMM_WORLD, &status);
+                number_of_node_waiting++;
+                getline(fh_in, line);
+                strcpy(szline, line.c_str());
+                string::size_type loc = line.find("MPI_Barrier", 0);
+                if (loc != string::npos)
+                {
+                    while (number_of_node_waiting < (nprocs - 1))
+                    {
+                        MPI_Recv(0, 0, MPI_INT, MPI_ANY_SOURCE, 0,
+                                 MPI_COMM_WORLD, &status);
+                        number_of_node_waiting++;
+                    }
+                    while (number_of_node_waiting > 0)
+                    {
+                        MPI_Send(&szline,
+                                 1,
+                                 MPI_CHAR,
+                                 number_of_node_waiting,
+                                 TAG_WAIT,
+                                 MPI_COMM_WORLD);
+                        number_of_node_waiting--;
+                    }
+                    continue;
+                }
+                //send work
+                MPI_Send(&szline,
+                         MAX_LINE,
+                         MPI_CHAR,
+                         status.MPI_SOURCE,
+                         TAG_WORK,
+                         MPI_COMM_WORLD);
+                number_of_node_waiting--;
+                //cout << line << endl;
+            }
+
+            fh_in.close();
+            for (int i = 1; i < nprocs; i++)
+            {
+                MPI_Send(0, 0, MPI_INT, i, TAG_STOP, MPI_COMM_WORLD);
+            }
+
+        }
+        else
+        {
+
+            while (1)
+            {
+                char szline[1024];
+                //any message from de master, is tag is TAG_STOP then stop
+                MPI_Send(0, 0, MPI_INT, 0, 0, MPI_COMM_WORLD);
+                //get yor next task
+                MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+                if (status.MPI_TAG == TAG_STOP)//I am free
+                {
+                    break;
+                }
+                if (status.MPI_TAG == TAG_WAIT)//I am free
+                {
+                    MPI_Recv(&szline, 1, MPI_CHAR, 0, TAG_WAIT, MPI_COMM_WORLD, &status);
+                    continue;
+                }
+                MPI_Recv(&szline, MAX_LINE, MPI_CHAR, 0, TAG_WORK, MPI_COMM_WORLD, &status);
+                cout << szline << endl;
+                //do the job
+                system(szline);
+
+
+            }
+        }
+
+        MPI_Finalize();
+    }
+
+    /* a short function to print a message and exit */
+    void error_exit(char * msg)
+    {
+        fprintf(stderr, "%s", msg);
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
+
+};
 
 int main(int argc, char *argv[])
 {
