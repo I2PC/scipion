753467b04c07dd82d22b68dfd3972607e092dc47
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-07-26 13:48:20 +0000

    implemented polar class
    git-svn-id: file:///home/roberto/Git/svn@2161 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 217c5d425243588a666e9edd3215f27abf4889c6

--- trunk/xmipp/libraries/data/polar.cpp
+++ trunk/xmipp/libraries/data/polar.cpp
@@ -31,162 +31,75 @@
  ***************************************************************************/
 #include "polar.h"
 
-
-vector<int> getPolarRingInfo2D(int first_ring, int last_ring, 
-			       int step, int mode, bool const_sam)
-{
-    vector<int> out;
-    out.clear();
-    int nsam, totsam = 0;
-    double twopi;
-
-    if (mode == FULL_CIRCLES)
-	twopi = 2. * PI;
-    else if (mode == HALF_CIRCLES)
-	twopi = PI;
-    else
-	REPORT_ERROR(1,"Incorrect mode for getPolarSampling2D!");
-
-    for (int iring = first_ring; iring <= last_ring; iring++)
-    {
-	if (!const_sam)
-	    // Oversample each ring twice
-	    nsam = MAX(1, (int)twopi * iring * 2);
-	else
-	    nsam = (int)twopi * last_ring * 2;
-	totsam += nsam;
-	out.push_back(iring);
-	out.push_back(totsam);
-	out.push_back(nsam);
-    }
-
-    return out;
-}
-
-vector<double> getPolarWeights2D(vector<int> ring_info, int mode) 
-{
-    vector<double> out;
-    out.clear();
-    double w,twopi;
-    
-    if (mode == FULL_CIRCLES)
-	twopi = 2. * PI;
-    else if (mode == HALF_CIRCLES)
-	twopi = PI;
-    else
-	REPORT_ERROR(1,"Incorrect mode for getPolarWeights2D!");
-
-    int nring = ring_info.size() / 3;
-    double maxsam = (double) ring_info[3 * nring + 2];
-    for (int iring = 0; iring < nring; iring++)
-    {
-	// copied from sparx
-	w = twopi * ring_info[3 * iring] / (double) ring_info[3 * iring + 2];
-	w *= maxsam / (double) ring_info[3 * iring + 2];
-	out.push_back(w);
-
-	// OR:Weight is reciprocal of number of sampling points???;
-	//w = 1. / ring_info[3 * iring + 2];
-	//out.push_back(w);
-    }
-
-    return out;
-}
-
-
-
 // Calculate 1D FFTs of all rings
-vector<Matrix1D<complex<double> > > fourierTransformRings(
-    const vector<Matrix1D<double> > &in, bool conjugated)
+Polar<complex<double> > Polar<double>::fourierTransformRings(bool conjugated)
 {
-    vector<Matrix1D<complex<double> > > out;
+    Polar<complex<double> > out;
     Matrix1D<complex<double> > Fring;
     out.clear();
-
-    for (int iring = 0; iring < in.size(); iring++)
+    for (int iring = 0; iring < rings.size(); iring++)
     { 
-	FourierTransform(in[iring],Fring);
+	FourierTransform(rings[iring],Fring);
 	if (conjugated)
 	{
 	    for (int i = 0; i < XSIZE(Fring); i++)
 		Fring(i)=conj(Fring(i));
 	}
-	out.push_back(Fring);
+	out.rings.push_back(Fring);
     }
 
+    out.mode = mode;
+    out.ring_radius = ring_radius;
     return out;
 }
 
 // Calculate 1D inverse FFTs of all rings
-vector<Matrix1D<double> > inverseFourierTransformRings(
-    const vector<Matrix1D<complex<double> > > &in)
+Polar<double> Polar<complex<double> >::inverseFourierTransformRings()
 {
-    vector<Matrix1D<double> > out;
-    Matrix1D<double > Mring;
+    Polar<double> out;
+    Matrix1D<double> Mring;
     out.clear();
-
-    for (int iring = 0; iring < in.size(); iring++)
+    for (int iring = 0; iring < rings.size(); iring++)
     { 
-	InverseFourierTransform(in[iring],Mring);
-	out.push_back(Mring);
+	InverseFourierTransform(rings[iring],Mring);
+	out.rings.push_back(Mring);
     }
-
+    out.mode = mode;
+    out.ring_radius = ring_radius;
     return out;
 }
 
-// Rotationally align 2 images (from FTs of their polar rings)
-double getBestAnglePolarGridding(const vector<Matrix1D<complex<double> > > &M1,
-				 const vector<Matrix1D<complex<double> > > &M2,
-				 double &maxcorr, bool interpolate)
+// Calculate rotational cross-correlation via convolution theorem
+void rotationalCrossCorrelation(const Polar<complex<double> > &M1,
+				const Polar<complex<double> > &M2,
+				Matrix1D<double> &angles, Matrix1D<double> &corr)
 {
+    Matrix1D<complex<double> > Fsum;
+    complex<double> aux;
 
-    vector<Matrix1D<complex<double> > > Fcorr;
-    vector<Matrix1D<double> > Mcorr;
-    Matrix1D<complex<double> > Fring;
-    Matrix1D<double> corr;
-
-    int nrings = M1.size();
-    int nrings2 = M2.size();
-    if (nrings != nrings2)
+    int nrings = M1.getRingNo();
+    if (nrings != M2.getRingNo())
 	REPORT_ERROR(1,"getBestAnglePolarGridding: polar structures have unequal number of rings!");
+    int nsam_last = M1.getSampleNo(nrings - 1);
 
-    int nsam_last = XSIZE(M1[nrings-1]);
-    Fcorr.clear();
-    
-    // Multiply M1 and M2 over all rings
+    // Multiply M1 and M2 over all rings and sum
+    Fsum.resize(nsam_last);
     for (int iring = 0; iring < nrings; iring++)
     { 
-	int nsam = XSIZE(M1[iring]);
-	Fring.resize(nsam_last);
+	int nsam = M1.getSampleNo(iring);
 	for (int i = 0; i < nsam; i++)
-	    Fring(i) = M1[iring](i) * M2[iring](i);
-	for (int i = nsam; i < nsam_last; i++)
-	    Fring(i) = 0.;
-	Fcorr.push_back(Fring);
-    }
-    
-    // InverseFourierTransform each colum of Mcorr
-    Mcorr=inverseFourierTransformRings(Fcorr);
-
-    corr.resize(nsam_last);
-    int iopt = 0;
-    maxcorr = -99.e99;
-    for (int i = 0; i < nsam_last; i++)
-    {
-	for (int iring = 0; iring < nrings; iring++)
-	{ 
-	    corr(i) += Mcorr[iring](i);
-	}
-	if (corr(i) > maxcorr)
 	{
-	    iopt = i;
-	    maxcorr = corr(i);
+	    // Multiply M1 with M2. Assume M2 is already complex conjugated!
+	    aux = M1(iring,i) * M2(iring,i);
+	    // Apply weights for distinct sizes of the rings
+	    Fsum(i) += (double)nsam * aux; 
 	}
     }
 
-    if (interpolate)
-	REPORT_ERROR(1,"getBestAnglePolarGridding: interpolation not yet implemented.");
+    // Inverse FFT to get real-space correlations
+    InverseFourierTransform(Fsum,corr);
+    angles.resize(nsam_last);
+    for (int i = 0; i < nsam_last; i++)
+	angles(i)=(double)i*360./nsam_last;
 
-    return (double)(360. * iopt / nsam_last);
-    
 }
--- trunk/xmipp/libraries/data/polar.h
+++ trunk/xmipp/libraries/data/polar.h
@@ -35,96 +35,374 @@
 #include "matrix2d.h"
 #include "gridding.h"
 
-#define HALF_CIRCLES 0
-#define FULL_CIRCLES 1
+#define FULL_CIRCLES 0
+#define HALF_CIRCLES 1
 
-/** Produces a vector of integers with the information for 2D polar
- *  coordinates generation.
- *  For each ring, this vector contains 3 numbers:
- *  [i*3]:   Radius of the ring
- *  [i*3+1]: Total number of samples of all inner rings
- *  [i*3+2]: Number of samples of this ring.
- */
-vector<int> getPolarRingInfo2D(int first_ring, int last_ring, 
-			       int step = 1, int mode = FULL_CIRCLES,
-			       bool const_sam = false);
-
-/** Produces a vector of doubles with the weights for the 2D polar
- *  coordinates. 
- *  
- *  These weights are ~ 1/radius
- */
-vector<double> getPolarWeights2D(vector<int> ring_info, int mode = FULL_CIRCLES);
-
-/** Converts input matrix2D M1 to an output vector, which is in polar
- *  coordinates, according to the ring structur as stored in ring_info.
- * 
- *  Note that the shifts options are only valid for real-space matrices.
- */
 template<typename T>
-vector<Matrix1D<T> > polarCoordinatesGridding(const Matrix2D<T> &M1, vector<int> ring_info,
-					      KaiserBessel &kb, int mode = FULL_CIRCLES,
-					      double xoff = 0., double yoff = 0.)
+class Polar
 {
-    int nsam;
-    int nring = ring_info.size()/3;
-    double radius, twopi, dphi, phi; 
-    double xp, yp;
-    vector<Matrix1D<T> > out;
-    Matrix1D<T> Mring;
-    out.clear();
+protected:
+    FileName fn_pol; // name of the polar
+public:
+    int                   mode; // Use full or half circles
+    vector<double>        ring_radius; // radius of each ring
+    vector<Matrix1D<T> >  rings; //vector with all rings
+public:
+    /// @defgroup PolarConstructors Polar constructors
+    /// @ingroup Polars
 
-    // Check no pixel falls outside the image
-    if (ABS(xoff) + ring_info[3 * (nring - 1)] > XSIZE(M1)/4 || 
-	ABS(yoff) + ring_info[3 * (nring - 1)] > YSIZE(M1)/4)
-	REPORT_ERROR(1,"polarCoordinatesGridding: last ring falls outside image");
-
-    if (mode == FULL_CIRCLES)
-	twopi = 2*PI;
-    else if (mode == HALF_CIRCLES)
-	twopi = PI;
-    else
-	REPORT_ERROR(1,"Incorrect mode for getPolarWeights2D!");
-
-    for (int iring = 0; iring < nring; iring++)
+    /** Empty constructor
+     * @ingroup PolarConstructors
+     *
+     * An empty image with size 0x0 is created.
+     *
+     * @code
+     * Polar P;
+     * @endcode
+     */
+    Polar()
     {
-	radius = (double) ring_info[3 * iring];
-	nsam = ring_info[3 * iring + 2];
-	dphi = twopi / (double)nsam;
-	Mring.resize(nsam);
-	for (int iphi = 0; iphi < nsam; iphi++)
-	{
-	    // polar coordinates
-	    phi = iphi * dphi;
-	    xp = sin(phi) * radius;
-	    yp = cos(phi) * radius;
-	    xp += xoff;
-	    yp += yoff; 
-	    Mring(iphi) = (T) interpolatedElementGridding(M1,xp,yp,kb);
-	}
-	out.push_back(Mring);
+        fn_pol = "";
+	ring_radius.clear();
+	rings.clear();
+	mode = FULL_CIRCLES;
     }
-    return out;
+    
+    /** Copy constructor
+     * @ingroup PolarConstructors
+     *
+     * @code
+     * Polar P2(P1);
+     * @endcode
+     */
+    Polar(const Polar& P)
+    {
+        rings = P.rings;
+        fn_pol = P.fn_pol;
+	ring_radius = P.ring_radius;
+	mode = P.mode;
+    }
 
-}
+    /// @defgroup PolarOperations Some operations
+    /// @ingroup Polars
+    /** Assignment
+     * @ingroup PolarOperations
+     */
+    Polar& operator=(const Polar& P)
+    {
+        if (this != &P)
+        {
+            fn_pol = P.fn_pol;
+            rings = P.rings;
+	    ring_radius = P.ring_radius;
+	    mode = P.mode;
+        }
+        return *this;
+    }
+  
+    /** Rename polar
+     * @ingroup PolarOperations
+     *
+     * Give a new name to the polar.
+     *
+     * @code
+     * P.rename("new_name");
+     * @endcode
+     */
 
-vector<Matrix1D<complex<double> > > fourierTransformRings(
-    const vector<Matrix1D<double> > &in, bool conjugated = false);
+    void rename(FileName newName)
+    {
+        fn_pol = newName;
+    }
 
-vector<Matrix1D<double> > inverseFourierTransformRings(
-    const vector<Matrix1D<complex<double> > > &in);
+    /** Empty polar
+     * @ingroup PolarOperations
+     *
+     * This function clears the polar to an empty vector without name.
+     *
+     * @code
+     * P.clear();
+     * @endcode
+     */
+    void clear()
+    {
+        fn_pol = "";
+        rings.clear();
+	ring_radius.clear();
+	mode = FULL_CIRCLES;
+    }
 
-/** This function find the optimal rotational angle between images img
- *  and ref. 
+    /// @defgroup PolarAccess Polar access
+    /// @ingroup Polars
 
- * It is assumed that ref has the corresponding weights applied and
- * that both images are Fourier-transforms of the rings of the images
- * in polar coordinates.
+    /** Name access
+     * @ingroup PolarAccess
+     *
+     * This function is used to know the name of the polar. It cannot be used to
+     * assign a new one. You may use rename() for that.
+     *
+     * @code
+     * cout << "Polar name: " << P.name() << endl;
+     * @endcode
+     */
+    const FileName name() const
+    {
+        return fn_pol;
+    }
+
+    /** Number of rings access
+     * @ingroup PolarAccess
+     *
+     * This function is used to know the number of rings in the polar. 
+     *
+     * @code
+     * cout << "Number of rings: " << P.getRingNo() << endl;
+     * @endcode
+     */
+    const int getRingNo() const
+    {
+        return rings.size();
+    }
+
+    /** Mode access
+     * @ingroup PolarAccess
+     *
+     * This function is used to know the "mode" of the polar. 
+     *
+     * There are two modes: 
+     * FULL_CIRCLES = 0 (used for asymmetric functions)
+     * HALF_CIRCLES = 0 (used for symmetric functions, e.g. Fourier transforms)
+     *
+     * @code
+     * cout << "Mode: " << P.getMode() << endl;
+     * @endcode
+     */
+    const int getMode() const
+    {
+        return mode;
+    }
+
+    /** Number of samples in each ring access
+     * @ingroup PolarAccess
+     *
+     * This function is used to know the number of samples in a given ring.
+     *
+     * @code
+     * cout << "Number of samples in second ring: " << P.getSampleNo(1) << endl;
+     * @endcode
+     */
+    const int getSampleNo(int iring) const
+    {
+        return XSIZE(rings[iring]);
+    }
+
+    /** The radius of each ring access
+     * @ingroup PolarAccess
+     *
+     * This function is used to know the radius of a given ring.
+     *
+     * @code
+     * cout << "Radius of second ring: " << P.getRadius(1) << endl;
+     * @endcode
+     */
+    const double getRadius(int iring) const
+    {
+        return ring_radius[iring];
+    }
+
+    /** 1D Matrix access
+     * @ingroup PolarAccess
+     *
+     * This operator can be used to access any ring of the polar as Matrix1D.
+     *
+     * @code
+     * Matrix1D<double> secondring = P.getRing(1);
+     * @endcode
+     */
+    Matrix1D< T >& getRing(int i)
+    {
+        return rings[i];
+    }
+    const  Matrix1D< T >& getRing(int i) const
+    {
+        return rings[i];
+    }
+
+    /** 1D Matrix access
+     * @ingroup PolarAccess
+     *
+     * This operator can be used to set any ring of the polar with a Matrix1D.
+     *
+     * @code
+     * Matrix1D<double> ring;
+     * P.setRing(1,ring);
+     * @endcode
+     */
+    void setRing(int i, Matrix1D< T > val) const
+    {
+        rings[i] = val;
+    }
+
+    /** Pixel access
+     * @ingroup PolarAccess
+     *
+     * This operator is used to access a pixel within the polar. That
+     * means, given the ring and the number of the pixel in the
+     * rotational direction.
+     *
+     * @code
+     * int ring = 1, phi = 0;
+     * cout <<" first pixel of second ring= "<<P.getPixel(ring,phi)<<endl;
+     * @endcode
+     */
+    T& getPixel(int r, int f) const
+    {
+        return rings[r](f);
+    }
+
+    /** Pixel access
+     * @ingroup PolarAccess
+     *
+     * This operator is used to access a pixel within the polar. That
+     * means, given the ring and the number of the pixel in the
+     * rotational direction.
+     *
+     * @code
+     * int ring = 1, phi = 0;
+     * cout <<" first pixel of second ring= "<<P(ring,phi)<<endl;
+     * @endcode
+     */
+    T& operator()(int r, int f) const
+    {
+	return rings[r](f);
+    }
+
+    /** Pixel access
+     * @ingroup PolarAccess
+     *
+     * This operator is used to set a pixel within the polar. That
+     * means, given the ring and the number of the pixel in the
+     * rotational direction.
+     *
+     * @code
+     * int ring = 1, phi = 0;
+     * double val = 1.2;
+     * P.setPixel(ring,phi,val);
+     * @endcode
+     */
+    void setPixel(int r, int f, T val) const
+    {
+        rings[r](f) = val;
+    }
+
+
+    /** Convert cartesian Matrix2D to Polar
+     * @ingroup PolarFunctions
+     *
+     * Use gridding for interpolation. The input Matrix2D is assumed
+     * to be pre-processed for gridding purposes.
+     *
+     * @code
+     * Polar P;
+     * KaiserBessel kb;
+     * Matrix2D<double> Maux;
+     * produceGriddingMatrix2D(img(),Maux,kb);
+     * P.getPolarFromCartesian(Maux,kb,1,15);
+     * @endcode
+     */
+    void getPolarFromCartesian(const Matrix2D<T> &M1, KaiserBessel &kb, 
+			       int first_ring, int last_ring, 
+			       int step = 1, int mode1 = FULL_CIRCLES, 
+			       double xoff = 0., double yoff = 0.)
+    {
+	int nsam;
+	int nring = last_ring - first_ring + 1;
+	double radius, twopi, dphi, phi; 
+	double xp, yp;
+
+	Matrix1D<T> Mring;
+	rings.clear();
+	ring_radius.clear();
+	mode = mode1;
+
+	// Check that no pixel falls outside the image
+	if ((int)ABS(xoff) + last_ring > XSIZE(M1)/4 || 
+	    (int)ABS(yoff) + last_ring > YSIZE(M1)/4)
+	    REPORT_ERROR(1,"polarCoordinatesGridding: last ring falls outside image");
+
+	if (mode == FULL_CIRCLES)
+	    twopi = 2*PI;
+	else if (mode == HALF_CIRCLES)
+	    twopi = PI;
+	else
+	    REPORT_ERROR(1,"Incorrect mode for getPolarWeights2D!");
+
+	for (int iring = first_ring; iring <= last_ring; iring++)
+	{
+	    radius = (double) iring;
+	    // Oversample each ring twice (don't sample origin)
+	    nsam = (int)twopi * iring * 2;
+	    dphi = twopi / (double)nsam;
+	    Mring.resize(nsam);
+	    for (int iphi = 0; iphi < nsam; iphi++)
+	    {
+		// polar coordinates
+		phi = iphi * dphi;
+		xp = sin(phi) * radius;
+		yp = cos(phi) * radius;
+		// Origin offsets
+		xp += xoff;
+		yp += yoff; 
+		Mring(iphi) = (T) interpolatedElementGridding(M1,xp,yp,kb);
+	    }
+	    rings.push_back(Mring);
+	    ring_radius.push_back(radius);
+	}
+
+    }
+
+    /** Fourier transform all rings
+     * @ingroup PolarFunctions
+     *
+     * 1D Fourier transform of all rings
+     *
+     * @code
+     * Polar<double> P;
+     * Polar<complex<double> > Pf;
+     * KaiserBessel kb;
+     * Matrix2D<double> Maux;
+     * produceGriddingMatrix2D(img(),Maux,kb);
+     * P.getPolarFromCartesian(Maux,kb,1,15);
+     * Pf = P.fourierTransformRings();
+     * @endcode
+     */
+    Polar<complex<double> > fourierTransformRings(bool conjugated = false);
+
+    /** inverse Fourier transform all rings
+     * @ingroup PolarFunctions
+     *
+     * 1D inverse Fourier transform of all rings
+     */
+    Polar<double> inverseFourierTransformRings();
+
+};
+
+/** @defgroup PolarRelated Polar Related functions
+ * @ingroup Polar
+ *
+ * These functions are not methods of Polar
  */
-double getBestAnglePolarGridding(const vector<Matrix1D<complex<double> > > &img,
-				 const vector<Matrix1D<complex<double> > > &ref,
-				 double &maxcorr, bool interpolate = false);
-				 
 
-
+/** Rotational Cross-Correlation funtion
+ * @ingroup PolarRelated
+ *
+ *  This function returns the rotational cross-correlation function of
+ *  two Polars M1 and M2 in Fourier-space, using the cross-correlation
+ *  convolution theorem. M2 is assumed to be the complex conjugated.
+ *
+ */
+void rotationalCrossCorrelation(const Polar<complex<double> > &M1,
+				const Polar<complex<double> > &M2,
+				Matrix1D<double> &angles, Matrix1D<double> &corr);
+    
 #endif
