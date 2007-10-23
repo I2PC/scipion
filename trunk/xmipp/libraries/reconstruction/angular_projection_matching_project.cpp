9538d694a48822d4845fa0e09adf9b77768a3a15
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2007-10-23 22:15:51 +0000

    git-svn-id: file:///home/roberto/Git/svn@2327 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    Former-commit-id: cae0f7d491270398d97344c7408bbe8ccdb99bcc

--- trunk/xmipp/libraries/reconstruction/angular_projection_matching_project.cpp
+++ trunk/xmipp/libraries/reconstruction/angular_projection_matching_project.cpp
@@ -45,6 +45,17 @@ void Prog_Angular_Projection_Matching_Project_Parameters::read(int argc, char **
     psi_sampling = textToFloat(getParameter(argc, argv, "-psi_sampling", "360"));
     max_tilt_angle = textToFloat(getParameter(argc, argv, "-max_tilt_angle","91"));
     min_tilt_angle = textToFloat(getParameter(argc, argv, "-min_tilt_angle","-91"));
+    angular_distance_bool = checkParameter(argc, argv,"-angular_distance");
+    if(angular_distance_bool)
+    {
+         if(!checkParameter(argc, argv, "-experimental_images"))
+         {
+             cerr << "Docfile with experimental images euler angles is missing" << endl;
+             exit(0);
+         } 
+            FnexperimentalImages = getParameter(argc, argv, "-experimental_images","");
+            angular_distance = textToFloat(getParameter(argc, argv,"-angular_distance"));
+    }
 }
 
 /* Usage ------------------------------------------------------------------- */
@@ -58,9 +69,11 @@ void Prog_Angular_Projection_Matching_Project_Parameters::usage()
     << "                                i.e.  ci, cs, cn, cnv, cnh, sn, dn, dnv, dnh, t, td, th, o, oh, i, ih\n"
     << "  [-sym_order 1]               : For infinite groups symmetry order\n"
     << "  [-sampling_rate 5]           : Distance in degrees between sampling points\n"
-    << "  [-psi_sampling 360]         : sampling in psi, 360 -> no sampling in psi\n"
-    << "  [-max_tilt_angle  91]       : maximum tilt angle in degrees\n"
-    << "  [-min_tilt_angle -91]       : minimum tilt angle in degrees\n"
+    << "  [-psi_sampling 360]          : sampling in psi, 360 -> no sampling in psi\n"
+    << "  [-max_tilt_angle  91]        : maximum tilt angle in degrees\n"
+    << "  [-min_tilt_angle -91]        : minimum tilt angle in degrees\n"
+    << "  [-experimental_images \"\"]  : doc file with experimental data\n"
+    << "  [-angular_distance 20]       : do not search a distance larger than...\n"
     << "\n"
     << "Example of use: Sample at 2degres and use c6 symmetry\n"
     << "   xmipp_angular_projection_matching_project -i in.vol -o out "
@@ -75,23 +88,24 @@ void Prog_Angular_Projection_Matching_Project_Parameters::show()
     
     cout
     << "output input_volume root:  " << input_volume << endl
-    << "output files root:  " << output_file_root << endl
-    << "Sampling rate:      " << sampling    << endl
-    << "symmetry group:     " << symmetry << endl
-    << "symmetry order:     " << sym_order << endl
-    << "max_tilt_angle:     " << max_tilt_angle << endl
-    << "min_tilt_angle:     " << min_tilt_angle << endl
-    << "psi_sampling:       " << psi_sampling << endl
+    << "output files root:         " << output_file_root << endl
+    << "Sampling rate:             " << sampling    << endl
+    << "symmetry group:            " << symmetry << endl
+    << "symmetry order:            " << sym_order << endl
+    << "max_tilt_angle:            " << max_tilt_angle << endl
+    << "min_tilt_angle:            " << min_tilt_angle << endl
+    << "psi_sampling:              " << psi_sampling << endl
     ;
+    if(angular_distance_bool)
+        cout << "angular_distance:          " << angular_distance << endl
+             << "experimental_images:       " << FnexperimentalImages << endl
+        ;
 }
 
 
 
 void Prog_Angular_Projection_Matching_Project_Parameters::project_angle_vector(int my_init, int my_end)
 {
-//psi_sampling
-//////project only close angles
-/////create sel file
     Projection P;
     FileName fn_proj;
     double rot,tilt,psi;
@@ -106,9 +120,9 @@ void Prog_Angular_Projection_Matching_Project_Parameters::project_angle_vector(i
        for (int i=my_init;i<=my_end;i++)
        {    
            progress_bar(i-my_init);
-           psi= mypsi+ZZ(mysampling.no_redundant_sampling_points_angles[i]);
-           tilt=      YY(mysampling.no_redundant_sampling_points_angles[i]);
-           rot=       XX(mysampling.no_redundant_sampling_points_angles[i]);
+           psi= mypsi+ZZ(close_points_angles[i]);
+           tilt=      YY(close_points_angles[i]);
+           rot=       XX(close_points_angles[i]);
 
     //progress bar     
            project_Volume(inputVol(), P, Ydim, Xdim,rot,tilt,psi);
@@ -121,6 +135,102 @@ void Prog_Angular_Projection_Matching_Project_Parameters::project_angle_vector(i
 
 }
 
+#ifdef NEVEDEFINED
+//UNTESTED CODE
+void
+Prog_Angular_Projection_Matching_Project_Parameters::createDocFileWithExperimentalImages(void)
+{   
+    SelFile         mySF
+    DocFile         myDF;
+    Matrix1D<double> docline;
+    FileName        fn_img,fn_doc;
+    headerXmipp     nyhead;
+
+    if( (symmetry!="cn" || sym_order!=1) && () )
+    {
+        cerr << "experimental_images option only available for symmetry "
+             << "cn and order 1" 
+             <<endl;
+        exit(0);     
+    }  
+    
+    if(SFexperimentalImages!="")
+    {
+        docline.initZeros(3);
+
+        mySF.read(DFexperimentalImages);
+        mySF.go_beginning(); 
+
+        myDF.reserve(mySF.ImgNo());
+        myDF.append_comment("Headerinfo columns: rot (1) , tilt (2), psi (3)");
+
+        while (!mySF.eof())
+        {
+            fn_img = mySF.NextImg();
+            nyhead.read(fn_img);
+            docline(0) = head.Phi();
+            docline(1) = head.Theta();
+            docline(2) = head.Psi();
+            myDF.append_comment(fn_img);
+            myDF.append_data_line(docline);
+        }
+        fn_doc = DFexperimentalImages
+        fn_doc.remove_extension("sel");   
+        fn_doc.add_extension("exp_doc");
+        myDF.write(.substitute_extension("sel", "doc"));
+    }
+}
+#endif
+
+void
+Prog_Angular_Projection_Matching_Project_Parameters::remove_points_not_close_to_experimental_points(void)
+{   
+    if((symmetry!="cn" || sym_order!=1) && angular_distance_bool==true)
+    {
+        cerr << "experimental_images option only available for symmetry "
+             << "cn and order 1" 
+             <<endl;
+        exit(0);     
+    }  
+    //return new docfile if not restrictions
+    if(!angular_distance_bool)
+    {
+        for (int i = 0; i < mysampling.no_redundant_sampling_points_angles.size(); i++)
+        {
+         close_points_angles.push_back(mysampling.no_redundant_sampling_points_angles[i]);
+        }    
+    }
+    else
+    {
+
+        DocFile          DFi;
+        //init both df
+        DFi.read(FnexperimentalImages);//experiemntal points
+        double ref_tilt,ref_rot;
+        double img_tilt,img_rot;
+        for (int i = 0; i < mysampling.no_redundant_sampling_points_angles.size(); i++)
+        {
+            ref_rot  = XX(mysampling.no_redundant_sampling_points_angles[i]);
+            ref_tilt = YY(mysampling.no_redundant_sampling_points_angles[i]);
+            // act_rot_range is tilt-angle dependent!
+            DFi.go_first_data_line();//experimental data
+            while (!DFi.eof())
+            {
+                img_rot=DFi(0);
+                img_tilt=DFi(1);
+                // I knw this comparison is not accurate
+                //but is fast and we do not need much precission
+                if ( ABS(realWRAP(img_rot-ref_rot,-180.,180.)) <= angular_distance &&
+                     ABS(realWRAP(img_tilt-ref_tilt,-180.,180.)) <= angular_distance )
+                {
+                     close_points_angles.push_back(mysampling.sampling_points_angles[i]);
+                     break;
+                }
+            DFi.next();
+            }
+         }      
+    }                     
+}
 /* Run --------------------------------------------------------------------- */
 void Prog_Angular_Projection_Matching_Project_Parameters::run()
 {
@@ -138,9 +248,13 @@ void Prog_Angular_Projection_Matching_Project_Parameters::run()
     mysampling.create_sym_file(symmetry, sym_order);
     //mpi_barrier here
     //all working nodes must read symmetry file
+    //and experimental docfile if apropiate
     //symmetry_file = symmetry + ".sym";
     //SL.read_sym_file(symmetry_file)
     mysampling.remove_redundant_points(symmetry, sym_order);
+    //remove points not close to experimental points, only for no symmetric cases
+    //if(DFexperimentalImages!="")
+    remove_points_not_close_to_experimental_points();
     //only rank 0
     mysampling.create_asym_unit_file(output_file_root);
     //all nodes
@@ -157,7 +271,7 @@ void Prog_Angular_Projection_Matching_Project_Parameters::run()
 
     //Run for all works
     project_angle_vector(0,
-                 mysampling.no_redundant_sampling_points_vector.size()-1);
+                 close_points_angles.size()-1);
                  
     //only rank 0 create sel file
     SelFile  mySF;
@@ -165,7 +279,7 @@ void Prog_Angular_Projection_Matching_Project_Parameters::run()
     int myCounter=0;
     
     for (int mypsi=0;mypsi<360;mypsi += psi_sampling)
-       for (int i=0;i<=mysampling.no_redundant_sampling_points_vector.size()-1;i++)
+       for (int i=0;i<=close_points_angles.size()-1;i++)
        {    
         fn_temp.compose(output_file_root, myCounter++,"xmp");
         mySF.insert(fn_temp);
