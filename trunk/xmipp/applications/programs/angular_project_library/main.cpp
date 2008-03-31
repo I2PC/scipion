c3093eac73e352787fbd2b823e5a6b88ec15755f
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2008-03-31 10:42:09 +0000

    git-svn-id: file:///home/roberto/Git/svn@2664 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    Former-commit-id: 08114da75c10064990ae6aafc55cc2a165a3852d

---
+++ trunk/xmipp/applications/programs/angular_project_library/main.cpp
@@ -0,0 +1,60 @@
+/***************************************************************************
+ *
+ * Authors:     Roberto Marabini
+ *
+ * Unidad de Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.uam.es'
+ ***************************************************************************/
+
+/* ------------------------------------------------------------------------- */
+/* Includes                                                                  */
+/* ------------------------------------------------------------------------- */
+#include <reconstruction/angular_project_library.h>
+#include <data/error.h>
+#include <iostream>
+
+/* ------------------------------------------------------------------------- */
+/* Program                                                                   */
+/* ------------------------------------------------------------------------- */
+int main(int argc, char *argv[])
+{
+    Prog_angular_project_library_Parameters prm;
+    try
+    {
+        prm.read(argc, argv);
+    }
+    catch (Xmipp_error XE)
+    {
+        std::cout << XE;
+        prm.usage();
+        exit(1);
+    }
+
+    try
+    {
+        prm.run();
+    }
+    catch (Xmipp_error XE)
+    {
+        std::cout << XE;
+        exit(1);
+    }
+    exit(0);
+}
