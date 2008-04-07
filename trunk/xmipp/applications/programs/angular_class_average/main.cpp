0a6d1efc05d138ca61c5660cbf4b2cd25f442a96
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2008-04-07 23:10:42 +0000

    reformatting
    git-svn-id: file:///home/roberto/Git/svn@2702 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: d6e3a5f2257dc6757f858622fb931110e0e48426

--- trunk/xmipp/applications/programs/angular_class_average/main.cpp
+++ trunk/xmipp/applications/programs/angular_class_average/main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char **argv)
     Prog_angular_class_average_prm  prm;
 
     int              i, nmax, nr_ref;
-    double           lib_rot, lib_tilt, rot, tilt, psi, xshift, yshift, mirror;
+    double           ref_number, rot, tilt, psi, xshift, yshift, mirror;
     double           w, w1, w2;
     SelFile          SFclasses, SFclasses1, SFclasses2;
     FileName         fn_tmp;
@@ -44,17 +44,17 @@ int main(int argc, char **argv)
     try
     {
         // Read command line & produce side info
-        prm.read(argc, argv);
-        prm.show();
+        prm.read(argc, argv); //all nodes
+        prm.show(); // only rank =0
 
         // Project reference volume etc.
-        prm.produceSideInfo();
+        prm.produceSideInfo();// prerun, ONCE PER WORKING NODE
 
     }
     catch (Xmipp_error XE)
     {
         std::cout << XE;
-        prm.usage();
+        prm.usage(); //only rank = 0 
         exit(0);
     }
 
@@ -62,57 +62,55 @@ int main(int argc, char **argv)
     try
     {
 
-	// Initialize
-	SFclasses.clear();
-	SFclasses1.clear();
-	SFclasses2.clear();
+	    // Initialize
+	    SFclasses.clear(); // master
+	    SFclasses1.clear(); // master
+	    SFclasses2.clear(); // master
 
-	nr_ref = prm.DFlib.dataLineNo();
-	init_progress_bar(nr_ref);
+	    nr_ref = prm.DFlib.dataLineNo();// master//library docfile
+	    init_progress_bar(nr_ref);// master
 
-	// Loop over all classes
-	prm.DFlib.go_beginning();
-	for (int dirno = 1; dirno <= nr_ref; dirno++)
-	{
-	    prm.DFlib.adjust_to_data_line();
-	    lib_rot = prm.DFlib(ABS(prm.col_rot) - 1);
-	    lib_tilt = prm.DFlib(ABS(prm.col_tilt) - 1);
-	    
-	    prm.processOneClass(dirno, lib_rot, lib_tilt, w, w1, w2);
+	    // Loop over all classes
+	    //prm.DFlib.go_beginning();// master
+	    for (int dirno = 1; dirno <= nr_ref; dirno++)
+	    {   //col_rot colum numbers
+	        //prm.DFlib.adjust_to_data_line();
+	        prm.processOneClass(dirno, w, w1, w2);
 
-	    if (w > 0.)
-	    {
-		fn_tmp.compose(prm.fn_out,dirno,"xmp");
-		SFclasses.insert(fn_tmp);
-	    }
+	        if (w > 0.)
+	        {
+		        fn_tmp.compose(prm.fn_out,dirno,"xmp");
+		        SFclasses.insert(fn_tmp);
+	        }
+	        if (prm.do_split)
+	        {
+		        if (w1 > 0.)
+		        {
+		            fn_tmp.compose(prm.fn_out1,dirno,"xmp");
+		            SFclasses1.insert(fn_tmp);
+		        }
+		        if (w2 > 0.)
+		        {
+		            fn_tmp.compose(prm.fn_out2,dirno,"xmp");
+		            SFclasses2.insert(fn_tmp);
+		        }
+	        }
+                progress_bar(dirno);
+	        //prm.DFlib.next();
+            }
+            progress_bar(nr_ref);
+ 
+        //maSTER SHOULD DO THE FOLLOWING...
+	    // Write selfile with all classes
+	    fn_tmp=prm.fn_out+"es.sel";
+	    SFclasses.write(fn_tmp);
 	    if (prm.do_split)
 	    {
-		if (w1 > 0.)
-		{
-		    fn_tmp.compose(prm.fn_out1,dirno,"xmp");
-		    SFclasses1.insert(fn_tmp);
-		}
-		if (w2 > 0.)
-		{
-		    fn_tmp.compose(prm.fn_out2,dirno,"xmp");
-		    SFclasses2.insert(fn_tmp);
-		}
+	        fn_tmp=prm.fn_out1+"es.sel";
+	        SFclasses1.write(fn_tmp);
+	        fn_tmp=prm.fn_out2+"es.sel";
+	        SFclasses2.write(fn_tmp);
 	    }
-            progress_bar(dirno);
-	    prm.DFlib.next();
-        }
-        progress_bar(nr_ref);
-
-	// Write selfile with all classes
-	fn_tmp=prm.fn_out+"es.sel";
-	SFclasses.write(fn_tmp);
-	if (prm.do_split)
-	{
-	    fn_tmp=prm.fn_out1+"es.sel";
-	    SFclasses1.write(fn_tmp);
-	    fn_tmp=prm.fn_out2+"es.sel";
-	    SFclasses2.write(fn_tmp);
-	}
     }
     catch (Xmipp_error XE)
     {
