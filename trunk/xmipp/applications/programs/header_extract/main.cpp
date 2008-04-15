b745a15a1d002c146b0a179b6063c127a31a8ad7
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2008-04-15 15:30:48 +0000

    changed headerinfo Mirror to Flip
    git-svn-id: file:///home/roberto/Git/svn@2796 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 6cee15bf65eef09e42b3366479117dd5c2fb4ebf

--- trunk/xmipp/applications/programs/header_extract/main.cpp
+++ trunk/xmipp/applications/programs/header_extract/main.cpp
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 
         DF.reserve(SF.ImgNo());
         Matrix1D<double> docline;
-        DF.append_comment("Headerinfo columns: rot (1) , tilt (2), psi (3), Xoff (4), Yoff (5), Weight (6), Mirror (7)");
+        DF.append_comment("Headerinfo columns: rot (1) , tilt (2), psi (3), Xoff (4), Yoff (5), Weight (6), Flip (7)");
 
         docline.initZeros(7);
         SF.go_beginning();
