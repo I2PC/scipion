f519d3279662e54caa55e9326e4470ac9efcea99
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2008-10-08 09:16:28 +0000

    Bug fix when the last image is discarded
    git-svn-id: file:///home/roberto/Git/svn@3178 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: f99f9371f802305d74478952b7bdcc622b1c66f3

--- trunk/xmipp/applications/programs/header_extract/main.cpp
+++ trunk/xmipp/applications/programs/header_extract/main.cpp
@@ -69,6 +69,7 @@ int main(int argc, char *argv[])
         while (!SF.eof())
         {
             fn_img = SF.NextImg();
+            if (SF.eof()) break;
             head.read(fn_img);
             head.get_originOffsets(xx, yy);
             if (round_shifts)
