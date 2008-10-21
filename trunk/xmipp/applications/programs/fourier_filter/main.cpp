3d2ecbd73c6f0c77b91221b1d355f336722e180b
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2008-10-21 13:22:13 +0000

    Adapted for using FFTW
    git-svn-id: file:///home/roberto/Git/svn@3228 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 82bf3c50562c8dfebcbaaf1df2d78c81941a0295

--- trunk/xmipp/applications/programs/fourier_filter/main.cpp
+++ trunk/xmipp/applications/programs/fourier_filter/main.cpp
@@ -31,10 +31,6 @@ class FourierFilter_parameters: public Prog_parameters
 {
 public:
     FourierMask fmask;
-    FileName    fn_mask;
-    FileName    fn_amplitude;
-    bool        do_not_center;
-
     bool        first;
     int         dim;
 public:
@@ -42,10 +38,6 @@ public:
     {
         fmask.read(argc, argv);
         Prog_parameters::read(argc, argv);
-        fn_mask = getParameter(argc, argv, "-save_mask", "");
-        fn_amplitude = getParameter(argc, argv, "-save_amplitude", "");
-        do_not_center = checkParameter(argc, argv, "-do_not_center");
-
         first = true;
     }
 
@@ -59,24 +51,18 @@ public:
     {
         Prog_parameters::usage();
         fmask.usage();
-        std::cerr << "  [-save_mask <fn_mask>]                : Save applied filter\n"
-                  << "  [-save_amplitude <fn_ampl>]           : Save amplitude of filter\n"
-                  << "  [-do_not_center]                      : For the amplitude file\n";
     }
 };
 
 bool process_img(ImageXmipp &img, const Prog_parameters *prm)
 {
     FourierFilter_parameters *eprm = (FourierFilter_parameters *) prm;
-    Matrix2D< std::complex<double> > fft;
-    FourierTransform(img(), fft);
     if (eprm->first)
     {
-        eprm->fmask.generate_mask(fft);
+        eprm->fmask.generate_mask(img());
         eprm->first = false;
     }
-    eprm->fmask.apply_mask_Fourier(fft);
-    InverseFourierTransform(fft, img());
+    eprm->fmask.apply_mask_Space(img());
     eprm->dim = 2;
     return true;
 }
@@ -84,16 +70,9 @@ bool process_img(ImageXmipp &img, const Prog_parameters *prm)
 bool process_vol(VolumeXmipp &vol, const Prog_parameters *prm)
 {
     FourierFilter_parameters *eprm = (FourierFilter_parameters *) prm;
-    Matrix3D< std::complex<double> > fft;
-    FourierTransform(vol(), fft);
-    if (eprm->first)
-    {
-        eprm->fmask.generate_mask(fft);
-        eprm->first = false;
-    }
-    eprm->fmask.apply_mask_Fourier(fft);
-    InverseFourierTransform(fft, vol());
+    eprm->fmask.apply_mask_Space(vol());
     eprm->dim = 3;
+    eprm->first = false;
     return true;
 }
 
@@ -101,8 +80,4 @@ int main(int argc, char **argv)
 {
     FourierFilter_parameters prm;
     SF_main(argc, argv, &prm, (void *)&process_img, (void *)&process_vol);
-    if (prm.fn_mask != "")
-       prm.fmask.write_mask(prm.fn_mask, prm.dim);
-    if (prm.fn_amplitude != "") prm.fmask.write_amplitude(prm.fn_amplitude,
-                prm.dim, prm.do_not_center);
 }
