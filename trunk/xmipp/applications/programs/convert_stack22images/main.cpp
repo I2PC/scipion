bb95d229a45ed81b5de6cd9ead3683ef900688ce
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2008-10-29 12:36:35 +0000

    The program now converts to and from selfiles, volumes, and stacks in any combination
    git-svn-id: file:///home/roberto/Git/svn@3290 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: e178b2d54f7bb602be9f4c51b4289fbc2401b9f6

--- trunk/xmipp/applications/programs/convert_stack22images/main.cpp
+++ trunk/xmipp/applications/programs/convert_stack22images/main.cpp
@@ -38,13 +38,14 @@ int main(int argc, char **argv)
     // Read arguments --------------------------------------------------------
     try
     {
-        fn_stack = getParameter(argc,argv,"-stack");
+        fn_stack = getParameter(argc,argv,"-stack","");
         fn_sel   = getParameter(argc,argv,"-sel","");
         fn_vol   = getParameter(argc,argv,"-vol","");
         reversed = checkParameter(argc,argv,"-reverse");
         
-        if (fn_sel=="" && fn_vol=="")
-           REPORT_ERROR(1,"stack22images: Please, provide, -sel or -vol");
+        if (fn_sel=="" && fn_vol=="" || fn_stack=="" && fn_vol=="" ||
+            fn_sel=="" && fn_stack=="")
+           REPORT_ERROR(1,"stack22images: Please, provide at two of -stack, -sel and -vol");
     }
     catch (Xmipp_error XE)
     {
@@ -56,10 +57,13 @@ int main(int argc, char **argv)
     // True work -----------------------------------------------------------
     try
     {
-        // From stack to selfile or volume
+        ImageXmippStack stack;
+        VolumeXmipp V;
+        SelFile SF;
+
+        // From stack to selfile or volume .................................
         if (exists(fn_stack))
         {
-            ImageXmippStack stack;
             stack.readFromStack(fn_stack,reversed,false,false);
 
             // From stack to selfile
@@ -74,34 +78,53 @@ int main(int argc, char **argv)
                 stack.writeAsVolume(fn_vol);
             }
         }
-        else
+        // From volume to stack or selfile .................................
+        else if (exists(fn_vol) && Is_VolumeXmipp(fn_vol))
         {
-            ImageXmippStack stack;
-            // From selfile to stack
+            V.read(fn_vol);
+
+            // From volume to selfile
             if (fn_sel!="")
             {
-                SelFile SF;
-                SF.read(fn_sel);
-                while (!SF.eof())
-                {
-                    FileName fn_img=SF.NextImg();
-                    if (fn_img=="") break;
-                    ImageXmipp I(fn_img);
-                    stack.appendImage(I);
-                }
+                FileName fn_root=fn_sel.without_extension();
+                V.writeAsSelFile(fn_root);
             }
             // From volume to stack
             else
             {
-                VolumeXmipp V(fn_vol);
                 for (int k=0; k<ZSIZE(V()); k++)
                 {
                     ImageXmipp I;
                     V().getSlice(k,I());
                     stack.appendImage(I);
                 }
+                stack.writeAsStack(fn_stack);
             }
-            stack.writeAsStack(fn_stack);
+        // From selfile to volume or stack .................................
+        } else {
+            SF.read(fn_sel);
+            if (fn_vol!="")
+            {
+                int Zdim, Ydim, Xdim;
+                SF.ImgSize(Ydim,Xdim);
+                Zdim=SF.ImgNo();
+                V().initZeros(Zdim,Ydim,Xdim);
+            }
+            int k=0;
+            while (!SF.eof())
+            {
+                FileName fn_img=SF.NextImg();
+                if (fn_img=="") break;
+                ImageXmipp I(fn_img);
+                if (fn_vol!="")
+                    V().setSlice(k++,I());
+                else
+                    stack.appendImage(I);
+            }
+            if (fn_vol!="")
+                V.write(fn_vol);
+            else
+                stack.writeAsStack(fn_stack);
         }
     }
     catch (Xmipp_error XE)
@@ -115,11 +138,12 @@ int main(int argc, char **argv)
 void Usage()
 {
     std::cerr << "Purpose:\n";
-    std::cerr << "    Converts Spider stacks into images or volumes or viceversa\n";
+    std::cerr << "    Converts Spider stacks into images or volumes or any other\n"
+              << "    combination of these three elements\n";
     std::cerr << "Usage: stack22images " << std::endl
-         << "    -stack stackFile : Stack with the set of images\n"
-         << "   [-sel selFile]    : Selfile with the set of images\n"
-         << "   [-vol volume]     : Volume with the set of images\n"
-         << "   [-reverse]        : Reverse endiannness for reading the stack\n"
+              << "   [-stack stackFile]: Stack with the set of images\n"
+              << "   [-sel selFile]    : Selfile with the set of images\n"
+              << "   [-vol volume]     : Volume with the set of images\n"
+              << "   [-reverse]        : Reverse endiannness for reading the stack\n"
     ;
 }
