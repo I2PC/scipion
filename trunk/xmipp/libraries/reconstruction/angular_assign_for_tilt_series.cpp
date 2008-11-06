e92eeb11901f4f97f2d1dfbf1c79b54265a6faae
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2008-11-06 10:51:26 +0000

    cout should be precede by std::
    git-svn-id: file:///home/roberto/Git/svn@3356 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: b37ac5d2797e10964c29fd3ba55b2d626b93af09

--- trunk/xmipp/libraries/reconstruction/angular_assign_for_tilt_series.cpp
+++ trunk/xmipp/libraries/reconstruction/angular_assign_for_tilt_series.cpp
@@ -470,7 +470,7 @@ void * threadComputeTransform( void * args )
 	pthread_mutex_lock( &printingMutex );
         affineTransformations[jj_1][jj]=Aij;
         affineTransformations[jj][jj_1]=Aji;
-	std::cout << "Cost for [" << jj_1 << "] - [" << jj << "] = " << std::cost << endl;
+	std::cout << "Cost for [" << jj_1 << "] - [" << jj << "] = " << cost << endl;
 	pthread_mutex_unlock( &printingMutex );
 	affineTransformations[jj_1][jj]=Aij;
 	affineTransformations[jj][jj_1]=Aji;
