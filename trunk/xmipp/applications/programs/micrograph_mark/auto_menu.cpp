9d98e2fb49765cbe064de1d45d9b75689d6d0742
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2008-11-18 13:26:25 +0000

    Exit saving the particles the user has selected
    git-svn-id: file:///home/roberto/Git/svn@3384 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 9f7f2849e94be92fe85d1816c011dc49b68941f5

--- trunk/xmipp/applications/programs/micrograph_mark/auto_menu.cpp
+++ trunk/xmipp/applications/programs/micrograph_mark/auto_menu.cpp
@@ -100,5 +100,6 @@ void QtAutoMenu::slotLearnSaveQuit()
 {
     ((QtWidgetMicrograph *)parentWidget())->learnParticles();
     ((QtWidgetMicrograph *)parentWidget())->saveModels(false);
+    ((QtWidgetMicrograph *)parentWidget())->file_menu()->slotQuit();
     exit(0);
 }
