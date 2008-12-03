085483994844b677f6b2721c86ae035751c0b8dd
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2008-12-03 22:41:23 +0000

    git-svn-id: file:///home/roberto/Git/svn@3400 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    Former-commit-id: 915070b9f6cd46bed59ce9e0fb397bddc03405ce

--- trunk/xmipp/libraries/reconstruction/virus_vertex.cpp
+++ trunk/xmipp/libraries/reconstruction/virus_vertex.cpp
@@ -229,8 +229,10 @@ void VirusVertex::processAngles()
                 irandom=rnd_unif(0, 4);
                 Matrix2D<double> euler(3, 3), temp;
                 Euler_angles2matrix(rot, tilt, psi, euler);
-                //temp = euler * R_repository[symmetryMatrixVertex(i,1)];
-                temp = euler;
+                temp = R_repository[symmetryMatrixVertex(i,1)].inv() * 
+                       euler * 
+                       R_repository[symmetryMatrixVertex(i,1)];
+                //temp = euler;
                 Euler_matrix2angles(temp, rotp, tiltp, psip);
 
                 proj_aux.set_rot(rotp);
