291e84bd81a5af7813c427c46865be052adc477a
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2009-01-28 03:04:57 +0000

    Some magnitude, phase and correlation functions moved from fft to fftw
    git-svn-id: file:///home/roberto/Git/svn@3543 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 34df27770f155569b14afb3f2cabcefc45c85c01

--- trunk/xmipp/libraries/data/fftw.h
+++ trunk/xmipp/libraries/data/fftw.h
@@ -30,6 +30,7 @@
 #include <complex>
 #include "../../external/fftw-3.1.2/api/fftw3.h"
 #include "matrix3d.h"
+#include "fft.h"
 
 /** @defgroup FourierW FFTW Fourier transforms
   * @ingroup DataLibrary
@@ -135,7 +136,7 @@ public:
             switch (ndim)
             {
                 case 1:
-                    FOR_ALL_ELEMENTS_IN_MATRIX1D(V)
+                    FOR_ALL_DIRECT_ELEMENTS_IN_MATRIX1D(V)
                         if (i<XSIZE(fFourier))
                             DIRECT_VEC_ELEM(V,i)=DIRECT_VEC_ELEM(fFourier,i);
                         else
@@ -144,7 +145,7 @@ public:
                                     XSIZE(*fReal)-i));
                     break;
                 case 2:
-                    FOR_ALL_ELEMENTS_IN_MATRIX2D(V)
+                    FOR_ALL_DIRECT_ELEMENTS_IN_MATRIX2D(V)
                         if (j<XSIZE(fFourier))
                             DIRECT_MAT_ELEM(V,i,j)=
                                 DIRECT_MAT_ELEM(fFourier,i,j);
@@ -155,7 +156,7 @@ public:
                                      XSIZE(*fReal)-j));
                     break;
                 case 3:
-                    FOR_ALL_ELEMENTS_IN_MATRIX3D(V)
+                    FOR_ALL_DIRECT_ELEMENTS_IN_MATRIX3D(V)
                         if (j<XSIZE(fFourier))
                             DIRECT_VOL_ELEM(V,k,i,j)=
                                 DIRECT_VOL_ELEM(fFourier,k,i,j);
@@ -199,6 +200,180 @@ public:
         No plan is updated. */
     void setFourier(MultidimArray<std::complex<double> > &imgFourier);
 };
+
+/** FFT Magnitude 1D
+ * @ingroup FourierOperations
+ */
+void FFT_magnitude(const Matrix1D< std::complex< double > > & v,
+                   Matrix1D< double >& mag);
+
+/** FFT Magnitude 2D
+ * @ingroup FourierOperations
+ */
+void FFT_magnitude(const Matrix2D< std::complex< double > > & v,
+                   Matrix2D< double >& mag);
+
+/** FFT Magnitude 3D
+ * @ingroup FourierOperations
+ */
+void FFT_magnitude(const Matrix3D< std::complex< double > > & v,
+                   Matrix3D< double >& mag);
+
+/** FFT Phase 1D
+ * @ingroup FourierOperations
+ */
+void FFT_phase(const Matrix1D< std::complex< double > > & v,
+               Matrix1D< double >& phase);
+
+/** FFT Phase 2D
+ * @ingroup FourierOperations
+ */
+void FFT_phase(const Matrix2D< std::complex< double > > & v,
+               Matrix2D< double >& phase);
+
+/** FFT Phase 3D
+ * @ingroup FourierOperations
+ */
+void FFT_phase(const Matrix3D< std::complex< double > > & v,
+               Matrix3D< double >& phase);
+
+/** Autocorrelation function of a Xmipp vector
+ * @ingroup FourierOperations
+ *
+ * Fast calcuation of the autocorrelation vector of a given one using Fast
+ * Fourier Transform. (Using the correlation theorem)
+ */
+template <typename T>
+void auto_correlation_vector(const Matrix1D< T > & Img, Matrix1D< double >& R)
+{
+    // Compute the Fourier Transform
+    Matrix1D< std::complex< double > > FFT1;
+    XmippFftw transformer1;
+    R=Img;
+    transformer1.FourierTransform(R, FFT1, false);
+
+    // Multiply FFT1 * FFT1'
+    double dSize=XSIZE(Img);
+    FOR_ALL_ELEMENTS_IN_MATRIX1D(FFT1)
+        FFT1(i) *= dSize * conj(FFT1(i));
+
+    // Invert the product, in order to obtain the correlation image
+    transformer1.inverseFourierTransform();
+
+    // Center the resulting image to obtain a centered autocorrelation
+    CenterFFT(R, true);
+}
+
+/** Autocorrelation function of a Xmipp vector
+ * @ingroup FourierOperations
+ *
+ * Fast calcuation of the correlation matrix on two matrices using Fast Fourier
+ * Transform. (Using the correlation theorem). The output matrix must be already
+ * resized
+ */
+template <typename T>
+void correlation_vector(const Matrix1D< T > & m1,
+                        const Matrix1D< T > & m2,
+                        Matrix1D< double >& R)
+{
+    // Compute the Fourier Transforms
+    Matrix1D< std::complex< double > > FFT1, FFT2;
+    XmippFftw transformer1, transformer2;
+    R=m1;
+    transformer1.FourierTransform(R, FFT1, false);
+    transformer2.FourierTransform((Matrix1D<T> &)m2, FFT2, false);
+
+    // Multiply FFT1 * FFT2'
+    double dSize=XSIZE(m1);
+    FOR_ALL_ELEMENTS_IN_MATRIX1D(FFT1)
+        FFT1(i) *= dSize * conj(FFT2(i));
+
+    // Invert the product, in order to obtain the correlation image
+    transformer1.inverseFourierTransform();
+
+    // Center the resulting image to obtain a centered autocorrelation
+    CenterFFT(R, true);
+}
+
+/** Compute the correlation vector without using Fourier.
+ * @ingroup FourierOperations
+ *
+ *  The results are the same as the previous ones but this function
+ *  is threadsafe while the previous one is not.
+ *  
+ *  It is assumed that the two vectors v1, and v2 are of the same size. */
+template <class T>
+void correlation_vector_no_Fourier(const Matrix1D<T> &v1, const Matrix1D<T> &v2,
+    Matrix1D<T> &result)
+{
+    result.initZeros(v1);
+    result.setXmippOrigin();
+    int N=XSIZE(v1)-1;
+    FOR_ALL_ELEMENTS_IN_MATRIX1D(result)
+        for (int k=0; k<XSIZE(v1); k++)
+            result(i)+=DIRECT_VEC_ELEM(v1,intWRAP(k+i,0,N))*
+                       DIRECT_VEC_ELEM(v2,k);
+    STARTINGX(result)=0;
+}
+
+
+/** Correlation of two matrices
+ * @ingroup FourierOperations
+ *
+ * Fast calcuation of the correlation matrix on two matrices using Fast Fourier
+ * Transform. (Using the correlation theorem). The output matrix must be already
+ * resized
+ */
+template <typename T>
+void correlation_matrix(const Matrix2D< T > & m1,
+                        const Matrix2D< T > & m2,
+                        Matrix2D< double >& R)
+{
+    // Compute the Fourier Transforms
+    Matrix2D< std::complex< double > > FFT1, FFT2;
+    XmippFftw transformer1, transformer2;
+    R=m1;
+    transformer1.FourierTransform(R, FFT1, false);
+    transformer2.FourierTransform((Matrix2D<T> &)m2, FFT2, false);
+
+    // Multiply FFT1 * FFT2'
+    double dSize=MULTIDIM_SIZE(R);
+    FOR_ALL_ELEMENTS_IN_MATRIX2D(FFT1)
+        FFT1(i, j) *= dSize * conj(FFT2(i, j));
+
+    // Invert the product, in order to obtain the correlation image
+    transformer1.inverseFourierTransform();
+
+    // Center the resulting image to obtain a centered autocorrelation
+    CenterFFT(R, true);
+}
+
+/** Autocorrelation function of a Xmipp matrix
+ * @ingroup FourierOperations
+ *
+ * Fast calcuation of the autocorrelation matrix of a given one using Fast
+ * Fourier Transform. (Using the correlation theorem)
+ */
+template <typename T>
+void auto_correlation_matrix(const Matrix2D< T > & Img, Matrix2D< double >& R)
+{
+    // Compute the Fourier Transform
+    Matrix2D< std::complex< double > > FFT1;
+    XmippFftw transformer1;
+    R=Img;
+    transformer1.FourierTransform(R, FFT1);
+
+    // Multiply FFT1 * FFT1'
+    double dSize=MULTIDIM_SIZE(Img);
+    FOR_ALL_ELEMENTS_IN_MATRIX2D(FFT1)
+        FFT1(i, j) *= dSize * conj(FFT1(i, j));
+
+    // Invert the product, in order to obtain the correlation image
+    transformer1.inverseFourierTransform();
+
+    // Center the resulting image to obtain a centered autocorrelation
+    CenterFFT(R, true);
+}
 
 /** Fourier-Ring-Correlation between two 2D-matrices using FFT
  * @ingroup FourierOperations
