793a17f026606ffb34e43a541811ce153605e892
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2009-02-24 16:38:32 +0000

    improved job launch
    git-svn-id: file:///home/roberto/Git/svn@3574 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 24f6256e1e27e059794b9edd6f008ad1568eb936

--- trunk/xmipp/applications/scripts/protocols/launch_job.py
+++ trunk/xmipp/applications/scripts/protocols/launch_job.py
@@ -15,14 +15,30 @@ def launch_job(programname,
     else:
         mpiprogramname=programname.replace('xmipp','xmipp_mpi')
 
-        if (SystemFlavour=='SLURM-BSC'):
+        if (SystemFlavour=='SLURM-MPICH'): # like BSCs MareNostrum, LaPalma etc
             mpicommand = 'srun '
-        elif (SystemFlavour=='PBS-FT'):
+
+        elif (SystemFlavour=='TORQUE-OPENMPI'): # like our crunchy
+            if (int(NumberOfThreads) > 1):
+                mpicommand = 'mpirun --bynode -np ' + str(NumberOfMpiProcesses) 
+            else:
+                mpicommand = 'mpirun -np ' + str(NumberOfMpiProcesses) 
+
+        elif (SystemFlavour=='SGE-OPENMPI'): # like cluster at imp.ac.at (no variable nr_cpus yet...)
+            mpicommand = 'mpiexec -n ' + str(NumberOfMpiProcesses) 
+
+        elif (SystemFlavour=='PBS'): # like in Vermeer and FinisTerrae
             mpicommand = 'mpirun -np ' + str(NumberOfMpiProcesses) + ' -machinefile ' + os.environ.get('PBS_NODEFILE')
-        elif (SystemFlavour=='XMIPP_MACHINEFILE'):
+
+        elif (SystemFlavour=='XMIPP_MACHINEFILE'): # environment variable $XMIPP_MACHINEFILE points to machinefile
             mpicommand = 'mpirun -np ' + str(NumberOfMpiProcesses) + ' -machinefile ' + os.environ.get('XMIPP_MACHINEFILE')
+
+        elif (SystemFlavour=='HOME_MACHINEFILE'): # machinefile is called $HOME/machines.dat
+            mpicommand = 'mpirun -np ' + str(NumberOfMpiProcesses) + ' -machinefile ' + os.environ.get('HOME') + '/machinefile.dat'
+
         elif (SystemFlavour==''):
             mpicommand = 'mpirun -np ' + str(NumberOfMpiProcesses)
+
         else:
             message= "Error: unrecognized SystemFlavour: ", SystemFlavour
             print '* ',message
