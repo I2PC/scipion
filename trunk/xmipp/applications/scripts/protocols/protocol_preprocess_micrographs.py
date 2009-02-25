aef289aa8dcfdc6e81b8274f11451a271e83848f
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2009-02-25 11:09:56 +0000

    according to users: set ctffind default
    git-svn-id: file:///home/roberto/Git/svn@3578 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: bf3d4d7844ab20949cef7f00e3caec52f6e40af2

--- trunk/xmipp/applications/scripts/protocols/protocol_preprocess_micrographs.py
+++ trunk/xmipp/applications/scripts/protocols/protocol_preprocess_micrographs.py
@@ -102,7 +102,7 @@ HighResolCutoff=0.35
 """ Some people prefer the faster CTFFIND program.
     Note however that this option will yield no information about the CTF envelope, and therefore this option cannot be used with the high-resolution refinement protocol.
 """
-DoCtffind=False
+DoCtffind=True
 # {file} Location of the CTFFIND executable
 CtffindExec='../../bin/ctffind3.exe'
 # {expert} Window size
