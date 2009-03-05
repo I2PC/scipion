15f107772a2eb33c9633308eb19ad860d9f0a920
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2009-03-05 09:03:06 +0000

    change MLalign2d docfile to MLFalign2d in fourier_mode
    git-svn-id: file:///home/roberto/Git/svn@3627 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: ad2dda2c1e7e74053f1cfd721a5d5eb2d0f76177

--- trunk/xmipp/libraries/reconstruction/ml_refine3d.cpp
+++ trunk/xmipp/libraries/reconstruction/ml_refine3d.cpp
@@ -77,10 +77,21 @@ void Prog_Refine3d_prm::read(int argc, char ** argv, int &argc2, char ** &argv2)
         DFi.read(getParameter(argc, argv, "-restart"));
         DFi.go_beginning();
         comment = (DFi.get_current_line()).get_text();
-        if (strstr(comment.c_str(), "MLalign2D-logfile") == NULL)
+        if (fourier_mode)
         {
-            std::cerr << "Error!! Docfile is not of MLalign2D-logfile type. " << std::endl;
-            exit(1);
+            if (strstr(comment.c_str(), "MLFalign2D-logfile") == NULL)
+            {
+                std::cerr << "Error!! Docfile is not of MLFalign2D-logfile type. " << std::endl;
+                exit(1);
+            }
+        }
+        else
+        {
+            if (strstr(comment.c_str(), "MLalign2D-logfile") == NULL)
+            {
+                std::cerr << "Error!! Docfile is not of MLalign2D-logfile type. " << std::endl;
+                exit(1);
+            }
         }
         else
         {
