8f3e6bc4640585d266e04ae89f158c98b4704b3d
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2009-03-09 07:27:45 +0000

    Now we can import tilt series from MRC
    git-svn-id: file:///home/roberto/Git/svn@3644 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: c6eb3efa075dd0e3005b430b4daa374d22a6b1a2

--- trunk/xmipp/applications/programs/convert_spi22ccp4/main.cpp
+++ trunk/xmipp/applications/programs/convert_spi22ccp4/main.cpp
@@ -30,7 +30,7 @@
 #include <interface/ccp4.h>
 
 #include <sys/stat.h>
-
+#include <fstream>
 
 /* Prototypes -============================================================= */
 void Usage();
@@ -40,6 +40,7 @@ int main(int argc, char *argv[])
     /* Input Parameters ======================================================== */
     FileName       fn_in;    // input file
     FileName       fn_out;   // output file
+    FileName       fn_tilt;  // File with tilt angles in case of a series
     double         x_length = 0;  // Cell Dimensions (Angstroms) for x-axis
     double         y_length = 0;  // Cell Dimensions (Angstroms) for y-axis
     double         z_length = 0;  // Cell Dimensions (Angstroms) for z-axis
@@ -49,8 +50,9 @@ int main(int argc, char *argv[])
     /* Parameters ============================================================== */
     try
     {
-        fn_in  = getParameter(argc, argv, "-i");
-        fn_out = getParameter(argc, argv, "-o");
+        fn_in   = getParameter(argc, argv, "-i");
+        fn_out  = getParameter(argc, argv, "-o");
+        fn_tilt = getParameter(argc, argv, "-tilt_angles","");
         if (checkParameter(argc, argv, "-x_length"))
         {
             y_length = x_length = textToFloat(getParameter(argc, argv, "-x_length"));
@@ -89,7 +91,33 @@ int main(int argc, char *argv[])
             if (mrcimage.my_mrc_header.nz > 1)
             {
                 mrcimage.read(fn_in, V, reverse_endian);
-                V.write(fn_out);
+                if (fn_tilt!="")
+                {
+                    std::ifstream fh_tilt;
+                    fh_tilt.open(fn_tilt.c_str());
+                    if (!fh_tilt)
+                        REPORT_ERROR(1,(std::string)"Cannot read "+fn_tilt);
+                    SelFile SF;
+                    for (int k=0; k<ZSIZE(V()); k++)
+                    {
+                        float tilt;
+                        if (fh_tilt.eof())
+                            REPORT_ERROR(1,(std::string)"Not enough angles in "+
+                                fn_tilt);
+                        fh_tilt >> tilt;
+                        ImageXmipp I;
+                        V().getSlice(k,I());
+                        I.set_tilt(tilt);
+                        FileName fn_final;
+                        fn_final.compose(fn_out, k, "xmp");
+                        I.write(fn_final);
+                        SF.insert(fn_final);
+                    }
+                    fh_tilt.close();
+                    SF.write(fn_out+".sel");
+                }
+                else
+                    V.write(fn_out);
             }
             else
             {
@@ -114,11 +142,12 @@ void Usage()
     printf(
         "Usage: ccp42spi [Purpose and Parameters]"
         "\nPurpose: Convert between CCP4 (map) and Spider/Xmipp format"
-        "\n    -i    file_in        input CCP4/Xmipp file (2D or 3D)"
-        "\n    -o    file_out       output Xmipp/CCP4 file"
-        "\n    [-x_length  length] Cell Dimensions (Angstroms) for x-axis"
-        "\n    [-y_length  length] Cell Dimensions (Angstroms) for y-axis. By default y_length=x_length"
-        "\n    [-z_length  length] Cell Dimensions (Angstroms) for z-axis. By default z_length=1"
-        "\n    [-reverse_endian]     by default, output has the same endiannes as input"
-        "\n                         use this option to change endianness\n");
+        "\n    -i   <file_in>           input CCP4/Xmipp file (2D or 3D)"
+        "\n    -o   <file_out>          output Xmipp/CCP4 file. If CCP4 tilt series, rootname."
+        "\n   [-tilt_angles <filename>] Filename with angles for a CCP4 tilt series"
+        "\n   [-x_length  <length>]     Cell Dimensions (Angstroms) for x-axis"
+        "\n   [-y_length  <length>]     Cell Dimensions (Angstroms) for y-axis. By default y_length=x_length"
+        "\n   [-z_length  <length>]     Cell Dimensions (Angstroms) for z-axis. By default z_length=1"
+        "\n   [-reverse_endian]         by default, output has the same endiannes as input"
+        "\n                             use this option to change endianness\n");
 }
