f0e8744ca1decbfd50fb6893fa703109ebc573e8
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2009-05-25 12:10:16 +0000

    deafult iter2=10
    git-svn-id: file:///home/roberto/Git/svn@3893 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: f26c776dfd3b0448cd01c1c404f0077ac1f89813

--- trunk/xmipp/libraries/reconstruction/ml_tomo.cpp
+++ trunk/xmipp/libraries/reconstruction/ml_tomo.cpp
@@ -114,7 +114,7 @@ void Prog_ml_tomo_prm::read(int argc, char **argv, bool ML3D)
     fn_root = getParameter(argc2, argv2, "-o", "mltomo");
     fn_sym = getParameter(argc2, argv2, "-sym", "c1");
     Niter = textToInteger(getParameter(argc2, argv2, "-iter", "100"));
-    Niter2 = textToInteger(getParameter(argc2, argv2, "-iter2", "1"));
+    Niter2 = textToInteger(getParameter(argc2, argv2, "-iter2", "10"));
     istart = textToInteger(getParameter(argc2, argv2, "-istart", "1"));
     sigma_noise = textToFloat(getParameter(argc2, argv2, "-noise", "1"));
     sigma_offset = textToFloat(getParameter(argc2, argv2, "-offset", "3"));
