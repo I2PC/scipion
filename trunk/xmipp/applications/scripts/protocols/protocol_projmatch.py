5ce9725bbea6ccb4f4dee376e60e6e7adadf8bdc
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2009-09-16 13:55:31 +0000

    bug parsing percentange of particles to skip
    git-svn-id: file:///home/roberto/Git/svn@4052 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: c1318544fa2d41b42e11fd94b953701fb5c1745d

--- trunk/xmipp/applications/scripts/protocols/protocol_projmatch.py
+++ trunk/xmipp/applications/scripts/protocols/protocol_projmatch.py
@@ -1339,8 +1339,8 @@ def execute_projection_matching(_mylog,
       parameters =  ' -i '      + outputname + '.doc'  + \
                     ' -lib '    + ProjectLibraryRootName + '_angles.doc' + \
                     ' -dont_write_selfiles ' + \
-                    ' -limit0 ' + str(MinimumCrossCorrelation) + \
-                    ' -limitR ' + str(DiscardPercentage)
+                    ' -limit0 ' + str(_MinimumCrossCorrelation) + \
+                    ' -limitR ' + str(_DiscardPercentage)
       if (_DoCtfCorrection):
          # On-the fly apply Wiener-filter correction and add all CTF groups together
          parameters += \
