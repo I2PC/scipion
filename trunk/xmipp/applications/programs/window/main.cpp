1715a8f2bd6e8c108154048fb28d4c7f7f171edc
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2009-09-30 15:01:47 +0000

    Crop option added
    git-svn-id: file:///home/roberto/Git/svn@4101 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: c0084f537ea65931de4dcd9c2cbe415e0bd85bc2

--- trunk/xmipp/applications/programs/window/main.cpp
+++ trunk/xmipp/applications/programs/window/main.cpp
@@ -30,8 +30,10 @@ class Window_parameters: public Prog_parameters
 {
 public:
     bool size_mode;
+    bool crop_mode;
     bool physical_coords;
     int sizeX, sizeY, sizeZ;
+    int cropX, cropY, cropZ;
     int x0, y0, z0;
     int xF, yF, zF;
     double init_value;
@@ -39,13 +41,14 @@ public:
     int average_pad;
     int corner_pad;
 
-
     void read(int argc, char **argv)
     {
         init_value = 0.;
         wrong_parameters = 0;
         average_pad = false;
         corner_pad = false;
+        size_mode=false;
+        crop_mode=false;
 
         Prog_parameters::read(argc, argv);
 
@@ -139,7 +142,27 @@ public:
                 }
 
             physical_coords = checkParameter(argc, argv, "-physical");
+        } else if (checkParameter(argc, argv, "-crop")) {
+            crop_mode = true;
+            int i = paremeterPosition(argc, argv, "-crop");
+            if (i + 2 >= argc)
+            {
+                cropZ = cropY = cropX = textToInteger(argv[i+1]);
+            }
+            else if (i + 3 >= argc)
+            {
+                cropZ = cropY = textToInteger(argv[i+2]);
+                cropX = textToInteger(argv[i+1]);
+            }
+            else if (i + 4 >= argc)
+            {
+                cropZ = textToInteger(argv[i+3]);
+                cropY = textToInteger(argv[i+2]);
+                cropX = textToInteger(argv[i+1]);
+            }
+            else REPORT_ERROR(1, "Not enough parameters after -crop");
         }
+
         else
             REPORT_ERROR(1, "Unknown windowing type");
     }
@@ -149,12 +172,15 @@ public:
         Prog_parameters::show();
         if (size_mode)
             std::cout << "New size: (XxYxZ)=" << sizeX << "x" << sizeY << "x"
-            << sizeZ << std::endl;
+                << sizeZ << std::endl;
+        else if (crop_mode)
+            std::cout << "Crop: (XxYxZ)=" << cropX << "x" << cropY << "x"
+                << cropZ << std::endl;
         else
             std::cout << "New window: from (z0,y0,x0)=(" << z0 << ","
-            << y0 << "," << x0 << ") to (zF,yF,xF)=(" << zF << "," << yF
-            << "," << xF << ")\n"
-            << "Physical: " << physical_coords << std::endl;
+                << y0 << "," << x0 << ") to (zF,yF,xF)=(" << zF << "," << yF
+                << "," << xF << ")\n"
+                << "Physical: " << physical_coords << std::endl;
     }
 
     void usage()
@@ -171,6 +197,9 @@ public:
              << "  [-size <sizeX> [<sizeY>] [<sizeZ>]]: Window to a new size\n"
              << "                            : if only one is given, the other two\n"
              << "                              are supposed to be the same\n"
+             << "  [-crop <sizeX> [<sizeY>] [<sizeZ>]]: Crop this amount of pixels in each direction\n"
+             << "                            : if only one is given, the other two\n"
+             << "                              are supposed to be the same\n"
         ;
     }
 };
@@ -182,10 +211,20 @@ bool process_img(ImageXmipp &img, const Prog_parameters *prm)
         eprm->init_value = (img()).computeAvg();
     else if (eprm->corner_pad)
         eprm->init_value = DIRECT_MAT_ELEM(img(), 0, 0);
-    if (!eprm->physical_coords)
-        img().window(eprm->y0, eprm->x0, eprm->yF, eprm->xF, eprm->init_value);
-    else img().window(STARTINGY(img()) + eprm->y0, STARTINGX(img()) + eprm->x0,
-                          STARTINGY(img()) + eprm->yF, STARTINGX(img()) + eprm->xF, eprm->init_value);
+    if (eprm->crop_mode) {
+        int xl=eprm->cropX/2;
+        int xr=eprm->cropX-xl;
+        int yl=eprm->cropY/2;
+        int yr=eprm->cropY-yl;
+        img().window(STARTINGY(img())+yl,STARTINGX(img())+xl,
+            FINISHINGY(img())-yr,FINISHINGX(img())-xr);
+    } else
+        if (!eprm->physical_coords)
+            img().window(eprm->y0, eprm->x0, eprm->yF, eprm->xF,
+                eprm->init_value);
+        else img().window(STARTINGY(img()) + eprm->y0,
+            STARTINGX(img()) + eprm->x0, STARTINGY(img()) + eprm->yF,
+            STARTINGX(img()) + eprm->xF, eprm->init_value);
     return true;
 }
 
@@ -196,12 +235,25 @@ bool process_vol(VolumeXmipp &vol, const Prog_parameters *prm)
         eprm->init_value = (vol()).computeAvg();
     else if (eprm->corner_pad)
         eprm->init_value = DIRECT_VOL_ELEM(vol(), 0, 0, 0);
-    if (!eprm->physical_coords)
-        vol().window(eprm->z0, eprm->y0, eprm->x0, eprm->zF, eprm->yF, eprm->xF,
-                     eprm->init_value);
-    else vol().window(STARTINGZ(vol()) + eprm->z0, STARTINGY(vol()) + eprm->y0,
-                          STARTINGX(vol()) + eprm->x0, STARTINGZ(vol()) + eprm->zF,
-                          STARTINGY(vol()) + eprm->yF, STARTINGX(vol()) + eprm->xF, eprm->init_value);
+    if (eprm->crop_mode) {
+        int xl=eprm->cropX/2;
+        int xr=eprm->cropX-xl;
+        int yl=eprm->cropY/2;
+        int yr=eprm->cropY-yl;
+        int zl=eprm->cropZ/2;
+        int zr=eprm->cropZ-zl;
+        vol().window(STARTINGZ(vol())+zl,STARTINGY(vol())+yl,
+            STARTINGX(vol())+xl, FINISHINGZ(vol())-zr,FINISHINGY(vol())-yr,
+            FINISHINGX(vol())-xr);
+    } else
+        if (!eprm->physical_coords)
+            vol().window(eprm->z0, eprm->y0, eprm->x0, eprm->zF, eprm->yF,
+                eprm->xF, eprm->init_value);
+        else vol().window(STARTINGZ(vol()) + eprm->z0,
+                STARTINGY(vol()) + eprm->y0,
+                STARTINGX(vol()) + eprm->x0, STARTINGZ(vol()) + eprm->zF,
+                STARTINGY(vol()) + eprm->yF, STARTINGX(vol()) + eprm->xF,
+                eprm->init_value);
     return true;
 }
 
@@ -210,48 +262,3 @@ int main(int argc, char **argv)
     Window_parameters prm;
     SF_main(argc, argv, &prm, (void*)&process_img, (void*)&process_vol);
 }
-
-/* Menus ------------------------------------------------------------------- */
-/*Colimate:
-   PROGRAM Window {
-      url="http://www.cnb.uam.es/~bioinfo/NewXmipp/Applications/Src/Window/Help/window.html";
-      help="Window volumes and images, extract a part of them";
-      OPEN MENU menu_window;
-      COMMAND LINES {
- + usual: xmipp_window
-               #include "prog_line.mnu"
-               $WINDOWING_METHOD
-               [-r0 $X0 $Y0 [$Z0] -rf $XF $YF [$ZF]]
-               [-size $XDIM [$YDIM] [$ZDIM]]
-      }
-      PARAMETER DEFINITIONS {
-        #include "prog_vars.mnu"
-        $WINDOWING_METHOD {
-           label="Window action";
-           type=list {
-              "Logical corners" {OPT(-r0)=1; OPT(-size)=0;}
-              "Centered cube"   {OPT(-r0)=0; OPT(-size)=1;}
-           };
-        }
-        OPT(-r0) {label="Window using logical corners";}
-           $X0 {type=float; label="Initial X";}
-           $Y0 {type=float; label="Initial Y";}
-           $Z0 {type=float; label="Initial Z";}
-           $XF {type=float; label="Final X";}
-           $YF {type=float; label="Final Y";}
-           $ZF {type=float; label="Final Z";}
-        OPT(-size) {label="Window within a centered cube";}
-           $ZDIM  {type=float; label="Cube Zdim"; by default=$XDIM;}
-           $YDIM  {type=float; label="Cube Ydim"; by default=$XDIM;}
-           $XDIM  {type=float; label="Cube Xdim";}
-      }
-   }
-
-   MENU menu_window {
-      #include "prog_menu.mnu"
-      "Window parameters"
-      $WINDOWING_METHOD
-      OPT(-r0)
-      OPT(-size)
-   }
-*/
