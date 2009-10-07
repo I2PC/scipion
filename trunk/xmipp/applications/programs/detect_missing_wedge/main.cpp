08e2f37ba617b6c5cb48e6773f94be5849949364
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2009-10-07 17:23:41 +0000

    Program creation
    git-svn-id: file:///home/roberto/Git/svn@4134 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 57db6e66733dcd5ca95421b068abedcdaeaec0d1

---
+++ trunk/xmipp/applications/programs/detect_missing_wedge/main.cpp
@@ -0,0 +1,53 @@
+/***************************************************************************
+ *
+ * Authors:     Carlos Oscar S. Sorzano (coss@cnb.uam.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.uam.es'
+ ***************************************************************************/
+
+#include <data/detect_missing_wedge.h>
+
+int main(int argc, char **argv)
+{
+    DetectMissingWedge_parameters prm;
+    try
+    {
+        prm.read(argc,argv);
+    }
+    catch (Xmipp_error XE)
+    {
+        std::cerr << XE << std::endl;
+        prm.usage();
+        exit(1);
+    }
+    try
+    {
+        prm.show();
+        prm.produceSideInfo();
+        prm.run();
+    }
+    catch (Xmipp_error XE)
+    {
+        std::cerr << XE << std::endl;
+        exit(2);
+    }
+    exit(0);
+}
