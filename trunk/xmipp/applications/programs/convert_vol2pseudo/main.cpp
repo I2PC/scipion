252fd9f5b2cbec8d3042f0be213ef4798d7d2fd4
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-01-18 08:54:12 +0000

    Added the method based on volume approximation
    git-svn-id: file:///home/roberto/Git/svn@4380 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: a4d9738e4f4ce93b8603ce603a1b9bb6bfa3df8d

--- trunk/xmipp/applications/programs/convert_vol2pseudo/main.cpp
+++ trunk/xmipp/applications/programs/convert_vol2pseudo/main.cpp
@@ -25,6 +25,7 @@
 
 #include <data/volume.h>
 #include <data/args.h>
+#include <reconstruction/convert_vol2pseudo.h>
 
 #include <cstdio>
 #include <cstdlib>
@@ -50,242 +51,261 @@ int main(int argc, char **argv)
     bool FourD = false;
     int npoints;
 
-
+    Prog_Convert_Vol2Pseudo prm;
+    bool newMode=false; 
     // Read arguments
-
     try
     {
-        vname = getParameter(argc, argv, "-vname");
-        T = textToFloat(getParameter(argc, argv, "-T", "2"));
-        fname = getParameter(argc, argv, "-fname", "out.dat");
-        bmname = getParameter(argc, argv, "-bmname", "mask.spi");
-        vmname = getParameter(argc, argv, "-vmname", "vol_mask.spi");
-        minCoord = textToFloat(getParameter(argc, argv, "-minCoord", "0"));
-        maxCoord = textToFloat(getParameter(argc, argv, "-maxCoord", "10"));
-        if (!checkParameter(argc, argv, "-sampling"))
-            sampling = false;
-        if (checkParameter(argc, argv, "-nomask"))
-            nomask = true;
-        if (checkParameter(argc, argv, "-4"))
-            FourD = true;
-        npoints = textToInteger(getParameter(argc, argv, "-npoints", "100000"));
+        if (checkParameter(argc,argv,"-vname"))
+        {
+            vname = getParameter(argc, argv, "-vname");
+            T = textToFloat(getParameter(argc, argv, "-T", "2"));
+            fname = getParameter(argc, argv, "-fname", "out.dat");
+            bmname = getParameter(argc, argv, "-bmname", "mask.spi");
+            vmname = getParameter(argc, argv, "-vmname", "vol_mask.spi");
+            minCoord = textToFloat(getParameter(argc, argv, "-minCoord", "0"));
+            maxCoord = textToFloat(getParameter(argc, argv, "-maxCoord", "10"));
+            if (!checkParameter(argc, argv, "-sampling"))
+                sampling = false;
+            if (checkParameter(argc, argv, "-nomask"))
+                nomask = true;
+            if (checkParameter(argc, argv, "-4"))
+                FourD = true;
+            npoints = textToInteger(getParameter(argc, argv, "-npoints", "100000"));
+            newMode=false;
+        }
+        else
+        {
+            prm.read(argc,argv);
+            newMode=true;
+        }
     }
     catch (Xmipp_error)
     {
         std::cout << "Usage:" << std::endl;
-        std::cout << "-vname         : Input Volume file name" << std::endl;
-        std::cout << "-bmname        : Input Mask file name (default: mask.spi)" << std::endl;
-        std::cout << "[-vmname]      : Volume Mask file name (default: vol_mask.spi)" << std::endl;
-        std::cout << "[-fname]       : Output file name (default: out.dat)" << std::endl;
-        std::cout << "[-nomask]      : set if the mask is not going to be used" << std::endl;
-        std::cout << "[-T]           : Threshold (default: 2)" << std::endl;
-        std::cout << "[-sampling]    : Use to create a dataset based on sampling" << std::endl;
-        std::cout << "[-npoints]     : Number of points of the dataset (default: 100000)" << std::endl;
-        std::cout << "[-minCoord]    : 'Density-Coordinates' minimum value (default: 0)" << std::endl;
-        std::cout << "[-maxCoord]    : 'Density-Coordinates' maximum value (default: 10)" << std::endl;
-        std::cout << "[-4]           : Set if the dataset will be 4-dimensional (default: false)" << std::endl;
+        prm.usage();
+        std::cout << "Sampling algorithm:\n";
+        std::cout << "   -vname         : Input Volume file name" << std::endl;
+        std::cout << "   -bmname        : Input Mask file name (default: mask.spi)" << std::endl;
+        std::cout << "   [-vmname]      : Volume Mask file name (default: vol_mask.spi)" << std::endl;
+        std::cout << "   [-fname]       : Output file name (default: out.dat)" << std::endl;
+        std::cout << "   [-nomask]      : set if the mask is not going to be used" << std::endl;
+        std::cout << "   [-T]           : Threshold (default: 2)" << std::endl;
+        std::cout << "   [-sampling]    : Use to create a dataset based on sampling" << std::endl;
+        std::cout << "   [-npoints]     : Number of points of the dataset (default: 100000)" << std::endl;
+        std::cout << "   [-minCoord]    : 'Density-Coordinates' minimum value (default: 0)" << std::endl;
+        std::cout << "   [-maxCoord]    : 'Density-Coordinates' maximum value (default: 10)" << std::endl;
+        std::cout << "   [-4]           : Set if the dataset will be 4-dimensional (default: false)" << std::endl;
         exit(1);
     }
 
-
-    std::cout << "Given parameters are: " << std::endl;
-    std::cout << "vname = " << vname << std::endl;
-    if (!nomask)
+    if (!newMode)
     {
-        std::cout << "bmname = " << bmname << std::endl;
-        std::cout << "vmname = " << vmname << std::endl;
-    }
-    else
-        std::cout << "No mask is going to be used" << std::endl;
-    std::cout << "fname = " << fname << std::endl;
-    std::cout << "T = " << T << std::endl;
-    if (sampling)
-    {
-        std::cout << "Sampling method is used to generate the dataset" << std::endl;
-        std::cout << "number of points = " << npoints << std::endl;
-    }
-    else
-    {
-        std::cout << "Exhaustive method is used to generate the dataset" << std::endl;
-        std::cout << "minCoord = " << minCoord << std::endl;
-        std::cout << "maxCoord = " << maxCoord << std::endl;
-    }
-    if (FourD)
-        std::cout << "Dataset will be 4-Dimensional" << std::endl;
+        std::cout << "Given parameters are: " << std::endl;
+        std::cout << "vname = " << vname << std::endl;
+        if (!nomask)
+        {
+            std::cout << "bmname = " << bmname << std::endl;
+            std::cout << "vmname = " << vmname << std::endl;
+        }
+        else
+            std::cout << "No mask is going to be used" << std::endl;
+        std::cout << "fname = " << fname << std::endl;
+        std::cout << "T = " << T << std::endl;
+        if (sampling)
+        {
+            std::cout << "Sampling method is used to generate the dataset" << std::endl;
+            std::cout << "number of points = " << npoints << std::endl;
+        }
+        else
+        {
+            std::cout << "Exhaustive method is used to generate the dataset" << std::endl;
+            std::cout << "minCoord = " << minCoord << std::endl;
+            std::cout << "maxCoord = " << maxCoord << std::endl;
+        }
+        if (FourD)
+            std::cout << "Dataset will be 4-Dimensional" << std::endl;
 
 
-    // Read spider volumen
+        // Read spider volumen
 
-    std::cout << std::endl << "reading volume " << vname << "......" << std::endl << std::endl;
-    VolumeXmipp V(vname);    // Reads the volumen
-    std::cout << V;      // Output Volumen Information
+        std::cout << std::endl << "reading volume " << vname << "......" << std::endl << std::endl;
+        VolumeXmipp V(vname);    // Reads the volumen
+        std::cout << V;      // Output Volumen Information
 
-    // Read spider mask
+        // Read spider mask
 
-    if (!nomask)
-    {
-        std::cout << std::endl << "reading mask " << bmname << "......" << std::endl << std::endl;
-        mask.read(bmname);        // Reads the mask
-        std::cout << mask;    // Output Volumen Information
-    }
-
-
-    // Extract the data
-
-    V().setXmippOrigin();          // sets origin at the center of the volume.
-    mask().setXmippOrigin();       // sets origin at the center of the mask.
-    VolumeXmipp vol_mask(V);
-
-//  vol_mask().resize(V());       // Resizes volumen_mask.
-    vol_mask().setXmippOrigin();   // sets origin at the center of the volumen mask.
+        if (!nomask)
+        {
+            std::cout << std::endl << "reading mask " << bmname << "......" << std::endl << std::endl;
+            mask.read(bmname);        // Reads the mask
+            std::cout << mask;    // Output Volumen Information
+        }
 
 
-    std::cout << std::endl << "Finding minimum and maximum......" << std::endl;
+        // Extract the data
 
-    // Find Minimum and Maximum density values inside the mask.
+        V().setXmippOrigin();          // sets origin at the center of the volume.
+        mask().setXmippOrigin();       // sets origin at the center of the mask.
+        VolumeXmipp vol_mask(V);
 
-    minVoxel = MAXFLOAT;
-    maxVoxel = -MAXFLOAT;
-    for (int z = STARTINGZ(V()); z <= FINISHINGZ(V()); z++)
-    {
-        for (int y = STARTINGY(V()); y <= FINISHINGY(V()); y++)
-            for (int x = STARTINGX(V()); x <= FINISHINGX(V()); x++)
-            {
-                if (nomask || VOLVOXEL(mask, z, y, x) != 0)
+    //  vol_mask().resize(V());       // Resizes volumen_mask.
+        vol_mask().setXmippOrigin();   // sets origin at the center of the volumen mask.
+
+
+        std::cout << std::endl << "Finding minimum and maximum......" << std::endl;
+
+        // Find Minimum and Maximum density values inside the mask.
+
+        minVoxel = MAXFLOAT;
+        maxVoxel = -MAXFLOAT;
+        for (int z = STARTINGZ(V()); z <= FINISHINGZ(V()); z++)
+        {
+            for (int y = STARTINGY(V()); y <= FINISHINGY(V()); y++)
+                for (int x = STARTINGX(V()); x <= FINISHINGX(V()); x++)
                 {
-                    if (VOLVOXEL(V, z, y, x) > maxVoxel)
-                        maxVoxel = VOLVOXEL(V, z, y, x);
-                    if (VOLVOXEL(V, z, y, x) < minVoxel)
-                        minVoxel = VOLVOXEL(V, z, y, x);
-                } // if
-            } // for x
-    } // for z
-
-    std::cout << std::endl << "minimum: " << minVoxel << std::endl << "maximum: " << maxVoxel << std::endl;
-
-
-    // Generates coordinates (data points)
-
-    std::cout << std::endl << "Generating coordinates......" << std::endl;
-
-    if (!sampling)
-        std::cout << std::endl << "using exhaustive generation......" << std::endl;
-
-    for (int z = STARTINGZ(V()); z <= FINISHINGZ(V()); z++)
-        for (int y = STARTINGY(V()); y <= FINISHINGY(V()); y++)
-            for (int x = STARTINGX(V()); x <= FINISHINGX(V()); x++)
-            {
-
-                // Checks if voxel is different from zero (it's inside the binary mask)
-
-                bool cond;
-                if (!nomask)
-                    cond = VOLVOXEL(mask, z, y, x) != 0;
-                else
-                    cond = true;
-                if (cond && (VOLVOXEL(V, z, y, x) > T))
-                {
-
-                    VOLVOXEL(vol_mask, z, y, x) = VOLVOXEL(V, z, y, x);
-
-                    if (!sampling)
+                    if (nomask || VOLVOXEL(mask, z, y, x) != 0)
                     {
-                        // Lineary transform the density into "number of coordinates"
+                        if (VOLVOXEL(V, z, y, x) > maxVoxel)
+                            maxVoxel = VOLVOXEL(V, z, y, x);
+                        if (VOLVOXEL(V, z, y, x) < minVoxel)
+                            minVoxel = VOLVOXEL(V, z, y, x);
+                    } // if
+                } // for x
+        } // for z
 
-                        if (FourD)
+        std::cout << std::endl << "minimum: " << minVoxel << std::endl << "maximum: " << maxVoxel << std::endl;
+
+
+        // Generates coordinates (data points)
+
+        std::cout << std::endl << "Generating coordinates......" << std::endl;
+
+        if (!sampling)
+            std::cout << std::endl << "using exhaustive generation......" << std::endl;
+
+        for (int z = STARTINGZ(V()); z <= FINISHINGZ(V()); z++)
+            for (int y = STARTINGY(V()); y <= FINISHINGY(V()); y++)
+                for (int x = STARTINGX(V()); x <= FINISHINGX(V()); x++)
+                {
+
+                    // Checks if voxel is different from zero (it's inside the binary mask)
+
+                    bool cond;
+                    if (!nomask)
+                        cond = VOLVOXEL(mask, z, y, x) != 0;
+                    else
+                        cond = true;
+                    if (cond && (VOLVOXEL(V, z, y, x) > T))
+                    {
+
+                        VOLVOXEL(vol_mask, z, y, x) = VOLVOXEL(V, z, y, x);
+
+                        if (!sampling)
                         {
-                            std::vector <float> v;
-                            v.push_back(x);
-                            v.push_back(y);
-                            v.push_back(z);
-                            v.push_back((float)VOLVOXEL(V, z, y, x));
-                            randomCoord.push_back(v);
-                        }
-                        else
-                        { // !FourD
-                            if (minVoxel != maxVoxel)
-                                slope = (double)(maxCoord - minCoord) / (double)(maxVoxel - minVoxel);
-                            else
-                                slope = 0;
+                            // Lineary transform the density into "number of coordinates"
 
-                            tmpCoord = (int)(minCoord + (slope * (double)(VOLVOXEL(V, z, y, x) - minVoxel)));
-
-                            // Saves the coordinates proportional to the density of the voxel.
-
-                            for (unsigned i = 0; i < tmpCoord; i++)
+                            if (FourD)
                             {
                                 std::vector <float> v;
                                 v.push_back(x);
                                 v.push_back(y);
                                 v.push_back(z);
+                                v.push_back((float)VOLVOXEL(V, z, y, x));
                                 randomCoord.push_back(v);
                             }
-                        } // FourD
+                            else
+                            { // !FourD
+                                if (minVoxel != maxVoxel)
+                                    slope = (double)(maxCoord - minCoord) / (double)(maxVoxel - minVoxel);
+                                else
+                                    slope = 0;
+
+                                tmpCoord = (int)(minCoord + (slope * (double)(VOLVOXEL(V, z, y, x) - minVoxel)));
+
+                                // Saves the coordinates proportional to the density of the voxel.
+
+                                for (unsigned i = 0; i < tmpCoord; i++)
+                                {
+                                    std::vector <float> v;
+                                    v.push_back(x);
+                                    v.push_back(y);
+                                    v.push_back(z);
+                                    randomCoord.push_back(v);
+                                }
+                            } // FourD
+
+                        }
+                        else
+                        { // sampling
+                            // If sampling, then normalize the masked volume (devide by
+                            // maxdensity)
+                            VOLVOXEL(vol_mask, z, y, x) /= maxVoxel;
+                        }
 
                     }
                     else
-                    { // sampling
-                        // If sampling, then normalize the masked volume (devide by
-                        // maxdensity)
-                        VOLVOXEL(vol_mask, z, y, x) /= maxVoxel;
+                    { // if VOLVOXEL
+                        VOLVOXEL(vol_mask, z, y, x) = 0;
                     }
 
-                }
-                else
-                { // if VOLVOXEL
-                    VOLVOXEL(vol_mask, z, y, x) = 0;
-                }
+                } // for x
 
-            } // for x
-
-    if (sampling)
-    { // Generate "npoints" coordinates
-        std::cout << std::endl << "using sampling generation......" << std::endl;
-        randomize_random_generator();
-        for (int i = 0; i < npoints; i++)
-        {
-            bool found = false;
-            int X, Y, Z;
-            do
+        if (sampling)
+        { // Generate "npoints" coordinates
+            std::cout << std::endl << "using sampling generation......" << std::endl;
+            randomize_random_generator();
+            for (int i = 0; i < npoints; i++)
             {
-                X = (int) rnd_unif(STARTINGX(vol_mask()), FINISHINGX(vol_mask()));
-                Y = (int) rnd_unif(STARTINGY(vol_mask()), FINISHINGY(vol_mask()));
-                Z = (int) rnd_unif(STARTINGZ(vol_mask()), FINISHINGZ(vol_mask()));
-                if (VOLVOXEL(vol_mask, Z, Y, X) > rnd_unif()) found = true;
+                bool found = false;
+                int X, Y, Z;
+                do
+                {
+                    X = (int) rnd_unif(STARTINGX(vol_mask()), FINISHINGX(vol_mask()));
+                    Y = (int) rnd_unif(STARTINGY(vol_mask()), FINISHINGY(vol_mask()));
+                    Z = (int) rnd_unif(STARTINGZ(vol_mask()), FINISHINGZ(vol_mask()));
+                    if (VOLVOXEL(vol_mask, Z, Y, X) > rnd_unif()) found = true;
+                }
+                while (!found);
+                std::vector <float> v;
+                v.push_back(X);
+                v.push_back(Y);
+                v.push_back(Z);
+                if (FourD)
+                    v.push_back((float)VOLVOXEL(vol_mask, Z, Y, X));
+                randomCoord.push_back(v);
             }
-            while (!found);
-            std::vector <float> v;
-            v.push_back(X);
-            v.push_back(Y);
-            v.push_back(Z);
-            if (FourD)
-                v.push_back((float)VOLVOXEL(vol_mask, Z, Y, X));
-            randomCoord.push_back(v);
         }
-    }
 
-    std::cout << std::endl << "Saving masked volumen......" << std::endl;
-    if (!nomask)
-        vol_mask.write(vmname);
+        std::cout << std::endl << "Saving masked volumen......" << std::endl;
+        if (!nomask)
+            vol_mask.write(vmname);
 
-    std::cout << std::endl << "Saving coordinates at random......" << std::endl;
+        std::cout << std::endl << "Saving coordinates at random......" << std::endl;
 
-    fp = fopen(fname, "w");
-    if (FourD)
-        fprintf(fp, "4 %d\n", randomCoord.size()); // 4-dimensional
-    else
-        fprintf(fp, "3 %d\n", randomCoord.size()); // 3-dimensional
-    if (!sampling)
-        random_shuffle(randomCoord.begin(), randomCoord.end());
-    for (unsigned i = 0; i < randomCoord.size(); i++)
+        fp = fopen(fname, "w");
         if (FourD)
-            fprintf(fp, "%d  %d %d %3.2f \n", (int) randomCoord[i][0], (int) randomCoord[i][1], (int) randomCoord[i][2], randomCoord[i][3]);
+            fprintf(fp, "4 %d\n", randomCoord.size()); // 4-dimensional
         else
-            fprintf(fp, "%d  %d %d \n", (int) randomCoord[i][0], (int) randomCoord[i][1], (int) randomCoord[i][2]);
+            fprintf(fp, "3 %d\n", randomCoord.size()); // 3-dimensional
+        if (!sampling)
+            random_shuffle(randomCoord.begin(), randomCoord.end());
+        for (unsigned i = 0; i < randomCoord.size(); i++)
+            if (FourD)
+                fprintf(fp, "%d  %d %d %3.2f \n", (int) randomCoord[i][0], (int) randomCoord[i][1], (int) randomCoord[i][2], randomCoord[i][3]);
+            else
+                fprintf(fp, "%d  %d %d \n", (int) randomCoord[i][0], (int) randomCoord[i][1], (int) randomCoord[i][2]);
 
-    fclose(fp);    // close file
+        fclose(fp);    // close file
 
-    exit(0);
+        exit(0);
+    }
+    else
+    {
+        prm.show();
+        prm.produceSideInfo();
+        prm.run();
+    }
 }
 
 
