32209245c05d3a7ee97addbef2808b4557d18044
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-03-15 01:03:52 +0000

    added function sqlite3_table_column_metadata
    git-svn-id: file:///home/roberto/Git/svn@4500 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: d86931c4cd0173603dd2187c28a87d89096a5765

--- trunk/xmipp/libraries/data/metadata_container.h
+++ trunk/xmipp/libraries/data/metadata_container.h
@@ -26,17 +26,73 @@
 #ifndef METADATACONTAINER_H
 #define METADATACONTAINER_H
 
-#include <map>
-#include <string>
-#include <iostream>
 #include <external/sqlite-3.6.23/sqlite3.h>
 #include "funcs.h"
+#include <iostream>
+#include <map>
+#include <string>
+
+/** valid data types
+ *
+ */
+#define metaDataInteger "INTEGER"
+#define metaDataReal    "FLOAT"
+#define metaDataString  "TEXT"
+//primary key integer
+#define metaDataPK                "PRIMARY KEY AUTOINCREMENT"
+#define metaDataStringUnique      "TEXT UNIQUE"
+
+/** Some other variables that may be of interest
+ *
+ */
+#define metaDataBlob  SQLITE_BLOB
+#define metaDataNull  SQLITE_NULL
+
+
+/**
+ * Valid image formats // check Sjors,...
+ */
+
+#define CCP4      1
+#define MRC       2
+#define PIF       3
+#define EM        4
+#define IMAGIC    5
+#define SPIDER    6
+#define SUPRIM    7
+#define TIFF      8
+#define PNG       9
+#define JPEG     10
+
+/**
+ * Whenever make sense follow this naming convention
+ * Images
+ */
+
 class metaDataContainer
 {
     private:
-		sqlite3* mpDB;      //database pointer
-		sqlite3_stmt* mpVM; //prepared statment
+		/** database handler
+		 *
+		 */
+	    sqlite3* dB;
+	    /** prepared statement for database
+	     *
+	     */
+		sqlite3_stmt* statement;
+		/**lock time for database (see function setBusyTimeout)
+		 *
+		 */
 	    int mnBusyTimeoutMs; //lock time
+	    /**
+	     * valid attribute names are in map
+	     */
+	    std::map<std::string, std::string> validAttributes;
+
+	    /** iterator for validAttributes
+	    *
+	    */
+	    std::map<std::string, std::string>::iterator validAttributesIterator;
 
         /** open metaContainer DataBase for readind/writing ...  */
 	    void open(const FileName fileName,int flag);
@@ -44,23 +100,56 @@ class metaDataContainer
         /** close metaContainer DataBase */
 	    void close();
 
+	    /** Check if table exists */
+	    bool tableExists(const std::string tableName);
+		/** Block common for all creators
+		 *
+		 */
+	    void init(const FileName baseFileName,  int flag);
+
+	    /** check if attributes exits
+	     *
+	     */
+	    int checkAttributes(const std::string tableName,
+	    		              std::vector<std::string> &attributeNames,
+                              std::string &invalidAttribute
+	    		              );
+
+    public:
+
+	    /** Check if types are compatible */
+	    bool tableTypeCompatible(const std::string tableName,
+	    		                  std::vector<std::string> &attributeNames,
+	    		                  std::vector<int> &attributeTypes);
+
     public:
 		/** Constructor open data base
-
+            vector has column names that should be retrieved
+            from table tablename
 	    valid flags
 
-	    #define SQLITE_OPEN_READONLY         0x00000001   Ok
-	    #define SQLITE_OPEN_READWRITE        0x00000002   Ok
-	    #define SQLITE_OPEN_CREATE           0x00000004   Ok
-	    #define SQLITE_OPEN_NOMUTEX          0x00008000   Ok
-	    #define SQLITE_OPEN_FULLMUTEX        0x00010000   Ok
-	    #define SQLITE_OPEN_SHAREDCACHE      0x00020000   Ok
-	    #define SQLITE_OPEN_PRIVATECACHE     0x00040000   Ok
+	    #define SQLITE_OPEN_READONLY         0x00000001
+	    #define SQLITE_OPEN_READWRITE        0x00000002
+	    #define SQLITE_OPEN_CREATE           0x00000004
+	    #define SQLITE_OPEN_NOMUTEX          0x00008000
+	    #define SQLITE_OPEN_FULLMUTEX        0x00010000
+	    #define SQLITE_OPEN_SHAREDCACHE      0x00020000
+	    #define SQLITE_OPEN_PRIVATECACHE     0x00040000
 	    */
+        /** Read metaData from file
+         *  check open for flags (read(write by default)
+         */
+	    metaDataContainer(FileName fileNameBase,\
+	    		          std::string tableName,\
+	    		          std::vector<std::string> &attributeNames,\
+	    		          int flag=(SQLITE_OPEN_READONLY|SQLITE_OPEN_SHAREDCACHE));
 
-	    metaDataContainer(FileName fileName,\
-				          int flag=SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
-
+	    /**
+	     * check open for flags (read only by default)
+	     */
+	    metaDataContainer(FileName fileNameBase,\
+	    		          std::vector<std::string> &attributeNames,\
+	    		          int flag=SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE);
 	    /** Destructor */
 		~metaDataContainer();
 
@@ -74,7 +163,10 @@ class metaDataContainer
 		allowed to proceed.
 	    */
 		void setBusyTimeout(int nMillisecs);
-
+		/** Create table
+		 * create (if needed) table with primary key
+		 */
+		void createTable(const std::string tableName);
 
 #ifdef NEVER
 	/** Container for pairs "name" and value. Note that void * allows to use
