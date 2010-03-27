ac1a3565e7ffbbff3cf03d82f680c90d1a902e58
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-03-27 00:29:15 +0000

    converted to metadata style
    git-svn-id: file:///home/roberto/Git/svn@4623 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 85d1a479c3348ea5046f431efb4ccc13c951b97c

--- trunk/xmipp/applications/programs/header_assign/main.cpp
+++ trunk/xmipp/applications/programs/header_assign/main.cpp
@@ -22,7 +22,7 @@
  *  All comments concerning this program package may be sent to the
  *  e-mail address 'xmipp@cnb.csic.es'
  ***************************************************************************/
-//#define METADATA
+#define METADATA
 #ifndef METADATA
 
 #include <data/args.h>
@@ -285,11 +285,11 @@ void Usage();
 int main(int argc, char *argv[])
 {
     double          rot, tilt, psi, xshift, yshift;
-    double           weight, mirror;
+    double           weight;
+    bool            mirror;
     FileName        fn_img, fn_out, fn_in;
     ImageXmipp      img;
     int             levels,round_shifts;
-    bool            verb;
 
 // Check command line options ===========================================
     try
@@ -305,9 +305,6 @@ int main(int argc, char *argv[])
             std::cout << XE;
             Usage();
         }
-        //no longer suported
-        //fn_out = getParameter(argc, argv, "-o", "");
-        verb  = checkParameter(argc, argv, "-verb");
 
         MetaData SF(fn_in);
         SF.removeObjects( MDL_ENABLED, -1 );
@@ -329,21 +326,49 @@ int main(int argc, char *argv[])
 				 strIt != SF.activeLabels.end();
 				 strIt ++ )
 			{
-				std::cout << *strIt << std::endl;
+				//std::cout << MetaDataContainer::decodeLabel(*strIt) << std::endl;
+
+
+				switch ((*strIt)) {
+					case MDL_ANGLEROT:
+						rot    = SF.angleRot();
+						img.set_rot(rot);
+						break;
+					case MDL_ANGLETILT:
+						tilt   = SF.angleTilt();
+						img.set_tilt(tilt);
+						break;
+					case MDL_ANGLEPSI:
+						psi    = SF.anglePsi();
+						img.set_psi(psi);
+						break;
+					case MDL_SHIFTX:
+						xshift = SF.shiftX();
+					    if (levels != 0)
+							xshift /= pow(2.0, levels);
+		                if (round_shifts)
+		                    xshift = ROUND(xshift);
+						img.set_Xoff(xshift);
+						break;
+					case MDL_SHIFTY:
+						yshift = SF.shiftY();
+					    if (levels != 0)
+							yshift /= pow(2.0, levels);
+		                if (round_shifts)
+		                    yshift = (float)ROUND(yshift);
+						img.set_Yoff(yshift);
+						break;
+					case MDL_WEIGHT:
+						weight = SF.weight();
+						img.set_weight(weight);
+						break;
+					case MDL_FLIP:
+						mirror = SF.flip();
+						img.set_flip(mirror);
+						break;
+				}
 			}
-			//rot    = SF.AngleRot();
-			//tilt   = SF.AngleTilt();
-			//psi    = SF.AnglePsi();
-			//xshift = SF.ShiftX();
-			//yshift = SF.ShiftY();
-			//weight = SF.Weight();
-			//mirror = SF.Flip();
-			//img.set_eulerAngles(rot, tilt, psi);
-			//img.set_originOffsets(xshift, yshift);
-			//img.set_weight(weight);
-			//img.set_flip(mirror);
-			//img.write(fn_img);
-			////LEVELS -VERB
+			img.write(fn_img);
 		}
 		while (SF.nextObject()!= MetaData::NO_MORE_OBJECTS);
     }
@@ -362,19 +387,6 @@ void Usage()
     printf("Usage:\n");
     printf("   header_assign  \n");
     printf("        -i <docfile>       : input metaData file\n");
-    //no longer suported
-    //printf("       [-o <selfile>]      : selfile containing the images to be assessed\n");
-    //printf("                             (only necessary for non-NewXmipp docfiles) \n");
-    printf("       [-verb]             : output assigned information to screen \n");
-//Instead of this offer a utility to operate with metadata files: suppress one colum
-//multiply, divide, add by a number, etc, add a constant column, merge,...
-//    printf("       [-columns] <rot=1> <tilt=2> <psi=3> <Xoff=4> <Yoff=5> \n"
-//           "                           : where the 5 integers are the column numbers for the \n"
-//           "                           : respective angles and offsets in the docfile\n"
-//           "                           : Negative column numbers result in a sign change\n"
-//           "                           : Zero means: leave this entry intact in the images\n");
-//    printf("       [-weight <col_w=6>] : Set ML-weights (from column number col_w) \n");
-//    printf("       [-mirror <col_m=7>] : Set mirror-flag (from column col_m) (0=no-flip; 1=flip)\n");
     printf("       [-round_shifts]     : Round shifts to integers \n");
     printf("       [-levels <n=0>]     : Levels of pyramidal reduction, n=1, 2, ...\n");
     exit(1);
