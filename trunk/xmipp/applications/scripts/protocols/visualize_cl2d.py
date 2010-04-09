64a07b1590568ad87f45b08ed58567c158cd5f22
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-04-09 16:59:05 +0000

    CL2D analysis added
    git-svn-id: file:///home/roberto/Git/svn@4760 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 79ab14590d3c537a303e3477521379ac8151fb02

--- trunk/xmipp/applications/scripts/protocols/visualize_cl2d.py
+++ trunk/xmipp/applications/scripts/protocols/visualize_cl2d.py
@@ -12,12 +12,93 @@
 #------------------------------------------------------------------------------------------------
 # {section} Global parameters
 #------------------------------------------------------------------------------------------------
-# Visualize the class averages of all iterations in matrix-view?
-DoShowAllIter=False
-# Separately visualize class averages of the last iteration?
-DoShowLastIter=True
-# Open files with the number of images assigned?
-DoShowFractions=True
+# Show results for levels
+""" You may specify more than one iteration here 
+    This can be done by a sequence of numbers (for instance, "0 2" 
+    specifies iteration 0 and 2. The first level is always level 0.
+    You can use negative numbers: -1 is the last iteration, -2 is the
+    one before last, ...
+"""
+DisplayLevelsNo='1'
+
+# Show Raw CL2D results
+""" Show plain CL2D results as they come out without postprocessing """
+DoShowRawCL2D=False
+
+# Show Filtered and sorted CL2D results
+""" Show plain CL2D results as they come out without postprocessing """
+DoShowFilteredSortedCL2D=True
+
+# Show Analysis Summary
+""" Show plain CL2D results as they come out without postprocessing """
+DoShowAnalysisSummary=True
+
+#------------------------------------------------------------------------------------------------
+# {section} Filter 1: General Z-score
+#------------------------------------------------------------------------------------------------
+# {expert} Show general image sorting by quality
+""" This is the basis for the first filter. F1 selfiles are constructed
+    by intersecting the classes with the images whose average image features
+    Z-score when considering the whole dataset. If you think there are two
+    few images passing this filter, increase the junk Zscore.
+"""
+DoShowF1GeneralSort=False
+
+# {expert} Show classes after this filter
+DoShowF1=False
+
+#------------------------------------------------------------------------------------------------
+# {section} Filter 2: Class PCA Z-score
+#------------------------------------------------------------------------------------------------
+# {expert} Show general image sorting by quality
+""" After filtering with F1, images remaining in the classes are
+    Z-scored within each class using a PCA projection.
+    Those images whose Z-score is larger than Junk Zscore are discarded.
+    If you feel too many images have been discarded, increase this threshold.
+    For a Gaussian distribution 99.5% of the values are within a Z-score
+    of 3.
+"""
+DoShowF2GeneralSort=False
+
+# {expert} Show classes after this filter
+DoShowF2=False
+
+#------------------------------------------------------------------------------------------------
+# {section} Filter 3: Class cores
+#------------------------------------------------------------------------------------------------
+# {expert} Show classes after this filter
+""" Filter 3 removes from each class all those images that have not been
+    in the same class during the whole hierarchical classification. This is
+    called the core. Moreover, if the core of a class is not larger than
+    a certain threshold, the whole class is removed. If you think too
+    many classes have been discarded, decrease the Good class core size.
+"""
+DoShowF3=False
+
+#------------------------------------------------------------------------------------------------
+# {section} Filter 4: Class Z-score
+#------------------------------------------------------------------------------------------------
+# {expert} Show classes after this filter
+""" After filtering with F3, images remaining in the classes are
+    Z-scored again within each class using general image features.
+    Those images whose Z-score is larger than Junk Zscore are discarded.
+    If you feel too many images have been discarded, increase this threshold.
+    For a Gaussian distribution 99.5% of the values are within a Z-score
+    of 3.
+"""
+DoShowF4=False
+
+#------------------------------------------------------------------------------------------------
+# {section} Filter 5: Class PCA Z-score
+#------------------------------------------------------------------------------------------------
+# {expert} Show classes after this filter
+""" After filtering with F4, images remaining in the classes are Z-scored
+    again within each class using a PCA projection. Those images whose
+    Z-score is larger than Junk Zscore are discarded. If you feel too
+    many images have been discarded, increase this threshold. For a
+    Gaussian distribution 99.5% of the values are within a Z-score of 3.
+"""
+DoShowF5=False
 
 #------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------
@@ -29,9 +110,17 @@ class visualize_CL2D_class:
 
     #init variables
     def __init__(self,
-                 DoShowAllIter,
-                 DoShowLastIter,
-                 DoShowFractions,
+                 DisplayLevelsNo,
+                 DoShowRawCL2D,
+                 DoShowFilteredSortedCL2D,
+                 DoShowAnalysisSummary,
+                 DoShowF1GeneralSort,
+                 DoShowF1,
+                 DoShowF2GeneralSort,
+                 DoShowF2,
+                 DoShowF3,
+                 DoShowF4,
+                 DoShowF5,
                  ProtocolName):
 	     
         import os,sys,shutil
@@ -43,12 +132,29 @@ class visualize_CL2D_class:
         shutil.copy(ProtocolName,'protocol.py')
         import protocol
         self.WorkingDir=protocol.WorkingDir
-        self.DoShowFractions=DoShowFractions
+        self.DisplayLevelsNo=DisplayLevelsNo
 
-        if (DoShowAllIter):
-            self.show_all_iter()
-        if (DoShowLastIter):
-            self.show_last_iter()
+        # Show
+        if DoShowRawCL2D:
+            self.showRawCL2D()
+        if DoShowFilteredSortedCL2D:
+            self.showFilteredSorted()
+        if DoShowAnalysisSummary:
+            self.showAnalysisSummary()
+        if DoShowF1GeneralSort:
+            self.showF1GeneralSort()
+        if DoShowF1:
+            self.showSuffix('class_level_??_.sel',"_F1")
+        if DoShowF2GeneralSort:
+            self.showF2GeneralSort()
+        if DoShowF2:
+            self.showSuffix('class_level_??_.sel',"_F12")
+        if DoShowF3:
+            self.showSuffix('class_level_??_.sel',"_F123")
+        if DoShowF4:
+            self.showSuffix('class_level_??_.sel',"_F1234")
+        if DoShowF5:
+            self.showSuffix('class_level_??_F12345.sel',"")
 
         # Return to parent dir and remove protocol.py(c)
         if (os.path.exists('protocol.py')):
@@ -59,44 +165,79 @@ class visualize_CL2D_class:
         # Finish
         self.close()
 
-    def show_all_iter(self):
+    def showRawCL2D(self):
         import os,glob
-        import selfile
         
         selfiles=glob.glob(self.WorkingDir+'/class_level_??_.sel')
         selfiles.sort()
         if len(selfiles)==0:
             print "No selfiles yet. Visualize after job completion..."
         else:
-            for selfile in selfiles:
-                command='xmipp_show -sel '+selfile+ ' &'
+            levels=self.DisplayLevelsNo.split(' ')
+            for level in levels:
+                selfile=selfiles[int(level)]
+                command='xmipp_show -cl2d '+selfile.replace('.sel','')+ ' &'
                 print '* ',command
                 os.system(command)
-                if (self.DoShowFractions):
-                    docfile=selfile.replace('.sel','.doc')
-                    command='xmipp_edit -i '+docfile+ ' &'
-                    print '* ',command
-                    os.system(command)                    
 
-    def show_last_iter(self):
-        # Visualize class averages:
+    def showFilteredSorted(self):
         import os,glob
-        selfiles=glob.glob(self.WorkingDir+'/class_level_??_.sel')
+        
+        selfiles=glob.glob(self.WorkingDir+'/class_level_??_F12345_sorted.sel')
         selfiles.sort()
         if len(selfiles)==0:
             print "No selfiles yet. Visualize after job completion..."
         else:
-            # print logfile to screen:
-            lastselfile=selfiles[-1]
-            command='xmipp_show -sel '+lastselfile+ ' &'
+            levels=self.DisplayLevelsNo.split(' ')
+            for level in levels:
+                selfile=selfiles[int(level)]
+                command='xmipp_show -cl2d '+selfile.replace('.sel','')+ ' &'
+                print '* ',command
+                os.system(command)
+
+    def showAnalysisSummary(self):
+        import os
+        fnSummary=self.WorkingDir+'/class_analysis_summary.txt'
+        if os.path.exists(fnSummary):
+            command='xmipp_edit -i '+fnSummary
             print '* ',command
             os.system(command)
-            if (self.DoShowFractions):
-                docfile=lastselfile.replace('.sel','.doc')
-                command='xmipp_edit -i '+docfile+ ' &'
+
+    def showF1GeneralSort(self):
+        import os
+        command='xmipp_show -sel '+self.WorkingDir+'/class_F1_sorted.sel '+\
+            self.WorkingDir+'/class_F1_good.sel &'
+        print '* ',command
+        os.system(command)
+        command='xmipp_edit -i '+self.WorkingDir+'/class_F1.sumZ &'
+        print '* ',command
+        os.system(command)
+
+    def showF2GeneralSort(self):
+        import os
+        command='xmipp_show -sel '+self.WorkingDir+'/class_F12_sorted.sel &'
+        print '* ',command
+        os.system(command)
+        command='xmipp_edit -i '+self.WorkingDir+'/class_F12_sorted_score.txt &'
+        print '* ',command
+        os.system(command)
+
+    def showSuffix(self,pattern,suffix):
+        import os,glob
+        
+        selfiles=glob.glob(self.WorkingDir+'/'+pattern)
+        selfiles.sort()
+        if len(selfiles)==0:
+            print "No selfiles yet. Visualize after job completion..."
+        else:
+            levels=self.DisplayLevelsNo.split(' ')
+            for level in levels:
+                selfile=selfiles[int(level)]
+                command='xmipp_show -cl2d '+selfile.replace('.sel','')+\
+                    ' -filterSuffix '+suffix+' &'
                 print '* ',command
-                os.system(command)                    
-                          
+                os.system(command)
+
     def close(self):
         message='Done!'
         print '*',message
@@ -111,7 +252,15 @@ if __name__ == '__main__':
     ProtocolName=sys.argv[1]
 
     # create ML2D_class object
-    visualize_CL2D=visualize_CL2D_class(DoShowAllIter,
-                                        DoShowLastIter,
-                                        DoShowFractions,
+    visualize_CL2D=visualize_CL2D_class(DisplayLevelsNo,
+                                        DoShowRawCL2D,
+                                        DoShowFilteredSortedCL2D,
+                                        DoShowAnalysisSummary,
+                                        DoShowF1GeneralSort,
+                                        DoShowF1,
+                                        DoShowF2GeneralSort,
+                                        DoShowF2,
+                                        DoShowF3,
+                                        DoShowF4,
+                                        DoShowF5,
                                         ProtocolName)
