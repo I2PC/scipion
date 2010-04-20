086431ec6ba57584afcd63432c377f5dc4d8fe3b
scheres <scheres@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-04-20 14:03:21 +0000

    adapted for new  image
    git-svn-id: file:///home/roberto/Git/svn@4834 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 0adab01c6092af06cadfa0ff8df2811029f47498

--- tags/devel-3/xmipp/applications/programs/average/main.cpp
+++ tags/devel-3/xmipp/applications/programs/average/main.cpp
@@ -30,8 +30,7 @@
 class Statis_parameters: public Prog_parameters
 {
 public:
-    ImageXmipp  sumI, sumI2;
-    VolumeXmipp sumV, sumV2;
+    Image<double>  sumI, sumI2;
     int         nI, nV;
     double      sumweight;
     bool        set_weight, weighted_avg, more_options, only_avg, keep_first_header, is_first;
@@ -50,8 +49,8 @@ public:
         Prog_parameters::read(argc, argv);
         set_weight = checkParameter(argc, argv, "-set_weight");
         weighted_avg = checkParameter(argc, argv, "-weighted_avg");
-	only_avg = checkParameter(argc, argv, "-only_avg");
-	keep_first_header = checkParameter(argc, argv, "-keep_first_header");
+        only_avg = checkParameter(argc, argv, "-only_avg");
+        keep_first_header = checkParameter(argc, argv, "-keep_first_header");
         sumweight = 0.;
     }
 
@@ -74,7 +73,7 @@ public:
 
 };
 
-bool process_img(ImageXmipp &img, const Prog_parameters *prm)
+bool process_img(Image<double> &img, const Prog_parameters *prm)
 {
     Statis_parameters *eprm = (Statis_parameters *) prm;
     if (eprm->keep_first_header)
@@ -98,40 +97,19 @@ bool process_img(ImageXmipp &img, const Prog_parameters *prm)
         img() *= img.weight();
         eprm->sumweight += img.weight();
     }
-    FOR_ALL_ELEMENTS_IN_MATRIX2D(img())
+    FOR_ALL_ELEMENTS_IN_ARRAY3D(img())
     {
-        MAT_ELEM(eprm->sumI(), i, j) += MAT_ELEM(img(), i, j);
+        A3D_ELEM(eprm->sumI(), k, i, j) += A3D_ELEM(img(), k, i, j);
     }
     if (!eprm->only_avg)
     {
-	FOR_ALL_ELEMENTS_IN_MATRIX2D(img())
+	FOR_ALL_ELEMENTS_IN_ARRAY3D(img())
 	{
-	    MAT_ELEM(eprm->sumI2(), i, j) += MAT_ELEM(img(), i, j) *
-                MAT_ELEM(img(), i, j);
+	    A3D_ELEM(eprm->sumI2(), k, i, j) += A3D_ELEM(img(), k, i, j) *
+                A3D_ELEM(img(), k, i, j);
 	}
     }
     eprm->nI++;
-    return true;
-}
-
-bool process_vol(VolumeXmipp &vol, const Prog_parameters *prm)
-{
-    Statis_parameters *eprm = (Statis_parameters *) prm;
-    eprm->sumV().resize(vol());
-    eprm->sumV2().resize(vol());
-    FOR_ALL_ELEMENTS_IN_MATRIX3D(vol())
-    {
-        VOL_ELEM(eprm->sumV(), k, i, j) += VOL_ELEM(vol(), k ,i, j);
-    }
-    if (!eprm->only_avg)
-    {
-	FOR_ALL_ELEMENTS_IN_MATRIX3D(vol())
-	{
-	    VOL_ELEM(eprm->sumV2(), k, i, j) +=
-                VOL_ELEM(vol(), k, i, j) * VOL_ELEM(vol(), k, i, j);
-	}
-    }
-    eprm->nV++;
     return true;
 }
 
@@ -140,65 +118,39 @@ void Statis_parameters::final_process()
     FileName fnt, fn_root = fn_in.without_extension();
     if (nI != 0)
     {
-        FOR_ALL_ELEMENTS_IN_MATRIX2D(sumI())
+        FOR_ALL_ELEMENTS_IN_ARRAY3D(sumI())
         {
-            MAT_ELEM(sumI(),  i, j) /= nI;
+            A3D_ELEM(sumI(), k, i, j) /= nI;
         }
-	if (!only_avg)
-	{
-	    FOR_ALL_ELEMENTS_IN_MATRIX2D(sumI())
-	    {
-		MAT_ELEM(sumI2(), i, j) /= nI;
-		MAT_ELEM(sumI2(), i, j) -= MAT_ELEM(sumI(), i, j) *
-                    MAT_ELEM(sumI(), i, j);
-		MAT_ELEM(sumI2(), i, j) = sqrt(ABS(MAT_ELEM(sumI2(), i, j)));
-	    }
-	}
+		if (!only_avg)
+		{
+			FOR_ALL_ELEMENTS_IN_ARRAY3D(sumI())
+			{
+			A3D_ELEM(sumI2(), k, i, j) /= nI;
+			A3D_ELEM(sumI2(), k, i, j) -= A3D_ELEM(sumI(), k, i, j) *
+						A3D_ELEM(sumI(), k, i, j);
+			A3D_ELEM(sumI2(), k, i, j) = sqrt(ABS(A3D_ELEM(sumI2(), k, i, j)));
+			}
+		}
         if (weighted_avg)
         {
             sumI() /= sumweight;
-            sumI.set_weight(sumweight);
+            sumI.setWeight(sumweight);
         }
         else if (set_weight)
         {
-            sumI.set_weight((double)nI);
+            sumI.setWeight((double)nI);
             std::cerr << " Setting weight in the header of the average image to " << integerToString(nI) << std::endl;
         }
-	if (only_avg)
-	{
-	    sumI.write(fn_root + ".xmp");
-	}
-	else
-	{
-	    sumI.write(fn_root + ".med.xmp");
-	    sumI2.write(fn_root + ".sig.xmp");
-	}
-    }
-    if (nV != 0)
-    {
-        FOR_ALL_ELEMENTS_IN_MATRIX3D(sumV())
-        {
-            VOL_ELEM(sumV(),  k, i, j) /= nV;
-        }
-	if (!only_avg)
-	{
-	    FOR_ALL_ELEMENTS_IN_MATRIX3D(sumV())
-	    {
-		VOL_ELEM(sumV2(), k, i, j) /= nV;
-		VOL_ELEM(sumV2(), k, i, j) -= VOL_ELEM(sumV(), k, i, j) *
-                    VOL_ELEM(sumV(), k, i, j);
-		VOL_ELEM(sumV2(), k, i, j) = sqrt(ABS(VOL_ELEM(sumV2(), k, i, j)));
-	    }
-	}
-	if (only_avg)
-	{
-	    sumV.write(fn_root + ".vol");
-	}
-	else
-	{
-	    sumV.write(fn_root + ".med.vol");
-	    sumV2.write(fn_root + ".sig.vol");
-	}
+		if (only_avg)
+		{
+			sumI.write(fn_root + ".xmp");
+		}
+		else
+		{
+			sumI.write(fn_root + ".med.xmp");
+			sumI2.write(fn_root + ".sig.xmp");
+		}
     }
 }
 
@@ -208,6 +160,6 @@ int main(int argc, char **argv)
     prm.each_image_produces_an_output = false;
     // Set default action for application of header transformation
     prm.apply_geo = true;
-    SF_main(argc, argv, &prm, (void*)&process_img, (void*)&process_vol);
+    SF_main(argc, argv, &prm, (void*)&process_img);
     prm.final_process();
 }
