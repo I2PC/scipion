a527f2e6b5388a2eee29b0f7b7439b0f85e06664
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-04-26 09:54:14 +0000

    rename batch_selfile_create to batch_selfile_create.py so eclipse understand it
    git-svn-id: file:///home/roberto/Git/svn@4932 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: f8dcd15a7f6cf524c05e6c0d880a9bdb3ab8dc46

---
+++ trunk/xmipp/applications/scripts/selfile_create/batch_selfile_create.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import os,glob,sys
+scriptdir=os.path.split(os.path.dirname(os.popen('which xmipp_protocols','r').read()))[0]+'/lib'
+sys.path.append(scriptdir) # add default search path
+import XmippData
+outFile=XmippData.FileName()
+if len(sys.argv) == 1:
+   print 'Usage: selfile_create "pattern"  metadataFile'
+   sys.exit()
+elif len(sys.argv) == 2:
+   outFile='/dev/stdout'
+elif len(sys.argv) == 3:
+   outFile=sys.argv[2]
+else:
+   print 'Usage   : xmipp_selfile_create "pattern"  metadataFile'
+   print 'Example1: xmipp_selfile_create "Images/*xmp"    all_images.sel'
+   print 'Example2: xmipp_selfile_create "Images/*xmp"  > all_images.sel'
+   sys.exit()
+
+
+mD=XmippData.MetaData()
+
+files = glob.glob(sys.argv[1])
+files.sort()
+for file in files:
+    mD.addObject();
+    mD.setValue( XmippData.MDL_IMAGE, file)
+    mD.setValue( XmippData.MDL_ENABLED, 1)
+mD.write(outFile)
