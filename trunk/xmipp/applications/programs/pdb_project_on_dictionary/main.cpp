56a0422bb0ece21e961bfe7d32ba819120637f69
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-04-26 06:53:21 +0000

    synchronization of the paclage
    git-svn-id: file:///home/roberto/Git/svn@4928 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: f4714d6efc88d1813ebbbe32344f0cbe2eff6ae1

--- trunk/xmipp/applications/programs/pdb_project_on_dictionary/main.cpp
+++ trunk/xmipp/applications/programs/pdb_project_on_dictionary/main.cpp
@@ -34,28 +34,19 @@ double projectOntoDictionary(const Matrix2D<double> &D,
     bool restore, int patchSize,
     Matrix3D<double> &Vin, Matrix3D<double> &Vout)
 {
+    // Get the size of the different vectors
+    int N1=patchSize;
+    int N0=patchSize;
+    int N0_3=N0*N0*N0;
+    int N1_3=N1*N1*N1;
+    int L1=(patchSize-1)/2;
+    int L0=(patchSize-1)/2;
+
     // Some initialization
     Matrix3D<double> &V0=Vin; // An alias
     Vout.initZeros(Vin);
     Matrix3D<double> Weight;
     Weight=Vout;
-
-    // Check if LASSO
-    Matrix2D<double> DtD, DtDlambda, DtDlambdaInv;
-    if (S==0)
-    {
-        int K=XSIZE(D);
-        DtD.initZeros(K,K);
-        FOR_ALL_DIRECT_ELEMENTS_IN_MATRIX2D(DtD)
-            // Compute the dot product between the columns i and j of D
-            for (int k=0; k<YSIZE(D); k++)
-                DIRECT_MAT_ELEM(DtD,i,j)+=
-                    DIRECT_MAT_ELEM(D,k,i)*DIRECT_MAT_ELEM(D,k,j);
-        DtDlambda=DtD;
-        for (int i=0; i<K; i++)
-            DIRECT_MAT_ELEM(DtDlambda,i,i)+=lambda;
-        DtDlambda.inv(DtDlambdaInv);
-    }
 
     // Separate the two dictionaries
     Matrix2D<double> D1, D2;
@@ -68,29 +59,57 @@ double projectOntoDictionary(const Matrix2D<double> &D,
     // Readjust the columns of each dictionary so that they are
     // again unitary
     Matrix1D<double> normD1;  normD1.initZeros(XSIZE(D1));
+    Matrix1D<double> normD10; normD10.initZeros(XSIZE(D1));
     for (int j=0; j<XSIZE(D1); j++)
     {
-        for (int i=0; i<YSIZE(D1); i++)
+        // Compute the norm of the high resolution part of the vector
+        for (int i=0; i<N0; i++)
+            DIRECT_VEC_ELEM(normD10,j)+=
+                DIRECT_MAT_ELEM(D1,i,j)*DIRECT_MAT_ELEM(D1,i,j);
+        DIRECT_VEC_ELEM(normD1,j)=DIRECT_VEC_ELEM(normD10,j);
+        
+        // Compute the norm of the rest of the vector
+        for (int i=N0; i<YSIZE(D1); i++)
             DIRECT_VEC_ELEM(normD1,j)+=
                 DIRECT_MAT_ELEM(D1,i,j)*DIRECT_MAT_ELEM(D1,i,j);
+        DIRECT_VEC_ELEM(normD10,j)=sqrt(DIRECT_VEC_ELEM(normD10,j));
         DIRECT_VEC_ELEM(normD1,j)=sqrt(DIRECT_VEC_ELEM(normD1,j));
         double inormD1j=1/DIRECT_VEC_ELEM(normD1,j);
+
         for (int i=0; i<YSIZE(D1); i++)
             DIRECT_MAT_ELEM(D1,i,j)*=inormD1j;
+
+        // Multiply D2 by a number so that it can be used in the recovery
+        std::cout << normD10(j) << " " << normD1(j) << std::endl;
+        double normD2j=DIRECT_VEC_ELEM(normD1,j)/DIRECT_VEC_ELEM(normD10,j);
+        for (int i=0; i<YSIZE(D2); i++)
+            DIRECT_MAT_ELEM(D2,i,j)*=normD2j;
     }
+    std::cout << "Fin\n";
     
-    // Build the pyramid and the differences between pyramid approximations
+    // Check if LASSO
+    Matrix2D<double> DtD, DtDlambda, DtDlambdaInv;
+    if (S==0)
+    {
+        int K=XSIZE(D1);
+        DtD.initZeros(K,K);
+        FOR_ALL_DIRECT_ELEMENTS_IN_MATRIX2D(DtD)
+            // Compute the dot product between the columns i and j of D
+            for (int k=0; k<YSIZE(D1); k++)
+                DIRECT_MAT_ELEM(DtD,i,j)+=
+                    DIRECT_MAT_ELEM(D1,k,i)*DIRECT_MAT_ELEM(D1,k,j);
+        DtDlambda=DtD;
+        for (int i=0; i<K; i++)
+            DIRECT_MAT_ELEM(DtDlambda,i,i)+=lambda;
+        DtDlambda.inv(DtDlambdaInv);
+    }
+
+    // Build the pyramid
     Matrix3D<double> V1;
     V0.pyramidReduce(V1);
     STARTINGX(V0)=STARTINGY(V0)=STARTINGZ(V0)=0;
 
     // Extract the training vectors from the volume
-    int N1=patchSize;
-    int N0=patchSize;
-    int N0_3=N0*N0*N0;
-    int N1_3=N1*N1*N1;
-    int L1=(patchSize-1)/2;
-    int L0=(patchSize-1)/2;
     double error=0, Nerror=0;
     Matrix1D<double> v1(N1*N1*N1+N0*N0*N0);
     Matrix1D<double> alpha(XSIZE(D1)), vp1(N1*N1*N1+N0*N0*N0), vp2(N0*N0*N0);
@@ -139,64 +158,62 @@ double projectOntoDictionary(const Matrix2D<double> &D,
                     DIRECT_VEC_ELEM(v1,idx)-=avg1;
 
                 // Project this vector onto the dictionary
-                if (S!=0) orthogonalMatchingPursuit(v1,D,S,alpha);
-                else lasso(v1,D,DtD,DtDlambdaInv,lambdaDictionary,alpha);
-                vp1.initZeros(); // vp1=D1*alpha
-                for (int j=0; j<XSIZE(D); j++)
-                    if (DIRECT_VEC_ELEM(alpha,j)!=0)
-                        for (int i=0; i<YSIZE(D); i++)
-                            DIRECT_VEC_ELEM(vp1,i)+=
-                                DIRECT_MAT_ELEM(D,i,j)*
-                                DIRECT_VEC_ELEM(alpha,j);
-
-                // Add the mean
-                for (idx=0; idx<N0; idx++)
-                    DIRECT_VEC_ELEM(vp1,idx)+=avg0;
-                for (idx=N0; idx<XSIZE(vp1); idx++)
-                    DIRECT_VEC_ELEM(vp1,idx)+=avg1;
-
-/*
-                orthogonalMatchingPursuit(v1,D1,S,alpha);
+                if (S!=0) orthogonalMatchingPursuit(v1,D1,S,alpha);
+                else lasso(v1,D1,DtD,DtDlambdaInv,lambdaDictionary,alpha);
                 vp1.initZeros(); // vp1=D1*alpha
                 vp2.initZeros(); // vp2=D2*alpha
+                bool proceed=false;
                 for (int j=0; j<XSIZE(D1); j++)
                     if (DIRECT_VEC_ELEM(alpha,j)!=0)
                     {
+                        proceed=true;
                         for (int i=0; i<YSIZE(D1); i++)
                             DIRECT_VEC_ELEM(vp1,i)+=
                                 DIRECT_MAT_ELEM(D1,i,j)*
-                                DIRECT_VEC_ELEM(alpha,j)*
-                                DIRECT_VEC_ELEM(normD1,j);
+                                DIRECT_VEC_ELEM(alpha,j);
                         if (restore)
                             for (int i=0; i<YSIZE(D2); i++)
                                 DIRECT_VEC_ELEM(vp2,i)+=
                                     DIRECT_MAT_ELEM(D2,i,j)*
-                                    DIRECT_VEC_ELEM(alpha,j)*
-                                    DIRECT_VEC_ELEM(normD1,j);
+                                    DIRECT_VEC_ELEM(alpha,j);
                     }
-*/                
-
-                // Measure the projection error
-                FOR_ALL_DIRECT_ELEMENTS_IN_MATRIX1D(vp1)
-                    error+=ABS(DIRECT_VEC_ELEM(v1,i)-DIRECT_VEC_ELEM(vp1,i));
-                Nerror+=XSIZE(vp1);
                 
-                // Put it back in the volume and update weights
-                idx=0;
+                if (proceed)
+                {
+                    if (vp1.module()!=0)
+                        std::cout << vp1.module() << " " << vp2.module() << std::endl;
 
-                // Copy the pixels at level 0
-                for (int kk=-L0; kk<=L0; kk++)
-                    for (int ii=-L0; ii<=L0; ii++)
-                        for (int jj=-L0; jj<=L0; jj++)
-                        {
-                            if (restore)
-                                DIRECT_VOL_ELEM(Vout,k0+kk,i0+ii,j0+jj)+=
-                                    DIRECT_VEC_ELEM(vp2,idx++);
-                            else
-                                DIRECT_VOL_ELEM(Vout,k0+kk,i0+ii,j0+jj)+=
-                                    DIRECT_VEC_ELEM(vp1,idx++);
-                            DIRECT_VOL_ELEM(Weight,k0+kk,i0+ii,j0+jj)++;
-                        }
+                    // Add the mean
+                    for (idx=0; idx<N0; idx++)
+                    {
+                        DIRECT_VEC_ELEM(vp1,idx)+=avg0;
+                        if (restore) DIRECT_VEC_ELEM(vp2,idx)+=avg0;
+                    }
+                    for (idx=N0; idx<XSIZE(vp1); idx++)
+                        DIRECT_VEC_ELEM(vp1,idx)+=avg1;
+
+                    // Measure the projection error
+                    FOR_ALL_DIRECT_ELEMENTS_IN_MATRIX1D(vp1)
+                        error+=ABS(DIRECT_VEC_ELEM(v1,i)-DIRECT_VEC_ELEM(vp1,i));
+                    Nerror+=XSIZE(vp1);
+
+                    // Put it back in the volume and update weights
+                    idx=0;
+
+                    // Copy the pixels at level 0
+                    for (int kk=-L0; kk<=L0; kk++)
+                        for (int ii=-L0; ii<=L0; ii++)
+                            for (int jj=-L0; jj<=L0; jj++)
+                            {
+                                if (restore)
+                                    DIRECT_VOL_ELEM(Vout,k0+kk,i0+ii,j0+jj)+=
+                                        DIRECT_VEC_ELEM(vp2,idx++);
+                                else
+                                    DIRECT_VOL_ELEM(Vout,k0+kk,i0+ii,j0+jj)+=
+                                        DIRECT_VEC_ELEM(vp1,idx++);
+                                DIRECT_VOL_ELEM(Weight,k0+kk,i0+ii,j0+jj)++;
+                            }
+                }
             }
         progress_bar(k0-4);
     }
