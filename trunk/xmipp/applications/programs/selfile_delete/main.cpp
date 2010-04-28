185d5dbe118034100882f30578f8f96b8cc32797
sjonic <sjonic@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-04-28 23:51:27 +0000

    obsolete
    git-svn-id: file:///home/roberto/Git/svn@4994 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: ed3c569a35bc11ab2c254f4f8919a3195fa15e6c

--- trunk/xmipp/applications/programs/selfile_delete/main.cpp
+++ trunk/xmipp/applications/programs/selfile_delete/main.cpp
@@ -23,108 +23,19 @@
  *  e-mail address 'xmipp@cnb.csic.es'
  ***************************************************************************/
 
+#include <data/args.h>
 #include <data/selfile.h>
 
 #include <cstdlib>
 
-/* Prototypes -============================================================= */
 
-void Usage(char **argv);
-
-
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-
-    /* Input Parameters ======================================================== */
-    FileName       sel_file;   // selection file
-
-    /* Parameters ============================================================== */
-    try
-    {
-        if (argc != 2)
-        {
-            Usage(argv);
-            exit(0);
-        }
-        else
-        {
-            sel_file = argv[1];
-        }
-    }
-    catch (Xmipp_error XE)
-    {
-        std::cout << XE;
-        Usage(argv);
-    }
-
-    try
-    {
-
-        /* Perform copy or move =================================================== */
-
-        // Finds last slash
-        std::string org_path;
-        int break_point = -1;
-        for (int i = sel_file.size() - 1; i >= 0; i--)
-        {
-            if (sel_file[i] == '/')
-            {
-                break_point = i;
-                break;
-            }
-        }
-
-        // Copy only the path
-        if (break_point >= 0)
-        {
-            org_path.resize(break_point + 1);
-            for (int j = 0; j <= break_point; j++)
-                org_path[j] = sel_file[j];
-        }
-
-        SelFile SF(sel_file);
-        std::string comStr;
-        while (!SF.eof())
-        {
-            // Get file
-            SelLine line = SF.current();
-            if (line.Is_data())
-            {   //The SelLine is not a comment
-                FileName in_name = line.get_text();
-                comStr = "rm -f " + org_path + in_name;
-                system(comStr.c_str());
-            }
-            SF.next();
-        }  // while
-
-
-        // now remove sel file
-        comStr = "rm -f " + sel_file;
-        system(comStr.c_str());
-    }
-    catch (Xmipp_error XE)
-    {
-        std::cout << XE;
-    }
-    exit(0);
-} //main
-
-/* ------------------------------------------------------------------------- */
-/* Help Message for this Program                                             */
-/* ------------------------------------------------------------------------- */
-void Usage(char **argv)
-{
-    printf(
-        "Usage: %s [Purpose and Parameters]"
-        "\nPurpose: Remove the images in a sel file (and the sel file) "
-        "\nParameter Values: (note space before value)"
-        "\nI/O parameters"
-        "\n    input_file    input sel file"
-        "\n  "
-        "\nExample: "
-        "\n    rmsel c3u.sel "
-        "\n    (will remove all images in c3u.sel file ) "
-
-        "\n"
-        , argv[0]);
+    std::cerr << "\n\nThis program has been remplaced by" 
+              << " metadata_utilities "
+              << "check also "
+	      << "metadata_operate "
+	      << "and "
+	      << "metadata_selfile_create"
+	      << "\n\n";
 }
