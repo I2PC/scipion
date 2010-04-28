40aef3ce8da8bf0ab8d4690e7b998e1000961225
sjonic <sjonic@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-04-28 23:31:55 +0000

    git-svn-id: file:///home/roberto/Git/svn@4988 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    Former-commit-id: ece6d183860cae8479ce4e258afc3372b9c144d6

--- trunk/xmipp/applications/scripts/selfile_create/batch_selfile_create.py
+++ trunk/xmipp/applications/scripts/selfile_create/batch_selfile_create.py
@@ -1,4 +1,29 @@
 #!/usr/bin/env python
+"""/***************************************************************************
+ *
+ * Authors:     Roberto Marabini
+ *
+ * Universidad Autonoma de Madrid
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.csic.es'
+ ***************************************************************************/
+"""
 import os,glob,sys
 scriptdir=os.path.split(os.path.dirname(os.popen('which xmipp_protocols','r').read()))[0]+'/lib'
 sys.path.append(scriptdir) # add default search path
@@ -19,11 +44,14 @@ else:
 
 
 mD=XmippData.MetaData()
-
+ss=XmippData.stringP()
+ii=XmippData.intP()
 files = glob.glob(sys.argv[1])
 files.sort()
+ii.assign(1)
 for file in files:
     mD.addObject();
-    XmippData.setValueString(mD, XmippData.MDL_IMAGE, file,-1)
-    XmippData.setValueInt(mD, XmippData.MDL_ENABLED, 1,-1)
+    ss.assign(file)
+    XmippData.setValueString(mD, XmippData.MDL_IMAGE, ss,-1)
+    XmippData.setValueInt(mD, XmippData.MDL_ENABLED, ii,-1)
 mD.write(outFile)
