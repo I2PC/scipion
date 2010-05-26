0d6c30a5eb8f404ce73fb0ee918ca43917d979bb
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-05-26 07:24:59 +0000

    Moved implementations of MDValue to .h
    git-svn-id: file:///home/roberto/Git/svn@5446 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 4e569d4697899f8ac565c0e3f6506f73a2e8cf54

--- tags/metadata_sqlite/xmipp/libraries/data/metadata_label.h
+++ tags/metadata_sqlite/xmipp/libraries/data/metadata_label.h
@@ -212,118 +212,32 @@ public:
     std::vector<double> vectorValue;
     MDLabel label;
 
-    void labelTypeCheck(MDLabelType type) const
-    {
-        if (MDL::labelType(this->label) != type)
-        {
-            REPORT_ERROR(-55,"Mismatch Label and Value types.");
-        }
-    }
-
+    void labelTypeCheck(MDLabelType type) const;
     //Just a simple constructor with the label
     //dont do any type checking as have not value yet
-    MDValue(MDLabel label)
-    {
-        this->label = label;
-    }
+    MDValue(MDLabel label);
     ///Constructors for each Label supported type
     ///these constructor will do the labels type checking
-    MDValue(MDLabel label, const int &intValue)
-    {
-        this->label = label;
-        labelTypeCheck(LABEL_INT);
-        this->intValue = intValue;
-    }
-    MDValue(MDLabel label, const double &doubleValue)
-    {
-        this->label = label;
-        labelTypeCheck(LABEL_DOUBLE);
-        this->doubleValue = doubleValue;
-    }
-    MDValue(MDLabel label, const bool &boolValue)
-    {
-        this->label = label;
-        labelTypeCheck(LABEL_BOOL);
-        this->boolValue = boolValue;
-    }
-    MDValue(MDLabel label, const std::string &stringValue)
-    {
-        this->label = label;
-        labelTypeCheck(LABEL_STRING);
-        this->stringValue = stringValue;
-    }
-    MDValue(MDLabel label, const std::vector<double> &vectorValue)
-    {
-        this->label = label;
-        labelTypeCheck(LABEL_VECTOR);
-        this->vectorValue = vectorValue;
-    }
-    MDValue(MDLabel label, const long int longintValue)
-    {
-        this->label = label;
-        labelTypeCheck(LABEL_LONG);
-        this->longintValue = longintValue;
-
-    }
-    MDValue(MDLabel label, const float &floatValue)
-    {
-        std::cerr << "Do not use setValue with floats, use double"<< std::endl;
-        std::cerr << "Floats are banned from metadata class"<< std::endl;
-        exit(1);
-    }
-    MDValue(MDLabel label, const char  &charValue)
-    {
-        std::cerr << "Do not use setValue with char, use string"<< std::endl;
-        std::cerr << "chars are banned from metadata class"<< std::endl;
-        exit(1);
-    }
-
+    MDValue(MDLabel label, const int &intValue);
+    MDValue(MDLabel label, const double &doubleValue);
+    MDValue(MDLabel label, const bool &boolValue);
+    MDValue(MDLabel label, const std::string &stringValue);
+    MDValue(MDLabel label, const std::vector<double> &vectorValue);
+    MDValue(MDLabel label, const long int longintValue);
+    MDValue(MDLabel label, const float &floatValue);
+    MDValue(MDLabel label, const char  &charValue);
     //These getValue also do a compilation type checking
     //when expanding templates functions and only
     //will allow the supported types
     //TODO: think if the type check if needed here
-    void  getValue(int &iv) const
-    {
-        labelTypeCheck(LABEL_INT);
-        iv = this->intValue;
-    }
-    void  getValue(double &dv) const
-    {
-        labelTypeCheck(LABEL_DOUBLE);
-        dv = this->doubleValue;
-    }
-    void  getValue(bool &bv) const
-    {
-        labelTypeCheck(LABEL_BOOL);
-        bv = this->boolValue;
-    }
-    void  getValue(std::string &sv) const
-    {
-        labelTypeCheck(LABEL_STRING);
-        sv = this->stringValue;
-    }
-    void  getValue(std::vector<double> &vv) const
-    {
-        labelTypeCheck(LABEL_VECTOR);
-        vv = this->vectorValue;
-    }
-    void  getValue(long int &lv) const
-    {
-        labelTypeCheck(LABEL_INT);
-        lv = this->longintValue;
-    }
-    void  getValue(float &floatvalue) const
-    {
-        std::cerr << "Do not use setValue with floats, use double"<< std::endl;
-        std::cerr << "Floats are banned from metadata class"<< std::endl;
-        exit(1);
-    }
-    void  getValue(char  &charvalue) const
-    {
-        std::cerr << "Do not use setValue with char, use string"<< std::endl;
-        std::cerr << "chars are banned from metadata class"<< std::endl;
-        exit(1);
-    }
+    void  getValue(int &iv) const;
+    void  getValue(double &dv) const;
+    void  getValue(bool &bv) const;
+    void  getValue(std::string &sv) const;
+    void  getValue(std::vector<double> &vv) const;
+    void  getValue(long int &lv) const;
+    void  getValue(float &floatvalue) const;
+    void  getValue(char  &charvalue) const;
 
 #define DOUBLE2STREAM(d) \
         if (withFormat) {\
@@ -335,75 +249,10 @@ public:
         if (withFormat) os << std::setw(10); \
         os << i;
 
-    void toStream(std::ostream &os, bool withFormat = false) const
-    {
-        switch (MDL::labelType(label))
-        {
-        case LABEL_BOOL: //bools are int in sqlite3
-            os << boolValue;
-            break;
-        case LABEL_INT:
-            INT2STREAM(intValue);
-            break;
-        case LABEL_LONG:
-            INT2STREAM(longintValue);
-        case LABEL_DOUBLE:
-            DOUBLE2STREAM(doubleValue);
-            break;
-        case LABEL_STRING:
-            os << stringValue;
-            break;
-        case LABEL_VECTOR:
-            os << "** ";
-            int size = vectorValue.size();
-            for (int i = 0; i < size; i++)
-            {
-                DOUBLE2STREAM(vectorValue[i]);
-                os << " ";
-            }
-            os << "**";
-            break;
-        }//close switch
-    }//close function toStream
-
-    std::string toString(bool withFormat = false) const
-    {
-        std::stringstream ss;
-        toStream(ss, withFormat);
-        return ss.str();
-    }
-
-    bool fromStream(std::istream &is)
-    {
-        switch (MDL::labelType(label))
-        {
-        case LABEL_BOOL: //bools are int in sqlite3
-            is >> boolValue;
-            break;
-        case LABEL_INT:
-            is >> intValue;
-            break;
-        case LABEL_LONG:
-            is >> longintValue;
-            break;
-        case LABEL_DOUBLE:
-            is >> doubleValue;
-            break;
-        case LABEL_STRING:
-            is >> stringValue;
-            break;
-        case LABEL_VECTOR:
-            REPORT_ERROR(-55, "Not yet supported");
-            break;
-        }
-        return true;
-    }
-
-    bool fromString(const std::string &str)
-    {
-        std::stringstream ss(str);
-        fromStream(ss);
-    }
+    void toStream(std::ostream &os, bool withFormat = false) const;
+    std::string toString(bool withFormat = false) const;
+    bool fromStream(std::istream &is);
+    bool fromString(const std::string &str);
 
 }//close class MDValue
 ;
