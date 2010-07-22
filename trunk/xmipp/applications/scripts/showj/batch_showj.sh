81cbb9272e5196633374dd5ff9277216dbe109a5
juanjovega <juanjovega@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-07-22 13:14:29 +0000

    Fixed problem copying .svn/ when compiling ij plugins; Fixed bug related to ij plugins dir (it was not accesible from everywhere)
    git-svn-id: file:///home/roberto/Git/svn@5686 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 0277d2dbba06690e65ee54a8f6c5a26c67f33a3c

--- trunk/xmipp/applications/scripts/showj/batch_showj.sh
+++ trunk/xmipp/applications/scripts/showj/batch_showj.sh
@@ -3,5 +3,5 @@ XMIPP_PROTOCOLS=`which xmipp_protocols`
 XMIPP_BASE=`dirname $XMIPP_PROTOCOLS`/..
 # GETOPTS
 MEM=512m
-java -Xmx$MEM -jar $XMIPP_BASE/external/imagej/ij.jar
+java -Xmx$MEM -Dplugins.dir=$XMIPP_BASE/external/imagej/plugins/ -jar $XMIPP_BASE/external/imagej/ij.jar
 
