7a8533cfc5d4932787203e92c990e33934dff3f3
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-07-27 10:51:39 +0000

    added back option dpr, this is needed because when procesing large volumes atan2 computation takes for ever. Furthermore no body uses any longer dpr
    git-svn-id: file:///home/roberto/Git/svn@5724 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: f7a5ae69ac78483c2b68ca58b78ac90dbc8bbea7

--- trunk/xmipp/applications/programs/resolution_fsc/main.cpp
+++ trunk/xmipp/applications/programs/resolution_fsc/main.cpp
@@ -35,7 +35,7 @@ void writeFiles(const FileName &fnRoot,
                 const MultidimArray<double> &frc_noise,
                 const MultidimArray<double> &dpr,
                 const MultidimArray<double> &error_l2,
-                float max_sam)
+                float max_sam, bool do_dpr)
 {
     MetaData MD;
 
@@ -48,12 +48,14 @@ void writeFiles(const FileName &fnRoot,
             MD.addObject();
             if(max_sam >=0 && ((1./dAi(freq, i))<max_sam) )
             {
-                dAi(dpr, i)=0.;
+                if(do_dpr)
+                    dAi(dpr, i)=0.;
                 dAi(frc, i)=0.;
             }
             MD.setValue(MDL_RESOLUTION_FREQ,dAi(freq, i));
             MD.setValue(MDL_RESOLUTION_FRC,dAi(frc, i));
-            MD.setValue(MDL_RESOLUTION_DPR,dAi(dpr, i));
+            if(do_dpr)
+                MD.setValue(MDL_RESOLUTION_DPR,dAi(dpr, i));
             MD.setValue(MDL_RESOLUTION_ERRORL2,dAi(error_l2, i));
             MD.setValue(MDL_RESOLUTION_FRCRANDOMNOISE,dAi(frc_noise, i));
             MD.setValue(MDL_RESOLUTION_FREQREAL,1./dAi(freq, i));
@@ -69,6 +71,7 @@ public:
     Image<double>  refI;
     float       sam;
     float       max_sam;
+    bool        do_dpr;
 public:
     void read(int argc, char **argv)
     {
@@ -84,6 +87,8 @@ public:
         std::cout << "Reference file  = " << fn_ref  << std::endl;
         std::cout << "Sampling rate   = " << sam     << std::endl;
         std::cout << "Max Resolution  = " << max_sam << std::endl;
+        if(do_dpr)
+            std::cout << "do_dpr is ON  = " << std::endl;
     }
     void usage()
     {
@@ -95,6 +100,7 @@ public:
         std::cerr << " For both modes:\n";
         std::cerr << "   -sam <sampling rate>     : i.e. pixel size (in Angstrom) \n";
         std::cerr << "  [-dont_apply_geo]         : for 2D-images: do not apply transformation stored in the header\n";
+        std::cerr << "  [-do_dpr]                 : compute dpr, by default only frc is computed\n";
         std::cerr << "  [-max_sam=-1]             : set fsc to 0 for frequencies above this one (Ang), -1-> all fequencies\n";
     }
 };
@@ -104,18 +110,20 @@ bool process_img(Image<double> &img, const Prog_parameters *prm)
     Resolution_parameters *eprm = (Resolution_parameters *) prm;
     MultidimArray<double> freq, frc, dpr, frc_noise, error_l2;
     frc_dpr(eprm->refI(), img(), eprm->sam, freq, frc, frc_noise, dpr, error_l2);
-    writeFiles(img.name(), freq, frc, frc_noise, dpr, error_l2, eprm->max_sam);
+    writeFiles(img.name(), freq, frc, frc_noise, dpr, error_l2, eprm->max_sam,eprm->do_dpr);
     return true;
 }
 
 int main(int argc, char **argv)
 {
     float max_sam = textToFloat(getParameter(argc, argv, "-max_sam","-1"));
+    bool do_dpr=checkParameter(argc, argv, "-do_dpr");
     if (!checkParameter(argc, argv, "-set_of_images"))
     {
         Resolution_parameters prm;
         prm.each_image_produces_an_output = false;
         prm.apply_geo = true;
+        prm.do_dpr=do_dpr;
         prm.max_sam=max_sam;
         SF_main(argc, argv, &prm, (void*)&process_img);
     }
@@ -146,7 +154,7 @@ int main(int argc, char **argv)
             Image<double>       I1, I2, Id;
             double      dummy;
             MultidimArray<double> freq, frc, dpr, frc_noise, ssnr, error_l2,
-                pixel;
+            pixel;
 
             SFaux.read(fn_sel);
             SF.randomize(SFaux);
@@ -155,8 +163,8 @@ int main(int argc, char **argv)
             getStatistics(SF2,I2,Id,dummy,dummy,true);
             I1().setXmippOrigin();
             I2().setXmippOrigin();
-            frc_dpr(I1(), I2(), sam, freq, frc, frc_noise, dpr,error_l2);
-            writeFiles(fn_sel, freq, frc, frc_noise, dpr,error_l2,max_sam);
+            frc_dpr(I1(), I2(), sam, freq, frc, frc_noise, dpr,error_l2,do_dpr);
+            writeFiles(fn_sel, freq, frc, frc_noise, dpr,error_l2,max_sam,do_dpr);
         }
         catch (Xmipp_error XE)
         {
