bb23c20bc518baafff26a403e3da1a9a1afc7213
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-08-06 07:39:07 +0000

    Histogram ported
    git-svn-id: file:///home/roberto/Git/svn@5791 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 3c1338f26ad27ee23d12f0ac4729b87f88f5cd2a

--- trunk/xmipp/applications/programs/histogram/main.cpp
+++ trunk/xmipp/applications/programs/histogram/main.cpp
@@ -25,8 +25,8 @@
 
 #include <data/args.h>
 #include <data/image.h>
-#include <data/selfile.h>
-#include <data/volume.h>
+#include <data/metadata.h>
+#include <data/image.h>
 #include <data/mask.h>
 #include <data/histogram.h>
 
@@ -34,25 +34,21 @@ void Usage();
 
 int main(int argc, char **argv)
 {
-    VolumeXmipp     volume;
-    SelFile         SF;
-    ImageXmipp      image;
-    bool            image_mode, volume_mode;
+    Image<double>   image;
+    MetaData        SF;
     FileName        fn_in, fn_out, fn_sel;
     Mask_Params     mask_prm(INT_MASK);
     bool            automatic_range, do_normalize, is_first = true;
     double          m, M; // range for histogram
     int             StepsNo;
     Histogram1D     hist, histb;
-#define         V VOLMATRIX(volume)
-#define         I IMGMATRIX(image)
 
     // Read arguments --------------------------------------------------------
     try
     {
         fn_sel      = getParameter(argc, argv, "-sel","");
         if (fn_sel=="")
-            fn_in     = getParameter(argc, argv, "-i");
+            fn_in = getParameter(argc, argv, "-i");
         fn_out     = getParameter(argc, argv, "-o", "");
 
         StepsNo = textToInteger(getParameter(argc, argv, "-steps", "100"));
@@ -66,8 +62,9 @@ int main(int argc, char **argv)
             M = textToFloat(argv[i+2]);
             automatic_range = false;
         }
-        else automatic_range = true;
-        
+        else
+            automatic_range = true;
+
         if (fn_sel!="" && automatic_range)
             REPORT_ERROR(1,"Error: only use selfile in combination with fixed range!");
 
@@ -84,68 +81,44 @@ int main(int argc, char **argv)
     try
     {
         if (fn_sel!="")
-        {
             SF.read(fn_sel);
-        }
         else
         {
-            SF.insert(fn_in);
+            SF.addObject();
+            SF.setValue(MDL_IMAGE,fn_in);
         }
-        SF.go_beginning();
-        while (!SF.eof())
+        SF.firstObject();
+        FOR_ALL_OBJECTS_IN_METADATA(SF)
         {
-            fn_in = SF.NextImg();
-            if (fn_in=="") break;
-
-            if (Is_ImageXmipp(fn_in))
-            {
-                image_mode = true;
-                image.read(fn_in);
-            }
-            else if (Is_VolumeXmipp(fn_in))
-            {
-                image_mode = false;
-                volume.read(fn_in);
-            }
-            else EXIT_ERROR(1, "Histogram: Input file is not an image nor a volume");
+            SF.getValue(MDL_IMAGE,fn_in);
+            image.read(fn_in);
+            image().setXmippOrigin();
+            mask_prm.generate_mask(image());
 
             // Compute histogram ----------------------------------------------------
-            if (image_mode)
-            {
-                I.setXmippOrigin();
-                mask_prm.generate_2Dmask(I);
-                const Matrix2D<int> & mask2D = mask_prm.get_binary_mask2D();
-                if (automatic_range)
-                    compute_hist_within_binary_mask(mask2D, I, hist, StepsNo);
-                else
-                    compute_hist_within_binary_mask(mask2D, I, hist, m, M, StepsNo);
-            }
+            if (automatic_range)
+                compute_hist_within_binary_mask(mask_prm.get_binary_mask(), image(),
+                                                hist, StepsNo);
             else
-            {
-                V.setXmippOrigin();
-                mask_prm.generate_3Dmask(V);
-                const Matrix3D<int> & mask3D = mask_prm.get_binary_mask3D();
-                if (automatic_range)
-                    compute_hist_within_binary_mask(mask3D, V, hist, StepsNo);
-                else
-                    compute_hist_within_binary_mask(mask3D, V, hist, m, M, StepsNo);
-            }
+                compute_hist_within_binary_mask(mask_prm.get_binary_mask(), image(),
+                                                hist, m, M, StepsNo);
+
             if (is_first)
             {
                 is_first = false;
                 histb = hist;
             }
             else
-            {
                 histb += hist;
-            }
         }
-        
+
         if (do_normalize)
             histb /= (histb.sum() * histb.step_size);
 
-        if (fn_out != "") histb.write(fn_out);
-        else            std::cout << hist;
+        if (fn_out != "")
+            histb.write(fn_out);
+        else
+            std::cout << hist;
     }
     catch (Xmipp_error Xe)
     {
@@ -165,43 +138,3 @@ void Usage()
     << "  [-steps <N=100>]             : number of subdivisions\n"
     << "  [-norm]                      : normalize histogram \n";
 }
-
-/* Menus ------------------------------------------------------------------- */
-/*Colimate:
-   PROGRAM Histogram {
-      url="http://www.cnb.uam.es/~bioinfo/NewXmipp/Applications/Src/Histogram/Help/histogram.html";
-      help="Compute histograms of images and volumes within a mask";
-      OPEN MENU Histogram;
-      COMMAND LINES {
-         + usual: histogram -i $FILE_IN [-o $FILE_OUT]
-             [-range $MIN $MAX]  [-steps $STEPS]
-             #include "binary_mask_line.mnu"
-      }
-      PARAMETER DEFINITIONS {
-         $FILE_IN {
-            label="Input file";
-            help="Image or volume";
-            type=FILE EXISTING;
-         }
-         $FILE_OUT {
-            label="Output Text file";
-            type=FILE;
-            help="Column 1: density value; Column 2: voxel count";
-         }
-         OPT(-range) {label="Range";}
-            $MIN {label="Minimum"; type=FLOAT;}
-            $MAX {label="Maximum"; type=FLOAT;}
-         $STEPS {label="Steps"; type=NATURAL; by default=100;}
-         #include "binary_mask_vars.mnu"
-      }
-   }
-   MENU Histogram {
-      "I/O Parameters"
-      $FILE_IN
-      OPT(-o)
-      "Histogram Specification"
-      OPT(-range)
-      OPT(-steps)
-      #include "binary_mask_menu.mnu"
-   }
-*/
