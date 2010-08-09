c76ee46aaa93199d41c30f536c58c2ea78fc5457
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-08-09 08:33:11 +0000

    It has been integrated into image.h
    git-svn-id: file:///home/roberto/Git/svn@5817 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 51478a81d5e86c103d088aabad477f7cae7b4700

--- trunk/xmipp/libraries/data/define.h
+++
@@ -1,8 +0,0 @@
-#ifndef DEFINE_H
-#define DEFINE_H
-
-#include "funcs.h"
-    #define WRITE_OVERWRITE 0 //forget about the old file and overwrite it
-    #define WRITE_APPEND 1    //append and object at the end of a stack, so far can not append stacks
-    #define WRITE_REPLACE 2   //replace a particular object by another
-#endif
