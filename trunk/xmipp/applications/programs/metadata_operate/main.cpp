4570c6fd5b6f7013205f1749cb2b69d2de40e375
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-09-30 08:47:16 +0000

    Changed metadata_operate
    git-svn-id: file:///home/roberto/Git/svn@6133 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: d0b8a525d18d213164310ca3581ab73725cc9154

--- trunk/xmipp/applications/programs/metadata_operate/main.cpp
+++ trunk/xmipp/applications/programs/metadata_operate/main.cpp
@@ -25,51 +25,59 @@
 
 #include <data/metadata.h>
 #include <data/args.h>
+#include <data/program.h>
 
-void Usage();
-
-int main(int argc, char **argv)
+class ProgMetadataOperate: public XmippProgram
 {
     FileName fn_in, fn_out;
     std::string expressionStr;
     MetaData mdIn;
 
-    try
+protected:
+
+    void defineParams()
     {
-        fn_in = getParameter(argc, argv, "-i");
-        fn_out = getParameter(argc, argv, "-o", "/dev/stdout");
-        expressionStr = getParameter(argc, argv, "-e");
-    }
-    catch (XmippError)
-    {
-        Usage();
-        exit(1);
+        addUsageLine("Perform operations on MetaData columns. See examples below.");
+        addUsageLine("Example:  ");
+        addUsageLine("  operate_metadata  -i a.doc -o b.doc -e  \"angleRot=(angleRot*3.1416/180.)\"  ");
+        addUsageLine("  operate_metadata  -i a.doc -o b.doc -e  \"image=replace(image, 'xmp','spi')\"  ");
+        addParamsLine("  -i <inputMetadata>     :MetaData input file name       ");
+        addParamsLine("     alias --input;");
+        addParamsLine("  -o <outputMetadata=\"/dev/stdout\"> :MetaData output file name, by default print to screen");
+        addParamsLine("     alias --output;");
+        addParamsLine("  -e <expression>                     :any valid operation in SQLite (expression must be between quotes) ");
+        addParamsLine("     alias --expression;");
     }
 
-    try
+    void readParams()
+    {
+        fn_in = getParam("-i");
+        fn_out = getParam("-o");
+        expressionStr = getParam("-e");
+    }
+
+public:
+    void run()
     {
         mdIn.read(fn_in);
         mdIn.operate(expressionStr);
         mdIn.write(fn_out);
     }
-    catch (XmippError)
-    {
-        std::cerr << "ERROR, exiting..." << std::endl;
-        exit(1);
-    }
 
 }
+;//end of class ProgMetadataOperate
 
-void Usage()
+/* MAIN -------------------------------------------------------------------- */
+int main(int argc, char *argv[])
 {
-    std::cout << "Usage: metadata_operate [options]\n"
-            << "  Example:  \n"
-            << " operate_metadata  -i a.doc -o b.doc -e  \"angleRot=(angleRot*3.1416/180.)\"  \n"
-            << " operate_metadata  -i a.doc -o b.doc -e  \"image=replace(image, 'xmp','spi')\"  \n"
-            << " Options:\n"
-            << "  -i inputMetadata     MetaData input file name       \n"
-            << "  -o outputMetadata    MetaData output file name, by default print to screen\n"
-            << "  -e expression,       any valid operation in SQLite (expression must be between quotes) \n"
-
-    ;
+    try
+    {
+        ProgMetadataOperate program;
+        program.read(argc, argv);
+        program.run();
+    }
+    catch (XmippError xe)
+    {
+        std::cerr << xe;
+    }
 }
