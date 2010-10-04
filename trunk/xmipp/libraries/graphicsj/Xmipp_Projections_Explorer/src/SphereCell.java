ba61a8474536f3732c370b006d475363dd455aef
juanjovega <juanjovega@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-10-04 14:12:34 +0000

    Updating projections explorer
    git-svn-id: file:///home/roberto/Git/svn@6168 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 5cc2586461df5dbcc01051fd2aca1c6868fe7f1d

---
+++ trunk/xmipp/libraries/graphicsj/Xmipp_Projections_Explorer/src/SphereCell.java
@@ -0,0 +1,23 @@
+
+
+
+import java.util.Vector;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Juanjo Vega
+ */
+public class SphereCell {
+
+    public Vector<String> fileNames;    // Image file name.
+    public double value = 0.0;
+    public boolean locked = false;
+
+    public SphereCell() {
+        fileNames = new Vector<String>();
+    }
+}
