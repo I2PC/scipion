f9c77c1837ef5504655ab3de4d8c383fc356781f
juanjovega <juanjovega@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-10-14 15:30:19 +0000

    updating plugins
    git-svn-id: file:///home/roberto/Git/svn@6241 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: b5278216ca3f2915ce98b0114a60ec1b84940385

---
+++ trunk/xmipp/libraries/graphicsj/Xmipp_Browser/src/browser/Model.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package browser;
+
+/**
+ *
+ * @author Juanjo Vega
+ */
+public class Model {
+
+    public static boolean SHOW_PREVIEWS = true;
+}
