0e3f61fc5c0b2f06fbfad492d709a9f405e756ef
juanjovega <juanjovega@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-10-14 09:04:41 +0000

    Updating ImageJ plugin
    git-svn-id: file:///home/roberto/Git/svn@6235 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 1111a57152c58a247ed7839abde44adb79aa68b9

---
+++ trunk/xmipp/libraries/graphicsj/Xmipp_Browser/src/browser/windows/ImagesWindowFactory.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package browser.windows;
+
+import ij.IJ;
+import ij.ImagePlus;
+import java.io.File;
+
+/**
+ *
+ * @author Juanjo Vega
+ */
+public class ImagesWindowFactory {
+
+    public static ImagePlus openImageWindow(File file) {
+        return openImageWindow(file, -1);
+    }
+
+    public static void openImageWindow(String file, boolean poll) {
+        File f = new File(file);
+        ImagePlus ip = IJ.openImage(f.getAbsolutePath());
+        openImageWindow(ip, poll);
+    }
+
+    public static ImagePlus openImageWindow(File file, int slice) {
+        ImagePlus ip = null;
+
+        if (file != null) {
+            ip = IJ.openImage(file.getAbsolutePath());
+
+            if (ip != null) {
+                ImagePlus ip2 = ip;
+
+                if (slice >= 0) {
+                    ip2 = new ImagePlus();
+                    ip2.getProcessor().setPixels(ip.getStack().getPixels(slice));
+                }
+
+                openImageWindow(ip2, false);
+            } else {
+                // Not an image, so let ImageJ open it as usually.
+                IJ.open(file.getAbsolutePath());
+            }
+        } else {
+            IJ.showMessage("Trying to open a null file.");
+        }
+
+        return ip;
+    }
+
+    public static void openImageWindow(ImagePlus ip, boolean poll) {
+        if (ip != null) {
+            if (ip.getStackSize() > 1) {
+                new WindowStackOperations(ip, poll);
+            } else {
+                new WindowImageOperations(ip, poll);
+            }
+        } else {
+            IJ.showMessage("Trying to open a null image.");
+        }
+    }
+}
---
+++ trunk/xmipp/libraries/graphicsj/Xmipp_Browser/src/browser/windows/WindowStackOperations.java
@@ -0,0 +1,130 @@
+package browser.windows;
+
+import browser.windows.menubar.XmippMenuBar;
+import ij.IJ;
+import ij.ImagePlus;
+import ij.gui.ImageLayout;
+import ij.gui.StackWindow;
+import ij.io.FileInfo;
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.WindowEvent;
+import java.io.File;
+import java.util.Timer;
+import java.util.TimerTask;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Juanjo Vega
+ */
+public class WindowStackOperations extends StackWindow implements iPollImageWindow {
+
+    protected boolean poll = false;
+    protected Timer timer = null;
+    protected final static int PERIOD = 5000;  // repeat every 5 seconds
+    private String path;// = imp.getOriginalFileInfo().directory + imp.getOriginalFileInfo().fileName;
+    private File f;// = new File(path);
+    private long last;// = f.lastModified();
+
+    public WindowStackOperations(ImagePlus imp) {
+        this(imp, false);
+    }
+
+    public WindowStackOperations(ImagePlus imp, boolean poll) {
+        super(imp);
+
+        JPanel previousContent = new JPanel();
+        previousContent.setLayout(new ImageLayout(ic));
+
+        Component components[] = getComponents();
+        for (int i = 0; i < components.length; i++) {
+            previousContent.add(components[i]);
+        }
+
+        removeAll();
+        setLayout(new BorderLayout());
+
+        add(previousContent, BorderLayout.CENTER);
+
+        // Sets if image can poll (reloaded from disk) or not.
+        FileInfo ofi = imp.getOriginalFileInfo();
+
+        if (ofi != null) {
+            path = ofi.directory + ofi.fileName;
+            f = new File(path);
+            last = f.lastModified();
+
+            setPoll(poll);  // If isPoll, starts timer to reload image form disk every period.
+        }
+
+        setMenuBar(new XmippMenuBar(this, imp, ofi != null));
+
+        fixSize();
+    }
+
+    public boolean isPoll() {
+        return poll;
+    }
+
+    public synchronized void setPoll(boolean poll) {
+        if (this.poll != poll) {    // If state changes.
+            this.poll = poll;
+
+            if (poll) {
+                startTimer();
+            } else {
+                if (timer != null) {
+                    timer.cancel();
+                    timer = null;
+                }
+            }
+        }
+    }
+
+    private void startTimer() {
+        // Avoid multiple timers running simultaneously.
+        if (timer == null) {
+            timer = new Timer();
+
+            timer.scheduleAtFixedRate(new TimerTask() {
+
+                public void run() {
+                    // Reverts only when image has changed since last time.
+                    if (last < f.lastModified()) {
+                        IJ.showStatus("Reloading " + imp.getTitle());
+
+                        ImagePlus imp2 = IJ.openImage(path);
+                        imp.setStack(imp2.getStack(), imp.getNChannels(), imp.getNSlices(), imp.getNFrames());
+                        imp.updateAndDraw();
+
+                        last = f.lastModified();
+
+                        IJ.showStatus("");
+                    }
+                }
+            }, 0, PERIOD);
+        }
+    }
+
+    @Override
+    public void windowClosing(WindowEvent e) {
+        setPoll(false);
+
+        super.windowClosing(e);
+    }
+
+    private void fixSize() {
+        pack();
+
+        Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        Point loc = getLocation();
+        Dimension size = getSize();
+        if (loc.y + size.height > screen.height) {
+            getCanvas().zoomOut(0, 0);
+        }
+    }
+}
