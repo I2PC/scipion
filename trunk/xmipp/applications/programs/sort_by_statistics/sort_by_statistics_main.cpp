3b862d8285d12926515657854661316fa24fc083
joaquinoton <joaquinoton@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-11-04 08:04:20 +0000

    "include" updated because of reconstruction/pca.* moved to data/basic_pca.*
    git-svn-id: file:///home/roberto/Git/svn@6409 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: d29d1fa7ade3728eb8901d6f6b1d380c4e6ab675

--- trunk/xmipp/applications/programs/sort_by_statistics/sort_by_statistics_main.cpp
+++ trunk/xmipp/applications/programs/sort_by_statistics/sort_by_statistics_main.cpp
@@ -30,7 +30,7 @@
 #include <data/args.h>
 #include <data/metadata.h>
 #include <data/metadata_extension.h>
-#include <classification/pca.h>
+#include <data/basic_pca.h>
 
 class Sort_junk_parameters
 {
