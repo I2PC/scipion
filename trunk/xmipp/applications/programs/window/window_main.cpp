cea1a6659224380c700520623a00ba19393d3088
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-11-10 14:32:21 +0000

    Ported to new standards
    git-svn-id: file:///home/roberto/Git/svn@6458 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 6b2018e9371ef6362a87e4c4e93f857a874a86e8

--- trunk/xmipp/applications/programs/window/window_main.cpp
+++ trunk/xmipp/applications/programs/window/window_main.cpp
@@ -27,236 +27,203 @@
 #include <data/args.h>
 #include <data/image.h>
 
-class Window_parameters: public Prog_parameters
+class ProgWindow: public XmippMetadataProgram
 {
 public:
-    bool size_mode;
-    bool crop_mode;
+    typedef enum {CORNERMODE, SIZEMODE, CROPMODE} WindowMode;
+
     bool physical_coords;
     int sizeX, sizeY, sizeZ;
     int cropX, cropY, cropZ;
     int x0, y0, z0;
     int xF, yF, zF;
-    double init_value;
-    int wrong_parameters;
-    int average_pad;
-    int corner_pad;
+    double padValue;
+    std::string padType;
+    WindowMode mode;
+    Image<char>                IChar;
+    Image<unsigned char>       IUChar;
+    Image<short int>           IShort;
+    Image<unsigned short int>  IUShort;
+    Image<float>               IFloat;
 
-    void read(int argc, char **argv)
+    void defineParams()
     {
-        init_value = 0.;
-        wrong_parameters = 0;
-        average_pad = false;
-        corner_pad = false;
-        size_mode=false;
-        crop_mode=false;
+        each_image_produces_an_output = true;
+        XmippMetadataProgram::defineParams();
+        addUsageLine("This program takes a region from the input images");
+        addUsageLine("Regions may be larger than the image itself, in that case");
+        addUsageLine("padding is applied.");
+        addParamsLine("  -corners <...>                        : Windows corners (by default indexes are logical)");
+        addParamsLine("                                        : 2D: <x0> <y0> <xF> <yF>");
+        addParamsLine("                                        : 3D: <x0> <y0> <z0> <xF> <yF> <zF>");
+        addParamsLine("  or -size <sizeX> <sizeY=0> <sizeZ=0>  : Window to a new size");
+        addParamsLine("                                        : if only one is given, the other two");
+        addParamsLine("                                        : are supposed to be the same");
+        addParamsLine("  or -crop <sizeX> <sizeY=0> <sizeZ=0>  : Crop this amount of pixels in each direction");
+        addParamsLine("                                        : if only one is given, the other two");
+        addParamsLine("                                        : are supposed to be the same");
+        addParamsLine("  [-physical]                           : use physical instead of logical coordinates");
+        addParamsLine("    requires -corners;");
+        addParamsLine("  [-pad <padtype=value>]                : value used for padding");
+        addParamsLine("   where <padtype>");
+        addParamsLine("         value <v=0>                    : use this value for padding");
+        addParamsLine("         corner                         : use the top-left corner for padding");
+        addParamsLine("         avg                            : use the image average for padding");
+    }
 
-        Prog_parameters::read(argc, argv);
-
-        if (checkParameter(argc, argv, "-pad_value"))
+    void readParams()
+    {
+        XmippMetadataProgram::readParams();
+        padType=getParam("-pad");
+        padValue=getDoubleParam("-pad","value");
+        if (checkParam("-corners"))
         {
-            init_value = textToFloat(getParameter(argc, argv, "-pad_value"));
-            wrong_parameters = 1;
-        }
-        if (checkParameter(argc, argv, "-corner_pad_value"))
-        {
-            corner_pad = true;
-            if (wrong_parameters > 0)
-                wrong_parameters = -1;
-            else
-                wrong_parameters = 1;
-        }
-        if (checkParameter(argc, argv, "-average_pad_value"))
-        {
-            average_pad = true;
-            if (wrong_parameters > 0)
-                wrong_parameters = -1;
-            else
-                wrong_parameters = 1;
-        }
-        if (wrong_parameters == -1)
-            REPORT_ERROR(ERR_ARG_INCORRECT, "incompatible options");
-
-        if (checkParameter(argc, argv, "-size"))
-        {
-            size_mode = true;
-            int i = paremeterPosition(argc, argv, "-size");
-            if (i + 2 >= argc)
+            int nparams=getCountParam("-corners");
+            if (nparams==4 && nparams==6)
             {
-                sizeZ = sizeY = sizeX = textToInteger(argv[i+1]);
-            }
-            else if (i + 3 >= argc)
-            {
-                sizeZ = sizeY = textToInteger(argv[i+2]);
-                sizeX = textToInteger(argv[i+1]);
-            }
-            else if (i + 4 >= argc)
-            {
-                sizeZ = textToInteger(argv[i+3]);
-                sizeY = textToInteger(argv[i+2]);
-                sizeX = textToInteger(argv[i+1]);
+                x0=getIntParam("-corners",0);
+                y0=getIntParam("-corners",1);
+                if (nparams==4)
+                {
+                    xF=getIntParam("-corners",2);
+                    yF=getIntParam("-corners",3);
+                }
+                else
+                {
+                    z0=getIntParam("-corners",2);
+                    xF=getIntParam("-corners",3);
+                    yF=getIntParam("-corners",4);
+                    zF=getIntParam("-corners",5);
+                }
             }
             else
-                REPORT_ERROR(ERR_ARG_DEPENDENCE, "Not enough parameters after -size");
-
+                REPORT_ERROR(ERR_ARG_INCORRECT,"Incorrect number of arguments after -corners");
+            physical_coords = checkParam("-physical");
+            mode=CORNERMODE;
+            // TODO Chequear el número de parámetros
+        }
+        else if (checkParam("-size"))
+        {
+            sizeX=getIntParam("-size",0);
+            sizeY=getIntParam("-size",1);
+            sizeZ=getIntParam("-size",2);
+            if (sizeY==0)
+                sizeY=sizeX;
+            if (sizeZ==0)
+                sizeZ=sizeX;
             x0 = FIRST_XMIPP_INDEX(sizeX);
             y0 = FIRST_XMIPP_INDEX(sizeY);
             z0 = FIRST_XMIPP_INDEX(sizeZ);
             xF = LAST_XMIPP_INDEX(sizeX);
             yF = LAST_XMIPP_INDEX(sizeY);
             zF = LAST_XMIPP_INDEX(sizeZ);
+            mode=SIZEMODE;
             physical_coords = false;
         }
-        else if (checkParameter(argc, argv, "-r0"))
+        else if (checkParam("-crop"))
         {
-            size_mode = false;
-            // Get r0
-            int i = paremeterPosition(argc, argv, "-r0");
-            if (i + 2 >= argc)
-                REPORT_ERROR(ERR_ARG_DEPENDENCE, "Not enough parameters after -r0");
-            else
-            {
-                x0 = textToInteger(argv[i+1]);
-                y0 = textToInteger(argv[i+2]);
-            }
-            if (i + 3 < argc)
-                try
-                {
-                    z0 = textToInteger(argv[i+3]);
-                }
-                catch (XmippError XE)
-                {
-                    z0 = 1;
-                }
-
-            // Get rF
-            i = paremeterPosition(argc, argv, "-rF");
-            if (i == -1)
-                REPORT_ERROR(ERR_ARG_MISSING, "-rF not present");
-            if (i + 2 >= argc)
-                REPORT_ERROR(ERR_ARG_DEPENDENCE, "Not enough parameters after -rF");
-            else
-            {
-                xF = textToInteger(argv[i+1]);
-                yF = textToInteger(argv[i+2]);
-            }
-            if (i + 3 < argc)
-                try
-                {
-                    zF = textToInteger(argv[i+3]);
-                }
-                catch (XmippError XE)
-                {
-                    zF = 1;
-                }
-
-            physical_coords = checkParameter(argc, argv, "-physical");
+            cropX=getIntParam("-crop",0);
+            cropY=getIntParam("-crop",1);
+            cropZ=getIntParam("-crop",2);
+            if (cropY==0)
+                cropY=cropX;
+            if (cropZ==0)
+                cropZ=cropX;
+            mode=CROPMODE;
+            physical_coords = false;
         }
-        else if (checkParameter(argc, argv, "-crop"))
-        {
-            crop_mode = true;
-            int i = paremeterPosition(argc, argv, "-crop");
-            if (i + 2 >= argc)
-            {
-                cropZ = cropY = cropX = textToInteger(argv[i+1]);
-            }
-            else if (i + 3 >= argc)
-            {
-                cropZ = cropY = textToInteger(argv[i+2]);
-                cropX = textToInteger(argv[i+1]);
-            }
-            else if (i + 4 >= argc)
-            {
-                cropZ = textToInteger(argv[i+3]);
-                cropY = textToInteger(argv[i+2]);
-                cropX = textToInteger(argv[i+1]);
-            }
-            else
-                REPORT_ERROR(ERR_ARG_DEPENDENCE, "Not enough parameters after -crop");
-        }
-
-        else
-            REPORT_ERROR(ERR_ARG_INCORRECT, "Unknown windowing type");
     }
 
     void show()
     {
-        Prog_parameters::show();
-        if (size_mode)
+        XmippMetadataProgram::show();
+        switch (mode)
+        {
+        case SIZEMODE:
             std::cout << "New size: (XxYxZ)=" << sizeX << "x" << sizeY << "x"
             << sizeZ << std::endl;
-        else if (crop_mode)
+            break;
+        case CROPMODE:
             std::cout << "Crop: (XxYxZ)=" << cropX << "x" << cropY << "x"
             << cropZ << std::endl;
-        else
+            break;
+        case CORNERMODE:
             std::cout << "New window: from (z0,y0,x0)=(" << z0 << ","
             << y0 << "," << x0 << ") to (zF,yF,xF)=(" << zF << "," << yF
             << "," << xF << ")\n"
             << "Physical: " << physical_coords << std::endl;
+        }
     }
 
-    void usage()
+    template <typename T>
+    void processImage(Image<T> &Iin, const FileName &fnImg, const FileName &fnImgOut)
     {
-        Prog_parameters::usage();
-        std::cerr << "  [-physical]               : Use physical instead of logical\n"
-        << "                             coordinates\n"
-        << "  [-pad_value <val>]        : value used for padding\n"
-        << "  [-corner_pad_value]       : use the value of the upper\n"
-        << "                              left corner for padding\n"
-        << "  [-average_pad_value]      : use the image average for padding\n"
-        << "  [-r0 <x0> <y0> [<z0>]     : Window using window corners\n"
-        << "   -rF <xF> <yF> [<zF>]]    : by default indexes are logical\n"
-        << "  [-size <sizeX> [<sizeY>] [<sizeZ>]]: Window to a new size\n"
-        << "                            : if only one is given, the other two\n"
-        << "                              are supposed to be the same\n"
-        << "  [-crop <sizeX> [<sizeY>] [<sizeZ>]]: Crop this amount of pixels in each direction\n"
-        << "                            : if only one is given, the other two\n"
-        << "                              are supposed to be the same\n"
-        ;
+        Iin.read(fnImg);
+        double init_value(padValue);
+        if (padType=="avg")
+            init_value=Iin().computeAvg();
+        else if (padType=="corner")
+            init_value=DIRECT_MULTIDIM_ELEM(Iin(), 0);
+        if (mode==CROPMODE)
+        {
+            int xl=cropX/2;
+            int xr=cropX-xl;
+            int yl=cropY/2;
+            int yr=cropY-yl;
+            int zl=cropZ/2;
+            int zr=cropZ-zl;
+            if (ZSIZE(Iin())==1)
+            {
+                zl=zr=0;
+            }
+            //call to a generic 4D function;
+            Iin().window(0,  STARTINGZ(Iin())+zl, STARTINGY(Iin())+yl,  STARTINGX(Iin())+xl,
+                         0, FINISHINGZ(Iin())-zr,FINISHINGY(Iin())-yr, FINISHINGX(Iin())-xr);
+        }
+        else
+            if (!physical_coords)
+                Iin().window(0, z0, y0, x0, 0, zF, yF,xF, init_value);
+            else
+                Iin().window(0,STARTINGZ(Iin()) + z0,
+                             STARTINGY(Iin()) + y0,
+                             STARTINGX(Iin()) + x0,
+                             0,STARTINGZ(Iin()) + zF,
+                             STARTINGY(Iin()) + yF, STARTINGX(Iin()) + xF,
+                             init_value);
+        Iin.write(fnImgOut);
+    }
+
+    void processImage()
+    {
+        img.read(fnImg,false);
+
+        switch (img.dataType())
+        {
+        case Unknown_Type:
+            REPORT_ERROR(ERR_IMG_UNKNOWN,"");
+            break;
+        case UChar:
+            processImage(IUChar,fnImg,fnImgOut);
+            break;
+        case SChar:
+            processImage(IChar,fnImg,fnImgOut);
+            break;
+        case UShort:
+            processImage(IUShort,fnImg,fnImgOut);
+            break;
+        case Short:
+            processImage(IShort,fnImg,fnImgOut);
+            break;
+        default:
+            processImage(IFloat,fnImg,fnImgOut);
+            break;
+        }
     }
 };
 
-bool process_img(Image<double> &img, const Prog_parameters *prm)
-{
-
-    Window_parameters *eprm = (Window_parameters *) prm;
-    if (eprm->average_pad)
-        eprm->init_value = (img()).computeAvg();
-    else if (eprm->corner_pad)
-        eprm->init_value = DIRECT_MULTIDIM_ELEM(img(), 0);
-    if (eprm->crop_mode)
-    {
-        int xl=eprm->cropX/2;
-        int xr=eprm->cropX-xl;
-        int yl=eprm->cropY/2;
-        int yr=eprm->cropY-yl;
-        int zl=eprm->cropZ/2;
-        int zr=eprm->cropZ-zl;
-        //call to a generic 4D function;
-        img().window(0,  STARTINGZ(img())+zl, STARTINGY(img())+yl,  STARTINGX(img())+xl,
-        		     0, FINISHINGZ(img())-zr,FINISHINGY(img())-yr, FINISHINGX(img())-xr);
-    }
-    else
-        if (!eprm->physical_coords)
-        {
-            img().window(0, eprm->z0, eprm->y0, eprm->x0,
-            		     0, eprm->zF, eprm->yF,eprm->xF,
-            		     eprm->init_value);
-        }
-        else
-        {
-            img().window(0,STARTINGZ(img()) + eprm->z0,
-                         STARTINGY(img()) + eprm->y0,
-                         STARTINGX(img()) + eprm->x0,
-                         0,STARTINGZ(img()) + eprm->zF,
-                         STARTINGY(img()) + eprm->yF, STARTINGX(img()) + eprm->xF,
-                         eprm->init_value);
-        }
-    return true;
-}
-
 int main(int argc, char **argv)
 {
-    Window_parameters prm;
-    SF_main(argc, argv, &prm, (void*)&process_img);
+    ProgWindow prm;
+    prm.read(argc, argv);
+    prm.tryRun();
 }
