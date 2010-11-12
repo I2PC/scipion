4756d205677de7987f96e7a7ada88eeef80a7c70
juanjovega <juanjovega@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-11-12 10:21:31 +0000

    Updating IO plugin
    git-svn-id: file:///home/roberto/Git/svn@6484 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 24167cd4a01a255535a3c04e41f19d63e4c57f1b

---
+++ trunk/xmipp/libraries/graphicsj/Xmipp_IO/src/xmipp/io/MRC_Reader.java
@@ -0,0 +1,231 @@
+package xmipp.io;
+
+
+
+import ij.plugin.*;
+import ij.*;
+import ij.io.FileInfo;
+import ij.io.OpenDialog;
+import xmipp.io.ij.*;
+import java.io.*;
+
+/**
+ *  Reading MRC files modified the 18/10/2007 adding substraction of 32768 to
+ *  the pixels value of FEI files - FEI don't use correct version of MRC format!
+ *
+ *@author     cedric
+ *@created    04/01/2005
+ */
+public class MRC_Reader extends ImagePlus implements PlugIn {
+
+    byte[] header;
+    boolean FEI = false;
+
+    /**
+     *  Main processing method for the SpiderReader_ object
+     *
+     *@param  arg  Description of the Parameter
+     */
+    public void run(String arg) {
+        String directory = "";
+        String fileName = arg;
+        if ((arg == null) || (arg.isEmpty())) {
+            // Choose a file since none specified
+            OpenDialog od = new OpenDialog("Load MRC File...", arg);
+            fileName = od.getFileName();
+            if (fileName == null) {
+                return;
+            }
+            directory = od.getDirectory();
+        } else {
+            // we were sent a filename to open
+            File dest = new File(arg);
+            directory = dest.getParent();
+            fileName = dest.getName();
+        }
+
+        // Load in the image
+        ImagePlus imp = load(directory, fileName);
+        if (imp == null) {
+            return;
+        }
+        if (FEI) {
+            ImageStack stack = imp.getStack();
+            for (int i = 1; i <= stack.getSize(); i++) {
+                stack.getProcessor(i).add(-32768);
+            }
+            imp.getProcessor().resetMinAndMax();
+        }
+
+        // Attach the Image Processor
+        if (imp.getNSlices() > 1) {
+            setStack(fileName, imp.getStack());
+        } else {
+            setProcessor(fileName, imp.getProcessor());
+        }
+        // Copy the scale info over
+        copyScale(imp);
+
+        // Show the image if it was selected by the file
+        // chooser, don't if an argument was passed ie
+        // some other ImageJ process called the plugin
+        if (arg.equals("")) {
+            show();
+        }
+    }
+
+    /**
+     *  Description of the Method
+     *
+     *@param  directory  Description of the Parameter
+     *@param  fileName   Description of the Parameter
+     *@return            Description of the Return Value
+     */
+    public ImagePlus load(String directory, String fileName) {
+        if ((fileName == null) || (fileName.equals(""))) {
+            return null;
+        }
+
+        if (!directory.endsWith(File.separator)) {
+            directory += File.separator;
+        }
+
+        IJ.showStatus("Loading MRC File: " + directory + fileName);
+
+        // Try calling the parse routine
+        try {
+            parseMRC(directory, fileName);
+        } catch (Exception e) {
+            IJ.showStatus("parseMRC() error");
+            IJ.showMessage("DM3_Reader", "" + e);
+            return null;
+        }
+
+        // Set the file information
+        FileInfo fi = new FileInfo();
+        // Go and fetch the DM3 specific file Information
+        try {
+            fi = getMRCFileInfo(directory, fileName);
+        } // This is in case of trouble parsing the tag table
+        catch (Exception e) {
+            IJ.showStatus("");
+            IJ.showMessage("MRC_Reader", "gMRC:" + e);
+            return null;
+        }
+
+        // Open the image!
+        FileOpener fo = new FileOpener(fi);
+        return fo.open(false);
+    }
+
+    /**
+     *  Description of the Method
+     *
+     *@param  directory        Description of the Parameter
+     *@param  fileName         Description of the Parameter
+     *@exception  IOException  Description of the Exception
+     */
+    void parseMRC(String directory, String fileName) throws IOException {
+        // This reads through the MRC file, extracting useful tags
+        // which allow one to determine the data offset etc.
+
+        // alternative way to read from file - allows seeks!
+        // and therefore keeps track of position (use long getFilePointer())
+        // also has DataInput Interface allowing
+        // reading of specific types
+        RandomAccessFile f = new RandomAccessFile(directory + fileName, "r");
+
+        header = new byte[1024];
+        for (int i = 0; i < 1024; i++) {
+            header[i] = f.readByte();
+        }
+
+        // Close the input stream
+        f.close();
+    }
+
+    /**
+     *  Gets the mRCFileInfo attribute of the MRC_Reader object
+     *
+     *@param  directory        Description of the Parameter
+     *@param  fileName         Description of the Parameter
+     *@return                  The mRCFileInfo value
+     *@exception  IOException  Description of the Exception
+     */
+    FileInfo getMRCFileInfo(String directory, String fileName) throws IOException {
+        // this gets the basic file information using the contents of the tag
+        // tables created by parseMRC()
+        FileInfo fi = new FileInfo();
+        fi.fileFormat = fi.RAW;
+        fi.fileName = fileName;
+        fi.directory = directory;
+        // Originally forgot to do this - tells ImageJ what endian form the actual
+        // image data is in
+        boolean littleEndian = false;
+        int mode = readIntInHeader(3 * 4, header, littleEndian);
+        if (mode == 0) {
+            fi.fileType = FileInfo.GRAY8;
+            fi.width = readIntInHeader(0, header, littleEndian);
+            fi.height = readIntInHeader(1 * 4, header, littleEndian);
+            fi.nImages = readIntInHeader(2 * 4, header, littleEndian);
+            if (fi.width < 0 || fi.height < 0 || fi.nImages < 0) {
+                littleEndian = true;
+            }
+        } else if (mode == 1) {
+            fi.fileType = FileInfo.GRAY16_UNSIGNED;
+        } else if (mode == 2) {
+            fi.fileType = FileInfo.GRAY32_FLOAT;
+        } else {
+            //it is not a big endian data
+            littleEndian = true;
+            mode = readIntInHeader(3 * 4, header, littleEndian);
+            if (mode == 1) {
+                fi.fileType = FileInfo.GRAY16_UNSIGNED;
+            } else if (mode == 2) {
+                fi.fileType = FileInfo.GRAY32_FLOAT;
+            } else {
+                throw new IOException("Unimplemented ImageData mode=" + mode + " in MRC file.");
+            }
+        }
+        //littleEndian = true;
+        if (littleEndian) {
+            fi.intelByteOrder = littleEndian;
+        }
+        fi.width = readIntInHeader(0, header, littleEndian);
+        fi.height = readIntInHeader(1 * 4, header, littleEndian);
+        fi.nImages = readIntInHeader(2 * 4, header, littleEndian);
+        int extrabytes = readIntInHeader(23 * 4, header, littleEndian);
+        fi.offset = 1024 + extrabytes;
+        //IJ.write("" + extrabytes);
+        if (extrabytes == 131072) {
+            IJ.write("FEI");
+            //fi.fileType = FileInfo.GRAY16_SIGNED;
+            FEI = true;
+        }
+
+        IJ.write("mode=" + mode + " littleEndian=" + littleEndian);
+        IJ.write("x=" + fi.width + " y=" + fi.height + " z=" + fi.nImages);
+        IJ.write("extrabytes=" + extrabytes);
+
+        return fi;
+    }
+
+    /**
+     *  Description of the Method
+     *
+     *@param  index         Description of the Parameter
+     *@param  h             Description of the Parameter
+     *@param  littleEndian  Description of the Parameter
+     *@return               Description of the Return Value
+     */
+    private static int readIntInHeader(int index, byte[] h, boolean littleEndian) {
+        byte b1 = h[index];
+        byte b2 = h[index + 1];
+        byte b3 = h[index + 2];
+        byte b4 = h[index + 3];
+        if (littleEndian) {
+            return ((((b4 & 0xff) << 24) | ((b3 & 0xff) << 16) | ((b2 & 0xff) << 8) | (b1 & 0xff)));
+        }
+        return ((((b1 & 0xff) << 24) | ((b2 & 0xff) << 16) | ((b3 & 0xff) << 8) | (b4 & 0xff)));
+    }
+}
---
+++ trunk/xmipp/libraries/graphicsj/Xmipp_IO/src/xmipp/io/MRC_Writer.java
@@ -0,0 +1,177 @@
+package xmipp.io;
+
+
+
+import ij.plugin.filter.*;
+import ij.*;
+import ij.io.*;
+import ij.process.*;
+import java.io.*;
+
+/**
+ *  Description of the Class
+ *
+ *@author     cedric
+ *@created    04/01/2005
+ */
+public class MRC_Writer implements PlugInFilter {
+	ImagePlus img;
+	String directory;
+	String name;
+	boolean littleEndian=true;
+
+	/**
+	 *  Main processing method for the SpiderReader_ object
+	 *
+	 *@param  arg  Description of the Parameter
+	 *@param  imp  Description of the Parameter
+	 *@return      Description of the Return Value
+	 */
+	public int setup(String arg, ImagePlus imp) {
+		img = imp;
+		if(!(arg==null||arg.equals(""))){
+			File dest = new File(arg);
+			directory = dest.getParent()+ System.getProperty("file.separator");
+			name = dest.getName();
+			System.out.println("saving as mrc "+directory+name);
+		}else{
+			name=null;
+		}
+		return DOES_32 + DOES_16 + DOES_8G + NO_CHANGES;
+	}
+
+
+	/**
+	 *  Main processing method for the SpiderWriter_ object
+	 *
+	 *@param  ip  Description of the Parameter
+	 */
+	public void run(ImageProcessor ip) {
+		if(name==null){
+			SaveDialog sd = new SaveDialog("save as...", img.getTitle(), ".mrc");
+			directory=sd.getDirectory();
+			name=sd.getFileName();
+		}
+		if (name == null) {
+			return;
+		}
+		IJ.showStatus("saving: " + directory + name);
+		//ImageStack stack = myimp.getStack();
+		//save(stack, directory + name);
+		//SPIDER.saveImage(directory, name, myimp, true);
+
+
+		byte[] header = createHeader();
+		FileInfo fi = img.getFileInfo();
+		fi.fileFormat = fi.RAW;
+		System.out.println("saving...intel order=" + littleEndian);
+		fi.intelByteOrder = littleEndian;
+		fi.fileName = name;
+		fi.directory = directory;
+		ImageWriter file = new ImageWriter(fi);
+		try {
+			DataOutputStream out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(directory +System.getProperty("file.separator")+ name)));
+			for (byte aHeader : header) {
+				out.writeByte(aHeader);
+			}
+			file.write(out);
+			out.close();
+		} catch (IOException ioe) {
+			IJ.error("" + ioe);
+		}
+	}
+
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@return    Description of the Return Value
+	 */
+	private byte[] createHeader() {
+		ImageProcessor ip = img.getProcessor();
+		int mode = 0;
+		if (ip instanceof ByteProcessor) {
+			mode = 0;
+		} else if (ip instanceof ShortProcessor) {
+			mode = 1;
+		} else if (ip instanceof FloatProcessor) {
+			mode = 2;
+		}
+
+		double sum = 0;
+		int nbPix = 0;
+		float min = Float.NaN;
+		float max = Float.NaN;
+		float value;
+		int mini = 0;
+		int maxi = 0;
+		int avgi = 0;
+		for (int z = 0; z < img.getNSlices(); z++) {
+			ip = img.getImageStack().getProcessor(z + 1);
+			for (int y = 0; y < img.getHeight(); y++) {
+				for (int x = 0; x < img.getWidth(); x++) {
+					value = ip.getPixelValue(x, y);
+					sum += value;
+					nbPix++;
+					if (Float.isNaN(min)) {
+						min = value;
+					}
+					if (Float.isNaN(max)) {
+						max = value;
+					}
+					if (max < value) {
+						max = value;
+					} else if (min > value) {
+						min = value;
+					}
+				}
+			}
+		}
+		float avg = (float) (sum / nbPix);
+		maxi = Float.floatToRawIntBits(max);
+		mini = Float.floatToRawIntBits(min);
+		avgi = Float.floatToRawIntBits(avg);
+
+		byte[] header = new byte[1024];
+		writeIntInHeader(img.getWidth(), 0 * 4, header, littleEndian);
+		writeIntInHeader(img.getHeight(), 1 * 4, header, littleEndian);
+		writeIntInHeader(img.getNSlices(), 2 * 4, header, littleEndian);
+		writeIntInHeader(mode, 3 * 4, header, littleEndian);
+		writeIntInHeader(1, 16 * 4, header, littleEndian);
+		writeIntInHeader(2, 17 * 4, header, littleEndian);
+		writeIntInHeader(3, 18 * 4, header, littleEndian);
+		writeIntInHeader(mini, 19 * 4, header, littleEndian);
+		writeIntInHeader(maxi, 20 * 4, header, littleEndian);
+		writeIntInHeader(avgi, 21 * 4, header, littleEndian);
+		return header;
+	}
+
+
+	/**
+	 *  Description of the Method
+	 *
+	 *@param  value         Description of the Parameter
+	 *@param  index         Description of the Parameter
+	 *@param  h             Description of the Parameter
+	 *@param  littleEndian  Description of the Parameter
+	 */
+	private static void writeIntInHeader(int value, int index, byte[] h, boolean littleEndian) {
+		byte b1 = (byte) (value >> 24);
+		byte b2 = (byte) ((value << 8) >> 24);
+		byte b3 = (byte) ((value << 16) >> 24);
+		byte b4 = (byte) ((value << 24) >> 24);
+		if (littleEndian) {
+			h[index] = b4;
+			h[index + 1] = b3;
+			h[index + 2] = b2;
+			h[index + 3] = b1;
+		} else {
+			h[index] = b1;
+			h[index + 1] = b2;
+			h[index + 2] = b3;
+			h[index + 3] = b4;
+		}
+	}
+
+}
