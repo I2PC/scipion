fe3d7b32f814fb143318ec60f9efc80cf896ce5c
aerey <aerey@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-11-17 17:41:06 +0000

    Converted to XmippProgram  class (and new parameter format)
    git-svn-id: file:///home/roberto/Git/svn@6544 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: aa2b53db7800c781f25ecaf58bf561adfb49908e

--- trunk/xmipp/applications/programs/resolution_fsc/resolution_fsc_main.cpp
+++ trunk/xmipp/applications/programs/resolution_fsc/resolution_fsc_main.cpp
@@ -30,9 +30,9 @@
 #include <data/metadata_extension.h>
 
 
-//class ProgResolutionFsc : public XmippProgram
-//{
-//public:
+class ProgResolutionFsc : public XmippProgram
+{
+public:
 
     void writeFiles(const FileName &fnRoot,
                     const MultidimArray<double> &freq,
@@ -69,113 +69,122 @@
         MD.write(fn_frc);
     }
 
-class Resolution_parameters: public Prog_parameters
-    {
-    public:
-        FileName    fn_ref, fn_root;
-        Image<double>  refI;
-        float       sam;
-        float       max_sam;
-        bool        do_dpr;
-    public:
-        void read(int argc, char **argv)
-        {
-            Prog_parameters::read(argc, argv);
-            fn_ref = getParameter(argc, argv, "-ref");
-            sam = textToFloat(getParameter(argc, argv, "-sam"));
-            refI.read(fn_ref, true, -1, apply_geo);
-            refI().setXmippOrigin();
-        }
-        void show()
-        {
-            Prog_parameters::show();
-            std::cout << "Reference file  = " << fn_ref  << std::endl;
-            std::cout << "Sampling rate   = " << sam     << std::endl;
-            std::cout << "Max Resolution  = " << max_sam << std::endl;
-            if(do_dpr)
-                std::cout << "do_dpr is ON  = " << std::endl;
-        }
-        void usage()
-        {
-            std::cerr << " EITHER:\n";
-            std::cerr << "   -ref <input file>        : Filename for reference image/volume \n";
-            std::cerr << "   -i <input file>          : either an image/volume or a selection file\n";
-            std::cerr << " OR:\n";
-            std::cerr << "   -set_of_images <selfile> : SelFile containing a set of 2D-images\n";
-            std::cerr << " For both modes:\n";
-            std::cerr << "   -sam <sampling rate>     : i.e. pixel size (in Angstrom) \n";
-            std::cerr << "  [-dont_apply_geo]         : for 2D-images: do not apply transformation stored in the header\n";
-            std::cerr << "  [-do_dpr]                 : compute dpr, by default only frc is computed\n";
-            std::cerr << "  [-max_sam=-1]             : set fsc to 0 for frequencies above this one (Ang), -1-> all fequencies\n";
-        }
-    };
+    FileName    fn_ref, fn_root,fn_img;
+    float       sam;
+    float       max_sam;
+    bool        do_dpr, do_set_of_images;
 
-    bool process_img(Image<double> &img, const Prog_parameters *prm)
+    FileName    fn_sel;
+    bool        apply_geo;
+
+    void readParams()
     {
-        Resolution_parameters *eprm = (Resolution_parameters *) prm;
+        sam = getDoubleParam("--sam");
+
+        apply_geo = !checkParam("--dont_apply_geo");
+
+
+        max_sam = getDoubleParam("--max_sam");
+        do_dpr = checkParam("--do_dpr");
+        do_set_of_images = checkParam("--set_of_images");
+        if(do_set_of_images)
+        {
+            fn_sel = getParam("--set_of_images");
+            if (checkParam("-i") || checkParam("--ref"))
+                REPORT_ERROR(ERR_ARG_INCORRECT, "--set_of_images should not be provided with -i or --ref");
+        }
+        else
+        {
+            fn_ref = getParam("--ref");
+            fn_img = getParam("-i");
+        }
+    }
+
+    void defineParams()
+    {
+
+        apply_geo = true;
+
+        addUsageLine("Calculate the resolution of one or more volumes or images with respect to a single reference.");
+        addUsageLine("Example of use: Resolution of subset2.vol volume with respect to subset1.vol reference volume using 5.6 pixel size (in Angstrom)");
+        addUsageLine("   xmipp_resolution_fsc --ref subset1.vol  -i subset2.vol --sam 5.6 ");
+        addUsageLine("Example of use: Resolution of a set of images using 5.6 pixel size (in Angstrom)");
+        addUsageLine("   xmipp_resolution_fsc --set_of_images selfile.sel --sam 5.6 ");
+
+        addParamsLine("   -i <input_file>           : either an image/volume or a selection file");
+        addParamsLine("   requires --ref;");
+        addParamsLine("or --set_of_images <selfile> : SelFile containing a set of 2D-images");
+        addParamsLine("   [--ref <input_file>]        : Filename for reference image/volume");
+        addParamsLine("   --sam <sampling_rate>     : i.e. pixel size (in Angstrom)");
+        addParamsLine("   [--dont_apply_geo]        : for 2D-images: do not apply transformation stored in the header");
+        addParamsLine("   [--do_dpr]                : compute dpr, by default only frc is computed");
+        addParamsLine("   [--max_sam <max_sr=-1>]   : set fsc to 0 for frequencies above this one (Ang), -1-> all fequencies");
+    }
+
+
+    bool process_img()
+    {
+        Image<double>  refI,img;
+        refI.read(fn_ref, true, -1, apply_geo);
+        refI().setXmippOrigin();
+        img.read(fn_img);
+        img().setXmippOrigin();
         MultidimArray<double> freq, frc, dpr, frc_noise, error_l2;
-        frc_dpr(eprm->refI(), img(), eprm->sam, freq, frc, frc_noise, dpr, error_l2);
-        writeFiles(img.name(), freq, frc, frc_noise, dpr, error_l2, eprm->max_sam,eprm->do_dpr);
+        frc_dpr(refI(), img(), sam, freq, frc, frc_noise, dpr, error_l2);
+        writeFiles(img.name(), freq, frc, frc_noise, dpr, error_l2, max_sam, do_dpr);
         return true;
     }
 
-//};
+    bool process_sel()
+    {
+        MetaData     SFaux,SF, SF1, SF2;
+        std::vector<MetaData> vMD;
+        vMD.push_back(SF1);
+        vMD.push_back(SF2);
+
+        Image<double>       I1, I2, Id;
+        double      dummy;
+        MultidimArray<double> freq, frc, dpr, frc_noise, ssnr, error_l2, pixel;
+
+        SFaux.read(fn_sel);
+        SF.randomize(SFaux);
+        SF.split(2,vMD,MDL_IMAGE);
+        getStatistics(SF1,I1,Id,dummy,dummy,true);
+        getStatistics(SF2,I2,Id,dummy,dummy,true);
+        I1().setXmippOrigin();
+        I2().setXmippOrigin();
+        frc_dpr(I1(), I2(), sam, freq, frc, frc_noise, dpr,error_l2,do_dpr);
+        writeFiles(fn_sel, freq, frc, frc_noise, dpr,error_l2,max_sam,do_dpr);
+    }
+
+    void run()
+    {
+
+        if (!do_set_of_images)
+        {
+            process_img();
+        }
+        else
+        {
+            process_sel();
+        }
+    }
+
+};
 
 int main(int argc, char **argv)
 {
-    float max_sam = textToFloat(getParameter(argc, argv, "-max_sam","-1"));
-    bool do_dpr=checkParameter(argc, argv, "-do_dpr");
-    if (!checkParameter(argc, argv, "-set_of_images"))
+    try
     {
-        Resolution_parameters prm;
-        prm.each_image_produces_an_output = false;
-        prm.apply_geo = true;
-        prm.do_dpr=do_dpr;
-        prm.max_sam=max_sam;
-        SF_main(argc, argv, &prm, (void*)&process_img);
+        ProgResolutionFsc program;
+        program.read(argc, argv);
+        program.run();
     }
-    else
+    catch (XmippError xe)
     {
-        FileName    fn_sel;
-        float       sam;
-        bool        apply_geo;
-        try
-        {
-            fn_sel = getParameter(argc, argv, "-set_of_images");
-            sam = textToFloat(getParameter(argc, argv, "-sam"));
-            apply_geo = !checkParameter(argc, argv, "-dont_apply_geo");
-        }
-        catch (XmippError XE)
-        {
-            std::cout << XE;
-            Resolution_parameters prm;
-            prm.usage();
-        }
-
-        try
-        {
-            MetaData     SFaux,SF, SF1, SF2;
-            std::vector<MetaData> vMD;
-            vMD.push_back(SF1);
-            vMD.push_back(SF2);
-            Image<double>       I1, I2, Id;
-            double      dummy;
-            MultidimArray<double> freq, frc, dpr, frc_noise, ssnr, error_l2,
-            pixel;
-
-            SFaux.read(fn_sel);
-            SF.randomize(SFaux);
-            SF.split(2,vMD,MDL_IMAGE);
-            getStatistics(SF1,I1,Id,dummy,dummy,true);
-            getStatistics(SF2,I2,Id,dummy,dummy,true);
-            I1().setXmippOrigin();
-            I2().setXmippOrigin();
-            frc_dpr(I1(), I2(), sam, freq, frc, frc_noise, dpr,error_l2,do_dpr);
-            writeFiles(fn_sel, freq, frc, frc_noise, dpr,error_l2,max_sam,do_dpr);
-        }
-        catch (XmippError XE)
-        {
-            std::cout << XE;
-        }
+        std::cerr << xe;
+        return 1;
     }
+    return 0;
+
 }
