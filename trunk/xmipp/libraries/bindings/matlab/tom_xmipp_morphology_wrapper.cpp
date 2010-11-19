6f525ec1ec35abc53cb31c8de0c84e346b4e3a11
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-11-19 08:49:53 +0000

    Directory matlab_bindings moved into libraries
    git-svn-id: file:///home/roberto/Git/svn@6559 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: adec19b20e90daf0c7811df8a68dc59cf4996c21

---
+++ trunk/xmipp/libraries/bindings/matlab/tom_xmipp_morphology_wrapper.cpp
@@ -0,0 +1,94 @@
+/*=================================================================
+ *
+ * tom_xmipp_morphology is a wrapper to xmipp_morphology
+ *
+ * The calling syntax is:
+ *
+ *		im_out = tom_xmipp_morphology_wrapper(image,operation,size,count,neig)
+ *
+ * Electron Tomography toolbox of the
+ * Max-Planck-Institute for Biochemistry
+ * Dept. Molecular Structural Biology
+ * 82152 Martinsried, Germany
+ * http://www.biochem.mpg.de
+ *
+ * and
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ *
+ * created: 30/08/2007
+ * by: Andreas Korinek
+ *
+ *=================================================================*/
+
+/*xmipp includes */
+#include "image.h"
+#include "volume.h"
+#include "morphology.h"
+#include "tom_xmipp_helpers.h"
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray*prhs[])
+{
+    #define DILATION 1
+    #define EROSION  2
+    #define OPENING  3
+    #define CLOSING  4
+     
+    int operation = (int) mxGetScalar(prhs[1]);
+    int size = (int) mxGetScalar(prhs[2]);
+    int count = (int) mxGetScalar(prhs[3]);
+    int neig = (int) mxGetScalar(prhs[4]);
+    
+    mwSize ndims = mxGetNumberOfDimensions(prhs[0]);
+    
+    if (ndims == 2)
+    {
+        Image img, img_out;
+        getMatrix2D(prhs[0],img());
+        img_out() = img();
+
+        switch (operation)
+        {
+        case DILATION:
+            dilate2D(img(),img_out(),neig,count,size);
+            break;
+        case EROSION:
+            erode2D(img(),img_out(),neig,count,size);
+            break;
+        case OPENING:
+            opening2D(img(),img_out(),neig,count,size);
+            break;
+        case CLOSING:
+            closing2D(img(),img_out(),neig,count,size);
+            break;
+        }
+        
+        setMatrix2D(img_out(),plhs[0]);     
+    }
+    else
+    {
+        Volume vol, vol_out;
+        getMatrix3D(prhs[0],vol());
+        vol_out = vol();
+        
+        switch (operation)
+        {
+        case DILATION:
+            dilate3D(vol(),vol_out(),neig,count,size);
+            break;
+        case EROSION:
+            erode3D(vol(),vol_out(),neig,count,size);
+            break;
+        case OPENING:
+            opening3D(vol(),vol_out(),neig,count,size);
+            break;
+        case CLOSING:
+            closing3D(vol(),vol_out(),neig,count,size);
+            break;
+        }
+        
+        setMatrix3D(vol_out(),plhs[0]);     
+    }
+    
+}
