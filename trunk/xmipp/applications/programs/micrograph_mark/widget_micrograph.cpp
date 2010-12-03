bbf97f149b68c5586560291bef6531ae9780bb60
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2010-12-03 19:55:20 +0000

    Removal of debugging information
    git-svn-id: file:///home/roberto/Git/svn@6731 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 77cb4879b870567f9eb9d4352e4f325ca72774c9

--- trunk/xmipp/applications/programs/micrograph_mark/widget_micrograph.cpp
+++ trunk/xmipp/applications/programs/micrograph_mark/widget_micrograph.cpp
@@ -2644,12 +2644,8 @@ void AutoParticlePicking::loadModels(const FileName &fn)
 void AutoParticlePicking::saveAutoParticles()
 {
     if (__autoselection_done && __auto_label!=-1)
-    {
-        std::cout << "Writing at " << __outputRoot+
-        "."+__modelRootName.removeDirectories()+".pos" << std::endl;
         __m->write_coordinates(__auto_label, __minCost, __outputRoot+
                                "."+__modelRootName.removeDirectories()+".pos");
-    }
 }
 
 /* Save models ------------------------------------------------------------- */
