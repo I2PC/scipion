8dc493b71e4ede3e8026bbef28d6c7e51a7acdbe
joaquinoton <joaquinoton@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-01-12 11:21:44 +0000

    authors update
    git-svn-id: file:///home/roberto/Git/svn@6961 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: f72a360928615d22f1078a8a16b8491f0c3a9bf3

--- trunk/xmipp/applications/programs/convert_image/convert_image_main.cpp
+++ trunk/xmipp/applications/programs/convert_image/convert_image_main.cpp
@@ -1,6 +1,7 @@
 /***************************************************************************
  *
- * Authors:    Carlos Oscar            coss@cnb.csic.es (2007)
+ * Authors:    Carlos Oscar            coss@cnb.csic.es  (2007)
+ *             Joaquin Oton            joton@cnb.csic.es (2010)
  *
  * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
  *
