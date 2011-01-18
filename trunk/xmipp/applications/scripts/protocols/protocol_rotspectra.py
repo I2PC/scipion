56759dfa91fd2b983dd9ce54a0ea7d368f5bcc5d
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-01-18 07:35:24 +0000

    Preconditions, status and HowCenter added
    git-svn-id: file:///home/roberto/Git/svn@6991 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 87388a3d0010533bb72e1c79ce7a98c933a21eb2

--- trunk/xmipp/applications/scripts/protocols/protocol_rotspectra.py
+++ trunk/xmipp/applications/scripts/protocols/protocol_rotspectra.py
@@ -32,15 +32,15 @@ LogDir='Logs'
 #-----------------------------------------------------------------------------
 # {section} Rotational spectra calculation
 #-----------------------------------------------------------------------------
-# *** Falta poner diferentes formas de calcular el centro 
-
+# {list}|Use the middle of the image|Minimize first harmonic| How to find the center of rotation?
+HowCenter='Use the middle of the image'
 # Inner radius for rotational harmonics calculation:
 """ These values are in pixels from the image center
     See http://xmipp.cnb.uam.es/twiki/bin/view/Xmipp/Makespectra
 """
 SpectraInnerRadius=7
 # Outer radius for rotational harmonics calculation:
-SpectraOuterRadius=10
+SpectraOuterRadius=32
 # {expert} Lowest harmonic to calculate
 SpectraLowHarmonic=1
 # {expert} Highest harmonic to calculate
@@ -76,16 +76,51 @@ AnalysisScript='visualize_rotspectra.py'
 #-----------------------------------------------------------------------------
 # {end-of-header} do not change anything bellow this line unless you know what you are doing
 #-----------------------------------------------------------------------------
-import os,sys
+import os,shutil,sys,time
 scriptdir=os.path.split(os.path.dirname(os.popen('which xmipp_protocols','r').read()))[0]+'/protocols'
 sys.path.append(scriptdir) # add default search path
 
+def stepPerformed(step,filename):
+    import re
+    f = open(filename, 'r')
+    lines=f.readlines()
+    f.close()
+    expr = re.compile(step)
+    return len(filter(expr.search,lines))>0
+
 class rotational_spectra_class:
+   def saveAndCompareParameters(self, listOfParameters):
+        fnOut=self.WorkingDir + "/protocolParameters.txt"
+        linesNew=[];
+        for prm in listOfParameters:
+            eval("linesNew.append('"+prm +"='+str("+prm+")+'\\n')")
+        if os.path.exists(fnOut):
+            f = open(fnOut, 'r')
+            linesOld=f.readlines()
+            f.close()
+            same=True;
+            if len(linesOld)==len(linesNew):
+                for i in range(len(linesNew)):
+                    if not linesNew[i]==linesOld[i]:
+                        same=False
+                        break;
+            else:
+                same=False
+            if not same:
+                print("Deleting")
+                self.mylog.info("Deleting working directory since it is run with different parameters")
+                shutil.rmtree(self.WorkingDir)
+                os.makedirs(self.WorkingDir)
+        f = open(fnOut, 'w')
+        f.writelines(linesNew)
+        f.close()
+
    #init variables
    def __init__(self,
                 SelFileName,
                 WorkingDir,
                 ProjectDir,
+                HowCenter,
                 SpectraInnerRadius,
                 SpectraOuterRadius,
                 SpectraLowHarmonic,
@@ -101,6 +136,7 @@ class rotational_spectra_class:
        self.SelFileName=SelFileName
        self.WorkingDir=WorkingDir
        self.ProjectDir=ProjectDir
+       self.HowCenter=HowCenter
        self.SpectraInnerRadius=SpectraInnerRadius
        self.SpectraOuterRadius=SpectraOuterRadius
        self.SpectraLowHarmonic=SpectraLowHarmonic
@@ -120,14 +156,38 @@ class rotational_spectra_class:
        if not os.path.exists(self.WorkingDir):
            os.makedirs(self.WorkingDir)
 
+       # Save parameters and compare to possible previous runs
+       self.saveAndCompareParameters([
+                 "SelFileName",
+                 "HowCenter",
+                 "SpectraInnerRadius",
+                 "SpectraOuterRadius",
+                 "SpectraLowHarmonic",
+                 "SpectraHighHarmonic",
+                 "SomXdim",
+                 "SomYdim",
+                 "SomReg0",
+                 "SomReg1",
+                 "SomSteps",
+                 "KerdensomExtraCommand"]);
+
        #made backup of this script
        log.make_backup_of_script_file(sys.argv[0],self.WorkingDir)
+
+       # Update status
+       fh=open(self.WorkingDir + "/status.txt", "a")
+       fh.write("Step 0: Process started at " + time.asctime() + "\n")
+       fh.close()
 
        # Effectively run
        self.execute_findcenter()
        self.execute_spectra()
        self.execute_KerDenSOM()
   
+       fh=open(self.WorkingDir + "/status.txt", "a")
+       fh.write("Step F: Process finished at " + time.asctime() + "\n")
+       fh.close()
+       
    #------------------------------------------------------------------------
    #execute_findcenter
    #------------------------------------------------------------------------
@@ -141,19 +201,36 @@ class rotational_spectra_class:
           R=xmipp.SingleImgSize(MD.getValue(xmipp.MDL_IMAGE))[1]/2
       else:
           R=xmipp.SingleImgSize(self.SelFileName)[1]/2.0
-      command='xmipp_find_center2d'+ \
-              ' -i ' + self.SelFileName + \
-              ' --oroot '+self.WorkingDir+"/center2d" + \
-              ' --r1 ' + str(100.0*self.SpectraInnerRadius/R) + \
-              ' --r2 ' + str(100.0*self.SpectraOuterRadius/R) + \
-              ' --r3 ' + str(100.0*(self.SpectraOuterRadius+2)/R) + \
-              ' --r4 ' + str(100.0*(self.SpectraOuterRadius+5)/R)
-      print '* ',command
-      self.mylog.info(command)
-      os.system(command)
-      MD=xmipp.MetaData(self.WorkingDir+"/center2d_center.xmd")
-      self.xOffset=MD.getValue(xmipp.MDL_X)
-      self.yOffset=MD.getValue(xmipp.MDL_Y)
+      if self.HowCenter=='Minimize first harmonic':
+          command='xmipp_find_center2d'+ \
+                  ' -i ' + self.SelFileName + \
+                  ' --oroot '+self.WorkingDir+"/center2d" + \
+                  ' --r1 ' + str(100.0*self.SpectraInnerRadius/R) + \
+                  ' --r2 ' + str(100.0*self.SpectraOuterRadius/R) + \
+                  ' --r3 ' + str(100.0*(self.SpectraOuterRadius+2)/R) + \
+                  ' --r4 ' + str(100.0*(self.SpectraOuterRadius+5)/R)
+          print '* ',command
+          self.mylog.info(command)
+          os.system(command)
+          if os.path.exists(self.WorkingDir+"/center2d_center.xmd"):
+              MD=xmipp.MetaData(self.WorkingDir+"/center2d_center.xmd")
+              self.xOffset=MD.getValue(xmipp.MDL_X)
+              self.yOffset=MD.getValue(xmipp.MDL_Y)
+              fh=open(self.WorkingDir + "/status.txt", "a")
+              fh.write("Step 1: Center found at " + time.asctime() + "\n")
+              fh.close()
+      elif self.HowCenter=='Use the middle of the image':
+          if fn.isMetaData():
+              MD=xmipp.MetaData(fn)
+              fnAux=MD.getValue(xmipp.MDL_IMAGE)
+              dimensions=xmipp.SingleImgSize(fnAux)
+          else:
+              dimensions=xmipp.SingleImgSize(fn)
+          self.xOffset=dimensions[0]/2.0
+          self.yOffset=dimensions[1]/2.0
+          fh=open(self.WorkingDir + "/status.txt", "a")
+          fh.write("Step 1: Center defined at " + time.asctime() + "\n")
+          fh.close()
 
    #------------------------------------------------------------------------
    #execute_spectra
@@ -161,6 +238,8 @@ class rotational_spectra_class:
    def execute_spectra(self):
       import os, string
       import launch_job
+      if not stepPerformed("Step 1",self.WorkingDir + "/status.txt"):
+         return
       print '*********************************************************************'
       print '* Computing rotational power spectra'
       command=' -i ' + self.SelFileName + \
@@ -175,6 +254,10 @@ class rotational_spectra_class:
                             command,
                             self.mylog,
                             False,1,1,'')
+      if os.path.exists(self.WorkingDir+"/rotSpectra.txt"):
+          fh=open(self.WorkingDir + "/status.txt", "a")
+          fh.write("Step 2: Spectra created at " + time.asctime() + "\n")
+          fh.close()
   
    #------------------------------------------------------------------------
    #execute_KerDenSOM
@@ -182,6 +265,8 @@ class rotational_spectra_class:
    def execute_KerDenSOM(self):
       import launch_job
 
+      if not stepPerformed("Step 2",self.WorkingDir + "/status.txt"):
+         return
       print '*********************************************************************'
       print '* Computing kerdensom ...'
       command=' -v 1 -i '  + self.WorkingDir+"/rotSpectra.txt" + \
@@ -198,6 +283,31 @@ class rotational_spectra_class:
                             False,1,1,'')
       os.system("rm -f "+self.WorkingDir+"/som_*")
 
+# Preconditions
+def preconditions(gui):
+    retval=True
+    # Check if there is workingdir
+    if WorkingDir == "":
+        message="No working directory given"
+        if gui:
+            import tkMessageBox
+            tkMessageBox.showerror("Error", message)
+        else:
+            print message
+        retval=False
+    
+    # Check that there are any micrograph to process
+    if not os.path.exists(SelFileName):
+        message="The input selfile is not valid"
+        if gui:
+            import tkMessageBox
+            tkMessageBox.showerror("Error", message)
+        else:
+            print message
+        retval=False
+    
+    return retval
+
 #
 # main
 #     
@@ -205,6 +315,7 @@ if __name__ == '__main__':
    myspectra=rotational_spectra_class(SelFileName,
                                       WorkingDir,
                                       ProjectDir,
+                                      HowCenter,
                                       SpectraInnerRadius,
                                       SpectraOuterRadius,
                                       SpectraLowHarmonic,
