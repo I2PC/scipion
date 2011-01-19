83af3f5c31f2284c7c6f5fb092e69007288288a1
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-01-19 09:59:18 +0000

    Program creation
    git-svn-id: file:///home/roberto/Git/svn@7004 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 6233b31fc40cc11c4c43016a1e20da8e615d2561

---
+++ trunk/xmipp/applications/programs/mrc_create_metadata/mrc_create_metadata_main.cpp
@@ -0,0 +1,105 @@
+/***************************************************************************
+ *
+ * Authors:     Carlos Oscar S. Sorzano (coss@cnb.csic.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.csic.es'                                  
+ ***************************************************************************/
+#include <data/args.h>
+#include <interface/spider.h>
+#include <data/program.h>
+
+class ProgMrcCreateMetaData: public XmippProgram
+{
+public:
+    FileName fnStack, fnAngles, fnOut;
+
+    void defineParams()
+    {
+    	addUsageLine("Produces a selfile valid for tomography with an MRC stack and a list of angles");
+        addParamsLine(" --stack <stack>       : Stack file (not necessarily MRC)");
+        addParamsLine(" --tiltAngles <rawtlt> : Tilt angle file");
+        addParamsLine(" -o <metadata>         : Output metadata");
+    }
+
+    void readParams()
+    {
+    	fnStack = getParam("--stack");
+    	fnAngles = getParam("--tiltAngles");
+    	fnOut = getParam("-o");
+    }
+
+    void show()
+    {
+        std::cout
+        << "Stack   = " << fnStack  << std::endl
+        << "Angles  = " << fnAngles << std::endl
+        << "Output  = " << fnOut << std::endl
+        ;
+    }
+
+    void run()
+    {
+    	std::string extension=fnStack.getExtension();
+    	if (extension=="mrc")
+    		fnStack+=":mrcs";
+    	else if (extension=="ali")
+    		fnStack+=":mrcs";
+
+    	std::ifstream fhAngles;
+    	fhAngles.open(fnAngles.c_str());
+    	if (!fhAngles)
+    		REPORT_ERROR(ERR_IO_NOTOPEN,fnAngles);
+
+    	Image<double> stack;
+    	stack.read(fnStack,false);
+    	int xdim, ydim, zdim;
+    	unsigned long ndim;
+    	stack.getDimensions(xdim, ydim, zdim, ndim);
+
+    	MetaData MD;
+    	FileName fnSlice;
+    	for (unsigned long slice=0; slice<ndim; slice++)
+    	{
+    		double angle;
+    		fhAngles >> angle;
+    		fnSlice.compose((int)slice,fnStack);
+    		MD.addObject();
+    		MD.setValue(MDL_IMAGE,fnSlice);
+    		MD.setValue(MDL_ANGLETILT,angle);
+    	}
+    	MD.write(fnOut);
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    try
+    {
+    	ProgMrcCreateMetaData program;
+        program.read(argc, argv);
+        program.run();
+    }
+    catch(XmippError xe)
+    {
+        std::cerr << xe;
+    }
+    return 0;
+}
