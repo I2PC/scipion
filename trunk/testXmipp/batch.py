e030696ab0ece7bd4398347da5c611a112eff255
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-01-27 12:41:04 +0000

    Batch has been rewritten in Python
    git-svn-id: file:///home/roberto/Git/svn@7199 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 8e5ca4aa8050a59bd5a143c705021bdfb782eb0b

---
+++ trunk/testXmipp/batch.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+class Tester:
+    def __init__(self,fnDir):
+        self.fnDir=fnDir
+
+    def testProgram(self,program,arguments):
+        print "Testing "+program
+        os.makedirs(self.fnDir+"/"+program)
+        os.system(program+" "+arguments+\
+            " > "+self.fnDir+"/"+program+"/stdout.txt 2>"+\
+            self.fnDir+"/"+program+"/stderr.txt")
+
+#		
+# Main
+#     
+import os,sys
+if __name__ == '__main__':
+    if not sys.argv[1:] or len(sys.argv)<=1:
+        print "Usage: ./batch.py <directory>"
+        sys.exit()
+    args = sys.argv[1:]
+    fnDir=args[0]
+    
+    # Remove the output directory if it is not goldStandard
+    if fnDir!='goldStandard' and fnDir!='goldStandard/':
+        if os.path.exists(fnDir):
+            os.system("rm -rf "+fnDir)
+        os.makedirs(fnDir)
+
+    # Create tester
+    tester=Tester(fnDir)
+
+    # Test the programs
+    program="xmipp_xray_psf_create"
+    tester.testProgram(program,
+        "-i input/xray_psf.xmd -o "+fnDir+"/"+program+"/psf.vol")
