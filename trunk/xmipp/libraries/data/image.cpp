61273a3a7b0948a35cc49a44dc805f88d5a8e39b
joaquinoton <joaquinoton@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-02-23 14:16:28 +0000

    fixed error querying datatype
    git-svn-id: file:///home/roberto/Git/svn@7465 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 13a064dab7926bbff606771cc1361bde73ab1820

--- trunk/xmipp/libraries/data/image.cpp
+++ trunk/xmipp/libraries/data/image.cpp
@@ -153,5 +153,5 @@ void SingleImgSize(const FileName &filename, int &Xdim, int &Ydim, int &Zdim, si
     Image<char> img;
     img.read(filename, HEADER);
     img.getDimensions(Xdim, Ydim, Zdim, Ndim);
-    img.MDMainHeader.getValue(MDL_DATATYPE,(int)datatype);
+    datatype = img.dataType();
 }
