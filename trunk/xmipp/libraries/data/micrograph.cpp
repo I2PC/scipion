4aaa0edfda4e7f383d23c90946d5f79e894986e7
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-03-03 19:16:47 +0000

    -1 is not longer a valid image
    git-svn-id: file:///home/roberto/Git/svn@7628 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: bd62d5f1306904c0e7b8216dba74eb21ac543bf6

--- trunk/xmipp/libraries/data/micrograph.cpp
+++ trunk/xmipp/libraries/data/micrograph.cpp
@@ -115,32 +115,32 @@ void Micrograph::open_micrograph(const FileName &_fn_micrograph)
     {
     case UChar:
         IUChar = new(Image<unsigned char>);
-        result=IUChar->read(fn_micrograph,DATA, -1, true);
+        result=IUChar->read(fn_micrograph,DATA, ALL_IMAGES, true);
         pixelDesvFilter(IUChar->data, stdevFilter);
         break;
     case UShort:
         IUShort = new(Image<unsigned short>);
-        result=IUShort->read(fn_micrograph,DATA, -1, true);
+        result=IUShort->read(fn_micrograph,DATA, ALL_IMAGES, true);
         pixelDesvFilter(IUShort->data, stdevFilter);
         break;
     case Short:
         IShort = new(Image< short>);
-        result=IShort->read(fn_micrograph,DATA, -1, true);
+        result=IShort->read(fn_micrograph,DATA, ALL_IMAGES, true);
         pixelDesvFilter(IShort->data, stdevFilter);
         break;
     case Int:
         IInt = new(Image< int>);
-        result=IInt->read(fn_micrograph,DATA, -1, true);
+        result=IInt->read(fn_micrograph,DATA, ALL_IMAGES, true);
         pixelDesvFilter(IInt->data, stdevFilter);
         break;
     case UInt:
         IUInt = new(Image< unsigned int>);
-        result=IUInt->read(fn_micrograph,DATA, -1, true);
+        result=IUInt->read(fn_micrograph,DATA, ALL_IMAGES, true);
         pixelDesvFilter(IUChar->data, stdevFilter);
         break;
     case Float:
         IFloat = new(Image<float>);
-        result=IFloat->read(fn_micrograph,DATA, -1, true);
+        result=IFloat->read(fn_micrograph,DATA, ALL_IMAGES, true);
         pixelDesvFilter(IFloat->data, stdevFilter);
         break;
     default:
@@ -533,27 +533,27 @@ void Micrograph::write(const FileName &fileName, bool adjust)
 {
     if (datatype == UChar)
     {
-        IUChar->write(fileName, -1, false, WRITE_OVERWRITE, adjust);
+        IUChar->write(fileName, ALL_IMAGES, false, WRITE_OVERWRITE, adjust);
     }
     else if (datatype == UShort)
     {
-        IUShort->write(fileName, -1, false, WRITE_OVERWRITE, adjust);
+        IUShort->write(fileName, ALL_IMAGES, false, WRITE_OVERWRITE, adjust);
     }
     else if (datatype == Short)
     {
-        IShort->write(fileName, -1, false, WRITE_OVERWRITE, adjust);
+        IShort->write(fileName, ALL_IMAGES, false, WRITE_OVERWRITE, adjust);
     }
     else if (datatype == UInt)
     {
-        IUInt->write(fileName, -1, false, WRITE_OVERWRITE, adjust);
+        IUInt->write(fileName, ALL_IMAGES, false, WRITE_OVERWRITE, adjust);
     }
     else if (datatype == Int)
     {
-        IInt->write(fileName, -1, false, WRITE_OVERWRITE, adjust);
+        IInt->write(fileName, ALL_IMAGES, false, WRITE_OVERWRITE, adjust);
     }
     else if (datatype == Float)
     {
-        IFloat->write(fileName, -1, false, WRITE_OVERWRITE, adjust);
+        IFloat->write(fileName, ALL_IMAGES, false, WRITE_OVERWRITE, adjust);
     }
     else
         REPORT_ERROR(ERR_TYPE_INCORRECT, "Micrograph::set_val::(): unknown datatype");
