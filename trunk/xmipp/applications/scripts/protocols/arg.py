705bff3b27b57d93a4a74ec71e1a0c3c7cf971d6
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-03-15 23:31:45 +0000

    modify indentation
    git-svn-id: file:///home/roberto/Git/svn@7774 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 52aa227241965f2cd74635ce9c32a24fc610551d

--- trunk/xmipp/applications/scripts/protocols/arg.py
+++ trunk/xmipp/applications/scripts/protocols/arg.py
@@ -2,31 +2,31 @@
 #---------------------------------------------------------------------------
 # getComponentFromVector
 #---------------------------------------------------------------------------
-def getComponentFromVector(__vector,_iteration):
-   #print "getComponentFromVector",__vector,_iteration
-   _vector=__vector.strip()
-   listValues=getListFromVector(_vector)
-   if _iteration<0: _iteration=0
-   if _iteration<len(listValues): return listValues[_iteration]
-   else:                          return listValues[len(listValues)-1]
+def getComponentFromVector(__vector, _iteration):
+    #print "getComponentFromVector",__vector,_iteration
+    _vector = __vector.strip()
+    listValues = getListFromVector(_vector)
+    if _iteration < 0: _iteration = 0
+    if _iteration < len(listValues): return listValues[_iteration]
+    else:                          return listValues[len(listValues) - 1]
 
 
 #---------------------------------------------------------------------------
 # getListFromVector
 #---------------------------------------------------------------------------
 def getListFromVector(_vector):
-   import string
-   intervalos=string.split(_vector)
-   if len(intervalos)==0:
-      raise RuntimeError,"Empty vector"
-   listValues=[]
-   for i in range(len(intervalos)):
-      intervalo=intervalos[i]
-      listaIntervalo=string.split(intervalo,'x')
-      if len(listaIntervalo)==1:
-	 listValues+=listaIntervalo
-      elif len(listaIntervalo)==2:
-         listValues+=[ listaIntervalo[1] ] * string.atoi(listaIntervalo[0])
-      else:
-         raise RuntimeError,"Unknown syntax: "+intervalos
-   return listValues
+    import string
+    intervalos = string.split(_vector)
+    if len(intervalos) == 0:
+        raise RuntimeError, "Empty vector"
+    listValues = []
+    for i in range(len(intervalos)):
+        intervalo = intervalos[i]
+        listaIntervalo = string.split(intervalo, 'x')
+        if len(listaIntervalo) == 1:
+            listValues += listaIntervalo
+        elif len(listaIntervalo) == 2:
+            listValues += [ listaIntervalo[1] ] * string.atoi(listaIntervalo[0])
+        else:
+            raise RuntimeError, "Unknown syntax: " + intervalos
+    return listValues
