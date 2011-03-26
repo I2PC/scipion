5efafcf3c4dfca70e90f629aba7ee36374a8c9a3
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-03-26 01:41:54 +0000

    formating
    git-svn-id: file:///home/roberto/Git/svn@7882 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: ba4521e475e34926d43c0ef72be2de8ed5933b5e

--- trunk/xmipp/applications/scripts/protocols/launch_job.py
+++ trunk/xmipp/applications/scripts/protocols/launch_job.py
@@ -7,23 +7,34 @@ def launch_job(programname,
                NumberOfThreads,
                SystemFlavour,
                onlyBuildCommand=False):
-    import os,sys
-    command=buildCommand(programname,
+    import os, sys
+    command = buildCommand(programname,
                params,
                DoParallel,
                NumberOfMpiProcesses,
                NumberOfThreads,
                SystemFlavour)
-    if command=='':
-            message= "Error: unrecognized SystemFlavour: ", SystemFlavour
-            print '* ',message
+    if command == '':
+            message = "Error: unrecognized SystemFlavour: ", SystemFlavour
+            print '* ', message
             print '*********************************************************************'
             log.info(message)
             sys.exit(1)
-    print '* ',command,'\n'
+    print '* ', command, '\n'
     log.info(command)
-    if (not onlyBuildCommand): os.system(command)
-    return(command);
+    from subprocess import call
+
+    retcode = 0
+    if (not onlyBuildCommand):
+        try:
+            retcode = call(command, shell=True)
+            if retcode > 0:
+                print >> sys.stderr, "Warning:", command, ' was terminated by signal', retcode
+        except OSError, e:
+            print >> sys.stderr, "Execution failed:", e
+            exit(1)
+
+    return(command, retcode);
 
 def buildCommand(programname,
                params,
@@ -31,39 +42,39 @@ def buildCommand(programname,
                NumberOfMpiProcesses,
                NumberOfThreads,
                SystemFlavour):
-    import os,sys
+    import os, sys
 
     if not DoParallel:
         command = programname + ' ' + params
 
     else:
-        mpiprogramname=programname.replace('xmipp','xmipp_mpi')
+        mpiprogramname = programname.replace('xmipp', 'xmipp_mpi')
 
-        if (SystemFlavour=='SLURM-MPICH'): # like BSCs MareNostrum, LaPalma etc
+        if (SystemFlavour == 'SLURM-MPICH'): # like BSCs MareNostrum, LaPalma etc
             mpicommand = 'srun '
 
-        elif (SystemFlavour=='TORQUE-OPENMPI'): # like our crunchy
+        elif (SystemFlavour == 'TORQUE-OPENMPI'): # like our crunchy
             if (int(NumberOfThreads) > 1):
-                mpicommand = 'mpirun -mca mpi_yield_when_idle 1 --bynode -np ' + str(NumberOfMpiProcesses) 
+                mpicommand = 'mpirun -mca mpi_yield_when_idle 1 --bynode -np ' + str(NumberOfMpiProcesses)
             else:
-                mpicommand = 'mpirun -mca mpi_yield_when_idle 1 -np ' + str(NumberOfMpiProcesses) 
+                mpicommand = 'mpirun -mca mpi_yield_when_idle 1 -np ' + str(NumberOfMpiProcesses)
 
-        elif (SystemFlavour=='SGE-OPENMPI'): # like cluster at imp.ac.at (no variable nr_cpus yet...)
-            mpicommand = 'mpiexec -n ' + str(NumberOfMpiProcesses) 
+        elif (SystemFlavour == 'SGE-OPENMPI'): # like cluster at imp.ac.at (no variable nr_cpus yet...)
+            mpicommand = 'mpiexec -n ' + str(NumberOfMpiProcesses)
 
-        elif (SystemFlavour=='PBS'): # like in Vermeer and FinisTerrae
+        elif (SystemFlavour == 'PBS'): # like in Vermeer and FinisTerrae
             mpicommand = 'mpirun -np ' + str(NumberOfMpiProcesses) + ' -hostfile ' + os.environ.get('PBS_NODEFILE')
 
-        elif (SystemFlavour=='XMIPP_MACHINEFILE'): # environment variable $XMIPP_MACHINEFILE points to machinefile
+        elif (SystemFlavour == 'XMIPP_MACHINEFILE'): # environment variable $XMIPP_MACHINEFILE points to machinefile
             mpicommand = 'mpirun -np ' + str(NumberOfMpiProcesses) + ' -machinefile ' + os.environ.get('XMIPP_MACHINEFILE')
 
-        elif (SystemFlavour=='HOME_MACHINEFILE'): # machinefile is called $HOME/machines.dat
+        elif (SystemFlavour == 'HOME_MACHINEFILE'): # machinefile is called $HOME/machines.dat
             mpicommand = 'mpirun -np ' + str(NumberOfMpiProcesses) + ' -machinefile ' + os.environ.get('HOME') + '/machinefile.dat'
 
-        elif (SystemFlavour==''):
+        elif (SystemFlavour == ''):
             mpicommand = 'mpirun -mca mpi_yield_when_idle 1 -np ' + str(NumberOfMpiProcesses)
 
         else:
             return ''
-        command = mpicommand + ' `which '+ str(mpiprogramname) +'` ' + params
+        command = mpicommand + ' `which ' + str(mpiprogramname) + '` ' + params
     return(command);
