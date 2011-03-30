da371ffa50740777fbb64b2a9de8bc74b6da1257
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-03-30 09:45:55 +0000

    Speed-up of median filters
    git-svn-id: file:///home/roberto/Git/svn@7941 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: dc981528eb51a3326e4daafded07bd288c090f0a

--- trunk/xmipp/libraries/data/filters.h
+++ trunk/xmipp/libraries/data/filters.h
@@ -770,185 +770,185 @@ void mergeSort(MultidimArray< T >& v1, MultidimArray< T >& v2, MultidimArray< T 
 template <typename T>
 void fast_merge_sort(MultidimArray< T >& x, MultidimArray< T >& y, MultidimArray< T >& v)
 {
-    if (x(0) < y(0))
+    if (DIRECT_MULTIDIM_ELEM(x,0) < DIRECT_MULTIDIM_ELEM(y,0))
     {
-        v(0) = x(0);
-        if (x(1) < y(0))
+        DIRECT_MULTIDIM_ELEM(v,0) = DIRECT_MULTIDIM_ELEM(x,0);
+        if (DIRECT_MULTIDIM_ELEM(x,1) < DIRECT_MULTIDIM_ELEM(y,0))
         {
-            v(1) = x(1);
-            if (x(2) < y(0))
+            DIRECT_MULTIDIM_ELEM(v,1) = DIRECT_MULTIDIM_ELEM(x,1);
+            if (DIRECT_MULTIDIM_ELEM(x,2) < DIRECT_MULTIDIM_ELEM(y,0))
             {
-                v(2) = x(2);
-                v(3) = y(0);
-                v(4) = y(1);
-                v(5) = y(2);
+                DIRECT_MULTIDIM_ELEM(v,2) = DIRECT_MULTIDIM_ELEM(x,2);
+                DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(y,0);
+                DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(y,1);
+                DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(y,2);
             }
             else
             {
-                v(2) = y(0);
-                if (x(2) < y(1))
+                DIRECT_MULTIDIM_ELEM(v,2) = DIRECT_MULTIDIM_ELEM(y,0);
+                if (DIRECT_MULTIDIM_ELEM(x,2) < DIRECT_MULTIDIM_ELEM(y,1))
                 {
-                    v(3) = x(2);
-                    v(4) = y(1);
-                    v(5) = y(2);
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(x,2);
+                    DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(y,1);
+                    DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(y,2);
                 }
                 else
                 {
-                    v(3) = y(1);
-                    if (x(2) < y(2))
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(y,1);
+                    if (DIRECT_MULTIDIM_ELEM(x,2) < DIRECT_MULTIDIM_ELEM(y,2))
                     {
-                        v(4) = x(2);
-                        v(5) = y(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(x,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(y,2);
                     }
                     else
                     {
-                        v(4) = y(2);
-                        v(5) = x(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(y,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(x,2);
                     }
                 }
             }
         }
         else
         {
-            v(1) = y(0);
-            if (x(1) < y(1))
+            DIRECT_MULTIDIM_ELEM(v,1) = DIRECT_MULTIDIM_ELEM(y,0);
+            if (DIRECT_MULTIDIM_ELEM(x,1) < DIRECT_MULTIDIM_ELEM(y,1))
             {
-                v(2) = x(1);
-                if (x(2) < y(1))
+                DIRECT_MULTIDIM_ELEM(v,2) = DIRECT_MULTIDIM_ELEM(x,1);
+                if (DIRECT_MULTIDIM_ELEM(x,2) < DIRECT_MULTIDIM_ELEM(y,1))
                 {
-                    v(3) = x(2);
-                    v(4) = y(1);
-                    v(5) = y(2);
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(x,2);
+                    DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(y,1);
+                    DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(y,2);
                 }
                 else
                 {
-                    v(3) = y(1);
-                    if (x(2) < y(2))
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(y,1);
+                    if (DIRECT_MULTIDIM_ELEM(x,2) < DIRECT_MULTIDIM_ELEM(y,2))
                     {
-                        v(4) = x(2);
-                        v(5) = y(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(x,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(y,2);
                     }
                     else
                     {
-                        v(4) = y(2);
-                        v(5) = x(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(y,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(x,2);
                     }
                 }
             }
             else
             {
-                v(2) = y(1);
-                if (x(1) < y(2))
+                DIRECT_MULTIDIM_ELEM(v,2) = DIRECT_MULTIDIM_ELEM(y,1);
+                if (DIRECT_MULTIDIM_ELEM(x,1) < DIRECT_MULTIDIM_ELEM(y,2))
                 {
-                    v(3) = x(1);
-                    if (x(2) < y(2))
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(x,1);
+                    if (DIRECT_MULTIDIM_ELEM(x,2) < DIRECT_MULTIDIM_ELEM(y,2))
                     {
-                        v(4) = x(2);
-                        v(5) = y(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(x,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(y,2);
                     }
                     else
                     {
-                        v(4) = y(2);
-                        v(5) = x(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(y,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(x,2);
                     }
                 }
                 else
                 {
-                    v(3) = y(2);
-                    v(4) = x(1);
-                    v(5) = x(2);
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(y,2);
+                    DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(x,1);
+                    DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(x,2);
                 }
             }
         }
     }
     else
     {
-        v(0) = y(0);
-        if (x(0) < y(1))
+        DIRECT_MULTIDIM_ELEM(v,0) = DIRECT_MULTIDIM_ELEM(y,0);
+        if (DIRECT_MULTIDIM_ELEM(x,0) < DIRECT_MULTIDIM_ELEM(y,1))
         {
-            v(1) = x(0);
-            if (x(1) < y(1))
+            DIRECT_MULTIDIM_ELEM(v,1) = DIRECT_MULTIDIM_ELEM(x,0);
+            if (DIRECT_MULTIDIM_ELEM(x,1) < DIRECT_MULTIDIM_ELEM(y,1))
             {
-                v(2) = x(1);
-                if (x(2) < y(1))
+                DIRECT_MULTIDIM_ELEM(v,2) = DIRECT_MULTIDIM_ELEM(x,1);
+                if (DIRECT_MULTIDIM_ELEM(x,2) < DIRECT_MULTIDIM_ELEM(y,1))
                 {
-                    v(3) = x(2);
-                    v(4) = y(1);
-                    v(5) = y(2);
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(x,2);
+                    DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(y,1);
+                    DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(y,2);
                 }
                 else
                 {
-                    v(3) = y(1);
-                    if (x(2) < y(2))
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(y,1);
+                    if (DIRECT_MULTIDIM_ELEM(x,2) < DIRECT_MULTIDIM_ELEM(y,2))
                     {
-                        v(4) = x(2);
-                        v(5) = y(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(x,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(y,2);
                     }
                     else
                     {
-                        v(4) = y(2);
-                        v(5) = x(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(y,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(x,2);
                     }
                 }
             }
             else
             {
-                v(2) = y(1);
-                if (x(1) < y(2))
+                DIRECT_MULTIDIM_ELEM(v,2) = DIRECT_MULTIDIM_ELEM(y,1);
+                if (DIRECT_MULTIDIM_ELEM(x,1) < DIRECT_MULTIDIM_ELEM(y,2))
                 {
-                    v(3) = x(1);
-                    if (x(2) < y(2))
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(x,1);
+                    if (DIRECT_MULTIDIM_ELEM(x,2) < DIRECT_MULTIDIM_ELEM(y,2))
                     {
-                        v(4) = x(2);
-                        v(5) = y(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(x,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(y,2);
                     }
                     else
                     {
-                        v(4) = y(2);
-                        v(5) = x(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(y,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(x,2);
                     }
                 }
                 else
                 {
-                    v(3) = y(2);
-                    v(4) = x(1);
-                    v(5) = x(2);
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(y,2);
+                    DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(x,1);
+                    DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(x,2);
                 }
             }
         }
         else
         {
-            v(1) = y(1);
-            if (x(0) < y(2))
+            DIRECT_MULTIDIM_ELEM(v,1) = DIRECT_MULTIDIM_ELEM(y,1);
+            if (DIRECT_MULTIDIM_ELEM(x,0) < DIRECT_MULTIDIM_ELEM(y,2))
             {
-                v(2) = x(0);
-                if (x(1) < y(2))
+                DIRECT_MULTIDIM_ELEM(v,2) = DIRECT_MULTIDIM_ELEM(x,0);
+                if (DIRECT_MULTIDIM_ELEM(x,1) < DIRECT_MULTIDIM_ELEM(y,2))
                 {
-                    v(3) = x(1);
-                    if (x(2) < y(2))
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(x,1);
+                    if (DIRECT_MULTIDIM_ELEM(x,2) < DIRECT_MULTIDIM_ELEM(y,2))
                     {
-                        v(4) = x(2);
-                        v(5) = y(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(x,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(y,2);
                     }
                     else
                     {
-                        v(4) = y(2);
-                        v(5) = x(2);
+                        DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(y,2);
+                        DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(x,2);
                     }
                 }
                 else
                 {
-                    v(3) = y(2);
-                    v(4) = x(1);
-                    v(5) = x(2);
+                    DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(y,2);
+                    DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(x,1);
+                    DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(x,2);
                 }
             }
             else
             {
-                v(2) = y(2);
-                v(3) = x(0);
-                v(4) = x(1);
-                v(5) = x(2);
+                DIRECT_MULTIDIM_ELEM(v,2) = DIRECT_MULTIDIM_ELEM(y,2);
+                DIRECT_MULTIDIM_ELEM(v,3) = DIRECT_MULTIDIM_ELEM(x,0);
+                DIRECT_MULTIDIM_ELEM(v,4) = DIRECT_MULTIDIM_ELEM(x,1);
+                DIRECT_MULTIDIM_ELEM(v,5) = DIRECT_MULTIDIM_ELEM(x,2);
             }
         }
     }
@@ -958,31 +958,31 @@ void fast_merge_sort(MultidimArray< T >& x, MultidimArray< T >& y, MultidimArray
 template <typename T>
 void median(MultidimArray< T >& x, MultidimArray< T >& y, T& m)
 {
-    if (x(0) < y(1))
-        if (x(1) < y(1))
-            if (x(2) < y(1))
-                m = y(1);
+    if (DIRECT_MULTIDIM_ELEM(x,0) < DIRECT_MULTIDIM_ELEM(y,1))
+        if (DIRECT_MULTIDIM_ELEM(x,1) < DIRECT_MULTIDIM_ELEM(y,1))
+            if (DIRECT_MULTIDIM_ELEM(x,2) < DIRECT_MULTIDIM_ELEM(y,1))
+                m = DIRECT_MULTIDIM_ELEM(y,1);
             else
-                m = x(2);
+                m = DIRECT_MULTIDIM_ELEM(x,2);
         else
-            if (x(1) < y(2))
-                m = y(2);
+            if (DIRECT_MULTIDIM_ELEM(x,1) < DIRECT_MULTIDIM_ELEM(y,2))
+                m = DIRECT_MULTIDIM_ELEM(y,2);
             else
-                m = x(1);
+                m = DIRECT_MULTIDIM_ELEM(x,1);
     else
-        if (x(0) < y(2))
-            if (x(1) < y(2))
-                m = y(2);
+        if (DIRECT_MULTIDIM_ELEM(x,0) < DIRECT_MULTIDIM_ELEM(y,2))
+            if (DIRECT_MULTIDIM_ELEM(x,1) < DIRECT_MULTIDIM_ELEM(y,2))
+                m = DIRECT_MULTIDIM_ELEM(y,2);
             else
-                m = x(1);
+                m = DIRECT_MULTIDIM_ELEM(x,1);
         else
-            if (x(0) < y(3))
-                m = y(3);
+            if (DIRECT_MULTIDIM_ELEM(x,0) < DIRECT_MULTIDIM_ELEM(y,3))
+                m = DIRECT_MULTIDIM_ELEM(y,3);
             else
-                if (x(0) < y(4))
-                    m = x(0);
+                if (DIRECT_MULTIDIM_ELEM(x,0) < DIRECT_MULTIDIM_ELEM(y,4))
+                    m = DIRECT_MULTIDIM_ELEM(x,0);
                 else
-                    m = y(4);
+                    m = DIRECT_MULTIDIM_ELEM(y,4);
 }
 
 /** Median_filter with a 3x3 selfWindow
@@ -999,7 +999,7 @@ void median_filter3x3(MultidimArray< T >&m, MultidimArray< T >& out)
     MultidimArray< T > v(6);
 
     // Set the output matrix size
-    out.resize(m);
+    out.initZeros(m);
 
     // Set the initial and final matrix indices to explore
     int initialY = 1, initialX = 1;
@@ -1039,7 +1039,7 @@ void median_filter3x3(MultidimArray< T >&m, MultidimArray< T >& out)
                 sort(DIRECT_A2D_ELEM(m, i - 1, j + 1), DIRECT_A2D_ELEM(m, i, j + 1),
                      DIRECT_A2D_ELEM(m, i + 1, j + 1), v3);
                 fast_merge_sort(v2, v3, v);
-                median(v1, v, out(i, j));
+                median(v1, v, DIRECT_A2D_ELEM(out,i, j));
             }
             else
             {
@@ -1053,10 +1053,10 @@ void median_filter3x3(MultidimArray< T >&m, MultidimArray< T >& out)
                 fast_merge_sort(v2, v3, v);
 
                 // Find the first median and assign it to the output
-                median(v1, v, out(i, j));
+                median(v1, v, DIRECT_A2D_ELEM(out, i, j));
 
                 // Find the second median and assign it to the output
-                median(v4, v, out(i, j + 1));
+                median(v4, v, DIRECT_A2D_ELEM(out, i, j + 1));
             }
         }
     }
