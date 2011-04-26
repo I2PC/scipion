056f913ebce1a91266153658cfe53bef9f2d4058
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-04-26 09:54:33 +0000

    xmipp_volume_from_pdb added
    git-svn-id: file:///home/roberto/Git/svn@8198 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 8a640d2c212e38f0a5870c5468cfa772e1d36e45

--- trunk/testXmipp/batch.py
+++ trunk/testXmipp/batch.py
@@ -235,6 +235,9 @@ class Tester:
         self.addTest("-i input/singleImage.spi -o %o/image.xmp --crop -10")
         self.addTest("-i input/xray_import/Images/img48949.spe -o %o/image.xmp --size 512")
 
+        self.addProgram("xmipp_volume_from_pdb")
+        self.addTest("-i input/1o7d.pdb -o %o/1o7d")
+
         self.addProgram("xmipp_volume_reslice")
         self.addTest("-i input/phantomCandida.vol -o %o/resliced.vol --face top")
 
