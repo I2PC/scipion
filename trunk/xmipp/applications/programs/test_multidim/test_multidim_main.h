f4bfc975f50779b8b4018d22231c8fbbc264e637
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-02 21:18:50 +0000

    added gtest
    git-svn-id: file:///home/roberto/Git/svn@8228 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 8d650ff1cdefa828f8a68973b571d5b6cb1b5a1e

---
+++ trunk/xmipp/applications/programs/test_multidim/test_multidim_main.h
@@ -0,0 +1,26 @@
+#include <data/multidim_array.h>
+// MORE INFO HERE: http://code.google.com/p/googletest/wiki/AdvancedGuide
+// This test is named "Size", and belongs to the "MetadataTest"
+// test case.
+TEST( MultidimTest, Size)
+{
+    MultidimArray<int> md;
+    md.resize(2,3);
+    int x,y,z;
+    size_t n;
+    md.getDimensions(x,y,z,n);
+    EXPECT_EQ(1, n) << "MultidimArray: wrong n size";
+    EXPECT_EQ(1, z) << "MultidimArray: wrong y size";
+    EXPECT_EQ(2, y) << "MultidimArray: wrong z size";
+    EXPECT_EQ(3, x) << "MultidimArray: wrong x size";
+}
+
+TEST( MultidimTest, Copy)
+{
+    MultidimArray<int> mdtarget,mdsource;
+    mdsource.resize(2,3);
+    DIRECT_MULTIDIM_ELEM(mdsource,0)=1;
+    mdtarget=mdsource;
+    //DIRECT_MULTIDIM_ELEM(mdsource,0)=11;
+    EXPECT_EQ(mdtarget,mdsource) << "MultidimArray: copy operator failed";
+}
