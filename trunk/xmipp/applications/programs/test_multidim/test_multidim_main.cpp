77a32439e7970dee167e76b22e6eb3a5966d4ec1
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-03 11:48:26 +0000

    git-svn-id: file:///home/roberto/Git/svn@8237 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    Former-commit-id: 525d24f879f138ddbc8f0010b8267ec83e84f43d

--- trunk/xmipp/applications/programs/test_multidim/test_multidim_main.cpp
+++ trunk/xmipp/applications/programs/test_multidim/test_multidim_main.cpp
@@ -1,4 +1,6 @@
 #include <data/multidim_array.h>
+#include <iostream>
+#include "../../../external/gtest-1.6.0/fused-src/gtest/gtest.h"
 // MORE INFO HERE: http://code.google.com/p/googletest/wiki/AdvancedGuide
 // This test is named "Size", and belongs to the "MetadataTest"
 // test case.
@@ -21,6 +23,13 @@ TEST( MultidimTest, Copy)
     mdsource.resize(2,3);
     DIRECT_MULTIDIM_ELEM(mdsource,0)=1;
     mdtarget=mdsource;
-    //DIRECT_MULTIDIM_ELEM(mdsource,0)=11;
+    DIRECT_MULTIDIM_ELEM(mdsource,0)=1;
     EXPECT_EQ(mdtarget,mdsource) << "MultidimArray: copy operator failed";
+}
+
+GTEST_API_ int main(int argc, char **argv) {
+  std::cout << "Running main() from gtest_main.cc\n";
+
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
