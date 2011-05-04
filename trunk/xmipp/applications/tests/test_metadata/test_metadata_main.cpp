d52628bba793f676c04320a4d10f51e60112d41d
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-04 21:52:30 +0000

    adding test
    git-svn-id: file:///home/roberto/Git/svn@8273 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 346c706505cee7dfa9f50b5f8baaa2a099d18aa8

--- trunk/xmipp/applications/tests/test_metadata/test_metadata_main.cpp
+++ trunk/xmipp/applications/tests/test_metadata/test_metadata_main.cpp
@@ -2,40 +2,105 @@
 #include <iostream>
 #include "../../../external/gtest-1.6.0/fused-src/gtest/gtest.h"
 
-// This test is named "Size", and belongs to the "MetadataTest"
-// test case.
- TEST( MetadataTest, Size)
+/*
+ * Define a "Fixure so we may reuse the metadatas
+ */
+class MetadataTest : public ::testing::Test
 {
-    MetaData md;
-    md.addObject();
-    EXPECT_EQ(1, md.size()) << "Metadata size provides wrong size";
-}
+protected:
+    //init metadatas
+    virtual void SetUp()
+    {
+    	//Md1
+        id = mDsource.addObject();
+        mDsource.setValue(MDL_X,1.,id);
+        mDsource.setValue(MDL_Y,2.,id);
+        id = mDsource.addObject();
+        mDsource.setValue(MDL_X,3.,id);
+        mDsource.setValue(MDL_Y,4.,id);
+        //Md2
+        id = mDanotherSource.addObject();
+        mDanotherSource.setValue(MDL_X,11.,id);
+        mDanotherSource.setValue(MDL_Y,22.,id);
+        id = mDanotherSource.addObject();
+        mDanotherSource.setValue(MDL_X,33.,id);
+        mDanotherSource.setValue(MDL_Y,44.,id);
 
- /*
-  * These assertions can work with a user-defined type, but only if you
-  * define the corresponding comparison operator (e.g. ==, <, etc).
-  *  If the corresponding operator is defined, prefer using the ASSERT_*()
-  *  macros because they will print out not only the result of the c
-  *  omparison, but the two operands as well.
-  */
- TEST( MetadataTest, Copy)
+        //Md UnionAll
+        mDunion = mDsource;
+        id1 = mDunion.addObject();
+        mDunion.setValue(MDL_X,11.,id1);
+        mDunion.setValue(MDL_Y,22.,id1);
+        id2 = mDunion.addObject();
+        mDunion.setValue(MDL_X,33.,id2);
+        mDunion.setValue(MDL_Y,44.,id2);
+    }
+
+    // virtual void TearDown() {}//Destructor
+
+    MetaData mDsource,mDtarget,mDanotherSource;
+    MetaData mDunion, auxMetadata;
+    size_t id, id1,id2;
+};
+
+
+TEST_F( MetadataTest, Copy)
 {
-    MetaData mDsource,mDtarget;
-    size_t id1 = mDsource.addObject();
-    mDsource.setValue(MDL_X,1.,id1);
-    mDsource.setValue(MDL_Y,2.,id1);
-    size_t id2 = mDsource.addObject();
-    mDsource.setValue(MDL_X,1.,id2);
-    mDsource.setValue(MDL_Y,2.,id2);
     mDtarget=mDsource;
-    ///////////////////////////////////DELETE
-    //id1 = mDsource.addObject();
     EXPECT_EQ(mDsource,mDtarget);
 }
 
-GTEST_API_ int main(int argc, char **argv) {
-  std::cout << "Running main() from gtest_main.cc\n";
+TEST_F( MetadataTest, Size)
+{
+    EXPECT_EQ(2, mDsource.size());
+}
 
-  testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+TEST_F( MetadataTest, Clear)
+{
+    auxMetadata = mDsource;
+    EXPECT_EQ(2,auxMetadata.size());
+    auxMetadata.clear();
+    EXPECT_EQ(0,auxMetadata.size());
+}
+
+TEST_F( MetadataTest, importObjects)
+{
+	//FIXME importObjects is overloaded, only one case is tested
+    auxMetadata = mDsource;
+    auxMetadata.importObject(mDunion,id1,false);
+    auxMetadata.importObject(mDunion,id2,false);
+    EXPECT_EQ(auxMetadata,mDunion);
+}
+
+/*
+ * Operations in sets
+ */
+TEST_F( MetadataTest, union)
+{
+	//FIXME union all is missing
+    auxMetadata = mDsource;
+    auxMetadata.unionAll(mDanotherSource);
+    EXPECT_EQ(auxMetadata,mDunion);
+}
+
+TEST_F( MetadataTest, intersect)
+{
+    auxMetadata = mDunion;
+    auxMetadata.intersection(mDsource,MDL_X);
+    EXPECT_EQ(auxMetadata,mDsource);
+}
+
+TEST_F( MetadataTest, substraction)
+{
+    auxMetadata = mDunion;
+    auxMetadata.subtraction(mDanotherSource,MDL_X);
+    EXPECT_EQ(auxMetadata,mDsource);
+}
+
+GTEST_API_ int main(int argc, char **argv)
+{
+    std::cout << "Running main() from gtest_main.cc\n";
+
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
