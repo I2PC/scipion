fd4ef4b0a2a6fb4490d49eaafb7637b009ee3b6b
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-05 21:27:53 +0000

    trivial read/write test
    git-svn-id: file:///home/roberto/Git/svn@8308 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 67e8ddf1a96fd1da7f4dc29a8c4ab370812dbc2b

--- trunk/xmipp/applications/tests/test_metadata/test_metadata_main.cpp
+++ trunk/xmipp/applications/tests/test_metadata/test_metadata_main.cpp
@@ -1,7 +1,7 @@
 #include <data/metadata_extension.h>
 #include <iostream>
 #include "../../../external/gtest-1.6.0/fused-src/gtest/gtest.h"
-
+#include <stdlib.h>
 /*
  * Define a "Fixure so we may reuse the metadatas
  */
@@ -11,14 +11,14 @@ protected:
     //init metadatas
     virtual void SetUp()
     {
-    	//Md1
+        //Md1
         id = mDsource.addObject();
         mDsource.setValue(MDL_X,1.,id);
         mDsource.setValue(MDL_Y,2.,id);
         id = mDsource.addObject();
         mDsource.setValue(MDL_X,3.,id);
         mDsource.setValue(MDL_Y,4.,id);
-    	//Mdjoin
+        //Mdjoin
         id = mDjoin.addObject();
         mDjoin.setValue(MDL_X,1.,id);
         mDjoin.setValue(MDL_Z,222.,id);
@@ -52,8 +52,8 @@ protected:
 
 TEST_F( MetadataTest, similarToOperator)
 {
-    EXPECT_TRUE(mDsource==mDsource);
-    EXPECT_FALSE(mDsource==mDanotherSource);
+    ASSERT_TRUE(mDsource==mDsource);
+    ASSERT_FALSE(mDsource==mDanotherSource);
 }
 /** SORT FOR ROUTINE ALPHABETIC ORDER
  *
@@ -62,7 +62,7 @@ TEST_F( MetadataTest, similarToOperator)
 
 TEST_F( MetadataTest, Addlabel)
 {
-	MetaData auxMetadata = mDunion;
+    MetaData auxMetadata = mDunion;
     auxMetadata.addLabel(MDL_Z);
     std::vector<MDLabel> v1,v2;
     v1.push_back(MDL_X);
@@ -74,7 +74,7 @@ TEST_F( MetadataTest, Addlabel)
 
 TEST_F( MetadataTest, Clear)
 {
-	MetaData auxMetadata = mDsource;
+    MetaData auxMetadata = mDsource;
     EXPECT_EQ(2,auxMetadata.size());
     auxMetadata.clear();
     EXPECT_EQ(0,auxMetadata.size());
@@ -82,15 +82,15 @@ TEST_F( MetadataTest, Clear)
 
 TEST_F( MetadataTest, Copy)
 {
-	MetaData auxMetadata = mDsource;
+    MetaData auxMetadata = mDsource;
     EXPECT_EQ(mDsource,auxMetadata);
 }
 
 
 TEST_F( MetadataTest, ImportObject)
 {
-	//FIXME importObjects test is in the test named select
-	MetaData auxMetadata = mDsource;
+    //FIXME importObjects test is in the test named select
+    MetaData auxMetadata = mDsource;
     auxMetadata.importObject(mDunion,id1,false);
     auxMetadata.importObject(mDunion,id2,false);
     EXPECT_EQ(auxMetadata,mDunion);
@@ -98,40 +98,40 @@ TEST_F( MetadataTest, ImportObject)
 
 TEST_F( MetadataTest, InnerJoin)
 {
-	MetaData auxMetadata;
-	MetaData auxMetadata2 = mDsource;
+    MetaData auxMetadata;
+    MetaData auxMetadata2 = mDsource;
     auxMetadata2.setValue(MDL_Z,222.,auxMetadata2.firstObject());
     auxMetadata2.setValue(MDL_Z,444.,auxMetadata2.firstObject()+1);//A little bit irregular
-	auxMetadata.join(mDsource,mDjoin,MDL_X);
+    auxMetadata.join(mDsource,mDjoin,MDL_X);
     EXPECT_EQ(auxMetadata,auxMetadata2)<< mDjoin;//print mDjoin if error
 }
 
 TEST_F( MetadataTest, Intersect)
 {
-	MetaData auxMetadata = mDunion;
+    MetaData auxMetadata = mDunion;
     auxMetadata.intersection(mDsource,MDL_X);
     EXPECT_EQ(auxMetadata,mDsource);
 }
 
 TEST_F( MetadataTest, Merge)
 {
-	//FIXME is columns not in the same order equal to operator does not return OK
-	//should not be like this
-	MetaData auxMetadata3, auxMetadata,auxMetadata2;
+    //FIXME is columns not in the same order equal to operator does not return OK
+    //should not be like this
+    MetaData auxMetadata3, auxMetadata,auxMetadata2;
     id = auxMetadata3.addObject();
     auxMetadata3.setValue(MDL_Z,222.,id);
     id = auxMetadata3.addObject();
     auxMetadata3.setValue(MDL_Z,444.,id);
-	auxMetadata.join(mDsource,mDjoin,MDL_X);
+    auxMetadata.join(mDsource,mDjoin,MDL_X);
     auxMetadata2 = mDsource;
-	auxMetadata2.merge(auxMetadata3);
+    auxMetadata2.merge(auxMetadata3);
     EXPECT_EQ(auxMetadata,auxMetadata2);
 }
 
 TEST_F( MetadataTest, NaturalJoin)
 {
-	MetaData auxMetadata;
-	MetaData auxMetadata3;
+    MetaData auxMetadata;
+    MetaData auxMetadata3;
     id = auxMetadata3.addObject();
     auxMetadata3.setValue(MDL_X,1.,id);
     auxMetadata3.setValue(MDL_Y,2.,id);
@@ -145,21 +145,21 @@ TEST_F( MetadataTest, NaturalJoin)
     auxMetadata3.setValue(MDL_Y,6.,id);
     auxMetadata3.setValue(MDL_Z,444.,id);
 
-	auxMetadata.join(mDsource,auxMetadata3,MDL_X,NATURAL);
-	auxMetadata3.removeObject(id);
+    auxMetadata.join(mDsource,auxMetadata3,MDL_X,NATURAL);
+    auxMetadata3.removeObject(id);
     EXPECT_EQ(auxMetadata,auxMetadata3)<< auxMetadata3;//print mDjoin if error
 }
 
 TEST_F( MetadataTest, Operate)
 {
-	MetaData auxMetadata = mDunion;
-	MetaData auxMetadata2 = mDunion;
+    MetaData auxMetadata = mDunion;
+    MetaData auxMetadata2 = mDunion;
     auxMetadata.operate((String)"X=2*X");
     double x;
     FOR_ALL_OBJECTS_IN_METADATA(auxMetadata2)
     {
-    	auxMetadata2.getValue(MDL_X,x,__iter.objId);
-    	auxMetadata2.setValue(MDL_X,x*2,__iter.objId);
+        auxMetadata2.getValue(MDL_X,x,__iter.objId);
+        auxMetadata2.setValue(MDL_X,x*2,__iter.objId);
     }
 
     EXPECT_EQ(auxMetadata,auxMetadata2);
@@ -167,30 +167,70 @@ TEST_F( MetadataTest, Operate)
 
 TEST_F( MetadataTest, Randomize)
 {
-	int different,equal;
-	different=-1;
-	equal=-2;
-	MetaData auxMetadata;
-	for (int var = 0; var < 20; var++)
-	{
-	    auxMetadata.randomize(mDsource);
-	    if(mDsource==auxMetadata)
-	    	equal=1;
-	    else
-	    	different=1;
-	}
+    int different,equal;
+    different=-1;
+    equal=-2;
+    MetaData auxMetadata;
+    for (int var = 0; var < 20; var++)
+    {
+        auxMetadata.randomize(mDsource);
+        if(mDsource==auxMetadata)
+            equal=1;
+        else
+            different=1;
+    }
     EXPECT_EQ(different,equal);
+}
+
+#include <string.h>
+TEST_F( MetadataTest, ReadWrite)
+{
+    //temp file name
+    char sfn[32] = "";
+    strncpy(sfn, "/tmp/testWrite_XXXXXX", sizeof sfn);
+    mkstemp(sfn);
+    mDsource.write(sfn);
+
+    MetaData auxMetadata;
+    auxMetadata.read(sfn);
+
+    EXPECT_EQ(mDsource,auxMetadata);
+
+//    //read file as char string
+//    char * buffer;
+//    size_t size;
+//    std::ifstream file (sfn, std::ios::in|std::ios::binary|std::ios::ate);
+//    size = file.tellg();
+//    file.seekg (0, std::ios::beg);
+//    buffer = new char [size+1];
+//    file.read (buffer, size);
+//    file.close();
+//    buffer[size]=0;
+//
 }
 
 TEST_F( MetadataTest, Removelabel)
 {
-	MetaData auxMetadata = mDunion;
+    MetaData auxMetadata = mDunion;
     auxMetadata.removeLabel(MDL_X);
     std::vector<MDLabel> v1,v2;
     v1.push_back(MDL_Y);
     v2 = auxMetadata.getActiveLabels();
     EXPECT_EQ(v2,v1);
 }
+
+TEST_F( MetadataTest, Select)
+{
+    MetaData auxMetadata;
+    MetaData auxMetadata2;
+    id = auxMetadata2.addObject();
+    auxMetadata2.setValue(MDL_X,3.,id);
+    auxMetadata2.setValue(MDL_Y,4.,id);
+
+    auxMetadata.importObjects(mDsource,MDExpression((String)"x>2"));
+    EXPECT_EQ(auxMetadata,auxMetadata2);
+}
+
 
 TEST_F( MetadataTest, Size)
 {
@@ -199,7 +239,7 @@ TEST_F( MetadataTest, Size)
 
 TEST_F( MetadataTest, Sort)
 {
-	MetaData auxMetadata,auxMetadata2;
+    MetaData auxMetadata,auxMetadata2;
     id = auxMetadata.addObject();
     auxMetadata.setValue(MDL_X,3.,id);
     auxMetadata.setValue(MDL_Y,4.,id);
@@ -212,26 +252,19 @@ TEST_F( MetadataTest, Sort)
 
 TEST_F( MetadataTest, Substraction)
 {
-	MetaData auxMetadata = mDunion;
+    MetaData auxMetadata = mDunion;
     auxMetadata.subtraction(mDanotherSource,MDL_X);
     EXPECT_EQ(auxMetadata,mDsource);
 }
 
 TEST_F( MetadataTest, Union)
 {
-	//FIXME union all is missing
-	MetaData auxMetadata = mDsource;
+    //FIXME union all is missing
+    MetaData auxMetadata = mDsource;
     auxMetadata.unionAll(mDanotherSource);
     EXPECT_EQ(auxMetadata,mDunion);
 }
 
-
-//TEST_F( MetadataTest, select)
-//{
-//	MetaData auxMetadata;
-//    auxMetadata.importObjects(mDsource,MDExpression((String)"x>-2"));
-//    EXPECT_EQ(auxMetadata,mDsource);
-//}
 
 GTEST_API_ int main(int argc, char **argv)
 {
