596025be18277c3c00a7a29c840a2a99f373c61d
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-07 19:55:15 +0000

    creation
    git-svn-id: file:///home/roberto/Git/svn@8338 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: ca08dffa712ce6fa933b9d9c3983bda56a163001

---
+++ trunk/testXmipp/Script/config.py
@@ -0,0 +1,10 @@
+XMIPP_BASE='/home/xmipp/XMIPP'
+XMIPP_HOME=XMIPP_BASE+'/xmipp'
+XMIPP_TEST=XMIPP_HOME + '/applications/tests'
+XMIPP_LOGS=XMIPP_BASE + '/Logs'
+SERVER = 'www.cnb.csic.es'
+
+fromaddr = "xmipp@bioweb.cnb.csic.es"
+toaddrs  = ["roberto.marabini.cnb@gmail.com","jcuenca@cnb.csic.es","delarosatrevin@gmail.com,coss@cnb.csic.es,jvega@cnb.csic.es","joton@cnb.csic.es"]
+subject = "xmipp Compilation"
+testNames=['xmipp_test_metadata',  'xmipp_test_multidim', 'xmipp_test_image']
---
+++ trunk/testXmipp/Script/parse_test.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+import datetime, os,shutil,config
+import mail
+#create log file
+from xml.sax.handler import ContentHandler
+from xml.sax import make_parser
+class TaskXMLHandler(ContentHandler):    
+    #TODO start elemennt  processing must be centralized
+    nFailures= 0
+    inFailures = False
+    myMessage=""
+    mySuccess=True
+    def startElement(self, name, attrs): 
+        if (name == "testsuite") :
+            self.mySuccess=True
+            self.myMessage = ""
+        elif (name == "testcase") :
+ 	    self.inFailures = False   
+            self.name      = attrs.get("name")
+            self.classname = attrs.get("classname")
+        elif (name == "failure") :
+            self.message = attrs.get("message")
+	    self.inFailures = True   
+
+    def endElement(self, name):
+        if (name == "testcase" and self.inFailures) :
+            self.reportError()
+	elif (name == "testcase"):
+	    self.reportOk()
+
+    def reportError(self):
+	self.myMessage += "Test %s in testgroup %s FAILED with error %s\n"%(self.name,self.classname,self.message)
+	self.mySuccess = False
+	
+    def reportOk(self):
+	self.myMessage += "Test %s in testgroup %s was OK\n"%(self.name,self.classname)
+
+def main(filename):
+    """ Parse xml test files """
+    from  config import XMIPP_TEST
+    from  config import testNames
+
+    XMIPP_OUTPUT=XMIPP_TEST + '/OUTPUT'
+    task = TaskXMLHandler()
+    saxparser = make_parser()
+    saxparser.setContentHandler(task)
+    import glob
+    globalMessage=""
+    success=True
+    for testName in testNames:
+        globalMessage += "\n"
+        filename = XMIPP_OUTPUT+'/'+testName+'.xml'
+        if not os.path.exists(filename):
+	    globalMessage += "\n FAILED generation file %s by test %s"%(filename,testName)
+	    success = False
+	else:
+	    saxparser.parse(filename)
+	    globalMessage += task.myMessage
+	    if not task.mySuccess:
+	        success = False
+    if success:
+       summaryMessage='XMIPP compilation was OK'
+    else:
+       summaryMessage='XMIPP compilation FAILED'
+    mail.mail(config.toaddrs,config.fromaddr,summaryMessage,globalMessage)
+ 
