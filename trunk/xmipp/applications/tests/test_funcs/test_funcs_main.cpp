483e98f5eee9f787d08eb0aea0ca8b4a98e866be
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-07 21:30:50 +0000

    git-svn-id: file:///home/roberto/Git/svn@8342 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    Former-commit-id: 045d2bf60f22be41944168e99fc03101b6952676

--- trunk/xmipp/applications/tests/test_funcs/test_funcs_main.cpp
+++ trunk/xmipp/applications/tests/test_funcs/test_funcs_main.cpp
@@ -1,4 +1,4 @@
-#include <data/image.h>
+#include <data/funcs.h>
 #include <iostream>
 #include "../../../external/gtest-1.6.0/fused-src/gtest/gtest.h"
 // MORE INFO HERE: http://code.google.com/p/googletest/wiki/AdvancedGuide
@@ -10,18 +10,32 @@ protected:
     //init metadatas
     virtual void SetUp()
     {
-        a=0;
+#define len 128
+        //find binaries directory
+        char szTmp[len];
+        char pBuf[len];
+        sprintf(szTmp, "/proc/%d/exe", getpid());
+        int bytes = std::min(readlink(szTmp, pBuf, len), (ssize_t)len - 1);
+        if(bytes >= 0)
+            pBuf[bytes] = '\0';
+        //remove last token
+        FileName filename(pBuf);
+        filename = filename.removeFilename();
+        //get example images/staks
+        source1 = filename + "/../applications/tests/test_funcs/singleImage.spi";
+        source2 = filename + "/../applications/tests/test_funcs/singleImage.mrc";
     }
 
     // virtual void TearDown() {}//Destructor
-    int a;
+    FileName source1;
+    FileName source2;
 };
 
 
-TEST_F( FuncsTest, CompareTwoFiles)
+TEST_F( FuncTest, CompareTwoFiles)
 {
-    ASSERT_TRUE(myImage==myImage);
-    ASSERT_FALSE(myImage==myStack);
+    ASSERT_TRUE(compareTwoFiles(source1,source1,0));
+    ASSERT_FALSE(compareTwoFiles(source1,source2,0));
 }
 
 
