72704d4a7f43bd10c715b26eafdd80fd9e0fa827
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-12 22:08:33 +0000

    cleaning
    git-svn-id: file:///home/roberto/Git/svn@8414 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 0a3043507ad504d4c95938247ff33f6295fd2286

--- trunk/xmipp/applications/tests/test_fftw/test_fftw_main.cpp
+++ trunk/xmipp/applications/tests/test_fftw/test_fftw_main.cpp
@@ -4,8 +4,6 @@
 #include <iostream>
 #include "../../../external/gtest-1.6.0/fused-src/gtest/gtest.h"
 // MORE INFO HERE: http://code.google.com/p/googletest/wiki/AdvancedGuide
-// This test is named "Size", and belongs to the "MetadataTest"
-// test case.
 class FftwTest : public ::testing::Test
 {
 protected:
