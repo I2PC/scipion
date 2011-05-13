7457d9269583fa2f80fa7fdd728d6da9ee4163b3
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-13 09:06:28 +0000

    added fftw and filters test
    git-svn-id: file:///home/roberto/Git/svn@8421 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 1ee196a486cdfe026cd3e9e4da12e6b3cefd07e9

--- trunk/testXmipp/Script/config.py
+++ trunk/testXmipp/Script/config.py
@@ -6,8 +6,11 @@ SERVER = 'www.cnb.csic.es'
 
 fromaddr = "xmipp@bioweb.cnb.csic.es"
 toaddrs  = ["roberto.marabini.cnb@gmail.com","jcuenca@cnb.csic.es","delarosatrevin@gmail.com","coss@cnb.csic.es","jvega@cnb.csic.es","joton@cnb.csic.es","alejandro.e.rey@gmail.com"]
+#toaddrs  = ["roberto.marabini.cnb@gmail.com"]
 subject = "xmipp Compilation"
-testNames=['xmipp_test_funcs',\
+testNames=['test_fftw'\
+           'test_filters'\
+           'xmipp_test_funcs',\
 	   'xmipp_test_image',\
 	   'xmipp_test_image_generic',\
 	   'xmipp_test_metadata',\
