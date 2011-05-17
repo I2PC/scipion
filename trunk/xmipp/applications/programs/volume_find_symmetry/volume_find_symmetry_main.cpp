b1186cd5c5af5c3cb9b4ee1487096186f99e103f
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-17 11:18:11 +0000

    Search for helical symmetry added
    git-svn-id: file:///home/roberto/Git/svn@8455 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 875b864f4253ff1dc40a85ee6dae2f58e9e02250

--- trunk/xmipp/applications/programs/volume_find_symmetry/volume_find_symmetry_main.cpp
+++ trunk/xmipp/applications/programs/volume_find_symmetry/volume_find_symmetry_main.cpp
@@ -45,7 +45,9 @@ public:
     FileName fn_input, fn_output;
     double   rot0,  rotF,  step_rot;
     double   tilt0, tiltF, step_tilt;
+    double   z0, zF, step_z;
     bool     local;
+    bool     helical;
     Mask mask_prm;
     int numberOfThreads;
 
@@ -62,16 +64,24 @@ public:
         addUsageLine("+center of the volume and whose direction is this vector.");
         addUsageLine("+It is important that the volume is correctly centered.");
         addSeeAlsoLine("volume_center, transform_geometry");
-        addParamsLine(" -i <volumeFile>               : Volume to process");
-        addParamsLine("[-o+ <file=\"\">]              : Metadata with the orientation of the symmetry axis");
-        addParamsLine("--rot_sym <n>                  : Order of the rotational axis");
+        addParamsLine(" -i <volumeFile>                : Volume to process");
+        addParamsLine("[-o+ <file=\"\">]               : Metadata with the orientation of the symmetry axis");
+        addParamsLine("[--thr <N=1>]                   : Number of threads");
+        addParamsLine("--sym <mode>                    : Symmetry mode");
+        addParamsLine("    where <mode>");
+        addParamsLine("           rot <n>              : Order of the rotational axis");
+        addParamsLine("           helical              : Helical symmetry.");
+        addParamsLine("==Locate rotational axis==");
         addParamsLine("[--rot  <rot0=0>  <rotF=355> <step=5>]: Limits for rotational angle search");
         addParamsLine("[--tilt <tilt0=0> <tiltF=90> <step=5>]: Limits for tilt angle search");
-        addParamsLine("[--local <rot0> <tilt0>]       : Perform a local search around this angle");
-        addParamsLine("[--useSplines+]                : Use cubic B-Splines for the interpolations");
-        addParamsLine("[--thr <N=1>]                  : Number of threads");
+        addParamsLine("[--localRot <rot0> <tilt0>]     : Perform a local search around this angle");
+        addParamsLine("[--useSplines+]                 : Use cubic B-Splines for the interpolations");
+        addParamsLine("==Locate helical parameters==");
+        addParamsLine("[-z <z0> <zF> <zstep>]          : Search space for the shift in Z");
+        addParamsLine("[--rotHelical <rot0=0> <rotF=355> <step=5>]: Search space for rotation around Z");
+        addParamsLine("[--localHelical <rot0> <z0>]    : Perform a local search around this angle");
         mask_prm.defineParams(this,INT_MASK,NULL,"Restrict the comparison to the mask area.");
-        addExampleLine("A typical application is ",false);
+        addExampleLine("A typical application for a rotational symmetry axis is ",false);
         addExampleLine("xmipp_volume_center -i volume.vol");
         addExampleLine("xmipp_volume_find_symmetry -i volume.vol --rot_sym 3");
         addExampleLine("Presume the symmetry axis is in rot=20, tilt=10. To align vertically the axis use",false);
@@ -83,22 +93,51 @@ public:
     {
         fn_input = getParam("-i");
         fn_output = getParam("-o");
-        rot_sym = getIntParam("--rot_sym");
-        useSplines = checkParam("--useSplines");
-        local = checkParam("--local");
-        if (local)
+        String mode;
+        mode=getParam("--sym");
+        if (mode=="helical")
+        	helical=true;
+        else
         {
-            rot0=getDoubleParam("--local",0);
-            tilt0=getDoubleParam("--local",1);
+        	helical=false;
+            rot_sym = getIntParam("--sym",1);
+        }
+        useSplines = checkParam("--useSplines");
+        if (helical)
+        {
+            local=checkParam("--localHelical");
+            if (local)
+            {
+                z0=getDoubleParam("--localHelical",0);
+                rot0=getDoubleParam("--localHelical",1);
+            }
+            else
+            {
+                z0=getDoubleParam("-z",0);
+                zF=getDoubleParam("-z",1);
+                step_z=getDoubleParam("-z",2);
+                rot0=getDoubleParam("--rotHelical",0);
+                rotF=getDoubleParam("--rotHelical",1);
+                step_rot=getDoubleParam("--rotHelical",2);
+            }
         }
         else
         {
-            rot0=getDoubleParam("--rot",0);
-            rotF=getDoubleParam("--rot",1);
-            step_rot=getDoubleParam("--rot",2);
-            tilt0=getDoubleParam("--tilt",0);
-            tiltF=getDoubleParam("--tilt",1);
-            step_tilt=getDoubleParam("--tilt",2);
+            local=checkParam("--localRot");
+            if (local)
+            {
+                rot0=getDoubleParam("--local",0);
+                tilt0=getDoubleParam("--local",1);
+            }
+            else
+            {
+                rot0=getDoubleParam("--rot",0);
+                rotF=getDoubleParam("--rot",1);
+                step_rot=getDoubleParam("--rot",2);
+                tilt0=getDoubleParam("--tilt",0);
+                tiltF=getDoubleParam("--tilt",1);
+                step_tilt=getDoubleParam("--tilt",2);
+            }
         }
         mask_prm.allowed_data_types = INT_MASK;
         if (checkParam("--mask"))
@@ -111,7 +150,10 @@ public:
         Matrix1D<double> p(2);
         DIRECT_A1D_ELEM(vbest_corr,thrId) = 0;
         DIRECT_A1D_ELEM(vbest_rot,thrId)  = 0;
-        DIRECT_A1D_ELEM(vbest_tilt,thrId) = 0;
+        if (helical)
+        	DIRECT_A1D_ELEM(vbest_z,thrId) = 0;
+        else
+        	DIRECT_A1D_ELEM(vbest_tilt,thrId) = 0;
         size_t first, last;
         if (thrId==0)
             init_progress_bar(rotVector.size());
@@ -120,13 +162,26 @@ public:
             for (size_t i=first; i<=last; i++)
             {
                 XX(p)=rotVector[i];
-                YY(p)=tiltVector[i];
+                if (helical)
+                    YY(p)=zVector[i];
+                else
+                    YY(p)=tiltVector[i];
                 double corr=-evaluateSymmetry(MATRIX1D_ARRAY(p)-1);
+                if (helical)
+                {
+                	int xDim=XSIZE(helicalCorrelation());
+                    int jj=i%xDim;
+                    int ii=i/xDim;
+                    helicalCorrelation(ii,jj)=corr;
+                }
                 if (corr > DIRECT_A1D_ELEM(vbest_corr,thrId))
                 {
                     DIRECT_A1D_ELEM(vbest_corr,thrId) = corr;
                     DIRECT_A1D_ELEM(vbest_rot,thrId) = XX(p);
-                    DIRECT_A1D_ELEM(vbest_tilt,thrId) = YY(p);
+                    if (helical)
+                        DIRECT_A1D_ELEM(vbest_z,thrId) = YY(p);
+                    else
+                        DIRECT_A1D_ELEM(vbest_tilt,thrId) = YY(p);
                 }
             }
             if (thrId==0)
@@ -142,24 +197,93 @@ public:
         volume.read(fn_input);
         volume().setXmippOrigin();
         mask_prm.generate_mask(volume());
-        double best_corr, best_rot, best_tilt;
+        double best_corr, best_rot, best_tilt, best_z;
 
-        // Look for the rotational symmetry axis
-        if (!local)
+        if (!helical)
         {
-            if (verbose>0)
-                std::cerr << "Searching symmetry axis ...\n";
-            int i = 0;
-            for (double rot = rot0; rot <= rotF; rot += step_rot)
-                for (double tilt = tilt0; tilt <= tiltF; tilt += step_tilt)
+            // Look for the rotational symmetry axis
+            if (!local)
+            {
+                if (verbose>0)
+                    std::cerr << "Searching symmetry axis ...\n";
+                int i = 0;
+                for (double rot = rot0; rot <= rotF; rot += step_rot)
+                    for (double tilt = tilt0; tilt <= tiltF; tilt += step_tilt)
+                    {
+                        rotVector.push_back(rot);
+                        tiltVector.push_back(tilt);
+                    }
+                vbest_corr.resizeNoCopy(numberOfThreads);
+                vbest_corr.initConstant(-1e38);
+                vbest_rot.initZeros(numberOfThreads);
+                vbest_tilt.initZeros(numberOfThreads);
+                td = new ThreadTaskDistributor(rotVector.size(), 5);
+                ThreadManager * thMgr =  new ThreadManager(numberOfThreads,this);
+                thMgr->run(globalThreadEvaluateSymmetry);
+                best_corr=-1e38;
+                FOR_ALL_ELEMENTS_IN_ARRAY1D(vbest_corr)
+                if (vbest_corr(i)>best_corr)
                 {
-                    rotVector.push_back(rot);
-                    tiltVector.push_back(tilt);
+                    best_corr=vbest_corr(i);
+                    best_rot=vbest_rot(i);
+                    best_tilt=vbest_tilt(i);
                 }
+            }
+            else
+            {
+                Matrix1D<double> p(2), steps(2);
+                p(0)=rot0;
+                p(1)=tilt0;
+                double fitness;
+                int iter;
+                steps.initConstant(1);
+                powellOptimizer(p,1,2,&evaluateSymmetryWrapper,this,0.01,
+                                fitness,iter,steps,true);
+                best_rot=p(0);
+                best_tilt=p(1);
+            }
+            Matrix2D<double> Euler;
+            Matrix1D<double> sym_axis;
+            Euler_angles2matrix(best_rot, best_tilt, 0, Euler);
+            Euler.getRow(2, sym_axis);
+            if (verbose!=0)
+                std::cout << "Symmetry axis (rot,tilt)= " << best_rot << " "
+                << best_tilt << " --> " << sym_axis << std::endl;
+            if (fn_output!="")
+            {
+                std::vector<double> direction;
+                direction.push_back(XX(sym_axis));
+                direction.push_back(YY(sym_axis));
+                direction.push_back(ZZ(sym_axis));
+
+                MetaData MD;
+                size_t id=MD.addObject();
+                MD.setValue(MDL_ANGLEROT,best_rot,id);
+                MD.setValue(MDL_ANGLETILT,best_tilt,id);
+                MD.setValue(MDL_DIRECTION,direction,id);
+                MD.write(fn_output);
+            }
+        }
+        else
+        {
+            // If helical
+            int ydim=0, xdim=0;
+            for (double rot = rot0; rot <= rotF; rot += step_rot)
+            {
+                ydim++;
+                for (double z = z0; z <= zF; z += step_z)
+                {
+                    if (ydim==1)
+                        xdim++;
+                    rotVector.push_back(rot);
+                    zVector.push_back(z);
+                }
+            }
             vbest_corr.resizeNoCopy(numberOfThreads);
             vbest_corr.initConstant(-1e38);
             vbest_rot.initZeros(numberOfThreads);
-            vbest_tilt.initZeros(numberOfThreads);
+            vbest_z.initZeros(numberOfThreads);
+            helicalCorrelation().initZeros(ydim,xdim);
             td = new ThreadTaskDistributor(rotVector.size(), 5);
             ThreadManager * thMgr =  new ThreadManager(numberOfThreads,this);
             thMgr->run(globalThreadEvaluateSymmetry);
@@ -169,82 +293,73 @@ public:
             {
                 best_corr=vbest_corr(i);
                 best_rot=vbest_rot(i);
-                best_tilt=vbest_tilt(i);
+                best_z=vbest_z(i);
             }
-        }
-        else
-        {
-            Matrix1D<double> p(2), steps(2);
-            p(0)=rot0;
-            p(1)=tilt0;
-            double fitness;
-            int iter;
-            steps.initConstant(1);
-            powellOptimizer(p,1,2,&evaluateSymmetryWrapper,this,0.01,
-                            fitness,iter,steps,true);
-            best_rot=p(0);
-            best_tilt=p(1);
-        }
-        Matrix2D<double> Euler;
-        Matrix1D<double> sym_axis;
-        Euler_angles2matrix(best_rot, best_tilt, 0, Euler);
-        Euler.getRow(2, sym_axis);
-        if (verbose!=0)
-            std::cout << "Symmetry axis (rot,tilt)= " << best_rot << " "
-            << best_tilt << " --> " << sym_axis << std::endl;
-        if (fn_output!="")
-        {
-            std::vector<double> direction;
-            direction.push_back(XX(sym_axis));
-            direction.push_back(YY(sym_axis));
-            direction.push_back(ZZ(sym_axis));
-
-            MetaData MD;
-            size_t id=MD.addObject();
-            MD.setValue(MDL_ANGLEROT,best_rot,id);
-            MD.setValue(MDL_ANGLETILT,best_tilt,id);
-            MD.setValue(MDL_DIRECTION,direction,id);
-            MD.write(fn_output);
+            if (verbose!=0)
+                std::cout << "Symmetry parameters (rot,z)= " << best_rot << " "
+                << best_z << std::endl;
+            if (fn_output!="")
+            {
+                MetaData MD;
+                size_t id=MD.addObject();
+                MD.setValue(MDL_ANGLEROT,best_rot,id);
+                MD.setValue(MDL_SHIFTZ,best_z,id);
+                MD.write(fn_output);
+                helicalCorrelation.write(fn_output+".xmp");
+            }
         }
     }
 
     Image<double> volume;
-    std::vector<double> rotVector;
-    std::vector<double> tiltVector;
+    std::vector<double> rotVector, tiltVector, zVector;
     ThreadTaskDistributor * td;
-    MultidimArray<double> vbest_corr, vbest_rot, vbest_tilt;
+    MultidimArray<double> vbest_corr, vbest_rot, vbest_tilt, vbest_z;
+    Image<double> helicalCorrelation;
 
     /* Evaluate symmetry ------------------------------------------------------- */
     double evaluateSymmetry(double *p)
     {
         MultidimArray<double> volume_sym, volume_aux;
-        Matrix2D<double> Euler, sym_matrix;
-        Matrix1D<double> sym_axis;
-
-        double rot=p[1];
-        double tilt=p[2];
-
-        // Compute symmetry axis
-        Euler_angles2matrix(rot, tilt, 0, Euler);
-        Euler.getRow(2, sym_axis);
-        sym_axis.selfTranspose();
-
-        // Symmetrize along this axis
-        volume_sym = volume();
-        for (int n = 1; n < rot_sym; n++)
+        const MultidimArray<double> &mVolume=volume();
+        Matrix2D<double> sym_matrix;
+        if (!helical)
         {
-            rotation3DMatrix(360.0 / rot_sym * n, sym_axis, sym_matrix);
-            if (useSplines)
-                applyGeometry(BSPLINE3, volume_aux, volume(), sym_matrix,
-                              IS_NOT_INV, DONT_WRAP);
-            else
-                applyGeometry(LINEAR, volume_aux, volume(), sym_matrix,
-                              IS_NOT_INV, DONT_WRAP);
-            volume_sym += volume_aux;
+            Matrix2D<double> Euler;
+            Matrix1D<double> sym_axis;
+
+            double rot=p[1];
+            double tilt=p[2];
+
+            // Compute symmetry axis
+            Euler_angles2matrix(rot, tilt, 0, Euler);
+            Euler.getRow(2, sym_axis);
+            sym_axis.selfTranspose();
+
+            // Symmetrize along this axis
+            volume_sym = volume();
+            for (int n = 1; n < rot_sym; n++)
+            {
+                rotation3DMatrix(360.0 / rot_sym * n, sym_axis, sym_matrix);
+                if (useSplines)
+                    applyGeometry(BSPLINE3, volume_aux, mVolume, sym_matrix,
+                                  IS_NOT_INV, DONT_WRAP);
+                else
+                    applyGeometry(LINEAR, volume_aux, mVolume, sym_matrix,
+                                  IS_NOT_INV, DONT_WRAP);
+                volume_sym += volume_aux;
+            }
+        }
+        else
+        {
+            double rot=p[1];
+            double z=p[2];
+            rotation3DMatrix(rot,'Z',sym_matrix);
+            MAT_ELEM(sym_matrix,2,2)=z;
+            applyGeometry(LINEAR,volume_sym,mVolume,sym_matrix,IS_NOT_INV, DONT_WRAP);
         }
 
         // Measure correlation
-        return -correlationIndex(volume(), volume_sym, &mask_prm.get_binary_mask());
+        return -correlationIndex(mVolume, volume_sym, &mask_prm.get_binary_mask());
     }
 };
 
