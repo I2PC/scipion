9f745c5d02131133613bd6a10e1efdd373c1a1e7
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-23 21:43:51 +0000

    git-svn-id: file:///home/roberto/Git/svn@8573 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    Former-commit-id: e09471769a7fb518f6c435c8dd7c82299869f296

--- trunk/testXmipp/Script/config.py
+++ trunk/testXmipp/Script/config.py
@@ -4,14 +4,8 @@ XMIPP_TEST=XMIPP_HOME + '/applications/tests'
 XMIPP_LOGS=XMIPP_BASE + '/Logs'
 SERVER = 'www.cnb.csic.es'
 
-#fromaddr = "xmipp@bioweb.cnb.csic.es"
+fromaddr = "xmipp@bioweb.cnb.csic.es"
 toaddrs  = ["roberto.marabini.cnb@gmail.com","jcuenca@cnb.csic.es","delarosatrevin@gmail.com","coss@cnb.csic.es","jvega@cnb.csic.es","joton@cnb.csic.es","alejandro.e.rey@gmail.com"]
 #toaddrs  = ["roberto.marabini.cnb@gmail.com"]
-subject = "xmipp Compilation"
-#testNames=['xmipp_test_fftw'\
-#           'xmipp_test_filters'\
-#           'xmipp_test_funcs',\
-#	   'xmipp_test_image',\
-#	   'xmipp_test_image_generic',\
-#	   'xmipp_test_metadata',\
-#	   'xmipp_test_multidim']
+#toaddrs = ['delarosatrevin@gmail.com']
+
