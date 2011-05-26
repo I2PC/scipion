07f51cda697903f22f990ccafe82cca1d8d242ed
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-26 11:47:15 +0000

    add minus
    git-svn-id: file:///home/roberto/Git/svn@8683 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 6e96a43739cca46b0d00b311be5e535de494c105

--- trunk/xmipp/applications/tests/test_pythoninterface/batch_test_pythoninterface.py
+++ trunk/xmipp/applications/tests/test_pythoninterface/batch_test_pythoninterface.py
@@ -14,13 +14,12 @@ class TestXmippPythonInterface(unittest.TestCase):
         """This function performs all the setup stuff.      
         """
         pass
-    
-    def test_compareImage(self):
+        
+    def test_Image_compare(self):
         imgPath = os.path.join(self.testsPath, "test_image", "singleImage.spi")
         img1 = Image()
         img1.read(imgPath)
-        img2 = Image()
-        img2.read(imgPath)
+        img2 = Image(imgPath)
         # Test that image is equal to itself
         self.assertEqual(img1, img2)
         # Test different images
@@ -28,20 +27,43 @@ class TestXmippPythonInterface(unittest.TestCase):
         img2.read(imgPath)
         self.assertNotEqual(img1, img2)
         
-    def test_compose(self):
+    def test_Image_add(self):
+        path1 = "1@" + os.path.join(self.testsPath, "test_image", "smallStack.stk")
+        img1 = Image(path1)
+        path2 = "2@" + os.path.join(self.testsPath, "test_image", "smallStack.stk")
+        img2 = Image(path2)
+        img1.add(img2)
+        path2 = os.path.join(self.testsPath, "test_image_generic", "add.spi")
+        img2.read(path2)
+        self.assertEqual(img1, img2)
+        img1.add(img2)        
+        self.assertNotEqual(img1, img2)   
+             
+    def test_Image_minus(self):
+        pathSum = os.path.join(self.testsPath, "test_image_generic", "add.spi")
+        imgAdd =Image(pathSum)
+        path1 = "1@" + os.path.join(self.testsPath, "test_image", "smallStack.stk")
+        img1 = Image(path1)
+        path2 = "2@" + os.path.join(self.testsPath, "test_image", "smallStack.stk")
+        img2 = Image(path2)
+        
+        imgAdd.minus(img2)
+        self.assertEqual(img1, imgAdd)   
+             
+    def test_FileName_compose(self):
          fn1 = FileName("kk000001.xmp")
          fn2 = FileName("")
-         fn2.compose("kk",1,"xmp")
-         self.assertEqual(str(fn1),str(fn2))
-         self.assertNotEqual(str(fn1)+'kk',str(fn2))
+         fn2.compose("kk", 1, "xmp")
+         self.assertEqual(str(fn1), str(fn2))
+         self.assertNotEqual(str(fn1) + 'kk', str(fn2))
 
-    def test_isInStack(self):
+    def test_FileName_isInStack(self):
          fn1 = FileName("1@.xmp")
          fn2 = FileName("1.xmp")
          self.assertTrue (fn1.isInStack())
          self.assertFalse(fn2.isInStack())
          
-    def test_isMetaData(self):
+    def test_FileName_isMetaData(self):
          imgPath = os.path.join(self.testsPath, "test_image", "smallStack.stk")
          fn1 = FileName(imgPath)
          self.assertFalse(fn1.isMetaData())
@@ -49,18 +71,70 @@ class TestXmippPythonInterface(unittest.TestCase):
          fn2 = FileName(imgPath)
          self.assertTrue (fn2.isMetaData())
 
-    def test_metadataIter(self):
+    def test_Metadata_iter(self):
          mdPath = os.path.join(self.testsPath, "test_pythoninterface", "test.xmd")
          mD = MetaData(mdPath)
+         i = 1
          for id in mD:
-             img = mD.getValue(MDL_IMAGE,id)
-             if (id==1):
-                 self.assertEqual (img,'000001@Images/proj_ctf_1.stk')
-             elif (id==2):
-                 self.assertEqual (img,'000002@Images/proj_ctf_1.stk')
-             elif (id==3):
-                 self.assertEqual (img,'000003@Images/proj_ctf_1.stk')
-         
+             img = mD.getValue(MDL_IMAGE, id)
+             expImg = '00000%d@Images/proj_ctf_1.stk' % i
+             self.assertEqual(img, expImg)
+             i += 1
+                 
+    def test_Metadata_getValue(self):
+        '''MetaData_GetValues'''
+        mdPath = os.path.join(self.testsPath, "test_pythoninterface", "test.xmd")
+        mD = MetaData(mdPath)
+        
+        img = mD.getValue(MDL_IMAGE, 1L)
+        self.assertEqual(img, '000001@Images/proj_ctf_1.stk')
+        defocus = mD.getValue(MDL_CTF_DEFOCUSU, 2L)
+        self.assertAlmostEqual(defocus, 200.0)
+        count = mD.getValue(MDL_COUNT, 3L)
+        self.assertEqual(count, 30)
+        list = mD.getValue(MDL_ANGLE_COMPARISON, 1L)
+        self.assertEqual(list, [1.0, 2.0, 3.0])
+        ref = mD.getValue(MDL_REF3D, 2L)
+        self.assertEqual(ref, 2)
+        
+    def test_Metadata_setValue(self):
+        '''MetaData_setValues'''
+        '''This test should produce the following metadata, which is the same of 'test.xmd'
+        data_
+        loop_
+         _image
+         _CTFModel
+         _CTF_Defocus_U
+         _count
+         _angleComparison
+         _ref3d
+         000001@Images/proj_ctf_1.stk  CTFs/10.ctfparam  -100.000000         10 [     1.000000     2.000000     3.000000 ]         -1
+         000002@Images/proj_ctf_1.stk  CTFs/10.ctfparam   200.000000         20 [     1.000000     4.000000     9.000000 ]          2
+         000003@Images/proj_ctf_1.stk  CTFs/10.ctfparam  -300.000000         30 [     1.000000     8.000000    27.000000 ]         -3
+        ''' 
+        mdPath = os.path.join(self.testsPath, "test_pythoninterface", "test.xmd")
+        mdRef = MetaData(mdPath)
+        md = MetaData()
+        ii = -1
+        listOrig = [1.0, 2.0, 3.0]
+        for i in range(1, 4):
+            id = md.addObject() 
+            img = '00000%i@Images/proj_ctf_1.stk' % i
+            md.setValue(MDL_IMAGE, img, id)
+            md.setValue(MDL_CTFMODEL, 'CTFs/10.ctfparam', id)
+            md.setValue(MDL_CTF_DEFOCUSU, (i * ii * 100.0), id)
+            md.setValue(MDL_COUNT, (i * 10L), id)
+            list = [x**i for x in listOrig]
+            md.setValue(MDL_ANGLE_COMPARISON, list, id)
+            md.setValue(MDL_REF3D, (i * ii), id)
+            ii *= -1
+            
+            
+        #print mdRef, md
+        self.assertEqual(mdRef, md)
+        self.assertRaises(XmippError, md.setValue, MDL_COUNT, 5.5, 1L)
+       
+   
          
 class bcolors:
     HEADER = '\033[95m'
@@ -72,7 +146,7 @@ class bcolors:
            
 class XmippPythonTestResult(TestResult):
     xml = None
-    testFailed  = 0
+    testFailed = 0
     numberTests = 0
     def openXmlReport(self, classname, filename):
         self.xml = open(filename, 'w')
@@ -85,7 +159,7 @@ class XmippPythonTestResult(TestResult):
          if (self.testFailed):
              sys.stderr.write("%s[  FAILED  ] %s %d tests\n" % (bcolors.FAIL, bcolors.ENDC, self.testFailed))
          else:
-             sys.stderr.write("%s[  PASSED  ] %s %d tests\n" % (bcolors.OKGREEN, bcolors.ENDC, self.numberTests-self.testFailed))
+             sys.stderr.write("%s[  PASSED  ] %s %d tests\n" % (bcolors.OKGREEN, bcolors.ENDC, self.numberTests - self.testFailed))
              
     
     def getTestNames(self, test):
