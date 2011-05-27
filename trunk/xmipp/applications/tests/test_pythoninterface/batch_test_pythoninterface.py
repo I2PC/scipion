301882c5576cdeebbe22b1f6d16e7357a0f87fc5
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-27 10:50:14 +0000

    createe python lib, leave here only inforamtion related with test
    git-svn-id: file:///home/roberto/Git/svn@8720 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: ed0e71dd77b68fc4e128f8f48d8d09e677ffdc18

--- trunk/xmipp/applications/tests/test_pythoninterface/batch_test_pythoninterface.py
+++ trunk/xmipp/applications/tests/test_pythoninterface/batch_test_pythoninterface.py
@@ -140,63 +140,17 @@ class TestXmippPythonInterface(unittest.TestCase):
        
    
          
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-           
-class XmippPythonTestResult(TestResult):
-    xml = None
-    testFailed = 0
-    numberTests = 0
-    def openXmlReport(self, classname, filename):
-        self.xml = open(filename, 'w')
-        self.xml.write('<testsuite name="%s">\n' % classname)
-        
-    def closeXmlReport(self):
-         self.xml.write('</testsuite>\n')
-         self.xml.close()
-         sys.stderr.write("%s[==========]%s run %d tests\n" % (bcolors.OKGREEN, bcolors.ENDC, self.numberTests))
-         if (self.testFailed):
-             sys.stderr.write("%s[  FAILED  ] %s %d tests\n" % (bcolors.FAIL, bcolors.ENDC, self.testFailed))
-         else:
-             sys.stderr.write("%s[  PASSED  ] %s %d tests\n" % (bcolors.OKGREEN, bcolors.ENDC, self.numberTests - self.testFailed))
-             
-    
-    def getTestNames(self, test):
-        parts = str(test).split()
-        name = parts[0];
-        parts = parts[1].split('.')
-        classname = parts[1].replace(")", "")
-        self.numberTests += 1 
-        return (name, classname)
-    
-    def addSuccess(self, test):
-        name, classname = self.getTestNames(test)
-        self.xml.write('   <testcase name="%s" classname="%s"/>\n' % (name, classname))
-        sys.stderr.write("%s[ RUN      ]%s %s.%s\n" % (bcolors.OKGREEN, bcolors.ENDC, classname, name))
-        sys.stderr.write("%s[      OK  ]%s %s.%s\n" % (bcolors.OKGREEN, bcolors.ENDC, classname, name))
-    
-    def reportError(self, test, err):
-        name, classname = self.getTestNames(test)
-        self.xml.write('   <testcase name="%s" classname="%s">\n' % (name, classname))
-        self.xml.write('      <failure message=" "/>\n')
-        self.xml.write('   </testcase>\n')
-        sys.stderr.write("%s[   FAILED ]%s %s.%s\n" % (bcolors.FAIL, bcolors.ENDC, classname, name))
-        self.testFailed += 1
-                
-    def addError(self, test, err):
-        self.reportError(test, err)
-        
-    def addFailure(self, test, err):
-        self.reportError(test, err)
+from  XmippPythonTestResult import XmippPythonTestResult
+
                                         
 if __name__ == '__main__':
-    #unittest.main()      
-    xmlFile = sys.argv[1]
+    #unittest.main()   
+    argc = len(sys.argv)      
+    if  argc > 1:  
+        xmlFile = sys.argv[1]
+    else: 
+        xmlFile = '/dev/null'
+
     suite = unittest.TestLoader().loadTestsFromTestCase(TestXmippPythonInterface)
     result = XmippPythonTestResult()
     result.openXmlReport("TestXmippPythonInterface", xmlFile)    
