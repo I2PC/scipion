79fc1563b6069c264f6153ef1ce21ada14d80546
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-27 10:03:46 +0000

    Ported to v3
    git-svn-id: file:///home/roberto/Git/svn@8717 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 763986c0f489a4db19d34a34047e810a12fbc163

--- trunk/xmipp/applications/programs/mpi_run/mpi_run_main.cpp
+++ trunk/xmipp/applications/programs/mpi_run/mpi_run_main.cpp
@@ -23,110 +23,62 @@
  *  e-mail address 'xmipp@cnb.csic.es'
  ***************************************************************************/
 
-//#include "mpi_run.h"
-
-#include <mpi.h>
-#include <data/args.h>
-
-#include <cstring>
-#include <cstdlib>
+#include <parallel/mpi.h>
 #include <data/funcs.h>
+#include <data/program.h>
 
 #define TAG_WORK   0
 #define TAG_STOP   1
 #define TAG_WAIT   2
-//#define TAG_BYE    3
-class Prog_MPI_Run_Parameters
+
+class ProgMPIRun: public XmippProgram
 {
 public:
-    /** PDB file */
+    /** Command file */
     FileName fn_commands;
-
-    /** Number of Procesors **/
-    int nprocs;
-
-    /** computing node number. Master=0 */
-    int rank;
 
     /** status after am MPI call */
     MPI_Status status;
-    /** Empty constructor */
-    //Prog_MPI_Run_Parameters(int argc, char **argv);
 
-    /** Read from a command line.
-        An exception might be thrown by any of the internal conversions,
-        this would mean that there is an error in the command line and you
-        might show a usage message. */
-    //void read(int argc, char **argv);
+    // Mpi node
+    MpiNode *node;
 
-    /** Usage message.
-        This function shows the way of introducing this parameters. */
-    //void usage();
-
-    /** Show parameters. */
-    //void show();
-
-    /** Run. */
-    //void run();
-
-    /* Empty constructor ------------------------------------------------------- */
-    Prog_MPI_Run_Parameters(int argc, char **argv)
+    ProgMPIRun(int argc, char **argv)
     {
-        MPI_Comm_size(MPI_COMM_WORLD, &(nprocs));
-        MPI_Comm_rank(MPI_COMM_WORLD, &(rank));
-        if (nprocs < 2)
-            error_exit("This program cannot be executed in a single working node");
-        //Blocks until all process have reached this routine.
-        MPI_Barrier(MPI_COMM_WORLD);
+        node=new MpiNode(argc,argv);
+        if (!node->isMaster())
+            verbose=0;
     }
 
-    /** Replace substrings of a std::string by other std::strings */
-    std::string replaceAll(
-        std::string result,
-        const std::string& replaceWhat,
-        const std::string& replaceWithWhat)
+    void readParams()
     {
-        while(1)
-        {
-            const int pos = result.find(replaceWhat);
-            if (pos==-1)
-                break;
-            result.replace(pos,replaceWhat.size(),replaceWithWhat);
-        }
-        return result;
+        fn_commands = getParam("-i");
+        if (node->size < 2)
+            REPORT_ERROR(ERR_ARG_INCORRECT,
+                         "This program cannot be executed in a single working node");
     }
 
-    /* Read parameters --------------------------------------------------------- */
-    void read(int argc, char **argv)
+    void defineParams()
     {
-        fn_commands = getParameter(argc, argv, "-i");
+    	addUsageLine("Run commands in a text file in a parallel environment");
+    	addUsageLine("+You may use the tag MPI_BARRIER to separate execution blocks.");
+    	addUsageLine("+You may use the tag MPI_NEWLINE to force a line break (this is useful for programs accepting parameters directly from stdin).");
+    	addParamsLine("-i <commandFile>    : File with commands in different lines");
     }
 
-    /* Usage ------------------------------------------------------------------- */
-    void usage()
-    {
-        std::cerr << "MPI_Run\n"
-        << "   -i <command file>    : File with commands to send to mpirun\n"
-        << "\n"
-        << "Example of use:\n"
-        << "   xmipp_mpi_run -i commandd_file\n"
-        ;
-    }
-
-    /* Show -------------------------------------------------------------------- */
     void show()
     {
-        std::cout << "Commands  file:           " << fn_commands << std::endl
-        ;
+    	if (!verbose)
+    		return;
+        std::cout << "Commands  file: " << fn_commands << std::endl;
     }
-
 
     /* Run --------------------------------------------------------------------- */
 #define MAX_LINE 2048
     char szline[MAX_LINE+1];
     void run()
     {
-        if (rank == 0)
+        if (node->rank == 0)
         {
             std::ifstream fh_in;
             fh_in.open(fn_commands.c_str());
@@ -141,13 +93,13 @@ public:
                          MPI_COMM_WORLD, &status);
                 number_of_node_waiting++;
                 getline(fh_in, line);
-                line=replaceAll(line,"MPI_NEWLINE","\n");
+                line=findAndReplace(line,"MPI_NEWLINE","\n");
                 strcpy(szline, line.c_str());
 
-                std::string::size_type loc = line.find("MPI_Barrier", 0);
+                std::string::size_type loc = line.find("MPI_BARRIER", 0);
                 if (loc != std::string::npos)
                 {
-                    while (number_of_node_waiting < (nprocs - 1))
+                    while (number_of_node_waiting < (node->size - 1))
                     {
                         MPI_Recv(0, 0, MPI_INT, MPI_ANY_SOURCE, 0,
                                  MPI_COMM_WORLD, &status);
@@ -155,35 +107,22 @@ public:
                     }
                     while (number_of_node_waiting > 0)
                     {
-                        MPI_Send(&szline,
-                                 1,
-                                 MPI_CHAR,
-                                 number_of_node_waiting,
-                                 TAG_WAIT,
-                                 MPI_COMM_WORLD);
+                        MPI_Send(&szline, 1, MPI_CHAR, number_of_node_waiting,
+                                 TAG_WAIT, MPI_COMM_WORLD);
                         number_of_node_waiting--;
                     }
                     continue;
                 }
 
                 //send work
-                MPI_Send(&szline,
-                         MAX_LINE,
-                         MPI_CHAR,
-                         status.MPI_SOURCE,
-                         TAG_WORK,
-                         MPI_COMM_WORLD);
+                MPI_Send(&szline, MAX_LINE, MPI_CHAR, status.MPI_SOURCE,
+                         TAG_WORK, MPI_COMM_WORLD);
                 number_of_node_waiting--;
             }
 
             fh_in.close();
-            for (int i = 1; i < nprocs; i++)
-            {
+            for (int i = 1; i < node->size; i++)
                 MPI_Send(0, 0, MPI_INT, i, TAG_STOP, MPI_COMM_WORLD);
-                //MPI_Recv(0, 0, MPI_INT, MPI_ANY_SOURCE, TAG_BYE,
-                //                 MPI_COMM_WORLD, &status);
-            }
-
         }
         else
         {
@@ -194,10 +133,7 @@ public:
                 //get yor next task
                 MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
                 if (status.MPI_TAG == TAG_STOP)//I am free
-                {
-                    //MPI_Send(0, 0, MPI_INT, 0, TAG_BYE, MPI_COMM_WORLD);
                     break;
-                }
                 else if (status.MPI_TAG == TAG_WAIT)//I am free
                 {
                     MPI_Recv(&szline, 1, MPI_CHAR, 0, TAG_WAIT, MPI_COMM_WORLD, &status);
@@ -219,50 +155,11 @@ public:
         }
         MPI_Finalize();
     }
-
-    /* a short function to print a message and exit */
-    void error_exit(char * msg)
-    {
-        fprintf(stderr, "%s", msg);
-        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-    }
-
 };
 
 int main(int argc, char *argv[])
 {
-    if (MPI_Init(&argc, &argv) != MPI_SUCCESS)
-    {
-        fprintf(stderr, "MPI initialization error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    Prog_MPI_Run_Parameters prm(argc, argv);
-    try
-    {
-        prm.read(argc, argv);
-    }
-
-    catch (XmippError XE)
-    {
-        std::cout << XE;
-        prm.usage();
-        exit(1);
-    }
-
-    try
-    {
-        if (prm.rank == 0)
-            prm.show();
-        if (prm.nprocs==1)
-        	system(((std::string)"chmod x+u "+prm.fn_commands+" ; ./"+prm.fn_commands).c_str());
-        else
-        	prm.run();
-    }
-    catch (XmippError XE)
-    {
-        std::cerr << XE;
-        exit(1);
-    }
-    exit(0);
+	ProgMPIRun prm(argc, argv);
+    prm.read(argc, argv);
+    return prm.tryRun();
 }
