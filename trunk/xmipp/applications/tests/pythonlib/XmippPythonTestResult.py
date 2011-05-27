32206d69eec7c8bef10b9da8da4aa1c8c3efaead
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-05-27 10:51:10 +0000

    move  classes used in several python test to a librery
    git-svn-id: file:///home/roberto/Git/svn@8721 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: efdc3cf5b7f6615a9d8166384c5e3336aa051220

---
+++ trunk/xmipp/applications/tests/pythonlib/XmippPythonTestResult.py
@@ -0,0 +1,49 @@
+from unittest import TestResult, _TextTestResult, _WritelnDecorator
+import sys
+from bcolors import *
+class XmippPythonTestResult(TestResult):
+    xml = None
+    testFailed = 0
+    numberTests = 0
+    def openXmlReport(self, classname, filename):
+        self.xml = open(filename, 'w')
+        self.xml.write('<testsuite name="%s">\n' % classname)
+        
+    def closeXmlReport(self):
+         self.xml.write('</testsuite>\n')
+         self.xml.close()
+         sys.stderr.write("%s[==========]%s run %d tests\n" % (bcolors.OKGREEN, bcolors.ENDC, self.numberTests))
+         if (self.testFailed):
+             sys.stderr.write("%s[  FAILED  ] %s %d tests\n" % (bcolors.FAIL, bcolors.ENDC, self.testFailed))
+         else:
+             sys.stderr.write("%s[  PASSED  ] %s %d tests\n" % (bcolors.OKGREEN, bcolors.ENDC, self.numberTests - self.testFailed))
+             
+    
+    def getTestNames(self, test):
+        parts = str(test).split()
+        name = parts[0];
+        parts = parts[1].split('.')
+        classname = parts[1].replace(")", "")
+        self.numberTests += 1 
+        return (name, classname)
+    
+    def addSuccess(self, test):
+        name, classname = self.getTestNames(test)
+        self.xml.write('   <testcase name="%s" classname="%s"/>\n' % (name, classname))
+        sys.stderr.write("%s[ RUN      ]%s %s.%s\n" % (bcolors.OKGREEN, bcolors.ENDC, classname, name))
+        sys.stderr.write("%s[      OK  ]%s %s.%s\n" % (bcolors.OKGREEN, bcolors.ENDC, classname, name))
+    
+    def reportError(self, test, err):
+        name, classname = self.getTestNames(test)
+        self.xml.write('   <testcase name="%s" classname="%s">\n' % (name, classname))
+        self.xml.write('      <failure message=" "/>\n')
+        self.xml.write('   </testcase>\n')
+        sys.stderr.write("%s[   FAILED ]%s %s.%s\n" % (bcolors.FAIL, bcolors.ENDC, classname, name))
+        self.testFailed += 1
+                
+    def addError(self, test, err):
+        self.reportError(test, err)
+        
+    def addFailure(self, test, err):
+        self.reportError(test, err)
+                
