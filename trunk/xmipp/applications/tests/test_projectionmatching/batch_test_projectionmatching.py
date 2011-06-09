a8a4a8b00982b750aecd0c647d86422bb4734271
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-06-09 14:36:00 +0000

    git-svn-id: file:///home/roberto/Git/svn@8932 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    Former-commit-id: e53da29b084e43b6a4f48bf2d549f89f8abcf113

--- trunk/xmipp/applications/tests/test_projectionmatching/batch_test_projectionmatching.py
+++ trunk/xmipp/applications/tests/test_projectionmatching/batch_test_projectionmatching.py
@@ -121,7 +121,7 @@ class TestProjMatching(unittest.TestCase):
         print "bbb0",src, dst
 
         shutil.copy(src, dst)
-    tmpDirName = self.src + 'Iter_01/ReferenceLibrary'
+        tmpDirName = self.src + 'Iter_01/ReferenceLibrary'
         if not os.path.exists(tmpDirName):
             os.mkdir(tmpDirName)
         angular_project_library(self.log,dict)
