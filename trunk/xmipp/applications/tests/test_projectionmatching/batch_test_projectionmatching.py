de675eab12158a18a9be8264337cca42f9627d8a
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-06-14 22:22:57 +0000

    git-svn-id: file:///home/roberto/Git/svn@8985 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    Former-commit-id: 3f1a727170bac3f08b310d21c014cc57e3ef3360

--- trunk/xmipp/applications/tests/test_projectionmatching/batch_test_projectionmatching.py
+++ trunk/xmipp/applications/tests/test_projectionmatching/batch_test_projectionmatching.py
@@ -35,116 +35,116 @@ class TestProjMatching(unittest.TestCase):
         self.dst = 'ProjMatch/new20/'
 
                 
-#    def test_000execute_ctf_groups(self):
-#        CtfGroupDirectory = os.path.join(self.path,'CtfGroups')
-#        CtfGroupRootName  = 'ctf'
-#        dict = {
-#                'CTFDatName': 'new_ctf.ctfdat'
-#                ,'CtfGroupDirectory': CtfGroupDirectory
-#                ,'CtfGroupMaxDiff': 0.10000000000000001
-#                ,'CtfGroupMaxResol': 5.5999999999999996
-#                ,'CtfGroupRootName': CtfGroupRootName
-#                ,'DataArePhaseFlipped': True
-#                ,'DoAutoCtfGroup': True
-#                ,'DoCtfCorrection': True
-#                ,'PaddingFactor': 2
-#                ,'SelFileName': 'new20.sel'
-#                ,'SplitDefocusDocFile': ''
-#                ,'WienerConstant': -1
-#           }
-#        execute_ctf_groups(self.log,dict)
-#
-#        testName = CtfGroupDirectory+"/"+CtfGroupRootName+'Info.xmd'
-#        goldName = testName.replace(self.WorkingDir,self.goldWorkingDir)
-#        self.assertTrue(compareTwoFiles(goldName,testName))
-#        #check two file compare works
-#        self.assertFalse(compareTwoFiles(goldName,'/etc/passwd'))
-#
-#        testName1 = CtfGroupDirectory+"/"+CtfGroupRootName+'_ctf.stk'
-#        goldName = testName1.replace(self.WorkingDir,self.goldWorkingDir)
-#        self.assertTrue(ImgCompare(goldName,testName1))
-#
-#        testName = CtfGroupDirectory+"/"+CtfGroupRootName+'_wien.stk'
-#        goldName = testName.replace(self.WorkingDir,self.goldWorkingDir)
-#        self.assertTrue(ImgCompare(goldName,testName))
-#
-#        testName = CtfGroupDirectory+"/"+CtfGroupRootName+'_split.doc'
-#        goldName = testName.replace(self.WorkingDir,self.goldWorkingDir)
-#        self.assertTrue(compareTwoFiles(goldName,testName))
-#
-#        testName = CtfGroupDirectory+"/"+CtfGroupRootName+'_images.sel'
-#        goldName = testName.replace(self.WorkingDir,self.goldWorkingDir)
-#        self.assertTrue(compareTwoFiles(goldName,testName))
+    def test_000execute_ctf_groups(self):
+        CtfGroupDirectory = os.path.join(self.path,'CtfGroups')
+        CtfGroupRootName  = 'ctf'
+        dict = {
+                'CTFDatName': 'new_ctf.ctfdat'
+                ,'CtfGroupDirectory': CtfGroupDirectory
+                ,'CtfGroupMaxDiff': 0.10000000000000001
+                ,'CtfGroupMaxResol': 5.5999999999999996
+                ,'CtfGroupRootName': CtfGroupRootName
+                ,'DataArePhaseFlipped': True
+                ,'DoAutoCtfGroup': True
+                ,'DoCtfCorrection': True
+                ,'PaddingFactor': 2
+                ,'SelFileName': 'new20.sel'
+                ,'SplitDefocusDocFile': ''
+                ,'WienerConstant': -1
+           }
+        execute_ctf_groups(self.log,dict)
 
-#    def test_010execute_mask(self):
-#        maskedFileNamesIter='ProjMatch/new20/Iter_01/masked_reference_ref_01.vol'
-#        dict = {
-#                'DoMask': True,
-#                'DoSphericalMask': True,
-#                'maskRadius': 64,
-#                'maskedFileName': maskedFileNamesIter ,
-#                'reconstructedFileName': 'ico.vol',
-#                'userSuppliedMask': 'mask.vol'
-#        }
-#        tmpDirName=os.path.dirname(maskedFileNamesIter)
-#        if not os.path.exists(tmpDirName):
-#            os.mkdir(tmpDirName)
-#        execute_mask(self.log,dict)
-#        testFileName = maskedFileNamesIter
-#        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
-#        self.assertTrue(ImgCompare(goldFileName,testFileName))
-#
-#    def test_020angular_project_library(self):
-#        dict = {'AngSamplingRateDeg': '1',
-#                'CtfGroupSubsetFileName': 'ProjMatch/new20/CtfGroups/ctf_images.sel',
-#                'DoCtfCorrection': True,
-#                'DoParallel': True,
-#                'DoRestricSearchbyTiltAngle': False,
-#                'DocFileInputAngles': 'ProjMatch/new20/original_angles.doc',
-#                'MaxChangeInAngles': '1000',
-#                'MpiJobSize': '1',
-#                'NumberOfMpiProcesses': 3,
-#                'NumberOfThreads': 1,
-#                'OnlyWinner': False,
-#                'PerturbProjectionDirections': False,
-#                'ProjectLibraryRootName': 'ProjMatch/new20/Iter_01/ReferenceLibrary/gallery_ref_01.stk',
-#                'SymmetryGroup': 'i3',
-#                'SymmetryGroupNeighbourhood': '',
-#                'SystemFlavour': 'TORQUE-OPENMPI',
-#                'Tilt0': 40,
-#                'TiltF': 90,
-#                'maskedFileNamesIter': 'ProjMatch/new20/Iter_01/masked_reference_ref_01.vol'
-#        }
-#        src = self.src + 'original_angles.doc'
-#        dst = self.dst 
-#
-#        shutil.copy(src, dst)
-#        tmpDirName = self.dst + 'Iter_01/ReferenceLibrary'
-#        if not os.path.exists(tmpDirName):
-#            os.mkdir(tmpDirName)
-#        angular_project_library(self.log,dict)
-#        tmpDirName = os.path.join(tmpDirName,'gallery_ref_01')
-#        
-#        #do not use os.path.join because adds an extra /
-#        testFileName = tmpDirName +'.stk'
-#        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
-#        self.assertTrue(ImgCompare(goldFileName,testFileName))
-#        
-#        testFileName = tmpDirName +'.doc'
-#        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
-#        self.assertTrue(compareTwoFiles(goldFileName,testFileName))
-#        
-#        testFileName = tmpDirName +'_sampling.xmd'
-#        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
-#        self.assertTrue(compareTwoFiles(goldFileName,testFileName))
-#        
-#        testFileName = tmpDirName +'_group000001_sampling.xmd'
-#        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
-#        self.assertTrue(compareTwoFiles(goldFileName,testFileName))
-#        
-#        testFileName = tmpDirName +'_group000002_sampling.xmd'
-#        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
-#        self.assertTrue(compareTwoFiles(goldFileName,testFileName))
+        testName = CtfGroupDirectory+"/"+CtfGroupRootName+'Info.xmd'
+        goldName = testName.replace(self.WorkingDir,self.goldWorkingDir)
+        self.assertTrue(compareTwoFiles(goldName,testName))
+        #check two file compare works
+        self.assertFalse(compareTwoFiles(goldName,'/etc/passwd'))
+
+        testName1 = CtfGroupDirectory+"/"+CtfGroupRootName+'_ctf.stk'
+        goldName = testName1.replace(self.WorkingDir,self.goldWorkingDir)
+        self.assertTrue(ImgCompare(goldName,testName1))
+
+        testName = CtfGroupDirectory+"/"+CtfGroupRootName+'_wien.stk'
+        goldName = testName.replace(self.WorkingDir,self.goldWorkingDir)
+        self.assertTrue(ImgCompare(goldName,testName))
+
+        testName = CtfGroupDirectory+"/"+CtfGroupRootName+'_split.doc'
+        goldName = testName.replace(self.WorkingDir,self.goldWorkingDir)
+        self.assertTrue(compareTwoFiles(goldName,testName))
+
+        testName = CtfGroupDirectory+"/"+CtfGroupRootName+'_images.sel'
+        goldName = testName.replace(self.WorkingDir,self.goldWorkingDir)
+        self.assertTrue(compareTwoFiles(goldName,testName))
+
+    def test_010execute_mask(self):
+        maskedFileNamesIter='ProjMatch/new20/Iter_01/masked_reference_ref_01.vol'
+        dict = {
+                'DoMask': True,
+                'DoSphericalMask': True,
+                'maskRadius': 64,
+                'maskedFileName': maskedFileNamesIter ,
+                'reconstructedFileName': 'ico.vol',
+                'userSuppliedMask': 'mask.vol'
+        }
+        tmpDirName=os.path.dirname(maskedFileNamesIter)
+        if not os.path.exists(tmpDirName):
+            os.mkdir(tmpDirName)
+        execute_mask(self.log,dict)
+        testFileName = maskedFileNamesIter
+        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
+        self.assertTrue(ImgCompare(goldFileName,testFileName))
+
+    def test_020angular_project_library(self):
+        dict = {'AngSamplingRateDeg': '1',
+                'CtfGroupSubsetFileName': 'ProjMatch/new20/CtfGroups/ctf_images.sel',
+                'DoCtfCorrection': True,
+                'DoParallel': True,
+                'DoRestricSearchbyTiltAngle': False,
+                'DocFileInputAngles': 'ProjMatch/new20/original_angles.doc',
+                'MaxChangeInAngles': '1000',
+                'MpiJobSize': '1',
+                'NumberOfMpiProcesses': 3,
+                'NumberOfThreads': 1,
+                'OnlyWinner': False,
+                'PerturbProjectionDirections': False,
+                'ProjectLibraryRootName': 'ProjMatch/new20/Iter_01/ReferenceLibrary/gallery_ref_01.stk',
+                'SymmetryGroup': 'i3',
+                'SymmetryGroupNeighbourhood': '',
+                'SystemFlavour': 'TORQUE-OPENMPI',
+                'Tilt0': 40,
+                'TiltF': 90,
+                'maskedFileNamesIter': 'ProjMatch/new20/Iter_01/masked_reference_ref_01.vol'
+        }
+        src = self.src + 'original_angles.doc'
+        dst = self.dst 
+
+        shutil.copy(src, dst)
+        tmpDirName = self.dst + 'Iter_01/ReferenceLibrary'
+        if not os.path.exists(tmpDirName):
+            os.mkdir(tmpDirName)
+        angular_project_library(self.log,dict)
+        tmpDirName = os.path.join(tmpDirName,'gallery_ref_01')
+        
+        #do not use os.path.join because adds an extra /
+        testFileName = tmpDirName +'.stk'
+        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
+        self.assertTrue(ImgCompare(goldFileName,testFileName))
+        
+        testFileName = tmpDirName +'.doc'
+        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
+        self.assertTrue(compareTwoFiles(goldFileName,testFileName))
+        
+        testFileName = tmpDirName +'_sampling.xmd'
+        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
+        self.assertTrue(compareTwoFiles(goldFileName,testFileName))
+        
+        testFileName = tmpDirName +'_group000001_sampling.xmd'
+        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
+        self.assertTrue(compareTwoFiles(goldFileName,testFileName))
+        
+        testFileName = tmpDirName +'_group000002_sampling.xmd'
+        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
+        self.assertTrue(compareTwoFiles(goldFileName,testFileName))
         
     def test_030projection_matching(self):
         tmpDirName ='ProjMatch/new20/Iter_01/ProjMatchClasses'
@@ -181,27 +181,27 @@ class TestProjMatching(unittest.TestCase):
         md2=MetaData(goldFileName)
         projection_matching(self.log,dict)
         self.assertTrue(md1==md2)
-#
-#    def test_040assign_images_to_references(self):
-#        dict = {   
-#                'DocFileInputAngles': 'ProjMatch/new20/Iter_01/current_angles.doc',
-#                'NumberOfCtfGroups': 2L,
-#                'NumberOfReferences': 3,
-#                'ProjMatchRootName': [   None,
-#                    'ProjMatch/new20/Iter_01/ProjMatchClasses/proj_match_ref_01.doc',
-#                    'ProjMatch/new20/Iter_01/ProjMatchClasses/proj_match_ref_02.doc',
-#                    'ProjMatch/new20/Iter_01/ProjMatchClasses/proj_match_ref_03.doc']}
-#        #cp from goldstandard
-#        src = self.src + 'Iter_01/ProjMatchClasses/'
-#        dst = self.dst + 'Iter_01/ProjMatchClasses'
-#        shutil.copy(src+'proj_match_ref_01.doc', dst)
-#        shutil.copy(src+'proj_match_ref_02.doc', dst)
-#        shutil.copy(src+'proj_match_ref_03.doc', dst)
-#        assign_images_to_references(self.log,dict)
-#        testFileName = dict['DocFileInputAngles']
-#        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
-#        print "aaaa",goldFileName,testFileName
-#        self.assertTrue(compareTwoFiles(goldFileName,testFileName))
+
+    def test_040assign_images_to_references(self):
+        dict = {   
+                'DocFileInputAngles': 'ProjMatch/new20/Iter_01/current_angles.doc',
+                'NumberOfCtfGroups': 2L,
+                'NumberOfReferences': 3,
+                'ProjMatchRootName': [   None,
+                    'ProjMatch/new20/Iter_01/ProjMatchClasses/proj_match_ref_01.doc',
+                    'ProjMatch/new20/Iter_01/ProjMatchClasses/proj_match_ref_02.doc',
+                    'ProjMatch/new20/Iter_01/ProjMatchClasses/proj_match_ref_03.doc']}
+        #cp from goldstandard
+        src = self.src + 'Iter_01/ProjMatchClasses/'
+        dst = self.dst + 'Iter_01/ProjMatchClasses'
+        shutil.copy(src+'proj_match_ref_01.doc', dst)
+        shutil.copy(src+'proj_match_ref_02.doc', dst)
+        shutil.copy(src+'proj_match_ref_03.doc', dst)
+        assign_images_to_references(self.log,dict)
+        testFileName = dict['DocFileInputAngles']
+        goldFileName = testFileName.replace(self.WorkingDir,self.goldWorkingDir)
+        print "aaaa",goldFileName,testFileName
+        self.assertTrue(compareTwoFiles(goldFileName,testFileName))
         
 from  XmippPythonTestResult import XmippPythonTestResult
 
