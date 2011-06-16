273e260a6dce1dd057790799033c4a2fda6f87b8
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-06-16 11:27:52 +0000

    CTFCriterion_Combined removed
    git-svn-id: file:///home/roberto/Git/svn@9013 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: dbb13a7648b0d076d739bd3a69c815af21c9d8d7

--- trunk/xmipp/libraries/data/metadata_label.h
+++ trunk/xmipp/libraries/data/metadata_label.h
@@ -122,7 +122,6 @@ enum MDLabel
     MDL_CTF_CRITERION_PSDVARIANCE, ///< PSD variance
     MDL_CTF_CRITERION_PSDPCA1VARIANCE, ///< Variance in the first principal component of the PSDs
     MDL_CTF_CRITERION_PSDPCARUNSTEST, ///< Runs test on the projection of the PSD on the first principal component
-    MDL_CTF_CRITERION_COMBINED, ///< Combined criterion formed by several other criteria
     MDL_CTF_CRITERION_NORMALITY, ///< Normality test between histogram of micrography and gaussian distribution
     MDL_CTF_XRAY_DIMENSIONS, // Size in pixels of the 3D PSF to be created (Xdim, Ydim, Zdim)
     MDL_CTF_XRAY_LAMBDA, /// X-ray wavelength (nm)
@@ -583,7 +582,6 @@ private:
         MDL::addLabel(MDL_CTF_CRITERION_PSDVARIANCE, LABEL_DOUBLE, "CTFCrit_PSDStdQ");
         MDL::addLabel(MDL_CTF_CRITERION_PSDPCA1VARIANCE, LABEL_DOUBLE, "CTFCrit_PSDPCA1");
         MDL::addLabel(MDL_CTF_CRITERION_PSDPCARUNSTEST, LABEL_DOUBLE, "CTFCrit_PSDPCARuns");
-        MDL::addLabel(MDL_CTF_CRITERION_COMBINED, LABEL_DOUBLE, "CTFCrit_Combined");
         MDL::addLabel(MDL_CTF_CRITERION_NORMALITY, LABEL_DOUBLE, "CTFCrit_Normality");
 
         MDL::addLabel(MDL_CTF_XRAY_DIMENSIONS, LABEL_VECTOR, "CTF_Xray_dimensions");
