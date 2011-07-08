262d7e3c8ae97ad1495f0c55ef062054ec2f2f0b
juanjovega <juanjovega@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-07-08 10:42:15 +0000

    Added metadata browser
    git-svn-id: file:///home/roberto/Git/svn@9311 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: fbadc683e96879be22f87ea7d7337b5dd755f1e8

---
+++ trunk/xmipp/libraries/graphicsj/Xmipp_MetaData_Browser/src/XmippMetaDataBrowser.java
@@ -0,0 +1,86 @@
+
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import browser.COMMAND_PARAMETERS;
+import metadata.JFrameMetaData;
+import ij.IJ;
+import ij.Macro;
+import ij.plugin.PlugIn;
+import java.io.File;
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Options;
+
+/**
+ *
+ * @author Juanjo Vega
+ */
+public class XmippMetaDataBrowser implements PlugIn {
+
+    public void run(String args) {
+        if (IJ.isMacro() && Macro.getOptions() != null && !Macro.getOptions().trim().isEmpty()) { // From macro.
+            // "args" is used when called from another plugin or installed command.
+            // "Macro.getOptions()" used when called from a run("command", arg) macro function.
+            String FILES[] = processArgs(Macro.getOptions().trim());
+
+            if (FILES != null) {
+                for (int i = 0; i < FILES.length; i++) {
+                    if (!FILES[i].isEmpty()) {
+                        final String fileName = FILES[i];
+                        java.awt.EventQueue.invokeLater(new Runnable() {
+
+                            public void run() {
+                                openFile(fileName);
+                            }
+                        });
+                    }
+                }
+            }
+        }
+    }
+
+    public static void openFile(String fileName) {
+        try {
+            String path = "";
+            if (!fileName.startsWith(File.separator)) {
+                path = System.getProperty("user.dir") + File.separator;
+            }
+
+            path += fileName;
+
+            JFrameMetaData frameMetaData = new JFrameMetaData(path);
+            frameMetaData.setLocationRelativeTo(null);
+            frameMetaData.setVisible(true);
+        } catch (Exception ex) {
+            IJ.error(ex.getMessage());
+            throw new RuntimeException(ex);
+        }
+    }
+
+    private static String[] processArgs(String args) {
+        String argsList[] = args.split(" ");
+        Options options = new Options();
+
+        options.addOption(COMMAND_PARAMETERS.OPTION_INPUT_FILE, true, COMMAND_PARAMETERS.OPTION_INPUT_FILE_DESCRIPTION);
+
+        // It should be able to handle multiple files.
+        options.getOption(COMMAND_PARAMETERS.OPTION_INPUT_FILE).setOptionalArg(true);
+        options.getOption(COMMAND_PARAMETERS.OPTION_INPUT_FILE).setArgs(Integer.MAX_VALUE);
+
+        try {
+            BasicParser parser = new BasicParser();
+            CommandLine cmdLine = parser.parse(options, argsList);
+
+            if (cmdLine.hasOption(COMMAND_PARAMETERS.OPTION_INPUT_FILE)) {
+                return cmdLine.getOptionValues(COMMAND_PARAMETERS.OPTION_INPUT_FILE);
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+
+        return null;
+    }
+}
