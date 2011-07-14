615321797876d2ca75740156d7eeb899ed42ce37
rmarabini <rmarabini@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-07-14 11:59:20 +0000

    added selfApplyGeometry test
    git-svn-id: file:///home/roberto/Git/svn@9374 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 342e9d75f07b220e70f7359b722d9262338341d1

--- trunk/xmipp/applications/tests/test_matrix/test_matrix_main.cpp
+++ trunk/xmipp/applications/tests/test_matrix/test_matrix_main.cpp
@@ -42,7 +42,7 @@ TEST_F( MatrixTest, inverse)
     Matrix2D<double> B(3,3);
     Matrix2D<double> auxA(3,3);
     B.initIdentity();
-    EXPECT_EQ(B.inv(),B) << "MatrixTest_inverse: identity matrixix failed";
+    EXPECT_EQ(B.inv(),B) << "MatrixTest_inverse: identity matrix failed";
 
     auxA(0,0) =-0.9234482 ;
     auxA(0,1) =  0.38372311   ;
@@ -55,8 +55,20 @@ TEST_F( MatrixTest, inverse)
     auxA(2,0) =0;
     auxA(2,1) =0;
     auxA(2,2) =1 ;
-    EXPECT_EQ(auxA,A.inv()) << "MatrixTest_inverse: rotation  matrix failed";
+    EXPECT_EQ(auxA,A.inv()) << "MatrixTest_inverse: random rotation  matrix failed";
 
+    auxA(0,0) =  1.;
+    auxA(0,1) =  0.;
+    auxA(0,2) = -0.;
+
+    auxA(1,0) = -0.;
+    auxA(1,1) =  1.;
+    auxA(1,2) =  0 ;
+
+    auxA(2,0) = 0;
+    auxA(2,1) = 0;
+    auxA(2,2) = 1;
+    EXPECT_EQ(auxA,auxA.inv()) << "MatrixTest_inverse: identitity with negative 0 matrix failed";
 
 }
 
