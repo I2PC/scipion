2721bfc46441a0d87bd1f307d77f3c0a2ad31a88
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-07-21 11:52:33 +0000

    Separated and updated header for rct
    git-svn-id: file:///home/roberto/Git/svn@9438 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 9c10ec09b36c8ffc34ea0e0f4d71b27fda1d7d7d

--- trunk/xmipp/protocols/protocol_rct.py
+++ trunk/xmipp/protocols/protocol_rct.py
@@ -7,204 +7,102 @@
 #
 # Author: Sjors Scheres, March 2007
 #
-# {begin_of_header}
-#------------------------------------------------------------------------------------------------
-# {section} Global parameters
-#------------------------------------------------------------------------------------------------
-# {file} Selfile with all untilted images:
-""" This selfile points to the spider single-file format images that make up your data set. The filenames can have relative or absolute paths, but it is strictly necessary that you put this selfile IN THE PROJECTDIR. 
-"""
-UntiltedSelFile="all_images_untilted.sel"
-# {file} Selfile with all tilted images:
-""" This selfile points to the spider single-file format images that make up your data set. The filenames can have relative or absolute paths, but it is strictly necessary that you put this selfile IN THE PROJECTDIR. 
-"""
-TiltedSelFile="all_images_tilted.sel"
-# Working subdirectory:
-""" This directory will be created if it doesn't exist, and will be used to store all output from this run. Don't use the same directory for multiple different runs, instead use a structure like run1, run2 etc. 
-"""
-WorkingDir="RCT/test1"
-# Delete working subdirectory if it already exists?
-""" Just be careful with this option...
-"""
-DoDeleteWorkingDir=False
-# {expert} Root directory name for this project:
-""" Absolute path to the root directory for this project. Often, each data set of a given sample has its own ProjectDir.
-"""
-ProjectDir="/home/scheres/work/protocols/G40P"
-# {expert} Directory name for logfiles:
-""" All logfiles will be stored in $ProjectDir/$LogDir
-"""
-LogDir="Logs"
-#------------------------------------------------------------------------------------------------
-# {section} Previous ML2D classification (WITHOUT INCLUDING MIRRORS!)
-#------------------------------------------------------------------------------------------------
-# {dir} Directory of previous ML2D-classification on the untilted images
-PreviousDirML2D="ML2D/ML12ref"
-# {expert} Rootname for ML2D run
-""" Only provide something if the ml2d output rootname was different from the default.
-    This will never be the case when using the standardized protocols 
-"""
-PreviousML2DRoot=""
-# Which of these classes do you want to reconstruct? 
-""" Use comma-separated lists, with the "-" sign to indicate ranges
-"""
-SelectClasses="1,3,6-9,12"
-#------------------------------------------------------------------------------------------------
-# {section} Prepare images
-#------------------------------------------------------------------------------------------------
-# Prepare local copies of all images?
-""" This will make local copies of all untilted images and generate corresponding selfiles. This has to be done at least once.
-"""
-DoImagePreparation=True
-# Set untilted image headers?
-""" This will re-align the untilted particles and set the RCT angles correctly
-"""
-DoUntiltedHeaders=True
-# Set tilted image headers?
-""" This will center the tilted particles and set the RCT angles correctly
-"""
-DoTiltedHeaders=True
-# Maximum allowed shift for tilted particles (in pixels):
-""" Particles that shift more will be discarded. A value larger than the image size will not discard any particle
-"""
-CenterMaxShift=999
-# {expert} Additional parameters for the align_tilt_pairs program
-"""  For example:
-    -skip_stretching will skip the cosine-stretching prior to centering
-    -skip_centering  will skip the entire centering, so that only the RCT angles will be set.
-    -force_x_zero    will force the shift in the X direction to be zero, and will only center in the Y direction
-"""
-AlignTiltPairsAdditionalParams=""
-#------------------------------------------------------------------------------------------------
-# {section} Reconstruction for each of the classes
-#------------------------------------------------------------------------------------------------
-# Perform 3D-reconstructions?
-DoReconstruct=True
-# {list}(fourier,art,wbp) Reconstruction method
-""" The art method is very slow, and the relaxation parameter (-l ) needs to be carefully tuned. 
-    The wbp and fourier methods are much faster, but wbp may give significantly worse results.
-    Therefore, the fourier method is the recommended one.
-"""
-ReconstructMethod='fourier'
-# {expert} Additional reconstruction parameters
-""" For fourier see: http://xmipp.cnb.csic.es/twiki/bin/view/Xmipp/Fourier
-    For art see: http://xmipp.cnb.csic.es/twiki/bin/view/Xmipp/Art
-    For wbp see: http://xmipp.cnb.csic.es/twiki/bin/view/Xmipp/Wbp
-"""
-ReconstructAdditionalParams=""
-#------------------------------------------------------------------------------------------------
-# {section} Low-pass filter reconstructions
-#------------------------------------------------------------------------------------------------
-# Low-pass filter the reconstructed volumes?
-""" Filtering may be useful to remove noise, especially when few particles contribute to the reconstruction.
-"""
-DoLowPassFilter=True
-# Resolution of the low-pass filter (in Angstroms):
-LowPassFilter=50
-# Pixel size (in Angstroms):
-PixelSize=5.6
-#------------------------------------------------------------------------------------------------
-# {expert} Analysis of results
-""" This variable serves only for GUI-assisted visualization of the results
-"""
-AnalysisScript="visualize_rct.py"
-#------------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------------
-# {end_of_header} USUALLY YOU DO NOT NEED TO MODIFY ANYTHING BELOW THIS LINE ...
-#------------------------------------------------------------------------------------------------
-#------------------------------------------------------------------------------------------------
-#
-class RCT_class:
+from protlib_base import *
+
+class ProtRCT(XmippProtocol):
+    def __init__(self, scriptname, project):
+        XmippProtocol.__init__(self, protDict.rct.key, scriptname, project)
+        self.Import = 'from protocol_rct import *'
 
     #init variables
-    def __init__(self,
-                 UntiltedSelFile,
-                 TiltedSelFile,
-                 WorkingDir,
-                 DoDeleteWorkingDir,
-                 ProjectDir,
-                 LogDir,
-                 PreviousDirML2D,
-                 SelectClasses,
-                 DoImagePreparation,
-                 DoUntiltedHeaders,
-                 DoTiltedHeaders,
-                 CenterMaxShift,
-                 AlignTiltPairsAdditionalParams,
-                 DoReconstruct,
-                 ReconstructMethod,
-                 ReconstructAdditionalParams,
-                 DoLowPassFilter,
-                 LowPassFilter,
-                 PixelSize):
-	     
-        import os,sys,shutil
-        scriptdir=os.path.split(os.path.dirname(os.popen('which xmipp_protocols','r').read()))[0]+'/protocols'
-        sys.path.append(scriptdir) # add default search path
-        from xmipp import MetaData
-        import log
-        import utils_xmipp
-        
-        self.WorkingDir = WorkingDir
-        self.ProjectDir = ProjectDir
-        self.PreviousDirML2D = os.path.abspath(PreviousDirML2D)
-        self.PreviousML2DRoot = PreviousML2DRoot
-        self.SelectClasses = utils_xmipp.getCommaSeparatedIntegerList(SelectClasses)
-        self.CenterMaxShift = CenterMaxShift
-        self.AlignTiltPairsAdditionalParams = AlignTiltPairsAdditionalParams
-        self.ReconstructMethod = ReconstructMethod
-        self.ReconstructAdditionalParams = ReconstructAdditionalParams
-        self.LowPassFilter = LowPassFilter
-        self.PixelSize = PixelSize
-        
-        # Setup logging
-        self.log=log.init_log_system(self.ProjectDir,
-                                     LogDir,
-                                     sys.argv[0],
-                                     self.WorkingDir)
-                
-        # Delete working directory if it exists, make a new one, and go there
-        if (DoDeleteWorkingDir): 
-            if (self.WorkingDir == ""):
-                raise RuntimeError,"No working directory given"
-            if os.path.exists(self.WorkingDir):
-                shutil.rmtree(self.WorkingDir)
-        if not os.path.exists(self.WorkingDir):
-            os.makedirs(self.WorkingDir)
-
-        # Create metadatas with absolute pathnames in the WorkingDir
-        self.UntiltedSelFile = os.path.abspath(self.WorkingDir+'/'+UntiltedSelFile)
-        self.TiltedSelFile = os.path.abspath(self.WorkingDir+'/'+TiltedSelFile)
-        mysel = MetaData(UntiltedSelFile)
-        mysel2 = MetaData(TiltedSelFile)
-        mysel.write(self.UntiltedSelFile)
-        mysel2.write(self.TiltedSelFile)
-
-        # Backup script
-        log.make_backup_of_script_file(sys.argv[0],
-                                       os.path.abspath(self.WorkingDir))
-        os.chdir(self.WorkingDir)
-
-        # Always prepare the library with all filenames
-        self.prepare_name_library()
-        
-        if (DoImagePreparation):
-            self.make_local_copies()
-            
-        if (DoUntiltedHeaders):
-            self.set_headers_untilted()
-            
-        if (DoTiltedHeaders):
-            self.set_headers_tilted()
-
-        if (DoReconstruct):
-            self.execute_reconstruction()
-
-        if (DoLowPassFilter):
-            self.execute_filter()
-
-        # Return to parent dir
-        os.chdir(os.pardir)
+#    def __init__(self,
+#                 UntiltedSelFile,
+#                 TiltedSelFile,
+#                 WorkingDir,
+#                 DoDeleteWorkingDir,
+#                 ProjectDir,
+#                 LogDir,
+#                 PreviousDirML2D,
+#                 SelectClasses,
+#                 DoImagePreparation,
+#                 DoUntiltedHeaders,
+#                 DoTiltedHeaders,
+#                 CenterMaxShift,
+#                 AlignTiltPairsAdditionalParams,
+#                 DoReconstruct,
+#                 ReconstructMethod,
+#                 ReconstructAdditionalParams,
+#                 DoLowPassFilter,
+#                 LowPassFilter,
+#                 PixelSize):
+#	     
+#        import os,sys,shutil
+#        scriptdir=os.path.split(os.path.dirname(os.popen('which xmipp_protocols','r').read()))[0]+'/protocols'
+#        sys.path.append(scriptdir) # add default search path
+#        from xmipp import MetaData
+#        import log
+#        import utils_xmipp
+#        
+#        self.WorkingDir = WorkingDir
+#        self.ProjectDir = ProjectDir
+#        self.PreviousDirML2D = os.path.abspath(PreviousDirML2D)
+#        self.PreviousML2DRoot = PreviousML2DRoot
+#        self.SelectClasses = utils_xmipp.getCommaSeparatedIntegerList(SelectClasses)
+#        self.CenterMaxShift = CenterMaxShift
+#        self.AlignTiltPairsAdditionalParams = AlignTiltPairsAdditionalParams
+#        self.ReconstructMethod = ReconstructMethod
+#        self.ReconstructAdditionalParams = ReconstructAdditionalParams
+#        self.LowPassFilter = LowPassFilter
+#        self.PixelSize = PixelSize
+#        
+#        # Setup logging
+#        self.log=log.init_log_system(self.ProjectDir,
+#                                     LogDir,
+#                                     sys.argv[0],
+#                                     self.WorkingDir)
+#                
+#        # Delete working directory if it exists, make a new one, and go there
+#        if (DoDeleteWorkingDir): 
+#            if (self.WorkingDir == ""):
+#                raise RuntimeError,"No working directory given"
+#            if os.path.exists(self.WorkingDir):
+#                shutil.rmtree(self.WorkingDir)
+#        if not os.path.exists(self.WorkingDir):
+#            os.makedirs(self.WorkingDir)
+#
+#        # Create metadatas with absolute pathnames in the WorkingDir
+#        self.UntiltedSelFile = os.path.abspath(self.WorkingDir+'/'+UntiltedSelFile)
+#        self.TiltedSelFile = os.path.abspath(self.WorkingDir+'/'+TiltedSelFile)
+#        mysel = MetaData(UntiltedSelFile)
+#        mysel2 = MetaData(TiltedSelFile)
+#        mysel.write(self.UntiltedSelFile)
+#        mysel2.write(self.TiltedSelFile)
+#
+#        # Backup script
+#        log.make_backup_of_script_file(sys.argv[0],
+#                                       os.path.abspath(self.WorkingDir))
+#        os.chdir(self.WorkingDir)
+#
+#        # Always prepare the library with all filenames
+#        self.prepare_name_library()
+#        
+#        if (DoImagePreparation):
+#            self.make_local_copies()
+#            
+#        if (DoUntiltedHeaders):
+#            self.set_headers_untilted()
+#            
+#        if (DoTiltedHeaders):
+#            self.set_headers_tilted()
+#
+#        if (DoReconstruct):
+#            self.execute_reconstruction()
+#
+#        if (DoLowPassFilter):
+#            self.execute_filter()
+#
+#        # Return to parent dir
+#        os.chdir(os.pardir)
 
 
     # Make a libray of untilted selfiles and averages for all selected classes
@@ -405,33 +303,4 @@ class RCT_class:
         message='Done!'
         print '*',message
         print '*********************************************************************'
-#		
-# Main
-#     
-if __name__ == '__main__':
-
-    # create ML2D_class object
-
-    RCT=RCT_class(UntiltedSelFile,
-                  TiltedSelFile,
-                  WorkingDir,
-                  DoDeleteWorkingDir,
-                  ProjectDir,
-                  LogDir,
-                  PreviousDirML2D,
-                  SelectClasses,
-                  DoImagePreparation,
-                  DoUntiltedHeaders,
-                  DoTiltedHeaders,
-                  CenterMaxShift,
-                  AlignTiltPairsAdditionalParams,
-                  DoReconstruct,
-                  ReconstructMethod,
-                  ReconstructAdditionalParams,
-                  DoLowPassFilter,
-                  LowPassFilter,
-                  PixelSize)
-
-    # close 
-    RCT.close()
 
---
+++ trunk/xmipp/protocols/protocol_rct_header.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+#------------------------------------------------------------------------------------------------
+# Protocol for random conical tilt reconstruction
+#
+# Example use:
+# ./xmipp_protocol_rct.py
+#
+# Author: Sjors Scheres, March 2007
+#
+# {begin_of_header}
+
+#------------------------------------------------------------------------------------------
+# {section}{has_question} Comment
+#------------------------------------------------------------------------------------------
+# Display comment
+DisplayComment = False
+
+# {text} Write a comment:
+""" 
+Describe your run here...
+"""
+#-----------------------------------------------------------------------------
+# {section} Global parameters
+#-----------------------------------------------------------------------------
+# Run name:
+""" This will identify your protocol run. It need to be unique for each protocol. You could have run1, run2 for protocol X, but not two
+run1 for it. This name together with the protocol output folder will determine the working dir for this run.
+"""
+RunName = "run_001"
+
+# {file} Selfile with all untilted images:
+""" This selfile points to the spider single-file format images that make up your data set. The filenames can have relative or absolute paths, but it is strictly necessary that you put this selfile IN THE PROJECTDIR. 
+"""
+UntiltedSelFile="all_images_untilted.sel"
+# {file} Selfile with all tilted images:
+""" This selfile points to the spider single-file format images that make up your data set. The filenames can have relative or absolute paths, but it is strictly necessary that you put this selfile IN THE PROJECTDIR. 
+"""
+TiltedSelFile="all_images_tilted.sel"
+
+#------------------------------------------------------------------------------------------------
+# {section} Previous ML2D classification (WITHOUT INCLUDING MIRRORS!)
+#------------------------------------------------------------------------------------------------
+# {dir} Classification directory
+"""Directory of previous ML2D-classification on the untilted images"""
+PreviousDirML2D="ML2D/ML12ref"
+# {expert} Rootname for ML2D run
+""" Only provide something if the ml2d output rootname was different from the default.
+    This will never be the case when using the standardized protocols 
+"""
+PreviousML2DRoot=""
+# Which of these classes do you want to reconstruct? 
+""" Use comma-separated lists, with the "-" sign to indicate ranges
+"""
+SelectClasses="1,3,6-9,12"
+#------------------------------------------------------------------------------------------------
+# {section}{has_question}Prepare images
+#------------------------------------------------------------------------------------------------
+# Prepare local copies of all images?
+""" This will make local copies of all untilted images and generate corresponding selfiles. This has to be done at least once.
+"""
+DoImagePreparation=True
+# Set untilted image headers?
+""" This will re-align the untilted particles and set the RCT angles correctly
+"""
+DoUntiltedHeaders=True
+# Set tilted image headers?
+""" This will center the tilted particles and set the RCT angles correctly
+"""
+DoTiltedHeaders=True
+# Maximum allowed shift for tilted particles (in pixels):
+""" Particles that shift more will be discarded. A value larger than the image size will not discard any particle
+"""
+CenterMaxShift=999
+# {expert} Additional parameters for the align_tilt_pairs program
+"""  For example:
+    -skip_stretching will skip the cosine-stretching prior to centering
+    -skip_centering  will skip the entire centering, so that only the RCT angles will be set.
+    -force_x_zero    will force the shift in the X direction to be zero, and will only center in the Y direction
+"""
+AlignTiltPairsAdditionalParams=""
+#------------------------------------------------------------------------------------------------
+# {section}{has_question} Reconstruction
+#------------------------------------------------------------------------------------------------
+# Reconstruct 3D-classes
+DoReconstruct=True
+# {list}(fourier,art,wbp) Reconstruction method
+""" The art method is very slow, and the relaxation parameter (-l ) needs to be carefully tuned. 
+    The wbp and fourier methods are much faster, but wbp may give significantly worse results.
+    Therefore, the fourier method is the recommended one.
+"""
+ReconstructMethod='fourier'
+# {expert} Additional reconstruction parameters
+""" For fourier see: http://xmipp.cnb.csic.es/twiki/bin/view/Xmipp/Fourier
+    For art see: http://xmipp.cnb.csic.es/twiki/bin/view/Xmipp/Art
+    For wbp see: http://xmipp.cnb.csic.es/twiki/bin/view/Xmipp/Wbp
+"""
+ReconstructAdditionalParams=""
+#------------------------------------------------------------------------------------------------
+# {section}{has_question} Low-pass filter 
+#------------------------------------------------------------------------------------------------
+# Filter reconstructed volumes
+""" Filtering may be useful to remove noise, especially when few particles contribute to the reconstruction.
+"""
+DoLowPassFilter=True
+# Resolution of the low-pass filter (in Angstroms):
+LowPassFilter=50
+# Pixel size (in Angstroms):
+PixelSize=5.6
+
+#------------------------------------------------------------------------------------------------
+# {end_of_header} USUALLY YOU DO NOT NEED TO MODIFY ANYTHING BELOW THIS LINE ...
+#------------------------------------------------------------------------------------------------
+
+from protocol_rct import *
+#		
+# Main
+#     
+if __name__ == '__main__':
+    protocolMain(ProtRCT)
