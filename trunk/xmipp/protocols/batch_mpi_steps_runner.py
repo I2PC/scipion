cc3815a73a922bcc3f36a396be70bc41763a0418
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-08-03 18:25:39 +0000

    aded the script for step gaps runner
    git-svn-id: file:///home/roberto/Git/svn@9561 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 4b5db524b0d66157e04cf7638dc88e944fe90060

---
+++ trunk/xmipp/protocols/batch_mpi_steps_runner.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+'''
+/***************************************************************************
+ * Authors:     J.M. de la Rosa Trevin (jmdelarosa@cnb.csic.es)
+ *
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.csic.es'
+ ***************************************************************************/
+ '''
+
+import sys
+from protlib_base import XmippProject, getProtocolFromModule
+from protlib_sql import runStepGaps
+
+if __name__ == '__main__':
+    script  = sys.argv[1]
+    project = XmippProject()
+    project.load()
+    protocol = getProtocolFromModule(script, project)
+    #Create database and other output files
+    protocol.runSetup()
+    runStepGaps(protocol.Db)
+    
+    
