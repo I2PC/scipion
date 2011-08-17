e7038c5c452997a49bf45a8db993d6014eba2b83
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-08-17 09:00:15 +0000

    changed to new program interface
    git-svn-id: file:///home/roberto/Git/svn@9633 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 5080dc7fef8efe54eb17ab90cae1454102d1b02d

--- trunk/xmipp/applications/scripts/metadata_selfile_create/batch_metadata_selfile_create.py
+++ trunk/xmipp/applications/scripts/metadata_selfile_create/batch_metadata_selfile_create.py
@@ -24,74 +24,59 @@
  *  e-mail address 'xmipp@cnb.csic.es'
  ***************************************************************************/
 """
-import os, glob, sys, optparse
-   
-def command_line_options():
-      """ add command line options here"""
-      _usage="""usage: %prog [options]
-Example:
-   xmipp_metadata_selfile_create -p "Images/*xmp" -o all_images.sel -isstack"""
-      parser = optparse.OptionParser(_usage)
-      parser.add_option("-p", "--pattern",  dest="pattern", type="string",
-            help="The pattern to match")        
-      parser.add_option("-o", "--outMetaDataFile", dest="outMetaDataFile",
-            default="", type="string",
-            help="MetaData output file name")
-      parser.add_option("-s", "--isstack",
-                  action="store_true", dest="isStack", default=False,
-                  help="Check if the images are stacks")
-      parser.add_option("-q", "--quiet",
-                  action="store_true", dest="quiet", default=False,
-                  help="Do not show any messages")
+#!/usr/bin/env python
+
+import os
+from protlib_xmipp import XmippScript
+
+class ScriptCreateMetadata(XmippScript):
+    def __init__(self):
+        XmippScript.__init__(self)
+        
+    def defineParams(self):
+        self.addUsageLine('Create a metadata from a file pattern.')
+        ## params
+        self.addParamsLine(' -p <pattern>          : Pattern to match')
+        self.addParamsLine('   alias --pattern;')
+        self.addParamsLine(' -o <metadata>         : Output metadata')
+        self.addParamsLine('[ -s]                  : Check if the images are stacks')
+        self.addParamsLine('   alias --isstack;')    
+        ## examples
+        self.addExampleLine('   xmipp_metadata_selfile_create -p "Images/*xmp" -o all_images.sel -isstack')
             
-      (options, args)      = parser.parse_args()
-      if(len(options.outMetaDataFile)<1):
-          parser.print_help()
-          sys.exit()
-      
-      if not options.quiet:
-          print 'Files pattern:        ', options.pattern
-          print 'Output Metadata File: ', options.outMetaDataFile
-          print 'Is stack:             ', options.isStack
+    def run(self):
+        pattern = self.getParam('--pattern')
+        isStack = self.checkParam('-s')
+        import glob
+        files = glob.glob(pattern)
+        files.sort()
     
-      return(options.pattern, 
-             options.outMetaDataFile,
-             options.isStack,
-             options.quiet)
+        from xmipp import MetaData, FileName, SingleImgSize, MDL_IMAGE, MDL_ENABLED
+        mD = MetaData()
+        inFile = FileName()
+        
+        nSize = 1
+        for file in files:
+            fileAux=file
+            if isStack:
+                if file.endswith(".mrc"):
+                    fileAux=file+":mrcs"
+                x, x, x, nSize = SingleImgSize(fileAux)
+            if nSize != 1:
+                counter = 1
+                for jj in range(nSize):
+                    inFile.compose(counter, fileAux)
+                    objId = mD.addObject()
+                    mD.setValue(MDL_IMAGE, inFile, objId)
+                    mD.setValue(MDL_ENABLED, 1, objId)
+                    counter += 1
+            else:
+                objId = mD.addObject()
+                mD.setValue(MDL_IMAGE, fileAux, objId)
+                mD.setValue(MDL_ENABLED, 1, objId)
+                
+        outFile = self.getParam('-o')
+        mD.write(outFile)
 
-
-
-scriptdir = os.path.split(os.path.dirname(os.popen('which xmipp_protocols', 'r').read()))[0] + '/lib'
-sys.path.append(scriptdir) # add default search path
-
-from xmipp import *
-
-pattern, outFile, isStack, quiet = command_line_options();
-
-files = glob.glob(pattern)
-files.sort()
-
-mD = MetaData()
-inFile = FileName()
-
-nSize = 1
-for file in files:
-    fileAux=file
-    if isStack:
-        if file.endswith(".mrc"):
-            fileAux=file+":mrcs"
-        x, x, x, nSize = SingleImgSize(fileAux)
-    if nSize != 1:
-        counter = 1
-        for jj in range(nSize):
-            inFile.compose(counter, fileAux)
-            objId = mD.addObject()
-            mD.setValue(MDL_IMAGE, inFile, objId)
-            mD.setValue(MDL_ENABLED, 1, objId)
-            counter = counter + 1
-    else:
-        objId=mD.addObject()
-        mD.setValue(MDL_IMAGE, fileAux, objId)
-        mD.setValue(MDL_ENABLED, 1, objId)
-
-mD.write(outFile)
+if __name__ == '__main__':
+    ScriptCreateMetadata().tryRun()
