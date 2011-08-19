4e0c2f9d956dbd687e7d82905e9d4e86548fb6e2
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-08-19 14:13:36 +0000

    runProgram exported to Java
    git-svn-id: file:///home/roberto/Git/svn@9710 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 8da8f7ef3fddfebf4bff993d6f41fe0fe49a4c51

---
+++ trunk/xmipp/libraries/bindings/java/xmipp/Program.java
@@ -0,0 +1,10 @@
+package xmipp;
+
+import java.io.File;
+
+/**
+ * Protocol for integrating native C++ code - @see ImageDouble.java
+ */
+public class Program {
+    public static native int runByName(String progName, String args) throws Exception;
+}
