3ee810e3e9438e9a74528d666bcf9f041d259ace
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-08-23 13:09:06 +0000

    Interface done
    git-svn-id: file:///home/roberto/Git/svn@9793 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: e566d46cb2255fb1d2e63ff713850b391e717bb7

--- trunk/xmipp/protocols/protocol_preprocess_particles_header.py
+++ trunk/xmipp/protocols/protocol_preprocess_particles_header.py
@@ -2,17 +2,6 @@
 #------------------------------------------------------------------------------------------------
 #
 # General script for Xmipp-based pre-processing of single-particles: 
-#  - phase flipping
-#  - extraction of particles
-#  - normalization
-#  - sort_junk
-#
-# It is assumed that you have already ran the preprocess_micrographs protocol,
-#  and that you have picked the particles for each micrograph
-#
-# Example use:
-# ./xmipp_preprocess_particles.py
-#
 # Author: Carlos Oscar, August 2011
 #
 # {begin_of_header}
@@ -39,53 +28,67 @@ RunName = "run_001"
 # {list}(Resume, Restart) Run behavior
 """ Resume from the last step, restart the whole process
 """
-Behavior = "Resume"
+Behavior = "Restart"
 
 #-----------------------------------------------------------------------------
-# {section} Preprocessing parameters
+# {section} Filter parameters
 #-----------------------------------------------------------------------------
-# {run}(particle_pick) Particle picking run
-PickingRun=''
+# {run}(extract_particles) Particle extraction run
+ExtractionRun=''
 
-# Take Logarithm
-""" Depending on your acquisition system you may have to take the logarithm
-    or not in order to have a linear relationship between the gray values
-    in the image and those in the volume """
-DoLog=False 
+# {wizard}(wizardChooseFamily) Family to preprocess
+Family=''
 
-# Phase flipping (Recommended)
-""" Use the information from the CTF to compensate for phase reversals."""
-DoFlip=True
+# Fourier bandpass filter
+""" You may do a lowpass filter by setting Freq_low to 0. You may do a high pass filter
+    by setting Freq_high to 0.5."""
+DoFourier=False 
 
-# Invert contrast
-""" Invert the contrast if your particles are black over a white background. """
-DoInvert=False
+#{condition}(DoFourier) Freq_low (0<f<0.5)
+""" Set it to 0 for low pass filters """
+Freq_low=0.02
 
-# Normalize (Recommended)
-""" It subtract a ramp in the gray values and normalizes so that in the background
-    there is 0 mean and standard deviation 1 """
-DoNorm=True
+#{condition}(DoFourier) Freq_high (0<f<0.5)
+""" Set it to 0.5 for high pass filters """
+Freq_low=0.35
 
-# {expert} Background radius
-"""Pixels outside this circle are assumed to be noise and their stddev is set to 1.
-   Radius for background circle definition (in pix.).
-   If this value is 0, then the same as the particle radius is used. """
-BackGroundRadius=0
+# Fourier Gaussian
+""" Gaussian filter defined in Fourier space"""
+DoGaussian=False
 
-# Perform dust particles removal?
+#{condition}(DoGaussian) Frequency sigma
+""" Remind that the Fourier frequency is normalized between 0 and 0.5"""
+Freq_sigma=0.04
+
+# Remove dust
 """ Sets pixels with unusually large values to random values from a Gaussian with zero-mean and unity-standard deviation.
     It requires a previous normalization, i.e., Normalization must be set to Yes.
 """
-DoRemoveDust=True
+DoRemoveDust=False
 
-# {expert} Threshold for dust removal:
+# {condition}(DoRemoveDust)  Threshold for dust removal:
 """ Pixels with a signal higher or lower than this value times the standard deviation of the image will be affected. For cryo, 3.5 is a good value.
     For high-contrast negative stain, the signal itself may be affected so that a higher value may be preferable.
 """
 DustRemovalThreshold=3.5
 
+# Normalize
+""" It subtract a ramp in the gray values and normalizes so that in the background
+    there is 0 mean and standard deviation 1 """
+DoNorm=False
+
+# {list_combo}(OldXmipp,NewXmipp,Ramp){condition}(DoNorm) Normalization type
+""" OldXmipp (mean(Image)=0, stddev(Image)=1). NewXmipp (mean(background)=0, stddev(background)=1), Ramp (subtract background+NewXmipp)"""
+NormType="Ramp"
+
+# {condition}(DoNorm) Background radius
+"""Pixels outside this circle are assumed to be noise and their stddev is set to 1.
+   Radius for background circle definition (in pix.).
+   If this value is 0, then the same as the particle radius is used. """
+BackGroundRadius=0
+
 #------------------------------------------------------------------------------------------
-# {section} Parallelization issues
+# {section} Parallelization
 #------------------------------------------------------------------------------------------
 # Number of MPI processes
 """ Set to 1 if you do not have MPI installed"""
@@ -96,12 +99,12 @@ NumberOfMpi = 8
 """
 SubmitToQueue = False
 
-# {condition}(SubmitToQueue=True) Queue name
+# {condition}(SubmitToQueue) Queue name
 """Name of the queue to submit the job
 """
 QueueName = "default"
 
-# {condition}(SubmitToQueue=True) Queue hours
+# {condition}(SubmitToQueue) Queue hours
 """This establish a maximum number of hours the job will
 be running, after that time it will be killed by the
 queue system
