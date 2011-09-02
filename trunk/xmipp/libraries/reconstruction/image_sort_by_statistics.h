3bda7145708807fd46337101c1135d0b4953d244
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-09-02 22:25:04 +0000

    The program has been moved into the library
    git-svn-id: file:///home/roberto/Git/svn@9948 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 0787252579a6de3dbc25ab706983eaa021d506be

---
+++ trunk/xmipp/libraries/reconstruction/image_sort_by_statistics.h
@@ -0,0 +1,60 @@
+/***************************************************************************
+ * Authors:     Carlos Oscar Sorzano (coss@cnb.csic.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.csic.es'
+ ***************************************************************************/
+
+#include <data/xmipp_program.h>
+
+#ifndef _PROG_IMAGE_SORT
+#define _PROG_IMAGE_SORT
+
+#include <data/xmipp_program.h>
+#include <data/basic_pca.h>
+#include <data/histogram.h>
+
+/**@defgroup ImageSort Image sort by statistics
+   @ingroup ReconsLibrary */
+//@{
+class ProgSortByStatistics: public XmippProgram
+{
+public:
+    FileName fn, fn_out, fn_train;
+    bool multivariate,addToInput;
+
+public:
+    double cutoff;
+    MultidimArray<double> vavg, vstddev, Zscore, ZscoreMultivariate;
+    PCAMahalanobisAnalyzer pcaAnalyzer;
+    MetaData SF, SFtrain;
+
+public:
+    void readParams();
+
+    void defineParams();
+
+    void process_selfile(MetaData &SF, bool do_prepare, bool multivariate);
+
+    void run();
+};
+//@}
+#endif
+
