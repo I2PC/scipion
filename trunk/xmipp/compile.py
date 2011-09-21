81eaec8f479d4e47726b430d784294dc541621ac
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-09-21 21:21:51 +0000

    Ãadded configure/compile script
    git-svn-id: file:///home/roberto/Git/svn@10161 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 893d4f547d9fde1d67d76cce51f253ffae9ede54

---
+++ trunk/xmipp/compile.py
@@ -0,0 +1,240 @@
+#!bin/xmipp_python
+'''
+/***************************************************************************
+ * Authors:     J.M. de la Rosa Trevin (jmdelarosa@cnb.csic.es)
+ *
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.csic.es'
+ ***************************************************************************/
+ '''
+
+from Tkinter import *
+import tkFont as font
+import ttk
+import os
+from subprocess import Popen
+
+from protlib_gui_ext import FilePollTextArea, centerWindows
+    
+class OptionsTab(Frame):
+    def __init__(self, master, text, **opts):
+        Frame.__init__(self, master)
+        self.config(**opts)
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=3)
+        self.lastRow = 0
+        self.options = []
+        self.optionsDict = {}
+        style = ttk.Style()
+        style.configure("Name.TLabel", foreground="red")
+        self.normal = font.Font(family='Helvetica', size=10)
+        self.bold = font.Font(family='Helvetica', size=10, weight='bold')
+        
+    def addOption(self, name, comment, default=''):
+        r = self.lastRow
+        var = StringVar()
+        var.set(default)
+        ttk.Label(self, text=comment, font=self.normal).\
+        grid(column=0, row=r, padx=5, pady=5, sticky=E)
+        ttk.Label(self, text=name, font=self.bold).\
+        grid(column=1, row=r, padx=5, pady=5, sticky=E)
+        if default in ['yes', 'no']: # Boolean option
+            w = ttk.Checkbutton(self, textvariable=var, variable=var, 
+                            onvalue='yes', offvalue='no',
+                            command=lambda:self.checked(name, var))
+            w.grid(column=2, row=r, padx=5, pady=5, sticky=W)
+        else:
+            w = ttk.Entry(self, width=20, textvariable=var)
+            w.grid(column=2, row=r, sticky=(W, E), padx=5, pady=5)
+        self.options.append((name, default, var, w))
+        self.optionsDict[name] = var
+        self.lastRow += 1
+        
+    def setValue(self, name, value):
+        self.optionsDict[name].set(value)
+        
+    def getValue(self, name, value):
+        return self.optionsDict[name].get()
+        
+    def checked(self, name, var):
+        value = var.get()
+        if value == 'yes':
+            state = 'normal'
+        else:
+            state = 'disabled'
+        name = name.lower()
+        for n, d, v, w in self.options:
+            if v != var and n.lower().startswith(name):
+                w['state'] = state
+                
+    def addSeparator(self):
+        ttk.Separator(self, orient=HORIZONTAL).\
+        grid(column=0, row=self.lastRow, columnspan=3, 
+             sticky=(W,E), padx=10, pady=5)
+        self.lastRow += 1
+        
+    def getConfigOptions(self):
+        optStr = ""
+        for n, d, v, w in self.options:
+            if v.get() != d:
+                optStr += ' %s="%s"' % (n, v.get())
+        return optStr
+        
+class ConfigNotebook(ttk.Notebook):
+    def __init__(self, master):
+        ttk.Notebook.__init__(self, master)
+        self.tabs = []
+        
+    def addTab(self, text):
+        tab = OptionsTab(self, text)
+        self.add(tab, text=text)
+        self.tabs.append(tab)
+        return tab
+    
+    def getConfigOptions(self):
+        '''return string with configs settings '''
+        return ' '.join([t.getConfigOptions() for t in self.tabs])
+
+
+def detectJava():
+    from subprocess import Popen, PIPE
+    cmd = "readlink -f `which javac`"
+    ps = Popen(cmd, shell=True, stdout=PIPE)
+    out, err = ps.communicate()
+    if err:
+        return ''
+    else:
+        import os
+        dirname = os.path.dirname        
+        java_home = dirname(dirname(out.strip()))
+        return java_home
+    
+def detectMpi():
+    pass
+
+def detectQt():
+    pass
+
+root = Tk()
+root.withdraw()
+root.title("Xmipp Install")
+root.minsize(width=600, height=350)
+
+nb = ConfigNotebook(root)
+
+tab = nb.addTab("Compiler")
+tab.addOption('CC', 'The C compiler', 'gcc')
+tab.addOption('CXX', 'The C++ compiler', 'g++')
+tab.addOption('LINKERFORPROGRAMS', 'Linker for programs', 'g++')
+tab.addOption('CCFLAGS', 'The C compiler flags', '')
+tab.addOption('CXXFLAGS', 'The C++ compiler flags', '')
+
+tab = nb.addTab("  MPI  ")
+tab.addOption('mpi', 'Build the MPI programs?', 'no')
+tab.addOption('MPI_CC', 'MPI C compiler', 'mpicc')
+tab.addOption('MPI_CXX', 'MPI C++ compiler', 'mpiCC')
+tab.addOption('MPI_LINKERFORPROGRAMS', 'MPI Linker for programs', 'mpiCC')
+tab.addOption('MPI_INCLUDE', 'MPI headers dir ', '/usr/include')
+tab.addOption('MPI_LIBDIR', 'MPI libraries dir ', '/usr/lib')
+tab.addOption('MPI_LIB', 'MPI library', 'mpi')
+
+tab = nb.addTab("Java & QT")
+tab.addOption('java', 'Build the java programs?', 'no')
+tab.addOption('JAVAC', 'Java compiler', 'javac')
+tab.addOption('JAVA_HOME', 'Java installation directory', '')
+java_home = detectJava()
+tab.setValue('JAVA_HOME', java_home)
+tab.addSeparator()
+tab.addOption('qt', 'Build the GUI (qt) programs?', 'yes')
+tab.addOption('QTDIR', 'Where is QT installed', '/usr/share/qt3')
+tab.addOption('QT_LIB', 'QT library to use', 'qt-mt')
+tab.addOption('QT4', 'Use Qt4 instead of Qt3?', 'no')
+tabJava = tab
+
+tab = nb.addTab("Advanced")
+tab.addOption('debug', 'Build debug version?', 'no')
+tab.addOption('profile', 'Build profile version?', 'no')
+tab.addOption('warn', 'Show warnings?', 'no')
+tab.addOption('fast', 'Fast?', 'no')
+tab.addOption('static', 'Prevent dynamic linking?', 'no')
+tab.addOption('prepend', 'What to prepend to executable names', 'xmipp')
+
+tab = nb.addTab("Configure")
+
+output = 'build/scons_output.log'
+if os.path.exists(output):
+    os.remove(output)
+text = FilePollTextArea(tab, output, 20, 80)
+text.goEnd()
+text.grid(column=0, row=0, sticky=(N, S, E, W), padx=10, pady=10)
+
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=5)
+root.rowconfigure(0, weight=1)
+leftFrame = ttk.Frame(root)
+img = PhotoImage(file='resources/xmipp_logo.gif')
+label = ttk.Label(leftFrame, image=img)
+label['image'] = img
+label.grid(column=0, row=0)
+leftFrame.grid(column=0, row=0, sticky=(N, S), padx=5, pady=5, rowspan=2)
+nb.grid(column=1, row=0, sticky=(N,W,E,S), padx=5, pady=5)
+btn = ttk.Button(root, text='Compile')
+btn.grid(column=1, row=1, padx=5, pady=5, sticky=E)
+proc = None
+
+def checkProcess():
+    if proc.poll() is not None:
+        text.stopRefresh()
+        text.fillTextArea(goEnd=True)
+        btn.config(command=runCompile, text="Compile")
+    else:
+        root.after(3000, checkProcess)
+    
+def runCompile(event=None):
+    out = output
+    opts = nb.getConfigOptions()
+    from protlib_utils import cyanStr, greenStr
+    msg1 = cyanStr("RUNNING SCONS.CONFIGURE...")     
+    cmd = 'echo -e "%(msg1)s" > %(out)s \n'
+    cmd1 = "./scons.configure %(opts)s >> %(out)s 2>&1 \n" % locals()
+    cmd += cmd1
+    msg2 = cyanStr("RUNNING SCONS.COMPILE...")
+    cmd += 'echo "%(msg2)s" >> %(out)s \n'
+    cmd2 = "./scons.compile >> %(out)s 2>&1 " % locals()
+    cmd += cmd2
+    #os.system(cmd % locals())
+    global proc
+    os.environ['JAVA_HOME'] = javaTab.getValue('JAVA_HOME')
+    proc = Popen(cmd % locals(), shell=True)    
+    text.fillTextArea()
+    text.doRefresh(3)
+    checkProcess()    
+    btn.config(command=stopCompile, text="Stop")
+    
+def stopCompile(event=None):
+    proc.terminate()
+    btn.config(command=runCompile, text="Compile")
+    
+btn.config(command=runCompile)
+centerWindows(root)
+root.deiconify()
+root.mainloop()
