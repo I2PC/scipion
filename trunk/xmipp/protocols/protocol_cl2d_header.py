336ac99e548ed0f52d86c0bd1bc23b45eda1d1bc
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2011-10-10 13:42:05 +0000

    added included
    git-svn-id: file:///home/roberto/Git/svn@10301 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 6e5807a7f4d3ab904a7f72668ffd26b5289c158f

--- trunk/xmipp/protocols/protocol_cl2d_header.py
+++ trunk/xmipp/protocols/protocol_cl2d_header.py
@@ -11,30 +11,7 @@
 
 # {begin_of_header}
 
-#------------------------------------------------------------------------------------------
-# {section}{has_question} Comment
-#------------------------------------------------------------------------------------------
-# Display comment
-DisplayComment = False
-
-# {text} Write a comment:
-""" 
-Describe your run here...
-"""
-
-#-----------------------------------------------------------------------------
-# {section} Run parameters
-#-----------------------------------------------------------------------------
-# Run name:
-""" This will identify your protocol run. It need to be unique for each protocol. You could have run1, run2 for protocol X, but not two
-run1 for it. This name together with the protocol output folder will determine the working dir for this run.
-"""
-RunName = "run_001"
-
-# {list}(Resume, Restart) Run behavior
-""" Resume from the last step, restart the whole process
-"""
-Behavior = "Restart"
+# {include} inc_comment_run.py
 
 #------------------------------------------------------------------------------------------------
 # {section} CL2D parameters
