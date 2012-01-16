f7342b013c5b00e2c78cd2daeb1a67e0572b920d
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-01-16 16:09:07 +0000

    updated java code
    git-svn-id: file:///home/roberto/Git/svn@10853 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: da31417e6d6d2c96f91ec94824bc00c3576e29f7

--- trunk/xmipp/applications/tests/test_java/xmipp/ImageGeneric_Test.java
+++ trunk/xmipp/applications/tests/test_java/xmipp/ImageGeneric_Test.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package xmipp;
+package xmipp.jni;
 
 import java.io.File;
 import org.junit.After;
