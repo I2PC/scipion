372691a7b422cc809c2f235aeacc954cebb718d5
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-01-16 09:07:54 +0000

    Moved all java code under 'java/src'
    git-svn-id: file:///home/roberto/Git/svn@10841 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: bc17148086e35636a1e497a34fcad3596e07f2d8

--- trunk/xmipp/libraries/bindings/java/src/xmipp/Program.java
+++
@@ -1,9 +0,0 @@
-package xmipp;
-
-/**
- * Protocol for integrating native C++ code - @see ImageDouble.java
- */
-public class Program {
-    public static native int runByName(String progName, String args) throws Exception;
-    public static native String getXmippPath();
-}
