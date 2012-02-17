3a6b0a67ac581ad1d6d50ca3c1fb1385f46cff27
airenzp <airenzp@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-02-17 07:09:43 +0000

    renaming WindowUtils to WindowUtil
    git-svn-id: file:///home/roberto/Git/svn@11108 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 23d3e729685bbf9b71024e0ec2baef451cdeb35d

--- trunk/xmipp/java/src/xmipp/utils/WindowUtil.java
+++ trunk/xmipp/java/src/xmipp/utils/WindowUtil.java
@@ -3,35 +3,20 @@ package xmipp.utils;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
-import java.awt.Point;
 import java.awt.Rectangle;
 
 
-public class WindowUtils {
-
-	/** This function will be used to place the location of 
-	 * a windows relative to another windows or screen
-	 */
-	private static void setLocation(float positionx, float positiony, Container w, Dimension dim, Point offset) {
-		Rectangle abounds = w.getBounds();
-		int x = (int) (positionx * (dim.width - abounds.width) + offset.x);
-		int y = (int) (positiony * (dim.height - abounds.height) + offset.y);
-		w.setLocation(x, y);
-	}
+public class WindowUtil {
 
 	public static void setLocation(float positionx, float positiony, Container w) {
-		setLocation(positionx, positiony, w, w.getToolkit().getScreenSize(), new Point(0, 0));
+		Dimension dim = w.getToolkit().getScreenSize();
+		Rectangle abounds = w.getBounds();
+		int x = (int) (positionx * (dim.width - abounds.width));
+		int y = (int) (positiony * (dim.height - abounds.height));
+		w.setLocation(x, y);
+
 	}
 	
-	/** Center the component in the screen */
-	public static void centerWindows(Container w){
-		setLocation(0.5f, 0.5f, w);		
-	}
-	
-	/** Center the component relative to parent component */
-	public static void centerWindows(Container w, Container parent){
-		setLocation(0.5f, 0.5f, w, parent.getSize(), parent.getLocation());		
-	}	
 
 	public static GridBagConstraints getConstraints(
 			GridBagConstraints constraints, int x, int y, int columns) {
