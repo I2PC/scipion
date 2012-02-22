a95646e13c4875d3e607eaca679a20950562fe9a
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-02-22 09:35:38 +0000

    git-svn-id: file:///home/roberto/Git/svn@11180 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    Former-commit-id: afd9e042de982c558866df2cb1d3ddd7cb1377c7

--- trunk/xmipp/applications/scripts/emx_convert/batch_emx_convert.py
+++ trunk/xmipp/applications/scripts/emx_convert/batch_emx_convert.py
@@ -36,18 +36,17 @@ class ScriptEmxConverter(XmippScript):
     def defineParams(self):
         self.addUsageLine('Plot some values from metadata.')
         ## params
-        self.addParamsLine(' -i <filename=/dev/stdin>          : Input file')
+        self.addParamsLine(' -i <filename="/dev/stdin">          : Input file')
         self.addParamsLine('   alias --input_filename;')
-        self.addParamsLine(' -o <filename=/dev/stdout>         : Output file')
+        self.addParamsLine(' -o <filename="/dev/stdout">         : Output file')
         self.addParamsLine('   alias --output_filename;')
-        self.addParamsLine(' -t <mode=coordinates>             : Conversion type')
+        self.addParamsLine(' -t <mode="coordinates">             : Conversion type')
         self.addParamsLine('     where <mode> coordinates alignment class ctf')
         self.addParamsLine('   alias --conversion_type;')
 
         ## examples
-        self.addExampleLine('Simple plot of label "sigmaNoise" from metadata', False)
-        self.addExampleLine('xmipp_metadata_plot -i results.xmd -y sigmaNoise')
-        self.addExampleLine('Additionally take values for X from other label and set title', False)
+        self.addExampleLine('convert to and from EMX format for data interchange', False)
+        self.addExampleLine('cat Test/particlePicking.emx | ./xmipp_emx_convert.py')
         
     def run(self):   
         inputFn = self.getParam('-i')
