31213f7392a74e97b641ee3864bf8101fef892e0
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-02-23 10:23:45 +0000

    organizing code
    git-svn-id: file:///home/roberto/Git/svn@11204 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 5e8e892f9333584aefb25bbfeb9ab3ce72a0203c

--- trunk/xmipp/java/src/xmipp/viewer/ImageOperations.java
+++ trunk/xmipp/java/src/xmipp/viewer/ImageOperations.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package xmipp.viewer.gallery;
+package xmipp.viewer;
 
 import xmipp.viewer.imageitems.tableitems.AbstractGalleryImageItem;
 import ij.ImagePlus;
