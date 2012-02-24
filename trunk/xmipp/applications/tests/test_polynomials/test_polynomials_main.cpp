b96c9da9eecfc7642182d8c90ae017172d48a346
javiervargas <javiervargas@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-02-24 09:25:05 +0000

    Xmipp Zernike polynomial test
    git-svn-id: file:///home/roberto/Git/svn@11211 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 36feed0317b3e0c4de5377224d0f5eb57ecaa26c

---
+++ trunk/xmipp/applications/tests/test_polynomials/test_polynomials_main.cpp
@@ -0,0 +1,76 @@
+#include <data/xmipp_polynomials.h>
+#include <data/xmipp_image.h>
+#include <data/matrix1d.h>
+#include <data/xmipp_fft.h>
+#include <data/matrix2d.h>
+#include <iostream>
+#include "../../../external/gtest-1.6.0/fused-src/gtest/gtest.h"
+// MORE INFO HERE: http://code.google.com/p/googletest/wiki/AdvancedGuide
+// This test is named "Size", and belongs to the "MetadataTest"
+// test case.
+class PolynomialsTest : public ::testing::Test
+{
+protected:
+    //init metadatas
+    virtual void SetUp()
+    {
+#define len 128
+
+        //matlabCoeffs
+        matlabCoeffs.resizeNoCopy(10);
+        VEC_ELEM(matlabCoeffs,0) =   1.2366;
+        VEC_ELEM(matlabCoeffs,1) =  -0.00002;
+        VEC_ELEM(matlabCoeffs,2) =  -0.00002;
+        VEC_ELEM(matlabCoeffs,3) =  -0.1813;
+        VEC_ELEM(matlabCoeffs,4) =  -2.0455;
+        VEC_ELEM(matlabCoeffs,5) =  -0.0383;
+        VEC_ELEM(matlabCoeffs,6) =  -0.00005;
+        VEC_ELEM(matlabCoeffs,7) =  -0.00005;
+        VEC_ELEM(matlabCoeffs,8) =  -0.00005;
+        VEC_ELEM(matlabCoeffs,9) =  0.00005;
+
+        xmippCoeffs.resizeNoCopy(10);
+        xmippCoeffs.initConstant(1);
+
+        //get example down1_42_Periodogramavg.psd
+        baseName = getenv("XMIPP_HOME");
+        imageName = baseName + "/applications/tests/test_polynomials/down1_42_Periodogramavg.psd";
+        im.read(imageName);
+    }
+
+    // coefficients obtained using Matlab
+    Matrix1D<double> matlabCoeffs;
+    // coefficients obtained using Matlab
+    Matrix1D<double> xmippCoeffs;
+    //Image to be fitted:
+    Image<double> im;
+    //File name of the image to process
+    FileName imageName;
+    FileName baseName;
+};
+
+
+TEST_F( PolynomialsTest, ZERNIKE)
+{
+    CenterFFT(MULTIDIM_ARRAY(im), true);
+	PolyZernikes polynom;
+	Matrix1D<int> coefs(10);
+	coefs.initConstant(1);
+
+	polynom.fit(coefs,MULTIDIM_ARRAY(im));
+	xmippCoeffs = COEFFICIENTS(polynom);
+
+    Matrix1D<double> error = xmippCoeffs - matlabCoeffs;
+
+    for(int i =0; i<  VEC_XSIZE(error); i++)
+    {
+    	ASSERT_TRUE(std::abs(VEC_ELEM(error,i))<0.01) << "Zernike fit: no correspondence between matlab and xmipp zernike coefficients";;
+    }
+
+}
+
+GTEST_API_ int main(int argc, char **argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
