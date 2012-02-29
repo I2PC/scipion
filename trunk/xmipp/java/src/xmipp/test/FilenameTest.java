d392eef366d80c84f9b35ce8c7c20c167d489fdc
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-02-29 09:49:54 +0000

    added new test for filename, cleanned the imagegeneric test
    git-svn-id: file:///home/roberto/Git/svn@11285 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 4a6907ae714fac74c0412f8e618faf36a37bc9a3

---
+++ trunk/xmipp/java/src/xmipp/test/FilenameTest.java
@@ -0,0 +1,85 @@
+/***************************************************************************
+ * Authors:     J.M. de la Rosa Trevin (jmdelarosa@cnb.csic.es)
+ * 				Roberto Marabini       (roberto@cnb.csic.es)
+ *
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.csic.es'
+ ***************************************************************************/
+package xmipp.test;
+
+ import java.io.File;
+ import org.junit.After;
+ import org.junit.AfterClass;
+ import org.junit.Before;
+ import org.junit.BeforeClass;
+ import org.junit.Test;
+
+ import xmipp.jni.Filename;
+ import xmipp.jni.ImageGeneric;
+ import xmipp.jni.MetaData;
+ import static org.junit.Assert.*;
+ import java.util.Arrays;
+
+/**
+ *
+ * @author Juanjo Vega
+ */
+public class FilenameTest {
+
+    // Xmipp dir
+    static String TESTS_PATH;
+    
+    String filename  = "singleImage.spi";
+    String filename2 = "singleImage2.spi";
+
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {        
+    }
+
+
+    @After
+    public void tearDown() {        
+    }
+
+    /**
+     * Test of getFilename method, of class ImageGeneric.
+     */
+    @Test
+    public void testGetXmippPath() {
+            try {
+                String xmipp_home = Filename.getXmippPath();
+                String xmipp_home2 = System.getenv("XMIPP_HOME");
+                assertEquals(xmipp_home, xmipp_home2);
+            } catch (Exception ex) {
+                fail("testGetFilename");
+            }
+    }//function testGetXmippPath
+
+}//class FilenameTest
