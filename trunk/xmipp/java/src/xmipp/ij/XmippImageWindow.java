293023dd9630f5d603df3f742b2912dac30f3546
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-03-09 08:36:03 +0000

    compilation error fixed
    git-svn-id: file:///home/roberto/Git/svn@11419 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: aa297ab61307dba70f9dfd0b79a2a393276d4012

--- trunk/xmipp/java/src/xmipp/ij/XmippImageWindow.java
+++ trunk/xmipp/java/src/xmipp/ij/XmippImageWindow.java
@@ -12,82 +12,74 @@ import java.io.File;
 import xmipp.jni.Filename;
 import xmipp.utils.XmippDialog;
 
+public class XmippImageWindow extends ImageWindow implements XmippIJWindow {
 
-public class XmippImageWindow extends ImageWindow implements XmippIJWindow
-{
-	
-	
+	public static void main(String[] args) {
+		try {
+			// openImageJ(Tool.VIEWER);
+			XmippImageWindow w = new XmippImageWindow(
+					"/home/airen/Coss/Xmipp/BPV_2/InputData/BPV_1386.mrc");
 
-	public static void main(String[] args)
-	{
-		try
-		{
-			//openImageJ(Tool.VIEWER);
-			XmippImageWindow w = new XmippImageWindow("/home/airen/Coss/Xmipp/BPV_2/InputData/BPV_1386.mrc");
-			
-		}
-		catch (Exception e)
-		{
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	private String file;
-	
-	public XmippImageWindow(String file) throws Exception
-	{
+
+	public XmippImageWindow(String file) throws Exception {
 		this(XmippImageConverter.loadImage(file), file);
 		this.file = file;
 	}
-	
+
 	public XmippImageWindow(ImagePlus imp) {
 		this(imp, "");
 	}
-	
-	public XmippImageWindow(ImagePlus imp, String title)
-	{
+
+	public XmippImageWindow(ImagePlus imp, String title) {
 		super(imp, new XmippImageCanvas(imp));
-		//file = imp.getOriginalFileInfo().directory + File.separator + imp.getOriginalFileInfo().fileName;
+		// file = imp.getOriginalFileInfo().directory + File.separator +
+		// imp.getOriginalFileInfo().fileName;
 		setTitle(title);
 		setMenuBar(new XmippMenuBar(this));
 	}
-	
-	public static void openImageJ(Tool tool){
-		if (IJ.getInstance() == null)
-		{
+
+	public static void openImageJ(Tool tool) {
+		if (IJ.getInstance() == null) {
 			new ImageJ();
-			IJ.run("Install...", "install=" + Filename.getXmippPath("java/src/xmipp/ij/XmippMacros.txt"));
-		}		
+			IJ.run("Install...",
+					"install="
+							+ Filename
+									.getXmippPath("java/src/xmipp/ij/XmippMacros.txt"));
+		}
 		boolean recognized = IJ.setTool(Tool.getTool(tool));
-		//System.out.println(recognized);
+		// System.out.println(recognized);
 	}
 
 	@Override
-	public void loadData()
-	{
-		if (file != null){
-			try{
-		ImagePlus imp = XmippImageConverter.loadImage(file);
-		setImage(imp);//second alone does not work
-		updateImage(imp);//first one alone does not work
+	public void loadData() {
+		try {
+			if (file != null) {
+				ImagePlus imp = XmippImageConverter.loadImage(file);
+				setImage(imp);// second alone does not work
+				updateImage(imp);// first one alone does not work
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	
-
 	@Override
-	public void saveDataAs(String file) throws Exception
-	{
+	public void saveDataAs(String file) throws Exception {
 		this.file = file;
 		XmippImageConverter.writeImagePlus(imp, file);
 	}
 
 	@Override
-	public void saveData() throws Exception
-	{
+	public void saveData() throws Exception {
 		System.out.println(imp.getTitle());
 		saveDataAs(imp.getTitle());
-		
 	}
-}//class XmippImageWindow
+}// class XmippImageWindow
