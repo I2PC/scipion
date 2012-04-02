85bf66287fee6c91eabc8023fa053323292a4112
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-04-02 10:48:14 +0000

    Mode added
    git-svn-id: file:///home/roberto/Git/svn@11826 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 8a352d4723b9afc3298fbb8fb3d27b2b42f7192c

--- trunk/xmipp/applications/scripts/micrograph_tiltpair_picking/batch_micrograph_tiltpair_picking.py
+++ trunk/xmipp/applications/scripts/micrograph_tiltpair_picking/batch_micrograph_tiltpair_picking.py
@@ -12,12 +12,17 @@ class ScriptTiltPairPicking(ScriptAppIJ):
     def defineOtherParams(self):
         self.addParamsLine(" -o <directory>                                       : Output directory for load/save session data without updating model.");
         self.addParamsLine("    alias --output;                                   ");
+        self.addParamsLine(" --mode <pick_mode=manual>                            : Mode in wich Particle Picker will be used");
+        self.addParamsLine("    where <pick_mode>");
+        self.addParamsLine("       manual                                         : Enables manual mode. User will pick particles manually.");
+        self.addParamsLine("       readonly                                       : Enables readonly mode. User can see the picked particles, but cannot modify them.");
         
     def readOtherParams(self):
         input = self.getParam('-i')
         output = self.getParam('-o')
+        mode = self.getParam('--mode')
 
-        self.args = "%(input)s %(output)s" % locals()
+        self.args = "%(input)s %(output)s %(mode)s" % locals()
         
         
 if __name__ == '__main__':
