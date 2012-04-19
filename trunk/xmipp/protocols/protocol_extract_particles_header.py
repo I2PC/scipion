0382f732e07711606fa0b64b6a60d970b1265b05
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-04-19 19:31:57 +0000

    DownsampleFactor added; Labels reorganized according to processing order
    git-svn-id: file:///home/roberto/Git/svn@12049 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: dbd1876d3156948b50cb62a973cb44ebb93f221b

--- trunk/xmipp/protocols/protocol_extract_particles_header.py
+++ trunk/xmipp/protocols/protocol_extract_particles_header.py
@@ -25,37 +25,34 @@ PickingRun=''
 # {wizard}(wizardChooseFamilyToExtract) Family 
 Family=''
 
+# Extraction downsampling factor
+""" This factor is always referred to the original sampling rate.
+You may use independent downsampling factors for extracting the particles, picking them
+and estimating the CTF. All downsampling factors are always referred to the original
+sampling rate, and the differences are correctly handled by Xmipp.
+"""
+DownsampleFactor=1
+
 # Particle box size
 """ In pixels. The box size is the size of the boxed particles,
 actual particles may be smaller than this. """
 ParticleSize=0
 
-# Take Logarithm
+# 1) Phase flipping (Recommended)
+""" Use the information from the CTF to compensate for phase reversals."""
+DoFlip=True
+
+# 2) Take Logarithm
 """ Depending on your acquisition system you may have to take the logarithm
     or not in order to have a linear relationship between the gray values
     in the image and those in the volume """
 DoLog=False 
 
-# Phase flipping (Recommended)
-""" Use the information from the CTF to compensate for phase reversals."""
-DoFlip=True
-
-# Invert contrast
+# 3) Invert contrast
 """ Invert the contrast if your particles are black over a white background. """
 DoInvert=False
 
-# Normalize (Recommended)
-""" It subtract a ramp in the gray values and normalizes so that in the background
-    there is 0 mean and standard deviation 1 """
-DoNorm=True
-
-# {expert} Background radius
-"""Pixels outside this circle are assumed to be noise and their stddev is set to 1.
-   Radius for background circle definition (in pix.).
-   If this value is 0, then half the box size is used. """
-BackGroundRadius=0
-
-# Dust particles removal
+# 4) Dust particles removal
 """ Sets pixels with unusually large values to random values from a Gaussian with zero-mean and unity-standard deviation.
     It requires a previous normalization, i.e., Normalization must be set to Yes.
 """
@@ -66,6 +63,17 @@ DoRemoveDust=False
     For high-contrast negative stain, the signal itself may be affected so that a higher value may be preferable.
 """
 DustRemovalThreshold=3.5
+
+# 5) Normalize (Recommended)
+""" It subtract a ramp in the gray values and normalizes so that in the background
+    there is 0 mean and standard deviation 1 """
+DoNorm=True
+
+# {expert} Background radius
+"""Pixels outside this circle are assumed to be noise and their stddev is set to 1.
+   Radius for background circle definition (in pix.).
+   If this value is 0, then half the box size is used. """
+BackGroundRadius=0
 
 # {eval} expandParallel(threads=0, hours=12, mpi=8)
 #
