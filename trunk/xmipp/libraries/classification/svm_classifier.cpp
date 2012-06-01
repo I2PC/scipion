219588c308775023d68a9e345199bed6ba285cd5
vahidabrishami <vahidabrishami@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-06-01 15:32:31 +0000

    Class for SVM Classifier. Need to be more completed!
    git-svn-id: file:///home/roberto/Git/svn@12427 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 2c3573c57583e218e70a59b638408eacf91191ad

---
+++ trunk/xmipp/libraries/classification/svm_classifier.cpp
@@ -0,0 +1,70 @@
+/***************************************************************************
+ *
+ * Authors:     Enrique Recarte Llorens   (erecallo@hotmail.com)
+ *              Carlos Oscar S. Sorzano   (coss@cnb.csic.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or   
+ * (at your option) any later version.                                 
+ *                                                                     
+ * This program is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of      
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the       
+ * GNU General Public License for more details.                        
+ *                                                                     
+ * You should have received a copy of the GNU General Public License   
+ * along with this program; if not, write to the Free Software         
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA            
+ * 02111-1307  USA                                                     
+ *                                                                     
+ *  All comments concerning this program package may be sent to the    
+ *  e-mail address 'xmipp@cnb.csic.es'                                  
+ ***************************************************************************/
+#include "svm_classifier.h"
+
+SVMClassifier::SVMClassifier()
+{
+    param.svm_type = C_SVC;
+    param.kernel_type = RBF;
+    param.degree = 3;
+    param.gamma = 0;
+    param.coef0 = 0;
+    param.nu = 0.5;
+    param.cache_size = 100;
+    param.C = 1;
+    param.eps = 1e-3;
+    param.p = 0.1;
+    param.shrinking = 1;
+    param.probability = 0;
+    param.nr_weight = 0;
+    param.weight_label = NULL;
+    param.weight = NULL;
+}
+void SVMClassifier::SVMTrain(MultidimArray<double> &trainSet,MultidimArray<int> &lable)
+{
+    prob.l = YSIZE(trainSet);
+    prob.y = new double[prob.l];
+    prob.x = new svm_node *[prob.l];
+    for (int i=0;i<YSIZE(trainSet);i++)
+    {
+        prob.x[i]=new svm_node[YSIZE(trainSet)];
+        int cnt = 0;
+        for (int j=0;j<XSIZE(trainSet);j++)
+        {
+            if (trainSet(i,j)==0)
+                continue;
+            else
+            {
+                prob.x[i][cnt].value=DIRECT_A2D_ELEM(trainSet,i,j);
+                prob.x[i][cnt].index=j+1;
+                cnt++;
+            }
+        }
+        prob.x[i][cnt].index=-1;
+        prob.y[i] = DIRECT_A1D_ELEM(lable,i);
+    }
+    model=svm_train(&prob, &param);
+}
