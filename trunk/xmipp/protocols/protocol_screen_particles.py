f376a360bc61928f06248316c51f3d04033e742b
coss <coss@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-06-14 17:34:30 +0000

    The protocol is now prepared to screen stacks of particles
    git-svn-id: file:///home/roberto/Git/svn@12535 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 63ab2deec8bfbf2fc12371d6585eee3191ef9834

--- trunk/xmipp/protocols/protocol_screen_particles.py
+++ trunk/xmipp/protocols/protocol_screen_particles.py
@@ -19,28 +19,46 @@ class ProtScreenParticles(XmippProtocol):
     def __init__(self, scriptname, project):
         XmippProtocol.__init__(self, protDict.screen_particles.name, scriptname, project)
         self.Import = 'from protocol_screen_particles import *'
-        self.outputFile=self.workingDirPath(os.path.basename(self.InputFile))
+        ext=os.path.splitext(self.InputFile)[1]
+        self.addToSelf=True;
+        if ext==".xmd" or ext==".sel": 
+            self.outputFile=self.workingDirPath(os.path.basename(self.InputFile))
+        else:
+            baseFile=os.path.splitext(os.path.basename(self.InputFile))[0]
+            self.outputFile=self.workingDirPath(baseFile+".xmd")
+            self.addToSelf=False;
 
     def defineSteps(self):
-        self.Db.insertStep("copyFile",verifyfiles=[self.outputFile],source=self.InputFile,dest=self.outputFile)
         self.Db.insertStep("linkAcquisitionInfoIfPresent",InputFile=self.InputFile,dirDest=self.WorkingDir)
-        self.Db.insertStep('sortImages',inputFile=self.outputFile)
+        if self.addToSelf:
+            self.Db.insertStep("copyFile",verifyfiles=[self.outputFile],source=self.InputFile,dest=self.outputFile)
+        self.Db.insertStep('sortImages',inputFile=self.InputFile,outputFile=self.outputFile,addToSelf=self.addToSelf)
                 
     def summary(self):
         message=[]
         message.append("Screening of [%s]" % self.InputFile)
         return message
+    
+    def validate(self):
+        errors=[]
+        ext=os.path.splitext(self.InputFile)[1]
+        if (ext!=".xmd" and ext!=".sel" and ext!=".stk" and ext!=".mrc" and ext!=".hed" and ext!=".img"):
+            errors.append("This protocol is designed for stacks")
+        return errors
 
     def visualize(self):
         if not os.path.exists(self.outputFile):
             from protlib_gui_ext import showWarning
-            showWarning("Error", "There is no result yet")
+            showWarning("Error", "There is no result yet",self.master)
         else:   
             runShowJ(self.outputFile)                                     
 
-def sortImages(log, inputFile):
+def sortImages(log, inputFile, outputFile, addToSelf):
     from xmipp import ImgSize
     (Xdim, Ydim, Zdim, Ndim) = ImgSize(inputFile)
     if Ndim > 0:
-        runJob(log,"xmipp_image_sort_by_statistics","-i "+inputFile+" --multivariate --addToInput")
+        if addToSelf: 
+            runJob(log,"xmipp_image_sort_by_statistics","-i "+outputFile+" --multivariate --addToInput")
+        else:
+            runJob(log,"xmipp_image_sort_by_statistics","-i "+inputFile+" --multivariate -o "+outputFile)
 
