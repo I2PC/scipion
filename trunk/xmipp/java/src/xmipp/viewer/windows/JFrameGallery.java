ffb4e9a93d6ca5724de39a021a5566f6283b7858
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-07-09 11:50:29 +0000

    using psd enhanced on recalculate CTF
    git-svn-id: file:///home/roberto/Git/svn@12643 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: c33061740e4605831c713b9aee10cfedb8e4ac34

--- trunk/xmipp/java/src/xmipp/viewer/windows/JFrameGallery.java
+++ trunk/xmipp/java/src/xmipp/viewer/windows/JFrameGallery.java
@@ -1363,7 +1363,7 @@ public class JFrameGallery extends JFrame implements iCTFGUI {
 				String ctfModel = data.md.getValueString(MDLabel.MDL_CTF_MODEL,
 						data.ids[row]);
 				String displayFilename = data.md.getValueString(
-						MDLabel.MDL_IMAGE2, data.ids[row]);
+						MDLabel.MDL_PSD_ENHANCED, data.ids[row]);
 				String psdFile = data.md.getValueString(MDLabel.MDL_PSD,
 						data.ids[row]);
 
