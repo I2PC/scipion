0aba5fc3b7f356a9624baec924c6faf3c41822ba
delarosatrevin <delarosatrevin@1a721e8d-d38c-4e91-ab98-6a0be4d754af>
2012-08-01 13:14:56 +0000

    After a day of work, the python module is splitted and organized in several .cpp and .h files....
    git-svn-id: file:///home/roberto/Git/svn@12782 1a721e8d-d38c-4e91-ab98-6a0be4d754af
    
    Former-commit-id: 46c7cf456ed3734556c55403478ce81723d7b09a

---
+++ trunk/xmipp/libraries/bindings/python/python_symmetry.cpp
@@ -0,0 +1,169 @@
+/***************************************************************************
+ *
+ * Authors:     J.M. De la Rosa Trevin (jmdelarosa@cnb.csic.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.csic.es'
+ ***************************************************************************/
+
+#include "xmippmodule.h"
+
+/***************************************************************/
+/*                            SymList                          */
+/***************************************************************/
+
+/* Constructor */
+PyObject *
+SymList_new(PyTypeObject *type, PyObject *args, PyObject *kwargs)
+{
+    SymListObject *self = (SymListObject*) type->tp_alloc(type, 0);
+    if (self != NULL)
+    {
+        self->symlist = new SymList();
+        //self->symlist->readSymmetryFile("i3");
+    }
+    return (PyObject *) self;
+}
+
+/* Destructor */
+ void SymList_dealloc(SymListObject* self)
+{
+    delete self->symlist;
+    self->ob_type->tp_free((PyObject*) self);
+}
+
+/* readSymmetryFile */
+PyObject *
+SymList_readSymmetryFile(PyObject * obj, PyObject *args, PyObject *kwargs)
+{
+    char *str = "";
+    if (PyArg_ParseTuple(args, "s", &str))
+    {
+        try
+        {
+            SymListObject *self = (SymListObject*) obj;
+            self->symlist->readSymmetryFile(str);
+            Py_RETURN_NONE;
+        }
+        catch (XmippError &xe)
+        {
+            PyErr_SetString(PyXmippError, xe.msg.c_str());
+        }
+    }
+    return NULL;
+}
+/* computeDistance */
+PyObject *
+SymList_computeDistance(PyObject * obj, PyObject *args, PyObject *kwargs)
+{
+
+    PyObject *pyMd = NULL;
+    PyObject *pyProjdirMode = NULL;
+    PyObject *pyCheckMirrors = NULL;
+    PyObject *pyObjectRotation = NULL;
+
+    if (PyArg_ParseTuple(args, "O|OOO", &pyMd,
+                         &pyProjdirMode,
+                         &pyCheckMirrors,
+                         &pyObjectRotation))
+    {
+        if (!MetaData_Check(pyMd))
+            PyErr_SetString(PyExc_TypeError,
+                            "Expected MetaData as first argument");
+        try
+        {
+            bool projdir_mode    = false;
+            bool check_mirrors   = false;
+            bool object_rotation = false;
+            if (PyBool_Check(pyProjdirMode))
+                projdir_mode = (pyProjdirMode == Py_True);
+            if (PyBool_Check(pyCheckMirrors))
+                check_mirrors = (pyCheckMirrors == Py_True);
+            if (PyBool_Check(pyObjectRotation))
+                object_rotation = (pyObjectRotation == Py_True);
+            SymListObject *self = (SymListObject*) obj;
+            self->symlist->computeDistance(MetaData_Value(pyMd),projdir_mode,
+                                                                check_mirrors,
+                                                                object_rotation);
+            Py_RETURN_NONE;
+        }
+        catch (XmippError &xe)
+        {
+            PyErr_SetString(PyXmippError, xe.msg.c_str());
+        }
+    }
+    return NULL;
+}
+
+
+/* SymList methods */
+PyMethodDef SymList_methods[] =
+{
+   { "readSymmetryFile", (PyCFunction) SymList_readSymmetryFile,
+     METH_VARARGS, "read symmetry file" },
+   { "computeDistance", (PyCFunction) SymList_computeDistance,
+       METH_VARARGS, "compute angular distance in a metadata" },
+   { NULL } /* Sentinel */
+};//SymList_methods
+
+/*SymList Type */
+PyTypeObject SymListType =
+{
+    PyObject_HEAD_INIT(NULL)
+    0, /*ob_size*/
+    "xmipp.SymList", /*tp_name*/
+    sizeof(SymListObject), /*tp_basicsize*/
+    0, /*tp_itemsize*/
+    (destructor)SymList_dealloc, /*tp_dealloc*/
+    0, /*tp_print*/
+    0, /*tp_getattr*/
+    0, /*tp_setattr*/
+    0, /*tp_compare*/
+    0, /*tp_repr*/
+    0, /*tp_as_number*/
+    0, /*tp_as_sequence*/
+    0, /*tp_as_mapping*/
+    0, /*tp_hash */
+    0, /*tp_call*/
+    0, /*tp_str*/
+    0, /*tp_getattro*/
+    0, /*tp_setattro*/
+    0, /*tp_as_buffer*/
+    Py_TPFLAGS_DEFAULT, /*tp_flags*/
+    "Python wrapper to Xmipp SymList class",/* tp_doc */
+    0, /* tp_traverse */
+    0, /* tp_clear */
+    0, /* tp_richcompare */
+    0, /* tp_weaklistoffset */
+    0, /* tp_iter */
+    0, /* tp_iternext */
+    SymList_methods, /* tp_methods */
+    0, /* tp_members */
+    0, /* tp_getset */
+    0, /* tp_base */
+    0, /* tp_dict */
+    0, /* tp_descr_get */
+    0, /* tp_descr_set */
+    0, /* tp_dictoffset */
+    0, /* tp_init */
+    0, /* tp_alloc */
+    SymList_new, /* tp_new */
+};//SymListType
+
---
+++ trunk/xmipp/libraries/bindings/python/xmippmodule.h
@@ -0,0 +1,144 @@
+/***************************************************************************
+ *
+ * Authors:     J.M. De la Rosa Trevin (jmdelarosa@cnb.csic.es)
+ *              Roberto Marabini       (roberto@cnb.csic.es)
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.csic.es'
+ ***************************************************************************/
+
+#ifndef _XMIPPMODULE_H
+#define _XMIPPMODULE_H
+
+#include "Python.h"
+#include "numpy/ndarraytypes.h"
+#include "numpy/ndarrayobject.h"
+
+#include <data/xmipp_program.h>
+#include <data/metadata_extension.h>
+#include <data/xmipp_image_generic.h>
+#include <data/xmipp_image_extension.h>
+#include <data/xmipp_color.h>
+#include <data/symmetries.h>
+#include <reconstruction/ctf_estimate_from_micrograph.h>
+
+#include "python_filename.h"
+#include "python_image.h"
+#include "python_program.h"
+#include "python_metadata.h"
+#include "python_symmetry.h"
+
+extern PyObject * PyXmippError;
+
+#define SymList_Check(v) (((v)->ob_type == &SymListType))
+#define SymList_Value(v)  ((*((SymListObject*)(v))->symlist))
+
+
+/***************************************************************/
+/*                            Global methods                   */
+/***************************************************************/
+PyObject *
+xmipp_str2Label(PyObject *obj, PyObject *args);
+
+PyObject *
+xmipp_label2Str(PyObject *obj, PyObject *args);
+
+PyObject *
+xmipp_colorStr(PyObject *obj, PyObject *args);
+
+PyObject *
+xmipp_labelType(PyObject *obj, PyObject *args);
+
+/* isInStack */
+PyObject *
+xmipp_isValidLabel(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+/* createEmptyFile */
+PyObject *
+xmipp_createEmptyFile(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+/* SingleImgSize */
+PyObject *
+xmipp_SingleImgSize(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+/* ImgSize (from metadata filename)*/
+PyObject *
+xmipp_ImgSize(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+PyObject *
+xmipp_CheckImageFileSize(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+PyObject *
+xmipp_CheckImageCorners(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+PyObject *
+xmipp_ImgCompare(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+PyObject *
+xmipp_compareTwoFiles(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+
+/***************************************************************/
+/*                   Some specific utility functions           */
+/***************************************************************/
+/* readMetaDataWithTwoPossibleImages */
+PyObject *
+xmipp_readMetaDataWithTwoPossibleImages(PyObject *obj, PyObject *args,
+                                        PyObject *kwargs);
+
+/* substituteOriginalImages */
+PyObject *
+xmipp_substituteOriginalImages(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+bool validateInputImageString(PyObject * pyImage, PyObject *pyStrFn, FileName &fn);
+
+PyObject *
+xmipp_compareTwoMetadataFiles(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+/* calculate enhanced psd and return preview*/
+PyObject *
+xmipp_fastEstimateEnhancedPSD(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+/* calculate enhanced psd and return preview
+* used for protocol preprocess_particles*/
+PyObject *
+xmipp_bandPassFilter(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+/* calculate enhanced psd and return preview
+ * used for protocol preprocess_particles*/
+PyObject *
+xmipp_gaussianFilter(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+/* calculate enhanced psd and return preview
+ * used for protocol preprocess_particles*/
+PyObject *
+xmipp_badPixelFilter(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+/* dump metadatas to database*/
+PyObject *
+xmipp_dumpToFile(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+PyObject *
+xmipp_Euler_angles2matrix(PyObject *obj, PyObject *args, PyObject *kwargs);
+
+void addIntConstant(PyObject * dict, const char * name, const long &value);
+void addLabels(PyObject * dict);
+
+#endif
