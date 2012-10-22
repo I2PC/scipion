1d607f880d496bef1d60527f158526fb478f7cf0
vahid <vabrishami@cnb.csic.es>
2012-10-22 11:04:07 +0200

    save data method has been removed.
    Former-commit-id: 7c9d64a29fa220fbb16a855d7506a31fc4e3e54a

--- java/src/xmipp/particlepicker/training/gui/TrainingPickerJFrame.java
+++ java/src/xmipp/particlepicker/training/gui/TrainingPickerJFrame.java
@@ -472,7 +472,10 @@ public class TrainingPickerJFrame extends ParticlePickerJFrame {
 		thresholdpn
 				.setVisible(getFamilyData().getState() == MicrographFamilyState.Correct);
 		pack();
-		saveChanges();// Saving changes when switching micrographs, by
+		//FIXME: Here should be saved only the current micrograph
+		// before the change, because saving all of them and all particles
+		// could be very very slow....
+		//saveChanges();// Saving changes when switching micrographs, by
 						// Coss suggestion
 		if (particlesdialog != null)
 			loadParticles();
