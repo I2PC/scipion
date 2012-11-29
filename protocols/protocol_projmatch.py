651eec852164f79efbeaa1baf788a05559b7dae7
roberto <roberto@cnb.csic.es>
2012-11-29 18:50:58 +0100

    iinitialize dims
    Former-commit-id: a17e20a27ee637487b43fc7c4744499b90f5f18a

--- protocols/protocol_projmatch.py
+++ protocols/protocol_projmatch.py
@@ -67,6 +67,9 @@ class ProtProjMatch(XmippProtocol):
             md = MetaData(acquisionInfo)
             self.ResolSam = md.getValue(MDL_SAMPLINGRATE, md.firstObject())
         
+        if self.MaskRadius == -1:
+           (Xdim, Ydim, Zdim, Ndim) = SingleImgSize(self.ReferenceFileNames[0])
+           self.MaskRadius = Xdim/2
     def validate(self):
         from protlib_xmipp import validateInputSize
         errors = []
@@ -428,7 +431,7 @@ data_
                     mdReferencesSize = mdReferences.size()
                     for id in mdReferences:
                         convert_refno_to_stack_position[mdReferences.getValue(MDL_NEIGHBOR,id)]=id
-                    file_nameImages = self.getFilename('DocfileInputAnglesIters', iter=it)
+                    file_nameImages = "ctfGroup[0-9][0-9][0-9][0-9][0-9][0-9]@"+self.getFilename('DocfileInputAnglesIters', iter=it)
                     if xmippExists(file_nameImages):
                         #print "OutClassesXmd", OutClassesXmd
                         MDtmp.read(file_nameImages)#query with ref3D
