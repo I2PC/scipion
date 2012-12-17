f2f7e3444e6b693e0848cfb2bb50c5cabe76771d
Airen Zaldivar Peraza <airenzp@gmail.com>
2012-12-17 12:21:31 +0100

    hiding action in manual
    Former-commit-id: 9be689ae25f9aeb75c5808b4ddc85c07ec83a1d8

--- java/src/xmipp/particlepicker/training/model/MicrographFamilyData.java
+++ java/src/xmipp/particlepicker/training/model/MicrographFamilyData.java
@@ -224,7 +224,7 @@ public class MicrographFamilyData
 
 	public String getAction()
 	{
-		if (family.getStep() == FamilyState.Manual)
+		if (family.getStep() != FamilyState.Supervised)
 			return null;
 		if (state == MicrographFamilyState.Manual)
 			return null;
