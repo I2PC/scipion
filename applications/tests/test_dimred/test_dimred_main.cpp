2473be71c5837d72b3caf7a64b924b1806f7c1e1
Carlos Oscar <coss@cnb.csic.es>
2013-01-15 21:52:24 +0100

    Helix test added
    Former-commit-id: 187fbbd5952b6c35bae0c3deeadd6830954be4b2

--- applications/tests/test_dimred/test_dimred_main.cpp
+++ applications/tests/test_dimred/test_dimred_main.cpp
@@ -24,7 +24,14 @@ TEST_F( DimRedTest, generate_data)
 	expectedX.read("dimred/swiss.txt");
 	ASSERT_TRUE(expectedX.equal(generator.X,1e-5));
 
-	//
+	// Helix
+	generator.generateNewDataset("helix",1000,0);
+	generator.X.write("dimred/helix.txt");
+	// load helix.txt; plot3(helix(:,1),helix(:,2),helix(:,3),'.');
+	expectedX.read("dimred/helix.txt");
+	ASSERT_TRUE(expectedX.equal(generator.X,1e-5));
+
+
 }
 
 GTEST_API_ int main(int argc, char **argv)
