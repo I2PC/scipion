29eef9c4f4c06af33cf8f0e9bba4e08d9d955aa0
Airen Zaldivar Peraza <airenzp@gmail.com>
2013-02-11 18:04:47 +0100

    displaying file path on metadata title
    Former-commit-id: dff2b34bc846d1e4cc6e90953ed4e3bcd519776d

--- java/src/xmipp/viewer/models/MetadataGalleryTableModel.java
+++ java/src/xmipp/viewer/models/MetadataGalleryTableModel.java
@@ -204,7 +204,7 @@ public class MetadataGalleryTableModel extends ImageGalleryTableModel {
 	@Override
 	public String getTitle() {
 		String title = "Metadata: "
-				+ (filename != null ? Filename.getBaseName(filename) : "");
+				+ (filename != null ? filename : "");
 		if (n > 1)
 			title += String.format(" %d items", n);
 		if (data.hasRenderLabel())
