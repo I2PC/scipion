d8d7bf39de16b37339de8d786e7fcbb8b509086f
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2013-02-22 09:20:40 +0100

    added tests folder, updated authors in xmlmapper

--- mapper/xmlmapper.py
+++ mapper/xmlmapper.py
@@ -1,6 +1,7 @@
 # **************************************************************************
 # *
 # * Authors:     J.M. De la Rosa Trevin (jmdelarosa@cnb.csic.es)
+# *              Roberto Marabini       (roberto@cnb.csic.es)
 # *
 # * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
 # *
