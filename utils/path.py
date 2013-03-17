94994149c2b84e9f3499a511ef52a125be33effc
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2013-03-17 18:06:31 +0100

    added utils sub-package

---
+++ utils/path.py
@@ -0,0 +1,50 @@
+# **************************************************************************
+# *
+# * Authors:     J.M. De la Rosa Trevin (jmdelarosa@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'jmdelarosa@cnb.csic.es'
+# *
+# **************************************************************************
+"""
+This module contains the PATH related utilities 
+inside the utils module
+"""
+
+from os.path import exists, join
+import pyworkflow as pw
+
+def findFile(filename, *paths):
+    """Search if the file is present in
+    some path in the *paths provided.
+    Return None if not found"""
+    for p in paths:
+        fn = join(p, filename)
+        if exists(fn):
+            return fn
+    return None
+
+def findResource(filename):
+    """This function will search for a give
+    resource filename in the paths specified
+    in pyworkflow.RESOURCES path list"""
+    return findFile(filename, *pw.RESOURCES)
+    
+
