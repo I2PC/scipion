048f6613aea7df4bd51b518b4c18a01f552b95c8
Airen Zaldivar Peraza <airenzp@gmail.com>
2013-03-24 21:32:38 +0100

    creating single chimera client on command line, with viewer and projection modes
    Former-commit-id: 25f49fb1e6127682a449aa4c2b9592453f9ece41

--- applications/scripts/chimera_client/batch_chimera_client.py
+++ applications/scripts/chimera_client/batch_chimera_client.py
@@ -9,23 +9,30 @@ from protlib_filesystem import getXmippPath
 class BatchXmippChimeraClient:
 
 	def __init__(self):
-		self.volfile = self.parseInput()
+		self.parseInput()
 		serverfile = getXmippPath('libraries/bindings/chimera/xmipp_chimera_server.py')
 		system("chimera %s&" % serverfile)
 		#print 'running client'
-		XmippProjectionExplorer(self.volfile)
+		print self.mode
+		if self.mode == 'projection':
+				XmippProjectionExplorer(volfile)
+		elif self.mode == 'viewer':
+				XmippChimeraClient(volfile)
 
 
 	def parseInput(self):
 		
 		try:
-			self.usage = "usage: %prog [options] Example: %prog -i hand.vol"
+			self.usage = "usage: %prog [options] Example: %prog -i hand.vol --mode projection"
 			self.parser = OptionParser(self.usage)
 			self.parser.add_option("-i", "--input", dest="volfile", default="/dev/stdin", type="string", help="Volume to display")
+			self.parser.add_option("-m", "--mode", dest="mode", default="/dev/stdin", type="string", help="client mode: viewer for visualization and projection for projection explorer")
 			(options, args) = self.parser.parse_args()
 			if options.volfile == '/dev/stdin' or not(exists(options.volfile)):#simple validation
 				raise ValueError(options.volfile)
-			return (options.volfile)
+			
+			self.volfile = options.volfile
+			self.mode = options.mode
 		except:
 			print self.usage
 			exit()
