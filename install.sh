5e510cc2b8f10cd2e1de4c4793177fbe1a7fba5c
Airen Zaldivar Peraza <airenzp@gmail.com>
2013-04-05 11:55:01 +0200

    changing alias to imagej
    Former-commit-id: 22fc4b9c3fd6aeeca38b55b525678c10c25144a1

--- install.sh
+++ install.sh
@@ -153,7 +153,7 @@ echo "alias xp='xmipp_protocols'             "    >> $INC_FILE
 echo "alias xmipp='xmipp_protocols'          "    >> $INC_FILE
 echo "alias xs='xmipp_show'                  "    >> $INC_FILE
 echo "alias xsj='xmipp_showj'                "    >> $INC_FILE
-echo "alias xmipp_imagej='java -Xmx512m -jar $XMIPP_HOME/external/imagej/ij.jar'" >> $INC_FILE
+echo "alias xmipp_imagej='$XMIPP_HOME/external/runImageJ'" >> $INC_FILE
 echo "alias xij='xmipp_imagej'               "    >> $INC_FILE
 echo "## Image ##                            "    >> $INC_FILE
 echo "alias xic='xmipp_image_convert'        "    >> $INC_FILE
