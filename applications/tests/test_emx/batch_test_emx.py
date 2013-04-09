41502c8e7469393dc5c1c4fba134aa3edc2c3694
Roberto Marabini <roberto@cnb.csic.es>
2013-04-09 23:48:20 +0200

    fix error emx test
    Former-commit-id: 062db861b353f3aeee2907a95b4aa49c176656f8

--- applications/tests/test_emx/batch_test_emx.py
+++ applications/tests/test_emx/batch_test_emx.py
@@ -314,13 +314,14 @@ class TestEMX(unittest.TestCase):
         p1.set('transformationMatrix__t31',31.1)
         p1.set('transformationMatrix__t32',32.1)
         p1.set('transformationMatrix__t33',33.1)
-        p1.set('transformationMatrix__t34',34.11)
+        p1.set('transformationMatrix__t34',34.1)
 
         p1.setForeignKey(m1)
         _list.append(p1)
 
         counter=0
         for _object in emxData:
+            print "Counter", counter, _object, _list[counter]
             self.assertTrue(_object.strongEq(_list[counter]))
             counter += 1
 
