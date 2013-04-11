e997c19167062821261b08dd4cd46b2e83bc8bc6
ldelcano <ldelcano@cnb.csic.es>
2013-04-11 12:09:17 +0200

    Extracted XmippPreprocessProtocol from xmipp3 module and created a module on its own

---
+++ em/packages/xmipp3/protocol_preprocess_micrographs.py
@@ -0,0 +1,166 @@
+# **************************************************************************
+# *
+# * Authors:     J.M. De la Rosa Trevin (jmdelarosa@cnb.csic.es)
+# *              Laura del Cano (ldelcano@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'jmdelarosa@cnb.csic.es'
+# *
+# **************************************************************************
+"""
+This sub-package contains the XmippPreprocessMicrographs protocol
+"""
+
+
+from pyworkflow.em import *  
+from pyworkflow.utils import *  
+from xmipp import MetaData, MDL_MICROGRAPH, MDL_MICROGRAPH_ORIGINAL, MDL_MICROGRAPH_TILTED, MDL_MICROGRAPH_TILTED_ORIGINAL
+from pyworkflow.em.packages.xmipp3.data import *
+
+
+def definePreprocessMicrograph():
+    """Create the definition of parameters for
+    the XmippPreprocessMicrographs protocol"""
+    f = Form()
+    
+    f.addSection(label='Input')
+    f.addParam('inputMicrographs', PointerParam, label="Micrographs", pointerClass='SetOfMicrographs')
+    
+    f.addSection(label='Crop',questionParam='doCrop')
+    f.addParam('doCrop', BooleanParam, default=False,
+               label='Crop borders?')
+    f.addParam('cropPixels', IntParam, default=10,
+               label='Pixels to crop')
+    f.addSection(label='Logarithm',questionParam='doLog')
+    f.addParam('doLog', BooleanParam, default=False,
+               label='Take logarithm?')
+    f.addParam('logA', FloatParam, default=4.431,
+               label='a')
+    f.addParam('logB', FloatParam, default=0.4018,
+               label='b')
+    f.addParam('logC', FloatParam, default=336.6,
+               label='c')
+    f.addSection(label='Remove bad pixels',questionParam='doRemoveBadPix')
+    f.addParam('doRemoveBadPix', BooleanParam, default=False,
+               label='Remove bad pixels?')
+    f.addParam('mulStddev', IntParam, default=5,
+               label='Multiple of Stddev')    
+    f.addSection(label='Downsample',questionParam='doDownsample')
+    f.addParam('doDownsample', BooleanParam, default=False,
+               label='Downsample micrographs?')
+    f.addParam('downFactor', IntParam, default=2,
+               label='Downsampling factor')
+    
+    return f
+
+class XmippProtPreprocessMicrographs(ProtDownsampleMicrographs):
+    
+    _definition = definePreprocessMicrograph()
+    
+    def __init__(self, **args):
+        
+        Protocol.__init__(self, **args)
+        
+    def defineSteps(self):
+        '''for each micrograph call the downsampling function
+        '''
+        self.inputMics = self.inputMicrographs.get() 
+        
+        IOTable = {}
+        
+        self.params = {'downFactor': self.downFactor.get(),
+                       'cropPixels': self.cropPixels.get(),
+                       'logA': self.logA.get(),
+                       'logB': self.logB.get(),
+                       'logC': self.logC.get(),
+                       'stddev': self.mulStddev.get()}
+        
+        for mic in self.inputMics:
+            fn = mic.getFileName()
+            fnOut = self.getPath(os.path.basename(fn))
+            self.insertStepsForMicrograph(fn, fnOut)
+            IOTable[fn] = fnOut
+        
+        #create output objects       
+        self.insertFunctionStep('createOutput', IOTable)
+        
+
+
+        
+    def _insertOneStep(self, condition, program, arguments):
+        """Insert operation if the condition is met.
+        Possible conditions are: doDownsample, doCrop...etc"""
+        if condition.get():
+            # If the input micrograph and output micrograph differss, 
+            # add the -o option
+            if self.params['inputMic'] != self.params['outputMic']:
+                arguments += " -o %(outputMic)s"
+            # Insert the command with the formatted parameters
+            self.insertRunJobStep(program, arguments % self.params)
+            # Update inputMic for next step as outputMic
+            self.params['inputMic'] = self.params['outputMic']
+            
+        
+    def insertStepsForMicrograph(self, inputMic, outputMic):
+        self.params['inputMic'] = inputMic
+        self.params['outputMic'] = outputMic
+        # Downsample
+        self._insertOneStep(self.doDownsample, "xmipp_transform_downsample",
+                            "-i %(inputMic)s --step %(downFactor)f --method fourier")
+                    
+        # Crop
+        self._insertOneStep(self.doCrop, "xmipp_transform_window",
+                            " -i %(inputMic)s --crop %(cropPixels)d -v 0")
+        
+            
+        # Take logarithm
+        self._insertOneStep(self.doLog, "xmipp_transform_filter",
+                            " -i %(inputMic)s --log --fa %(logA)f --fb %(logB)f --fc %(logC)f")
+                    
+        # Remove bad pixels
+        self._insertOneStep(self.doRemoveBadPix, "xmipp_transform_filter",
+                            " -i %(inputMic)s --bad_pixels outliers %(stddev)f -v 0")
+                
+    def createOutput(self, IOTable):
+
+        mdOut = self.getPath("micrographs.xmd")
+                
+        self.outputMicrographs = XmippSetOfMicrographs(value=mdOut)     
+        self.outputMicrographs.microscope.voltage.set(self.inputMics.microscope.voltage.get())
+        self.outputMicrographs.microscope.sphericalAberration.set(self.inputMics.microscope.sphericalAberration.get())
+        self.outputMicrographs.samplingRate.set(self.inputMics.samplingRate.get())
+        
+        #Create the xmipp metadata micrographs.xmd  
+         
+        md = MetaData()      
+        for i, v in IOTable.iteritems():
+            objId = md.addObject()
+            md.setValue(MDL_MICROGRAPH,v,objId)
+            md.setValue(MDL_MICROGRAPH_ORIGINAL,i,objId)
+            #TODO: Handle Tilted micrographs
+#            if tiltPairs:
+#                MD.setValue(xmipp.MDL_MICROGRAPH_TILTED,IOTable[fnMicrographTilted],objId)
+#                MD.setValue(xmipp.MDL_MICROGRAPH_TILTED_ORIGINAL,fnMicrographTilted,objId)
+        md.write("micrographs"+"@"+mdOut)
+        
+        self.outputMicrographs.setFileName(mdOut)
+
+        self.defineOutputs(micrograph=self.outputMicrographs)
+        pass
