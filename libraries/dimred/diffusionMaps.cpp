ccb2a9edc2a98f340e1269c3e5bcb23902614daf
Carlos Oscar <coss@cnb.csic.es>
2013-05-19 08:32:03 +0200

    Bug fix in diffusionMaps
    Former-commit-id: 153de4d417ee620345447c2ba5c6c66cdd0b97c3

--- libraries/dimred/diffusionMaps.cpp
+++ libraries/dimred/diffusionMaps.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  *
  * Authors:    Carlos Oscar Sanchez Sorzano      coss@cnb.csic.es (2013)
- * 			   Tomas Bolgiani La Placa
+ *                Tomas Bolgiani La Placa
  *
  * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
  *
@@ -28,49 +28,52 @@
 
 void DiffusionMaps::setSpecificParameters(double t, double sigma)
 {
-	this->t=t;
-	this->sigma=sigma;
+    this->t=t;
+    this->sigma=sigma;
 }
 
 void DiffusionMaps::reduceDimensionality()
 {
-	//Normalize data (between 0 and 1)
-	normalizeColumnsBetween0and1(*X);
+    //Normalize data (between 0 and 1)
+    normalizeColumnsBetween0and1(*X);
 
-	// Compute Gaussian Kernel Matrix.
-	// First, compute the distance of all vs all.
-	Matrix2D<double> L2distance;
-	computeDistance(*X,L2distance,distance,false);
+    // Compute Gaussian Kernel Matrix.
+    // First, compute the distance of all vs all.
+    Matrix2D<double> L2distance;
+    computeDistance(*X,L2distance,distance,false);
 
-	// Now use this distance to build a similarity matrix
-	computeSimilarityMatrix(L2distance,sigma);
+    // Now use this distance to build a similarity matrix
+    computeSimilarityMatrix(L2distance,sigma);
 
-	// Normalize L2distance to be a stochastic matrix
-	Matrix1D<double> p;
-	L2distance.rowSum(p);
-	if (t!=1.)
-		FOR_ALL_ELEMENTS_IN_MATRIX2D(L2distance)
-				MAT_ELEM(L2distance,i,j)/=pow(VEC_ELEM(p,i)*VEC_ELEM(p,j),t);
+    // Normalize L2distance to be a stochastic matrix
+    Matrix1D<double> p;
+    L2distance.rowSum(p);
+    if (t!=1.)
+        FOR_ALL_ELEMENTS_IN_MATRIX2D(L2distance)
+            MAT_ELEM(L2distance,i,j)/=pow(VEC_ELEM(p,i)*VEC_ELEM(p,j),t);
+    else
+        FOR_ALL_ELEMENTS_IN_MATRIX2D(L2distance)
+            MAT_ELEM(L2distance,i,j)/=pow(VEC_ELEM(p,i);
 
-	// Normalize L2distance again
-	L2distance.rowSum(p);
-	FOR_ALL_ELEMENTS_IN_MATRIX1D(p)
-		VEC_ELEM(p,i)=sqrt(VEC_ELEM(p,i));
-	FOR_ALL_ELEMENTS_IN_MATRIX2D(L2distance)
-			MAT_ELEM(L2distance,i,j)/=VEC_ELEM(p,i)*VEC_ELEM(p,j);
+    // Normalize L2distance again
+    L2distance.rowSum(p);
+    FOR_ALL_ELEMENTS_IN_MATRIX1D(p)
+        VEC_ELEM(p,i)=sqrt(VEC_ELEM(p,i));
+    FOR_ALL_ELEMENTS_IN_MATRIX2D(L2distance)
+        MAT_ELEM(L2distance,i,j)/=VEC_ELEM(p,i)*VEC_ELEM(p,j);
 
-	// L2distance=U*S*V^t
-	Matrix2D<double> U,V;
-	Matrix1D<double> S;
-	svdcmp(L2distance,U,S,V);
+    // L2distance=U*S*V^t
+    Matrix2D<double> U,V;
+    Matrix1D<double> S;
+    svdcmp(L2distance,U,S,V);
 
-	// Get columns 1 to outputDim of U as output
-	// normalzied by the first element in its row
-	Y.resizeNoCopy(MAT_YSIZE(U),outputDim);
-	for (size_t i=0;i<MAT_YSIZE(U);++i)
-	{
-		double iK=1/MAT_ELEM(U,i,0);
-		for (size_t j=1;j<=outputDim;++j)
-		    MAT_ELEM(Y,i,j-1)=MAT_ELEM(U,i,j)*iK;
-	}
+    // Get columns 1 to outputDim of U as output
+    // normalzied by the first element in its row
+    Y.resizeNoCopy(MAT_YSIZE(U),outputDim);
+    for (size_t i=0;i<MAT_YSIZE(U);++i)
+    {
+        double iK=1/MAT_ELEM(U,i,0);
+        for (size_t j=1;j<=outputDim;++j)
+            MAT_ELEM(Y,i,j-1)=MAT_ELEM(U,i,j)*iK;
+    }
 }
