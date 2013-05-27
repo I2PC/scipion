fd61982ffbf237acfc761051303074d96721abbb
poza <apoza@cnb.csic.es>
2013-05-27 17:03:58 +0200

    Test improvement

--- tests/test_get_files.py
+++ tests/test_get_files.py
@@ -19,16 +19,19 @@ class TestGetFiles(unittest.TestCase):
     def setUp(self):
         manager = Manager()
         self.proj = manager.createProject(projName)
-      
-    def testGetFiles(self):       
-        print ('*************************************************************************************************************************')
+        
+    def testXmippSetOfMicrographs(self): 
+        print ('*****************************************   XmippSetOfMicrographs   ***************************************************')
         result = self.proj.mapper.selectByClass('XmippSetOfMicrographs')
         if len(result):    
             for xmippSetOfMicrographs in result:
                 print ("XmippSetOfMicrographs files: " + str(xmippSetOfMicrographs.getFiles()))
         else:
             print "Not XmippSetOfMicrographs found"
-        print ('*************************************************************************************************************************')
+        print ('*************************************************************************************************************************')    
+    
+    def testXmippSetOfCoordinates(self):
+        print ('****************************************  XmippSetOfCoordinates   ****************************************************')
         result = self.proj.mapper.selectByClass('XmippSetOfCoordinates')
         if len(result):    
             for xmippSetOfCoordinates in result:
@@ -36,35 +39,88 @@ class TestGetFiles(unittest.TestCase):
         else:
             print "Not XmippSetOfCoordinates found"
         print ('*************************************************************************************************************************')
+        self.assertTrue(True)    
+      
+    def testEmanSetOfCoordinates(self): 
+        print ('****************************************  EmanSetOfCoordinates  ******************************************************')
         result = self.proj.mapper.selectByClass('EmanSetOfCoordinates')
         if len(result):    
             for emanSetOfCoordinates in result:
                 print ("EmanSetOfCoordinates files: " + str(emanSetOfCoordinates.getFiles()))
         else:
-            print "Not EmanSetOfCoordinates found"
-        print ('*************************************************************************************************************************')   
-        result = self.proj.mapper.selectByClass('XmippCTFModel')
-        if len(result):    
-            for xmippCTFModel in result:
-                print ("XmippCTFModel files: " + str(xmippCTFModel.getFiles()))
-        else:
-            print "Not XmippCTFModel found"
-        print ('*************************************************************************************************************************')   
+            print "Not EmanSetOfCoordinates found"  
+        print ('*************************************************************************************************************************')
+        self.assertTrue(True)
+    
+    def testProtImportMicrographs(self):
+        print ('*****************************************  ProtImportMicrographs  *********************************************************')   
         result = self.proj.mapper.selectByClass('ProtImportMicrographs')
         if len(result):    
             for protImportMicrographs in result:
                 print ("ProtImportMicrographs files: " + str(protImportMicrographs.getFiles()))
         else:
             print "Not ProtImportMicrographs found"
-        print ('*************************************************************************************************************************')    
+        print ('*************************************************************************************************************************')
+        self.assertTrue(True)
+        
+    def testXmippProtPreprocessMicrographs(self):
+        print ('****************************************  XmippProtPreprocessMicrographs  ******************************************************')
+        result = self.proj.mapper.selectByClass('XmippProtPreprocessMicrographs')
+        if len(result):    
+            for xmippProtPreprocessMicrographs in result:
+                print ("XmippProtPreprocessMicrographs files: " + str(xmippProtPreprocessMicrographs.getFiles()))
+        else:
+            print "Not XmippProtPreprocessMicrographs found"
+        print ('*************************************************************************************************************************')  
+        self.assertTrue(True) 
+    
+    def testXmippProtParticlePicking(self):
+        print ('****************************************  XmippProtParticlePicking  ******************************************************')
+        result = self.proj.mapper.selectByClass('XmippProtParticlePicking')
+        if len(result):    
+            for xmippProtParticlePicking in result:
+                print ("XmippProtParticlePicking files: " + str(xmippProtParticlePicking.getFiles()))
+        else:
+            print "Not XmippProtParticlePicking found"
+        print ('*************************************************************************************************************************')  
+        self.assertTrue(True) 
+    
+    def testXmippProtExtractParticles(self):
+        print ('****************************************  XmippProtExtractParticles  ******************************************************')
+        result = self.proj.mapper.selectByClass('XmippProtExtractParticles')
+        if len(result):    
+            for xmippProtExtractParticles in result:
+                print ("XmippProtExtractParticles files: " + str(xmippProtExtractParticles.getFiles()))
+        else:
+            print "Not XmippProtExtractParticles found"
+        print ('*************************************************************************************************************************')  
+        self.assertTrue(True)        
+        
+    def testXmippCTFModel(self):
+        print ('****************************************  XmippCTFModel  ******************************************************')
+        result = self.proj.mapper.selectByClass('XmippCTFModel')
+        if len(result):    
+            for xmippCTFModel in result:
+                print ("XmippCTFModel files: " + str(xmippCTFModel.getFiles()))
+        else:
+            print "Not XmippCTFModel found"
+        print ('*************************************************************************************************************************')  
+        self.assertTrue(True) 
+    
+    def testEmanProtBoxing(self): 
+        print ('***************************************  EmanProtBoxing  ********************************************************')       
         result = self.proj.mapper.selectByClass('EmanProtBoxing')
         if len(result):    
             for emanProtBoxing in result:
                 print ("EmanProtBoxing files: " + str(emanProtBoxing.getFiles()))
         else:
             print "Not EmanProtBoxing found"
-        print ('*************************************************************************************************************************')
+        print ('*************************************************************************************************************************')    
         self.assertTrue(True)
+        
+        
+        
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
