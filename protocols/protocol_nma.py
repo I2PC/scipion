bc9cbdc1208a05df9b8ec5801711c7cbf862bc0e
Carlos Oscar <coss@cnb.csic.es>
2013-05-29 07:33:08 +0200

    Some advances
    Former-commit-id: db34b9859ea3c8826e99d174db0a9ddb74dc4506

--- protocols/protocol_nma.py
+++ protocols/protocol_nma.py
@@ -9,6 +9,7 @@ import glob,os,re,sys,shutil,time
 from protlib_base import *
 from config_protocols import protDict
 from protlib_utils import runJob
+from protlib_filesystem import createLink, getExt
 
 class ProtNMA(XmippProtocol):
     def __init__(self, scriptname, project):
@@ -17,11 +18,29 @@ class ProtNMA(XmippProtocol):
 
     def createFilenameTemplates(self):
         return {
-            'normal_modes': "normal_modes.xmd"
+            'normal_modes': "%(WorkingDir)s/normal_modes.xmd",
+            'pseudoatoms':  '%(WorkingDir)s/pseudoatoms'
             }
 
     def defineSteps(self):
         self.insertStep('createDir',path=self.ExtraDir)
+        if self.StructureType=="EM":
+            # Mask or link input structure
+            fnMask=""
+            if self.MaskMode=="Threshold":
+                fnMask=self.extraPath('mask.vol')
+                self.insertRunJobStep("xmipp_transform_threshold", params="-i %s -o %s --select below %f --substitute binarize"%\
+                                      (self.InputStructure,fnMask,self.Threshold),verifyFiles=[fnMask])
+            else:
+                fnMask=self.MaskFile
+            
+            # Convert to pseudoatoms
+            fnOut=self.getFileName("pseudoatoms")
+            params="-i %s -o %s --sigma %f --targetError %f --sampling_rate %f"%\
+                (self.InputStructure,fnOut,self.PseudoAtomRadius*self.Sampling,self.PseudoAtomTarget,self.Sampling)
+            if fnMask!="":
+                params+=" --mask binary_file %s"%fnMask
+            self.insertRunJobStep("xmipp_volume_to_pseudoatoms", params=params,verifyFiles=[fnOut])
     
     def summary(self):
         message=[]
@@ -30,6 +49,8 @@ class ProtNMA(XmippProtocol):
     
     def validate(self):
         errors = []
+        if self.MaskMode=="Binary mask" and not os.path.exists(self.MaskFile):
+            errors.append(self.MaskFile+" does not exist")
         return errors
     
     def visualize(self):
--- protocols/protocol_nma_header.py
+++ protocols/protocol_nma_header.py
@@ -16,18 +16,35 @@
 # {file}{validate}(PathExists) Input structure:
 InputStructure=''
 
-# {list}(EM, PDB) Kind of structure
+# {list}(EM, PDB) Kind of structure:
 """ Choose PDB for atomic structures and EM for Electron Microscopy Volumes 
 """
 StructureType='EM'
 
-# {expert}{condition}(StructureType=="EM")Volume approximation error
-"""This value is a percentage (between 0 and 1) specifying how fine you want to
+#------------------------------------------------------------------------------------------------
+# {section}{condition}(StructureType=="EM") Pseudoatom conversion
+#------------------------------------------------------------------------------------------------
+# Sampling rate (A/voxel):
+Sampling=1
+
+# {list_combo}(No mask, Threshold, Binary mask) Mask mode:
+MaskMode='No mask'
+
+# {condition}(MaskMode=="Threshold") Threshold value:
+Threshold=0.01
+
+# {condition}(MaskMode=="Binary mask") Mask file:
+MaskFile=""
+
+#{expert} Pseudoatom radius (voxels):
+""" Pseudoatoms are defined as Gaussians whose standard deviation is this value """
+PseudoAtomRadius=2
+
+# {expert} Volume approximation error (%):
+"""This value is a percentage (between 0.001 and 100) specifying how fine you want to
 approximate the EM volume by the pseudoatomic structure. Lower values imply lower approximation error,
 and consequently, more atoms."""
-PseudoAtomTarget=0.02
-
-# {eval} expandParallel(threads=0)
+PseudoAtomTarget=5
 
 #------------------------------------------------------------------------------------------------
 # {section}{visualize} Visualization
