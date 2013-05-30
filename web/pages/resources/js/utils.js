a6cb56804aa15a5a8d9cde87d5eb651a282cf498
josegutab <josegutab@josegutab>
2013-05-30 16:52:39 +0200

    * Coding refactor * Close popup and refresh parent window when you execute a protocol

--- web/pages/resources/js/utils.js
+++ web/pages/resources/js/utils.js
@@ -1,3 +1,20 @@
+function popup(URL) {
+	var popup_width = 470
+	var popup_height = 615
+	day = new Date();
+	id = day.getTime();
+	eval("page"
+			+ id
+			+ " = window.open(URL, '"
+			+ id
+			+ "', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=0,width='+popup_width+',height='+popup_height+'');");
+}
+
+function closePopup(){
+   opener.location.reload(true);
+   self.close();
+}
+
 function launchToolbar(projName, id, elm) {
 	var row = $("div#toolbar");
 
