34cac96567ab896aec3277fb37c4eebd4d9897ba
Carlos Oscar <coss@cnb.csic.es>
2013-06-06 12:21:03 +0200

    Projection explorer added
    Former-commit-id: 068a882ec70bff953e76934788abc39b7ad2fc52

--- protocols/protocol_screen_classes.py
+++ protocols/protocol_screen_classes.py
@@ -63,8 +63,9 @@ class ProtScreenClasses(XmippProtocol):
         return errors    
 
     def visualize(self):
-        fnAligned='classes_aligned@'+self.workingDirPath('classes.xmd')
-        runShowJ(fnAligned)
+        fnAligned = 'classes_aligned@' + self.workingDirPath('classes.xmd')
+        runShowJ(fnAligned, extraParams="--mode metadata --render first")
+        os.system('xmipp_chimera_client -i '+self.Volume+' --mode projector 256 &')
 
 def substituteReferenceImages(log,fnAngles,fnGallery):
     MD=MetaData(fnAngles)
