8d22bda4510d0d17e5eaa54afdb21aa2ee96846c
josegutab <josegutab@josegutab>
2013-06-06 13:00:06 +0200

    Little fix

--- web/static/js/projectForm.js
+++ web/static/js/projectForm.js
@@ -1,7 +1,67 @@
-function createProject() {
-	new Messi('This is a message with Messi.');
+function createProjectForm() {
+
+	var html = "Project Name: <input type='text' id='newProjName' class='content'/>";
+
+	new Messi(html, {
+		title : 'Enter the project name',
+		modal : true,
+		buttons : [ {
+			id : 0,
+			label : 'Ok',
+			val : 'Y',
+			btnClass : 'btn-select',
+			btnFunc : 'createProject'
+		}, {
+			id : 1,
+			label : 'Cancel',
+			val : 'C',
+			btnClass : 'btn-cancel'
+		} ]
+	});
 }
 
-function deleteProject() {
-	new Messi('This is a message with Messi.');
+function createProject(elm) {
+	var projName = elm.attr('value');
+	$.ajax({
+		type : "GET",
+		url : "/create_project/?projectName=" + projName,
+		success : function() {
+			window.location.href = "/projects/";
+		}
+	});
+}
+
+function deleteProjectForm(projName) {
+
+	var msg = "<div class='content' value='" + projName
+			+ "'>Are you sure to <strong>DELETE</strong> project <strong>"
+			+ projName + "</strong> and all its <strong>DATA</strong></div>";
+
+	new Messi(msg, {
+		title : 'Confirm project deletion',
+		modal : true,
+		buttons : [ {
+			id : 0,
+			label : 'Yes',
+			val : 'Y',
+			btnClass : 'btn-select',
+			btnFunc : 'deleteProject'
+		}, {
+			id : 1,
+			label : 'No',
+			val : 'C',
+			btnClass : 'btn-cancel'
+		} ]
+	});
+}
+
+function deleteProject(elm) {
+	var projName = elm.attr('value');
+	$.ajax({
+		type : "GET",
+		url : "/delete_project/?projectName=" + projName,
+		success : function() {
+			window.location.href = "/projects/";
+		}
+	});
 }
