7a25eaa2ecd715f4e3b9d68c9e503be1aac18201
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2013-06-07 14:19:09 +0200

    updated relion parameters with 1.1 version
    Former-commit-id: 4f8290377a8f8dd4ecddd5503bb3d8acafabc70d

--- protocols/protocol_relion3d.py
+++ protocols/protocol_relion3d.py
@@ -20,182 +20,23 @@ from protlib_filesystem import xmippExists, findAcquisitionInfo, moveFile
 from protocol_ml2d import lastIteration
 
 
-"""
-std::string RelionMainWindow::getCommandLine()
-{
-    std::string cline;
-    if (nr_mpi.getValue() > 1)
-        cline="`which relion_refine_mpi`";
-    else
-        cline="`which relion_refine`";
-
-    // I/O
-    // Save the real output name (could be with _ctX for continuation)
-    // This name will also be used for the stderr and stdout outputs and the submit script and gui settings filenames
-    cline += " --o " + output_name;
-    if (is_continue)
-    {
-        cline += " --continue " + fn_cont.getValue();
-    }
-    else
-    {
-        cline += " --i " + fn_img.getValue();
-        cline += " --particle_diameter " + floatToString(particle_diameter.getValue());
-        cline += " --angpix " + floatToString(angpix.getValue());
-        if (run_type == RUN3D)
-        {
-            cline += " --ref " + fn_ref.getValue();
-            if (textToFloat(padding.getValue().c_str()) < 3)
-            {
-                cline += " --lin --pad " + padding.getValue();
-            }
-            if (!ref_correct_greyscale.getValue())
-            {
-                cline += " --firstiter_cc";
-            }
-            if (ini_high.getValue() > 0.)
-            {
-                cline += " --ini_high " + floatToString(ini_high.getValue());
-            }
-        }
-    }
-
-    // Optimisation
-    cline += " --iter " + floatToString(nr_iter.getValue());
-    cline += " --tau2_fudge " + floatToString(tau_fudge.getValue());
-
-    if (do_solvent_flattening.getValue())
-    {
-        cline += " --flatten_solvent";
-        if (fn_mask.getValue().length() > 0)
-            cline += " --solvent_mask " + fn_mask.getValue();
-    }
-
-    if (do_scale_correction.getValue())
-    {
-        cline += " --scale";
-    }
-
-    if (!is_continue)
-    {
-        // Only split data in two halves from the start
-        if (do_split_random_halves.getValue())
-        {
-            cline += " --split_random_halves";
-        }
-    }
-    else
-    {
-        // Only join in a continue run
-        if (do_join_random_halves.getValue())
-        {
-            cline += " --join_random_halves";
-        }
-    }
-
-    if (!is_continue)
-    {
-        if (do_ctf_correction.getValue())
-        {
-            cline += " --ctf";
-            if (run_type == RUN3D)
-            {
-                if (ctf_corrected_ref.getValue())
-                    cline += " --ctf_corrected_ref";
-            }
-            if (only_flip_phases.getValue())
-                cline += " --only_flip_phases";
-            if (ctf_phase_flipped.getValue())
-                cline += " --ctf_phase_flipped";
-            if (ctf_intact_first_peak.getValue())
-                cline += " --ctf_intact_first_peak";
-        }
-    }
-
-    // Sampling stuff
-    if (!is_continue)
-    {
-        if (run_type == RUN3D)
-        {
-            if (strcmp((sym_group.getValue()).c_str(), "O") == 0 ||
-                strcmp((sym_group.getValue()).c_str(), "T") == 0 )
-                cline += " --sym " + sym_group.getValue();
-            else
-                cline += " --sym " + sym_group.getValue() + floatToString(sym_nr.getValue());
-        }
-        cline += " --K " + floatToString(nr_classes.getValue());
-    }
-    int iover = (int)textToFloat((oversampling.getValue()).c_str());
-    if (run_type == RUN3D)
-    {
-        for (int i = 0; i < 10; i++)
-        {
-            if (strcmp((sampling.getValue()).c_str(), sampling_options[i].label()) == 0)
-            {
-                // The sampling given in the GUI will be the oversampled one!
-                cline += " --healpix_order " + floatToString((float)i + 1 - iover);
-                break;
-            }
-        }
-
-        if (do_local_ang_searches.getValue())
-            cline += " --sigma_ang " + floatToString(sigma_angles.getValue() / 3.);
-
-    }
-    if (run_type == RUN2D)
-    {
-        // The sampling given in the GUI will be the oversampled one!
-        cline += " --psi_step " + floatToString(psi_sampling.getValue() * pow(2., iover));
-    }
-    cline += " --oversampling " + oversampling.getValue();
-    cline += " --offset_range " + floatToString(offset_range.getValue());
-    // The sampling given in the GUI will be the oversampled one!
-    cline += " --offset_step " + floatToString(offset_step.getValue() * pow(2., iover));
-
-    // Always do norm correction
-    cline += " --norm";
-
-    cline += " " + other_args.getValue();
-
-    // Running stuff
-    cline += " --j " + floatToString(nr_threads.getValue());
-
-    return cline;
-
-}
-"""
-
 class ProtRelion3D(XmippProtocol):
     def __init__(self, scriptname, project):
         XmippProtocol.__init__(self, protDict.relion3d.name, scriptname, project)
-        self.Import = 'from protocol_ml3d import *'
+        self.Import = 'from protocol_relion3d import *'
         self.ParamsStr = ''
-        if self.DoMlf:
-            self.progId = 'mlf'
+        if self.NumberOfMpi > 1:
+            self.program = 'relion_refine_mpi'
         else:
-            self.progId = 'ml'
-        self.ParamsDict['ProgId'] = self.progId
+            self.program = 'relion_refine'
+        self.ParamsDict['program'] = self.program
         
-        self.addParam('ORoot', self.WorkingDir + '/')
-        self.addParam('Extra2D', self.workingDirPath("%s2dextra" % self.progId))
+        self.addParam('ORoot', self.WorkingDir + '/')        
         acquisionInfo = self.findAcquisitionInfo(self.ImgMd)
 
         if not acquisionInfo is None: 
             md = MetaData(acquisionInfo)
             self.addParam('SamplingRate', md.getValue(MDL_SAMPLINGRATE, md.firstObject()))
-                        
-    def createFilenameTemplates(self):
-        extraRefs = '%(Extra2D)s/iter%(iter)03d/iter_classes.xmd'
-        return {
-                'iter_logs': "info@" + extraRefs,
-                'iter_refs': "classes@" + extraRefs,
-                'iter_vols': '%(ExtraDir)s/iter%(iter)03d/iter_volumes.xmd',
-                #'refs': 'iter%(iter)06d@' + extraRefs,
-                'initial_vols':  '%(ExtraDir)s/initial_volumes.stk',
-                'corrected_vols': '%(ExtraDir)s/corrected_volumes.stk',
-                'filtered_vols': '%(ExtraDir)s/filtered_volumes.stk',
-                'generated_vols': '%(ExtraDir)s/generated_volumes.stk'
-                }
         
     def summary(self):
         md = MetaData(self.ImgMd)
@@ -256,19 +97,10 @@ class ProtRelion3D(XmippProtocol):
             
             self.insertStep('renameOutput', WorkingDir=self.WorkingDir, ProgId=self.progId)
             
-    # Insert the step of launch some program
-    def insertRunJob(self, prog, vf=[], files=None, useProcs=True):
-        if files is None:
-            files = [self.ParamsDict[k] for k in vf]
-        NumberOfMpi = NumberOfThreads = 1
-        if useProcs:
-            NumberOfMpi = self.NumberOfMpi
-            NumberOfThreads = self.NumberOfThreads
-        self.insertStep('runJob', programname=prog, 
-                        params = self.ParamsStr % self.ParamsDict, 
-                        verifyfiles = files, 
-                        NumberOfMpi = NumberOfMpi, 
-                        NumberOfThreads = NumberOfThreads)
+    def insertRelionRefine(self):
+        pass
+
+
     
     def insertML3DStep(self, inputImg, oRoot, initialVols, numberOfIters, amplitudCorrected):
         self.ParamsDict.update({
@@ -433,6 +265,18 @@ def copyVolumes(log, inputMd, outputStack):
     for i, idx in enumerate(md):
         img.read(md.getValue(MDL_IMAGE, idx))
         img.write('%d@%s' % (i + 1, outputStack))
+        
+def convertImagesMetaData(inputMd, outputRelion):
+    """ Convert the Xmipp style MetaData to one ready for Relion.
+    Main differences are: STAR labels are named different and
+    in Xmipp the images rows contains a path to the CTFModel, 
+    while Relion expect the explict values in the row.
+    Params:
+     input: input filename with the Xmipp images metadata
+     output: output filename for the Relion metadata
+     """
+     md = MetaData(inputMd)
+     
         
 def renameOutput(log, WorkingDir, ProgId):
     ''' Remove ml2d prefix from:
