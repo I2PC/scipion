8f2fb712ef0cb7a1fb31a59b4d5ba560e0fc8424
josegutab <josegutab@josegutab>
2013-07-26 13:04:27 +0200

    Completed save protocol form with the new style
    Former-commit-id: 7dd21b0d3701c8f6abbddc3616bb9c59f57fa955

--- pyworkflow/web/app/views.py
+++ pyworkflow/web/app/views.py
@@ -412,6 +412,7 @@ def updateParam(request, project, protocol, paramName):
         else:
             value = None
     attr.set(value)
+    print "setting attr %s with value:" % paramName, value 
     
 SPECIAL_PARAMS = ['runName', 'numberOfMpi', 'numberOfThreads', 'hostName']
 
