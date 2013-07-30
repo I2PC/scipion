b09836c8fb4b38e385a7444438db9e864e0707ad
ldelcano <ldelcano@cnb.csic.es>
2013-07-30 12:30:30 +0200

    Added test to test EmanBoxing (Faked)
    Former-commit-id: 0f6ce18d0382b65c43c587cae65a7f0ce1c71698

---
+++ pyworkflow/tests/em/protocols/test_protocols_eman.py
@@ -0,0 +1,39 @@
+'''
+Created on Jun 6, 2013
+
+@author: laura
+'''
+import unittest, sys
+from pyworkflow.tests import *
+from pyworkflow.em import *
+from pyworkflow.em.packages.xmipp3 import *
+from pyworkflow.em.packages.eman2 import *
+
+class TestEmanBoxing(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):    
+        # Create a new project
+        setupProject(cls)
+        cls.pattern = getInputPath('Micrographs_BPV3', '*.mrc')        
+        cls.importFolder = getInputPath('EmanTestProject2')
+        
+    def testCreateOutput(self):    
+        #First, import a set of micrographs
+        protImport = ProtImportMicrographs(pattern=self.pattern, samplingRate=1.237, voltage=300)
+        self.proj.launchProtocol(protImport, wait=True)
+        
+        self.assertIsNotNone(protImport.outputMicrographs, "There was a problem with the import")
+        
+        print "Running Eman fake particle picking..."   
+        protPP = EmanProtBoxing(importFolder=self.importFolder, runMode=1)                
+#        protPP.inputMicrographs.set(protCTF.outputMicrographs)        
+        protPP.inputMicrographs.set(protImport.outputMicrographs)
+        self.proj.launchProtocol(protPP, wait=True)
+        self.protDict['protPicking'] = protPP
+            
+        self.assertIsNotNone(protPP.outputCoordinates, "There was a problem with the faked picking")
+
+if __name__ == "__main__":
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestEmanBoxing)
+    unittest.TextTestRunner(verbosity=2).run(suite)
