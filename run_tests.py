787b05f08c335bdae76ec6d47815a067637f205d
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2013-08-02 12:33:29 +0200

    improved run_tests.py to be more flexible, minor change in test_ xmipp_workflow
    Former-commit-id: 84fdbed70d53253019ddbb36272bf61eb99900f1

--- run_tests.py
+++ run_tests.py
@@ -7,8 +7,12 @@ import pyworkflow as pw
 from pyworkflow.tests import GTestResult
 
 
-def discoverTests(path='.'):
-    return unittest.defaultTestLoader.discover(join('pyworkflow','tests', path), top_level_dir=pw.HOME)
+def discoverTests(pathList=[]):
+    tests = unittest.TestSuite()
+    for path in pathList:
+        testPath = join('pyworkflow','tests', path)
+        tests.addTests(unittest.defaultTestLoader.discover(testPath, top_level_dir=pw.HOME))
+    return tests
 
 def printTests(tests):
     for t in tests:
@@ -23,27 +27,58 @@ def runTests(tests):
     else:
         sys.exit(0)
        
-       
+  
+PATH = 'path'
+CASE = 'case'
+PRINT = 'print'
+
+CASE_DICT = {'short': 'classes em/data',
+             'medium': 'classes em/data em/workflows'}
+  
+def parseArgs(args):
+    """ Parse arguments in the form of:
+        path="em/data classes"
+        case="short"
+        print
+        And store in a dictionary
+    """
+    d = {}
+    for a in args:
+        if '=' in a:
+            k, v = a.split('=')
+        else:
+            k, v = a, True
+        d[k.lower()] = v
+
+    return d
+
+
 if __name__ == '__main__':
-    
     arg = None
     tests = []
     doPrint = False
     
     if len(sys.argv) > 1:
-        arg = sys.argv[1].lower()
-      
-    if arg is None:
-        pass  
-    elif arg == 'print':
-        tests = discoverTests()
-        doPrint = True
+        argsDict = parseArgs(sys.argv[1:])
     else:
-        tests = discoverTests(arg)
-        if 'print' in sys.argv:
-            doPrint = True
-
-    if doPrint:
+        argsDict = {PATH: 'classes'}
+      
+    print "argsDict: ", argsDict
+    
+    # CASE and PATH are excusive
+    if CASE in argsDict:
+        cases = argsDict[CASE].split()
+        path = ''
+        for c in cases:
+            path += CASE_DICT[c] + ' '
+    else:
+        path = argsDict[PATH]
+    
+    pathList = path.split()
+    
+    tests = discoverTests(pathList)
+    
+    if PRINT in argsDict:
         printTests(tests)
     else:
         runTests(tests)
