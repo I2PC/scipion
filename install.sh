a1d6d8fc289df7453de0a5a632ebcc5b25b1074f
Carlos Oscar Sorzano <coss@cnb.csic.es>
2013-08-21 16:03:23 +0200

    Produce a float version of FFTW
    Former-commit-id: 86634a0236aa884214e017cfd1dab79dc4574873

--- install.sh
+++ install.sh
@@ -388,9 +388,10 @@ compile_pymodule()
 install_libs()
 {
   cd $XMIPP_HOME
-  LIBPATH=../external/$1; shift
+  LIBPATH=external/$1; shift
   COMMON="$1"; shift
-  VERSION=$1
+  VERSION=$1; shift
+  COPY=$1
   SUFFIXES=".a .la "
   if $IS_MAC; then
 	SUFFIXES="$SUFFIXES .dylib .$VERSION.dylib"
@@ -402,8 +403,16 @@ install_libs()
   
   for suffix in $SUFFIXES; do
      LIBNAME=$COMMON$suffix
-     echo "--> ln -sf $LIBPATH/$LIBNAME lib/$LIBNAME"
-     ln -sf $LIBPATH/$LIBNAME lib/$LIBNAME  
+     if $COPY; then
+     	     if [ -e lib/$LIBNAME ]; then
+	         rm -f lib/$LIBNAME
+             fi
+	     echo "--> cp -f $LIBPATH/$LIBNAME lib/$LIBNAME"
+	     cp -f $LIBPATH/$LIBNAME lib/$LIBNAME  
+     else
+	     echo "--> ln -sf ../$LIBPATH/$LIBNAME lib/$LIBNAME"
+	     ln -sf ../$LIBPATH/$LIBNAME lib/$LIBNAME  
+     fi
   done
 }
 
@@ -465,7 +474,7 @@ if $DO_SQLITE; then
   #execute sqlite to avoid relinking in the future
   echo "select 1+1 ;" | $VSQLITE/sqlite3
   install_bin $VSQLITE/sqlite3 xmipp_sqlite3
-  install_libs $VSQLITE/.libs libsqlite3 0
+  install_libs $VSQLITE/.libs libsqlite3 0 false
   #compile math library for sqlite
   cd $EXT_PATH/$VSQLITE_EXT
   if $IS_MINGW; then
@@ -483,31 +492,37 @@ fi
 #################### FFTW ###########################
 if $DO_FFTW; then
   if $IS_MINGW; then
-    compile_library $VFFTW "." "." "--enable-threads CPPFLAGS=-I/c/MinGW/include CFLAGS=-I/c/MinGW/include"
+    FFTWFLAGS=" CPPFLAGS=-I/c/MinGW/include CFLAGS=-I/c/MinGW/include"
   else
-    compile_library $VFFTW "." "." "--enable-threads"
+    FFTWFLAGS=""
   fi
-  install_libs $VFFTW/.libs libfftw3 3
-  install_libs $VFFTW/threads/.libs libfftw3_threads 3
+  FLAGS="$FFTWFLAGS --enable-threads"
+  compile_library $VFFTW "." "." $FLAGS
+  install_libs $VFFTW/.libs libfftw3 3 true
+  install_libs $VFFTW/threads/.libs libfftw3_threads 3 true
+
+  FLAGS="$FFTWFLAGS --enable-float"
+  compile_library $VFFTW "." "." $FLAGS
+  install_libs $VFFTW/.libs libfftw3f 3 true
 fi
 
 #################### JPEG ###########################
 if $DO_JPEG; then
 compile_library $VJPEG "." "." "CPPFLAGS=-w"
-install_libs $VJPEG/.libs libjpeg 8
+install_libs $VJPEG/.libs libjpeg 8 false
 fi
 
 #################### TIFF ###########################
 if $DO_TIFF; then
   compile_library $VTIFF "." "." "CPPFLAGS=-w --with-jpeg-include-dir=$EXT_PATH/$VJPEG --with-jpeg-lib-dir=$XMIPP_HOME/lib"
-  install_libs $VTIFF/libtiff/.libs libtiff 3
+  install_libs $VTIFF/libtiff/.libs libtiff 3 false
 fi
 
 #################### HDF5 ###########################
 if $DO_HDF5; then
 compile_library $VHDF5 "." "." "CPPFLAGS=-w --enable-cxx"
-install_libs $VHDF5/src/.libs libhdf5 7
-install_libs $VHDF5/c++/src/.libs libhdf5_cpp 7
+install_libs $VHDF5/src/.libs libhdf5 7 false
+install_libs $VHDF5/c++/src/.libs libhdf5_cpp 7 false
 fi
 
 #################### TCL/TK ###########################
@@ -670,7 +685,7 @@ fi
 #################### ARPACK ###########################
 if $DO_ARPACK; then
   compile_library $VARPACK "." "." ""
-  install_libs $VARPACK/src/.libs libarpack++ 2
+  install_libs $VARPACK/src/.libs libarpack++ 2 false
 fi
 
 # Launch the configure/compile python script 
