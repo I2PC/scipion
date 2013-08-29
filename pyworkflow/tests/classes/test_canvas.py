888e74d895d9ff2a5384424f67091984eb16afb4
Jesus <jesuscal@gmail.com>
2013-08-29 12:10:28 +0200

    canvas - update testcanvas after refactoring
    Former-commit-id: eb4246e781d2a8e005931b10b181682b789861ff

--- pyworkflow/tests/classes/test_canvas.py
+++ pyworkflow/tests/classes/test_canvas.py
@@ -4,7 +4,7 @@
 
 
 import unittest
-from pyworkflow.gui.canvas import Canvas
+import pyworkflow.gui.canvas
 import Tkinter
 import math
 from sets import Set
@@ -22,10 +22,10 @@ class TestCanvas(unittest.TestCase):
 
     def test_connectorsCoords(self):
         root = Tkinter.Tk()
-        canvas = Canvas(root, width=800, height=600)
+        canvas = pyworkflow.gui.canvas.Canvas(root, width=800, height=600)
         tb1 = canvas.createTextbox("First", 100, 100, "blue")
 
-        connectorsCoords=canvas.getConnectorsCoordinates(tb1)
+        connectorsCoords=tb1.getConnectorsCoordinates()
         self.assertTrue(self.allDifferent(connectorsCoords))
 
         print connectorsCoords
@@ -40,16 +40,16 @@ class TestCanvas(unittest.TestCase):
 
     def test_closestConnectors(self):
         root = Tkinter.Tk()
-        canvas = Canvas(root, width=800, height=600)
+        canvas = pyworkflow.gui.canvas.Canvas(root, width=800, height=600)
         tb1 = canvas.createTextbox("Textbox1", 100, 100, "blue")
         tb2 = canvas.createTextbox("Textbox2", 300, 100, "blue")
         tb3 = canvas.createTextbox("Textbox3", 100, 300, "blue")
 
-        tb1ConnectorsCoords=canvas.getConnectorsCoordinates(tb1)
-        tb2ConnectorsCoords=canvas.getConnectorsCoordinates(tb2)
-        tb3ConnectorsCoords=canvas.getConnectorsCoordinates(tb3)
-        c1,c2= canvas.findClosestConnectors(tb1ConnectorsCoords,tb2ConnectorsCoords)
-        c3,c4= canvas.findClosestConnectors(tb1ConnectorsCoords,tb3ConnectorsCoords)
+        tb1ConnectorsCoords=tb1.getConnectorsCoordinates()
+        tb2ConnectorsCoords=tb2.getConnectorsCoordinates()
+        tb3ConnectorsCoords=tb3.getConnectorsCoordinates()
+        c1,c2= pyworkflow.gui.canvas.findStrictClosestConnectors(tb1,tb2)
+        c3,c4= pyworkflow.gui.canvas.findStrictClosestConnectors(tb1,tb3)
         # tb1 and tb2 are aligned vertically. tb1 and tb3, horizontally.
         # So, their closest connectors must share one coordinate (y in case of c1&c2, x i n case of c3&c4)
         self.assertEqual(c1[1],c2[1])
