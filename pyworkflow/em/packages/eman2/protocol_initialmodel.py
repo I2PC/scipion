7a1895a87c806009c342b945484fa42a40fdf8f9
ldelcano <ldelcano@cnb.csic.es>
2013-09-16 09:52:28 +0200

    Print for Nacho's testing.
    Former-commit-id: f0efc2694890e4165cfd4f748715252f1262a39a

--- pyworkflow/em/packages/eman2/protocol_initialmodel.py
+++ pyworkflow/em/packages/eman2/protocol_initialmodel.py
@@ -89,6 +89,7 @@ class EmanProtInitModel(ProtInitialVolume):
 
     def getXmippStackFilename(self):
         for cls in self.inputClasses.get():
+            print "QUE PASA CONTIGO PISHAAAAA"
             img = cls.getRepresentativeImage()
             return img.getFileName()
 
