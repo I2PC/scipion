da167a87bf62f61335c891bf5c7867a72dab817a
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2013-10-22 16:57:59 +0200

    removed test function
    Former-commit-id: 303cb8b18f241fc5dd7f3e7c0a9b9cb457e827b7

--- protocols/protocol_ml2d.py
+++ protocols/protocol_ml2d.py
@@ -224,9 +224,6 @@ def launchML2DPlots(protML, selectedPlots):
     ll = []
     pmax = []
     for iter in iters:
-#    for id in md:
-#        iter = md.getValue(MDL_ITER, id)
-#        if iter > 0:
         logs = protML.getFilename('iter_logs', iter=iter)
         md = MetaData(logs)
         id = md.firstObject()
@@ -282,9 +279,4 @@ def collectResults(log, WorkingDir, Prefix):
         dst = join(WorkingDir, 'result_%s.xmd' % k)
         renameFile(log, src, dst)
 
-
-def longSleep(log):
-    """ Just for test. """
-    import time
-    time.sleep(30)
     
