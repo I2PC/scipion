ac1f635cf9f797c74206ee2edcbe6e3478ced7ca
ldelcano <ldelcano@cnb.csic.es>
2013-10-31 10:44:53 +0100

    Second version of viewer ml3d (still developing)
    Former-commit-id: 13303cad60f8d1915c9b483edc5aff79969a81d4

--- pyworkflow/em/packages/xmipp3/viewer_ml3d.py
+++ pyworkflow/em/packages/xmipp3/viewer_ml3d.py
@@ -32,6 +32,8 @@ from pyworkflow.em import *
 from pyworkflow.gui.form import FormWindow
 from protocol_ml3d import XmippProtML3D
 from viewer import runShowJ
+from plotter import XmippPlotter
+from xmipp import MetaData, AGGR_SUM, MDL_REF3D, MDL_WEIGHT, MDL_WEIGHT, MDValueEQ
 
 import numpy as np
 
@@ -73,10 +75,11 @@ class XmippML3DViewer(ProtocolViewer):
         return {'doShowGreyScaleRefVol': self._viewCorrectedVols,
                 'doShowFilterRefVol': self._viewFilteredVols,
                 'doShowSeedsVols': self._viewGeneratedVols,
-#                'doShow2DAvgs': self._view2DAvgs,
-#                'doShow3DRefsVolumes': self._view3DRefsVolumes,
-#                'doShowAngDist': self._viewAngDist,
-#                'doShowDataDist': self._viewDataDist
+                'doShow2DAvgs': self._view2DAvgs,
+                'doShow3DRefsVolumes': self._view3DRefsVolumes,
+                'doShowAngDist': self._plotAngularDistribution,
+                'doShowDataDist': self._plotClassDistribution,
+                'doShowStatistics': self._plotStatistics
                 }
         
         
@@ -89,19 +92,107 @@ class XmippML3DViewer(ProtocolViewer):
     def _viewGeneratedVols(self, e=None):
         runShowJ(self.protocol._getExtraPath("generated_volumes.stk"))
         
+    def _view2DAvgs(self, e=None):
+        self._viewIterationFile("ml2dextra/iter%03d/iter_classes.xmd")
+        
+    def _view3DRefsVolumes(self, e=None):
+        self._viewIterationFile("extra/iter%03d/vol000001.vol")
+    
+    def _plotAngularDistribution(self, e=None):
+        self.setVisualizeIterations()
+        for iter in self.visualizeIters:
+            extraPath = self.protocol._getPath("ml2dextra/iter%03d/iter_classes.xmd" % iter)
+            print "extraPath=%s" % extraPath
+            if not os.path.exists(extraPath):
+                self.formWindow.showError('Iteration %s does not exist.' % iter)        
+            else:
+                md = MetaData("classes@%s" % extraPath)
+                md2 = MetaData()
+                md2.aggregate(md, AGGR_SUM, MDL_REF3D, MDL_WEIGHT, MDL_WEIGHT)
+                nrefs = md2.size()
+                figsize = None
+                if nrefs == 1:
+                    gridsize = [1, 1]
+                    figsize = (4, 4)
+                elif nrefs == 2:
+                    gridsize = [1, 2]
+                    figsize = (8, 4)
+                else:
+                    gridsize = [(nrefs+1)/2, 2]
+                    figsize = (8, 12)
+        
+                xplotter = XmippPlotter(*gridsize, figsize=figsize, 
+                                        windowTitle="Angular distribution - iteration %d" % iter)
+                
+                for r in range(1, nrefs+1):
+                    md2.importObjects(md, MDValueEQ(MDL_REF3D, r))  
+                    plot_title = 'ref %d' % r
+                    xplotter.plotAngularDistribution(plot_title, md2)
+                if xplotter is not None:
+                    xplotter.show()
+
+    def _plotClassDistribution(self, e=None):
+        from numpy import arange
+        from matplotlib.ticker import FormatStrFormatter
+        self.setVisualizeIterations()
+        for iter in self.visualizeIters:
+            extraPath = self.protocol._getPath("ml2dextra/iter%03d/iter_classes.xmd" % iter)
+            print "extraPath=%s" % extraPath
+            if not os.path.exists(extraPath):
+                self.formWindow.showError('Iteration %s does not exist.' % iter)        
+            else:
+                xplotter = XmippPlotter(1, 1, figsize=(4,4),
+                                        windowTitle="Images distribution - iteration %d" % iter)
+                md = MetaData("classes@%s" % extraPath)
+                md2 = MetaData()    
+                md2.aggregate(md, AGGR_SUM, MDL_REF3D, MDL_WEIGHT, MDL_WEIGHT)
+                weights = [md2.getValue(MDL_WEIGHT, objId) for objId in md2]
+                nrefs = len(weights)
+                refs3d = arange(1, nrefs + 1)
+                width = 0.85
+                a = xplotter.createSubPlot('3D references weights on last iteration', 'references', 'weight')
+                a.set_xticks(refs3d + 0.45)
+                a.xaxis.set_major_formatter(FormatStrFormatter('%1.0f'))
+                a.set_xlim([0.8, nrefs + 1])
+                a.bar(refs3d, weights, width, color='b')
+                if xplotter is not None:
+                    xplotter.show() 
+                    
+    def _plotStatistics(self, e=None):
+        pass
+#        from viewer_ml2d import createPlots
+#        xplotter = createPlots(self.protocol, ['DoShowLL', 'DoShowPmax'])
+#        if xplotter is not None:
+#            xplotter.show() 
+                
+    def _viewIterationFile(self, filePath):
+        self.setVisualizeIterations()
+        for iter in self.visualizeIters:
+            extraPath = self.protocol._getPath(filePath % iter)
+            print "extraPath=%s" % extraPath
+            if os.path.exists(extraPath):
+                runShowJ(extraPath)        
+            else:
+                self.formWindow.showError('Iteration %s does not exist.' % iter)        
         
     def setVisualizeIterations(self):
         '''Validate and set the set of iterations to visualize.
         If not set is selected, only use the last iteration'''
-        self.lastIter = self.protocol._lastIteration(self)
+        self.lastIter = self.protocol._lastIteration()
+        self.visualizeIters = []
         
         if self.iterToShow.get() == LAST_ITER:
             self.visualizeIters = [self.lastIter]
         elif self.iterToShow.get() == ALL_ITER:
-            self.visualizeIters = range(1, self.lastIter+1)
+            self.visualizeIters = range(1, self.lastIter + 1)
         elif self.iterToShow.get() == SELECTED_ITERS:
-            selection = self._getListFromRangeString(self.selectedIters.get())
-            self.visualizeIters = [it for it in selection if (it > 0 and it <= self.lastIter)]
+            if self.selectedIters.empty():
+                self.formWindow.showError('Please select the iterations that you want to visualize.')
+            else:
+                try:
+                    self.visualizeIters = self._getListFromRangeString(self.selectedIters.get())
+                except Exception, ex:
+                    self.formWindow.showError('Invalid iterations range.')
             
     def _viewPlot(self, paramName):
         xplotter = self.createPlots(self.protocol, [paramName])
