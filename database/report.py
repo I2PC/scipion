51750e6726ad7cdd7b6c529c4936ac370215fb82
rmarabini <roberto@cnb.csic.es>
2013-11-07 12:25:20 +0100

    add report
    Former-commit-id: fd2a0ba1b923578c4494c8eaeca7a4e6f66d5815

---
+++ database/report.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from platform import platform
+from urllib import urlopen
+#urlopen('http://localhost:8080/WebApplication3/NewServlet?osystem='+platform())
+#urlopen('http://glassfishdev.cnb.csic.es:8880/WebApplication3/NewServlet?osystem='+platform())
+#urlopen('http://glassfishprod.cnb.csic.es:8880/WebApplication3/NewServlet?osystem='+platform())
+urlopen('http://i2pc.cnb.csic.es/xmipp/WebApplication3/NewServlet?osystem='+platform())
+
