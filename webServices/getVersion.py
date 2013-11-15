f2de8a6e1af3cf6ce06a0026ac8238754b11f743
rmarabini <roberto@cnb.csic.es>
2013-11-16 00:30:09 +0100

    update scrips
    Former-commit-id: 309fc94bfcf08a692e912c09cc0214b1b402623c

--- webServices/getVersion.py
+++ webServices/getVersion.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 from urllib import urlopen
 
-#urlopen('http://localhost:8080/xmippWebServices/GetVersion')
 #urlopen('http://glassfishdev.cnb.csic.es:8880/xmippWebServices/GetVersion')
-#urlopen('http://glassfishprod.cnb.csic.es:8880/xmippWebServices/GetVersion')
-#urlopen('http://i2pc.cnb.csic.es/xmippWebServices/GetVersion')
+#a = urlopen('http://glassfishprod.cnb.csic.es:8880/xmippWebServices/GetVersion')
+#a = urlopen('http://i2pc.cnb.csic.es/xmippWebServices/GetVersion')
 a = urlopen('http://localhost:8080/xmippWebServices/GetVersion')
 exec (a.read())
 print git_date, version, git_hash
