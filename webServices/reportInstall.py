565d2a764eed099f951792673c08672a2dd6350b
rmarabini <roberto@cnb.csic.es>
2013-11-15 11:28:05 +0100

    modify webservice for reporting installation
    Former-commit-id: b79b48376a90ffc532b80f511dff26d1b381463b

--- webServices/reportInstall.py
+++ webServices/reportInstall.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from platform import platform
 from urllib import urlopen
-#urlopen('http://localhost:8080/WebApplication3/NewServlet?osystem='+platform())
-urlopen('http://localhost:8080/WebApplication3/NewServlet?osystem=doNotStore')
-#urlopen('http://glassfishdev.cnb.csic.es:8880/WebApplication3/NewServlet?osystem='+platform())
-#urlopen('http://glassfishprod.cnb.csic.es:8880/WebApplication3/NewServlet?osystem='+platform())
-#urlopen('http://i2pc.cnb.csic.es/xmipp/NewServlet?osystem='+platform())
+#urlopen('http://localhost:8080/xmippWebServices/ReportInstallation?osystem='+platform())
+#urlopen('http://localhost:8080/xmippWebServices/ReportInstallation?osystem=doNotStore')
+#urlopen('http://glassfishdev.cnb.csic.es:8880/xmippWebServices/ReportInstallation?osystem='+platform())
+#urlopen('http://glassfishprod.cnb.csic.es:8880/xmippWebServices/ReportInstallation?osystem='+platform())
+urlopen('http://i2pc.cnb.csic.es/xmippWebServices/ReportInstallation?osystem='+platform())
+#urlopen('http://i2pc.cnb.csic.es/xmippWebServices/ReportInstallation?osystem='+'kkkk')
 
