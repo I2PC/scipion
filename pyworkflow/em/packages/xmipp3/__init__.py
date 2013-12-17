d3c552858de71aa70391baa1c1162e8efe4304ad
jvargas <jvargas@cnb.csic.es>
2013-12-17 17:23:33 +0100

    Working in RANSAC protocol
    Former-commit-id: 6c7c41ad5d86f049bd360dc2eee1a0a2929bcab8

--- pyworkflow/em/packages/xmipp3/__init__.py
+++ pyworkflow/em/packages/xmipp3/__init__.py
@@ -53,6 +53,7 @@ from protocol_filters import XmippProtMask, XmippProtResize
 from protocol_create_mask import XmippProtCreateGeo3DMask
 from protocol_particle_pick_automatic import XmippParticlePickingAutomatic
 from protocol_screen_particles import XmippProtScreenParticles
+from protocol_ransac import XmippProtRansac
 
 # Wizards
 from wizard import *
