01a13c1307f7d84c4feb275c821f021e5db3cbed
rmarabini <roberto@cnb.csic.es>
2013-12-19 19:20:05 +0100

    retrieve pdb file from pdb.com
    Former-commit-id: 86f88d0d71e97f4825e33675c46d549b6791c2ce

--- pyworkflow/em/packages/xmipp3/protocol_convert_pdb.py
+++ pyworkflow/em/packages/xmipp3/protocol_convert_pdb.py
@@ -6,14 +6,19 @@ class XmippProtConvertPdb(ProtInitialVolume):
     _pdb_file = ''
     _sampling_rate = 0.0
     _output_file = ''
-    
+    PDB_ID=1
+
     def _defineParams(self, form):
         """ Define the parameters that will be input for the Protocol.
         This definition is also used to generate automatically the GUI.
         """
         form.addSection(label='Input')
-        form.addParam('pdb_file', StringParam, label="Input PDB",
-                       help='File to process')
+	form.addParam('inputPdbData', EnumParam, choices=['local_file', 'PDB_ID'],
+                      label="Retrieve data from", default=self.PDB_ID,
+                      display=EnumParam.DISPLAY_COMBO,
+                      help='Retrieve PDB data from server or use local file')
+        form.addParam('pdb_file', StringParam, label="FileName or PDB ID",
+                       help='type local PDB File Name or PDB ID')
         form.addParam('sampling', FloatParam, label="Sampling rate",
                       help='Sampling rate (Angstroms/pixel) ')
 
@@ -23,6 +28,7 @@ class XmippProtConvertPdb(ProtInitialVolume):
         """
         self._pdb_file = self.pdb_file.get()
         self._sampling_rate = self.sampling.get()
+        self._inputPdbData = self.inputPdbData.get()
         self._insertFunctionStep('convertPdb')
         self._insertFunctionStep('createOutput')
 
@@ -31,8 +37,20 @@ class XmippProtConvertPdb(ProtInitialVolume):
         register the resulting outputs in the database.
         """
         import xmipp
+        from tempfile import NamedTemporaryFile
+
+        if self._inputPdbData == self.PDB_ID:
+		import urllib
+		src = "http://www.rcsb.org/pdb/downloadFile.do?fileFormat=pdb&compression=NO&structureId=%s"
+		urllib.urlretrieve(src % self._pdb_file,'%s.pdb'%self._pdb_file)
+		_inFile = '%s.pdb' % self._pdb_file
+		_outFile= '%s' % self._pdb_file
+	else:
+		_inFile = self._pdb_file
+		_outFile = _inFile.rsplit( ".", 1 )[ 0 ]+".vol"
+
         program = "xmipp_volume_from_pdb"
-        args = '-i %s --sampling %f' % (self._pdb_file, self._sampling_rate)
+        args = '-i %s --sampling %f -o %s ' % (_inFile, self._sampling_rate,_outFile)
         self.runJob(None, program, args)
 
     def createOutput(self):
