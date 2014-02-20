e4ae4c3cfb5d2ea3549ef17e854005754beffdb2
Airen Zaldivar Peraza <airenzp@gmail.com>
2014-02-20 10:49:19 +0100

    working on bsoft particle imports
    Former-commit-id: 48d26485b8671a78c2dcd29eec33be9d5738147f

--- pyworkflow/em/packages/bsoft/protocol_particle_pick.py
+++ pyworkflow/em/packages/bsoft/protocol_particle_pick.py
@@ -30,23 +30,21 @@ This sub-package contains the XmippParticlePicking protocol
 
 from pyworkflow.em import *  
 from pyworkflow.utils.path import *  
-import xmipp
-from xmipp3 import XmippProtocol
-
-from convert import createXmippInputMicrographs, readSetOfCoordinates
 
 
-class XmippProtParticlePicking(ProtParticlePicking, XmippProtocol):
 
-    """Protocol to pick particles in a set of micrographs of the project
-    either manually or using automatic picking support  """
-    _label = 'supervised picking'
+
+
+class BsoftProtParticlePicking(ProtParticlePicking):
+
+    """Bsoft protocol to pick particles in a set of micrographs of the project"""
+    _label = 'particle picking'
 
     
     def __init__(self, **args):        
         ProtParticlePicking.__init__(self, **args)
         # The following attribute is only for testing
-        self.importFolder = String(args.get('importFolder', None))
+
         
     def _defineParams(self, form):
     
@@ -55,64 +53,39 @@ class XmippProtParticlePicking(ProtParticlePicking, XmippProtocol):
                       pointerClass='SetOfMicrographs',
                       help='Select the SetOfMicrograph ')
         form.addParam('memory', FloatParam, default=2,
-                   label='Memory to use (In Gb)', expertLevel=2)        
+                   label='Memory to use (In Gb)', expertLevel=2)    
+        
+
         
     def _insertAllSteps(self):
         """The Particle Picking proccess is realized for a set of micrographs"""
         
         # Get pointer to input micrographs 
         self.inputMics = self.inputMicrographs.get()
-        # Parameters needed 
-        
-        
-        # Convert input SetOfMicrographs to Xmipp if needed
-#        self._insertConvertStep('inputMics', XmippSetOfMicrographs, 
-#                                 self._getPath('micrographs.xmd'))
         # Launch Particle Picking GUI
-        if not self.importFolder.hasValue():
-            self._insertFunctionStep('launchParticlePickGUIStep', isInteractive=True)
-        else: # This is only used for test purposes
-            self._insertFunctionStep('_importFromFolderStep')       
+        self._insertFunctionStep('launchParticlePickGUIStep', isInteractive=True)
         # Insert step to create output objects       
         self._insertFunctionStep('createOutputStep')
         
         
     def launchParticlePickGUIStep(self):
-        
-        # Get the converted input micrographs in Xmipp format
-        # if not exists, means the input was already in Xmipp
-        micFn = createXmippInputMicrographs(self, self.inputMics)
-        self._params = {'memory': self.memory.get(),
-                        'inputMicsXmipp': micFn,
-                        'extraDir': self._getExtraPath(),
-                        'pickingMode': 'manual'
-                        }
-        
+        self._enterWorkingDir()
         # Launch the particle picking GUI
-        program = "xmipp_micrograph_particle_picking"
-        arguments = "-i %(inputMicsXmipp)s -o %(extraDir)s --mode %(pickingMode)s --memory %(memory)dg"
-        # TiltPairs
-#        if self.inputMics.hasTiltPairs():
-#            self._params['inputMicsXmipp'] = "TiltedPairs@" + fn
-#            program = "xmipp_micrograph_tiltpair_picking"
-        # Run the command with formatted parameters
-        self.runJob(program, arguments % self._params)
-        
-    def _importFromFolderStep(self):
-        """ This function will copy Xmipp .pos files for
-        simulating a particle picking run...this is only
-        for testing purposes.
-        """
-        for f in getFiles(self.importFolder.get()):
-            copyFile(f, self._getExtraPath())
+        program = "bshow"  
+       
+        for mic in self.inputMics:
+            micfile = os.path.relpath(mic.getFileName(), self.workingDir.get())
+            print micfile
+            self.runJob(program, micfile)
+        self._leaveWorkingDir()
+   
         
     def createOutputStep(self):
-        posDir = self._getExtraPath()
+        outputDir = self._getExtraPath()
         coordSet = self._createSetOfCoordinates()
         coordSet.setMicrographs(self.inputMics)
-        readSetOfCoordinates(posDir, self.inputMics, coordSet)
-        self._defineOutputs(outputCoordinates=coordSet)
         
+        self._defineOutputs(outputCoordinates=coordSet)        
         self._defineSourceRelation(self.inputMics, coordSet)
         
 
@@ -125,15 +98,5 @@ class XmippProtParticlePicking(ProtParticlePicking, XmippProtocol):
             picked = self.outputCoordinates.getSize()
         return  "Number of particles picked: %d (from %d micrographs)" % (picked, self.inputMicrographs.get().getSize())
     
-    def _summary(self):
-        summary = []
-        if not hasattr(self, 'outputCoordinates'):
-            summary.append("Output coordinates not ready yet.") 
-        else:
-            summary.append("Number of particles picked: %d (from %d micrographs)" % (self.outputCoordinates.getSize(), self.inputMicrographs.get().getSize()))
-        return summary
-    
-    
-    def _citations(self):
-        return ['Abrishami2013']
+  
 
