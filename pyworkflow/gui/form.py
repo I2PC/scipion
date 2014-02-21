063021c47477d5a0385ab87384d1cd1d105d266e
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2014-02-21 09:04:57 +0100

    fixed bug when searching for ctf relations
    Former-commit-id: 312be19b88ac8668d38602f09ce24db87426610b

--- pyworkflow/gui/form.py
+++ pyworkflow/gui/form.py
@@ -190,8 +190,9 @@ class SubclassesTreeProvider(TreeProvider):
 class RelationsTreeProvider(SubclassesTreeProvider):
     """Will implement the methods to provide the object info
     of subclasses objects(of className) found by mapper"""
-    def __init__(self, protocol, relationParam):
+    def __init__(self, protocol, relationParam, selected=None):
         self.mapper = protocol.mapper
+        self.selected = selected
         parentObject = protocol.getAttributeValue(relationParam.relationParent.get())
         if parentObject is not None:
             queryFunc =  protocol.mapper.getRelationChilds      
@@ -459,7 +460,7 @@ class ParamWidget():
     def _browseRelation(self, e=None):
         """Select a relation from DB
         This function is suppose to be used only for RelationParam"""
-        tp = RelationsTreeProvider(self.window.protocol, self.param)
+        tp = RelationsTreeProvider(self.window.protocol, self.param, selected=self.get())
         dlg = ListDialog(self.parent, "Select object", tp)
         if dlg.value is not None:
             self.set(dlg.value)
