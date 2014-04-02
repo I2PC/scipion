88b970d9e317e457343a6b6765e59175d5d5ffe7
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2014-04-02 16:19:42 +0200

    moved the download_packages into an app and added scipion entry for that action
    Former-commit-id: 104a244b542f23f13585442486b1715a26facfcc

---
+++ pyworkflow/apps/pw_download_packages.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# **************************************************************************
+# *
+# * Authors:     J.M. De la Rosa Trevin (jmdelarosa@cnb.csic.es)
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'jmdelarosa@cnb.csic.es'
+# *
+# **************************************************************************
+"""
+This module serve to download the needed software packages.
+"""
+import os, sys
+
+
+PACKAGES_URL = "http://scipionwiki.cnb.csic.es/files/scipion/software/em/"
+PACKAGES_DICT = {
+                 'xmipp': "xmipp.tgz",
+                 'brandeis': "brandeis.tgz",
+                 'eman': "eman2.pre2-1.linux64.tar.gz",
+                 'spider': "spiderweb.21.13.tar.gz",
+                 'relion': "relion-1.2.tar.bz2"                 
+                 }
+
+
+def download(package):
+    os.system("wget " + PACKAGES_URL + PACKAGES_DICT[package])
+    
+def downloadAll():
+    for package in PACKAGES_DICT:
+        download(package)
+
+
+if __name__ == '__main__':
+    # Enter to EM packages directory
+    os.chdir(os.path.join(os.environ['SCIPION_HOME'], 'software', 'em'))
+    
+    n = len(sys.argv)
+    if n > 1:
+        package = sys.argv[1]
+        download(package)
+    else:
+        downloadAll()
