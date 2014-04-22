3f349190fdd6ecd2c704f379fd462e38829b56ea
josegutab <josegutab@josegutab>
2014-04-22 08:36:24 +0200

    Updated methods and comments for the documentation.
    Former-commit-id: 22a373f2ccf50c49b7cc29e6ab3f36e0dac3f3da

--- pyworkflow/web/pages/resources/js/templates_libs/utils.js
+++ pyworkflow/web/pages/resources/js/templates_libs/utils.js
@@ -80,6 +80,9 @@
  * function updateLabelComment()
  * 	->	Method to store the label and comment for an object.
  * 
+ *  * function launchViewer(id)
+ * 	->	Launch the viewers to analyze the results for an object (protocol or object).
+ * 
  ******************************************************************************/
 
 /** METHODS *******************************************************************/
