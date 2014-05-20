f6b38e9df2c3e2d4bd7d012e526174ce30714f7d
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2014-05-21 00:21:57 +0200

    adapted full spider capca protocol to use new parsing mechanism
    Former-commit-id: 72a7da0b0ce3e96edd00a68a79ebbb9c0f4bece8

--- pyworkflow/em/packages/spider/protocol/protocol_ca_pca.py
+++ pyworkflow/em/packages/spider/protocol/protocol_ca_pca.py
@@ -28,9 +28,12 @@ This sub-package contains protocol for
 Correspondence Analysis or Principal Component Analysis
 """
 
-from pyworkflow.em import *  
+from os.path import join
 
-from ..constants import *
+from pyworkflow.protocol.params import IntParam, PointerParam, EnumParam, FloatParam
+from pyworkflow.em.convert import ImageHandler
+
+from ..constants import CA
 from ..spider import copyTemplate, runSpiderTemplate, PcaFile
 from protocol_base import SpiderProtocol
 
@@ -51,11 +54,12 @@ class SpiderProtCAPCA(SpiderProtocol):
     
     def __init__(self, **kwargs):
         SpiderProtocol.__init__(self, **kwargs)
+        
         self._params = {'ext': 'stk',
-                        'spiderParticles': 'particles_spider',
-                        'spiderSel': 'particles_selfile',
-                        'outputParticles': 'particles_output',
-                        'spiderMask': 'mask',
+                        'particles': 'input_particles',
+                        'particlesSel': 'input_particles_sel',
+                        'outputParticles': 'output_particles',
+                        'mask': 'mask',
                         'imcFile': join('CA', 'cas_IMC'),
                         'seqFile': join('CA', 'cas_SEQ'),
                         'eigFile': join('CA', 'cas_EIG'),
@@ -90,43 +94,44 @@ class SpiderProtCAPCA(SpiderProtocol):
     def _insertAllSteps(self):
         # Insert processing steps
         self._insertFunctionStep('convertInput', 'inputParticles',
-                                 self._getFileName('spiderParticles'), self._getFileName('spiderSel'))
-        self._insertFunctionStep('convertMask')
-        self._insertFunctionStep('runCAPCA', self.analysisType.get(), 
+                                 self._getFileName('particles'), self._getFileName('particlesSel'))
+        self._insertFunctionStep('convertMaskStep', self.maskType.get())
+        self._insertFunctionStep('capcaStep', self.analysisType.get(), 
                                  self.numberOfFactors.get(), self.maskType.get())
+        self._insertFunctionStep('createOutputStep')
         
-    def convertMask(self):
+    def convertMaskStep(self, maskType):
         """ Convert the input mask if needed and
         copy some spider needed scripts. 
         """
         # Copy mask if selected
-        if self.maskType > 0: # mask from file
-            maskFn = self._getFileName('spiderMask')
+        if maskType > 0: # mask from file
+            maskFn = self._getFileName('mask')
             ImageHandler().convert(self.maskImage.get().getLocation(), 
                        (1, maskFn))
         # Copy template scripts
         copyTemplate('ploteigen.gnu', self._getPath())
         copyTemplate('eigendoc.py', self._getPath())
         
-    def runCAPCA(self, analysisType, numberOfFactors, maskType):
+    def capcaStep(self, analysisType, numberOfFactors, maskType):
         """ Apply the selected filter to particles. 
         Create the set of particles.
         """
         dim = self.inputParticles.get().getDimensions()[0]
         
-        self._params.update({'dim': dim,
-                             'radius': self.radius.get(),
-                             'analysisType': analysisType + 1, # Index starts at 0
-                             'addConstant': self.addConstant.get(),
-                             'numberOfFactors': numberOfFactors
+        self._params.update({'[idim]': dim,
+                             '[radius]': self.radius.get(),
+                             '[cas-option]': analysisType + 1, # Index starts at 0
+                             '[add-constant]': self.addConstant.get(),
+                             '[num-factors]': numberOfFactors,
+                             '[selection_doc]': self._params['particlesSel'],
+                             '[particles]': self._params['particles'] + '@******',
+                             '[custom_mask]': self._params['mask'] + '@1'
                              })
-                                
-        self._enterWorkingDir() # Do operations inside the run working dir
+                   
+        self.runScript('mda/ca-pca.msa', self.getExt(), self._params)
         
-        runSpiderTemplate('ca_pca.txt', self._params['ext'], self._params)
-        
-        self._leaveWorkingDir() # Go back to project dir
-        
+    def createOutputStep(self):
         # Generate outputs
         imc = PcaFile()
         imc.filename.set(self._getFileName('imcFile'))
