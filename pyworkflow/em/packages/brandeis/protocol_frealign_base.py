8ba37cd42b749f08a9cc26dd39fb934abea5a629
josuegbl <josuegbl@git.code.sf.net>
2014-06-09 11:04:51 +0200

    changed mpi for threads in frealing protocols
    Former-commit-id: d930682c8c920ac981fed95e7c768cd80d95c6b0

--- pyworkflow/em/packages/brandeis/protocol_frealign_base.py
+++ pyworkflow/em/packages/brandeis/protocol_frealign_base.py
@@ -450,7 +450,7 @@ class ProtFrealignBase(EMProtocol):
                            'high values in the FSC curve (se publication #2 above). FREALIGN uses an\n'
                            'automatic weighting scheme and RBFACT should normally be set to 0.0.')
 
-        form.addParallelSection(threads=4, mpi=0)
+        form.addParallelSection(threads=0, mpi=4)
     
     #--------------------------- INSERT steps functions --------------------------------------------
     def _insertAllSteps(self):
