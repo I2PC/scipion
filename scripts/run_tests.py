1989174e4d959158833ab430bb065b0323c610de
jordiburcas <jordi.burguet.castell@gmail.com>
2014-06-13 16:24:05 +0200

    upgrade run_tests.py so it uses argparse for arguments
    Former-commit-id: 7a282f05646b5e0d0cf24d7011c449edb17a7443

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -1,47 +1,66 @@
 #!/usr/bin/env python
 
+"""
+Launch tests.
+"""
+
 import sys
 from os.path import join
+import argparse
 import unittest
 
 import pyworkflow as pw
 from pyworkflow.tests import failStr, GTestResult
 
 
-CASE = {
-    'model': {'path': 'model em/data', 'pattern': 'test*.py'},
-    'xmipp': {'path': 'em/workflows', 'pattern': 'test*xmipp*.py'},
-    'mixed': {'path': 'em/workflows', 'pattern': 'test*mixed*.py'},
-    'protocols': {'path': 'em/protocols', 'pattern': 'test_protocols*.py'}}
+PATH_PATTERN = {'model': ('model em/data', 'test*.py'),
+                'xmipp': ('em/workflows', 'test*xmipp*.py'),
+                'mixed': ('em/workflows', 'test*mixed*.py'),
+                'protocols': ('em/protocols', 'test_protocols*.py')}
 
 
 def main():
-    args = parseArgs(sys.argv[1:])
+    # First a hack (add --) so we use argparse in spite of our peculiar syntax.
+    sys.argv[1:] = [x if x[0] == '-' else '--'+x for x in sys.argv[1:]]
+    # Also, if no parameters given at all, do as before, run only paths=model
+    if not sys.argv[1:]:
+        print 'No explicit paths given, running only tests in paths=model'
+        sys.argv[1:] = ['--paths=model']
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    add = parser.add_argument  # shortcut
+    add('--case', choices=['model', 'xmipp', 'mixed', 'protocols'],
+        help='get pre-defined paths and patterns for the specified case')
+    add('--paths', default='.',
+        help='space-separated list of paths where the tests are searched')
+    add('--pattern', default='test*.py',
+        help='pattern for the files that will be used in the tests')
+    add('--show', action='store_true', help='show the tests available')
+    add('--mode', default='modules', choices=['modules', 'classes', 'all'],
+        help='how much detail to give in show mode')
+    args = parser.parse_args()
 
     # If 'case' is given, 'path' and 'pattern' are (re-)written from it
-    if 'case' in args:
-        args.update(CASE[args['case']])
+    if args.case:
+        print 'Using paths and pattern for given case "%s"' % args.case
+        args.paths, args.pattern = PATH_PATTERN[args.case]
 
-    defaultPath = 'model' if not 'print' in args else '.'
+    tests = discoverTests(args.paths.split(), args.pattern)
 
-    tests = discoverTests(args.get('path', defaultPath).split(),
-                          args.get('pattern', 'test*.py'))
-
-    if 'print' in args:
-        printTests(tests, args.get('mode', 'modules'))
+    if args.show:
+        printTests(tests, args.mode)
     else:
         runTests(tests)
 
 
-def discoverTests(pathList, pattern):
-    """ Return tests discovered in pathList that follow the given pattern """
+def discoverTests(paths, pattern):
+    """ Return tests discovered in paths that follow the given pattern """
 
     tests = unittest.TestSuite()
-    for path in pathList:
-        testPath = join('pyworkflow', 'tests', path)
-        print "Discovering tests in '%s'" % testPath
+    for path in [join('pyworkflow', 'tests', x) for x in paths]:
+        print "Discovering tests in '%s'" % path
         tests.addTests(unittest.defaultTestLoader.discover(
-            testPath, pattern=pattern, top_level_dir=pw.HOME))
+            path, pattern=pattern, top_level_dir=pw.HOME))
     return tests
 
 
@@ -69,7 +88,7 @@ def printTests(tests, mode='modules'):
 
         # If there is a failure loading the test, show it
         if moduleName.startswith('unittest.loader.ModuleImportFailure'):
-            print failStr(moduleName), "  test: ", t.id()
+            print failStr(moduleName), "  test:", t.id()
             continue
 
         if moduleName != lastModule:
@@ -90,24 +109,6 @@ def runTests(tests):
         sys.exit(1)
     else:
         sys.exit(0)
-
-
-def parseArgs(args):
-    """ Parse arguments in the form of:
-          path="em/data classes"
-          case="short"
-          print
-        And store them in a dictionary
-    """
-    d = {}
-    for a in args:
-        if '=' in a:
-            k, v = a.split('=')
-        else:
-            k, v = a, True
-        d[k.lower()] = v
-
-    return d
 
 
 
