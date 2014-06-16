8ab3c0fd8c4fa88641173991b773e8192dd7669a
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2014-06-16 14:21:53 +0200

    draft for pw_create_coords.py, still not working
    Former-commit-id: 23c04f155fa95d3d275b1aafa045bb0f4f6b96d4

--- pyworkflow/apps/pw_create_coords.py
+++ pyworkflow/apps/pw_create_coords.py
@@ -1,9 +1,30 @@
+#!/usr/bin/env python
+# **************************************************************************
+# *
+# * Authors:    Airen Zaldivar         (airenzp@gmail.com) 
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'jmdelarosa@cnb.csic.es'
+# *
+# **************************************************************************
 
-'''
-Created on Jan 27, 2014
-
-@author: airen
-'''
 import os, sys
 from pyworkflow.manager import Manager
 
@@ -17,13 +38,15 @@ from pyworkflow.em.packages.xmipp3 import readSetOfCoordinates
 
 if __name__ == '__main__':
 
+    projectId = sys.argv[1]
+    project = Manager().loadProject(projectId)
+    protId = int(sys.argv[2])
+    prot = project.getProtocol(protId)
+    
     
     outputdir = sys.argv[1]
     dbpath = sys.argv[2]
-    protid = sys.argv[3]
-    print outputdir, dbpath, protid
-    prot = getProtocolFromDb(dbpath, protid)
-    prot.printAll()
+    
     inputset = prot.inputMicrographs.get()
     extradir = prot._getExtraPath()
     count = 0;
@@ -37,6 +60,7 @@ if __name__ == '__main__':
     outputs = {outputName: outputset}
     prot._defineOutputs(**outputs)
     prot._defineSourceRelation(inputset, outputset)
-    prot._store()
+    
+    project._updateProtocol(prot)
         
     
