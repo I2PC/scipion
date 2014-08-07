94630406b705604be745d02dd5536c9c79a23375
jordiburcas <jordi.burguet.castell@gmail.com>
2014-08-07 12:42:08 +0200

    uncomment, now that we do have the ProtCTFAssign protocol
    Former-commit-id: 674f8f73e9cdadc27a3960bfe9fee5c5a710deb1

--- pyworkflow/em/protocol/__init__.py
+++ pyworkflow/em/protocol/__init__.py
@@ -36,9 +36,7 @@ from protocol_2d import *
 from protocol_3d import *
 from protocol_sets import *
 from protocol_tiltpairs import *
-#from protocol_ctf_assign import ProtCTFAssign
-# Commented out, waiting for the protocol to appear in the repository.
-# Dale JoseMi ;)
+from protocol_ctf_assign import ProtCTFAssign
 
 from parallel import ProtTestParallel
 
