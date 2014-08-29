20cac3920d22c41273b9492ce372a2477fcc57ee
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2014-08-29 15:55:44 -0400

    setting also the micrograph_id
    Former-commit-id: 3fa9b05a849b40bafc9d50fd243f9eecf5203ce7

--- pyworkflow/em/packages/xmipp3/convert.py
+++ pyworkflow/em/packages/xmipp3/convert.py
@@ -385,7 +385,10 @@ def rowToParticle(partRow, **kwargs):
     img.setCoordinate(rowToCoordinate(partRow))
     # Setup the micId if is integer value
     try:
-        img.setMicId(int(partRow.getValue(xmipp.MDL_MICROGRAPH)))
+        if partRow.hasLabel(xmipp.MDL_MICROGRAPH_ID):
+            img.setMicId(partRow.getValue(xmipp.MDL_MICROGRAPH_ID))
+        else:
+            img.setMicId(int(partRow.getValue(xmipp.MDL_MICROGRAPH)))
     except Exception:
         pass    
     return img
@@ -397,7 +400,8 @@ def particleToRow(part, partRow, **kwargs):
     coord = part.getCoordinate()
     if coord is not None:
         coordinateToRow(coord, partRow, copyId=False)
-    if part.getMicId() is not None:
+    if part.hasMicId():
+        partRow.setValue(xmipp.MDL_MICROGRAPH_ID, long(part.getMicId()))
         partRow.setValue(xmipp.MDL_MICROGRAPH, str(part.getMicId()))
 
 def rowToClass(classRow, classItem):
