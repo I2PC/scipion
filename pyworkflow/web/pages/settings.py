160d7af221f14b4216eef49e27cfb3027e37826c
josegutab <josegutab@josegutab>
2014-09-19 15:17:54 +0200

    Fixed the collectstatic function for absolute paths
    Former-commit-id: bd6da1c88058f08da7e11cbca49279cd28248449

--- pyworkflow/web/pages/settings.py
+++ pyworkflow/web/pages/settings.py
@@ -82,7 +82,9 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(pw.HOME, 'web', 'pages', 'resources'),
+        os.path.join(pw.HOME, 'web', 'pages','resources'),
+    #    os.path.join(pw.HOME, 'web', 'pages', 'resources'),
+    
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
