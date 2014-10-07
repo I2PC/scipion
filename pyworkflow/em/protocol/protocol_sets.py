cef294e39f8b5f0ae6710149b00b1b01505d2ac7
Airen Zaldivar Peraza <airenzp@gmail.com>
2014-10-07 13:00:19 +0200

    keeping track of selection sqlite to use copy/restart subset protocol
    Former-commit-id: da818d903d1d7a45c1ce0d3e21fd00568e5dc0f7

--- pyworkflow/em/protocol/protocol_sets.py
+++ pyworkflow/em/protocol/protocol_sets.py
@@ -246,9 +246,9 @@ class ProtUserSubSet(ProtSets):
     def _loadDbNamePrefix(self):
         """ Setup filename and prefix for db connection. """
         
-        _dbName, self._dbPrefix = self.sqliteFile.get().split(',')
-        self._dbName = self._getPath('subset.sqlite')
-        os.rename(_dbName, self._dbName)
+        self._dbName, self._dbPrefix = self.sqliteFile.get().split(',')
+        #self._dbName = self._getPath('subset.sqlite')
+        #os.rename(_dbName, self._dbName)
 
         if self._dbPrefix.endswith('_'):
             self._dbPrefix = self._dbPrefix[:-1] 
