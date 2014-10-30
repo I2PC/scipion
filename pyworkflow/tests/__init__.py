a2dd10d67e29c09bd68f718269a54120ba408415
rmarabini <roberto@cnb.csic.es>
2014-10-30 12:50:15 +0100

    add flip test
    Former-commit-id: 843a34d63efab3ea331fc17529871a3714cc2862 [formerly 9fbc5840e23def743451a80efdb112c516df356d]
    Former-commit-id: 3ac5a47b6f240cb35375632df467ea607a21da70

--- pyworkflow/tests/__init__.py
+++ pyworkflow/tests/__init__.py
@@ -124,6 +124,7 @@ DataSet(name='emx',  folder='emx',
         files={
 
                'alignFlip':'alignment/alignFlip.mrcs',
+               'alignFlip2':'alignment/alignFlip2.mrcs',
                'alignShiftRotExp': 'alignment/Test1/images.mrc',
                'alignShiftRotExpGold': 'alignment/gold/align_shift_rot_exp_gold.mrcs',
                
