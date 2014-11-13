7cb70ad0d27e0f09af512871d7f50608f93eccba
jordiburcas <jordi.burguet.castell@gmail.com>
2014-11-13 15:32:04 +0100

    fleshed up a bit the test_sets.py set operations testing script
    Former-commit-id: 7d272c22f9ffafd671f5a3f343e93f398e11eea9 [formerly 56ef44ea40fa5b490cc6b5ab43c78b907437a675]
    Former-commit-id: 4583fda383413000c4f9f8f37461a26517a72d79

--- pyworkflow/tests/test_sets.py
+++ pyworkflow/tests/test_sets.py
@@ -29,6 +29,8 @@
 
 from pyworkflow.tests import BaseTest, setupTestProject, DataSet
 
+from pyworkflow.em.data import EMObject
+
 
 # TODO: finish copying stuff from test_prot_sets2.py and remove this
 # comment.
@@ -41,41 +43,80 @@ class TestSets(BaseTest):
         cls.dataset_mda = DataSet.getDataSet('mda')
         cls.dataset_ribo = DataSet.getDataSet('ribo_movies')
 
-    def testImports(self):
+        #
+        # Imports
+        #
+        new = cls.proj.newProtocol  # short notation
+        launch = cls.proj.launchProtocol
+
         # Micrographs
         from pyworkflow.em.protocol.protocol_import import ProtImportMicrographs
-        p_imp_micros = self.proj.newProtocol(ProtImportMicrographs,
-                                            pattern=self.dataset_xmipp.getFile('allMics'),
-                                            samplingRate=1.237, voltage=300)
-        self.proj.launchProtocol(p_imp_micros, wait=True)
-        
+        p_imp_micros = new(ProtImportMicrographs,
+                           pattern=cls.dataset_xmipp.getFile('allMics'),
+                           samplingRate=1.237, voltage=300)
+        launch(p_imp_micros, wait=True)
+        cls.micros = p_imp_micros.outputMicrographs
+
         # Volumes
         from pyworkflow.em.protocol.protocol_import import ProtImportVolumes
-        p_imp_volumes = self.proj.newProtocol(ProtImportVolumes,
-                                         pattern=self.dataset_xmipp.getFile('volumes'),
-                                         samplingRate=9.896)
-        self.proj.launchProtocol(p_imp_volumes, wait=True)
+        p_imp_volumes = new(ProtImportVolumes,
+                            pattern=cls.dataset_xmipp.getFile('volumes'),
+                            samplingRate=9.896)
+        launch(p_imp_volumes, wait=True)
+        cls.vols = p_imp_volumes.outputVolumes
 
         # Movies
         from pyworkflow.em.protocol.protocol_import import ProtImportMovies
-        p_imp_movies = self.proj.newProtocol(ProtImportMovies,
-                                             pattern=self.dataset_ribo.getFile('movies'),
-                                             samplingRate=2.37, magnification=59000,
-                                             voltage=300, sphericalAberration=2.0)
-        self.proj.launchProtocol(p_imp_movies, wait=True)
+        p_imp_movies = new(ProtImportMovies,
+                           pattern=cls.dataset_ribo.getFile('movies'),
+                           samplingRate=2.37, magnification=59000,
+                           voltage=300, sphericalAberration=2.0)
+        launch(p_imp_movies, wait=True)
+        cls.movies = p_imp_movies.outputMovies
 
         # Particles
         from pyworkflow.em.protocol.protocol_import import ProtImportParticles
-        p_imp_particles = self.proj.newProtocol(ProtImportParticles,
-                                                pattern=self.dataset_mda.getFile('particles'),
-                                                samplingRate=3.5)
-        self.proj.launchProtocol(p_imp_particles, wait=True)
+        p_imp_particles = new(ProtImportParticles,
+                              pattern=cls.dataset_mda.getFile('particles'),
+                              samplingRate=3.5)
+        launch(p_imp_particles, wait=True)
+        cls.particles = p_imp_particles.outputParticles
 
         # Coordinates
         # Oh, I don't know of any example of coordinates imported :(
 
-    def testSets(self):
-        pass
+    def testSplit(self):
+        from pyworkflow.em.protocol.protocol_sets import ProtSplitSet
+
+        def split(em_set, n, randomize):
+            "Return a run split protocol over input set em_set."
+            p_split = self.proj.newProtocol(ProtSplitSet)
+            p_split.inputSet.set(em_set)
+            p_split.numberOfSets.set(n)
+            p_split.randomize.set(randomize)
+            self.proj.launchProtocol(p_split, wait=True)
+            return p_split
+
+        def outputs(p):
+            "Iterate over all the elements in the outputs of protocol p."
+            for key, output in p.iterOutputAttributes(EMObject):
+                yield output
+
+        def check(set0, n=2, randomize=False):
+            split_set = split(set0, n=n, randomize=randomize)
+            unsplit_set = [x.strId() for x in set0]
+            for em_set in outputs(split_set):
+                for elem in em_set:
+                    self.assertTrue(elem.strId() in unsplit_set)
+            self.assertTrue(sum(len(x) for x in outputs(split_set)) == len(set0))
+
+        check(self.micros)
+        check(self.micros, randomize=True)
+        check(self.vols)
+        check(self.movies)
+        check(self.particles)
+        check(self.particles, n=4)
+
 
 
 if __name__ == '__main__':
