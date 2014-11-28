aa6a349d69187dfca973acc99877a9c8b9e74e4a
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2014-11-28 15:42:12 +0100

    removing ocurrences of ProtEmxImport from test ctf discrepancy
    Former-commit-id: cf4bbdc2456ab06676f0900ded8d8ca4ec70b62f [formerly b047cf54d855a665bd8290a9470a731fd1bb7c92]
    Former-commit-id: a25c2cd3e3fe2125ea8ce4fc3a587c94e6b9f3f9

--- pyworkflow/tests/em/workflows/test_workflow_xmipp_ctf_discrepancy.py
+++ pyworkflow/tests/em/workflows/test_workflow_xmipp_ctf_discrepancy.py
@@ -27,36 +27,44 @@ Please,  do not  generate or  distribute
 a modified version of this file under its original name. 
 """
 
+import os
 from itertools import izip
+
 from pyworkflow.em.data import SetOfCTF
-from pyworkflow.em.packages.emxlib import ProtEmxImport
 from pyworkflow.em.packages.xmipp3 import XmippProtCTFDiscrepancy
+from pyworkflow.em.protocol import ProtImportMicrographs
 from pyworkflow.object import PointerList
 from test_workflow import TestWorkflow
 import pyworkflow.tests as tests
-import unittest
-import os
+
+
 
 class TestXmippCTFDiscrepancyBase(TestWorkflow):
     @classmethod
     def setUpClass(cls):
         tests.setupTestProject(cls)
         cls.dataset = tests.DataSet.getDataSet('CTFDiscrepancy')
-
+        
     def testCtfdiscrepancyWorkflow(self):
         """ Import  3 EMX files with micrographs and defocus and compare them
         """
         emxFn1 = self.dataset.getFile('emxMicrographCtf1')
         emxFn2 = self.dataset.getFile('emxMicrographCtf2')
         emxFn3 = self.dataset.getFile('emxMicrographCtf3')
-        protEmxImport1 = self.newProtocol(ProtEmxImport,
-                                          inputEMX=emxFn1
+        protEmxImport1 = self.newProtocol(ProtImportMicrographs,
+                                          importFrom=ProtImportMicrographs.IMPORT_FROM_EMX,
+                                          samplingRate=1,
+                                          emxFile=emxFn1
         )
-        protEmxImport2 = self.newProtocol(ProtEmxImport,
-                                          inputEMX=emxFn2
+        protEmxImport2 = self.newProtocol(ProtImportMicrographs,
+                                          importFrom=ProtImportMicrographs.IMPORT_FROM_EMX,
+                                          samplingRate=1,
+                                          emxFile=emxFn2
         )
-        protEmxImport3 = self.newProtocol(ProtEmxImport,
-                                          inputEMX=emxFn3
+        protEmxImport3 = self.newProtocol(ProtImportMicrographs,
+                                          importFrom=ProtImportMicrographs.IMPORT_FROM_EMX,
+                                          samplingRate=1,
+                                          emxFile=emxFn3
         )
         self.launchProtocol(protEmxImport1)
         self.launchProtocol(protEmxImport2)
@@ -76,14 +84,3 @@ class TestXmippCTFDiscrepancyBase(TestWorkflow):
             ctf2.getMicrograph().setFileName(os.path.basename(ctf2.getMicrograph().getFileName()))
             self.assertTrue(ctf1.equalAttributes(ctf2))
 
-if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        className = sys.argv[1]
-        cls = globals().get(className, None)
-        if cls:
-            suite = unittest.TestLoader().loadTestsFromTestCase(cls)
-            unittest.TextTestRunner(verbosity=2).run(suite)
-        else:
-            print "Test: '%s' not found." % className
-    else:
-        unittest.main()
