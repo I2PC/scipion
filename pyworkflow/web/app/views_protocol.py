b0e4d43dd01d8a82a522a7457c35643023cb14ee
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2014-12-03 13:52:01 +0100

    fixed bug when selecting object from browse web
    Former-commit-id: fcffdb1f53e4148a419564406c082e1674805533 [formerly d774dc69420ac90507b11fb011bb248ea6e3ed6c]
    Former-commit-id: 9578e7c458ddd28b60ec7858eb2d63c2971e2f75

--- pyworkflow/web/app/views_protocol.py
+++ pyworkflow/web/app/views_protocol.py
@@ -275,7 +275,10 @@ def setPointerValue(project, attr, htmlValue, paramName, pointer):
     """
     
     if len(htmlValue.strip()) > 0:
-        value, extended = htmlValue.split('::')
+        if '::' in htmlValue:
+            value, extended = htmlValue.split('::')
+        else:
+            value, extended = htmlValue, None
         obj = project.getObject(int(value))  # Get the object from its id
         id1 = attr.getObjId()
         id2 = obj.getObjId()
