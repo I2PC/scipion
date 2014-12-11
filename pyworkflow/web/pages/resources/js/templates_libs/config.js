dcc687609e2137247a84c25bff70b5cffbb5aa0e
josegutab <jose.gutierrez@cnb.csic.es>
2014-12-11 15:18:46 +0100

    updated highlighted links in the protocols help
    Former-commit-id: 0c8181a55f97199b8ded2fa4d4e313e99a787dcd [formerly 1fb05d3df96d2e193bfd56af1a8838ebdf0ecc27]
    Former-commit-id: 1e5e33fd42c2fffe83a8f81960809cd574bfc230

--- pyworkflow/web/pages/resources/js/templates_libs/config.js
+++ pyworkflow/web/pages/resources/js/templates_libs/config.js
@@ -1,3 +1,3 @@
-function getSubDomainURL(){return "/myfirstmap"}
+function getSubDomainURL(){return ""}
 function setSubDomainURL(url){return getSubDomainURL()+url}
 function goWithSubDomainURL(url){window.location.href=setSubDomainURL(url)}
