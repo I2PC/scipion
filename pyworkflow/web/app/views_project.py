9c72f73de9321e62c68cd69f5a82e31c261e22f1
josegutab <jose.gutierrez@cnb.csic.es>
2014-12-11 14:47:11 +0100

    menu_initialvolume.conf created in other folder
    Former-commit-id: 1670c84e276e41409adb8bc194687b3f5c863fe9 [formerly 053791fc481d19a2f05f849f5c61d0a6edc22e0b]
    Former-commit-id: a88a9c3ce05a780dbe914e5b67c5ffdd26c0b463

--- pyworkflow/web/app/views_project.py
+++ pyworkflow/web/app/views_project.py
@@ -374,7 +374,7 @@ def service_projects(request):
 
 def writeCustomMenu(customMenu):
     if not exists(customMenu):
-        f = open(customMenu, 'w')
+        f = open(customMenu, 'w+')
         f.write('''
 [PROTOCOLS]
 
@@ -405,9 +405,9 @@ def create_service_project(request):
         # Filename to use as test data 
         testDataKey = request.GET.get('testData')
         
-        customMenu = os.path.join(os.path.dirname(os.environ['SCIPION_MENU']), 'menu_initvolume.conf')
+        #customMenu = os.path.join(os.path.dirname(os.environ['SCIPION_MENU']), 'menu_initvolume.conf')
+        customMenu = os.path.join(os.environ['HOME'], '.config/scipion/menu_initvolume.conf')
         writeCustomMenu(customMenu)
-        #customMenu = '/home/scipionweb/.config/scipion/menu_initvolume.conf'
         confs = {'protocols': customMenu}
         project = manager.createProject(projectName, confs, graphView=True)   
         
