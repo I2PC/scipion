658940954b15adff434af93e98c0391488b282d2
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2014-12-14 00:40:19 +0100

    code formatting and some other minor fixings
    Former-commit-id: bedd3329ac1244979207bd86d53d13bc8eafc679 [formerly 0061027f05bf4c7f9d337b9a8e4cf62b7031096b]
    Former-commit-id: 82a59c3431a5bc9fe1202d5c0643620c5c96e69b

--- pyworkflow/em/packages/simple/protocol_prime.py
+++ pyworkflow/em/packages/simple/protocol_prime.py
@@ -23,18 +23,22 @@
 # *  e-mail address 'jgomez@cnb.csic.es'
 # *
 # **************************************************************************
-from pyworkflow.em.packages.xmipp3.convert import writeSetOfVolumes, volumeToRow
-from pyworkflow.em.packages.xmipp3.xmipp3 import XmippMdRow
-from protlib_filesystem import deleteFile
 """
 This sub-package contains wrapper around reconstruct_significant Xmipp program
 """
 
-from pyworkflow.em import *  
+import os
+from glob import glob
+
+import pyworkflow.protocol.params as params
+from pyworkflow.utils.path import cleanPath, cleanPattern
+
+import pyworkflow.em as em  
+import simple
 
 
 
-class ProtPrime(ProtInitialVolume):
+class ProtPrime(em.ProtInitialVolume):
     """ Produces one or several initial volumes using reconstruct_significant """
     _label = 'prime'
 
@@ -42,30 +46,50 @@ class ProtPrime(ProtInitialVolume):
     
     def _defineParams(self, form):
         form.addSection('Input')
-        form.addParam('inputClasses', PointerParam, label="Input classes", important=True, 
+        form.addParam('inputClasses', params.PointerParam, label="Input classes", important=True, 
                       pointerClass='SetOfClasses2D, SetOfAverages',
                       help='Select the input classes2D from the project.\n'
                            'It should be a SetOfClasses2D class with class representative')
-        form.addParam('symmetryGroup', TextParam, default='c1',
+        form.addParam('symmetryGroup', params.TextParam, default='c1',
                       label="Symmetry group",
-                      help='cn or dn. For icosahedral viruses, use c5. If no symmetry is present, give c1.')  
-        form.addParam('Nvolumes', IntParam, label='Number of volumes', help="Number of volumes to reconstruct",
-                      default=1)
-        form.addParam('maximumShift', IntParam, default=0, expertLevel=LEVEL_ADVANCED,
-                      label='Maximum shift (px):', help="Set to 0 for free shift search")
-        form.addParam('shiftStep', IntParam, default=1, expertLevel=LEVEL_ADVANCED,
-                      label='Shift step (px):', help="Step for exhaustive shift search")
-        form.addParam('outerMask', FloatParam, default=-1, expertLevel=LEVEL_ADVANCED,
-                      label='Outer mask radius (px):', help="Set to -1 for half the image size")
-        form.addParam('dynamicFilter', BooleanParam, default=False, expertLevel=LEVEL_ADVANCED,
-                      label='Dynamic filtering:', help="Let the program estimate the maximum resolution")
-        form.addParam('maxResolution', FloatParam, default=20, expertLevel=LEVEL_ADVANCED, condition="not dynamicFilter",
-                      label='Max. Resolution (A):', help="The reconstructed volume will be limited to this resolution in Angstroms.")
-        form.addParam('fractionParticles', FloatParam, default=1, expertLevel=LEVEL_ADVANCED,
-                      label='Fraction of particles:', help="Fraction of particles to include in the refinement. 1=all particles, 0.8=80% of particles")
-        form.addParam('molecularWeight', FloatParam, default=-1, expertLevel=LEVEL_ADVANCED,
-                      label='Molecular weight (kD):', help="Molecular weight in kilodaltons, set to -1 for no constraint")
-        form.addParam('keepIntermediate', BooleanParam, default=False, expertLevel=LEVEL_ADVANCED,
+                      help='cn or dn. For icosahedral viruses, use c5. \n'
+                           'If no symmetry is present, give c1.')  
+        form.addParam('Nvolumes', params.IntParam, default=1, 
+                      label='Number of volumes', 
+                      help="Number of volumes to reconstruct")
+        form.addParam('maximumShift', params.IntParam, default=0, 
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label='Maximum shift (px):', 
+                      help="Set to 0 for free shift search")
+        form.addParam('shiftStep', params.IntParam, default=1, 
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label='Shift step (px):', 
+                      help="Step for exhaustive shift search")
+        form.addParam('outerMask', params.FloatParam, default=-1, 
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label='Outer mask radius (px):', 
+                      help="Set to -1 for half the image size")
+        form.addParam('dynamicFilter', params.BooleanParam, default=False, 
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label='Dynamic filtering:', 
+                      help="Let the program estimate the maximum resolution")
+        form.addParam('maxResolution', params.FloatParam, default=20, 
+                      expertLevel=params.LEVEL_ADVANCED, condition="not dynamicFilter",
+                      label='Max. Resolution (A):', 
+                      help="The reconstructed volume will be limited to \n"
+                           "this resolution in Angstroms.")
+        form.addParam('fractionParticles', params.FloatParam, default=1, 
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label='Fraction of particles:', 
+                      help="Fraction of particles to include in the refinement. \n"
+                           "1=all particles, 0.8=80% of particles")
+        form.addParam('molecularWeight', params.FloatParam, default=-1, 
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label='Molecular weight (kD):', 
+                      help="Molecular weight in kilodaltons, \n"
+                           "set to -1 for no constraint")
+        form.addParam('keepIntermediate', params.BooleanParam, default=False, 
+                      expertLevel=params.LEVEL_ADVANCED,
                       label='Keep intermediate volumes',
                       help='Keep all volumes along iterations')
 
@@ -94,73 +118,68 @@ class ProtPrime(ProtInitialVolume):
         #              [dynlp=<yes|no{no}>] [nstates=nstates to reconstruct>] [frac=<fraction of ptcls to include{1}>]
         #              [mw=<molecular weight (in kD)>] [oritab=<previous rounds alignment doc>] [nthr=<nr of OpenMP threads{1}>]
 
-        inputClasses=self.inputClasses.get()
+        inputClasses = self.inputClasses.get()
         xdim, _, _ = inputClasses.getDimensions()
-        args="stk=classes.spi box=%d smpd=%f pgrp=%s"%(xdim,inputClasses.getSamplingRate(),self.symmetryGroup)
+        args = "stk=classes.spi box=%d smpd=%f pgrp=%s" % (xdim, inputClasses.getSamplingRate(), self.symmetryGroup)
+        
         if self.dynamicFilter:
-            args+=" dynlp=yes"
+            args += " dynlp=yes"
         else:
-            args+=" lp=%f"%self.maxResolution.get()
-        args+=" trs=%d trsstep=%d"%(self.maximumShift.get(),self.shiftStep.get())
-        args+=" nstates=%d"%self.Nvolumes.get()
-        args+=" nthr=%d"%self.numberOfThreads.get()
-        if self.outerMask>0:
-            args+=" ring2=%f"%self.outerMask.get()
-        args+=" frac=%f"%self.fractionParticles.get()
-        if self.molecularWeight>0:
-            args+=" mw=%f"%self.molecularWeight.get()
+            args += " lp=%f" % self.maxResolution
+        args += " trs=%d trsstep=%d" % (self.maximumShift, self.shiftStep)
+        args += " nstates=%d" % self.Nvolumes
+        args += " nthr=%d" % self.numberOfThreads
         
+        if self.outerMask > 0:
+            args += " ring2=%f" % self.outerMask
+        args += " frac=%f" % self.fractionParticles
         
-        self._enterDir(self._getExtraPath(''))
-        self.runJob("simple_prime", args)
-        self._leaveDir()
+        if self.molecularWeight > 0:
+            args += " mw=%f" % self.molecularWeight
+        
+        self.runJob("simple_prime", args, cwd=self._getExtraPath())
 
     def getLastIteration(self):
-        self._enterDir(self._getExtraPath(''))
-        self.lastIter=1
-        while True:
-            if os.path.exists("recvol_state1_iter%d.spi"%self.lastIter):
-                self.lastIter+=1
-            else:
-                self.lastIter-=1
-                break
-        self._leaveDir()
+        lastIter = 1
+        pattern = self._getExtraPath("recvol_state1_iter%d.spi")
+        while os.path.exists(pattern % lastIter):
+            lastIter += 1
+        return lastIter - 1
 
     def cleanPrime(self):
-        self._enterDir(self._getExtraPath(''))
+        self._enterDir(self._getExtraPath())
         cleanPath("cmdline.txt")
-        for fn in glob("*.txt"):
-            cleanPath(fn)
-        for fn in glob("startvol_state*.spi"):
-            cleanPath(fn)
-
+        cleanPattern("*.txt")
+        cleanPattern("startvol_state*.spi")
         # Get last iteration
-        for i in range(1,self.lastIter):
-            for fn in glob("recvol_state*_iter%d.spi"%i):
-                cleanPath(fn)
+        for i in range(1, self.getLastIteration()):
+            cleanPattern("recvol_state*_iter%d.spi"%i)
         self._leaveDir()
     
     def createOutputStep(self):
-        if self.lastIter<=1:
+        lastIter = self.getLastIteration()
+        
+        if lastIter <= 1:
             return
         
-        if self.Nvolumes==1:
-            vol = Volume()
-            print "Adding ",self._getExtraPath('recvol_state1_iter%d.spi'%self.lastIter)
-            vol.setLocation(self._getExtraPath('recvol_state1_iter%d.spi'%self.lastIter))
+        if self.Nvolumes == 1:
+            vol = em.Volume()
+            print "Adding ",self._getExtraPath('recvol_state1_iter%d.spi' % lastIter)
+            vol.setLocation(self._getExtraPath('recvol_state1_iter%d.spi' % lastIter))
             vol.setSamplingRate(self.inputClasses.get().getSamplingRate())
+            self._defineOutputs(outputVol=vol)
         else:
             vol = self._createSetOfVolumes()
             vol.setSamplingRate(self.inputClasses.get().getSamplingRate())
-            fnVolumes=glob(self._getExtraPath('recvol_state*_iter%d.spi')%self.lastIter)
-            print "Adding ",self._getExtraPath('recvol_state*_iter%d.spi'%self.lastIter)
+            fnVolumes=glob(self._getExtraPath('recvol_state*_iter%d.spi') % lastIter)
+            print "Adding ",self._getExtraPath('recvol_state*_iter%d.spi' % lastIter)
             fnVolumes.sort()
             for fnVolume in fnVolumes:
-                aux=Volume()
+                aux=em.Volume()
                 aux.setLocation(fnVolume)
                 vol.append(aux)
+            self._defineOutputs(outputVolumes=vol)
 
-        self._defineOutputs(outputVol=vol)
         self._defineSourceRelation(vol, self.inputClasses)
 
     #--------------------------- INFO functions --------------------------------------------
@@ -174,6 +193,6 @@ class ProtPrime(ProtInitialVolume):
         return ['Elmlund2013']
     
     def _methods(self):
-        retval="We used prime [Elmlund2013] to produce an initial volume from the set of classes %s."%\
-           self.inputClasses.get().getNameId()
-        return [retval]
+        retval="We used *simple_prime* program [Elmlund2013] to produce an initial volume from the set of classes %s."
+           
+        return [retval % self.inputClasses.get().getNameId()]
