34c47b643c15041343a982bc709ec73227f9ee13
Airen Zaldivar Peraza <airenzp@gmail.com>
2014-12-18 18:20:02 +0100

    registering volumes from proj matching
    Former-commit-id: 0ff642a49c1079694db488ec02559041e9355a8f [formerly fbf1b81900ce2f32f5d59aa6fc6d59c65f0d3c66]
    Former-commit-id: 5037aa6e57cac31bb721ab2658e7c4fb42a7901d

--- pyworkflow/em/packages/xmipp3/viewer_ml2d.py
+++ pyworkflow/em/packages/xmipp3/viewer_ml2d.py
@@ -100,7 +100,6 @@ class XmippML2DViewer(ProtocolViewer):
 
         
         if not exists(fnSqlite): # Create the sqlite if not exists
-            print "   print creating fnSqlite=", fnSqlite
             classes2DSet = SetOfClasses2D(filename=fnSqlite)
             classes2DSet.setImages(self.protocol.inputParticles.get())
             readSetOfClasses2D(classes2DSet, fnXmd)
