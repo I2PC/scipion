fd0bce07fc43fe0bd3561fe59d7239cd9a504a16
rmarabini <roberto@cnb.csic.es>
2015-01-12 10:25:29 +0100

    set tests
    Former-commit-id: 010862aa728b8a2594161993f7548ccab8b4a99f [formerly 2b542d04e6e098cb38e7e017fec66840ac2adfc4]
    Former-commit-id: 9d541640a977f322458bbd967d2951808e02e34b

---
+++ pyworkflow/tests/em/protocols/test_protocols_sets.py
@@ -0,0 +1,105 @@
+# ***************************************************************************
+# * Authors:     Roberto Marabini (roberto@cnb.csic.es)
+# *
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'xmipp@cnb.csic.es'
+# ***************************************************************************/
+
+import pyworkflow.tests as tests
+from pyworkflow.em.data import Particle, SetOfParticles
+from pyworkflow.em.protocol import ProtSplitSet
+from pyworkflow.tests import envVarOn
+from pyworkflow.protocol import MODE_RESTART
+from pyworkflow.em.protocol import ProtImportParticles
+from os import remove
+from os.path import exists
+import xmipp
+
+class TestSetsBase(tests.BaseTest):
+    @classmethod
+    def setUpClass(cls):
+        tests.setupTestProject(cls)
+        #cls.dataset = tests.DataSet.getDataSet('emx')
+    
+    @classmethod
+    def newProtocol(cls, protocolClass, **kwargs):
+        """ Create new protocols instances throught the project
+        and return a newly created protocol of the given class
+        """
+        # Try to continue from previous execution
+        if envVarOn('SCIPION_TEST_CONTINUE'):
+            candidates = cls.proj.mapper.selectByClass(protocolClass.__name__)
+            if candidates:
+                c = candidates[0]
+                if c.isFinished():
+                    setattr(c, '_run', False)
+                else:
+                    c.runMode.set(MODE_RESTART)
+                return c
+        return cls.proj.newProtocol(protocolClass, **kwargs)
+
+    def test_orderBy(self):
+        """ create set of particles and orderby a given attribute
+        """
+        #create set of particles
+
+        inFileNameMetadata = self.getOutputPath('particlesOrderBy.sqlite')
+        inFileNameData = '/tmp/images.stk'
+        #create fake binary file
+        #xmipp.createEmptyFile(inFileNameData, 32, 32,9)
+        #open(inFileNameData, "a").close()
+
+        imgSet = SetOfParticles(filename=inFileNameMetadata)
+        imgSet.setSamplingRate(1.5)
+        img = Particle()
+
+        for i in range(1, 10):
+            img.setLocation(i, inFileNameData)
+            img.setMicId(i%3)
+            img.setClassId(i%5)
+            imgSet.append(img)
+            img.cleanObjId()
+        imgSet.write()
+        imgSet.close()
+        #now import the dataset
+        #this newProtocols should be a class method?
+        prot1 = self.newProtocol(ProtImportParticles,
+                                 importFrom=ProtImportParticles.IMPORT_FROM_SCIPION,
+                                 sqliteFile=inFileNameMetadata,
+                                 magnification=10000,
+                                 samplingRate=1.5
+                                 )
+        prot1.setObjLabel('from sqlite (test-sets)')
+        self.launchProtocol(prot1)
+
+        if prot1.outputParticles is None:
+            raise Exception('Import of images: %s, failed. outputParticles is None.' % inFileNameMetadata)
+        #save it
+        protSplitSet   = self.newProtocol(ProtSplitSet,
+                                          inputSet=prot1.outputParticles,
+                                          numberOfSets=2,
+                                          randomize=True)
+        protSplitSet.inputSet.set(imgSet)
+        self.launchProtocol(protSplitSet)
+        setParticles01 = protSplitSet.outputParticles01.read()
+        setParticles01.printAll()
+        #assert
+        #delete file
+        ##remove(inFileNameMetadata)
+        ##remove(inFileNameData)
