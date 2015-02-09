0fb3f2da85876ead56d39ab1d507db48affdf2ac
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2015-02-09 08:19:01 +0100

    removed commented code and allowed to show more than one file
    Former-commit-id: 580c8f017f7fa58f3b316c0486b88d5ae7c34573 [formerly f55fdae0f0b6c549c0fe3a47ecba007f0971aa53]
    Former-commit-id: 67f612144756bc34c644ffa3cb58ccae6d42bbd1

--- pyworkflow/apps/pw_viewer.py
+++ pyworkflow/apps/pw_viewer.py
@@ -1,36 +1,16 @@
 #!/usr/bin/env python
 
 
-# from protlib_xmipp import ScriptShowJ
-# 
-# class ScriptScipionViewer(ScriptShowJ):
-#     def __init__(self):
-#         ScriptShowJ.__init__(self, 'xmipp.viewer.scipion.ScipionViewer')
-#         
-#     def defineOtherParams(self):
-#         ScriptShowJ.defineOtherParams(self)
-#         self.addParamsLine('  --command  <button> <script> <projectid> <protid> <dbpath>  : Command button will be included on the GUI to execute script associated');
-#        
-#         
-#     def readOtherParams(self):
-#          ScriptShowJ.readOtherParams(self)
-#          cmdbutton = self.getParam('--command', 0)
-#          cmdscript = self.getParam('--command', 1)
-#          projectid = self.getParam('--command', 2)
-#          protid = self.getParam('--command', 3)
-#          dbpath = self.getParam('--command', 4)
-#          self.args += " --command %(cmdbutton)s %(cmdscript)s %(projectid) %(protid) %(dbpath)" %locals()
-#         
-# if __name__ == '__main__':
-#     ScriptScipionViewer().tryRun()
 import sys
 from pyworkflow.em.viewer import DataView
 
-if __name__ == '__main__':
+
+
+if __name__ == '__main__':    
     
-     if len(sys.argv) > 1:
-        fn = sys.argv[1]
-        DataView(fn).show()
-     else:
-        print "usage: pw_viewer.py filename"
+    if len(sys.argv) > 1:
+        for fn in sys.argv[1:]:
+            DataView(fn).show()
+    else:
+        print "usage: pw_viewer.py file1 [file2 file3 ... fileN]"
      
