71013ba2ae8a193fabd9d1c8a8b2233abdb72be7
rmarabini <roberto@cnb.csic.es>
2015-03-23 09:57:05 +0100

    chimera virus stuff
    Former-commit-id: e67e3d4e24d4b1408935a1f59d352d4cfb17facb [formerly 6a4d4d64af11092d970c3c6f5523f4f1c1fea708]
    Former-commit-id: bd95ae508308c1fadd5a416ffab344d6ca5f9837

--- pyworkflow/em/chimera_server.py
+++ pyworkflow/em/chimera_server.py
@@ -19,7 +19,7 @@ import sys
 
 class ChimeraServer:
     
-    def __init__(self):
+    def __init__(self,centerVolume=True):
         #print 'init'
         address = ''
         port = int(sys.argv[1])
@@ -32,8 +32,9 @@ class ChimeraServer:
         chimera.triggers.addHandler(chimera.APPQUIT, self.onAppQuit, None)
         # Add sub-classes handlers
         self.addHandlers()
-
+        self.centerVolume = centerVolume
         self.initListenShowJ()
+
 
     def addHandlers(self):
         """ Override this methods to specify which Chimera triggers want
@@ -70,17 +71,8 @@ class ChimeraServer:
                     elif msg == 'voxel_size':
                         self.voxelSize = self.vol_conn.recv()
                         cmd = "volume #0 voxelSize %s"%self.voxelSize
-                        #print cmd
                         runCommand(cmd)
-                        #print reference spheres. Usefull for debuging, remove
-                        runCommand('shape sphere radius %s center %s,%s,%s color %s '%(self.voxelSize, -64 * self.voxelSize ,   0                  , 0 , 'orange'))
-                        runCommand('shape sphere radius %s center %s,%s,%s color %s '%(self.voxelSize,   0                  , -64 * self.voxelSize , 0 , 'forest green'))
-                        runCommand('shape sphere radius %s center %s,%s,%s color %s '%(self.voxelSize,   0                  ,   0                  , -64 * self.voxelSize  , 'magenta'))
-                        runCommand('shape sphere radius %s center %s,%s,%s color %s '%(self.voxelSize,  64 * self.voxelSize ,   0                  , 0 , 'red'))
-                        runCommand('shape sphere radius %s center %s,%s,%s color %s '%(self.voxelSize,   0                  ,  64 * self.voxelSize , 0 , 'green'))
-                        runCommand('shape sphere radius %s center %s,%s,%s color %s '%(self.voxelSize,   0                  ,   0                  , 64 * self.voxelSize , 'blue'))
-                        runCommand("focus")
-                        #end debug
+
 
                     elif msg == 'command_list':
                         commandList = self.vol_conn.recv()
@@ -106,17 +98,20 @@ class ChimeraServer:
 
     def listenShowJ(self):
         try:
+
             om = chimera.openModels
             mlist = om.list()
+            m = mlist[0]
             #assume that volume is 0 may be dangerous
             #this should be in init but it does not work there
-            m = mlist[0]
-            centerVec = m.bbox()[1].center().toVector()
-            xform = chimera.Xform.xform(1, 0, 0, -centerVec[0],
-                                        0, 1, 0, -centerVec[1],
-                                        0, 0, 1, -centerVec[2], orthogonalize=True)
-            m.openState.globalXform(xform)
-            #end of this
+
+            if self.centerVolume:
+                self.centerVec = m.bbox()[1].center().toVector()
+                xform = chimera.Xform.xform(1, 0, 0, -self.centerVec[0],
+                                            0, 1, 0, -self.centerVec[1],
+                                            0, 0, 1, -self.centerVec[2], orthogonalize=True)
+                m.openState.globalXform(xform)
+                #end of this
 
             self.motion = identity(3)
 
@@ -164,22 +159,64 @@ def printCmd(cmd):
 
 class ChimeraVirusServer(ChimeraServer):
 
+    def __init__(self):
+        ChimeraServer.__init__(self, centerVolume=True)
+
     def addHandlers(self):
         """ Override this methods to specify which Chimera triggers want
         to be handled.
         """
         pass
         #chimera.triggers.addHandler(chimera.MOTION_STOP, self.onMotionStop, None)####
+        chimera.triggers.addHandler('selection changed', self.onselectionChanged, None)####
 
+    def onselectionChanged(self, trigName, myData, trigData):
+        sel = chimera.selection.currentGraphs()
+        self.vol_conn.send('id')
+        self.vol_conn.send(sel[0].id)#send serialized motion
 
     def handleInitMessage(self, msg):
         """execute a single command and return values"""
         if msg == 'hk_icosahedron_lattice':
             from IcosahedralCage import cages
-            h,k,radius,sym = self.vol_conn.recv()
-            varray, tarray, hex_edges = cages.hk_icosahedron_lattice(h,k,radius,sym)
-            self.vol_conn.send('hk_icosahedron_lattice')
-            self.vol_conn.send(varray)
+            h,k,radius,shellRadius,spheRadius,sym,sphere,color = \
+                                      self.vol_conn.recv()
+            ###
+            #get vertexes of canonical triangle (20 per icosahedron)
+            #get triangles defined by h k for canonical triangle
+            corners, triangles, t_hex_edges = cages.hk_triangle(h, k)
+
+            #get vertex for icosahedron
+            #get vertex for each face
+            from Icosahedron import icosahedron_geometry
+            ivarray, itarray = icosahedron_geometry(sym)
+
+            tlist = []
+            #for a single face
+            #map triangles to a single face in the given orientation
+            for i0,i1,i2 in itarray:
+                face = ivarray[i0], ivarray[i1], ivarray[i2]
+                tmap = cages.triangle_map(corners, face)
+                tlist.extend(cages.map_triangles(tmap, triangles))
+                break#!!!!!!!!!!!!!!!!!!!!!!!!!
+            va, ta = cages.surface_geometry(tlist, tolerance = 1e-5)
+
+            from numpy import multiply
+            multiply(va, shellRadius, va)    # Scale to requested radius            for point in va:
+            for point in va:
+                command = 'shape sphere radius %s center %s,%s,%s color %s '%\
+                                                (spheRadius,
+                                                 point[0],
+                                                 point[1],
+                                                 point[2],
+                                                 color
+                                                 )
+                runCommand(command)
+            self.vol_conn.send('axis')
+            self.vol_conn.send(va)#send serialized motion
+            ###!!!!hex_edges = array(t_hex_edges * len(itarray), intc)
+
+            #show va spheres
 
 if len(sys.argv)> 1:
    serverName = sys.argv[2]
