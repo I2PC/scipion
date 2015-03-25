5b2e7d8c8d58980f47a0fa354215c043ac7042a5
rmarabini <roberto@cnb.csic.es>
2015-03-25 12:22:43 +0100

    several virus and chimera related stuff
    Former-commit-id: 5e4e924af1b1cf8f52a0aa91e4ff0d5911c04ef9

--- protocols/protlib_chimera.py
+++ protocols/protlib_chimera.py
@@ -41,7 +41,8 @@ import sys
 
 class XmippChimeraClient:
     
-    def __init__(self, volfile, port, angulardist=[], voxelSize=None):
+    def __init__(self, volfile, port, angulardist=None,
+                 voxelSize=None, extraCommandList=None):
         
         if volfile is None:
             raise ValueError(volfile)
@@ -49,7 +50,7 @@ class XmippChimeraClient:
             [index, file] = volfile.split('@'); 
         else :
             file = volfile 
-
+        self.extraCommandList = extraCommandList
         self.volfile = volfile
         self.voxelSize = voxelSize
         self.address = ''
@@ -59,8 +60,9 @@ class XmippChimeraClient:
         self.client = Client((self.address, self.port), authkey=self.authkey)
         
         printCmd('initVolumeData')
-        self.initVolumeData()   
-        
+        self.initVolumeData()#fills self.vol
+
+        #angular dist is an special case should not be here ROB
         if angulardist:
             self.angulardistfile = angulardist[0]
             self.spheres_color = angulardist[1]
@@ -68,12 +70,29 @@ class XmippChimeraClient:
             spheres_maxradius = angulardist[3]
             self.spheres_distance = float(spheres_distance) if not spheres_distance == 'default' else 0.75 * max(self.xdim, self.ydim, self.zdim)
             self.spheres_maxradius = float(spheres_maxradius) if not spheres_maxradius == 'default' else 0.02 * self.spheres_distance
-               
+        else:
+            self.angulardistfile = None
+
         printCmd('openVolumeOnServer')
         self.openVolumeOnServer(self.vol)
         self.initListenThread()
     
     
+    def openVolumeOnServer(self, volume):
+        self.send('open_volume', volume)
+        if not self.voxelSize is None:
+            self.send('voxelSize', self.voxelSize)
+        if not self.angulardistfile is None:
+            self.loadAngularDist()
+            self.send('draw_angular_distribution', self.angulardist)
+        if not self.extraCommandList is None:
+            for command in self.extraCommandList:
+               self.send("command",command)
+               f = open("/tmp/kk.txt", "a")
+               f.write(command + " <-- client")
+               f.close()
+        self.client.send('end')
+            
     def loadAngularDist(self):
 
         md = MetaData(self.angulardistfile)
@@ -86,23 +105,23 @@ class XmippChimeraClient:
             anglePsiLabel = RLN_ORIENT_PSI
         if not md.containsLabel(MDL_WEIGHT):
             md.fillConstant(MDL_WEIGHT, 1.)
-            
+
         maxweight = md.aggregateSingle(AGGR_MAX, MDL_WEIGHT)
         minweight = md.aggregateSingle(AGGR_MIN, MDL_WEIGHT)
         interval = maxweight - minweight
         if interval < 1:
             interval = 1
         minweight = minweight - 1#to avoid 0 on normalized weight
-        
-        self.angulardist = []  
+
+        self.angulardist = []
         x2=self.xdim/2
         y2=self.ydim/2
         z2=self.zdim/2
         #cofr does not seem to work!
         #self.angulardist.append('cofr %d,%d,%d'%(x2,y2,z2))
-        
+
         for id in md:
-            
+
             rot = md.getValue(angleRotLabel, id)
             tilt = md.getValue(angleTiltLabel, id)
             psi = md.getValue(anglePsiLabel, id)
@@ -116,24 +135,15 @@ class XmippChimeraClient:
             z = z * self.spheres_distance + z2
             command = 'shape sphere radius %s center %s,%s,%s color %s '%(radius, x, y, z, self.spheres_color)
 
-            self.angulardist.append(command)    
+            self.angulardist.append(command)
             printCmd(command)
-            
+
+
     def send(self, cmd, data):
         #print cmd
         self.client.send(cmd)
         self.client.send(data)
-        
-        
-    def openVolumeOnServer(self, volume):
-         self.send('open_volume', volume)
-         if not self.voxelSize is None:
-             self.send('voxelSize', self.voxelSize)
-         if not self.angulardistfile is None:
-             self.loadAngularDist()
-             self.send('draw_angular_distribution', self.angulardist)
-         self.client.send('end')
-        
+
 
     def initListenThread(self):
             self.listen_thread = Thread(target=self.listen)
@@ -234,7 +244,57 @@ class XmippProjectionExplorer(XmippChimeraClient):
             self.listen_thread = Thread(target=self.listen)
             self.listen_thread.daemon = True
             self.listen_thread.start() 
+
+class XmippVirusSym(XmippChimeraClient):
+    
+    def __init__(self, volfile, port, voxelSize=None, h=5, k=0, sym='n25',radius=100):
+        angulardist = None
+        extraComamndList=['move -64,-64,-64 models #0   ',
+                          'hkcage %d %d orientation %s'%(h,k,sym),
+                          'hkcage %d %d orientation %s'%(h,7,sym)
+        ]
+        XmippChimeraClient.__init__(self, volfile, port,
+                                    angulardist, voxelSize,extraComamndList)
+        
+
+    def rotate(self, rot, tilt, psi):
+        printCmd('image.projectVolumeDouble')
+        self.fourierprojector.projectVolume(self.projection, rot, tilt, psi)
+        printCmd('flipud')
+        self.vol = flipud(getImageData(self.projection))
+        printCmd('iw.updateData')
+        self.iw.updateData(self.vol)
+        printCmd('end rotate')
+    
+        
+    def exit(self):
+        XmippChimeraClient.exit(self)
+        if hasattr(self, "iw"):
+            self.iw.root.destroy()
+        
+    def answer(self, msg):
+        XmippChimeraClient.answer(self, msg)
+        if msg == 'motion_stop':
+            data = self.client.recv()#wait for data
+            printCmd('reading motion')
+            self.motion = array(data)
+            printCmd('getting euler angles')
+            rot, tilt, psi = Euler_matrix2angles(self.motion)
+            printCmd('calling rotate')  
+            self.rotate(rot, tilt, psi)
+        else:
+           pass
             
+            
+    def exitClient(self):
+        if not self.listen:
+            sys.exit(0)
+    
+    def initListenThread(self):
+            self.listen_thread = Thread(target=self.listen)
+            self.listen_thread.daemon = True
+            self.listen_thread.start() 
+     
 def printCmd(cmd):
         timeformat = "%S.%f" 
         #print datetime.now().strftime(timeformat) + ' %s'%cmd
