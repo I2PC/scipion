328f6b53ed5e5dd421afc0629c9c4390ea2e92bf
Airen Zaldivar Peraza <airenzp@gmail.com>
2015-04-10 14:20:38 +0200

    adding psd to xmipp supported extensions

--- software/em/xmipp/java/src/xmipp/jni/Filename.java
+++ software/em/xmipp/java/src/xmipp/jni/Filename.java
@@ -72,7 +72,7 @@ public class Filename {
 			EXT_ERR, EXT_OUT, EXT_BOX };
         
         public final static String[] XMIPP_SUPPORTED = new String[]{EXT_RAW, EXT_SPI, EXT_XMP, EXT_VOL, EXT_STK, EXT_MRC, EXT_MRC2, EXT_MRCS2, EXT_MRCS, 
-            EXT_HED, EXT_IMG, EXT_INF, EXT_RAW, EXT_TIF, EXT_DM3, EXT_SER, EXT_SPE, EXT_EM, EXT_EMS, EXT_PIF, EXT_HDF, EXT_HDF5, EXT_H5};
+            EXT_HED, EXT_IMG, EXT_INF, EXT_RAW, EXT_TIF, EXT_DM3, EXT_SER, EXT_SPE, EXT_EM, EXT_EMS, EXT_PIF, EXT_HDF, EXT_HDF5, EXT_H5, EXT_PSD};
 
 	public static boolean isPSD(String filename) {
 		return filename != null && filename.endsWith(EXT_PSD);
