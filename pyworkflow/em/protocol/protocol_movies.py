2606f71d7a304c997f840fafffd48ebabca7c46d
rmarabini <roberto@cnb.csic.es>
2015-06-15 22:43:01 +0200

    store sampling rate, needed for align_movie_correlation

--- pyworkflow/em/protocol/protocol_movies.py
+++ pyworkflow/em/protocol/protocol_movies.py
@@ -81,6 +81,7 @@ class ProtProcessMovies(ProtPreprocessMicrographs):
         # Build the list of all processMovieStep ids by 
         # inserting each of the steps for each movie
         allMovies = [self._insertMovieStep(movie) for movie in self.inputMovies.get()]
+        self.samplingRate = self.inputMovies.get().getSamplingRate()
         self._insertFunctionStep('createOutputStep', prerequisites=allMovies)
 
     def _insertMovieStep(self, movie):
