4e18e8f4669e0d7597f77eba81eab42158ee09bf
rmarabini <roberto@cnb.csic.es>
2015-07-03 16:19:04 +0200

    make license dual

--- pyworkflow/em/packages/emxlib/emxlib.py
+++ pyworkflow/em/packages/emxlib/emxlib.py
@@ -4,8 +4,8 @@
  *              J.M. De la Rosa Trevin (jmdelarosa@cnb.csic.es)
  *
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * You may use this program under the terms of either the  Apache License 
+ * or the GNU General Public License (GPL) as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
