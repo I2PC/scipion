00fbd95123c067d675f16c405431f1cf9a1d683b
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2015-07-09 11:07:28 +0200

    added first version of a protocol to extract coordinates from particles

--- pyworkflow/em/protocol/__init__.py
+++ pyworkflow/em/protocol/__init__.py
@@ -41,6 +41,7 @@ from protocol_ctf_assign import ProtCTFAssign
 from protocol_alignment_assign import ProtAlignmentAssign
 from protocol_batch import *
 from protocol_classes_consensus import ProtClassesConsensus, ViewerClassesConsensus
+from protocol_extract_coordinates import ProtExtractCoords
 
 from parallel import ProtTestParallel
 
