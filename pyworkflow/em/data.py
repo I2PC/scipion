e737e1303b094ce754e7f80f82bb94d77cfda555
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2015-08-10 15:20:57 +0200

    getMicName() now return None if not set for Coordinate class

--- pyworkflow/em/data.py
+++ pyworkflow/em/data.py
@@ -1075,10 +1075,7 @@ class Coordinate(EMObject):
         self._micName.set(micName)
     
     def getMicName(self):
-        if self._micName is not None:
-            return self._micName.get()
-        else:
-            self.getFileName()
+        return self._micName.get()
 
 
 class SetOfCoordinates(EMSet):
