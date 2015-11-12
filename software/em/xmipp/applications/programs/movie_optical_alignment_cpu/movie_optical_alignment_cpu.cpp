5047f78c645e3e57c44a6fd869214484525c2ea8
vahid <vabrishami@cnb.csic.es>
2015-11-12 10:40:54 +0100

    Computing the half-half PSD inside the protocol itself

--- software/em/xmipp/applications/programs/movie_optical_alignment_cpu/movie_optical_alignment_cpu.cpp
+++ software/em/xmipp/applications/programs/movie_optical_alignment_cpu/movie_optical_alignment_cpu.cpp
@@ -51,8 +51,7 @@ public:
     FileName fname, foname, gianRefFilename, darkRefFilename;
     MultidimArray<double> gainImage, darkImage;
     int winSize, gpuDevice, fstFrame, lstFrame;
-    int psdPieceSize;
-    bool doAverage, psd, saveCorrMovie;
+    bool doAverage, saveCorrMovie;
     bool gainImageCorr, darkImageCorr;
 
     void defineParams()
@@ -64,7 +63,6 @@ public:
         addParamsLine("     [--ned <int=0>]     : last frame used in alignment (0 = last frame in the movie ");
         addParamsLine("     [--winSize <int=150>]     : window size for optical flow algorithm");
         addParamsLine("     [--simpleAverage]: if we want to just compute the simple average");
-        addParamsLine("     [--psd]             : save raw PSD and corrected PSD");
         addParamsLine("     [--ssc]             : save corrected stack");
         addParamsLine("     [--gain <gainReference>]             : gain reference");
         addParamsLine("     [--dark <darkReference>]             : dark reference");
@@ -90,7 +88,6 @@ public:
         lstFrame  = getIntParam("--ned");
         winSize   = getIntParam("--winSize");
         doAverage = checkParam("--simpleAverage");
-        psd = checkParam("--psd");
         saveCorrMovie = checkParam("--ssc");
 
 #ifdef GPU
@@ -278,7 +275,7 @@ public:
         ImageGeneric movieStack, movieStackNormalize;
         Image<double> II;
         MetaData MD; // To save plot information
-        FileName motionInfFile, correctedPSDFile, rawPSDFile;
+        FileName motionInfFile;
         ArrayDim aDim;
 
         // For measuring times (both for whole process and for each level of the pyramid)
@@ -349,32 +346,17 @@ public:
         // Compute the average of the whole stack
         fstFrame++; // Just to adapt to Li algorithm
         lstFrame++; // Just to adapt to Li algorithm
-        psdPieceSize = 400; // Currently we set it as a constant
         if (lstFrame>=imagenum || lstFrame==1)
             lstFrame=imagenum;
         imagenum=lstFrame-fstFrame+1;
         levelNum=sqrt(double(imagenum));
         computeAvg(fname, fstFrame, lstFrame, avgCurr);
         // if the user want to save the PSD
-        if (psd)
+        if (doAverage)
         {
             II() = avgCurr;
             II.write(foname);
-            if (doAverage)
-                rawPSDFile=foname.removeLastExtension()+"_corrected";
-            else
-                rawPSDFile=foname.removeLastExtension()+"_raw";
-            std::cerr<<"The file name is"<<rawPSDFile<<std::endl;
-            String args=formatString("--micrograph %s --oroot %s --dont_estimate_ctf --pieceDim %d --overlap 0.7",
-                                     foname.c_str(), rawPSDFile.c_str(), psdPieceSize);
-            String cmd=(String)" xmipp_ctf_estimate_from_micrograph "+args;
-            std::cerr<<"Computing the raw FFT"<<std::endl;
-            if (system(cmd.c_str())==-1)
-                REPORT_ERROR(ERR_UNCLASSIFIED,"Cannot open shell");
-            if (doAverage)
-                return 0;
-            else
-                foname.deleteFile();
+            return 0;
         }
         xmipp2Opencv(avgCurr, avgcurr);
         cout<<"Frames "<<fstFrame<<" to "<<lstFrame<<" under processing ..."<<std::endl;
@@ -493,30 +475,6 @@ public:
         {
             II()=outputMovie;
             II.write(foname.replaceExtension("mrcs"));
-        }
-
-        if (psd)
-        {
-            Image<double> psdCorr, psdRaw;
-            MultidimArray<double> psdCorrArr, psdRawArr;
-            correctedPSDFile=foname.removeLastExtension()+"_corrected";
-            String args=formatString("--micrograph %s --oroot %s --dont_estimate_ctf --pieceDim %d --overlap 0.7",
-                                     foname.c_str(), correctedPSDFile.c_str(), psdPieceSize);
-            String cmd=(String)" xmipp_ctf_estimate_from_micrograph "+args;
-            std::cerr<<"Computing the corrected FFT"<<std::endl;
-            if (system(cmd.c_str())==-1)
-                REPORT_ERROR(ERR_UNCLASSIFIED,"Cannot open shell");
-            psdRaw.read(rawPSDFile+".psd");
-            psdCorr.read(correctedPSDFile+".psd");
-            psdCorrArr=psdCorr();
-            psdRawArr=psdRaw();
-            for (size_t i=0;i<psdPieceSize;i++)
-                for (size_t j=0;j<size_t(psdPieceSize/2);j++)
-                    DIRECT_A2D_ELEM(psdCorrArr,i,j)=DIRECT_A2D_ELEM(psdRawArr,i,j);
-            psdCorr()=psdCorrArr;
-            psdCorr.write(correctedPSDFile+".psd");
-            FileName auxFile=rawPSDFile.addExtension("psd");
-            auxFile.deleteFile();
         }
 
         // Release the memory
