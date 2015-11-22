e7629d2bd050e7ce7d25ba7d495bb9405480ac6d
Grigory Sharov <sharov.grigory@gmail.com>
2015-11-22 23:47:57 +0100

    Update projmatch_form.py

--- pyworkflow/em/packages/xmipp3/protocol_projmatch/projmatch_form.py
+++ pyworkflow/em/packages/xmipp3/protocol_projmatch/projmatch_form.py
@@ -425,7 +425,7 @@ def _defineProjectionMatchingParams(self, form):
     You may specify this option for each iteration. 
     This can be done by a sequence of 0 or 1 numbers (for instance, "1 1 0 0" 
     specifies 4 iterations, the first two applied alig2d while the last 2
-    don't. an alternative compact notation is 
+    dont. An alternative compact notation is 
     is ("2x1 2x0", i.e.,
     2 iterations with value 1, and 2 with value 0).
     *Note:*if there are less values than iterations the last value is reused
