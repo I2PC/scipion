e65f396dd16675fb782ca1b154ea58e2a6f9bde1
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2015-11-23 12:01:39 +0100

    More columns to visualize at output

--- pyworkflow/em/packages/xmipp3/viewer_reconstruct_highres.py
+++ pyworkflow/em/packages/xmipp3/viewer_reconstruct_highres.py
@@ -191,9 +191,9 @@ Examples:
             obj = self.protocol.outputParticles
             fn = obj.getFileName()
             labels = 'id enabled _filename _xmipp_zScore _xmipp_cumulativeSSNR '
-            labels += '_ctfModel._defocusU _ctfModel._defocusV _xmipp_shiftX _xmipp_shiftY _xmipp_tilt _xmipp_continuousX _xmipp_continuousY _xmipp_scale _xmipp_maxCC _xmipp_weight'
+            labels += '_ctfModel._defocusU _ctfModel._defocusV _xmipp_shiftX _xmipp_shiftY _xmipp_tilt _xmipp_scale _xmipp_maxCC _xmipp_weight'
             labels += " _xmipp_cost _xmipp_weightContinuous2 _xmipp_angleDiff0 _xmipp_weightJumper0 _xmipp_angleDiff _xmipp_weightJumper _xmipp_angleDiff2 _xmipp_weightJumper2"
-            labels += "_xmipp_weightSSNR _xmipp_maxCCPerc _xmipp_costPerc _xmipp_continuousX _xmipp_continuousY"
+            labels += "_xmipp_weightSSNR _xmipp_maxCCPerc _xmipp_costPerc _xmipp_continuousScaleX _xmipp_continuousScaleY _xmipp_continuousX _xmipp_continuousY _xmipp_continuousA _xmipp_continuousB"
             views.append(ObjectView(self._project, obj.strId(), fn,
                                           viewParams={showj.ORDER: labels, 
                                                       showj.VISIBLE: labels, 
