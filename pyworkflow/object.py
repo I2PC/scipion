c8d40df203acc04c5db2227180e239910f04ed3d
Pablo Conesa <p.conesa.mingo@gmail.com>
2015-11-27 15:54:45 +0100

    WEB: Url of new projects now consider the port (for localhost usage) , input for Pointerparams disabled.

--- pyworkflow/object.py
+++ pyworkflow/object.py
@@ -738,7 +738,7 @@ class Pointer(Object):
         return value
     
     def set(self, other):
-        """ Set the pointer value but cleanning the extendend property. """
+        """ Set the pointer value but cleaning the extended property. """
         Object.set(self, other)
         # This check is needed because set is call from the Object constructor
         # when this attribute is not setup yet (a dirty patch, I know)
