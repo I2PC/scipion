331766963af04d1d391120a5f6826ce7b8c65f03
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2016-01-18 10:21:01 +0100

    Added support for sorted classes visualization

--- pyworkflow/em/packages/xmipp3/viewer_cl2d.py
+++ pyworkflow/em/packages/xmipp3/viewer_cl2d.py
@@ -29,12 +29,12 @@ This module implement the wrappers aroung Xmipp CL2D protocol
 visualization program.
 """
 import pyworkflow.utils as pwutils
-from pyworkflow.viewer import ProtocolViewer, DESKTOP_TKINTER, WEB_DJANGO
+from pyworkflow.viewer import ProtocolViewer, WEB_DJANGO
 from pyworkflow.em import *
 from protocol_cl2d import XmippProtCL2D
 from pyworkflow.gui.text import *
 from pyworkflow.gui.dialog import showError, showWarning
-from pyworkflow.protocol.params import LabelParam, LEVEL_ADVANCED
+from pyworkflow.protocol.params import LabelParam, BooleanParam, LEVEL_ADVANCED
 import glob
 
 CLASSES = 0
@@ -67,7 +67,13 @@ class XmippCL2DViewer(ProtocolViewer):
         form.addParam('showSeveralLevels', StringParam, default='',
               label='Levels selection', condition='doShowLastLevel==%d' % LEVEL_SEL,
               help='Specify a  list of levels like: 0,1,3 or 0-3 ') 
-        
+
+        # Sorted or not
+        form.addParam('doSorted', BooleanParam, default=False, 
+                      label="Display sorted classes",
+                      help="Note that the average of the images assigned to the sorted class are not, in general, aligned with the sorted class, "
+                           "because the sorted class representatives have been realigned to be shown all in the same orientation")     
+         
         # Classes
         CLASS_CHOICES = ['classes']
         if self.protocol.doCore:
@@ -152,9 +158,15 @@ class XmippCL2DViewer(ProtocolViewer):
     def _getInputParticles(self):
         return self.protocol.inputParticles.get()
     
+    def _setClassOrder(self, classItem, classRow):
+        """ Set the class order for display the classes sorted. """
+        lastOrder = getattr(self, 'lastOrder', 0) + 1
+        classItem._xmipp_cl2dOrder = Integer(lastOrder)
+        setattr(self, 'lastOrder', lastOrder)
+        
     def _getClassesSqlite(self, blockName, fn):
         """ Read the classes from Xmipp metadata and write as sqlite file. """
-        fnSqlite = fn + '.sqlite'
+        fnSqlite = fn + '_'+blockName+'.sqlite'
 
         # Generate the sqlite file from classes xmd if either
         # 1) Sqlite files has not been generated or
@@ -166,15 +178,17 @@ class XmippCL2DViewer(ProtocolViewer):
         if not os.path.exists(fnSqlite):
             classesSet = SetOfClasses2D(filename=fnSqlite)
             classesSet.setImages(self._getInputParticles())
-            readSetOfClasses(classesSet, fn, blockName)
+            readSetOfClasses(classesSet, fn, blockName, 
+                             preprocessClass=self._setClassOrder)
             classesSet.write()
             
         return fnSqlite
     
-    def _viewClasses(self, blockName, fn):
+    def _viewClasses(self, blockName, fn, viewParams=None):
         fnSqlite = self._getClassesSqlite(blockName, fn)
         return ClassesView(self._project, self.protocol.strId(), fnSqlite,
-                           other=self._getInputParticles().strId())
+                           other=self._getInputParticles().strId(),
+                           viewParams=viewParams or {})
     
     def _viewLevelFiles(self, e=None):
         fnSubset = self._getSubset()
@@ -184,8 +198,15 @@ class XmippCL2DViewer(ProtocolViewer):
         levelFiles = self.protocol._getLevelMdFiles(fnSubset)
 
         if levelFiles:
+            suffix = ''
+            viewParams = {}
+            
+            if self.doSorted:
+                suffix = '_sorted'
+                viewParams[showj.SORT_BY] = '_xmipp_cl2dOrder'
+                
             if self.doShowLastLevel == LEVEL_LAST:
-                views.append(self._viewClasses("classes", levelFiles[-1]))
+                views.append(self._viewClasses("classes"+suffix, levelFiles[-1], viewParams))
             else:
                 if self.showSeveralLevels.empty():
                     errors.append('Please select the levels that you want to visualize.')
@@ -200,12 +221,12 @@ class XmippCL2DViewer(ProtocolViewer):
                     for level in listOfLevels:
                         fn = self.protocol._getExtraPath("level_%02d/level_classes%s.xmd"%(level,fnSubset))
                         if os.path.exists(fn):
-                            files.append(("classes_sorted", fn))
+                            files.append(("classes"+suffix, fn))
                         else:
                             errors.append('Level %s does not exist.' % level)
                     
                     for block, fn in files:                        
-                        views.append(self._viewClasses(block, fn))
+                        views.append(self._viewClasses(block, fn, viewParams))
         else:
             errors.append('Classes %s have not been produced.' % fnSubset.replace("_", " "))
         
