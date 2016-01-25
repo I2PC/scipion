2c21e21f89ec29e30c9a0244d6fbcccc24680fc7
Pablo Conesa <p.conesa.mingo@gmail.com>
2016-01-25 14:16:11 +0100

    WEB: Links underlined  (style change) Make more obvious the download link and link to "how to install" wiki page.

--- pyworkflow/web/pages/prefix_urls.py
+++ pyworkflow/web/pages/prefix_urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url, patterns
 from pyworkflow.web.pages import settings as django_settings
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
     # ... snip ...
     url('^' + django_settings.ABSOLUTE_URL, include('pages.urls')),
+    # url('^.*', RedirectView.as_view(url=django_settings.ABSOLUTE_URL + 'home/')), This creates an infinite redirections
 ]
 
 patterns(urlpatterns)
