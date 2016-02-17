f4e604196f4c2a6d94c7c8b9c9be2d75576d1d7b
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2016-02-17 11:25:23 +0100

    add test to input location in constructor of Image

--- pyworkflow/tests/em/data/test_data.py
+++ pyworkflow/tests/em/data/test_data.py
@@ -36,6 +36,15 @@ class TestImage(unittest.TestCase):
         # Check that setFileName-getFileName is working properly        
         self.assertEqual(fn, mic.getFileName())
 
+        # Check the location is accepted from constructor
+        mic2 = Micrograph(fn)
+        self.assertEqual(fn, mic2.getFileName())
+
+        volStk = 'all_volumes.stk'
+        vol1 = Volume((1, volStk))
+        self.assertEqual(1, vol1.getIndex())
+        self.assertEqual(volStk, vol1.getFileName())
+
 
 class TestImageHandler(unittest.TestCase):
     @classmethod
