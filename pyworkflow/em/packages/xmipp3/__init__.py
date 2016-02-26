0eb6e6b1183a39a3ddafeab95bac8eb183765aa8
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2016-02-26 17:46:41 +0100

    viewer_multiple_fscs is not longer needed

--- pyworkflow/em/packages/xmipp3/__init__.py
+++ pyworkflow/em/packages/xmipp3/__init__.py
@@ -49,7 +49,6 @@ from viewer_cltomo import XmippCLTomoViewer
 from viewer_ctf_discrepancy import XmippCTFDiscrepancyViewer
 from viewer_ml2d import XmippML2DViewer
 from viewer_movie_alignment import XmippMovieAlignViewer
-from viewer_multiple_fscs import XmippMultipleFSCsViewer
 from viewer_normalize_strain import XmippNormalizeStrainViewer
 from viewer_resolution3d import XmippResolution3DViewer
 from viewer_validate_nontilt import XmippValidateNonTiltViewer
