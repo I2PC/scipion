d6d70e1a44703f734db2a82499d01c12a28b3dc7
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2016-02-27 22:35:24 +0100

    Some code formatting to avoid long lines in help messages.

--- pyworkflow/em/packages/gautomatch/protocol_gautomatch.py
+++ pyworkflow/em/packages/gautomatch/protocol_gautomatch.py
@@ -38,13 +38,16 @@ class ProtGautomatch(em.ProtParticlePicking):
     """
     Gautomatch is a GPU accelerated program for fully automatic particle picking.
 
-    Note about CTF: It is perfectly fine to use raw micrograph(before CTF correction) for particle picking
-    since 30~50A is sufficient to auto-pick the particles. Usually for the micrograph with defocus around 2-5um,
-    the first CTF zero is around 20~30A, so it is not useful to do CTF correction in general.
-    However, you can determine CTF and flip the phases before picking. Full CTF correction on micrographs or
-    applying full CTF on templates is not suggested, because these operation is normally targeting for high resolution.
-    Since particle picking is basically a low-resolution operation, higher resolution will only introduce more false picking
-    and template-bias, known as the so-called Einstein noise.
+    Note about CTF: It is perfectly fine to use raw micrograph(before CTF correction)
+    for particle picking since 30~50A is sufficient to auto-pick the particles.
+    Usually for the micrograph with defocus around 2-5um, the first CTF zero is
+    around 20~30A, so it is not useful to do CTF correction in general. However,
+    you can determine CTF and flip the phases before picking. Full CTF
+    correction on micrographs or applying full CTF on templates is not suggested,
+    because these operation is normally targeting for high resolution. Since
+    particle picking is basically a low-resolution operation, higher resolution
+    will only introduce more false picking and template-bias, known as the
+    so-called Einstein noise.
     """
     _label = 'auto-picking'
 
@@ -75,32 +78,44 @@ class ProtGautomatch(em.ProtParticlePicking):
 
         form.addParam('advanced', params.BooleanParam, default=True,
                       label='Guess advanced parameters?',
-                      help="By default, the program will optimize advanced parameters by itself, "
-                           "however if you want to modify them, select No")
+                      help="By default, the program will optimize advanced "
+                           "parameters by itself, however if you want to "
+                           "modify them, select No")
         form.addParam('boxSize', params.IntParam, default=128,
                       label='Box size (pix)', condition='not advanced',
                       help="Box size, in pixels; a suggested value will be "
-                           "automatically calculated using pixel size and particle size")
+                           "automatically calculated using pixel size and "
+                           "particle size")
         form.addParam('maxDist', params.IntParam, default=300,
                       label='Max search distance (A)', condition='not advanced',
-                      help='Maximum search distance in Angstrom\n Use value of 0.9~1.1X diameter; '
+                      help='Maximum search distance in Angstrom\n '
+                           'Use value of 0.9~1.1X diameter; '
                            'can be 0.3~0.5X for filament-like particle\n'
-                           'For each particle Pi we try to search a local area using this distance option: '
-                           'once program finds a better candidate Pj, the particle Pi is rejected. '
-                           'If all peaks <= max_dist have been checked and no better candidates were found, '
+                           'For each particle Pi we try to search a local area '
+                           'using this distance option: '
+                           'once program finds a better candidate Pj, the '
+                           'particle Pi is rejected. If all peaks <= max_dist '
+                           'have been checked and no better candidates were found, '
                            'then Pi is considered as a successfully picked particle.')
         form.addParam('speed', params.IntParam, default=2,
                       label='Speed', condition='not advanced',
-                      help='Speed level {0,1,2,3,4}. The bigger the faster, but less accurate.\n'
-                           'Suggested values: 2 for >1 MDa complex, 1 for <500 kD complex, 1 or 2 for 500~1000 kD.\n'
-                           '0 is not suggested, because the accuracy is simply fitting noise, unless for special noise-free micrographs. '
-                           'Use 3 for huge viruses, but 2 is still preferred. Probably do not use 4 at all, it is not accurate in general.')
+                      help='Speed level {0,1,2,3,4}. The bigger the faster, but '
+                           'less accurate.\n'
+                           'Suggested values: 2 for >1 MDa complex, 1 for <500 '
+                           'kD complex, 1 or 2 for 500~1000 kD.\n'
+                           '0 is not suggested, because the accuracy is simply '
+                           'fitting noise, unless for special noise-free micrographs. '
+                           'Use 3 for huge viruses, but 2 is still preferred. '
+                           'Probably do not use 4 at all, it is not accurate '
+                           'in general.')
 
         group = form.addGroup('Local sigma parameters')
         group.addParam('localSigmaCutoff', params.FloatParam, default=1.2,
                        label='Local sigma cut-off', condition='not advanced',
-                       help='Local sigma cut-off (relative value), 1.2~1.5 should be a good range\n'
-                            'Normally a value >1.2 will be ice, protein aggregation or contamination')
+                       help='Local sigma cut-off (relative value), 1.2~1.5 '
+                            'should be a good range\n'
+                            'Normally a value >1.2 will be ice, protein '
+                            'aggregation or contamination')
         group.addParam('localSigmaDiam', params.IntParam, default=100,
                        label='Local sigma diameter (A)', condition='not advanced',
                        help='Diameter for estimation of local sigma, in Angstrom')
@@ -108,22 +123,29 @@ class ProtGautomatch(em.ProtParticlePicking):
         group = form.addGroup('Local average parameters')
         line = group.addLine('Local average range', condition='not advanced',
                              help="Local average cut-off (relative value), "
-                                  "any pixel values outside the range will be considered as ice/aggregation/carbon etc.")
+                                  "any pixel values outside the range will be "
+                                  "considered as ice/aggregation/carbon etc.")
         line.addParam('localAvgMin', params.FloatParam, default=-1.0,
                       label='Min')
         line.addParam('localAvgMax', params.FloatParam, default=1.0,
                       label='Max')
         group.addParam('localAvgDiam', params.IntParam, default=100,
-                       label='Local average diameter (A)', condition='not advanced',
+                       condition='not advanced',
+                       label='Local average diameter (A)',
                        help='Diameter for estimation of local average, in Angstrom. '
                             '1.5~2.0X particle diameter suggested\n'
-                            'However, if you have sharp/small ice or any dark/bright dots, '
-                            'using a smaller value will be much better to get rid of these areas')
+                            'However, if you have sharp/small ice or any '
+                            'dark/bright dots, using a smaller value will be '
+                            'much better to get rid of these areas')
 
-        line = form.addLine('Micrograph band-pass filter range (A)', condition='not advanced',
+        line = form.addLine('Micrograph band-pass filter range (A)',
+                            condition='not advanced',
                             help="Apply band-pass filter on the micrographs:\n"
-                                 "low-pass filter to increase the contrast of raw micrographs, suggested range 20~50 A\n"
-                                 "high-pass filter to get rid of the global background of raw micrographs, suggested range 200~2000 A")
+                                 "low-pass filter to increase the contrast of "
+                                 "raw micrographs, suggested range 20~50 A\n"
+                                 "high-pass filter to get rid of the global "
+                                 "background of raw micrographs, suggested "
+                                 "range 200~2000 A")
         line.addParam('lowPass', params.IntParam, default=30,
                       label='Min')
         line.addParam('highPass', params.IntParam, default=1000,
@@ -132,7 +154,6 @@ class ProtGautomatch(em.ProtParticlePicking):
         form.addParam('GPUId', params.IntParam, default=0,
                       label='GPU ID', condition='not advanced',
                       help='GPU ID, normally it is 0')
-
 
         # --------------------------- INSERT steps functions --------------------------------------------
 
@@ -220,7 +241,8 @@ class ProtGautomatch(em.ProtParticlePicking):
                 for line in source:
                     tokens = line.split()
                     coord = em.Coordinate()
-                    coord.setPosition(int(tokens[0]) + int(tokens[2]) / 2, int(tokens[1]) + int(tokens[3]) / 2)
+                    coord.setPosition(int(tokens[0]) + int(tokens[2]) / 2,
+                                      int(tokens[1]) + int(tokens[3]) / 2)
                     coord.setMicrograph(mic)
                     coordSet.append(coord)
                     # FIXME this should be outside the loop
@@ -234,12 +256,20 @@ class ProtGautomatch(em.ProtParticlePicking):
     def _validate(self):
         errors = []
         # Check that the program exists
-        if not exists(self._getProgram()):
-            errors.append("Binary '%s' does not exist. \n"
-                          "Check configuration file: ~/.config/scipion/scipion.conf\n"
-                          "and set GAUTOMATCH variables properly." % self._getProgram())
-            print "os.environ['GAUTOMATCH_HOME']", os.environ['GAUTOMATCH_HOME']
-            print "os.environ['GAUTOMATCH']", os.environ['GAUTOMATCH']
+        program = self._getProgram()
+        if program is None:
+            errors.append("Missing variables GAUTOMATCH and/or GAUTOMATCH_HOME")
+        elif not exists(program):
+            errors.append("Binary '%s' does not exists.\n" % program)
+
+        # If there is any error at this point it is related to config variables
+        if errors:
+            errors.append("Check configuration file: ~/.config/scipion/scipion.conf")
+            errors.append("and set GAUTOMATCH and GAUTOMATCH_HOME variables properly.")
+            if program is not None:
+                errors.append("Current values:")
+                errors.append("GAUTOMATCH_HOME = %s" % os.environ['GAUTOMATCH_HOME'])
+                errors.append("GAUTOMATCH = %s" % os.environ['GAUTOMATCH'])
 
         if not self.localAvgMin < self.localAvgMax:
             errors.append('Wrong values of local average cut-off!')
@@ -247,7 +277,8 @@ class ProtGautomatch(em.ProtParticlePicking):
 
     def _summary(self):
         summary = []
-        summary.append("Number of input micrographs: %d" % self.getInputMicrographs().getSize())
+        summary.append("Number of input micrographs: %d"
+                       % self.getInputMicrographs().getSize())
         if (self.getOutputsSize() > 0):
             summary.append("Number of particles picked: %d" % self.getCoords().getSize())
             summary.append("Particle size: %d px" % self.getCoords().getBoxSize())
@@ -260,13 +291,15 @@ class ProtGautomatch(em.ProtParticlePicking):
         methodsMsgs = []
         if self.getInputMicrographs() is None:
             return ['Input micrographs not available yet.']
-        methodsMsgs.append("Input micrographs %s." % (self.getObjectTag(self.getInputMicrographs())))
+        methodsMsgs.append("Input micrographs %s."
+                           % (self.getObjectTag(self.getInputMicrographs())))
 
         if self.getOutputsSize() > 0:
             output = self.getCoords()
-            methodsMsgs.append("%s: User picked %d particles with a particle size of %d px and minimal threshold %0.2f."
-                               % (self.getObjectTag(output), output.getSize(), output.getBoxSize(),
-                                  self.threshold.get()))
+            methodsMsgs.append("%s: User picked %d particles with a particle "
+                               "size of %d px and minimal threshold %0.2f."
+                               % (self.getObjectTag(output), output.getSize(),
+                                  output.getBoxSize(), self.threshold))
         else:
             methodsMsgs.append(Message.TEXT_NO_OUTPUT_CO)
 
@@ -278,6 +311,9 @@ class ProtGautomatch(em.ProtParticlePicking):
     # --------------------------- UTILS functions --------------------------------------------------
     def _getProgram(self):
         """ Return the program binary that will be used. """
+        if (not 'GAUTOMATCH' in os.environ or
+            not 'GAUTOMATCH_HOME' in os.environ):
+            return None
         binary = os.environ['GAUTOMATCH']
         program = join(os.environ['GAUTOMATCH_HOME'], 'bin', basename(binary))
         return program
