78281ff5a7bc3084122c643ed8e2d03c9e0fd03d
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2016-03-06 01:11:35 +0100

    Update of assignment tilt-pairs to reuse more from base class

--- pyworkflow/em/packages/xmipp3/protocol_assignment_tilt_pair.py
+++ pyworkflow/em/packages/xmipp3/protocol_assignment_tilt_pair.py
@@ -25,13 +25,13 @@
 # **************************************************************************
 
 from itertools import izip
-from os.path import split, splitext
 
-from pyworkflow.protocol.params import (PointerParam, FloatParam, EnumParam,
-                                        STEPS_PARALLEL, LEVEL_ADVANCED)
+import pyworkflow.protocol.params as params
 from pyworkflow.utils.path import makePath, removeBaseExt
+from pyworkflow.em.data import SetOfParticles
 from pyworkflow.em.data_tiltpairs import TiltPair, CoordinatesTiltPair
-from pyworkflow.em.packages.xmipp3 import readAnglesFromMicrographs
+
+from convert import readAnglesFromMicrographs
 from protocol_particle_pick_pairs import XmippProtParticlePickingPairs
 
 from convert import readSetOfCoordinates, writeSetOfCoordinates
@@ -46,55 +46,72 @@ class XmippProtAssignmentTiltPair(XmippProtParticlePickingPairs):
     the affine transformation between these sets.
     """
     _label = 'assignment tiltpair'
-    
+
     def __init__(self, *args, **kwargs):
         XmippProtParticlePickingPairs.__init__(self, *args, **kwargs)
-        self.stepsExecutionMode = STEPS_PARALLEL
-        
+        self.stepsExecutionMode = params.STEPS_PARALLEL
+
+        def onChangeInputType():
+            """ Dynamically change the PointerClass depending on the
+             selected input type (either Coordinates or Particles).
+            """
+            pointerClass = 'SetOf%s' % self.getEnumText('inputType')
+            self.getParam('untiltedSet').setPointerClass(pointerClass)
+            self.getParam('tiltedSet').setPointerClass(pointerClass)
+
+        self.inputType.trace(onChangeInputType)
+
     #--------------------------- DEFINE param functions --------------------------------------------   
     def _defineParams(self, form):
         form.addSection(label='Input')
-        form.addParam('tiltpair', PointerParam, pointerClass='MicrographsTiltPair',
-                      label="Micrograph tilt pair",  
-                      help='Select micrographs tilt pair.')
-        
-        form.addParam('typeOfSet', EnumParam, choices=['Coordinates', 'Particles'], 
-                      default=0, label='Input type', display=EnumParam.DISPLAY_COMBO, 
-                      help='Select a Set of Coordinates or a Set or Particles.')
-        
-        form.addParam('untiltCoor', PointerParam, pointerClass='SetOfCoordinates', 
-                      label="Untilted coordinates", condition='typeOfSet==%d' % TYPE_COORDINATES,  
-                      help='Select the metadata with untilt coordinates.')
-        
-        form.addParam('tiltCoor', PointerParam, pointerClass='SetOfCoordinates', 
-                      label="Tilted coordinates", condition='typeOfSet==%d' % TYPE_COORDINATES,   
-                      help='Select the metadata with tilt coordinates.')
-        
-        form.addParam('untiltPar', PointerParam, pointerClass='SetOfParticles', 
-                      label="Untilted particles", condition='typeOfSet==%d' % TYPE_PARTICLES,
-                      help='Select the metadata with untilt particles.')
-        
-        form.addParam('tiltPar', PointerParam, pointerClass='SetOfParticles', 
-                      label="Tilted particles", condition='typeOfSet==%d' % TYPE_PARTICLES,   
-                      help='Select the metadata with tilt particles.')
-        
-        form.addParam('tiltAngle', FloatParam, default=-1, expertLevel=LEVEL_ADVANCED,
-                      label="Tilt angle",  
-                      help='Tilt angle estimation, the method will look for the assignment in the\n'
-                      ' interval of [tilt_angle-15, tilt_angle+15].\n'
-                      'By default: tilt angle = -1, if there is not any information about the tilt angle')     
-        
-        form.addParam('threshold', FloatParam, default=0.25, expertLevel=LEVEL_ADVANCED,
-                      label="Threshold value",  
-                      help='Parameter between 0 and 1 that allows to define if \n' 
-                      'a tilt point can be matched with a certain untilt point. \n'
-                      'The matching is performed only if the distance is lesser than \n'
-                      'threshold * particlesize.')
 
-        form.addParam('maxShift', FloatParam, default=1500, expertLevel=LEVEL_ADVANCED,
-                      label="Maximum shift (pixels)", 
-                      help='Maximum allowed distance (in pixels) that the tilt micrograph can be shifted' 
-                      'respect to the untilted micrograph')         
+        form.addParam('inputMicrographsTiltedPair', params.PointerParam,
+                      pointerClass='MicrographsTiltPair',
+                      important=True,
+                      label="Micrograph tilt pair",
+                      help='Select micrographs tilt pair.')
+
+        form.addParam('inputType', params.EnumParam,
+                      choices=['Coordinates', 'Particles'], default=0,
+                      display=params.EnumParam.DISPLAY_COMBO,
+                      label='Input type',
+                      help='Select a Set of Coordinates or a Set or Particles.')
+
+        form.addParam('untiltedSet', params.PointerParam,
+                      pointerClass='SetOfCoordinates,SetOfParticles',
+                      label="Untilted input",
+                      help='Select the untilted input set, it can be either '
+                           'coordinates or particles (that contains coordinates.')
+
+        form.addParam('tiltedSet', params.PointerParam,
+                      pointerClass='SetOfCoordinates,SetOfParticles',
+                      label="Tilted input",
+                      help='Select the tilted input set, it can be either '
+                           'coordinates or particles (that contains coordinates. '
+                           'It should be of the same type of the input untilted.')
+
+        form.addParam('tiltAngle', params.FloatParam, default=-1,
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label="Tilt angle",
+                      help='Tilt angle estimation, the method will look for '
+                           'the assignment in the interval of [tilt_angle-15, '
+                           'tilt_angle+15].\n By default: tilt angle = -1, if '
+                           'there is not any information about the tilt angle')
+
+        form.addParam('threshold', params.FloatParam, default=0.25,
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label="Threshold value",
+                      help='Parameter between 0 and 1 that allows to define if \n'
+                      'a tilt point can be matched with a certain untilt point.\n'
+                      'The matching is performed only if the distance is lesser\n'
+                      'than threshold * particlesize.')
+
+        form.addParam('maxShift', params.FloatParam, default=1500,
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label="Maximum shift (pixels)",
+                      help='Maximum allowed distance (in pixels) that the tilt '
+                           'micrograph can be shifted respect to the untilted '
+                           'micrograph')
 
         form.addParallelSection(threads=1, mpi=1)
 
@@ -106,12 +123,32 @@ class XmippProtAssignmentTiltPair(XmippProtParticlePickingPairs):
     def _micBaseName(self, mic):
         return removeBaseExt(mic.getFileName())
 
-    def _insertAllSteps(self):        
+    def _coordsFromParts(self, micSet, partSet, suffix):
+        """ Create and fill a SetOfCoordinates from a given SetOfParticles. """
+        coordSet = self._createSetOfCoordinates(micSet, suffix='_untilted')
+
+        for particle in partSet:
+            coord = particle.getCoordinate().clone()
+            coord.copyObjId(particle)
+            coordSet.append(coord)
+        coordSet.setBoxSize(partSet.getXDim())
+
+        return coordSet
+
+    def _getBoxSize(self):
+        untiltedSet = self.untiltedSet.get()
+        if isinstance(untiltedSet, SetOfParticles):
+            boxSize = untiltedSet.getXDim()
+        else:
+            boxSize = untiltedSet.getBoxSize() # coordinates
+        return boxSize
+
+    def _insertAllSteps(self):
         self.micsFn = self._getPath()
         # Convert input into xmipp Metadata format
         convertId = self._insertFunctionStep('convertInputStep')
         deps = []
-        for tiltPair in self.tiltpair.get():
+        for tiltPair in self.inputMicrographsTiltedPair.get():
             uName = self._micBaseName(tiltPair.getUntilted())
             tName = self._micBaseName(tiltPair.getTilted())
             stepId = self._insertFunctionStep('assignmentStep',
@@ -130,144 +167,69 @@ class XmippProtAssignmentTiltPair(XmippProtParticlePickingPairs):
         # Get the converted input micrographs in Xmipp format
         makePath(self._getExtraPath("untilted"))
         makePath(self._getExtraPath("tilted"))
-        
-        if self.typeOfSet.get() == TYPE_PARTICLES:
-            U_set = self.untiltPar.get().getCoordinates()
-            T_set = self.tiltPar.get().getCoordinates()
 
-            if (U_set or T_set) is None:
-                U_set = self._createSetOfCoordinates(self.tiltpair.get().getUntilted(), 
-                                                     suffix='_untilted')
-                T_set = self._createSetOfCoordinates(self.tiltpair.get().getTilted(), 
-                                                     suffix='_tilted')
-                
-                untiltPar = self.untiltPar.get()
-                for particle_u in untiltPar:  
-                    newCoord = particle_u.getCoordinate().clone()
-                    newCoord.copyObjId(particle_u)
-                    U_set.append(newCoord)
-#
-                tiltPar = self.tiltPar.get()
-                for particle_t in tiltPar:
-                    newCoord = particle_t.getCoordinate().clone()
-                    newCoord.copyObjId(particle_t)
-                    T_set.append(newCoord)
+        uSet = self.untiltedSet.get()
+        tSet = self.tiltedSet.get()
 
-                aux = self.untiltPar.get()
-                aux2 = aux[1]
-                bos, y_, z_ = aux2.getDim()
-                U_set.setBoxSize(bos)
-                T_set.setBoxSize(bos)
+        # Get the untilted and tilted coordinates, depending on the input type
+        if isinstance(uSet, SetOfParticles):
+            uCoords = uSet.getCoordinates()
+            tCoords = tSet.getCoordinates()
+
+            # If there are not Coordinates associated to particles
+            # we need to create and fill the set of coordinates
+            if uCoords is None or tCoords is None:
+                micTiltedPairs = self.inputMicrographsTiltedPair.get()
+                uCoords = self._coordsFromParts(micTiltedPairs.getUntilted(),
+                                                uSet, '_untilted')
+                tCoords = self._coordsFromParts(micTiltedPairs.getTilted(),
+                                                tSet, '_tilted')
         else:
-            U_set = self.untiltCoor.get()
-            T_set = self.tiltCoor.get() 
-            
-        writeSetOfCoordinates(self._getExtraPath("untilted"), U_set)
-        writeSetOfCoordinates(self._getExtraPath("tilted"), T_set)
+            uCoords = uSet
+            tCoords = tSet
 
-    def assignmentStep(self, fnuntilt, fntilt, fnmicsize,
-                       fnposUntilt, fnposTilt):
-        Unpath = self._getExtraPath()
-        params =  ' --untiltcoor %s' % fnuntilt        
+        writeSetOfCoordinates(self._getExtraPath("untilted"), uCoords)
+        writeSetOfCoordinates(self._getExtraPath("tilted"), tCoords)
+
+    def assignmentStep(self, fnuntilt, fntilt, fnmicsize, fnposUntilt, fnposTilt):
+        params =  ' --untiltcoor %s' % fnuntilt
         params += ' --tiltcoor %s' % fntilt
         params += ' --tiltmicsize %s' % fnmicsize
-        params += ' --maxshift %f' % self.maxShift.get()
-
-        if self.typeOfSet.get() == TYPE_PARTICLES:
-                aux = self.untiltPar.get()
-                aux2 = aux[1]
-                boxsize, y_, z_ = aux2.getDim()
-                params += ' --particlesize %f' % boxsize         
-        else:
-            params += ' --particlesize %f' % self.untiltCoor.get().getBoxSize()
-        params += ' --threshold %f' % self.threshold.get()
-        params += ' --odir %s' % Unpath
-
+        params += ' --maxshift %f' % self.maxShift
+        params += ' --particlesize %d' % self._getBoxSize()
+        params += ' --threshold %f' % self.threshold
+        params += ' --odir %s' % self._getExtraPath()
         self.runJob('xmipp_image_assignment_tilt_pair', params)
 
-        self.estimateTiltAxis(fnposUntilt, fnposTilt)
-
-    def estimateTiltAxis(self, fnposUntilt, fnposTilt):#, opath):
+        # Estimate the tilt axis
         params =  ' --untilted %s' % fnposUntilt
         params += ' --tilted %s' % fnposTilt
         params += ' -o %s' % self._getPath('input_micrographs.xmd')
-
         self.runJob('xmipp_angular_estimate_tilt_axis', params)
-        
+
     def createOutputStep(self):
-        extradir = self._getExtraPath()
-        inputset = self.tiltpair.get()
-        uSet = inputset.getUntilted()
-        tSet = inputset.getTilted()
-        
-        # Create Untilted and Tilted SetOfCoordinates
-        uCoordSet = self._createSetOfCoordinates(uSet, suffix='Untilted')
-        if self.typeOfSet.get() == TYPE_PARTICLES:
-            aux = self.untiltPar.get()
-            aux2 = aux[1]
-            boxsize_u, y_, z_ = aux2.getDim()
-        else:
-            boxsize_u = self.untiltCoor.get().getBoxSize()
-        
-        uCoordSet.setBoxSize(boxsize_u)    
-        tCoordSet = self._createSetOfCoordinates(tSet, suffix='Tilted')
-        readSetOfCoordinates(extradir, uSet, uCoordSet)
-        uCoordSet.write()
-        
-        if self.typeOfSet.get() == TYPE_PARTICLES:
-            aux = self.tiltPar.get()
-            aux2 = aux[1]
-            boxsize_t, y_, z_ = aux2.getDim()
-        else:
-            boxsize_t = self.tiltCoor.get().getBoxSize()
-        
-        tCoordSet.setBoxSize(boxsize_t)
-        readSetOfCoordinates(extradir, tSet, tCoordSet)
-        tCoordSet.write()
-        
-        setAngles = self._createSetOfAngles()
-        pathangles = self.micsFn + '/input_micrographs.xmd'
-        readAnglesFromMicrographs(pathangles, setAngles)
-        
-        setAngles.write()
-        # Create CoordinatesTiltPair object
-        outputset = CoordinatesTiltPair(filename=self._getPath('coordinates_pairs.sqlite'))
-        outputset.setTilted(tCoordSet)
-        outputset.setUntilted(uCoordSet)
-        outputset.setAngles(setAngles)
-        outputset.setMicsPair(inputset)
-        outputset.setObjComment(self.getSummary(outputset))
-
-        for coordU, coordT in izip(uCoordSet, tCoordSet):
-            outputset.append(TiltPair(coordU, coordT))
-
-        self._defineOutputs(outputCoordinatesTiltPair=outputset)
-        self._defineSourceRelation(self.tiltpair, outputset)
+        self.registerCoords(self._getExtraPath(), store=False)
 
     #--------------------------- INFO functions -------------------------------------------- 
     def _validate(self):
-        if self.typeOfSet.get() == TYPE_PARTICLES:
-            validateMsgs = []
-            if self.untiltPar.get() and not self.untiltPar.hasValue():
-                validateMsgs.append('Please provide input particles.')  
-            if self.tiltPar.get() and not self.tiltPar.hasValue():
-                validateMsgs.append('Please provide input particles.')         
-            return validateMsgs
-        else:
-            validateMsgs = []
-            if self.untiltCoor.get() and not self.untiltCoor.hasValue():
-                validateMsgs.append('Please provide input coordinates.')  
-            if self.tiltCoor.get() and not self.tiltCoor.hasValue():
-                validateMsgs.append('Please provide input coordinates.')         
-            return validateMsgs
-         
+        errors = []
+        uSet = self.untiltedSet.get()
+        tSet = self.tiltedSet.get()
+
+        if (uSet is not None and tSet is not None and
+            uSet.getClassName() != tSet.getClassName()):
+            errors.append('Both untilted and tilted inputs should be of the '
+                          'same type. ')
+
+        return errors
+
     def _methods(self):
         messages = []
         if hasattr(self,'outputCoordinatesTiltPair'):
             messages.append('The assignment has been performed using and '
                             'affinity transformation [Publication: Not yet]')
         return messages
-    
+
     def _citations(self):
         return ['Not yet']
 
