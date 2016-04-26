db781cb5aa6707aeb2c3b216ae27b263033bce00
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2016-04-26 11:32:32 +0200

    updated Authors of IMAGIC files

--- pyworkflow/em/packages/imagic/wizard.py
+++ pyworkflow/em/packages/imagic/wizard.py
@@ -1,7 +1,7 @@
 # **************************************************************************
 # *
-# * Authors:     J.M. De la Rosa Trevin (jmdelarosa@cnb.csic.es)
-# *              Jose Gutierrez (jose.gutierrez@cnb.csic.es)
+# * Authors:     Grigory Sharov (sharov@igbmc.fr)
+# *              J.M. De la Rosa Trevin (jmdelarosa@cnb.csic.es)
 # *
 # * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
 # *
