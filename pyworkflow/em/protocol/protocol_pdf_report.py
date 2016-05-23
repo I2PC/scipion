9c32776a96fffa4d81e98dde11770fdcb260fb5e
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2016-05-23 15:26:16 +0200

    Minor formatting and re-factor

--- pyworkflow/em/protocol/protocol_pdf_report.py
+++ pyworkflow/em/protocol/protocol_pdf_report.py
@@ -20,7 +20,7 @@
 # * 02111-1307  USA
 # *
 # *  All comments concerning this program package may be sent to the
-# *  e-mail address 'jgomez@cnb.csic.es'
+# *  e-mail address 'scipion@cnb.csic.es'
 # *
 # **************************************************************************
 
@@ -31,31 +31,41 @@ from pyworkflow.protocol.params import PathParam
 from pyworkflow.em.protocol import EMProtocol
 from pyworkflow.utils.path import moveFile, copyFile
 
+
 class ProtPDFReport(EMProtocol):
     """ 
-    Produce a pdf report from the files (*.tex, *.txt, *.jpg, *.png, *.pdf) files in a directory.
-    Files in the directory are sorted by name alphabetically, so if you want them to have the right order a possibility is to name them as
-    0010-myText.txt, 0020-aFigure.png, 0030-anotherFigure.jpg, 0040-aPaper.pdf, 0050-anotherText.tex, ... when these files are sorted, they will
-    be sorted by the number in front. 
+    Produce a pdf report from the files in a given directory.
+    Supported file formats: *.tex, *.txt, *.jpg, *.png, *.pdf
+    Files in the directory are sorted by name alphabetically,
+    so if you want them to have the right order a possibility is to name them as
+    0010-myText.txt
+    0020-aFigure.png
+    0030-anotherFigure.jpg
+    0040-aPaper.pdf
+    0050-anotherText.tex
+    ...
+    when these files are sorted, they will be sorted by the number in front.
     """    
     _label = 'pdf report'
 
-    #--------------------------- DEFINE param functions --------------------------------------------
+    #--------------------------- DEFINE param functions ------------------------
     
     def _defineParams(self, form):
         form.addSection(label='Input')
-        form.addParam('filesPath', PathParam, default=os.environ.get('SCIPION_NOTES_ARGS', ""),
-                      label="Files directory")
+        form.addParam('filesPath', PathParam,
+                      label="Files directory",
+                      help="Directory with the input files. \n"
+                           "Check protocol help for more details.")
     
-    #--------------------------- INSERT steps functions --------------------------------------------
+    #--------------------------- INSERT steps functions ------------------------
     
     def _insertAllSteps(self):
         self._insertFunctionStep('createReport')        
 
-    #--------------------------- STEPS functions --------------------------------------------   
+    #--------------------------- STEPS functions -------------------------------
     def createReport(self):
-        fnTex = self._getExtraPath("report.tex")
-        fhTex = open(fnTex,"w")
+        fnTex = "report.tex"
+        fhTex = open(self._getExtraPath(fnTex), "w")
         template ="""
 \\documentclass[12pt]{article}
 \\usepackage{amsmath,amsthm,amssymb,amsfonts} 
@@ -63,32 +73,34 @@ class ProtPDFReport(EMProtocol):
 \\usepackage{pdfpages}
 \\DeclareGraphicsExtensions{.pdf,.png,.jpg}
 \\begin{document}
-\\title{Scipion report}
-\\author{Scipion}
+\\title{User Report}
+\\author{Created by Scipion}
 \\maketitle
 """
         fhTex.write(template)
         
         fnDir = self.filesPath.get()
+
         if not os.path.isdir(fnDir):
             fnDir = os.path.basename(fnDir)
+
         for fileName in sorted(glob.glob(os.path.join(fnDir,"*"))):
-            _, fileNameWithoutDir = os.path.split(fileName)
-            fnDest = self._getExtraPath(fileNameWithoutDir)
+            fnDest = os.path.basename(fileName).lower()
             fnDest = fnDest.replace(" ","_")
             fnDest = fnDest.replace(":","_")
             fnDest = fnDest.replace(";","_")
-            copyFile(fileName,fnDest)
+            copyFile(fileName, self._getExtraPath(fnDest))
+
             if fnDest.endswith(".tex") or fnDest.endswith(".txt"):
-                fhTex.write("\\input{%s}\n"%fnDest)
+                fhTex.write("\\input{%s}\n" % fnDest)
                 fhTex.write("\n")
             elif fnDest.endswith(".png") or fnDest.endswith(".jpg"):
                 fhTex.write("\\begin{center}\n")
-                fhTex.write("\\includegraphics[width=14cm]{%s}\n"%fnDest)
+                fhTex.write("\\includegraphics[width=14cm]{%s}\n" % fnDest)
                 fhTex.write("\\end{center}\n")
                 fhTex.write("\n")
             elif fnDest.endswith(".pdf"):
-                fhTex.write("\\includepdf[pages=-]{%s}\n"%fnDest)
+                fhTex.write("\\includepdf[pages=-]{%s}\n" % fnDest)
                 fhTex.write("\\clearpage\n")
                 fhTex.write("\n")
         
@@ -96,17 +108,23 @@ class ProtPDFReport(EMProtocol):
 \\end{document}
 """
         fhTex.write(template)
-        
         fhTex.close()
-        self.runJob("pdflatex", "-interaction=nonstopmode -output-directory %s %s"%(self._getExtraPath(),self._getExtraPath("report.tex")))
+
+        args = "-interaction=nonstopmode " + fnTex
+        self.runJob("pdflatex", args, cwd=self._getExtraPath())
+
         fnPDF = self._getExtraPath("report.pdf")
+
         if os.path.exists(fnPDF):
             moveFile(fnPDF,self._getPath("report.pdf"))
+        else:
+            raise Exception("PDF file was not produced.")
 
-    #--------------------------- INFO functions --------------------------------------------
+    #--------------------------- INFO functions --------------------------------
     def _summary(self):
         summary = []
         summary.append("Input directory: %s" % self.filesPath.get())
         summary.append("Output report: %s" % self._getPath("report.pdf"))
         return summary
     
+
