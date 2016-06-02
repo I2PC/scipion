9fa671365e2442a29ef7d627367a9b6c761a86c8
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2016-06-02 15:26:23 +0200

    Isolated the monitoring logic into a separated MonitorCTF class

--- pyworkflow/em/protocol/protocol_monitor_ctf.py
+++ pyworkflow/em/protocol/protocol_monitor_ctf.py
@@ -24,9 +24,11 @@
 # *
 # **************************************************************************
 
+import os
+import sys
 
 import pyworkflow.protocol.params as params
-from protocol_monitor import ProtMonitor
+from protocol_monitor import ProtMonitor, Monitor, EmailNotifier
 import sqlite3 as lite
 import time, sys
 from matplotlib import pyplot
@@ -40,13 +42,7 @@ from pyworkflow.em.plotter import EmPlotter
 class ProtMonitorCTF(ProtMonitor):
     """ check CPU, mem and IO usage.
     """
-    _label = 'ctf_monitor'
-
-    def __init__(self, **kwargs):
-        ProtMonitor.__init__(self, **kwargs)
-        self.dataBase = 'log.sqlite'
-        self.tableName = 'log'
-        self.readCTFs = set()
+    _label = 'ctf monitor'
 
     #--------------------------- DEFINE param functions ----------------------
     def _defineParams(self, form):    
@@ -61,6 +57,7 @@ class ProtMonitorCTF(ProtMonitor):
                       label="Sampling Interval (sec)",
                       pointerClass='EMProtocol',
                       help="Take one sample each SamplinInteval seconds")
+
         form.addParam('maxDefocus', params.FloatParam,default=40000,
               label="Raise Alarm if maximum defocus (A) >",
               help="Raise alarm if defocus is greater than given value")
@@ -70,23 +67,33 @@ class ProtMonitorCTF(ProtMonitor):
         form.addParam('astigmatism', params.FloatParam,default=0.2,
               label="Raise Alarm if astigmatism >",
               help="Raise alarm if astigmatism is greater than given value")
-        form.addParam('interval', params.FloatParam,default=300,
+
+        form.addParam('monitorTime', params.FloatParam, default=300,
               label="Total Logging time (min)",
               help="Log during this interval")
+
         ProtMonitor._sendMailParams(self, form)
 
-    #--------------------------- STEPS functions --------------------------------------------
+    #--------------------------- STEPS functions -------------------------------
     def monitorStep(self):
-        baseFn = self._getPath(self.dataBase)
-        conn = lite.connect(baseFn, isolation_level=None)
-        cur = conn.cursor()
-        self.createTable(conn,self.tableName)
-        #TODO: interval should be protocol
-        interval = self.interval.get() # logging during these minutes
-        sleepSec = self.samplingInterval.get() # wait this seconds between logs
-        self.loopCTFCollect(cur,self.tableName,interval,sleepSec)
+        self.createMonitor().loop()
 
-    #--------------------------- INFO functions --------------------------------------------
+    def createMonitor(self):
+        ctfProt = self.inputProtocol.get()
+        ctfProt.setProject(self.getProject())
+
+        ctfMonitor = MonitorCTF(ctfProt,
+                                workingDir=self.workingDir.get(),
+                                samplingInterval=self.samplingInterval.get(),
+                                monitorTime=self.monitorTime.get(),
+                                email=self.createEmailNotifier(),
+                                stdout=True,
+                                minDefocus=self.minDefocus.get(),
+                                maxDefocus=self.maxDefocus.get(),
+                                astigmatism=self.astigmatism.get())
+        return ctfMonitor
+
+    #--------------------------- INFO functions --------------------------------
     def _validate(self):
         #TODO if less than 20 sec complain
         return []  # no errors
@@ -94,8 +101,32 @@ class ProtMonitorCTF(ProtMonitor):
     def _summary(self):
         return ['Monitor CTF defocus']
 
-    def _updateProtocol(self, prot):
-        prot2 = getProtocolFromDb(self.getProject().path,
+
+class MonitorCTF(Monitor):
+    """ This will will be monitoring a CTF estimation protocol.
+    It will internally handle a database to store produced
+    CTF values.
+    """
+    def __init__(self, protocol, **kwargs):
+        Monitor.__init__(self, **kwargs)
+        # The CTF protocol to monitor
+        self.protocol = protocol
+
+        self.maxDefocus = kwargs['maxDefocus']
+        self.minDefocus = kwargs['minDefocus']
+        self.astigmatism = kwargs['astigmatism']
+
+        self._dataBase = 'log.sqlite'
+        self._tableName = 'log'
+        self.readCTFs = set()
+
+        self.conn = lite.connect(os.path.join(self.workingDir, self._dataBase),
+                                              isolation_level=None)
+        self.cur = self.conn.cursor()
+
+    def _getUpdatedProtocol(self):
+        prot = self.protocol
+        prot2 = getProtocolFromDb(self.protocol.getProject().path,
                                   prot.getDbPath(),
                                   prot.getObjId())
         # Close DB connections
@@ -103,24 +134,29 @@ class ProtMonitorCTF(ProtMonitor):
         prot2.closeMappers()
         return prot2
 
-    def loopCTFCollect(self, cur, tableName, interval,sleepSec):
-        timeout = time.time() + 60.*interval   # interval minutes from now
+    def warning(self, msg):
+        self.notify("Scipion CTF Monitor WARNING", msg)
+
+    def loop(self):
+        self._createTable()
+        timeout = time.time() + 60. * self.monitorTime   # interval minutes from now
         #ignore first meassure because is very unrealiable
         while True:
-            prot = self._updateProtocol(self.inputProtocol.get())
-            finished = prot.getStatus()!=STATUS_RUNNING
-            #create set of processed CTF from CTF protocol
-            CTFset=prot.outputCTF.getIdSet()#set([ctf.getObjId() for ctf in prot.outputCTF])
+            prot = self._getUpdatedProtocol()
+            finished = prot.getStatus() != STATUS_RUNNING
+            # Create set of processed CTF from CTF protocol
+            CTFset = prot.outputCTF.getIdSet()
             #find difference
-            import sys
             sys.stdout.flush()
             diffSet = CTFset -  self.readCTFs
-            setOfCTFs = prot.outputCTF#.get()
-            astigmatism = self.astigmatism.get()
+            setOfCTFs = prot.outputCTF
+            astigmatism = self.astigmatism
+
             for ctfID in diffSet:
-                defocusU = setOfCTFs[ctfID].getDefocusU()
-                defocusV = setOfCTFs[ctfID].getDefocusV()
-                defocusAngle = setOfCTFs[ctfID].getDefocusAngle()
+                ctf = setOfCTFs[ctfID]
+                defocusU = ctf.getDefocusU()
+                defocusV = ctf.getDefocusV()
+                defocusAngle = ctf.getDefocusAngle()
 
                 if defocusU < defocusV:
                     aux = defocusV
@@ -133,51 +169,35 @@ class ProtMonitorCTF(ProtMonitor):
                 #get CTFs with this ids a fill table
                 #do not forget to compute astigmatism
                 sql = """INSERT INTO %s(defocusU,defocusV,astigmatism,ratio) VALUES(%f,%f,%f,%f);""" \
-                      %(tableName, defocusU, defocusV, defocusAngle, defocusU/defocusV)
+                      %(self._tableName, defocusU, defocusV, defocusAngle, defocusU/defocusV)
                 try:
-                    cur.execute(sql)
+                    self.cur.execute(sql)
                 except Exception as e:
                     print("ERROR: saving one data point (CTF monitor). I continue")
 
-                if  (defocusU/defocusV) > (1. + astigmatism):
-                     print("Error Message", "defocusU/defocusV =%f."%defocusU/defocusV)
-                     sys.stdout.flush()
+                if (defocusU/defocusV) > (1. + astigmatism):
+                     self.warning("Defocus ratio (defocusU / defocusV)  = %f."
+                                  % defocusU / defocusV)
 
-                     self.swapAlert = swap.percent
-                     if self.doMail:
-                          self.sendEMail("scipion system monitor warning", "defocus ratio  =%f."%defocusU/defocusV)
-
-                if  defocusU > self.maxDefocus:
-                     print("Error Message", "defocusU =%f."%defocusU)
-                     sys.stdout.flush()
-
-                     self.maxDefocus = defocusU
-
-                     if self.doMail:
-                         self.sendEMail("scipion system monitor warning", "defocusU  =%f."%defocusU)
+                if defocusU > self.maxDefocus:
+                    self.warning("DefocusU (%f) is larger than defocus "
+                                 "maximum (%f)" % (defocusU, self.maxDefocus))
+                    self.maxDefocus = defocusU
 
                 if  defocusV < self.minDefocus:
-                     print("Error Message", "defocusV =%f."%defocusV)
-                     sys.stdout.flush()
-
-                     self.maxDefocus = defocusV
-
-                     if self.doMail:
-                         self.sendEMail("scipion system monitor warning", "defocusV  =%f."%defocusV)
+                    self.warning("DefocusV (%f) is smaller than defocus "
+                                 "minumum (%f)" % (defocusV, self.maxDefocus))
+                    self.minDefocus = defocusV
 
             self.readCTFs.update(diffSet)
+
             if (time.time() > timeout) or finished:
-               break
-            time.sleep(sleepSec)
+                return
 
-         #self.setStatus(STATUS_FINISHED)
+            time.sleep(self.samplingInterval)
 
-    def _methods(self):
-        return []
-
-    def createTable(self,cur,tableName):
-
-        sql = """CREATE TABLE IF NOT EXISTS  %s(
+    def _createTable(self):
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS  %s(
                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                                 timestamp DATE DEFAULT (datetime('now','localtime')),
                                 defocusU FLOAT,
@@ -185,8 +205,26 @@ class ProtMonitorCTF(ProtMonitor):
                                 defocus FLOAT,
                                 astigmatism FLOAT,
                                 ratio FLOAT)
-                                """ % tableName
-        cur.execute(sql)
+                                """ % self._tableName)
+
+    def getData(self):
+        def get(name):
+            try:
+                self.cur.execute("select %s from %s" % (name, self._tableName))
+            except Exception as e:
+                print("MonitorCTF, ERROR reading data from db: %s" %
+                      os.path.join(self.workingDir, self._dataBase))
+            return [r[0] for r in self.cur.fetchall()]
+
+        data = {
+            'defocusU': get('defocusU'),
+            'defocusV': get('defocusV'),
+            'astigmatism': get('astigmatism'),
+            'ratio': get('ratio'),
+            'idValues': get('id')
+        }
+        # conn.close()
+        return data
 
 
 from pyworkflow.viewer import ( DESKTOP_TKINTER, WEB_DJANGO, Viewer)
@@ -201,18 +239,18 @@ class ProtMonitorCTFViewer(Viewer):
     _targets = [ProtMonitorCTF]
 
     def _visualize(self, obj, **kwargs):
-        return [CtfMonitorPlotter(obj)]
+        return [CtfMonitorPlotter(obj.createMonitor())]
 
 
 class CtfMonitorPlotter(EmPlotter):
-    def __init__(self, protocol):
+    def __init__(self, monitor):
         EmPlotter.__init__(self, windowTitle="CTF Monitor")
-        self.protocol = protocol
+        self.monitor = monitor
         self.y2 = 0.; self.y1 = 100.
         self.win = 250 # number of samples to be ploted
         self.step = 50 # self.win  will be modified in steps of this size
 
-        self.createSubPlot("Use scrool wheel to change view window (win=%d)\n S stops, C continues plotting" % self.win, "Micrographs", "Defocus (A)")
+        self.createSubPlot(self._getTitle(), "Micrographs", "Defocus (A)")
         self.fig = self.getFigure()
         self.ax = self.getLastSubPlot()
         self.ax.margins(0.05)
@@ -223,11 +261,9 @@ class CtfMonitorPlotter(EmPlotter):
         self.init = True
         self.stop = False
 
-        baseFn = self.protocol._getPath(self.protocol.dataBase)
-        self.tableName = self.protocol.tableName
-        self.samplingInterval = self.protocol.samplingInterval
-        self.conn  = lite.connect(baseFn, isolation_level=None)
-        self.cur   = self.conn.cursor()
+    def _getTitle(self):
+        return ("Use scrool wheel to change view window (win=%d)\n "
+                "S stops, C continues plotting" % self.win)
 
     def onscroll(self, event):
 
@@ -239,7 +275,7 @@ class CtfMonitorPlotter(EmPlotter):
                self.win = self.step
 
         if self.oldWin != self.win:
-            self.ax.set_title('use scroll wheel to change view window (win=%d)\n S stops, C continues plotting'%self.win)
+            self.ax.set_title(self._getTitle())
             self.oldWin= self.win
         self.animate()
         EmPlotter.show(self)
@@ -251,23 +287,22 @@ class CtfMonitorPlotter(EmPlotter):
             self.stop = True
             self.ax.set_title('Plot is Stopped. Press C to continue plotting')
         elif event.key == 'C':
-	    self.ax.set_title('use scroll wheel to change view window (win=%d)\n S stops, C continues plotting'%self.win)
+            self.ax.set_title(self._getTitle())
             self.stop = False
-        self.animate()
+            self.animate()
         EmPlotter.show(self)
-
 
     def has_been_closed(self,ax):
         fig = ax.figure.canvas.manager
         active_fig_managers = pyplot._pylab_helpers.Gcf.figs.values()
         return fig not in active_fig_managers
 
-    def animate(self,i=0): #do NOT remove i
+    def animate(self, i=0): #do NOT remove i
                            #FuncAnimation adds it as argument
         if self.stop:
             return
 
-        data = self.getData()
+        data = self.monitor.getData()
         self.x = data['idValues']
         for k,v in self.lines.iteritems():
             self.y = data[k]
@@ -282,39 +317,22 @@ class CtfMonitorPlotter(EmPlotter):
         self.ax.autoscale()
         self.ax.grid(True)
         self.ax.legend(loc=2).get_frame().set_alpha(0.5)
-	self.ax.axhline(y=self.protocol.minDefocus, c="red", linewidth=0.5, linestyle='dashed', zorder=0)
-	self.ax.axhline(y=self.protocol.maxDefocus, c="red", linewidth=0.5, linestyle='dashed', zorder=0)
+        self.ax.axhline(y=self.monitor.minDefocus, c="red",
+                        linewidth=0.5, linestyle='dashed', zorder=0)
+        self.ax.axhline(y=self.monitor.maxDefocus, c="red",
+                        linewidth=0.5, linestyle='dashed', zorder=0)
+
     def show(self):
         self.paint(['defocusU','defocusV'])
-
 
     def paint(self, labels):
         for label in labels:
             self.lines[label],=self.ax.plot([], [], '-',label=label)
 
-        anim = animation.FuncAnimation(self.fig, self.animate, interval = self.samplingInterval.get() * 1000)#miliseconds
+        anim = animation.FuncAnimation(self.fig, self.animate,
+                                       interval=self.monitor.samplingInterval*1000)#miliseconds
 
         self.fig.canvas.mpl_connect('scroll_event', self.onscroll)
         self.fig.canvas.mpl_connect('key_press_event', self.press)
         EmPlotter.show(self)
 
-    def getData(self):
-
-        cur = self.cur
-
-        def get(name):
-            try:
-                cur.execute("select %s from %s" % (name, self.tableName))
-            except Exception as e:
-                print("ERROR readind data (plotter). I continue")
-            return [r[0] for r in cur.fetchall()]
-
-        data = {
-                'defocusU': get('defocusU'),
-                'defocusV': get('defocusV'),
-                'astigmatism': get('astigmatism'),
-                'ratio': get('ratio'),
-                'idValues': get('id')
-                }
-        #conn.close()
-        return data
