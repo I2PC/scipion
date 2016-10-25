2ad1cfab687d156fbb812e00cb282d20375da223
josuegbl <josuegbl@git.code.sf.net>
2016-10-25 15:41:57 +0200

    more on validation improvement

--- pyworkflow/em/packages/motioncorr/protocol_motioncorr.py
+++ pyworkflow/em/packages/motioncorr/protocol_motioncorr.py
@@ -262,8 +262,8 @@ class ProtMotionCorr(ProtAlignMovies):
             if not self.useAlignToSum:
                 errors.append('Frame range for ALIGN and SUM must be '
                               'equivalent in case of motioncor2. \n Please, '
-							  'set *YES* _Use ALIGN frames range to SUM?_ '
-							  'flag or use motioncorr')
+                              'set *YES* _Use ALIGN frames range to SUM?_ '
+                              'flag or use motioncorr')
 
         return errors
 
