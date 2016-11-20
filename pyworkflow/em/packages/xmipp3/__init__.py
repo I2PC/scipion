8ece15f4093a28f82fda24856f15cf3eb3d789d3
Gregory Sharov <sharov.grigory@gmail.com>
2016-11-20 19:00:47 +0100

    fix merge with devel

--- pyworkflow/em/packages/xmipp3/__init__.py
+++ pyworkflow/em/packages/xmipp3/__init__.py
@@ -82,6 +82,7 @@ from protocol_extract_particles_pairs import XmippProtExtractParticlesPairs
 from protocol_helical_parameters import XmippProtHelicalParameters
 from protocol_kerdensom import XmippProtKerdensom
 from protocol_ml2d import XmippProtML2D
+from protocol_movie_gain import XmippProtMovieGain
 from protocol_mltomo import XmippProtMLTomo
 from protocol_movie_average import XmippProtMovieAverage
 from protocol_movie_correlation import XmippProtMovieCorr
