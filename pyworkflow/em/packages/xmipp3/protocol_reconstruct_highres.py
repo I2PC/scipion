1a5ac6d3c422b7ea0699c686c8a86f7084019440
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2016-12-07 09:17:12 +0100

    The number of MPI should be the general one

--- pyworkflow/em/packages/xmipp3/protocol_reconstruct_highres.py
+++ pyworkflow/em/packages/xmipp3/protocol_reconstruct_highres.py
@@ -1176,7 +1176,7 @@ class XmippProtReconstructHighRes(ProtRefine3D, HelicalFinder):
                          (fnAnglesToUse,fnGrayRoot,TsCurrent,R,self.contPadding.get(),fnRefVol,previousResolution,fnGrayRoot)
                     args+=" --max_gray_scale %f --max_gray_shift %f --Nsimultaneous %d"%\
                          (self.contMaxGrayScale.get(),self.contMaxGrayShift.get(),self.contSimultaneous.get())
-                    self.runJob("xmipp_transform_adjust_image_grey_levels",args,numberOfMpi=self.contSimultaneous.get())
+                    self.runJob("xmipp_transform_adjust_image_grey_levels",args)
                     fnAnglesToUse = fnGrayRoot+".xmd"
                     if deleteStack:
                         cleanPath(deletePattern)
