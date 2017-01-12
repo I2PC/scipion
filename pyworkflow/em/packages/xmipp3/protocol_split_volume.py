718eef43489e6c089f927cb1e3ad0f38914ff5e3
pconesa <p.conesa.mingo@gmail.com>
2017-01-12 15:03:02 +0100

    Remove protocol_directional_classes import Add clasify_first_split to Xmipp scons script.

--- pyworkflow/em/packages/xmipp3/protocol_split_volume.py
+++ pyworkflow/em/packages/xmipp3/protocol_split_volume.py
@@ -32,7 +32,6 @@ from pyworkflow.protocol.params import PointerParam, FloatParam, IntParam, Strin
 from pyworkflow.em.protocol import ProtClassify3D
 from pyworkflow.em.data import Volume
 from pyworkflow.em.convert import ImageHandler
-from pyworkflow.em.packages.xmipp3.protocol_directional_classes import XmippProtDirectionalClasses
 from convert import writeSetOfParticles
 
 class XmippProtSplitvolume(ProtClassify3D):
