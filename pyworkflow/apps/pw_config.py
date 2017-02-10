e67e89983d1510ef6e625372d8b17194b43788b4
roberto <roberto.marabini.uam.es>
2017-02-10 23:36:27 +0100

    add scipio notifier configutation

--- pyworkflow/apps/pw_config.py
+++ pyworkflow/apps/pw_config.py
@@ -117,37 +117,37 @@ def main():
 def chekReport(Config):
     "Check if protocol statistics should be collected"
     reportOn = Config.get('VARIABLES','SCIPION_NOTIFY')
-    print("ooooo", reportOn)
     if reportOn=='False':
-        print("aaaaa")
         # This works for  Python 2.x. and Python 3
         try: 
            input = raw_input
         except NameError: 
              pass
-        print("""The Scipion developers team would appreciate if you 
-anonymously share your protocol usage statistics. These statistics
-just include the name of the protocols executed in your machine.
-We understand, of course, that some people do not wish to have any 
-information collected from them. You can enable or disable the 
-collection of information at any time (by default it is disabled).
+        print("""-----------------------------------------------------------------
+-----------------------------------------------------------------
+It would be very helpful if you allow Scipion
+to send anonymous usage data. This information will help Scipion's 
+team to identify the more demanded protocols and prioritize 
+support for them.
 
-This information will help our team to understand the popularity of 
-the different protocols and prioritize support for the more popular ones.
-
-Please, note that the statistics are completely anonymous and do not include 
-protocol parameters, file names or any data that can be used to identify
-your account, machine or data. In the URL 
+The collected usage information is completely anonymous and does not 
+include protocol parameters, file names or any data that can be used 
+to identify your account, machine or data. In the URL 
 https://github.com/I2PC/scipion/wiki/Collecting-Usage-Statistics-for-Scipion 
 you may see examples of the transmited data as well as the 
 statistics created with it. You can always deactivate/activate 
 this option by editing the file $HOME/.config/scipion/scipion.conf 
 and setting the variable SCIPION_NOTIFY to False/True respectively.
+
+We understand, of course, that you may not wish to have any 
+information collected from you and we respect your privacy.
 """)
 
-        prompt = input("Press [y|Y]<enter> is you want to share data, otherwise just press <enter>: ")
-        if prompt == 'y' or prompt=='Y':
+        prompt = input("Press <enter> is you want to share data, otherwise press any key followed by <enter>: ")
+        if prompt == '':
             Config.set('VARIABLES','SCIPION_NOTIFY','True')
+        print(yellow("Statistics Collection has been set to: %s"%Config.get('VARIABLES','SCIPION_NOTIFY')))
+        print("-----------------------------------------------------------------\n-----------------------------------------------------------------")
 
 def createConf(fpath, ftemplate, remove=[], keep=[]):
     "Create config file in fpath following the template in ftemplate"
@@ -168,6 +168,7 @@ def createConf(fpath, ftemplate, remove=[], keep=[]):
 
     # Read the template configuration file.
     print(yellow("* Creating configuration file: %s" % fpath))
+    print("Please edit it to reflect the configuration of your system.\n")
     cf = ConfigParser()
     cf.optionxform = str  # keep case (stackoverflow.com/questions/1611799)
     assert cf.read(ftemplate) != [], 'Missing file: %s' % ftemplate
@@ -190,7 +191,7 @@ def createConf(fpath, ftemplate, remove=[], keep=[]):
 
     # Create the actual configuration file.
     cf.write(open(fpath, 'w'))
-    print("Please edit it to reflect the configuration of your system.\n")
+    #print("Please edit it to reflect the configuration of your system.\n")
 
 
 def checkPaths(conf):
@@ -296,6 +297,8 @@ def checkConf(fpath, ftemplate, remove=[], keep=[], update=False):
                       "parameter to update local config files." % (yellow(s), yellow(o), UPDATE_PARAM))
 
                 if update:
+                    if s=='VARIABLES' and o=='SCIPION_NOTIFY':
+                        chekReport(ct)
                     # Update config file with missing variable
                     value = ct.get(s, o)
                     cf.set(s, o, value)
