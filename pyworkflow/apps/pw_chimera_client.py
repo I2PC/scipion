465e41d0336227079ee19609d27eb0cece283d37
Carlos Oscar S. Sorzano <coss@cnb.csic.es>
2017-02-24 22:12:24 +0100

    The default value of Projection interpolation in Fourier space was set to BSpline2, but this was not implemented

--- pyworkflow/apps/pw_chimera_client.py
+++ pyworkflow/apps/pw_chimera_client.py
@@ -24,13 +24,12 @@ def main():
     projectorParser.add_argument('--projectionSize', help='Projection window dimensions', type=int)
     projectorParser.add_argument('--paddingFactor', default=1, type=float, help='Projection padding factor')
     projectorParser.add_argument('--maxFreq', default=0.5, type=float, help='Projection spline degree')
-    projectorParser.add_argument('--splineDegree', default='BSPLINE2',
-                        choices=['NEAREST', 'LINEAR', 'BSPLINE2', 'BSPLINE3', 'BSPLINE4'], help='projection window dimensions')
+    projectorParser.add_argument('--splineDegree', default='BSPLINE3',
+                        choices=['NEAREST', 'LINEAR', 'BSPLINE3'], help='projection window dimensions')
     projectorParser.add_argument('--showjPort', help='Port to link projections to chimera', type=int)
 
 
-    splineDegreeDict = {'NEAREST': xmipp.NEAREST, 'LINEAR': xmipp.LINEAR,
-                        'BSPLINE2': xmipp.BSPLINE2, 'BSPLINE3': xmipp.BSPLINE3, 'BSPLINE4': xmipp.BSPLINE4}
+    splineDegreeDict = {'NEAREST': xmipp.NEAREST, 'LINEAR': xmipp.LINEAR, 'BSPLINE3': xmipp.BSPLINE3}
 
     args = parentParser.parse_args()
     #print args
