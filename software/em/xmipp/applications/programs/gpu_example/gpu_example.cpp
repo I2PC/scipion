73d40f8f08a0f082228f89445878d0e087ee350c
Pablo Conesa <p.conesa.mingo@gmail.com>
2017-03-03 16:58:19 +0100

    Update gpu_example.cpp

--- software/em/xmipp/applications/programs/gpu_example/gpu_example.cpp
+++ software/em/xmipp/applications/programs/gpu_example/gpu_example.cpp
@@ -8,11 +8,10 @@
 #include <stdio.h>
 #include <time.h>
 
-#include <data/metadata.h>
-
 // For the CUDA runtime routines (prefixed with "cuda_")
 #include <cuda_runtime.h>
-
+// Xmipp stuff
+#include <data/metadata.h>
 /**
  * CUDA Kernel Device code
  *
@@ -121,8 +120,8 @@ main(void)
 
     // Reset the device and exit
     cudaDeviceReset();
-	
-    //Do something with xmipp libraries		
+
+    //Do something with xmipp libraries
     MetaData mDsource;
     size_t id = mDsource.addObject();
     mDsource.setValue(MDL_X,1.,id);
@@ -132,7 +131,6 @@ main(void)
     mDsource.setValue(MDL_Y,4.,id);
     mDsource.write("kk.xmd");
     printf("Metadata written in file %s\n","kk.xmd");
-
     printf("Done\n");
     return 0;
 }
