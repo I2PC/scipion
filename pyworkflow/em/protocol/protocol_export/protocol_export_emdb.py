93026cd63bf2556ec5acebd11b21ba047bd96c7b
ajimoreno <ajimenez@gmail.com>
2017-03-17 12:41:04 +0100

    Revised version of protocol_export_emdb with the supervision of @josuegbl

--- pyworkflow/em/protocol/protocol_export/protocol_export_emdb.py
+++ pyworkflow/em/protocol/protocol_export/protocol_export_emdb.py
@@ -25,27 +25,33 @@
 # **************************************************************************
 
 
-import re
+import os
+
 import pyworkflow.protocol.params as params
-from pyworkflow.em.protocol import EMProtocol
+
 from pyworkflow import VERSION_1_2
 from pyworkflow.em.convert import ImageHandler
-import os
+from pyworkflow.em.protocol import EMProtocol
 
 
 class ProtExportEMDB(EMProtocol):
-    """ stress  will  stress  test  a  computer system in various selectable
-       ways. Several options require the program stress-ng.
+    """ generates files for volumes and FSCs to submit structures to EMDB
     """
-    _label = 'exportEMDB'
+    _label = 'export emdb'
     _program = "" 
-    _version = VERSION_1_2
+    #_version = VERSION_1_2
 
     def __init__(self, **kwargs):
         EMProtocol.__init__(self, **kwargs)
-        self.xmippMic={}
 
-    #--------------------------- DEFINE param functions ----------------------
+    def _createFileNamesTemplates(self):
+        myDict = {
+                  'volume' : 'final_volume.mrc',
+                  'fsc' : 'final_fsc.xml'
+                  }
+        self._updateFilenamesDict(myDict)
+
+        #--------------------------- DEFINE param functions ----------------------
     def _defineParams(self, form):
         form.addSection(label='Input')
 
@@ -55,28 +61,29 @@ class ProtExportEMDB(EMProtocol):
         form.addParam('exportFSC', params.PointerParam, label="FSC to export", important=True,
                       pointerClass='FSC',
                       help='This FSC will be exported using XML emdb format')
+        form.addParam('filesPath', params.PathParam, important=True,
+                      label="Export to directory",
+                      help="Directory where the files will be generate.")
 
     #--------------------------- INSERT steps functions --------------------------------------------
     def _insertAllSteps(self):
-            self._insertFunctionStep('exportVolumeStep')
-            self._insertFunctionStep('exportFSCStep')
+        self._createFileNamesTemplates()
+        self._insertFunctionStep('exportVolumeStep')
+        self._insertFunctionStep('exportFSCStep')
 
     #--------------------------- STEPS functions --------------------------------------------
 
     def exportVolumeStep(self):
 
-        self.nombreVolumen, self.nombreFsc = self.getName()
-
         ih = ImageHandler()
-        cmdFile = self._getExtraPath(self.nombreVolumen)
-        ih.convert(self.exportVolume.get().getLocation(), cmdFile)
+        ih.convert(self.exportVolume.get().getLocation(), self.getFnPath())
 
     def exportFSCStep(self):
 
         x,y = self.exportFSC.get().getData()
-        cmdFSCFile = self._getExtraPath(self.nombreFsc)
-        fo = open(cmdFSCFile, "w")
-        fo.write('<fsc title="FSC(%s)" xaxis="Resolution(A-1)" yaxis="Correlation Coefficient">\n'%self.nombreVolumen)
+        fo = open(self.getFnPath("fsc"), "w")
+        fo.write('<fsc title="FSC(%s)" xaxis="Resolution(A-1)" '
+                 'yaxis="Correlation Coefficient">\n' % self._getFileName('volume'))
         for i in range(len(x)):
             fo.write("<coordinate>\n")
             fo.write("<x>%f</x>\n"%x[i])
@@ -86,14 +93,16 @@ class ProtExportEMDB(EMProtocol):
         fo.write("</fsc>\n")
         fo.close()
 
-
+    #--------------------------- INFO functions --------------------------------------------
     def _validate(self):
         message = []
+        fnPath = self.filesPath.get()
+        if fnPath == "" or fnPath is None:
+            message.append("You must set a path to export.")
         return message
 
     def _summary(self):
-        message = "Data Available at directory: *%s*"% os.path.abspath(self._getExtraPath())
-
+        message = "Data Available at : *%s*"% self.filesPath.get()
         return [message]
 
     def _methods(self):
@@ -101,14 +110,5 @@ class ProtExportEMDB(EMProtocol):
 
 #--------------------------- UTILS functions ---------------------------------------------------
 
-    def getName(self):
-        name = self.exportVolume.get().getFileName()
-        match = re.search(r'[\w.]+.mrc', name)
-        if match:
-            nombreVolumen = match.group()[:-4]
-            nombreFsc = match.group()[:-8] + '.xml'
-        else:
-            nombreVolumen = "volume.mrc"
-            nombreFsc = "fsc.xml"
-
-        return nombreVolumen, nombreFsc
+    def getFnPath(self, label='volume'):
+        return os.path.join(self.filesPath.get(), self._getFileName(label))
