4f5c24550d21e931dadf4ef9bfb1f41e89157b20
ajimoreno <ajimenez@gmail.com>
2017-03-24 13:03:50 +0100

    testing how to create a cuda library

--- software/em/xmipp/libraries/cuda_cubic_interpolation/bspline_kernel.cpp
+++ software/em/xmipp/libraries/cuda_cubic_interpolation/bspline_kernel.cpp
@@ -32,7 +32,7 @@ policies, either expressed or implied.
 
 When using this code in a scientific project, please cite one or all of the
 following papers:
-*  Daniel Ruijters and Philippe Thévenaz,
+*  Daniel Ruijters and Philippe Thï¿½venaz,
    GPU Prefilter for Accurate Cubic B-Spline Interpolation, 
    The Computer Journal, vol. 55, no. 1, pp. 15-20, January 2012.
    http://dannyruijters.nl/docs/cudaPrefilter3.pdf
@@ -45,7 +45,7 @@ following papers:
 #define _CUDA_BSPLINE_H_
 
 #include "cutil_math_bugfixes.h"
-#include "math_func.cu"
+#include "math_func.cpp"
 
 // Cubic B-spline function
 // The 3rd order Maximal Order and Minimum Support function, that it is maximally differentiable.
