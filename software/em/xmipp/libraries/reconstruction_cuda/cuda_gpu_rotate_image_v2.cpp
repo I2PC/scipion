81539e87e8187d9802a002224da27e4cc6a1981b
ajimoreno <ajimenez@gmail.com>
2017-03-27 18:03:52 +0200

    testing how to create a cuda library

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_v2.cpp
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_v2.cpp
@@ -7,6 +7,7 @@
 #include <cuda_runtime.h>
 #include "cuda_basic_math.cpp"
 #include "cuda_copy_data.cpp"
+#include "cuda_prefiltering_rotation.cpp"
 #include "cuda_gpu_rotate_image_v2.h"
 
 
@@ -16,110 +17,6 @@ texture<float, 2, cudaReadModeElementType> texRef;
 
 
 //CUDA functions
-
-template<class floatN>
-__device__ floatN InitialCausalCoefficient(
-	floatN* c,			// coefficients
-	uint DataLength,	// number of coefficients
-	int step)			// element interleave in bytes
-{
-	const uint Horizon = UMIN(12, DataLength);
-
-	// this initialization corresponds to clamping boundaries
-	// accelerated loop
-	float zn = Pole;
-	float Sum = *c;
-	for (uint n = 0; n < Horizon; n++) {
-		Sum += zn * *c;
-		zn *= Pole;
-		c = (floatN*)((uchar*)c + step);
-	}
-	return(Sum);
-}
-
-template<class floatN>
-__device__ floatN InitialAntiCausalCoefficient(
-	floatN* c,			// last coefficient
-	uint DataLength,	// number of samples or coefficients
-	int step)			// element interleave in bytes
-{
-	// this initialization corresponds to clamping boundaries
-	return((Pole / (Pole - 1.0f)) * *c);
-}
-
-template<class floatN>
-__device__ void ConvertToInterpolationCoefficients(
-	floatN* coeffs,		// input samples --> output coefficients
-	uint DataLength,	// number of samples or coefficients
-	int step)			// element interleave in bytes
-{
-	// compute the overall gain
-	const float Lambda = (1.0f - Pole) * (1.0f - 1.0f / Pole);
-
-	// causal initialization
-	floatN* c = coeffs;
-	floatN previous_c;  //cache the previously calculated c rather than look it up again (faster!)
-	*c = previous_c = Lambda * InitialCausalCoefficient(c, DataLength, step);
-	// causal recursion
-	for (uint n = 1; n < DataLength; n++) {
-		c = (floatN*)((uchar*)c + step);
-		*c = previous_c = Lambda * *c + Pole * previous_c;
-	}
-	// anticausal initialization
-	*c = previous_c = InitialAntiCausalCoefficient(c, DataLength, step);
-	// anticausal recursion
-	for (int n = DataLength - 2; 0 <= n; n--) {
-		c = (floatN*)((uchar*)c - step);
-		*c = previous_c = Pole * (previous_c - *c);
-	}
-}
-
-template<class floatN>
-__global__ void SamplesToCoefficients2DX(
-	floatN* image,		// in-place processing
-	uint pitch,			// width in bytes
-	uint width,			// width of the image
-	uint height)		// height of the image
-{
-	// process lines in x-direction
-	const uint y = blockIdx.x * blockDim.x + threadIdx.x;
-	floatN* line = (floatN*)((uchar*)image + y * pitch);  //direct access
-
-	ConvertToInterpolationCoefficients(line, width, sizeof(floatN));
-}
-
-template<class floatN>
-__global__ void SamplesToCoefficients2DY(
-	floatN* image,		// in-place processing
-	uint pitch,			// width in bytes
-	uint width,			// width of the image
-	uint height)		// height of the image
-{
-	// process lines in x-direction
-	const uint x = blockIdx.x * blockDim.x + threadIdx.x;
-	floatN* line = image + x;  //direct access
-
-	ConvertToInterpolationCoefficients(line, height, pitch);
-}
-
-
-template<class floatN>
-void CubicBSplinePrefilter2D(floatN* image, uint pitch, uint width, uint height)
-{
-
-	dim3 dimBlockX(min(PowTwoDivider(height), 64));
-	dim3 dimGridX(height / dimBlockX.x);
-	SamplesToCoefficients2DX<floatN><<<dimGridX, dimBlockX>>>(image, pitch, width, height);
-
-	dim3 dimBlockY(min(PowTwoDivider(width), 64));
-	dim3 dimGridY(width / dimBlockY.x);
-	SamplesToCoefficients2DY<floatN><<<dimGridY, dimBlockY>>>(image, pitch, width, height);
-
-}
-
-
-
-
 
 __device__ void bspline_weights(float2 fraction, float2& w0, float2& w1, float2& w2, float2& w3)
 {
