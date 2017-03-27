10c9f03b6d38d6ad291b48f21c9ca8f38b8f31ef
ajimoreno <ajimenez@gmail.com>
2017-03-27 17:59:26 +0200

    testing how to create a cuda library

---
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_copy_data.cpp
@@ -0,0 +1,51 @@
+/***************************************************************************
+ * Authors:     AUTHOR_NAME (amaia@cnb.csic.es)
+ *
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.csic.es'
+ ***************************************************************************/
+
+
+cudaPitchedPtr CopyVolumeHostToDevice(const float* host, uint width, uint height, uint depth)
+{
+	cudaPitchedPtr device = {0};
+	const cudaExtent extent = make_cudaExtent(width * sizeof(float), height, depth);
+	cudaMalloc3D(&device, extent);
+	cudaMemcpy3DParms p = {0};
+	p.srcPtr = make_cudaPitchedPtr((void*)host, width * sizeof(float), width, height);
+	p.dstPtr = device;
+	p.extent = extent;
+	p.kind = cudaMemcpyHostToDevice;
+	cudaMemcpy3D(&p);
+	return device;
+}
+
+void CopyVolumeDeviceToHost(float* host, const cudaPitchedPtr device, uint width, uint height, uint depth)
+{
+	const cudaExtent extent = make_cudaExtent(width * sizeof(float), height, depth);
+	cudaMemcpy3DParms p = {0};
+	p.srcPtr = device;
+	p.dstPtr = make_cudaPitchedPtr((void*)host, width * sizeof(float), width, height);
+	p.extent = extent;
+	p.kind = cudaMemcpyDeviceToHost;
+	cudaMemcpy3D(&p);
+	cudaFree(device.ptr);  //free the GPU volume
+}
