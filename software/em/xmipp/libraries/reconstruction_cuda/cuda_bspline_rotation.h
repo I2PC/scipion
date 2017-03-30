a4dfca8466c790fbd89fe0f3ef20b57121dfd9c3
ajimoreno <ajimenez@gmail.com>
2017-03-30 10:54:25 +0200

    testing how to create a cuda library

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_bspline_rotation.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_bspline_rotation.h
@@ -27,6 +27,6 @@
 
 // Cubic B-spline function
 // The 3rd order Maximal Order and Minimum Support function, that it is maximally differentiable.
-__device__ float bspline(float t);
+float bspline(float t);
 
 
