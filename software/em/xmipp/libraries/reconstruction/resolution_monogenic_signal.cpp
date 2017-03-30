8559b932c3e5525c04e10258a0ce50d6a49d9298
vilas <jlvilas@cnb.csic.es>
2017-03-30 15:50:44 +0200

    MonoRes: The band filter for the filtered monogenic amplitude has been changed to low pass filter

--- software/em/xmipp/libraries/reconstruction/resolution_monogenic_signal.cpp
+++ software/em/xmipp/libraries/reconstruction/resolution_monogenic_signal.cpp
@@ -356,16 +356,9 @@ void ProgMonogenicSignalRes::amplitudeMonogenicSignal3D(MultidimArray< std::comp
 	#endif // DEBUG
 //
 	// Low pass filter the monogenic amplitude
-	lowPassFilter.FilterBand = BANDPASS;
-	FilterBand.w1 = w1l;
-	FilterBand.w2 = w1h;
+	lowPassFilter.w1 = w1h;
 	amplitude.setXmippOrigin();
-	FilterBand.applyMaskSpace(amplitude);
-
-
-//	lowPassFilter.w1 = w1h;
-//	amplitude.setXmippOrigin();
-//	lowPassFilter.applyMaskSpace(amplitude);
+	lowPassFilter.applyMaskSpace(amplitude);
 
 	#ifdef DEBUG
 	//Image<double> saveImg2;
