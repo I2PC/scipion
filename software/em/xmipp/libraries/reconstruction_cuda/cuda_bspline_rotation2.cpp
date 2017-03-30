6ee994e5e68e3297dbd6cf879645a96de9ea5330
ajimoreno <ajimenez@gmail.com>
2017-03-30 10:53:35 +0200

    testing how to create a cuda library

---
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_bspline_rotation2.cpp
@@ -0,0 +1,41 @@
+/***************************************************************************
+ * Authors:     Amaya Jimenez (ajimenez@cnb.csic.es)
+ *
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.csic.es'
+ ***************************************************************************/
+
+
+
+// Cubic B-spline function
+// The 3rd order Maximal Order and Minimum Support function, that it is maximally differentiable.
+__device__ float bspline(float t)
+{
+	t = fabs(t);
+	const float a = 2.0f - t;
+
+	if (t < 1.0f) return 2.0f/3.0f - 0.5f*t*t*a;
+	else if (t < 2.0f) return a*a*a / 6.0f;
+	else return 0.0f;
+}
+
+
+
