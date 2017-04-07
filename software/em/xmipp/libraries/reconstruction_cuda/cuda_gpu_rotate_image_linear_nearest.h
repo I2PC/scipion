5f908f456706ef1c038845f82328f924ba5e37a9
ajimoreno <ajimenez@gmail.com>
2017-04-07 14:14:15 +0200

    comments to explain the purpose of this functions were added

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_linear_nearest.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_linear_nearest.h
@@ -1,2 +1,4 @@
 
-void cuda_rotate_image(float *image, float *rotated_image, size_t Xdim, size_t Ydim, size_t Zdim, double *ang, int interp);
+/** Native implementation when interpolation is either nearest neighbor or linear*/
+void cuda_rotate_image_linear_nearest(float *image, float *rotated_image, size_t Xdim, size_t Ydim, size_t Zdim, double *ang, int interp, int wrap, int first_call);
+
