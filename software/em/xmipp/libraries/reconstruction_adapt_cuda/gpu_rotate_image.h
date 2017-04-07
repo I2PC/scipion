d8537e18c15797706a409ff23f5d87c4ee55a55a
ajimoreno <ajimenez@gmail.com>
2017-04-07 13:06:27 +0200

    several libraries included

--- software/em/xmipp/libraries/reconstruction_adapt_cuda/gpu_rotate_image.h
+++ software/em/xmipp/libraries/reconstruction_adapt_cuda/gpu_rotate_image.h
@@ -25,39 +25,69 @@
 #ifndef _PROG_GPU_ROTATE_IMAGE
 #define _PROG_GPU_ROTATE_IMAGE
 
-
 #include <data/xmipp_program.h>
+#include <data/xmipp_image.h>
+#include <data/xmipp_program.h>
+#include <data/transformations.h>
+#include <data/args.h>
+#include <data/matrix2d.h>
 
+#include <stdio.h>
+#include <time.h>
+
+#include "../reconstruction_cuda/cuda_gpu_rotate_image_bspline.h"
+#include "../reconstruction_cuda/cuda_gpu_rotate_image_linear_nearest.h"
+
+#define CUDA_NEAREST 0 //xmipp NEAREST
+#define CUDA_LINEAR  1 //xmipp linear
+#define CUDA_BSPLINE 2 //xmipp_BSPLINE3
+
+#define CUDA_BORDER 3 //cudaAddressModeBorder =  3
+#define CUDA_CLAMP  1 //cudaAddressModeClamp =  1
+#define CUDA_WRAP 0 //cudaAddressModeWrap =  0
+
+#define USE_MD 0
+#define USE_COMMAND 1
+#define USE_BOTH 2
 
 /**@defgroup AngularDistance angular_distance (Distance between two angular assignments)
    @ingroup ReconsLibrary */
 //@{
 /** Angular Distance parameters. */
 
-class ProgGpuRotateImage: public XmippProgram
-{
-public:
-    /** Filename angle doc 1 */
-	FileName fnRef;
-    FileName fnOut;
-    int interp;
 
+void transformationMatrixCuda(const MDRow &imageGeo, Matrix2D<double> &A, bool isVol);
+
+//CUDA functions
+void cuda_rotate_image(float *image, float *rotated_image, size_t Xdim,
+		size_t Ydim, size_t Zdim, double* ang, int interp, int wrap, int first_call);
+
+
+class ProgGpuRotateImage: public XmippMetadataProgram
+{
+private:
+    /** Filename angle doc 1 */
+	//FileName fnRef;
+    //FileName fnOut;
+    int splineDegree, dim, wrapMode, use_md;
+    Matrix2D<double> R, A;
+    bool isVol, isMd, flip;
+    int first_call;
 
 public:
     /// Read argument from command line
     void readParams();
 
     /// Show
-    void show();
+    //void show();
 
     /// Define parameters
     void defineParams();
 
-    /** Run */
-    void run();
+    void preProcess();
 
-public:
-    double angIm, angVolY, angVolZ;
+    /** processImage */
+    void processImage(const FileName &fnImg, const FileName &fnImgOut, const MDRow &rowIn, MDRow &rowOut);
 
 };
 //@}
