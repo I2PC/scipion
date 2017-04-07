289a83cfd6ae842a8e819ef721141147c3f4db58
ajimoreno <ajimenez@gmail.com>
2017-04-07 14:12:39 +0200

    comment about the reference implementation included

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_bspline.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_bspline.h
@@ -1,2 +1,4 @@
 
-void cuda_rotate_image_v2(float *image, float *rotated_image, size_t Xdim, size_t Ydim, size_t Zdim, double *ang);
+/** Bspline interpolation as implemented in http://www.dannyruijters.nl/cubicinterpolation/ */
+void cuda_rotate_image_bspline(float *image, float *rotated_image, size_t Xdim, size_t Ydim, size_t Zdim, double *ang, int wrap, int first_call);
+
