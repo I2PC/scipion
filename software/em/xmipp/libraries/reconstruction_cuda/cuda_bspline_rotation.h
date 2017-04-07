5aedf070cd34d31a1737bfd0258df0756bb6a620
ajimoreno <ajimenez@gmail.com>
2017-04-07 14:20:45 +0200

    added a library with function to carry out a bspline interpolation

---
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_bspline_rotation.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ * Authors:     Amaya Jimenez (ajimenez@cnb.csic.es)
+ *
+ *
+ * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ * 02111-1307  USA
+ *
+ *  All comments concerning this program package may be sent to the
+ *  e-mail address 'xmipp@cnb.csic.es'
+ ***************************************************************************/
+
+
+#ifndef CUDA_BSPLINE_H
+#define CUDA_BSPLINE_H
+
+template<class floatN>
+__device__ void bspline_weights(floatN fraction, floatN& w0, floatN& w1, floatN& w2, floatN& w3)
+{
+	const floatN one_frac = 1.0f - fraction;
+	const floatN squared = fraction * fraction;
+	const floatN one_sqd = one_frac * one_frac;
+
+	w0 = 1.0f/6.0f * one_sqd * one_frac;
+	w1 = 2.0f/3.0f - 0.5f * squared * (2.0f-fraction);
+	w2 = 2.0f/3.0f - 0.5f * one_sqd * (2.0f-one_frac);
+	w3 = 1.0f/6.0f * squared * fraction;
+}
+
+#endif
