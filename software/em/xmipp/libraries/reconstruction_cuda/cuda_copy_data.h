74b90b728e72672e73dc1e21e6429b71384f7cb4
ajimoreno <ajimenez@gmail.com>
2017-04-07 14:10:44 +0200

    definitions to protect the includes of this library

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_copy_data.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_copy_data.h
@@ -1,4 +1,10 @@
 
+
+#ifndef CUDA_COPYDATA_H
+#define CUDA_COPYDATA_H
+
 cudaPitchedPtr CopyVolumeHostToDevice(const float* host, uint width, uint height, uint depth);
 
 void CopyVolumeDeviceToHost(float* host, const cudaPitchedPtr device, uint width, uint height, uint depth);
+
+#endif
