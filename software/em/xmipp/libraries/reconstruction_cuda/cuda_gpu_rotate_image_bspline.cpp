65f018501f3fa18ac72736bf8aad98a348c59bd2
ajimoreno <ajimenez@gmail.com>
2017-04-20 14:05:13 +0200

    printing times for debugging

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_bspline.cpp
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_bspline.cpp
@@ -14,26 +14,23 @@
 #include "cuda_interpolation2D_rotation.h"
 #include "cuda_interpolation3D_rotation.h"
 #include "cuda_check_errors.h"
+#include "cuda_utils.h"
 
 
 //CUDA functions
 
-void cuda_rotate_image_bspline(float *image, float *rotated_image, size_t Xdim, size_t Ydim, size_t Zdim, double *ang, int wrap, int first_call){
+void cuda_rotate_image_bspline(float *image, float *rotated_image, size_t Xdim, size_t Ydim, size_t Zdim,
+		double *ang, int wrap, int first_call, struct ioTime* mytime){
 
-//#define TIME
 
 	if(first_call==1){
 		gpuErrchk(cudaSetDevice(0));
 		gpuErrchk(cudaFree(0));
 	}
 
-
-#ifdef TIME
-	clock_t t_ini0, t_fin0;
-	double secs0;
-	t_ini0 = clock();
-#endif
-
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_ini_gpu_mem_inS, NULL);
+}
 
 	struct cudaPitchedPtr bsplineCoeffs, cudaOutput;
 	bsplineCoeffs = CopyVolumeHostToDevice(image, (uint)Xdim, (uint)Ydim, (uint)Zdim);
@@ -42,20 +39,17 @@ void cuda_rotate_image_bspline(float *image, float *rotated_image, size_t Xdim, 
     cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<float>();
     cudaArray* cuArray;
 
-#ifdef TIME
-    t_fin0 = clock();
-    secs0 = (double)(t_fin0 - t_ini0) / CLOCKS_PER_SEC;
-    printf("CopyVolumeHostToDevice: %.16g milisegundos\n", (secs0) * 1000.0);
-#endif
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_fin_gpu_mem_inS, NULL);
+    mytime->secs_gpu_mem_inS = timeval_diff(&mytime->t_fin_gpu_mem_inS, &mytime->t_ini_gpu_mem_inS);
+}
 
 
     if(Zdim==1){
 
-#ifdef TIME
-	clock_t t_ini2, t_fin2;
-	double secs2;
-	t_ini2 = clock();
-#endif
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_ini_gpu_filtS, NULL);
+}
 
     	//Filtering process (first step)
     	CubicBSplinePrefilter2D((float*)bsplineCoeffs.ptr, (uint)bsplineCoeffs.pitch, (uint)Xdim, (uint)Ydim);
@@ -74,51 +68,40 @@ void cuda_rotate_image_bspline(float *image, float *rotated_image, size_t Xdim, 
     	texRef.addressMode[0] = (cudaTextureAddressMode)wrap;
     	texRef.addressMode[1] = (cudaTextureAddressMode)wrap;
 
-#ifdef TIME
-    t_fin2 = clock();
-    secs2 = (double)(t_fin2 - t_ini2) / CLOCKS_PER_SEC;
-    printf("Memory host to device and prefiltering: %.16g milisegundos\n", (secs2) * 1000.0);
-#endif
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_fin_gpu_filtS, NULL);
+    mytime->secs_gpu_filtS = timeval_diff(&mytime->t_fin_gpu_filtS, &mytime->t_ini_gpu_filtS);
+}
 
-#ifdef TIME
-	clock_t t_ini3, t_fin3;
-	double secs3;
-	t_ini3 = clock();
-#endif
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_ini_gpu_kernelS, NULL);
+}
 
     	//Interpolation (second step)
     	cudaOutput = interpolate2D(Xdim, Ydim, ang);
     	gpuErrchk(cudaDeviceSynchronize());
 
-#ifdef TIME
-    t_fin3 = clock();
-    secs3 = (double)(t_fin3 - t_ini3) / CLOCKS_PER_SEC;
-    printf("Kernel (interpolation): %.16g milisegundos\n", secs3 * 1000.0);
-#endif
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_fin_gpu_kernelS, NULL);
+    mytime->secs_gpu_kernelS = timeval_diff(&mytime->t_fin_gpu_kernelS, &mytime->t_ini_gpu_kernelS);
+}
 
-#ifdef TIME
-	clock_t t_ini4, t_fin4;
-	double secs4;
-	t_ini4 = clock();
-#endif
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_ini_gpu_mem_outS, NULL);
+}
 
     	CopyVolumeDeviceToHost(rotated_image, cudaOutput, Xdim, Ydim, Zdim);
 
-#ifdef TIME
-    t_fin4 = clock();
-    secs4 = (double)(t_fin4 - t_ini4) / CLOCKS_PER_SEC;
-    printf("Memory device to host: %.16g milisegundos\n", secs4 * 1000.0);
-#endif
-
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_fin_gpu_mem_outS, NULL);
+    mytime->secs_gpu_mem_outS = timeval_diff(&mytime->t_fin_gpu_mem_outS, &mytime->t_ini_gpu_mem_outS);
+}
 
     }else if (Zdim>1){
 
-
-#ifdef TIME
-	clock_t t_ini5, t_fin5;
-	double secs5;
-	t_ini5 = clock();
-#endif
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_ini_gpu_filtS, NULL);
+}
 
     	//Filtering process (first step)
     	CubicBSplinePrefilter3D((float*)bsplineCoeffs.ptr, (uint)bsplineCoeffs.pitch, (uint)Xdim, (uint)Ydim, (uint)Zdim);
@@ -142,45 +125,40 @@ void cuda_rotate_image_bspline(float *image, float *rotated_image, size_t Xdim, 
     	texRefVol.addressMode[1] = (cudaTextureAddressMode)wrap;
     	texRefVol.addressMode[2] = (cudaTextureAddressMode)wrap;
 
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_fin_gpu_filtS, NULL);
+    mytime->secs_gpu_filtS = timeval_diff(&mytime->t_fin_gpu_filtS, &mytime->t_ini_gpu_filtS);
+}
+
     	float *d_output;
     	gpuErrchk(cudaMalloc((void **)&d_output, Xdim * Ydim * Zdim * sizeof(float)));
 
-#ifdef TIME
-    t_fin5 = clock();
-    secs5 = (double)(t_fin5 - t_ini5) / CLOCKS_PER_SEC;
-    printf("Memory host to device and prefiltering: %.16g milisegundos\n", (secs5) * 1000.0);
-#endif
 
-#ifdef TIME
-	clock_t t_ini6, t_fin6;
-	double secs6;
-	t_ini6 = clock();
-#endif
+
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_ini_gpu_kernelS, NULL);
+}
 
     	//Interpolation (second step)
     	interpolate3D(Xdim, Ydim, Zdim, ang, d_output);
     	gpuErrchk(cudaDeviceSynchronize());
 
-#ifdef TIME
-    t_fin6 = clock();
-    secs6 = (double)(t_fin6 - t_ini6) / CLOCKS_PER_SEC;
-    printf("Kernel (interpolation): %.16g milisegundos\n", secs6 * 1000.0);
-#endif
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_fin_gpu_kernelS, NULL);
+    mytime->secs_gpu_kernelS = timeval_diff(&mytime->t_fin_gpu_kernelS, &mytime->t_ini_gpu_kernelS);
+}
 
-#ifdef TIME
-	clock_t t_ini7, t_fin7;
-	double secs7;
-	t_ini7 = clock();
-#endif
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_ini_gpu_mem_outS, NULL);
+}
 
 		gpuErrchk(cudaMemcpy(rotated_image, d_output, Xdim * Ydim * Zdim * sizeof(float), cudaMemcpyDeviceToHost));
 		gpuErrchk(cudaFree(d_output));
 
-#ifdef TIME
-    t_fin7 = clock();
-    secs7 = (double)(t_fin7 - t_ini7) / CLOCKS_PER_SEC;
-    printf("Memory device to host: %.16g milisegundos\n", secs7 * 1000.0);
-#endif
+if(mytime->calcTime==true){
+    gettimeofday(&mytime->t_fin_gpu_mem_outS, NULL);
+    mytime->secs_gpu_mem_outS = timeval_diff(&mytime->t_fin_gpu_mem_outS, &mytime->t_ini_gpu_mem_outS);
+}
 
     }
 
