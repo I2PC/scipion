1c6ee54ac4b4093550dc6e1f15bfd18d7b69896e
ajimoreno <ajimenez@gmail.com>
2017-04-20 14:05:49 +0200

    printing times for debugging

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_linear_nearest.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_linear_nearest.h
@@ -1,4 +1,8 @@
+#include <time.h>
+#include <sys/time.h>
+
 
 /** Native implementation when interpolation is either nearest neighbor or linear*/
-void cuda_rotate_image_linear_nearest(float *image, float *rotated_image, size_t Xdim, size_t Ydim, size_t Zdim, double *ang, int interp, int wrap, int first_call);
+void cuda_rotate_image_linear_nearest(float *image, float *rotated_image, size_t Xdim, size_t Ydim, size_t Zdim,
+		double *ang, int interp, int wrap, int first_call, struct ioTime* time);
 
