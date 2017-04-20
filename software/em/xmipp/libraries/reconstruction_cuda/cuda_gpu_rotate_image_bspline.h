78fe4f2dd8a76a5907e655e5464bda5e7234b2a7
ajimoreno <ajimenez@gmail.com>
2017-04-20 14:05:25 +0200

    printing times for debugging

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_bspline.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_rotate_image_bspline.h
@@ -1,4 +1,9 @@
 
+#include <time.h>
+#include <sys/time.h>
+
+
 /** Bspline interpolation as implemented in http://www.dannyruijters.nl/cubicinterpolation/ */
-void cuda_rotate_image_bspline(float *image, float *rotated_image, size_t Xdim, size_t Ydim, size_t Zdim, double *ang, int wrap, int first_call);
+void cuda_rotate_image_bspline(float *image, float *rotated_image, size_t Xdim, size_t Ydim, size_t Zdim,
+		double *ang, int wrap, int first_call, struct ioTime* time);
 
