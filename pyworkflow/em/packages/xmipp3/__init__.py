ba8018f717de7c823f8d6f96407e13da2fa5cf4d
rmarabini <roberto@cnb.csic.es>
2017-04-22 20:33:12 +0200

    added viewer for viewerXmippProtExtractUnit

--- pyworkflow/em/packages/xmipp3/__init__.py
+++ pyworkflow/em/packages/xmipp3/__init__.py
@@ -58,6 +58,7 @@ from viewer_validate_overfitting import XmippValidateOverfittingViewer
 from viewer_volume_strain import XmippVolumeStrainViewer
 from viewer_reconstruct_highres import XmippReconstructHighResViewer
 from viewer_solid_angles import SolidAnglesViewer
+from viewer_extract_unit_cell import viewerXmippProtExtractUnit
 
 from plotter import XmippPlotter
 
