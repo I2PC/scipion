c40a302ab42c03ce08fdb3a042fbccfc08683e78
rmarabini <roberto@cnb.csic.es>
2017-04-30 17:48:57 +0200

    add unused imports and variables, polish a bit the code

--- pyworkflow/em/packages/ccp4/viewer_refmac.py
+++ pyworkflow/em/packages/ccp4/viewer_refmac.py
@@ -26,45 +26,37 @@
 
 
 from protocol_refmac import CCP4ProtRunRefmac
-from pyworkflow.em import data, TextFileView
-from pyworkflow.em.plotter import EmPlotter
-from pyworkflow.em.viewer import ObjectView
-from pyworkflow.em.showj import MODE, MODE_MD, ORDER, VISIBLE
-from pyworkflow.protocol.params import FloatParam, IntParam, LabelParam
+from pyworkflow.protocol.params import LabelParam
 from pyworkflow.viewer import DESKTOP_TKINTER, WEB_DJANGO, ProtocolViewer, Viewer
-from pyworkflow.utils.path import cleanPath
-import numpy as np
-import os
+from pyworkflow.gui.text import _open_cmd
 
 
 class CCP4ProtRunRefmacViewer(ProtocolViewer):
-    """ This protocol computes the maximum resolution up to which two
-     CTF estimations would be ``equivalent'', defining ``equivalent'' as having
-      a wave aberration function shift smaller than 90 degrees
+    """ Viewer for CCP4 program refmac
     """
-    _label = 'viewer Refmac results'
+    _label = 'Refmac Viewer'
     _environments = [DESKTOP_TKINTER, WEB_DJANGO]
     _targets = [CCP4ProtRunRefmac]
-    _memory = False
-    resolutionThresholdOLD = -1
+    #ROB: do we need this memory for something?
+    #_memory = False
     # temporary metadata file with ctf that has some resolution greathan than X
-    tmpMetadataFile = 'viewersTmp.sqlite'
+    #tmpMetadataFile = 'viewersTmp.sqlite'
 
     def _defineParams(self, form):
         form.addSection(label='Visualization of Refmac results')
         # group = form.addGroup('Overall results')
         form.addParam('displayMask', LabelParam,
-                      label="Masked volume",
-                      help="Display FSC of masked map")
+                      label="PDB based Mask",
+                      help="Display Masked map")
         form.addParam('showFinalResults', LabelParam,
                       label="Final Results Table",
                       help="Table of Final Results from refine.log file")
         form.addParam('showLogFile', LabelParam,
                       label = "Show log file",
-                      help="refine.log file contains all results from Refmac")
+                      help="open refmac log file in a text editor")
         form.addParam('showLastIteration', LabelParam,
-                      label = "Last Iteration Results Table",
-                      help= "Table of Refmac results obtained from the last iteration")
+                      label = "Results Table (last iteration)",
+                      help= "Table stored in log file summarizing the last iteration")
         form.addParam('displayRFactorPlot', LabelParam,
                       label = "R-factor vs. iteration",
                       help= "Plot R-factor as a function of the iteration")
@@ -79,9 +71,12 @@ class CCP4ProtRunRefmacViewer(ProtocolViewer):
                       help="Plot Log likelihood as a function of the iteration")
         form.addParam('displayGeometryPlot', LabelParam,
                       label="Geometry vs. iteration",
-                      help="Plot Geometry as a function of the iteration:\n"  "Geometry includes rmsBOND (root mean square bond lengths),\n"
-                           "zBOND (zscore of the deviation of bond lengths),\n rmsANGL (root mean square bond angles),\n" 
-                           "zANGL (zscore of the deviation of bond angles),\n and rmsCHIRAL (root mean square of chiral index")
+                      help="""Plot Geometry as a function of the iteration:
+Geometry includes rmsBOND (root mean square bond lengths)
+zBOND (zscore of the deviation of bond lengths)
+rmsANGL (root mean square bond angles)
+zANGL (zscore of the deviation of bond angles)
+and rmsCHIRAL (root mean square of chiral index""")
 
     def _getVisualizeDict(self):
         return {
@@ -100,25 +95,14 @@ class CCP4ProtRunRefmacViewer(ProtocolViewer):
         pass
 
     def _visualizeFinalResults(self, e=None):
-        views = []
-        numberOfBins = self.visualizeHistogram.get()
-        numberOfBins = min(numberOfBins, self.protocol.outputCTF.getSize())
-        plotter = EmPlotter()
-        plotter.createSubPlot("Resolution Discrepancies histogram",
-                              "Resolution (A)", "# of Comparisons")
-        resolution = [ctf._discrepancy_resolution.get() for ctf in self.protocol.outputCTF]
-        # print "resolution", resolution
-        # print "numberOfBins",numberOfBins,self.protocol.outputCTF.getSize()
-        plotter.plotHist(resolution, nbins=numberOfBins)
-        # ROB: why do I need this show?
-        plotter.show()
-        return views.append(plotter)
+        pass
 
     def _visualizeLogFile(self, e=None):
         """ Check if the refine.log file is generated and if so, read the file."""
         refineLogFileName = self.protocol._getExtraPath(self.protocol.refineLogFileName)
-        view = TextFileView(refineLogFileName)
-        view.show()
+        _open_cmd(refineLogFileName, self.getTkRoot())
+        #view = TextFileView(refineLogFileName, self.getTkRoot())
+        #view.show()
 
     def _visualizeLastIteration(self, e=None):
         pass
