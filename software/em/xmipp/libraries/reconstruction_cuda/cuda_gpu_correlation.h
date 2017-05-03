63dc6889b2b791f524e9a086d1a7687b909c1a63
ajimoreno <ajimenez@gmail.com>
2017-05-03 10:05:46 +0200

    added new functions

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_correlation.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_correlation.h
@@ -4,5 +4,7 @@
 
 
 
-void cuda_fft(float *image, float *rotated_image, float *module, float *angle, size_t Xdim, size_t Ydim, size_t Zdim);
+void cuda_fft(float *image, float *recovered_image, float *module, float *angle, size_t Xdim, size_t Ydim, size_t Zdim, size_t batch);
+
+void cuda_cart2polar(float *image, float *polar_image, size_t Xdim, size_t Ydim, size_t Zdim, size_t batch);
 
