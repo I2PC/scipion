cd33d3f53b1d89a667193810f0bb9cc1619fed3c
rmarabini <roberto@cnb.csic.es>
2017-05-05 19:16:35 +0200

    remove the singleton design. It is fancy but to complex and it does not allow to see results from two different refmacs. Two other minor fixes

--- pyworkflow/em/packages/ccp4/viewer_refmac.py
+++ pyworkflow/em/packages/ccp4/viewer_refmac.py
@@ -49,43 +49,33 @@ def errorWindow(tkParent, msg):
         print("Error:", msg)
 
 
-class SingletonParseFile(object):
-    """class that parse de log file. It is defined as a singleton so
-       it may be called from all routines that need to parse data but the actual
-       parsing happends only once"""
-    _instance = None
-
+class ParseFile():
+    """class that parse a refmac log file"""
+    LASTITERATIONRESULTS="lastIteration"
+    FINALRESULTS="finalResults"
+    FOMPLOT="fomplot"
     def __init__(self, fileName, tkParent=None, lastIteration=0):
-        if not self.__initialized:
-            self.__initialized = True
-            object.__init__(self)
-            self.headerDict = {}#parsed header goes here
-            self.dataDict = {}#parsed data goes here
-            self.tkParent = tkParent
-            self.fileName = fileName
-            self._parsefile(lastIteration)
-
-    def __new__(cls, *args, **kwargs):
-        """Singleton magic happend here """
-        if not cls._instance:
-            cls._instance = super(SingletonParseFile, cls).__new__(cls, *args, **kwargs)
-            cls._instance.__initialized = False
-
-        return cls._instance
+        self.headerDict = {}#parsed headers goes here
+        self.dataDict = {}#parsed data goes here
+        self.msgDict = {}#error messages goes here
+        self.tkParent = tkParent
+        self.fileName = fileName
+        self._parsefile(lastIteration)
 
     def _parseFinalResults(self, filePointer):
         headerList = []
         dataList = []
         stop = False
+        msg=""
         while 1:
             line = filePointer.readline()
-            if line.strip() == '$TEXT:Result: $$ Final results $$':  # detect final results
+            if line.find('$TEXT:Result: $$ Final results $$') != -1:  # detect final results
                 break
             if not line:
                 stop = True
                 break
         if stop:
-            self.msg = 'Can not find "Final result" information in log file: %s'% self.fileName
+            msg = 'Can not find "Final result" information in log file: %s'% self.fileName
         else:
             # finalResultsDict={'header':}
             #parse header
@@ -102,29 +92,28 @@ class SingletonParseFile(object):
                 row.extend([words[0]+" "+ words[1], words[2], words[3]])
                 dataList.append(tuple(row))
             #TODO: remove debug lines
-        print("FinalResults", headerList)
-        print("FinalResults", dataList)
-        return headerList, dataList
+        return headerList, dataList, msg
 
     def retrievefinalResults(self):
-        return self.headerDict['finalResults'], self.dataDict['finalResults']
+        return self.headerDict[self.FINALRESULTS], \
+               self.dataDict[self.FINALRESULTS], \
+               self.msgDict[self.FINALRESULTS]
 
     def _parseLastIteration(self, filePointer, iteration):
-        print("iteration",iteration)
         headerList = ["variable","value"]
         dataList = []
         stop = False
-        print("line","$GRAPHS:Cycle   %d. M(Fom) v. resln :N:1,3,5,7,8,9,10:"%iteration)
+        msg=""
         while 1:
             line = filePointer.readline()
-            if line.strip() == '$GRAPHS:Cycle   %d. M(Fom) v. resln :N:1,3,5,7,8,9,10:'%iteration:  # detect final results
+            #TODO: double check space after Cycle
+            if line.find('$GRAPHS:Cycle    %d. M(Fom) v. resln :N:1,3,5,7,8,9,10:'%iteration)!=-1:  # detect final results
                 break
             if not line:
                 stop = True
                 break
-        print("stop1",stop)
         if stop:
-            self.msg = 'Can not find "Last Iteration" information in log file: %s' % self.fileName
+            msg = 'Can not find "Last Iteration" information in log file: %s' % self.fileName
         else:
             # find three lines with $$
             counter=4
@@ -135,33 +124,32 @@ class SingletonParseFile(object):
                 if not line:
                     stop = True
                     break
-            print("stop2", stop)
 
             for i in range(14):
                 row = []
                 line = filePointer.readline()
-                print "LINE", line
                 words = line.strip().split("=")
                 # the first column has 2 words
                 row.extend([words[0].strip(), words[1].strip()])
                 dataList.append(tuple(row))
-            # TODO: remove debug lines
-            print("LastIteration", headerList)
-            print("LastIteration", dataList)
-        return headerList, dataList
+        return headerList, dataList, msg
 
     def retrievelastIteration(self):
-        return self.headerDict['lastIteration'], self.dataDict['lastIteration']
+        return self.headerDict[self.LASTITERATIONRESULTS],\
+               self.dataDict[self.LASTITERATIONRESULTS],\
+               self.msgDict[self.LASTITERATIONRESULTS]
 
     def _parsefile(self, lastIteration=0):
         """ call the different functions that parse the data in the right order"""
         with open(self.fileName,"r") as filePointer:
-            headerList,dataList = self._parseLastIteration(filePointer, lastIteration)
-            self.headerDict['lastIteration'] = headerList
-            self.dataDict['lastIteration']   = dataList
-            headerList,dataList = self._parseFinalResults(filePointer)
-            self.headerDict['finalResults'] = headerList
-            self.dataDict['finalResults']   = dataList
+            headerList,dataList, msg = self._parseLastIteration(filePointer, lastIteration)
+            self.headerDict[self.LASTITERATIONRESULTS] = headerList
+            self.dataDict[self.LASTITERATIONRESULTS]   = dataList
+            self.msgDict[self.LASTITERATIONRESULTS]    =  msg
+            headerList,dataList, msg = self._parseLastIteration(filePointer, lastIteration)
+            self.headerDict[self.LASTITERATIONRESULTS] = headerList
+            self.dataDict[self.LASTITERATIONRESULTS]   = dataList
+            self.msgDict[self.LASTITERATIONRESULTS]    =  msg
 
 class CCP4ProtRunRefmacViewer(ProtocolViewer):
     """ Viewer for CCP4 program refmac
@@ -174,7 +162,11 @@ class CCP4ProtRunRefmacViewer(ProtocolViewer):
     # _memory = False
     # temporary metadata file with ctf that has some resolution greathan than X
     # tmpMetadataFile = 'viewersTmp.sqlite'
-
+    def __init__(self,  **kwargs):
+        ProtocolViewer.__init__(self,  **kwargs)
+        self.parseFile = ParseFile(self.protocol._getExtraPath(self.protocol.refineLogFileName),
+                                   self.getTkRoot(),
+                                   self.protocol.nRefCycle.get() + 1)
     def _defineParams(self, form):
         form.addSection(label='Visualization of Refmac results')
         # group = form.addGroup('Overall results')
@@ -252,11 +244,9 @@ and rmsCHIRAL (root mean square of chiral index""")
         #Selection of lines from 'refine.log' file that include Refmac final results.
         #These lines will be saved in outputLines list
 
-        singletonParseFile = SingletonParseFile(self.protocol._getExtraPath(self.protocol.refineLogFileName),
-                                                self.getTkRoot(), self.protocol.nRefCycle.get() + 1)
-        headerList, dataList = singletonParseFile.retrievefinalResults()
+        headerList, dataList, msg = self.parseFile.retrievefinalResults()
         if not dataList:
-            errorWindow(self.getTkRoot(),singletonParseFile.msg)
+            errorWindow(self.getTkRoot(), msg)
             return
 
         TableView(headerList=headerList,
@@ -271,50 +261,19 @@ and rmsCHIRAL (root mean square of chiral index""")
         _open_cmd(refineLogFileName, self.getTkRoot())
 
     def _visualizeLastIteration(self, e=None):
+        # Selection of lines from 'refine.log' file that include last iteration .
+        # characteristics
+        headerList, dataList, msg = self.parseFile.retrievelastIteration()
+        if not dataList:
+            errorWindow(self.getTkRoot(), msg)
+            return
 
-        # Selection of lines from 'refine.log' file that include Refmac final results.
-        # These lines will be saved in outputLines list
-        singletonParseFile = SingletonParseFile(self.protocol._getExtraPath(self.protocol.refineLogFileName),
-                                                self.protocol.nRefCycle.get() + 1)
-        dataList=singletonParseFile.dataDict['lastIteration']
-        TableView(headerList=singletonParseFile.headerDict['lastIteration'],
+        TableView(headerList=headerList,
                   dataList=dataList,
                   mesg=" ",
                   title= "Refmac: Last Iteration summary",
                   height=len(dataList), width=200,padding=40)
 
-        """
-        f = self.protocol._getExtraPath(self.protocol.refineLogFileName)
-        outputLines = []
-        with open(f) as input_data:
-            for line in input_data:
-                if line.strip() == '$TABLE: Cycle   '+self.protocol.nRefCycle.get()+1+'. FSC and  Fom(<cos(DelPhi)>-acentric, centric, overall v resln:':
-                    break
-            for line in input_data:
-                if line.strip() == 'Things for loggraph, R factor and others vs cycle':
-                    break
-                outputLines.append(line)
-        outputLines = outputLines[25:38]
-        # Creation of two lists (headerList and dataList) with the first line and the remaining lines of outputLines, respectively
-        headerList = []
-        dataList = []
-        for i in range(0, len(outputLines)):
-            if i == 0:
-                headerList.extend([' ', outputLines[i].strip().split()[0], outputLines[i].strip().split()[1]])
-            else:
-                dataList.extend([outputLines[i].strip().split()[0] + ' ' + outputLines[i].strip().split()[1],
-                                 outputLines[i].strip().split()[2],
-                                 outputLines[i].strip().split()[3]])
-        # Conversion of dataList in a list of 3-element tuples
-        it = iter(dataList)
-        dataList = zip(it, it, it)
-        # Arrangement of the final table
-        TableView(headerList=headerList,
-                  dataList=dataList,
-                  mesg="This list includes a summary of Refmac execution final results",
-                  title="Refmac: Final Results Summary",
-                  height=len(dataList))
-"""
     def _visualizeRFactorPlot(self, e=None):
         """
         xplotter = Plotter(windowTitle="SAXS Curves")
@@ -330,7 +289,18 @@ and rmsCHIRAL (root mean square of chiral index""")
         pass
 
     def _visualizeFOMPlot(self, e=None):
-        pass
+        """ Plot FOM
+        """
+        headerList, dataList, msg = self.parseFile.retrieveFomPlot()
+        if not dataList:
+            errorWindow(self.getTkRoot(), msg)
+            return
+        #make plotter here
+        TableView(headerList=headerList,
+                  dataList=dataList,
+                  mesg=" ",
+                  title= "Refmac: Last Iteration summary",
+                  height=len(dataList), width=200,padding=40)
 
     def _visualizeLLPlot(self, e=None):
         pass
