2b023159e7ddaa0b55104a57786fcbeb8ef2541e
ajimoreno <ajimenez@gmail.com>
2017-05-12 22:01:13 +0200

    First attempt to write a program to calculate correlation with gpu

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_utils.cpp
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_utils.cpp
@@ -1,5 +1,36 @@
 
 #include "cuda_utils.h"
+#include "cuda_check_errors.h"
+#include <cuda_runtime.h>
+
 
 struct ioTime *mytimes;
+
+void gpuMalloc(void** d_data, size_t Nbytes)
+{
+	gpuErrchk(cudaMalloc(d_data, Nbytes));
+}
+
+void gpuFree(void* d_data)
+{
+	gpuErrchk(cudaFree(d_data));
+}
+
+void gpuCopyFromCPUToGPU(void* data, void* d_data, size_t Nbytes)
+{
+	gpuErrchk(cudaMemcpy(d_data, data, Nbytes, cudaMemcpyHostToDevice));
+}
+
+void gpuCopyFromGPUToCPU(void* d_data, void* data, size_t Nbytes)
+{
+	gpuErrchk(cudaMemcpy(data, d_data, Nbytes, cudaMemcpyDeviceToHost));
+}
+
+int gridFromBlock(int tasks, int Nthreads)
+{
+    int numBlk = tasks/Nthreads;
+    if(tasks%Nthreads>0)
+    	numBlk++;
+    return numBlk;
+}
 
