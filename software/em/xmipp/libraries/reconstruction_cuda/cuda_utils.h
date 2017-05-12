dbac23a57daebae0e0f5380fc08bbe2bd6976312
ajimoreno <ajimenez@gmail.com>
2017-05-12 22:01:19 +0200

    First attempt to write a program to calculate correlation with gpu

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_utils.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_utils.h
@@ -2,6 +2,13 @@
 #ifndef CUDA_UTILS_H
 #define CUDA_UTILS_H
 
+#include <complex>
+#include <stdio.h>
+
+
+std::complex<double>* cuda_fft(double* &image, double* recovered_image, double *module, double *angle, size_t Xdim, size_t Ydim, size_t Zdim, size_t batch);
+//double* cuda_ifft(std::complex<double>* &data, size_t Xdim, size_t Ydim, size_t Zdim, size_t batch);
+
 struct ioTime {
 
 	//AJ times for CPU processing
@@ -26,6 +33,7 @@ struct ioTime {
 };
 
 extern struct ioTime *mytimes;
+struct dim3;
 
 inline double timeval_diff(struct timeval *a, struct timeval *b)
 {
@@ -33,5 +41,108 @@ inline double timeval_diff(struct timeval *a, struct timeval *b)
     (double)(a->tv_sec + (double)a->tv_usec/1000000) -
     (double)(b->tv_sec + (double)b->tv_usec/1000000);
 }
+
+void gpuMalloc(void** d_data, size_t Nbytes);
+void gpuFree(void* d_data);
+void gpuCopyFromCPUToGPU(void* data, void* d_data, size_t Nbytes);
+void gpuCopyFromGPUToCPU(void* d_data, void* data, size_t Nbytes);
+int gridFromBlock(int tasks, int Nthreads);
+
+class XmippDim3 {
+public:
+	int x;
+	int y;
+	int z;
+
+	XmippDim3(int _x, int _y, int _z)
+	{
+		x=_x;
+		y=_y;
+		z=_z;
+	}
+
+	XmippDim3()
+	{
+		x=y=z=0;
+	}
+};
+
+#define CONVERT2DIM3(d) (dim3((d).x,(d).y,(d).z))
+
+template<typename T>
+class GpuMultidimArrayAtGpu
+{
+public:
+	size_t Xdim, Ydim, Zdim, Ndim, yxdim, zyxdim, nzyxdim;
+    T* d_data;
+
+	GpuMultidimArrayAtGpu()
+    {
+		Xdim=Ydim=Zdim=Ndim=yxdim=zyxdim=nzyxdim=0;
+		d_data=NULL;
+    }
+
+	GpuMultidimArrayAtGpu(size_t _Xdim, size_t _Ydim=1, size_t _Zdim=1, size_t _Ndim=1)
+    {
+        resize(_Xdim, _Ydim, _Zdim, _Ndim);
+    }
+
+	void resize(size_t _Xdim, size_t _Ydim=1, size_t _Zdim=1, size_t _Ndim=1)
+    {
+		Xdim=_Xdim;
+		Ydim=_Ydim;
+		Zdim=_Zdim;
+		Ndim=_Ndim;
+        yxdim=(size_t)_Ydim*_Xdim;
+        zyxdim=yxdim*_Zdim;
+        nzyxdim=zyxdim*_Ndim;
+        gpuMalloc((void**) &d_data,nzyxdim*sizeof(T));
+
+    }
+
+	bool isEmpty()
+	{
+		return d_data==NULL;
+	}
+
+	~GpuMultidimArrayAtGpu()
+	{
+		if (d_data!=NULL)
+			gpuFree((void*) d_data);
+	}
+
+	void calculateGridSize(const XmippDim3 &blockSize, XmippDim3 &gridSize)
+	{
+		gridSize.x=gridFromBlock(Xdim,blockSize.x);
+		gridSize.y=gridFromBlock(Ydim,blockSize.y);
+		gridSize.z=gridFromBlock(Zdim,blockSize.z);
+	}
+
+	void calculateGridSizeVectorized(const XmippDim3 &blockSize, XmippDim3 &gridSize)
+	{
+		gridSize.x=gridFromBlock(nzyxdim,blockSize.x);
+		gridSize.y=1;
+		gridSize.z=1;
+	}
+
+	std::complex<double>* fft()
+	{
+		return cuda_fft(d_data, NULL, NULL, NULL, Xdim, Ydim, Zdim, Ndim);
+	}
+
+	//AJ only for debugging purposes
+	std::complex<double>* fft(double* output)
+	{
+		return cuda_fft(d_data, output, NULL, NULL, Xdim, Ydim, Zdim, Ndim);
+	}
+	//END AJ
+
+	/*double* ifft()
+	{
+		return cuda_ifft(d_data, Xdim, Ydim, Zdim, Ndim);
+	}*/
+
+};
+
 
 #endif
