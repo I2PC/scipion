cc4983a7636a2f0765896805eed4a3ad63c9a03e
ajimoreno <ajimenez@gmail.com>
2017-05-12 22:00:43 +0200

    First attempt to write a program to calculate correlation with gpu

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_correlation.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_correlation.h
@@ -1,10 +1,18 @@
 
 #include <time.h>
 #include <sys/time.h>
+#include <complex>
+#include "cuda_utils.h"
 
 
+void cuda_check_gpu_memory(float* data);
 
-void cuda_fft(float *image, float *recovered_image, float *module, float *angle, size_t Xdim, size_t Ydim, size_t Zdim, size_t batch);
+std::complex<double>* cuda_fft(double* &image, double* recovered_image, double *module, double *angle, size_t Xdim, size_t Ydim, size_t Zdim, size_t batch);
 
-void cuda_cart2polar(float *image, float *polar_image, size_t Xdim, size_t Ydim, size_t Zdim, size_t batch);
+void cuda_cart2polar(GpuMultidimArrayAtGpu<double> &image, GpuMultidimArrayAtGpu<double> &polar_image, GpuMultidimArrayAtGpu<double> &polar2_image, bool rotate);
+
+//void cuda_masking(float *image, size_t Xdim, size_t Ydim, size_t Zdim, size_t batch, int radius);
+
+double** cuda_calculate_correlation(std::complex<double>* d_projFFTPointer, std::complex<double>* d_projSquaredFFTPointer, std::complex<double>* d_expFFTPointer, std::complex<double>* d_expSquaredFFTPointer, std::complex<double>* d_maskFFTPointer, size_t Xdim, size_t Ydim, size_t Zdim, size_t numProj, size_t numExp, int counting);
+
 
