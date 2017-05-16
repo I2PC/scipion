9f7ce2135960dc1af14aeca0d8005d72108969c5
rmarabini <roberto@cnb.csic.es>
2017-05-16 17:32:53 +0200

    add memory monitor for gpu

--- pyworkflow/em/protocol/monitors/protocol_monitor_system.py
+++ pyworkflow/em/protocol/monitors/protocol_monitor_system.py
@@ -45,27 +45,72 @@ import ctypes
 
 class DeviceProp(ctypes.Structure):
     _fields_ = [
-         ("name", 256*ctypes.c_char), #  < ASCII string identifying device
-         ("totalGlobalMem", ctypes.c_size_t), #  < Global memory available on device in bytes
-         ("sharedMemPerBlock", ctypes.c_size_t), #  < Shared memory available per block in bytes
-         ("regsPerBlock", ctypes.c_int), #  < 32-bit registers available per block
-         ("warpSize", ctypes.c_int), #  < Warp size in threads
-         ("memPitch", ctypes.c_size_t), #  < Maximum pitch in bytes allowed by memory copies
-         ("maxThreadsPerBlock", ctypes.c_int), #  < Maximum number of threads per block
-         ("maxThreadsDim", 3*ctypes.c_int), #  < Maximum size of each dimension of a block
-         ("maxGridSize", 3*ctypes.c_int), #  < Maximum size of each dimension of a grid
-         ("clockRate", ctypes.c_int), #  < Clock frequency in kilohertz
-         ("totalConstMem", ctypes.c_size_t), #  < Constant memory available on device in bytes
-         ("major", ctypes.c_int), #  < Major compute capability
-         ("minor", ctypes.c_int), #  < Minor compute capability
-         ("textureAlignment", ctypes.c_size_t), #  < Alignment requirement for textures
-         ("deviceOverlap", ctypes.c_int), #  < Device can concurrently copy memory and execute a kernel
-         ("multiProcessorCount", ctypes.c_int), #  < Number of multiprocessors on device
-         ("kernelExecTimeoutEnabled", ctypes.c_int), #  < Specified whether there is a run time limit on kernels
-         ("integrated", ctypes.c_int), #  < Device is integrated as opposed to discrete
-         ("canMapHostMemory", ctypes.c_int), #  < Device can map host memory with cudaHostAlloc/cudaHostGetDevicePointer
-         ("computeMode", ctypes.c_int), #  < Compute mode (See ::cudaComputeMode)
-         ("__cudaReserved", 36*ctypes.c_int)]
+            ('name', ctypes.c_char * 256),
+            ('totalGlobalMem', ctypes.c_size_t),
+            ('sharedMemPerBlock', ctypes.c_size_t),
+            ('regsPerBlock', ctypes.c_int),
+            ('warpSize', ctypes.c_int),
+            ('memPitch', ctypes.c_size_t),
+            ('maxThreadsPerBlock', ctypes.c_int),
+            ('maxThreadsDim', ctypes.c_int * 3),
+            ('maxGridSize', ctypes.c_int * 3),
+            ('clockRate', ctypes.c_int),
+            ('totalConstMem', ctypes.c_size_t),
+            ('major', ctypes.c_int),
+            ('minor', ctypes.c_int),
+            ('textureAlignment', ctypes.c_size_t),
+            ('texturePitchAlignment', ctypes.c_size_t),
+            ('deviceOverlap', ctypes.c_int),
+            ('multiProcessorCount', ctypes.c_int),
+            ('kernelExecTimeoutEnabled', ctypes.c_int),
+            ('integrated', ctypes.c_int),
+            ('canMapHostMemory', ctypes.c_int),
+            ('computeMode', ctypes.c_int),
+            ('maxTexture1D', ctypes.c_int),
+            ('maxTexture1DMipmap', ctypes.c_int),
+            ('maxTexture1DLinear', ctypes.c_int),
+            ('maxTexture2D', ctypes.c_int * 2),
+            ('maxTexture2DMipmap', ctypes.c_int * 2),
+            ('maxTexture2DLinear', ctypes.c_int * 3),
+            ('maxTexture2DGather', ctypes.c_int * 2),
+            ('maxTexture3D', ctypes.c_int * 3),
+            ('maxTexture3DAlt', ctypes.c_int * 3),
+            ('maxTextureCubemap', ctypes.c_int),
+            ('maxTexture1DLayered', ctypes.c_int * 2),
+            ('maxTexture2DLayered', ctypes.c_int * 3),
+            ('maxTextureCubemapLayered', ctypes.c_int * 2),
+            ('maxSurface1D', ctypes.c_int),
+            ('maxSurface2D', ctypes.c_int * 2),
+            ('maxSurface3D', ctypes.c_int * 3),
+            ('maxSurface1DLayered', ctypes.c_int * 2),
+            ('maxSurface2DLayered', ctypes.c_int * 3),
+            ('maxSurfaceCubemap', ctypes.c_int),
+            ('maxSurfaceCubemapLayered', ctypes.c_int * 2),
+            ('surfaceAlignment', ctypes.c_size_t),
+            ('concurrentKernels', ctypes.c_int),
+            ('ECCEnabled', ctypes.c_int),
+            ('pciBusID', ctypes.c_int),
+            ('pciDeviceID', ctypes.c_int),
+            ('pciDomainID', ctypes.c_int),
+            ('tccDriver', ctypes.c_int),
+            ('asyncEngineCount', ctypes.c_int),
+            ('unifiedAddressing', ctypes.c_int),
+            ('memoryClockRate', ctypes.c_int),
+            ('memoryBusWidth', ctypes.c_int),
+            ('l2CacheSize', ctypes.c_int),
+            ('maxThreadsPerMultiProcessor', ctypes.c_int),
+            ('streamPrioritiesSupported', ctypes.c_int),
+            ('globalL1CacheSupported', ctypes.c_int),
+            ('localL1CacheSupported', ctypes.c_int),
+            ('sharedMemPerMultiprocessor', ctypes.c_size_t),
+            ('regsPerMultiprocessor', ctypes.c_int),
+            ('managedMemSupported', ctypes.c_int),
+            ('isMultiGpuBoard', ctypes.c_int),
+            ('multiGpuBoardGroupID', ctypes.c_int),
+            ('singleToDoublePrecisionPerfRatio', ctypes.c_int),
+            ('pageableMemoryAccess', ctypes.c_int),
+            ('concurrentManagedAccess', ctypes.c_int),
+            ]
 
 
     def __str2__(self):
