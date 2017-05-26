eaad481f79e93a99f8edbb010c22c5e5162135e0
mmmtnez <mmmtnez@cnb.csic.es>
2017-05-26 15:45:21 +0200

    added i2 symmetry

--- pyworkflow/tests/em/protocols/test_protocol_extract_unit_cell.py
+++ pyworkflow/tests/em/protocols/test_protocol_extract_unit_cell.py
@@ -22,119 +22,110 @@
 # ***************************************************************************/
 import os
 import numpy as np
+import math
+import collections
 from tempfile import mkstemp
 from pyworkflow.utils import runJob
 from pyworkflow.tests import BaseTest, setupTestProject, DataSet
 from pyworkflow.em.packages.xmipp3 import getEnviron
+from pyworkflow.em.packages.xmipp3.constants import XMIPP_SYM_NAME
 from pyworkflow.em.protocol import ProtImportVolumes
 from pyworkflow.em.packages.xmipp3.protocol_extract_unit_cell import XmippProtExtractUnit
-from pyworkflow.em.constants import SYM_I222r
+from pyworkflow.em.constants import SYM_I222r, SYM_I222
 from pyworkflow.em.convert import ImageHandler
 from pyworkflow.em.packages.xmipp3.pdb.protocol_pseudoatoms_base import NMA_MASK_THRE
 from pyworkflow.em.packages.xmipp3.pdb.protocol_pseudoatoms import XmippProtConvertToPseudoAtoms
+from icosahedron import Icosahedron, PHI
 
-def generate(fiveFold, mode='xmipp',suffix=".feat"):
+def generate(kernel, mode='xmipp',suffix="_i2"):
+
+    if mode=='xmipp':
+        suffix += ".feat"
+    else:
+        suffix += ".bild"
 
     (fd, filename) = mkstemp(suffix=suffix)
     f = os.fdopen(fd, "w")
-    #f.write(geometricPhantom)
-    #f.close()
+
 
     if mode=='xmipp':
         f.write("""# Phantom description file, (generated with phantom help)
 # General Volume Parameters:
 #      Xdim      Ydim      Zdim   Background_Density Scale
-       5 5 5 0 60
+       3 3 3 0 60
 # Feature Parameters:
 #Type  +/=  Density X_Center Y_Center Z_Center
 """)
     else:
         f.write(""".scale 60
-.comment five fold
+.comment system of coordinates
+.color 1 0 0       
+.arrow 0 0 0 2 0 0 0.025
+.color 0 1 0
+.arrow 0 0 0 0 2 0 0.025
+.color 0 0 1
+.arrow 0 0 0 0 0 2 0.025
+.comment vertices
 .color red
 """)
-    counter=0
-    for vertex in fiveFold:
+    icosahedron = Icosahedron(point=kernel)
+
+    #print 5fold points
+    for key, value in icosahedron.getVertices().iteritems():
         if mode=='xmipp':
-            f.write("sph  + 1. %.3f %.3f %.3f .15\n"%(vertex[0], vertex[1], vertex[2]) )
+            f.write("sph  + 1. %.3f %.3f %.3f .15\n"%(key[0], key[1], key[2]) )
         else:
-            f.write(".cmov   %.3f %.3f %.3f\n%d\n"% (vertex[0], vertex[1], vertex[2], counter))
-            f.write('.sphere %.3f %.3f %.3f .15\n'% (vertex[0], vertex[1], vertex[2]))
-        counter += 1
+            f.write('.sphere %.3f %.3f %.3f .15\n'% (key[0], key[1], key[2]))
 
-    ### vertex that define a triangle
-    threeFold  = []
-    #zero
-    threeFold.append(np.array([0,2,5]))
-    threeFold.append(np.array([0,4,2]))
-    threeFold.append(np.array([0,5,10]))
-    threeFold.append(np.array([0,8,4]))
-    threeFold.append(np.array([0,10,8]))
-    #one
-    threeFold.append(np.array([1,3,6]))
-    threeFold.append(np.array([1,6,8]))
-    threeFold.append(np.array([1,7,3]))
-    threeFold.append(np.array([1,8,10]))
-    threeFold.append(np.array([1,10,7]))
-    #two
-    threeFold.append(np.array([2,4,9]))
-    threeFold.append(np.array([2,9,11]))
-    threeFold.append(np.array([2,11,5]))
-    #three
-    threeFold.append(np.array([3,6,9]))
-    threeFold.append(np.array([3,9,11]))
-    threeFold.append(np.array([3,11,7]))
-    #four
-    threeFold.append(np.array([4,6,9]))
-    threeFold.append(np.array([4,8,6]))
-    #five
-    threeFold.append(np.array([5,7,11]))
-    threeFold.append(np.array([5,10,7]))
-
-    if mode == "xmipp":
+    #print 3fold points
+    if mode == 'xmipp':
         pass
     else:
-        f.write(""".comment three fold
+        f.write(""".comment 3fold
 .color yellow
 """)
-    for cont in threeFold:
-        temp = (fiveFold[cont[0]]+fiveFold[cont[1]]+fiveFold[cont[2]])/3.
-        if mode == "xmipp":
-            f.write("sph  + 1. %.3f %.3f %.3f .15\n"%(temp[0], temp[1], temp[2]))
+
+    for key, value in icosahedron.getFaces().iteritems():
+        x = (key[0][0] + key[1][0] + key[2][0])/3.
+        y = (key[0][1] + key[1][1] + key[2][1])/3.
+        z = (key[0][2] + key[1][2] + key[2][2])/3.
+
+        if mode=='xmipp':
+            f.write("sph  + 1. %.3f %.3f %.3f .10\n"%(x,y,z ))
         else:
-            f.write(".sphere   %.3f %.3f %.3f .15\n"%(temp[0], temp[1], temp[2]))
-    ###
-    contFiveFold = []
-    for fold in threeFold:
-        contFiveFold.append(np.array([fold[0],fold[1]]))
-        contFiveFold.append(np.array([fold[1],fold[2]]))
-        contFiveFold.append(np.array([fold[2],fold[0]]))
-    if mode == "xmipp":
+            f.write('.sphere %.3f %.3f %.3f .10\n'% (x,y,z))
+
+    # print 2fold points
+    if mode == 'xmipp':
         pass
     else:
-        f.write(""".comment two fold
+        f.write(""".comment 2fold
 .color green
 """)
-    for cont in contFiveFold:
-        temp = (fiveFold[cont[0]]+fiveFold[cont[1]])/2.
-        if mode == "xmipp":
-            f.write("sph  + 1. %.3f %.3f %.3f .15\n"%(temp[0], temp[1], temp[2]))
-        else:
-            f.write(".sphere %.3f %.3f %.3f .15\n"%(temp[0], temp[1], temp[2]))
+    _2FoldList = []
+    def newPoint(newPoint):
+        for point in _2FoldList:
+            distance =  abs(point[0]-newPoint[0]) +\
+                        abs(point[1]-newPoint[1]) +\
+                        abs(point[2]-newPoint[2])
+            if distance < 0.01:
+                return False
+        return True
 
+    for key, aux in icosahedron.getPairs().iteritems():
+        for value in aux:
+            point =  ( (key[0] + value[0]) / 2.,
+                       (key[1] + value[1]) / 2.,
+                       (key[2] + value[2]) / 2.)
+            if not newPoint(point):
+                continue
+            else:
+                _2FoldList.append(point)
 
-    if mode == "bild":
-        f.write(""".comment edges
-.color blue
-""")
-        for cont in contFiveFold:
-            temp = (fiveFold[cont[0]]+fiveFold[cont[1]])/2.
-            f.write(".cylinder %.3f %.3f %.3f %.3f %.3f %.3f .03\n"%(fiveFold[cont[0]][0],
-         fiveFold[cont[0]][1],
-         fiveFold[cont[0]][2],
-         fiveFold[cont[1]][0],
-         fiveFold[cont[1]][1],
-         fiveFold[cont[1]][2]))
+            if mode == 'xmipp':
+                f.write("sph  + 1. %.3f %.3f %.3f .09\n" % point)
+            else:
+                f.write('.sphere %.3f %.3f %.3f .09\n' % point)
 
     f.close()
     return filename
@@ -143,33 +134,31 @@ class TestProtModelBuilding(BaseTest):
     @classmethod
     def setUpClass(cls):
         setupTestProject(cls)
-        #i2
-        fiveFold = []
-        fiveFold.append(np.array([+1.000,    0,    1.618]))
-        fiveFold.append(np.array([+1.000,    0,   -1.618]))
-        fiveFold.append(np.array([-1.000,    0,    1.618]))
-        fiveFold.append(np.array([-1.000,    0,   -1.618]))
-        #
-        fiveFold.append(np.array([ 0.,      1.618,  +1.000]))
-        fiveFold.append(np.array([ 0.,      -1.618, +1.000]))
-        fiveFold.append(np.array([ 0.,      1.618,  -1.000]))
-        fiveFold.append(np.array([ 0.,      -1.618, -1.000]))
-        #
-        fiveFold.append(np.array([ 1.618,  +1.000, 0.]))
-        fiveFold.append(np.array([-1.618,  +1.000, 0.]))
-        fiveFold.append(np.array([ 1.618,  -1.000, 0.]))
-        fiveFold.append(np.array([-1.618,  -1.000, 0.]))
+        Kernel = {}
+        cls.filename = {}
+        cls.box = {}
 
-        cls.filename = generate(fiveFold, 'xmipp',"_i2.feat")
+        Kernel[SYM_I222] = np.array([0, 1, PHI], dtype=np.float64)
+        cls.filename[SYM_I222] = generate(Kernel[SYM_I222] , 'xmipp',XMIPP_SYM_NAME[SYM_I222])
+        cls.box[SYM_I222] = (71,84,67)
+
+        Kernel[SYM_I222r] = np.array([-1, 0, PHI], dtype=np.float64)
+        cls.filename[SYM_I222r] = generate(Kernel[SYM_I222r] , 'xmipp',XMIPP_SYM_NAME[SYM_I222r])
+        cls.box[SYM_I222r] = (69,68,53)
 
     def test_extractunitCell(self):
+        self.extractunitCell(SYM_I222)#no crowther 222
+        self.extractunitCell(SYM_I222r)#crowther 222
+
+    def extractunitCell(self, sym):
+
         """ extract unit cell from icosahedral pahntom
             using xmipp_i2 symmetry
         """
         # create phantom (3D map)
         _, outputFile = mkstemp(suffix=".vol")
         command = "xmipp_phantom_create "
-        args    = " -i %s"%self.filename
+        args    = " -i %s"% self.filename[sym]
         args += " -o %s"%outputFile
         runJob(None, command, args,env=getEnviron())
 
@@ -180,14 +169,14 @@ class TestProtModelBuilding(BaseTest):
                 'copyFiles': True,
                 }
         prot = self.newProtocol(ProtImportVolumes, **args)
-        prot.setObjLabel('import volume')
+        prot.setObjLabel('import volume(%s)'% XMIPP_SYM_NAME[sym])
         self.launchProtocol(prot)
 
         # execute protocol extract unitCell
         args = {'inputVolumes': prot.outputVolume,
-                'symmetryGroup': SYM_I222r,
-                'innerRadius': 80,
-                'outerRadius': 124,
+                'symmetryGroup': sym,
+                'innerRadius': 37,
+                'outerRadius': 79,
                 'expandFactor': .2,
                 'offset': 0.
                 }
@@ -198,14 +187,14 @@ class TestProtModelBuilding(BaseTest):
         #check results
         ih = ImageHandler()
         xdim, ydim, zdim, ndim = ih.getDimensions(prot.outputVolume.getFileName())
-        self.assertEqual(xdim,111)
-        self.assertEqual(ydim,107)
-        self.assertEqual(zdim,69)
+        self.assertEqual(xdim,self.box[sym][0])
+        self.assertEqual(ydim,self.box[sym][1])
+        self.assertEqual(zdim,self.box[sym][2])
 
         #create pdb file
         args = {'inputStructure':prot.outputVolume,
                 'maskMode':NMA_MASK_THRE,
-                'maskThreshold':1.,
+                'maskThreshold':0.5,
                 'pseudoAtomRadius':1.5
                 }
         prot = self.newProtocol(XmippProtConvertToPseudoAtoms, **args)
@@ -215,7 +204,7 @@ class TestProtModelBuilding(BaseTest):
         filenamePdb = prot._getPath('pseudoatoms.pdb')
         self.assertTrue(os.path.isfile(filenamePdb))
         #delete temporary files
-        os.remove(self.filename)
+        os.remove(self.filename[sym])
         os.remove(outputFile)
 
 
