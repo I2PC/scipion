0ad9520f4db9b34a275d1586e6b267b21cc843de
pconesa <p.conesa.mingo@gmail.com>
2017-05-31 07:15:55 +0200

    files taken from rm_cuda to clean unwanted commits

--- pyworkflow/em/protocol/monitors/protocol_monitor_system.py
+++ pyworkflow/em/protocol/monitors/protocol_monitor_system.py
@@ -36,11 +36,28 @@ from pyworkflow import VERSION_1_1
 from pyworkflow.gui.plotter import plt
 from pyworkflow.protocol.constants import STATUS_RUNNING, STATUS_FINISHED
 from pyworkflow.protocol import getProtocolFromDb
-import sys
 from pyworkflow.em.plotter import EmPlotter
+from tkMessageBox import showerror
+
+from pynvml import nvmlInit, nvmlDeviceGetCount, nvmlDeviceGetHandleByIndex,\
+    nvmlDeviceGetName, nvmlDeviceGetMemoryInfo, nvmlDeviceGetUtilizationRates,\
+    NVMLError, nvmlDeviceGetTemperature, NVML_TEMPERATURE_GPU,\
+    nvmlDeviceGetComputeRunningProcesses
+
 
 SYSTEM_LOG_SQLITE = 'system_log.sqlite'
 
+
+def errorWindow(tkParent, msg):
+    try:
+        showerror("Error",  # bar title
+                  msg,  # message
+                  parent=tkParent)
+    except:
+        print("Error:", msg)
+
+def initGPU():
+    nvmlInit()
 
 class ProtMonitorSystem(ProtMonitor):
     """ check CPU, mem and IO usage.
@@ -52,6 +69,7 @@ class ProtMonitorSystem(ProtMonitor):
         ProtMonitor.__init__(self, **kwargs)
         self.dataBase = 'log.sqlite'
         self.tableName = 'log'
+
 
     #--------------------------- DEFINE param functions ----------------------
     def _defineParams(self, form):    
@@ -74,6 +92,15 @@ class ProtMonitorSystem(ProtMonitor):
               help="Log during this interval")
 
         ProtMonitor._sendMailParams(self, form)
+        group = form.addGroup('GPU')
+        group.addParam('doGpu', params.BooleanParam, default=False,
+                       label="Check GPU",
+                       help="Set to true if you want to monitor the GPU")
+        group.addParam('gpusToUse', params.StringParam, default='0',
+                          label='Which GPUs to use:', condition='doGpu',
+                          help='Providing a list of which GPUs '
+                               '(0,1,2,3, etc). Default is monitor GPU 0 only')
+
 
     #--------------------------- STEPS functions ------------------------------
 
@@ -95,7 +122,10 @@ class ProtMonitorSystem(ProtMonitor):
                                    stdout=True,
                                    cpuAlert=self.cpuAlert.get(),
                                    memAlert=self.memAlert.get(),
-                                   swapAlert=self.swapAlert.get())
+                                   swapAlert=self.swapAlert.get(),
+                                   doGpu=self.doGpu.get(),
+                                   gpusToUse = self.gpusToUse.get(),
+                                    )
         return sysMonitor
 
     #--------------------------- INFO functions --------------------------------
@@ -104,16 +134,32 @@ class ProtMonitorSystem(ProtMonitor):
         return []  # no errors
 
     def _summary(self):
-        return ['Stores CPU, memory and swap ussage in percentage']
+        summary = []
+        summary.append("GPU running Processes:")
+        initGPU()
+        try:
+            gpusToUse = [int(n) for n in (self.gpusToUse.get()).split()]
+            for i in gpusToUse:
+                handle = nvmlDeviceGetHandleByIndex(i)
+                cps = nvmlDeviceGetComputeRunningProcesses(handle)
+                for ps in cps:
+                    #p_tags['pid'] = ps.pid
+                    msg = " %d) "%i + psutil.Process(ps.pid).name()
+                    msg += " (mem =%.2f MB)"%(float(ps.usedGpuMemory)/1048576.)
+                    summary.append(msg)
+        except NVMLError as err:
+                summary.append(str(err))
+
+        return summary
 
     def _methods(self):
         return []
 
 
 class MonitorSystem(Monitor):
-    """ This will will be monitoring a CTF estimation protocol.
+    """ This will will be monitoring a System  protocol.
     It will internally handle a database to store produced
-    CTF values.
+    system values.
     """
     def __init__(self, protocols, **kwargs):
         Monitor.__init__(self, **kwargs)
@@ -123,7 +169,21 @@ class MonitorSystem(Monitor):
         self.swapAlert = kwargs['swapAlert']
         self._dataBase = kwargs.get('dbName', SYSTEM_LOG_SQLITE)
         self._tableName = kwargs.get('tableName', 'log')
-
+        self.doGpu = kwargs['doGpu']
+        self.labelList=["cpu","mem","swap"]
+        if self.doGpu:
+            self.gpuLabelList=[]
+            #get Gpus to monitor
+            self.gpusToUse = [int(n) for n in (kwargs['gpusToUse']).split()]
+            for i in self.gpusToUse:
+                self.gpuLabelList.append("gpuMem_%d"%i)
+                self.gpuLabelList.append("gpuUse_%d"%i)
+                self.gpuLabelList.append("gpuTem_%d"%i)
+            #init GPUs
+            nvmlInit()
+            self.labelList += self.gpuLabelList
+        else:
+            self.gpusToUse = None
         self.conn = lite.connect(os.path.join(self.workingDir, self._dataBase),
                                  isolation_level=None)
         self.cur = self.conn.cursor()
@@ -146,24 +206,52 @@ class MonitorSystem(Monitor):
         psutil.virtual_memory()
 
     def step(self):
-        cpu = psutil.cpu_percent(interval=0)
-        mem = psutil.virtual_memory()
-        swap = psutil.swap_memory()
+        valuesDict={}
+        valuesDict['table'] = self._tableName
+        cpu  = valuesDict['cpu'] = psutil.cpu_percent(interval=0)
+        mem  = valuesDict['mem'] = psutil.virtual_memory().percent
+        swap = valuesDict['swap'] = psutil.swap_memory().percent
+        #some code examples: https://github.com/ngi644/datadog_nvml/blob/master/nvml.py
+        if self.doGpu:
+            for i in self.gpusToUse:
+                try:
+                    handle = nvmlDeviceGetHandleByIndex(i)
+                    memInfo = nvmlDeviceGetMemoryInfo(handle)
+                    valuesDict["gpuMem_%d"%i] = float(memInfo.used)*100./float(memInfo.total)
+                    util = nvmlDeviceGetUtilizationRates(handle)
+                    valuesDict["gpuUse_%d"%i] = util.gpu
+                    temp = nvmlDeviceGetTemperature(handle,NVML_TEMPERATURE_GPU)
+                    valuesDict["gpuTem_%d"%i] = temp
+                except NVMLError as err:
+                    handle = nvmlDeviceGetHandleByIndex(i)
+                    msg = "Device %d -> %s not suported\n Remove device %d from FORM"%\
+                          (i,nvmlDeviceGetName(handle),i)
+                    errorWindow(None, msg)
 
         if self.cpuAlert < 100 and cpu > self.cpuAlert:
-            self.warning("CPU allocation =%f." % cpu.percent)
+            self.warning("CPU allocation =%f." % cpu)
             self.cpuAlert = cpu
 
         if self.memAlert < 100 and mem.percent > self.memAlert:
-            self.warning("Memory allocation =%f." % mem.percent)
-            self.memAlert = mem.percent
+            self.warning("Memory allocation =%f." % mem)
+            self.memAlert = mem
 
         if self.swapAlert < 100 and swap.percent > self.swapAlert:
-            self.warning("SWAP allocation =%f." % swap.percent)
-            self.swapAlert = swap.percent
+            self.warning("SWAP allocation =%f." % swap)
+            self.swapAlert = swap
 
-        sql = """INSERT INTO %s(mem,cpu,swap) VALUES(%f,%f,%f);""" % (
-            self._tableName, mem.percent, cpu, swap.percent)
+        sqlCommand = "INSERT INTO %(table)s ("
+        for label in self.labelList:
+            sqlCommand += "%s, "%label
+        #remove last comma
+        sqlCommand = sqlCommand[:-2]
+        sqlCommand += ") VALUES("
+        for label in self.labelList:
+            sqlCommand += "%"+"(%s)f, "%label
+        #remove last comma
+        sqlCommand = sqlCommand[:-2]
+        sqlCommand += ");"
+        sql = sqlCommand%valuesDict
 
         try:
             self.cur.execute(sql)
@@ -175,13 +263,20 @@ class MonitorSystem(Monitor):
                    for prot in self.protocols)
 
     def _createTable(self):
-        self.cur.execute("""CREATE TABLE IF NOT EXISTS  %s(
+        sqlCommand = """CREATE TABLE IF NOT EXISTS  %s(
                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                                 timestamp DATE DEFAULT (datetime('now','localtime')),
-                                cpu FLOAT,
-                                mem FLOAT,
-                                swap FLOAT)
-                                """ % self._tableName)
+                                """ % self._tableName
+        for label in self.labelList:
+            sqlCommand += "%s FLOAT,\n"%label
+        #remove last comma and new line
+        sqlCommand = sqlCommand[:-3]
+        sqlCommand +=")"
+        print("sqlCommand",sqlCommand)
+        self.cur.execute(sqlCommand)
+
+    def getLabels(self):
+        return self.labelList
 
     def getData(self):
         cur = self.cur
@@ -205,11 +300,10 @@ class MonitorSystem(Monitor):
 
         data = {'initTime': initTime,
                 'initTimeTitle': initTimeTitle,
-                'idValues': idValues,
-                'cpu': get('cpu'),
-                'mem': get('mem'),
-                'swap': get('swap'),
-                }
+                'idValues': idValues}
+        for label in self.labelList:
+            data[label] = get(label)
+
         #conn.close()
         return data
 
@@ -253,7 +347,8 @@ class SystemMonitorPlotter(EmPlotter):
 
     def _getTitle(self):
         return ("Use scrool wheel to change view window (win=%d)\n "
-                "S stops, C continues plotting" % self.win)
+                "S stops, C continues plotting. Toggle ON/OFF GPU_X by pressing X\n"
+                "c toggle ON/OFF cpu information" % self.win)
 
     def onscroll(self, event):
 
@@ -270,7 +365,39 @@ class SystemMonitorPlotter(EmPlotter):
         self.animate()
         EmPlotter.show(self)
 
+
     def press(self,event):
+        def numericKey(key):
+            self.colorChanged=True
+            number=int(key)
+            index = 3+number*3
+            if (index + 3) > self.lenPlots:
+                return
+            if self.color['gpuMem_%d'%number] != 'w':
+                self.oldColor['gpuMem_%d'%number] = self.color['gpuMem_%d'%number]
+                self.oldColor['gpuUse_%d'%number] = self.color['gpuUse_%d'%number]
+                self.oldColor['gpuTem_%d'%number] = self.color['gpuTem_%d'%number]
+                self.color['gpuMem_%d'%number]="w"
+                self.color['gpuUse_%d'%number]="w"
+                self.color['gpuTem_%d'%number]="w"
+            else:
+                self.color['gpuMem_%d'%number]=self.oldColor['gpuMem_%d'%number]
+                self.color['gpuUse_%d'%number]=self.oldColor['gpuUse_%d'%number]
+                self.color['gpuTem_%d'%number]=self.oldColor['gpuTem_%d'%number]
+        def cpuKey(key):
+            self.colorChanged=True
+            if self.color['cpu'] != 'w':
+                self.oldColor['cpu'] = self.color['cpu']
+                self.oldColor['mem'] = self.color['mem']
+                self.oldColor['swap'] = self.color['swap']
+                self.color['cpu']="w"
+                self.color['mem']="w"
+                self.color['swap']="w"
+            else:
+                self.color['cpu']=self.oldColor['cpu']
+                self.color['swap']=self.oldColor['swap']
+                self.color['mem']=self.oldColor['mem']
+
         sys.stdout.flush()
         if event.key == 'S':
             self.stop = True
@@ -278,6 +405,12 @@ class SystemMonitorPlotter(EmPlotter):
         elif event.key == 'C':
             self.ax.set_title(self._getTitle())
             self.stop = False
+            self.animate()
+        elif event.key.isdigit():
+            numericKey(event.key)
+            self.animate()
+        elif event.key == 'c':
+            cpuKey(event.key)
             self.animate()
         EmPlotter.show(self)
 
@@ -287,7 +420,6 @@ class SystemMonitorPlotter(EmPlotter):
         return fig not in active_fig_managers
 
     def animate(self,i=0): #do NOT remove i
-                                         #FuncAnimation add it as argument
 
         if self.stop:
             return
@@ -302,7 +434,9 @@ class SystemMonitorPlotter(EmPlotter):
             xdata = self.x[imin:lenght]
             ydata = self.y[imin:lenght]
             v.set_data(xdata,ydata)
-
+            if self.colorChanged:
+                v.set_color(self.color[k])
+        self.colorChanged = False
         self.ax.relim()
         self.ax.autoscale()
         self.ax.grid(True)
@@ -320,5 +454,16 @@ class SystemMonitorPlotter(EmPlotter):
         EmPlotter.show(self)
 
     def show(self):
-        self.paint(['mem','cpu', 'swap'])
+        colortypes=["k","b","r","g","y","c","m"]
+        lenColortypes=len(colortypes)
+        self.colorChanged = True
+        self.color={}
+        self.oldColor={}
+        counter=0
+        for key in self.monitor.getLabels():
+            self.color[key]    = colortypes[counter%lenColortypes]
+            self.oldColor[key] = colortypes[counter%lenColortypes]
+            counter += 1
+        self.lenPlots=len(self.color)
+        self.paint(self.monitor.getLabels())
 
