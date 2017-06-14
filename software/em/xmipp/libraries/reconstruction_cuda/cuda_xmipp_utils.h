a8baa2b302d64e69816eed00e170eaa819450b9e
ajimoreno <ajimenez@gmail.com>
2017-06-14 18:59:16 +0200

    several changes to calculate correlation using repeating steps

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_xmipp_utils.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_xmipp_utils.h
@@ -6,6 +6,10 @@
 
 void gpuMalloc(void** d_data, size_t Nbytes);
 void gpuFree(void* d_data);
+void gpuCopyFromGPUToGPU(void* d_dataFrom, void* d_dataTo, size_t Nbytes);
+void initializeIdentity(float* d_data, size_t Ndim);
+void setTranslationMatrix(float* d_data, float posX, float posY, int n);
+void setRotationMatrix(float* d_data, float ang, int n);
 void gpuCopyFromCPUToGPU(void* data, void* d_data, size_t Nbytes);
 void gpuCopyFromGPUToCPU(void* d_data, void* data, size_t Nbytes);
 int gridFromBlock(int tasks, int Nthreads);
@@ -63,6 +67,92 @@ public:
 };
 
 #define CONVERT2DIM3(d) (dim3((d).x,(d).y,(d).z))
+
+template<typename T>
+class TransformMatrix
+{
+public:
+	size_t Xdim, Ydim, Zdim, Ndim, yxdim, zyxdim, nzyxdim;
+    T* d_data;
+
+    TransformMatrix()
+    {
+		Xdim=Ydim=Zdim=Ndim=yxdim=zyxdim=nzyxdim=0;
+		d_data=NULL;
+    }
+
+    TransformMatrix(size_t _Ndim, size_t _Xdim=3, size_t _Ydim=3, size_t _Zdim=1)
+    {
+		Xdim=Ydim=Zdim=Ndim=yxdim=zyxdim=nzyxdim=0;
+		d_data=NULL;
+		resize(_Ndim, _Xdim, _Ydim, _Zdim);
+    }
+
+	template<typename T1>
+	void resize(const TransformMatrix<T1>& array)
+	{
+
+		resize(array.Ndim, array.Xdim, array.Ydim, array.Zdim);
+	}
+
+	void resize(size_t _Ndim, size_t _Xdim=3, size_t _Ydim=3, size_t _Zdim=1)
+    {
+		if (_Xdim*_Ydim*_Zdim*_Ndim==nzyxdim)
+			return;
+
+		clear();
+
+		Xdim=_Xdim;
+		Ydim=_Ydim;
+		Zdim=_Zdim;
+		Ndim=_Ndim;
+        yxdim=(size_t)_Ydim*_Xdim;
+        zyxdim=yxdim*_Zdim;
+        nzyxdim=zyxdim*_Ndim;
+        gpuMalloc((void**) &d_data,nzyxdim*sizeof(T));
+        initializeIdentity(d_data, Ndim);
+    }
+
+	bool isEmpty()
+	{
+		return d_data==NULL;
+	}
+
+	void clear()
+	{
+		if (d_data!=NULL)
+			gpuFree((void*) d_data);
+		Xdim=Ydim=Zdim=Ndim=yxdim=zyxdim=nzyxdim=0;
+		d_data=NULL;
+	}
+
+	~TransformMatrix()
+	{
+		clear();
+	}
+
+	void setTranslation(const float* posX, const float* posY)
+	{
+		for(int i=0; i<Ndim; i++)
+			setTranslationMatrix(d_data, posX[i], posY[i], i);
+	}
+
+	void setRotation(const float* ang)
+	{
+		for(int i=0; i<Ndim; i++)
+			setRotationMatrix(d_data, ang[i], i);
+	}
+
+	void copyMatrix(TransformMatrix<float> &lastMatrix)
+	{
+		if (lastMatrix.isEmpty())
+			lastMatrix.resize(Ndim);
+
+		gpuCopyFromGPUToGPU(d_data, lastMatrix.d_data, nzyxdim*sizeof(float));
+	}
+
+};
+
 
 template<typename T>
 class GpuMultidimArrayAtGpu
@@ -127,6 +217,19 @@ public:
 		clear();
 	}
 
+	void copyToGpu(T* data)
+	{
+		gpuCopyFromCPUToGPU((void *)data, (void *)d_data, nzyxdim*sizeof(T));
+	}
+
+	void copyGpuToGpu(GpuMultidimArrayAtGpu<T> &gpuArray)
+	{
+		if (gpuArray.isEmpty())
+			gpuArray.resize(Xdim,Ydim,Zdim,Ndim);
+
+		gpuCopyFromGPUToGPU(d_data, gpuArray.d_data, nzyxdim*sizeof(T));
+	}
+
 	void calculateGridSize(const XmippDim3 &blockSize, XmippDim3 &gridSize) const
 	{
 		gridSize.x=gridFromBlock(Xdim,blockSize.x);
@@ -147,6 +250,11 @@ public:
 	// RealSpace must already be resized
 	template <typename T1>
 	void ifft(GpuMultidimArrayAtGpu<T1> &realSpace);
+
+	void calculateMax(double *max_values, float *posX, float *posY);
+
 };
+
+
 
 #endif
