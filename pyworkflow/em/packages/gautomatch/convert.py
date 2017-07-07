feaa4062d6b527cc50bce04598ea0a321c0ffee6
josuegbl <josuegbl@gmail.com>
2017-07-07 09:14:08 +0200

    Revert "fixed bug when wizard is launched"
    This reverts commit 84ab1227def0c2fdad3c30fc258dda7ec8366c26.

--- pyworkflow/em/packages/gautomatch/convert.py
+++ pyworkflow/em/packages/gautomatch/convert.py
@@ -205,7 +205,7 @@ def runGautomatch(micName, refStack, workDir, args, env=None):
         args = ' %s --T %s %s' % (outMic, refStack, args)
     else:
         args = ' %s %s' % (outMic, args)
-    environ = env if env is not None else getEnviron()
-    pwutils.runJob(None, getProgram(), args, env=environ)
+
+    pwutils.runJob(None, getProgram(), args, env=env)
     # After picking we can remove the temporary file.
     pwutils.cleanPath(outMic)
