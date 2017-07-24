1b6dd0520a2d8206cadb3bc61948f328bbee6638
ajimoreno <ajimenez@gmail.com>
2017-07-24 10:26:01 +0200

    several changes to save time

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_correlation.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_correlation.h
@@ -6,7 +6,6 @@
 
 void cuda_cart2polar(GpuMultidimArrayAtGpu<double> &image, GpuMultidimArrayAtGpu<double> &polar_image, GpuMultidimArrayAtGpu<double> &polar2_image, bool rotate);
 
-//void cuda_masking(float *image, size_t Xdim, size_t Ydim, size_t Zdim, size_t batch, int radius);
 class GpuCorrelationAux {
 public:
 	GpuMultidimArrayAtGpu<double> d_original_image;
@@ -25,12 +24,29 @@ public:
 	GpuMultidimArrayAtGpu<double> debug;
 	//GpuMultidimArrayAtGpu< std::complex<double> > debug_complex;
 
-	void produceSideInfo();
+	//AJ new
+	GpuMultidimArrayAtGpu<double> d_padded_image;
+	GpuMultidimArrayAtGpu<double> d_padded2_image;
+	GpuMultidimArrayAtGpu<double> d_mask;
+	GpuMultidimArrayAtGpu<double> d_polar_image;
+	GpuMultidimArrayAtGpu<double> d_polar2_image;
+
+	void produceSideInfo(mycufftHandle &myhandlePadded, mycufftHandle &myhandleMask);
 };
 
-void cuda_calculate_correlation(GpuCorrelationAux &referenceAux, GpuCorrelationAux &experimentalAux, TransformMatrix<float> &transMat, double *max_vector, double maxShift);
-void cuda_calculate_correlation_rotation(GpuCorrelationAux &referenceAux, GpuCorrelationAux &experimentalAux, TransformMatrix<float> &transMat, double *max_vector, double maxShift);
+
+void cuda_calculate_correlation(GpuCorrelationAux &referenceAux, GpuCorrelationAux &experimentalAux, TransformMatrix<float> &transMat, double *max_vector, double maxShift, mycufftHandle &myhandlePadded);
+void cuda_calculate_correlation_rotation(GpuCorrelationAux &referenceAux, GpuCorrelationAux &experimentalAux, TransformMatrix<float> &transMat, double *max_vector, double maxShift, mycufftHandle &myhandlePadded);
 void apply_transform(GpuMultidimArrayAtGpu<double> &d_original_image, GpuMultidimArrayAtGpu<double> &d_transform_image, TransformMatrix<float> &transMat);
 void padding_masking(GpuMultidimArrayAtGpu<double> &d_orig_image, GpuMultidimArrayAtGpu<double> &mask, GpuMultidimArrayAtGpu<double> &padded_image_gpu, GpuMultidimArrayAtGpu<double> &padded_image2_gpu, GpuMultidimArrayAtGpu<double> &padded_mask_gpu, bool rotation, bool experimental);
+void fft_v2(size_t Xdim, size_t Ydim, size_t Ndim, size_t Zdim, double *in, std::complex<double> *out, mycufftHandle &myhandle);
+void ifft_v2(size_t Xdim, size_t Ydim, size_t Ndim, size_t Zdim, std::complex<double> *in, double *out, mycufftHandle &myhandle);
+//void createPlanFFT(size_t Xdim, size_t Ydim, size_t Ndim, size_t Zdim, mycufftHandle &myhandle, size_t &workSize);
+void calculateFFTPlanSize(mycufftHandle &myhandle);
+void produceSideInfoCuda(GpuCorrelationAux &aux, mycufftHandle &myhandlePadded, mycufftHandle &myhandleMask, mycufftHandle &myhandlePolar, size_t memoryUsed);
+void cuda_align_experimental_image(double *expImages, GpuCorrelationAux &d_referenceAux, GpuCorrelationAux &d_experimentalAux,
+		TransformMatrix<float> &transMat_tr, TransformMatrix<float> &transMat_rt, double *max_vector_tr, double *max_vector_rt,
+		int numImagesRef, GpuMultidimArrayAtGpu<double> &mask, double maxShift,
+		mycufftHandle &myhandlePadded, mycufftHandle &myhandleMask, mycufftHandle &myhandlePolar, size_t mdExpSize);
 
 #endif
