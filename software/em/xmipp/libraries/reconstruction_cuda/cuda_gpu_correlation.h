05a2af5670ce034beccac691440e0048d91a8663
ajimoreno <ajimenez@gmail.com>
2017-09-01 11:16:16 +0200

    cleaninng the code

--- software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_correlation.h
+++ software/em/xmipp/libraries/reconstruction_cuda/cuda_gpu_correlation.h
@@ -14,7 +14,6 @@ public:
 	GpuMultidimArrayAtGpu<float> polar_gpu;
 	GpuMultidimArrayAtGpu<float> polar2_gpu;
 	GpuMultidimArrayAtGpu< std::complex<float> > MF, MF2;
-	//GpuMultidimArrayAtGpu<float> MF2realSpace;
 	GpuMultidimArrayAtGpu< std::complex<float> > RefExpFourier;
 	GpuMultidimArrayAtGpu< float > RefExpRealSpace;
 	GpuMultidimArrayAtGpu< std::complex<float> > RefExpFourierPolar;
@@ -40,9 +39,7 @@ public:
 
 	GpuMultidimArrayAtGpu< float > d_denom;
 	GpuMultidimArrayAtGpu< float > MFrealSpace, MF2realSpace;
-	GpuMultidimArrayAtGpu<float> debug;
 
-	//AJ new
 	GpuMultidimArrayAtGpu<float> d_mask;
 
 	void produceSideInfo(mycufftHandle &myhandlePadded, mycufftHandle &myhandleMask, StructuresAux &myStructureAux);
@@ -54,9 +51,6 @@ void cuda_calculate_correlation(GpuCorrelationAux &referenceAux, GpuCorrelationA
 void cuda_calculate_correlation_rotation(GpuCorrelationAux &referenceAux, GpuCorrelationAux &experimentalAux, TransformMatrix<float> &transMat, float *max_vector, int maxShift, mycufftHandle &myhandlePadded, bool mirror, StructuresAux &myStructureAux);
 void apply_transform(GpuMultidimArrayAtGpu<float> &d_original_image, GpuMultidimArrayAtGpu<float> &d_transform_image, TransformMatrix<float> &transMat);
 void padding_masking(GpuMultidimArrayAtGpu<float> &d_orig_image, GpuMultidimArrayAtGpu<float> &mask, GpuMultidimArrayAtGpu<float> &padded_image_gpu, GpuMultidimArrayAtGpu<float> &padded_image2_gpu, GpuMultidimArrayAtGpu<float> &padded_mask_gpu, bool experimental);
-//void fft_v2(size_t Xdim, size_t Ydim, size_t Ndim, size_t Zdim, float *in, std::complex<float> *out, mycufftHandle &myhandle);
-//void ifft_v2(size_t Xdim, size_t Ydim, size_t Ndim, size_t Zdim, std::complex<float> *in, float *out, mycufftHandle &myhandle);
-//void createPlanFFT(size_t Xdim, size_t Ydim, size_t Ndim, size_t Zdim, mycufftHandle &myhandle, size_t &workSize);
 void calculateFFTPlanSize(mycufftHandle &myhandle);
 void produceSideInfoCuda(GpuCorrelationAux &aux, mycufftHandle &myhandlePadded, mycufftHandle &myhandleMask, mycufftHandle &myhandlePolar, size_t memoryUsed);
 void cuda_align_experimental_image(float *expImages, GpuCorrelationAux &d_referenceAux, GpuCorrelationAux &d_experimentalAux,
