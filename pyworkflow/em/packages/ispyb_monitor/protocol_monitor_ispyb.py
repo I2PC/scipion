4867f1dbbf263d7b81ae8ab2172d032b2f65700b
Kevin Savage <kevin.savage@diamond.ac.uk>
2017-09-04 13:18:05 +0100

    changing when we need to add ctfs

--- pyworkflow/em/packages/ispyb_monitor/protocol_monitor_ispyb.py
+++ pyworkflow/em/packages/ispyb_monitor/protocol_monitor_ispyb.py
@@ -275,8 +275,9 @@ class MonitorISPyB(Monitor):
         for mic in self.iter_updated_set(prot.outputMicrographs):
             micId = mic.getObjId()
             if self.movies.get(micId, None) is not None:
-                if 'comments' in self.movies[micId]:  # skip if we already have align info
+                if self.motion_corrections[micId] is not None and 'comments' in self.motion_corrections[micId]:  # skip if we already have align info
                     continue
+
                 micFn = mic.getFileName()
                 renderable_image = self.imageGenerator.generate_image(micFn, micFn)
 
@@ -306,8 +307,8 @@ class MonitorISPyB(Monitor):
     def update_ctf_params(self, prot, updateIds):
         for ctf in self.iter_updated_set(prot.outputCTF):
             micId = ctf.getObjId()
-            if self.ctfs.get(micId, None) is not None:
-                if 'estimatedDefocus' in self.ctfs[micId]:  # skip if we already have ctf info
+             if self.motion_corrections.get(micId, None) is not None:
+                if self.ctfs[micId] is not None and 'status' in self.ctfs[micId]:  # skip if we already have align info
                     continue
                 micFn = ctf.getMicrograph().getFileName()
                 psdName = pwutils.replaceBaseExt(micFn, 'psd.jpeg')
