de866f22fd4aa19c3a72ff754e069879d70e6196
Kevin Savage <kevin.savage@diamond.ac.uk>
2017-09-05 13:42:20 +0100

    python 3 print statements

--- pyworkflow/protocol/protocol.py
+++ pyworkflow/protocol/protocol.py
@@ -1785,19 +1785,19 @@ def runProtocolMain(projectPath, protDbPath, protId):
             sys.exit(retcode)
         elif nThreads > 1:
             if protocol.useQueueForJobs():
-                print "using QueueStepExecutor1"
+                print("using QueueStepExecutor1")
                 executor = QueueStepExecutor(hostConfig, protocol.getSubmitDict(), nThreads-1)
             else:
                 executor = ThreadStepExecutor(hostConfig, nThreads-1)
 
     if executor is None and protocol.useQueueForJobs():
-        print "using QueueStepExecutor2"
+        print("using QueueStepExecutor2")
         executor = QueueStepExecutor(hostConfig, protocol.getSubmitDict(), 1)
 
     if executor is None:
         executor = StepExecutor(hostConfig)
 
-    print "executor is set", type(executor)
+    print("executor is set", type(executor))
 
     protocol.setStepsExecutor(executor)
     # Finally run the protocol
