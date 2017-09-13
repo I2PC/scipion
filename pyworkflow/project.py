99cca8884ec7257754c4545373531d1f0109dcbe
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2017-09-13 14:29:26 +0200

    Avoid failure when wrong relation, print error message

--- pyworkflow/project.py
+++ pyworkflow/project.py
@@ -1104,6 +1104,15 @@ class Project(object):
             pObj = self.getObject(rel['object_parent_id'])
             pExt = rel['object_parent_extended']
             pp = pwobj.Pointer(pObj, extended=pExt)
+
+            if pObj is None or pp.get() is None:
+                print("project._getRelationGraph: ERROR, pointer to parent is "
+                      "None. IGNORING IT.\n")
+                for key in rel.keys():
+                    print("%s: %s" % (key, rel[key]))
+                    
+                continue
+
             pid = pp.getUniqueId()
             parent = g.getNode(pid)
 
