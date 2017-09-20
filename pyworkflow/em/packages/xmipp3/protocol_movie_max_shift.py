aca9fa69f871be4de812baf4c7de95545efb41c9
David <dmaluendan@gmail.com>
2017-09-20 18:08:07 +0200

    Ya funciona. Lo que quizá se podría mejorar la implementación.

--- pyworkflow/em/packages/xmipp3/protocol_movie_max_shift.py
+++ pyworkflow/em/packages/xmipp3/protocol_movie_max_shift.py
@@ -25,7 +25,7 @@
 # *
 # **************************************************************************
 
-
+import os
 from os.path import getmtime, exists
 from datetime import datetime
 import numpy as np
@@ -37,6 +37,10 @@ import pyworkflow.utils as pwutils
 
 from pyworkflow.object import Set
 from pyworkflow.protocol.constants import (STATUS_NEW)
+from pyworkflow.protocol.params import PointerParam
+from pyworkflow.utils.properties import Message
+from pyworkflow.em.data import (MovieAlignment, SetOfMovies, SetOfMicrographs,
+                                Image)
 
 
 class XmippProtMovieMaxShift(ProtProcessMovies):
@@ -51,18 +55,21 @@ class XmippProtMovieMaxShift(ProtProcessMovies):
 
     #--------------------------- DEFINE param functions ------------------------
     def _defineParams(self, form):
-        ProtProcessMovies._defineParams(self, form)
-        # form.addSection(label='Input')
+    #    ProtProcessMovies._defineParams(self, form)
+        form.addSection(label='Input')
+        form.addParam('inputMovies', PointerParam, pointerClass='SetOfMovies', 
+              important=True,
+              label=Message.LABEL_INPUT_MOVS,
+              help='Select a set of previously imported movies.')
         form.addParam('maxFrameShift', params.FloatParam, default=10, label='Max. frame shift (A)')
         form.addParam('maxMovieShift', params.FloatParam, default=100, label='Max. movie shift (A)')
         
     #--------------------------- INSERT steps functions ------------------------
-    def _filterMovie(self, movie):
-        # Try to use the 'original' fileName in case it is present
-        # the original could be different from the current filename if
-        # we are dealing with compressed movies (e.g., movie.mrc.bz2)
+
+    def _createOutputMovie(self, movie):
+        """ Create movie only if the alignment is less than the thresshold. """
         alignment = movie.getAlignment()
-        print(movie.hasAlignment())
+
         if alignment is not None:
             shiftListX, shiftListY = alignment.getShifts()
             shiftArrayX = np.asarray(shiftListX)
@@ -73,14 +80,117 @@ class XmippProtMovieMaxShift(ProtProcessMovies):
             cumsumY = np.cumsum(shiftArrayY)
             rejectedByMovie = np.max(np.abs(cumsumX))*self.samplingRate>self.maxMovieShift.get() or \
                               np.max(np.abs(cumsumY))*self.samplingRate>self.maxMovieShift.get()
-        # else
-        #     print("No alignment")
+
+            return not rejectedByFrame and not rejectedByMovie
+        else: # If movies are not aligned, this protocal makes not sense
+            return False # The protocol returns an empty SetOfMovies to poit out the non-sense.
 
 
-        print("OK=",not rejectedByFrame and not rejectedByMovie)
-        return not rejectedByFrame and not rejectedByMovie
-
+    #    # FIXME: Methods will change when using the streaming for the output
     def createOutputStep(self):
         # Do nothing now, the output should be ready.
-        print("Adeu")
-        
+        pass
+
+    def _createOutputMovies(self):
+        """ Returns True if an output set of movies will be generated.
+        The most common case is to always generate output movies,
+        either with alignment only or the binary aligned movie files.
+        Subclasses can override this function to change this behavior.
+        """
+        return True
+
+
+    def _loadOutputSet(self, SetClass, baseName, fixSampling=True):
+        """
+        Load the output set if it exists or create a new one.
+        fixSampling: correct the output sampling rate if binning was used,
+        except for the case when the original movies are kept and shifts
+        refers to that one.
+        """
+        setFile = self._getPath(baseName)
+
+        if os.path.exists(setFile):
+            outputSet = SetClass(filename=setFile)
+            outputSet.loadAllProperties()
+            outputSet.enableAppend()
+        else:
+            outputSet = SetClass(filename=setFile)
+            outputSet.setStreamState(outputSet.STREAM_OPEN)
+
+        inputMovies = self.inputMovies.get()
+        outputSet.copyInfo(inputMovies)
+
+        return outputSet
+
+
+
+    def _checkNewOutput(self):
+        if getattr(self, 'finished', False):
+            return
+
+        # Load previously done items (from text file)
+        doneList = self._readDoneList()
+        # Check for newly done items
+        newDone = [m for m in self.listOfMovies
+                   if m.getObjId() not in doneList and self._isMovieDone(m)]
+
+        # Update the file with the newly done movies
+        # or exit from the function if no new done movies
+        self.debug('_checkNewOutput: ')
+        self.debug('   listOfMovies: %s, doneList: %s, newDone: %s'
+                   % (len(self.listOfMovies), len(doneList), len(newDone)))
+
+        firstTime = len(doneList) == 0
+        allDone = len(doneList) + len(newDone)
+        # We have finished when there is not more input movies (stream closed)
+        # and the number of processed movies is equal to the number of inputs
+        self.finished = self.streamClosed and allDone == len(self.listOfMovies)
+        streamMode = Set.STREAM_CLOSED if self.finished else Set.STREAM_OPEN
+
+        if newDone:
+            self._writeDoneList(newDone)
+
+        elif not self.finished:
+            # If we are not finished and no new output have been produced
+            # it does not make sense to proceed and updated the outputs
+            # so we exit from the function here
+            return
+
+        self.debug('   finished: %s ' % self.finished)
+        self.debug('        self.streamClosed (%s) AND' % self.streamClosed)
+        self.debug('        allDone (%s) == len(self.listOfMovies (%s)'
+                   % (allDone, len(self.listOfMovies)))
+        self.debug('   streamMode: %s' % streamMode)
+
+        if self._createOutputMovies():
+
+            saveMovie = self.getAttributeValue('doSaveMovie', False)
+            movieSet = self._loadOutputSet(SetOfMovies, 'movies.sqlite',
+                                           fixSampling=saveMovie)
+            
+            for movie in newDone:
+                if self._createOutputMovie(movie):
+                    newMovie = movie.clone()
+                    movieSet.append(newMovie)
+
+            self._updateOutputSet('outputMovies', movieSet, streamMode)
+
+            # if firstTime:
+
+            #     # Probably is a good idea to store a cached summary for the
+            #     # first resulting movie of the processing.
+            #     # self._storeSummary(newDone[0])                 ----------------------
+
+            #     # If the movies are not written out, then dimensions can be
+            #     # copied from the input movies
+
+            #     if saveMovie:
+            #         movieSet.setDim(self.inputMovies.get().getDim())
+                    
+            #     self._defineTransformRelation(self.inputMovies, movieSet)
+
+        if self.finished:  # Unlock createOutputStep if finished all jobs
+            outputStep = self._getFirstJoinStep()
+            if outputStep and outputStep.isWaiting():
+                outputStep.setStatus(STATUS_NEW)
+
