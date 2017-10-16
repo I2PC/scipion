39452550db13f4bbe8d2763b326dccce5f70b71b
mmmtnez <mmmtnez@cnb.csic.es>
2017-10-16 10:11:34 +0200

    Format correction according PEP8 requirements

--- pyworkflow/em/packages/chimera/convert.py
+++ pyworkflow/em/packages/chimera/convert.py
@@ -31,38 +31,40 @@ TODO:
 """
 
 import os
-import pyworkflow.em as em
 import pyworkflow.utils as pwutils
 from pyworkflow.em.viewer import getChimeraEnviron
-from pyworkflow.em.constants import SYM_CYCLIC, SYM_DIHEDRAL, SYM_TETRAHEDRAL, SYM_OCTAHEDRAL, SYM_I222, SYM_I222r, SYM_In25, SYM_In25r
+from pyworkflow.em.constants import SYM_CYCLIC, SYM_DIHEDRAL, \
+    SYM_TETRAHEDRAL, SYM_OCTAHEDRAL, SYM_I222, SYM_I222r, SYM_In25, SYM_In25r
 
 #
-chimeraPdbTemplateFileName= "scipionOut%04d.pdb"
-chimeraMapTemplateFileName= "scipionOut%04d.mrc"
+chimeraPdbTemplateFileName = "scipionOut%04d.pdb"
+chimeraMapTemplateFileName = "scipionOut%04d.mrc"
 chimeraScriptFileName = "chimeraScript.py"
 
 symMapperScipionchimera = {}
-symMapperScipionchimera[SYM_CYCLIC]="Cn"
-symMapperScipionchimera[SYM_DIHEDRAL]="Dn"
-symMapperScipionchimera[SYM_TETRAHEDRAL]="T"
-symMapperScipionchimera[SYM_OCTAHEDRAL]="O"
-symMapperScipionchimera[SYM_I222]="222"
-symMapperScipionchimera[SYM_I222r]="222r"
-symMapperScipionchimera[SYM_In25]="n25"
-symMapperScipionchimera[SYM_In25r]="n25r"
+symMapperScipionchimera[SYM_CYCLIC] = "Cn"
+symMapperScipionchimera[SYM_DIHEDRAL] = "Dn"
+symMapperScipionchimera[SYM_TETRAHEDRAL] = "T"
+symMapperScipionchimera[SYM_OCTAHEDRAL] = "O"
+symMapperScipionchimera[SYM_I222] = "222"
+symMapperScipionchimera[SYM_I222r] = "222r"
+symMapperScipionchimera[SYM_In25] = "n25"
+symMapperScipionchimera[SYM_In25r] = "n25r"
+
 
 def getEnviron(ccp4First=True):
     return getChimeraEnviron()
 
+
 def runChimeraProgram(program, args=""):
     """ Internal shortcut function to launch chimera program. """
-    env=getEnviron()
+    env = getEnviron()
     pwutils.runJob(None, program, args, env=env)
+
 
 def getProgram(progName="chimera"):
     """ Return the program binary that will be used. """
-    if (not 'CHIMERA_HOME' in os.environ):
+    if 'CHIMERA_HOME' not in os.environ:
         return None
     return os.path.join(os.environ['CHIMERA_HOME'], 'bin',
-                           os.path.basename(progName))
-
+                        os.path.basename(progName))
