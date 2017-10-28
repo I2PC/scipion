127aec40d42666edc634065f8e3abbf0232ba054
ajimoreno <ajimoreno@gmail.com>
2017-10-28 21:18:20 +0200

    a new binding program for python to carry out the alignment of two images

--- software/em/xmipp/libraries/bindings/python/python_image.cpp
+++ software/em/xmipp/libraries/bindings/python/python_image.cpp
@@ -161,7 +161,9 @@ PyMethodDef Image_methods[] =
           "Compute image statistics, return mean, dev, min and max" },
         { "adjustAndSubtract", (PyCFunction) Image_adjustAndSubtract, METH_VARARGS,
           "I1=I1-adjusted(I2)" },
-        /* Equivalent methods to inplace operations, but without creating new instances of Image */
+		{ "align", (PyCFunction) Image_align, METH_VARARGS,
+		  "aligned(I2), align I2 to resemble I1" },
+		/* Equivalent methods to inplace operations, but without creating new instances of Image */
         { "inplaceAdd", (PyCFunction) Image_inplaceAdd, METH_VARARGS,
           "Add another image to self (does not create another Image instance)" },
         { "inplaceSubtract", (PyCFunction) Image_inplaceSubtract, METH_VARARGS,
@@ -1245,6 +1247,43 @@ Image_adjustAndSubtract(PyObject *obj, PyObject *args, PyObject *kwargs)
     }
     return (PyObject *)result;
 }//function Image_adjustAndSubtract
+
+/* Align an argument image to resemble self */
+PyObject *
+Image_align(PyObject *obj, PyObject *args, PyObject *kwargs)
+{
+    ImageObject *self = (ImageObject*) obj;
+    PyObject *pimg2 = NULL;
+    ImageObject * result = PyObject_New(ImageObject, &ImageType);
+    if (self != NULL)
+    {
+        try
+        {
+            if (PyArg_ParseTuple(args, "O", &pimg2))
+            {
+                ImageObject *img2=(ImageObject *)pimg2;
+                result->image = new ImageGeneric(Image_Value(img2));
+                *result->image = *img2->image;
+
+                result->image->convert2Datatype(DT_Double);
+                MultidimArray<double> *mimg2;
+                MULTIDIM_ARRAY_GENERIC(*result->image).getMultidimArrayPointer(mimg2);
+
+                self->image->convert2Datatype(DT_Double);
+                MultidimArray<double> *mimg1;
+                MULTIDIM_ARRAY_GENERIC(*self->image).getMultidimArrayPointer(mimg1);
+
+                Matrix2D<double> M;
+                alignImagesConsideringMirrors(*mimg1, *mimg2, M, true);
+            }
+        }
+        catch (XmippError &xe)
+        {
+            PyErr_SetString(PyXmippError, xe.msg.c_str());
+        }
+    }
+    return (PyObject *)result;
+}//function Image_align
 
 /* Add two images, operator + */
 PyObject *
