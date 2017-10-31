1dec9c42b58b33e07af65da92afa3509bd85556f
JaviMota <jamotga26@gmail.com>
2017-10-31 10:32:17 +0100

    all metadata labels

--- software/em/xmipp/libraries/data/metadata_label.h
+++ software/em/xmipp/libraries/data/metadata_label.h
@@ -191,11 +191,13 @@ enum MDLabel
     MDL_CTF_XRAY_LENS_TYPE, ///Algorithm used to generate Xray PSF
     MDL_CTF_XRAY_OUTER_ZONE_WIDTH, /// Outermost zone width of the X-ray Fresnel lens (nm)
     MDL_CTF_XRAY_ZONES_NUMBER, // Number of zones of the X-ray Fresnel lens
+	MDL_CTF_PHASE_SHIFT,	//Volta Phase Plate phase shift
     MDL_CUMULATIVE_SSNR, ///<Cumulative SSNR (double)
     MDL_DATATYPE, ///< if read from file original image datatype, this is an struct defined in image
     MDL_DEFGROUP, ///< Defocus group
     MDL_DIMRED, ///< Projection onto a reduced manifold (vector double)
     MDL_DIRECTION, ///< Direction in 3D
+
 
     MDL_DM3_IDTAG,
     MDL_DM3_NODEID,
@@ -1476,6 +1478,7 @@ private:
         MDL::addLabel(MDL_CTF_BG_GAUSSIAN_K, LABEL_DOUBLE, "ctfBgGaussianK");
         MDL::addLabel(MDL_CTF_BG_GAUSSIAN_SIGMAU, LABEL_DOUBLE, "ctfBgGaussianSigmaU");
         MDL::addLabel(MDL_CTF_BG_GAUSSIAN_SIGMAV, LABEL_DOUBLE, "ctfBgGaussianSigmaV");
+        MDL::addLabel(MDL_CTF_PHASE_SHIFT, LABEL_DOUBLE, "ctfVPPphaseshift");
 
         MDL::addLabelAlias(MDL_CTF_BG_GAUSSIAN2_CU, "CTFBG_Gaussian2_CU");//3.0
         MDL::addLabelAlias(MDL_CTF_BG_GAUSSIAN2_CV, "CTFBG_Gaussian2_CV");//3.0
