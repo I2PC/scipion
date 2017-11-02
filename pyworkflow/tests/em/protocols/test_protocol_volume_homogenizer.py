fcfa43f9080d100289e5744e234548e982b5f701
jvargas <jvargas@cnb.csic.es>
2017-11-02 20:41:47 +0100

    trying to do some test with sense

--- pyworkflow/tests/em/protocols/test_protocol_volume_homogenizer.py
+++ pyworkflow/tests/em/protocols/test_protocol_volume_homogenizer.py
@@ -1,6 +1,6 @@
 # ***************************************************************************
 # * Authors:     Mohsen Kazemi (mkazemi@cnb.csic.es)
-# *
+# *              Javier Vargas (javier.vargasbalbuena@mcgill.ca)  
 # *
 # * This program is free software; you can redistribute it and/or modify
 # * it under the terms of the GNU General Public License as published by
@@ -23,10 +23,10 @@
 
 import os
 from pyworkflow.tests import BaseTest, setupTestProject, DataSet
-from pyworkflow.em.protocol import ProtImportParticles, ProtImportVolumes, ProtSubSet
+from pyworkflow.em.protocol import ProtImportParticles, ProtImportVolumes, ProtSubSet 
 from pyworkflow.em.packages.xmipp3.protocol_volume_homogenizer import XmippProtVolumeHomogenizer
-from pyworkflow.em.data import SetOfParticles, Particle, Acquisition, Coordinate, CTFModel
-from pyworkflow.object import Float
+from pyworkflow.em.packages.relion import ProtRelionRefine3D
+from pyworkflow.em.data import SetOfParticles
     
 class TestVolumeHomogenizer(BaseTest):
     @classmethod
@@ -34,24 +34,10 @@ class TestVolumeHomogenizer(BaseTest):
         setupTestProject(cls)
         cls.dsRelion = DataSet.getDataSet('relion_tutorial')
         cls.dsXmipp = DataSet.getDataSet('xmipp_tutorial')
-        
-    def importVolumes(self):
-        """ Import several Volumes
-        """                
-        args = {'filesPath': self.dsXmipp.getFile('volumes/'),
-                'filesPattern': '*_64.vol',
-                'samplingRate': 1
-                }        
-        prot = self.newProtocol(ProtImportVolumes, **args)
-        prot.setObjLabel('import volume')
-        self.launchProtocol(prot)
-        
-        return prot
-    
+           
     def importFromRelionRefine3D(self):
         """ Import aligned Particles
         """
-    
         prot = self.newProtocol(ProtImportParticles,
                                 objLabel='particles from relion (auto-refine 3d)',
                                 importFrom=ProtImportParticles.IMPORT_FROM_RELION,
@@ -66,7 +52,7 @@ class TestVolumeHomogenizer(BaseTest):
     
         return prot
     
-    def getParticles(self, protImport, classid=1):
+    def getParticles(self, protImport, classid):
         dbPartSet = protImport._getPath("particles_class-%d.sqlite" % classid)
         class3D = protImport.outputClasses[classid]
         if os.path.exists(dbPartSet):
@@ -74,9 +60,7 @@ class TestVolumeHomogenizer(BaseTest):
         partSet = SetOfParticles(filename=dbPartSet)
         partSet.copyInfo(class3D)
     
-        # print "Type: ", type(class3D)
         for part in class3D:
-            # print "Particle: ", part.printAll()
             partSet.append(part)
         partSet.write()
         partSet.close()
@@ -94,34 +78,128 @@ class TestVolumeHomogenizer(BaseTest):
                              "There was a problem with the import")
         return protImportCls1
         
-    # def subSetOfParticles(self):
-    #
-    #     protSubset = self.newProtocol(ProtSubSet,
-    #                                   objLabel='subset of particles',
-    #                                   chooseAtRandom=True,
-    #                                   nElements=200)
-    #
-    #     protSubset.inputFullSet.set(self.inputParticles1)
-    #     self.launchProtocol(protSubset)
-    #     self.assertIsNotNone(protSubset.outputParticles.getFileName(),
-    #                          "There was a problem with the protSubset")
-    #
-    #     return protSubset
-         
+    def subSetOfParticles(self,importPartClass1):
+    
+        protSubset = self.newProtocol(ProtSubSet,
+                                       objLabel='subset of particles',
+                                       chooseAtRandom=True,
+                                       nElements=200)
+    
+        protSubset.inputFullSet.set(importPartClass1.outputParticles)
+        self.launchProtocol(protSubset)
+        self.assertIsNotNone(protSubset.outputParticles.getFileName(),
+                              "There was a problem with the protSubset")
+    
+        return protSubset
+    
+ 
+    def getVolume(self,  classid):
+        """ Import a Volumes
+        """                        
+        protImportVol = self.newProtocol(ProtImportVolumes,
+                                objLabel='vol class-%d' % classid,
+                                filesPath=self.dsRelion.getFile('import/classify3d/extra/relion_it015_class00%d.mrc' % classid),
+                                samplingRate=7.08,
+                                )
+        self.launchProtocol(protImportVol)        
+        return protImportVol
+    
+    def relionRefine(self,protSubSetClass, vol, classid):
+            
+            relionRefine = self.newProtocol(ProtRelionRefine3D,
+                                            objLabel='relion_%s' % classid,
+                                            doCTF=False, runMode=1,
+                                            memoryPreThreads=1,
+                                            maskDiameterA=424,
+                                            angularSamplingDeg=1,
+                                            localSearchAutoSamplingDeg = 2,
+                                            symmetryGroup="c1",
+                                            numberOfMpi=3, numberOfThreads=1)
+            
+            relionRefine.inputParticles.set(protSubSetClass.outputParticles)
+            relionRefine.referenceVolume.set(vol.outputVolume)
+                        
+            self.launchProtocol(relionRefine)
+            return relionRefine
+     
+    def homoNoGoldStandardNoAlign(self,relion1,relion2):
+        '''test without Goldstandard and alignment'''
+        protVolumeHomogenizer = self.newProtocol(XmippProtVolumeHomogenizer,
+                                objLabel='volume homogenizer1')
+        
+        protVolumeHomogenizer.referenceVolume.set(relion1.outputVolume)
+        protVolumeHomogenizer.inputVolume.set(relion2.outputVolume)
+        protVolumeHomogenizer.inputParticles.set(relion2.outputParticles)
+        protVolumeHomogenizer.doAlignment.set(False)  
+        self.launchProtocol(protVolumeHomogenizer)
+        
+        self.assertIsNotNone(protVolumeHomogenizer.outputParticles.getFileName(),
+                              "There was a problem with the homoNoGoldStandard")
+        
+        return protVolumeHomogenizer
+    
+    
+    def homoNoGoldStandardAlign(self,relion1,relion2):
+        '''test without Goldstandard and alignment'''
+        protVolumeHomogenizer = self.newProtocol(XmippProtVolumeHomogenizer,
+                                objLabel='volume homogenizer2')
+        
+        protVolumeHomogenizer.referenceVolume.set(relion1.outputVolume)
+        protVolumeHomogenizer.inputVolume.set(relion2.outputVolume)
+        protVolumeHomogenizer.inputParticles.set(relion2.outputParticles)
+        protVolumeHomogenizer.doAlignment.set(True)  
+        self.launchProtocol(protVolumeHomogenizer)
+        
+        self.assertIsNotNone(protVolumeHomogenizer.outputParticles.getFileName(),
+                              "There was a problem with the homoNoGoldStandard")
+        
+        return protVolumeHomogenizer
+    
+    def homoGoldStandardNoAlign(self,relion1,relion2):
+        '''test without Goldstandard and alignment'''
+        protVolumeHomogenizer = self.newProtocol(XmippProtVolumeHomogenizer,
+                                objLabel='volume homogenizer3',
+                                doGoldStandard = True)
+                
+        protVolumeHomogenizer.referenceVolume1.set(relion1.outputVolume)
+        protVolumeHomogenizer.inputVolume1.set(relion2.outputVolume)
+        protVolumeHomogenizer.referenceVolume2.set(relion1.outputVolume)
+        protVolumeHomogenizer.inputVolume2.set(relion2.outputVolume)
+        protVolumeHomogenizer.inputParticles.set(relion2.outputParticles)
+        protVolumeHomogenizer.doAlignment.set(False)
+          
+        self.launchProtocol(protVolumeHomogenizer)        
+        self.assertIsNotNone(protVolumeHomogenizer.outputParticles.getFileName(),
+                              "There was a problem with the homoNoGoldStandard")
+        
+        return protVolumeHomogenizer
+
+            
+        
     def test_volumeHomogenizer(self):
         """ Test protocol volume homogenizer
         """
-        #protImportVols = self.importVolumes()
         protImpClasses = self.importFromRelionRefine3D()
-        importPartClass1 = self.getParticles(protImpClasses, classid=1)
-        # protSubSet = self.subSetOfParticles()
         
-        '''
-        protVolumeHomogenizer = self.newProtocol(XmippProtVolumeHomogenizer,
-                                objLabel='volume homogenizer')
-        protVolumeHomogenizer.referenceVolume.set(protImportVols.outputVolume)
-        protVolumeHomogenizer.inputVolume.set(protImportVols.outputVolume)
-        protVolumeHomogenizer.inputParticles.set(protImportParts.outputParticles)
-        protVolumeHomogenizer.doAlignment.set(True)  
-        self.launchProtocol(protVolumeHomogenizer)
-        '''
+        importPartClass1 = self.getParticles(protImpClasses, classid=1)
+        protSubSetClass1 = self.subSetOfParticles(importPartClass1)
+    
+        importPartClass2 = self.getParticles(protImpClasses, classid=2)
+        protSubSetClass2 = self.subSetOfParticles(importPartClass2)
+        
+        vol1 = self.getVolume(classid=1)
+        vol2 = self.getVolume(classid=2)
+        
+        relion1 = self.relionRefine(protSubSetClass1,vol1,classid=1)
+        relion2 = self.relionRefine(protSubSetClass2,vol2,classid=2)
+                
+        homo1 = self.homoNoGoldStandardNoAlign(relion1,relion2)
+        #homo2 = self.homoNoGoldStandardAlign(relion1,relion2) #FAILS
+        homo3 = self.homoGoldStandardNoAlign(relion1,relion2)
+
+        
+
+        
+        
+        
+        
