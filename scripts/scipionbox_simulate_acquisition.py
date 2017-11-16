7270d62b5de84b9071ddfdd9c130fd8280bea80a
rmarabini <roberto@cnb.csic.es>
2017-11-16 22:12:36 +0100

    adapt to pep8

--- scripts/scipionbox_simulate_acquisition.py
+++ scripts/scipionbox_simulate_acquisition.py
@@ -7,8 +7,9 @@ time the last link will be 'touched' simulating an image that is being
 acquired by the microscope.
 """
 # target = /home/scipionuser/OffloadData/2017_11_09_administrator_test_sample/GRID_01/DATA/Images-Disc1/GridSquare_27783809/Data
-#origin = ~/data/md1/01 
-import sys, os
+#origin = ~/data/md1/01
+import sys
+import os
 import time
 import shutil
 from glob import glob
@@ -19,13 +20,13 @@ import contextlib
 def usage(prog, error):
     print """
     ERROR: %s
-    
+
     Usage: simulate_acquisition.py INPUT_PATTERN OUTPUT_FOLDER
         INPUT_PATTERN: input pattern matching input files.
         OUTPUT_FOLDER: where to create the output links.
     example: %s "/home/scipionuser/OffloadData/Data/01/*.mrc" /home/scipionuser/OffloadData/2017_11_09_administrator_test_sample/GRID_01/DATA/Images-Disc1/GridSquare_27783809/Data
     """ % (error, prog)
-    sys.exit(1)    
+    sys.exit(1)
 
 
 if len(sys.argv) != 3:
@@ -43,7 +44,7 @@ if os.path.exists(outputDir):
 os.makedirs(outputDir)
 
 aTime = 30  # sec
-n = 5  # number of touchs 
+n = 5  # number of touchs
 t = aTime / n  # touch image each aTime/n sec
 
 for f in inputFiles:
@@ -54,7 +55,7 @@ for f in inputFiles:
         open(outputPath, 'w').close()
         time.sleep(t)
     try:
-        os.remove(outputPath)  # remove file if exists        
+        os.remove(outputPath)  # remove file if exists
     except:
         pass
     os.symlink(f, outputPath)
