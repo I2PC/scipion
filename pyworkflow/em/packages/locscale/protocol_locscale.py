f9d0ac0457dbdbf18cffa51a572564fb39cb0349
D. Maluenda <dmaluenda@cnb.csic.es>
2017-12-05 11:07:17 +0100

    Updating the input refference as PDB and including the MPI option

--- pyworkflow/em/packages/locscale/protocol_locscale.py
+++ pyworkflow/em/packages/locscale/protocol_locscale.py
@@ -25,68 +25,98 @@
 # **************************************************************************
 
 import os
-from pyworkflow.em import ProtRefine3D
-from pyworkflow.protocol.params import PointerParam
+from pyworkflow.em import ProtRefine3D, downloadPdb
+import pyworkflow.protocol.params as params
 from pyworkflow.em.data import Volume
+from pyworkflow.utils import replaceBaseExt, removeBaseExt, removeExt
 from locscale import *
 
                                
 class ProtLocScale(ProtRefine3D):
     """ This Protocol refine volume using a PDB model previously prepared."""
     _label = 'local scale'
+    IMPORT_FROM_ID = 0
+    IMPORT_OBJ = 1
+    IMPORT_FROM_FILES = 2 
 
     #--------------------------- DEFINE param functions ------------------------
     def _defineParams(self, form):
         form.addSection(label='Input')
 
        
-        form.addParam('inputVolume', PointerParam, pointerClass='Volume',
-                    important=True, label='Input volume', help='Input EM volume')
+        form.addParam('inputVolume', params.PointerParam, pointerClass='Volume',
+                      important=True, label='Input volume',
+                      help='Input EM volume')
 
-        form.addParam('input3DReference', PointerParam,
-                    pointerClass='Volume', label='3D reference volume:',
-                    help='3D reference reconstruction.')
+        form.addParam('inputPdbData', params.EnumParam, 
+                      choices=['id', 'object', 'file'],
+                      label="Retrieve PDB model from", default=self.IMPORT_OBJ,
+                      display=params.EnumParam.DISPLAY_HLIST,
+                      help='Model to apply the local scale.\n'
+                           'The PDB can be retrieved from server, '
+                           'using a pdb Object or from a local file.')
+        form.addParam('pdbId', params.StringParam, 
+                      label="Pdb Id ", allowsNull=True,
+                      condition='inputPdbData == IMPORT_FROM_ID',
+                      help='Type a pdb Id (four alphanumeric characters).')
+        form.addParam('refObj', params.PointerParam, pointerClass='PdbFile',
+                      label="Input pdb ", allowsNull=True,
+                      condition='inputPdbData == IMPORT_OBJ',
+                      help='Choose a pdb object.')
+        form.addParam('pdbFile', params.FileParam,
+                      label="File path", allowsNull=True,
+                      condition='inputPdbData == IMPORT_FROM_FILES',
+                      help='Specify a path to desired PDB structure.')
 
-        form.addParam('binaryMask', PointerParam, pointerClass='VolumeMask',
-                    label='3D mask', help='Binary mask: where 0 ignore voxels'
-                          'and 1 let process it.')
+        form.addParam('binaryMask', params.PointerParam, pointerClass='VolumeMask',
+                    label='3D mask',
+                    help='Binary mask: 0 to ignore that voxel and 1 to process it.')
 
-        form.addParallelSection(mpi=1)
+        form.addParam('doParalelize', params.BooleanParam, default=False,
+                    label='Paralelize', help='Do it in paralel with 4 MPI.')
     
     #--------------------------- INSERT steps functions ------------------------
-    def _insertAllSteps(self):        
+    def _insertAllSteps(self):
+        if self.inputPdbData == self.IMPORT_FROM_ID:
+            self._insertFunctionStep('pdbDownloadStep')
         args = self._prepareParams()
+        self._insertFunctionStep('prepareInput')
         self._insertFunctionStep('refineStep', args)
         self._insertFunctionStep('createOutputStep')
     
-    #--------------------------- STEPS functions -------------------------------    
+    #--------------------------- STEPS functions -------------------------------
+    def pdbDownloadStep(self):
+        """Download all pdb files in file_list and unzip them."""
+        downloadPdb(self.pdbId.get(), self._getPdbFileName(), self._log)
+
+    def prepareInput(self):
+        """Convert the pdb to a density map."""
+        pdbFn = self._getPdbFileName()
+        outFile = removeExt(self._getVolName())
+        args = '-i %s --sampling %f -o %s' % (pdbFn, self.sampling, outFile)
+        
+        # args += ' --centerPDB'
+
+        args += ' --size %d' % self.inputVolume.get().getDim()[0]
+
+        self.info("Input file: " + pdbFn)
+        self.info("Output file: " +outFile)
+        
+        program = "xmipp_volume_from_pdb"
+        self.runJob(program, args)
+
     def refineStep(self, args):
-        """ Run the EMAN program to refine a volume. """
+        """ Run the LocScale program (with EMAN enviroment)
+            to refine a volume. """
         program = getProgram('locscale_mpi.py')
         self.runJob(program, args, cwd=self._getExtraPath())
     
     def createOutputStep(self):
-        iterN = self.numberOfIterations.get()
-        partSet = self._getInputParticles()
-        numRun = self._getRun()
-        
-        vol = Volume()
-        
-        
-        vol.setFileName(self._getFileName("mapFull",run=numRun, iter=iterN))
-        halfMap1 = self._getFileName("mapEvenUnmasked", run=numRun)
-        halfMap2 = self._getFileName("mapOddUnmasked", run=numRun)
-        vol.setHalfMaps([halfMap1, halfMap2])
-        vol.copyInfo(partSet)
-        
-        newPartSet = self._createSetOfParticles()
-        newPartSet.copyInfo(partSet)
-        self._fillDataFromIter(newPartSet, iterN)
-        
-        self._defineOutputs(outputVolume=vol)
-        self._defineSourceRelation(self._getInputParticlesPointer(), vol)
-        self._defineOutputs(outputParticles=newPartSet)
-        self._defineTransformRelation(self._getInputParticlesPointer(), newPartSet)
+        volume = Volume()
+        volume.setSamplingRate(self.sampling)
+        volume.setFileName(self._getVolName())
+        self._defineOutputs(outputVolume=volume)
+        self._defineSourceRelation(self.inputVolume, volume)
     
     #--------------------------- INFO functions -------------------------------------------- 
     def _validate(self):
@@ -124,20 +154,35 @@ class ProtLocScale(ProtRefine3D):
         def getAbsPath(fileName):
             return os.path.abspath(fileName).replace(":mrc","")
 
+        self.sampling = self.inputVolume.get().getSamplingRate()
+
         volumeFn = getAbsPath(self.inputVolume.get().getFileName())
         args  = ' --em_map %s' % volumeFn
-        args += ' --apix %s' % self.inputVolume.get().getSamplingRate()
+        args += ' --apix %s' % self.sampling
         
-        modelFn = getAbsPath(self.input3DReference.get().getFileName())
+        modelFn = getAbsPath(self._getVolName())
         args += ' --model_map %s' % modelFn
-        
+
         if self.binaryMask.hasValue():
             maskFn = getAbsPath(self.binaryMask.get().getFileName())
             args += ' --mask %s' % maskFn
 
-        # if self.numberOfMpi>1:
-        #     args += ' --mpi True'# % self.numberOfMpi
+        if self.doParalelize:
+            args += ' --mpi'
 
-        args += ' -o %s' %self._getExtraPath('result.vol')
+        inputFn = removeBaseExt(self.inputVolume.get().getFileName())
+        outFn = inputFn + '_scaled.vol'
+        args += ' -o %s' %self._getExtraPath(outFn)
 
         return args
+
+    def _getPdbFileName(self):
+        if self.inputPdbData == self.IMPORT_FROM_ID:
+            return self._getExtraPath('%s.pdb' % self.pdbId.get())
+        elif self.inputPdbData == self.IMPORT_OBJ:
+            return self.refObj.get().getFileName()
+        else:
+            return self.pdbFile.get()
+    
+    def _getVolName(self):
+        return self._getExtraPath(replaceBaseExt(self._getPdbFileName(), "vol"))
