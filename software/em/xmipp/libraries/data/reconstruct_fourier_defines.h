50321e03ad0e41fa4574ca86155841422d526cd9
DStrelak <david.strelak@seznam.cz>
2017-12-05 10:02:10 +0100

    adding performance optimization for different GPU architectures (Pascal default)

--- software/em/xmipp/libraries/data/reconstruct_fourier_defines.h
+++ software/em/xmipp/libraries/data/reconstruct_fourier_defines.h
@@ -30,12 +30,36 @@ typedef float MATRIX[3][3];
 #define BLOB_TABLE_SIZE_SQRT 10000
 #define ACCURACY 0.001
 
+#define PASCAL
+
+#ifdef KEPLER
 // GPU specific
 #define BLOCK_DIM 16
 #define SHARED_BLOB_TABLE 0
 #define SHARED_IMG 0
+#define PRECOMPUTE_BLOB_VAL 0
+#define TILE 8
+#define GRID_DIM_Z 1
+#endif
+
+#ifdef MAXWELL
+// GPU specific
+#define BLOCK_DIM 8
+#define SHARED_BLOB_TABLE 0
+#define SHARED_IMG 0
 #define PRECOMPUTE_BLOB_VAL 1
 #define TILE 4
+#define GRID_DIM_Z 8
+#endif
+
+#ifdef PASCAL
+// GPU specific
+#define BLOCK_DIM 16
+#define SHARED_BLOB_TABLE 1
+#define SHARED_IMG 0
+#define PRECOMPUTE_BLOB_VAL 1
+#define TILE 2
 #define GRID_DIM_Z 1
+#endif
 
 #endif /* XMIPP_LIBRARIES_DATA_RECONSTRUCT_FOURIER_DEFINES_H_ */
