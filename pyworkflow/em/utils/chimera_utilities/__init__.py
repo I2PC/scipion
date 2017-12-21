b344d3c08e6370e7ca861e427ed3b24215d615cd
mmmtnez <mmmtnez@cnb.csic.es>
2017-12-21 15:35:18 +0100

    Created new file

---
+++ pyworkflow/em/utils/chimera_utilities/__init__.py
@@ -0,0 +1,7 @@
+from pyworkflow.em.utils.chimera_utilities.convert import getEnviron
+
+_logo = "ChimeraLogoSmall.png"
+
+#from protocol_fit import ChimeraProtRigidFit
+#from viewer_fit import viewerChimeraProtRigidFit
+_environ = getEnviron()
