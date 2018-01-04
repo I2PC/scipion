3388484c15964f62056c63157cd166d93159b8fa
mmmtnez <mmmtnez@cnb.csic.es>
2018-01-04 11:59:03 +0100

    Deletion of file content

--- pyworkflow/em/utils/chimera_utilities/__init__.py
+++
@@ -1,5 +0,0 @@
-#from pyworkflow.em.utils.chimera_utilities.convert import getEnviron
-
-#from protocol_fit import ChimeraProtRigidFit
-#from viewer_fit import viewerChimeraProtRigidFit
-#_environ = getEnviron()
