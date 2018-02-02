bbfd573fd752a4f231c5973bcad2462d7bca3c8e
mmmtnez <mmmtnez@cnb.csic.es>
2018-02-02 17:26:20 +0100

    move .em.utils.ccp4_utilities.convert -> .em.convert_header.CCP4.convert and .em.utils.chimera_utilities.convert -> .em.viewers.chimera_utils

--- pyworkflow/em/packages/ccp4/viewer_coot.py
+++ pyworkflow/em/packages/ccp4/viewer_coot.py
@@ -28,11 +28,11 @@ import os
 
 from pyworkflow.em.convert import ImageHandler
 from pyworkflow.em.packages.xmipp3.viewer import XmippViewer, PdbFile
-from pyworkflow.em.utils.chimera_utilities.convert import \
+from pyworkflow.em.viewers.chimera_utils import \
     createCoordinateAxisFile, \
     adaptOriginFromCCP4ToChimera, runChimeraProgram, \
     getProgram, chimeraPdbTemplateFileName
-from pyworkflow.em.utils.ccp4_utilities.convert import cootPdbTemplateFileName
+from pyworkflow.em.convert_header.CCP4.convert import cootPdbTemplateFileName
 from pyworkflow.viewer import DESKTOP_TKINTER, Viewer
 from protocol_coot import CootRefine
 
