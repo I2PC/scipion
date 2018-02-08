8c766f33d31a50c5991d8b1f61697a5fef527db8
Marta Martinez <mmmtnez@users.noreply.github.com>
2018-02-08 16:04:19 +0100

    Update base.py

--- pyworkflow/em/protocol/protocol_import/base.py
+++ pyworkflow/em/protocol/protocol_import/base.py
@@ -85,8 +85,7 @@ class ProtImportFiles(ProtImport):
                            "'#' represents one digit that will be used as "
                            "micrograph ID\n\n"
                            "NOTE: wildcard characters ('*', '?', '#') "
-                           "cannot appear in the actual path.\n\n"
-                           "File extensions accepted: .map, .mrc, .vol., .spi, .xmp")
+                           "cannot appear in the actual path.)
         form.addParam('filesPattern', params.StringParam,
                       label='Pattern', 
                       condition=filesCondition,
