9d78e02e42a8a8b204cb6be883798f86d4da8fa5
JaviMota <jamotga26@gmail.com>
2018-02-12 15:47:09 +0100

    ctfCritFirstMinFirstZeroRatio added again. Issue checked for VPP

--- pyworkflow/em/packages/xmipp3/protocol_ctf_micrographs.py
+++ pyworkflow/em/packages/xmipp3/protocol_ctf_micrographs.py
@@ -54,7 +54,7 @@ class XmippProtCTFMicrographs(em.ProtCTFMicrographs):
 
     _criterion_phaseplate = ("ctfCritFirstZero<5 OR ctfCritMaxFreq>20 OR "
                   "ctfCritfirstZeroRatio<0.9 OR ctfCritfirstZeroRatio>1.1 OR "
-                  "ctfCritCorr13==0 OR "
+                  "ctfCritCorr13==0 OR ctfCritFirstMinFirstZeroRatio>10 OR "
                   "ctfCritNonAstigmaticValidty<=0 OR " 
                   "ctfCritNonAstigmaticValidty>25 OR ctfBgGaussian2SigmaU>70000 "
                   "OR ctfCritIceness>1")
