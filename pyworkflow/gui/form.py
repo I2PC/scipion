c1f23fde1f6e7c1f8ef4326445fb49d0fbeb9a8d
mmmtnez <mmmtnez@cnb.csic.es>
2018-03-05 18:00:41 +0100

    Addition of changes from mm_modeling

--- pyworkflow/gui/form.py
+++ pyworkflow/gui/form.py
@@ -310,6 +310,10 @@ def getObjectLabel(pobj, mapper):
         if not len(label):
             label = '%s.%s' % (prot.getRunName(), extended)
 
+    label = label.replace("\n"," ")
+    if obj is not None:
+        id = obj.getObjId()
+        return label + " (%d)"%obj.getObjId()
     return label
 
     
