bb05a24a716b4d6d369ef92e3a12a908d29ce477
mmmtnez <mmmtnez@cnb.csic.es>
2018-06-28 17:40:11 +0200

    Creation of file

---
+++ pyworkflow/em/packages/phenix/viewer_molprobity.py
@@ -0,0 +1,316 @@
+# **************************************************************************
+# *
+# * Authors:     Roberto Marabini
+# *              Marta Martinez
+# *
+# * Unidad de  Bioinformatica of Centro Nacional de Biotecnologia , CSIC
+# *
+# * This program is free software; you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation; either version 2 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program; if not, write to the Free Software
+# * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+# * 02111-1307  USA
+# *
+# *  All comments concerning this program package may be sent to the
+# *  e-mail address 'scipion@cnb.csic.es'
+# *
+# **************************************************************************
+
+import json
+import os
+from tkMessageBox import showerror
+from protocol_molprobity import PhenixProtRunMolprobity
+from pyworkflow.protocol.params import LabelParam, EnumParam
+from pyworkflow.viewer import DESKTOP_TKINTER, WEB_DJANGO, ProtocolViewer
+from pyworkflow.em.viewer import TableView
+import collections
+import glob
+from PIL import Image
+from convert import runPhenixProgram
+
+
+def errorWindow(tkParent, msg):
+    try:
+        # if tkRoot is null the error message may be behind
+        # other windows
+        showerror("Error",  # bar title
+                  msg,  # message
+                  parent=tkParent)
+    except:
+        print("Error:", msg)
+
+
+class PhenixProtRunMolprobityViewer(ProtocolViewer):
+    """ Viewer for Phenix program EMRinger
+    """
+    _label = 'MolProbity Viewer'
+    _environments = [DESKTOP_TKINTER, WEB_DJANGO]
+    _targets = [PhenixProtRunMolprobity]
+
+    def __init__(self,  **kwargs):
+        ProtocolViewer.__init__(self,  **kwargs)
+        MOLPROBITYOUTFILENAME = self.protocol._getExtraPath(
+            self.protocol.MOLPROBITYOUTFILENAME)
+        self._parseFile(MOLPROBITYOUTFILENAME)
+
+    def _defineParams(self, form):
+        form.addSection(label='Visualization of MolProbity results')
+        form.addParam('showFinalResults', LabelParam,
+                      label="Summary Table of Results",
+                      help="Validation of protein geometry. Statistics "
+                           "computed by the "
+                           "PHENIX package using the same distributions as "
+                           "the MolProbity web server."
+                           "\n\nRamachandran outliers: Outlier "
+                           "residues that show an unusual "
+                           "combination of their phi and psi "
+                           "torsion angles. Ramachandran outlier score is "
+                           "computed as the percentage of Ramachandran "
+                           "outliers regarding the total number of residues "
+                           "in the entry showing outlier assessment."
+                           "\n\nRamachandran "
+                           "favored: Residues that show an normal "
+                           "combination of their phi and psi "
+                           "torsion angles. Ramachandran favored score is "
+                           "computed as the percentage of Ramachandran "
+                           "outliers regarding the total number of residues "
+                           "in the entry showing outlier assessment. Between "
+                           "the favored and outlier regions in the "
+                           "Ramachandran plot, there is a small region of "
+                           "allowed residues.\n\nRotamer outliers: Residues "
+                           "that adopt unusual chi torsion angles "
+                           "(non-rotameric), used to describe "
+                           "the conformation of protein sidechains. The "
+                           "score of rotamer outliers is computed as "
+                           "the percentage of residues with non-rotameric "
+                           "angles.\n\nC-beta outliers: Residues that show an "
+                           "unusual deviation (higher than 0.25 A) of the "
+                           "beta carbon from its ideal position. "
+                           "This deviation of beta carbon "
+                           "indicates incompatibilities between sidechain and "
+                           "backbone.\n\nClashscore: Score associated to the "
+                           "number of pairs of non-bonded atoms in the model "
+                           "that are unusually close to each other. These "
+                           "clashing atoms show unfavorable steric overlaps "
+                           "of van der Waals shells. Steric clashes in "
+                           "proteins are defined based on the Van der Waals "
+                           "repulsion energy of the clashing atoms. "
+                           "The clashscore is computed as the "
+                           "number of serious clashes per 1000 atoms. "
+                           "\n\nRMS (bonds): Root-mean-square deviation of "
+                           "macromolecular bond lengths. "
+                           "\n\nRMS (angles): Root-mean-square deviation of "
+                           "macromolecular bond angles. "
+                           "\n\nOverall score: Score that represents the "
+                           "experimental resolution expected for a model of "
+                           "this quality; ideally the score should be lower "
+                           "than the actual resolution.\n")
+
+        form.addParam('showBLrestraints', LabelParam,
+                      label="Basic geometry: Bond length restraints",
+                      help="Statistics across all thresholds\n\n")
+        #
+        # self.thresList = [str("%0.3f" % thres) for thres in self.dataDict[
+        #     '_thresholds']]
+        # _maxScoreIndex = self.dataDict['_maxScoreIndex']
+        # form.addParam('threshold', EnumParam,
+        #               choices=self.thresList,
+        #               default=_maxScoreIndex,
+        #               label="Density threshold",
+        #               help="Choose one of the 20 map density cutoff values "
+        #                    "used to compute the percentage of rotameric "
+        #                    "residues. The Optimal threshold, at which the "
+        #                    "maximum EMRinger score was obtained, "
+        #                    "is shown by default.\n"
+        #               )
+        # form.addParam('showPeakCount', LabelParam,
+        #               label="Peak count for the selected density threshold",
+        #               help="Histograms for rotameric (blue) and non-rotameric "
+        #                    "(red) residues at the selected map density.")
+        # self.chainList = self.dataDict['_chains']
+        # form.addParam('chain', EnumParam,
+        #               choices=self.chainList,
+        #               default=0,
+        #               label="Chain",
+        #               help="Choose one of the model chains. The first chain "
+        #                    "is shown by default.")
+        # form.addParam('showRollingWindows', LabelParam,
+        #               label="Rolling window for the selected chain.",
+        #               help="Visualize the rolling window EMRinger analysis of "
+        #                    "your selected chain to distinguish regions of "
+        #                    "improved model quality.This analysis was "
+        #                    "performed on rolling sliding 21-residue windows "
+        #                    "along the primary sequence of proteins.")
+        # self.residueList = self.dataDict['_residues_format']
+        # form.addParam('residue', EnumParam,
+        #               choices=self.residueList,
+        #               default=0,
+        #               label="Residue",
+        #               help="Choose one of the gamma-carbon-containing "
+        #                    "residues (at least with one Chi angle) "
+        #                    "located in one of the chains in the "
+        #                    "position indicated. ")
+        # form.addParam('showRingerResults', LabelParam,
+        #               label="Ringer results for the selected residue.",
+        #               help="Individual plots for each Chi angle of the "
+        #                    "selected residue. Numeric values are showed in "
+        #                    "the extra/*.csv file.")
+
+    def _getVisualizeDict(self):
+        return {
+            'showFinalResults': self._visualizeFinalResults,
+            'showBLrestraints': self._showBLrestraints,
+            # 'showPeakCount': self._showPeakCount,
+            # 'showRollingWindows': self._showRollingWindows,
+            # 'showRingerResults': self._showRingerResults
+        }
+
+    def _visualizeFinalResults(self, e=None):
+
+        headerList = ['statistic', 'value']
+        dataList = []
+        for k, v in self.dictSummary.iteritems():
+            # if k[0] == "_":
+            #     continue
+            if isinstance(v, int):
+                dataList.append((k, v))
+            elif isinstance(v, float):
+                dataList.append((k, "%0.4f" % v))
+
+        if not dataList:
+            errorWindow(self.getTkRoot(), "No data available")
+            return
+
+        TableView(headerList=headerList,
+                  dataList=dataList,
+                  mesg="Model Final Statistics\n",
+                  title="Molprobity: Final Results Summary",
+                  height=len(dataList), width=250, padding=40)
+
+    def _showBLrestraints(self, e=None):
+        pass
+#     def showImage(self, fileName):
+#         fileName = os.path.join(self.plots, fileName)
+#         img = Image.open(fileName)
+#         img.show()
+#
+#     def _showThresholdScan(self, e=None):
+#         self.showImage(self.EMRINGERTOTALTHRESH)
+#
+#     def _showPeakCount(self, e=None):
+#         threshold_index = int(self.threshold)
+#         fileName = "%s.histogram.png" % self.thresList[threshold_index]
+#         self.showImage(fileName)
+#
+#     def _showRollingWindows(self, e=None):
+#         chain_index = int(self.chain)
+#         fileName = "%s_rolling.png" % self.chainList[chain_index]
+#         self.showImage(fileName)
+#
+#     def _showRingerResults(self, e=None):
+#         i = int(self.residue)
+#         index = int(self.dataDict['_residues_dict'][self.residueList[i]])
+#         # emringer main file
+#         mainDataFile = glob.glob(self.protocol._getExtraPath(
+#             "*_emringer.pkl"))[0]
+#         command = """from mmtbx.ringer.em_rolling import easy_pickle
+# import matplotlib.pyplot as plt
+#
+# # process file '%s'
+# file_name='%s'
+# ringer_results = easy_pickle.load(file_name)
+# figure = plt.figure() #  (figsize=(20,1000))
+#
+# def show_residue (residue, show_background_boxes=True) :
+#     subplots = []
+#     for i in range(1, residue.n_chi + 1) :
+#         chi = residue.get_angle(i)
+#         if (chi is None) : continue
+#         if (len(subplots) > 0) :
+#             p = figure.add_subplot(4, 1, i, sharex=subplots[0])
+#         else:
+#             p = figure.add_subplot(4, 1, i)
+#             p.set_title(residue.format())
+#         p.set_position([0.15, 0.725 - 0.225*(i-1), 0.8, 0.225])
+#         x = [ k*chi.sampling for k in range(len(chi.densities)) ]
+#         p.plot(x, chi.densities, 'r-', linewidth=1)
+#         if (chi.fofc_densities is not None) :
+#             p.plot(x, chi.fofc_densities, linestyle='--', color=[0.5,0.0,1.0])
+#         p.axvline(chi.angle_current, color='b', linewidth=2, linestyle='--')
+#         p.axhline(0, color=(0.4,0.4,0.4), linestyle='--', linewidth=1)
+#         if show_background_boxes:
+#             p.axhspan(0.3,1,facecolor="green",alpha=0.5)
+#             p.axhspan(-1,0.3,facecolor="grey",alpha=0.5)
+#         p.set_xlim(0,360)
+#         p.set_ylabel("Rho")
+#         p.set_xlabel("Chi" + str(i))
+#         subplots.append(p)
+#         plt.subplots_adjust(left = 0.18, bottom = 0.00, right = 0.94,
+#         top = 0.93,
+#                         wspace = 0.80, hspace = 0.43)
+#     plt.tight_layout()
+#     plt.show()
+# index = %d
+# show_residue(ringer_results[index])
+# """ % (mainDataFile, mainDataFile, index)
+#
+#         with open(self.EMRINGERSUBPLOTSFILENAME, "w") as f:
+#             f.write(command)
+#         # execute file with phenix.python
+#         runPhenixProgram("", self.EMRINGERSUBPLOTSFILENAME)
+    def _parseFile(self, fileName):
+        self.dictSummary = collections.OrderedDict()
+        self.dictBLRestraints = collections.OrderedDict()
+        with open(fileName) as f:
+            line = f.readline()
+            while line:
+                words = line.strip().split()
+                if len(words) > 1:
+                    if (words[0] == 'Ramachandran' and words[1] == 'outliers'):
+                        self.dictSummary['Ramachandran outliers'] = float(words[3])
+                    elif (words[0] == 'favored' and words[1] == '='):
+                        self.dictSummary['Ramachandran favored'] = float(words[2])
+                    elif (words[0] == 'Rotamer' and words[1] == 'outliers'):
+                        self.dictSummary['Rotamer outliers'] = float(words[3])
+                    elif (words[0] == 'C-beta' and words[1] == 'deviations'):
+                        self.dictSummary['C-beta outliers'] = int(words[3])
+                    elif (words[0] == 'Clashscore' and words[1] == '='):
+                        self.dictSummary['Clashscore'] = float(words[2])
+                    elif (words[0] == 'RMS(bonds)' and words[1] == '='):
+                        self.dictSummary['RMS (bonds)'] = float(words[2])
+                    elif (words[0] == 'RMS(angles)' and words[1] == '='):
+                        self.dictSummary['RMS (angles)'] = float(words[2])
+                    elif (words[0] == 'MolProbity' and words[1] == 'score'):
+                        self.dictSummary['Overall score'] = float(words[3])
+                    elif (words[0] == 'bond' and words[1] == ':'):
+                        self.dictBLRestraints['Number of restraints'] = int(words[4])
+                        self.dictBLRestraints['RMS (deviation)'] = float(words[2])
+                        self.dictBLRestraints['Max deviation'] = float(words[3])
+                    elif (words[0] == '----------Bond' and words[1] ==
+                        'lengths----------'):
+                        f.readline()
+                        line = f.readline()
+                        words = line.strip().split()
+                        if (words[0] == 'All' and words[1] == 'restrained'):
+                            self.dictBLRestraints['Number of outliers > 4sigma'] = \
+                                int(0)
+                        elif (words[0] == 'atoms'):
+                            cnt = 1
+                            line = f.readline()
+                            words = line.strip().split()
+                            while (len(words) > 1 and words[1] == 'DMS'):
+                                cnt += 1
+                                line = f.readline()
+                                words = line.strip().split()
+                            self.dictBLRestraints['Number of outliers > 4sigma'] = \
+                                int(cnt / 2)
+                line = f.readline()
