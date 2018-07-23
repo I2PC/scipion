5b07edff588dbc0202f735186537faa97aa8cd93
D. Maluenda <dmaluenda@cnb.csic.es>
2018-07-23 15:23:20 +0200

    Cleaning software/em/xmipp and ignoring in git

--- software/em/xmipp/WikiPHP/Download/connectionStrings.php
+++
@@ -1,4 +0,0 @@
-<?php
-$pgConnectStr1="host=xxxxxxxxxxxxxx port=xxxx dbname=xxxxx user=xxxxxxx password=xxxxxxxxx";
-$pgConnectStr2="host=xxxxxxxxxxxxxx port=xxxx dbname=xxxxx user=xxxxxxxxxx password=xxxxxxxx";
-?>
--- software/em/xmipp/external/mirt/readme.txt
+++
@@ -1,4 +0,0 @@
-The MIRT Toolbox has been written by Andriy Myronenko and is publicly available at
-
-https://sites.google.com/site/myronenko/research/mirt
-
