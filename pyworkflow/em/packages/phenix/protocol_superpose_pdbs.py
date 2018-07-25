0df237e2f3a51ad468b8c4b1be412b8389b3a811
mmmtnez <mmmtnez@cnb.csic.es>
2018-07-25 07:34:55 +0200

    Imported runPhenixProgram, getProgram2 methods from convert file and addition of _parseLogFile utils method to fill up the summary with RMSD values

--- pyworkflow/em/packages/phenix/protocol_superpose_pdbs.py
+++ pyworkflow/em/packages/phenix/protocol_superpose_pdbs.py
@@ -1,7 +1,8 @@
 # **************************************************************************
 # *
 # * Authors:     Carlos Oscar Sorzano (coss@cnb.csic.es)
-# *
+# *              Marta Martinez (mmmtnez@cnb.csic.es)
+# *              Roberto Marabini (roberto@cnb.csic.es)
 # *
 # * This program is free software; you can redistribute it and/or modify
 # * it under the terms of the GNU General Public License as published by
@@ -24,14 +25,13 @@
 # **************************************************************************
 
 import os
-import glob
-import json
+
 
 from pyworkflow.em.protocol import EMProtocol
 from pyworkflow.protocol.params import PointerParam
-from convert import runPhenixProgram, getProgram
-from pyworkflow.protocol.constants import LEVEL_ADVANCED
+from convert import runPhenixProgram, getProgram2
 from pyworkflow.em import PdbFile
+import collections
 
 
 class PhenixProtRunSuperposePDBs(EMProtocol):
@@ -39,12 +39,14 @@ class PhenixProtRunSuperposePDBs(EMProtocol):
     _label = 'superpose pdbs'
     _program = ""
     # _version = VERSION_1_2
+    SUPERPOSE = 'superpose_pdbs.py'
 
     # --------------------------- DEFINE param functions -------------------
     def _defineParams(self, form):
         form.addSection(label='Input')
-        form.addParam('inputStructureFixed', PointerParam, pointerClass="PdbFile",
-                      label='Fixed atomic structure', allowsNull=True,
+        form.addParam('inputStructureFixed', PointerParam,
+                      pointerClass="PdbFile",
+                      label='Fixed atomic structure', important=True,
                       help="The moving PDB will be aligned to the fixed one")
         form.addParam('inputStructureMoving', PointerParam,
                       pointerClass="PdbFile",
@@ -60,14 +62,18 @@ class PhenixProtRunSuperposePDBs(EMProtocol):
 
     def runSuperposePDBsStep(self):
         args = os.path.abspath(self.inputStructureFixed.get().getFileName())
-        args += " "+os.path.abspath(self.inputStructureMoving.get().getFileName())
-        self.runJob(getProgram("phenix.superpose_pdbs"), args, cwd=self._getExtraPath())
+        args += " "
+        args += os.path.abspath(self.inputStructureMoving.get().getFileName())
+
+        runPhenixProgram(getProgram2(self.SUPERPOSE), args,
+                         cwd=self._getExtraPath())
 
     def createOutputStep(self):
         fnPdb = os.path.basename(self.inputStructureMoving.get().getFileName())
         pdb = PdbFile()
         pdb.setFileName(self._getExtraPath(fnPdb+"_fitted.pdb"))
-        pdb.setVolume(self.inputStructureFixed.get().getVolume())
+        if self.inputStructureFixed.get().getVolume() is not None:
+            pdb.setVolume(self.inputStructureFixed.get().getVolume())
         self._defineOutputs(outputPdb=pdb)
         self._defineSourceRelation(self.inputStructureFixed.get(), pdb)
         self._defineSourceRelation(self.inputStructureMoving.get(), pdb)
@@ -77,9 +83,9 @@ class PhenixProtRunSuperposePDBs(EMProtocol):
     def _validate(self):
         errors = []
         # Check that the program exists
-        program = getProgram("phenix.superpose_pdbs")
+        program = getProgram2(self.SUPERPOSE)
         if not os.path.exists(program):
-            errors.append("Cannot find phenix.superpose_pdbs")
+            errors.append("Cannot find " + program)
 
         # If there is any error at this point it is related to config variables
         if errors:
@@ -89,5 +95,40 @@ class PhenixProtRunSuperposePDBs(EMProtocol):
             if program is not None:
                 errors.append("Current values:")
                 errors.append("PHENIX_HOME = %s" % os.environ['PHENIX_HOME'])
+                errors.append("SUPERPOSE = %s" % self.SUPERPOSE)
 
         return errors
+
+    def _summary(self):
+        summary = []
+        try:
+            logFile = os.path.abspath(self._getLogsPath()) + "/run.stdout"
+            self._parseLogFile(logFile)
+            summary.append("RMSD between fixed and moving atoms (start): " +
+                       str(self.dictRMSD['startRMSD']))
+            summary.append("RMSD between fixed and moving atoms (final): " +
+                       str(self.dictRMSD['finalRMSD']))
+        except:
+            summary.append("RMSD not yet computed")
+        summary.append(
+            "http://www.phenix-online.org/documentation/superpose_pdbs.htm")
+        summary.append("Peter Zwart, Pavel Afonine, Ralf W. Grosse-Kunstleve")
+
+        return summary
+
+    # --------------------------- UTILS functions --------------------------
+
+    def _parseLogFile(self, logFile):
+        self.dictRMSD = collections.OrderedDict()
+        with open(logFile) as f:
+            line = f.readline()
+            while line:
+                words = line.strip().split()
+                if len(words) > 1:
+                    if (words[0] == 'RMSD' and words[1] == 'between'and
+                        words[6] == '(start):'):
+                        self.dictRMSD['startRMSD'] = float(words[7])
+                    elif (words[0] == 'RMSD' and words[1] == 'between' and
+                          words[6] == '(final):'):
+                        self.dictRMSD['finalRMSD'] = float(words[7])
+                line = f.readline()
