625078e9f7b70df32ea1332d81f58fe31d19eb07
mmmtnez <mmmtnez@cnb.csic.es>
2018-07-26 11:29:21 +0200

    Replacement of getProgram1 and getProgram2 methods by getProgram

--- pyworkflow/em/packages/phenix/convert.py
+++ pyworkflow/em/packages/phenix/convert.py
@@ -34,10 +34,25 @@ TODO:
 import os
 import pyworkflow.utils as pwutils
 
+#python used to run phenix scripts
 PHENIX_PYTHON = 'phenix.python '  # keep the ending space
+
+#phenix binaries are in several directories
 PHENIX_SCRIPT_PATH1 = 'modules/cctbx_project/mmtbx/command_line'
 PHENIX_SCRIPT_PATH2 = 'modules/phenix/phenix/command_line'
 
+# list of phenix scripts and corresponding binary directory
+SUPERPOSE = 'superpose_pdbs.py'
+REALSPACEREFINE = 'real_space_refine.py'
+MOLPROBITY = 'molprobity.py'
+EMRINGER = 'emringer.py'
+
+mapBinarytoDirectory ={
+    REALSPACEREFINE : PHENIX_SCRIPT_PATH2,
+    SUPERPOSE : PHENIX_SCRIPT_PATH2,
+    MOLPROBITY : PHENIX_SCRIPT_PATH1,
+    EMRINGER : PHENIX_SCRIPT_PATH1
+}
 
 def getEnviron(first=True):
     environ = pwutils.Environ(os.environ)
@@ -67,21 +82,12 @@ def runPhenixProgram(program, args=None, extraEnvDict=None, cwd=None):
     pwutils.runJob(None, program, args, env=env, cwd=cwd)
 
 
-def getProgram1(progName):
+def getProgram(progName):
     """ Return the program binary that will be used. """
     if 'PHENIX_HOME' not in os.environ:
         return None
 
     return os.path.join(os.environ['PHENIX_HOME'],
-                        PHENIX_SCRIPT_PATH1,
-                        os.path.basename(progName))
-
-def getProgram2(progName):
-    """ Return the program binary that will be used. """
-    if 'PHENIX_HOME' not in os.environ:
-        return None
-
-    return os.path.join(os.environ['PHENIX_HOME'],
-                        PHENIX_SCRIPT_PATH2,
+                        mapBinarytoDirectory[progName],
                         os.path.basename(progName))
 
