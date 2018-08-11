e2e94aef487342a2b28994c2aed185e62b1359e2
mmmtnez <mmmtnez@cnb.csic.es>
2018-08-11 17:40:57 +0200

    Simplification of code so that we have included the base protocol protocol_refinement_base

--- pyworkflow/em/packages/phenix/protocol_real_space_refine.py
+++ pyworkflow/em/packages/phenix/protocol_real_space_refine.py
@@ -26,13 +26,8 @@
 # **************************************************************************
 
 import os
-import glob
-import math
-from pyworkflow.object import String, Float, Integer
-from pyworkflow.em.protocol import EMProtocol
-from pyworkflow.protocol.params import BooleanParam, PointerParam, FloatParam, IntParam
-from convert import runPhenixProgram, getProgram, REALSPACEREFINE
-from pyworkflow.em.headers import adaptFileToCCP4, START
+from pyworkflow.protocol.params import BooleanParam,  IntParam
+from convert import runPhenixProgram, getProgram, REALSPACEREFINE, MOLPROBITY
 from pyworkflow.protocol.constants import LEVEL_ADVANCED
 from pyworkflow.em import PdbFile
 from protocol_refinement_base import PhenixProtRunRefinementBase
@@ -50,10 +45,9 @@ class PhenixProtRunRSRefine(PhenixProtRunRefinementBase):
     # _version = VERSION_1_2
     REALSPACEFILE = 'real_space.mrc'
 
-
     # --------------------------- DEFINE param functions -------------------
     def _defineParams(self, form):
-        PhenixProtRunRefinementBase._defineParams(self, form)
+        super(PhenixProtRunRSRefine, self)._defineParams(form)
         form.addParam("doSecondary", BooleanParam, label="Secondary structure",
                       default=False, expertLevel=LEVEL_ADVANCED,
                       help="Set to TRUE to use secondary structure "
@@ -65,48 +59,78 @@ class PhenixProtRunRSRefine(PhenixProtRunRefinementBase):
                            "which model geometry or/and model-to-map fit is "
                            "poor the use of more macro-cycles could be "
                            "helpful.\n")
-        
+
     # --------------------------- INSERT steps functions ---------------
     def _insertAllSteps(self):
         self._insertFunctionStep('convertInputStep', self.REALSPACEFILE)
         self._insertFunctionStep('runRSrefineStep', self.REALSPACEFILE)
+        self._insertFunctionStep('runMolprobityStep', self.REALSPACEFILE)
         self._insertFunctionStep('createOutputStep')
 
     # --------------------------- STEPS functions --------------------------
     def runRSrefineStep(self, tmpMapFile):
-        args = os.path.abspath(self.inputStructure.get().getFileName())
+        pdb = os.path.abspath(self.inputStructure.get().getFileName())
+        args = "" + pdb
         vol = os.path.abspath(self._getExtraPath(tmpMapFile))
         args += " " + vol
         args += " resolution=%f" % self.resolution
-        args += " secondary_structure.enabled=%s"%self.doSecondary
-        args += " run=minimization_global+local_grid_search+morphing+" \
-                "simulated_annealing"
-        args += " macro_cycles=%d"%self.macroCycles
+        args += " secondary_structure.enabled=%s" % self.doSecondary
+        # args += " run=minimization_global+local_grid_search+morphing+" \
+        #         "simulated_annealing"
+        args += " macro_cycles=%d" % self.macroCycles
+        args += " write_pkl_stats=True"
+
         runPhenixProgram(getProgram(REALSPACEREFINE), args,
                          cwd=self._getExtraPath())
 
+    def runMolprobityStep(self, tmpMapFile):
+        # PDBx/mmCIF
+        outPdbName = self._getPdbRSRefineOutput()
+        args = ""
+        args += os.path.abspath(outPdbName)
+        # starting volume (.mrc)
+        vol = os.path.abspath(self._getExtraPath(tmpMapFile))
+        args += " "
+        args += "map_file_name=%s" % vol
+        args += " "
+        args += "d_min=%f" % self.resolution.get()
+        args += " "
+        args += "pickle=True"
+        # args += " wxplots=True" # TODO: Avoid the direct opening of plots
+        # script with auxiliary files
+        runPhenixProgram(getProgram(MOLPROBITY), args,
+                         cwd=self._getExtraPath())
+
     def createOutputStep(self):
-        fnPdb = os.path.basename(self.inputStructure.get().getFileName())
+        outPdbName = self._getPdbRSRefineOutput()
         pdb = PdbFile()
-        pdb.setFileName(self._getExtraPath(
-            fnPdb.replace(".pdb", "_real_space_refined.pdb")))
+        pdb.setFileName(outPdbName)
+
         if self.inputVolume.get() is not None:
             pdb.setVolume(self.inputVolume.get())
         else:
-            pdb.setVolume(self.inputStructure.get().getVolume())
+            self.pdb.setVolume(self.inputStructure.get().getVolume())
         self._defineOutputs(outputPdb=pdb)
         self._defineSourceRelation(self.inputStructure.get(), pdb)
         if self.inputVolume.get() is not None:
             self._defineSourceRelation(self.inputVolume.get(), pdb)
 
-        fileName = glob.glob(self._getExtraPath("*.log"))[0]
-        self._parseFile(fileName)
+        # fileName = glob.glob(self._getExtraPath("*.log"))[0]
+        # self._parseFile(fileName)
+
+        MOLPROBITYOUTFILENAME = self._getExtraPath(
+            self.MOLPROBITYOUTFILENAME)
+        self._parseFile(MOLPROBITYOUTFILENAME)
         self._store()
 
     # --------------------------- INFO functions ---------------------------
 
     def _validate(self):
         errors = self.validateBase(REALSPACEREFINE, 'REALSPACEREFINE')
+
+        # Check that the input volume exist
+        if self._getInputVolume() is None:
+            errors.append("Error: You should provide a volume.\n")
         return errors
 
     def _citations(self):
@@ -120,51 +144,9 @@ class PhenixProtRunRSRefine(PhenixProtRunRefinementBase):
         return summary
 
     # --------------------------- UTILS functions --------------------------
-    def _parseFile(self, fileName):
-        with open(fileName) as f:
-            line = f.readline()
-            while line:
-                words = line.strip().split()
-                if len(words) > 1:
-                    if (words[0] == 'Final' and words[1] == 'info'
-                        and words[2] == '(overall)'):
-                        line = f.readline()
-                        while line:
-                            words = line.strip().split()
-                            if len(words) > 1:
-                                if (words[0] == 'all-atom' and
-                                    words[1] =='clashscore'):
-                                    self.clashscore = Float(words[3])
-                                elif (words[0] == 'outliers'and
-                                      words[1] == ':'):
-                                    self.ramachandranOutliers = Float(words[2])
-                                elif (words[0] == 'favored'and
-                                      words[1] == ':'):
-                                    self.ramachandranFavored = Float(words[2])
-                                elif (words[0] == 'rotamer' and
-                                      words[1] == 'outliers'):
-                                    self.rotamerOutliers = Float(words[3])
-                                elif (words[0] == 'cbeta' and
-                                      words[1] == 'deviations'):
-                                    self.cbetaOutliers = Integer(words[3])
 
-                            line = f.readline()
-                line = f.readline()
-        self.molprobity_score(self.clashscore.get(),
-                              self.rotamerOutliers.get(),
-                              self.ramachandranFavored.get()
-                              )
-
-    def molprobity_score(self, clashscore, rota_out, rama_fav):
-        """
-        Calculate the overall Molprobity score, as described here:
-          http://www.ncbi.nlm.nih.gov/pmc/articles/PMC2877634/?tool=pubmed
-          http://kinemage.biochem.duke.edu/suppinfo/CASP8/methods.html
-        """
-        if (clashscore >= 0) and (rota_out >= 0) and (rama_fav >= 0):
-            rama_iffy = 100. - rama_fav
-            self.overallScore = Float(((0.426 * math.log(1 + clashscore)) +
-                       (0.33 * math.log(1 + max(0, rota_out - 1))) +
-                       (0.25 * math.log(1 + max(0, rama_iffy - 2)))) + 0.5)
-        else:
-            return -1  # FIXME prevents crashing on RNA
+    def _getPdbRSRefineOutput(self):
+        inPdbName = os.path.basename(self.inputStructure.get().getFileName())
+        outPdbName = self._getExtraPath(
+            inPdbName.replace(".pdb", "_real_space_refined.pdb"))
+        return outPdbName
