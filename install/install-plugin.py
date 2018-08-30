26039da55e55eb3ef9ade731ffdd71a7b1adf3b8
pconesa <p.conesa.mingo@gmail.com>
2018-08-30 08:39:32 +0200

    pass -j param correctly.

--- install/install-plugin.py
+++ install/install-plugin.py
@@ -177,7 +177,10 @@ elif parsedArgs.mode == MODE_UNINSTALL_PLUGIN:
 elif parsedArgs.mode == MODE_INSTALL_BINS:
     plugin = PluginInfo(parsedArgs.pluginName, remote=False)
     if plugin.isInstalled():
-        parsedArgs.binName.append(["-j", parsedArgs.j])
+
+        import time
+        time.sleep(10)
+        parsedArgs.binName.extend(["-j", parsedArgs.j])
         plugin.installBin(args=parsedArgs.binName)
 
 elif parsedArgs.mode == MODE_UNINSTALL_BINS:
