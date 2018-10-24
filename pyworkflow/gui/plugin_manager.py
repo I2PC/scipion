e91b1efc939100024695e1fa28d99afc972f51bf
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2018-10-24 22:38:14 +0200

    Use a Scrollable class from form.py

--- pyworkflow/gui/plugin_manager.py
+++ pyworkflow/gui/plugin_manager.py
@@ -38,7 +38,7 @@ from text import TaggedText, openTextFileEditor
 from widgets import Button, HotButton
 from pyworkflow.config import MenuConfig
 from pyworkflow.utils.properties import Icon
-#from pyworkflow.gui.form import *
+from pyworkflow.gui.form import *
 
 
 class CheckboxTreeview(ttk.Treeview):
@@ -133,50 +133,6 @@ class CheckboxTreeview(ttk.Treeview):
             else:
                 if "checked" in tags:
                     self.uncheck_descendant(item)
-
-class VerticalScrolledFrame(tk.Frame):
-    """A pure Tkinter scrollable frame that actually works!
-    * Use the 'interior' attribute to place widgets inside the scrollable frame
-    * Construct and pack/place/grid normally
-    * This frame only allows vertical scrolling
-
-    """
-    def __init__(self, parent, *args, **kw):
-        tk.Frame.__init__(self, parent, *args, **kw)
-
-        # create a canvas object and a vertical scrollbar for scrolling it
-        vscrollbar = tk.Scrollbar(self, orient=tk.VERTICAL)
-        vscrollbar.pack(fill=tk.Y, side=tk.RIGHT, expand=tk.FALSE)
-        canvas = Canvas(self, bd=0, highlightthickness=0,
-                        yscrollcommand=vscrollbar.set)
-        canvas.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.TRUE)
-        vscrollbar.config(command=canvas.yview)
-
-        # reset the view
-        canvas.xview_moveto(0)
-        canvas.yview_moveto(0)
-
-        # create a frame inside the canvas which will be scrolled with it
-        self.interior = interior = tk.Frame(canvas)
-        interior_id = canvas.create_window(0, 0, window=interior,
-                                           anchor=tk.NW)
-
-        # track changes to the canvas and frame width and sync them,
-        # also updating the scrollbar
-        def _configure_interior(event):
-            # update the scrollbars to match the size of the inner frame
-            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
-            canvas.config(scrollregion="0 0 %s %s" % size)
-            if interior.winfo_reqwidth() != canvas.winfo_width():
-                # update the canvas's width to fit the inner frame
-                canvas.config(width=interior.winfo_reqwidth())
-        interior.bind('<Configure>', _configure_interior)
-
-        def _configure_canvas(event):
-            if interior.winfo_reqwidth() != canvas.winfo_width():
-                # update the inner frame's width to fill the canvas
-                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
-        canvas.bind('<Configure>', _configure_canvas)
 
 class PluginBrowser(tk.Frame):
     """ This class will implement a frame.
