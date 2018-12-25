b2d6fbc8bad8e675e730cf84f2ae731543a9ee14
Jose Miguel de la Rosa Trevin <delarosatrevin@gmail.com>
2018-12-25 23:03:03 +0100

    Moved project related scripts under pyworkflow/project/scripts

--- scripts/scipionbox_wizard_demo.py
+++ scripts/scipionbox_wizard_demo.py
@@ -252,7 +252,7 @@ class BoxWizardView(tk.Frame):
         projectName = self._getValue(PROJECT_NAME)
 
         scipion = pw.getScipionScript()
-        scriptsPath = pw.join('..', 'scripts')
+        scriptsPath = pw.join('pyworkflow', 'project', 'scripts')
 
         # Download the required data
         # pwutils.runCommand(scipion +
